a2e34060640eb5cfd97ad774aa1dac372cc0b99b
Jonas Boner <jonas@jonasboner.com>
2009-02-17 21:05:07 +0100

    completed Guice API for ActiveObjects

--- kernel/src/main/scala/ActiveObject.scala
+++ kernel/src/main/scala/ActiveObject.scala
@@ -17,46 +17,42 @@ class ActiveObjectInvocationTimeoutException(msg: String) extends ActiveObjectEx
 /**
  * @author <a href="http://jonasboner.com">Jonas Bon&#233;r</a>
  */
+class ActiveObjectFactory {
+  def newInstance[T](intf: Class[_], proxy: ActiveObjectProxy): T = ActiveObject.newInstance(intf, proxy)
+
+  def supervise(restartStrategy: RestartStrategy, components: JList[Worker]): Supervisor =
+    ActiveObject.supervise(restartStrategy, components.toArray.toList.asInstanceOf[List[Worker]])  
+}
+
+
+/**
+ * @author <a href="http://jonasboner.com">Jonas Bon&#233;r</a>
+ */
 object ActiveObject {
-
-  def newInstance[T](intf: Class[T] forSome {type T}, target: AnyRef, timeout: Int): T = {
-    val proxy = new ActiveObjectProxy(target, timeout)
-    supervise(proxy)
-    newInstance(intf, proxy)
-  }
-
-  def newInstance[T](intf: Class[T] forSome {type T}, proxy: ActiveObjectProxy): T = {
-    Proxy.newProxyInstance(
-      proxy.target.getClass.getClassLoader,
-      Array(intf),
-      proxy).asInstanceOf[T]
-  }
+   def newInstance[T](intf: Class[_], proxy: ActiveObjectProxy): T = {
+     Proxy.newProxyInstance(
+       intf.getClassLoader,
+       Array(intf),
+       proxy).asInstanceOf[T]
+   }
 
   def supervise(restartStrategy: RestartStrategy, components: List[Worker]): Supervisor = {
   	object factory extends SupervisorFactory {
-      override def getSupervisorConfig: SupervisorConfig = {
-        SupervisorConfig(restartStrategy, components)
-      }
+      override def getSupervisorConfig = SupervisorConfig(restartStrategy, components)
     }
     val supervisor = factory.newSupervisor
     supervisor ! scala.actors.behavior.Start
     supervisor
   }
-
-  private def supervise(proxy: ActiveObjectProxy): Supervisor = 
-    supervise(
-      RestartStrategy(OneForOne, 5, 1000),
-      Worker(
-        proxy.server,
-        LifeCycle(Permanent, 100))
-      :: Nil)
 }
 
 /**
  * @author <a href="http://jonasboner.com">Jonas Bon&#233;r</a>
  */
-class ActiveObjectProxy(val target: AnyRef, val timeout: Int) extends InvocationHandler {
+class ActiveObjectProxy(val intf: Class[_], val target: Class[_], val timeout: Int) extends InvocationHandler {
   private val oneway = classOf[scala.actors.annotation.oneway]
+  private var targetInstance: AnyRef = _
+  private[akka] def setTargetInstance(instance: AnyRef) = targetInstance = instance
 
   private[ActiveObjectProxy] object dispatcher extends GenericServer {
     override def body: PartialFunction[Any, Unit] = {
@@ -72,10 +68,10 @@ class ActiveObjectProxy(val target: AnyRef, val timeout: Int) extends Invocation
     }
   }
 
-  private[akka] val server = new GenericServerContainer(target.getClass.getName, () => dispatcher)
+  private[akka] val server = new GenericServerContainer(target.getName, () => dispatcher)
   server.setTimeout(timeout)
   
-  def invoke(proxy: AnyRef, m: Method, args: Array[AnyRef]): AnyRef = invoke(Invocation(m, args, target))
+  def invoke(proxy: AnyRef, m: Method, args: Array[AnyRef]): AnyRef = invoke(Invocation(m, args, targetInstance))
 
   def invoke(invocation: Invocation): AnyRef =  {
     if (invocation.method.isAnnotationPresent(oneway)) server ! invocation
@@ -92,9 +88,11 @@ class ActiveObjectProxy(val target: AnyRef, val timeout: Int) extends Invocation
  * @author <a href="http://jonasboner.com">Jonas Bon&#233;r</a>
  */
 case class Invocation(val method: Method, val args: Array[Object], val target: AnyRef) {
+  method.setAccessible(true);
+  
   def invoke: AnyRef = method.invoke(target, args: _*)
 
-  override def toString: String = "Invocation [method: " + method.getName + ", args: " + args + ", target: " + target + "]"
+  override def toString: String = "Invocation[method: " + method.getName + ", args: " + args + ", target: " + target + "]"
 
   override def hashCode(): Int = {
     var result = HashCode.SEED
--- kernel/src/main/scala/Configuration.scala
+++ kernel/src/main/scala/Configuration.scala
@@ -5,11 +5,15 @@
 package com.scalablesolutions.akka.api
 
 import com.scalablesolutions.akka.kernel.{ActiveObject, ActiveObjectProxy}
+import google.inject.{AbstractModule}
 
-import java.util.{List => JList}
+import java.util.{List => JList, ArrayList}
 
 import scala.actors.behavior._
 import scala.reflect.BeanProperty
+
+// ============================================
+// Java version of the configuration API
 
 sealed abstract class Configuration
 
@@ -44,48 +48,58 @@ class OneForOne extends FailOverScheme {
 }
 
 abstract class Server extends Configuration
-class SupervisorConfig(@BeanProperty val restartStrategy: RestartStrategy, @BeanProperty val servers: JList[Server]) extends Server {
-  def transform = scala.actors.behavior.SupervisorConfig(restartStrategy.transform, servers.toArray.toList.asInstanceOf[List[Component]].map(_.transform))
+//class SupervisorConfig(@BeanProperty val restartStrategy: RestartStrategy, @BeanProperty val servers: JList[Server]) extends Server {
+//  def transform = scala.actors.behavior.SupervisorConfig(restartStrategy.transform, servers.toArray.toList.asInstanceOf[List[Server]].map(_.transform))
+//}
+class Component(@BeanProperty val intf: Class[_],
+                @BeanProperty val target: Class[_],
+                @BeanProperty val lifeCycle: LifeCycle,
+                @BeanProperty val timeout: Int) extends Server {
+  def newWorker(proxy: ActiveObjectProxy) = scala.actors.behavior.Worker(proxy.server, lifeCycle.transform)
 }
-class Component(@BeanProperty val proxy: ActiveObjectProxy, @BeanProperty val lifeCycle: LifeCycle) extends Server {
-  def transform = scala.actors.behavior.Worker(proxy.server, lifeCycle.transform)
-}
 
 
-object Configuration {
-  import com.google.inject.{AbstractModule, CreationException, Guice, Injector, Provides, Singleton}
-  import com.google.inject.spi.CloseFailedException
-  import com.google.inject.jsr250.{ResourceProviderFactory}
-  import javax.annotation.Resource
-  import javax.naming.Context
+// ============================================
 
-  def supervise(restartStrategy: RestartStrategy, components: JList[Component]): Supervisor = {
-    val componentList = components.toArray.toList.asInstanceOf[List[Component]]
-
-    val injector = Guice.createInjector(new AbstractModule {
-      protected def configure = {
-        bind(classOf[ResourceProviderFactory[_]])
-        componentList.foreach(c => bind(c.getClass).in(classOf[Singleton]))
-      }
-
-      // @Provides
-      // def createJndiContext: Context = {
-      //   val answer = new JndiContext
-      //   answer.bind("foo", new AnotherBean("Foo"))
-      //   answer.bind("xyz", new AnotherBean("XYZ"))
-      //   answer
-      // }
-    })
-
-    val injectedComponents = componentList.map(c => injector.getInstance(c.getClass))
-
-    // TODO: swap 'target' in proxy before running supervise
-    
-    ActiveObject.supervise(
-      restartStrategy.transform, 
-      componentList.map(c => scala.actors.behavior.Worker(c.proxy.server, c.lifeCycle.transform)))
-
-  }
-}
+/**
+ * @author <a href="http://jonasboner.com">Jonas Bon&#233;r</a>
+ */
+//object Configuration {
+//  import com.google.inject.{Module, AbstractModule, CreationException, Guice, Injector, Provides, Singleton, Binder}
+//  import com.google.inject.jsr250.{ResourceProviderFactory}
+//
+//  private val modules = new ArrayList[Module]
+//
+//  def addModule(module: Module) = modules.add(module)
+//
+//  def supervise(restartStrategy: RestartStrategy, components: Array[Component]): Supervisor = {
+//    val componentList = components.toList.asInstanceOf[List[Component]]
+//
+//    object defaultModule extends AbstractModule {
+//       protected def configure {
+//         bind(classOf[ResourceProviderFactory[_]])
+//         //componentList.foreach(c => bind(c.proxy.intf.asInstanceOf[Class[_]]).to(c.proxy.target.getClass.asInstanceOf[Class[_]]).in(classOf[Singleton]))
+//       }
+//
+//      // @Provides
+//         // def createJndiContext: Context = {
+//         //   val answer = new JndiContext
+//         //   answer.bind("foo", new AnotherBean("Foo"))
+//         //   answer.bind("xyz", new AnotherBean("XYZ"))
+//         //   answer
+//         // }
+//       }
+//    modules.add(defaultModule)
+//    val injector = Guice.createInjector(modules)
+//
+//    // swap 'target' in proxy before running supervise
+//  //  componentList.foreach(c => c.proxy.target = injector.getInstance(c.proxy.targetClass))
+//
+//    ActiveObject.supervise(
+//      restartStrategy.transform,
+//      componentList.map(c => scala.actors.behavior.Worker(c.proxy.server, c.lifeCycle.transform)))
+//
+//  }
+//}
 
 
