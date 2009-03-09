606f34e908ac518550c69aa2e9d5f87dd1b6ad1f
Jonas Boner <jonas@jonasboner.com>
2009-03-10 00:59:10 +0100

    added util-java module with annotations

---
+++ util-java/src/main/java/com/scalablesolutions/akka/annotation/oneway.java
@@ -0,0 +1,11 @@
+/**
+ * Copyright (C) 2009 Scalable Solutions.
+ */
+
+package com.scalablesolutions.akka.annotation;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface oneway {}
