6b20b9c2b9fae76514e1f42af525a40568350bf1
jboner <jboner@c-8b78e055.427-1-64736c11.cust.bredbandsbolaget.se>
2009-08-17 20:46:05 +0200

    added lift header

--- kernel/src/main/scala/util/Scheduler.scala
+++ kernel/src/main/scala/util/Scheduler.scala
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2007 WorldWide Conferencing, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package se.scalablesolutions.akka.kernel.util
 
 import java.util.concurrent._
@@ -46,7 +59,7 @@ object Scheduler extends Actor {
     unlink(actor)
     schedulers.remove(actor)
   }
-  
+
   override def shutdown = {
     schedulers.values.asScala.foreach(_ ! UnSchedule)
     service.shutdown
@@ -60,6 +73,7 @@ object Scheduler extends Actor {
 private object SchedulerThreadFactory extends ThreadFactory {
   private var count = 0
   val threadFactory = Executors.defaultThreadFactory()
+
   def newThread(r: Runnable): Thread = {
     val thread = threadFactory.newThread(r)
     thread.setName("Scheduler-" + count)
