08a914fd4a6670b98e101fe47ec397f6bde3ee0d
jboner <jboner@darkstar.local>
2009-09-23 09:03:02 +0200

    added @inittransactionalstate

---
+++ akka-util-java/src/main/java/se/scalablesolutions/akka/annotation/inittransactionalstate.java
@@ -0,0 +1,11 @@
+/**
+ * Copyright (C) 2009 Scalable Solutions.
+ */
+
+package se.scalablesolutions.akka.annotation;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface inittransactionalstate {}
