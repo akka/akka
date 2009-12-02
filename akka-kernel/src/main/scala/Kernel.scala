2bec67210e2aa379bb97c857a6bdcd4b0c462bf4
jboner <jboner@darkstar.local>
2009-12-02 10:44:31 +0100

    removed unused jars in embedded repo, added to changes.xml

--- akka-kernel/src/main/scala/Kernel.scala
+++ akka-kernel/src/main/scala/Kernel.scala
@@ -16,6 +16,8 @@ import se.scalablesolutions.akka.nio.RemoteNode
 import se.scalablesolutions.akka.util.Logging
 
 /**
+ * The Akka Kernel. 
+ * 
  * @author <a href="http://jonasboner.com">Jonas Bon&#233;r</a>
  */
 object Kernel extends Logging {
@@ -36,9 +38,18 @@ object Kernel extends Logging {
 
   def main(args: Array[String]) = boot
 
-  def boot = synchronized {
+  /**
+   * Boots up the Kernel. 
+   */   
+  def boot: Unit = boot(true)
+
+  /**
+   * Boots up the Kernel. 
+   * If you pass in false as parameter then the Akka banner is not printed out.
+   */   
+  def boot(withBanner: Boolean): Unit = synchronized {
     if (!hasBooted) {
-      printBanner
+      if (withBanner) printBanner
       log.info("Starting Akka...")
 
       runApplicationBootClasses
@@ -50,7 +61,7 @@ object Kernel extends Logging {
       hasBooted = true
     }
   }
-
+  
   def startRemoteService = {
     // FIXME manage remote serve thread for graceful shutdown
     val remoteServerThread = new Thread(new Runnable() {
@@ -112,125 +123,16 @@ object Kernel extends Logging {
 
   private def printBanner = {
     log.info(
-"""==============================
-              __    __
-       _____  |  | _|  | _______
-       \__  \ |  |/ /  |/ /\__  \
-        / __ \|    <|    <  / __ \_
-       (____  /__|_ \__|_ \(____  /
-            \/     \/    \/     \/
+"""
+==============================
+          __    __
+ _____  |  | _|  | _______
+ \__  \ |  |/ /  |/ /\__  \
+  / __ \|    <|    <  / __ \_
+ (____  /__|_ \__|_ \(____  /
+      \/     \/    \/     \/
 """)
     log.info("     Running version " + VERSION)
     log.info("==============================")
   }
-
-  private def cassandraBenchmark = {
-    import se.scalablesolutions.akka.state.CassandraStorage
-    val NR_ENTRIES = 100000
-
-    println("=================================================")
-    var start = System.currentTimeMillis
-    for (i <- 1 to NR_ENTRIES) CassandraStorage.insertMapStorageEntryFor("test", i.toString, "data")
-    var end = System.currentTimeMillis
-    println("Writes per second: " + NR_ENTRIES / ((end - start).toDouble / 1000))
-
-    println("=================================================")
-    start = System.currentTimeMillis
-    val entries = new scala.collection.mutable.ArrayBuffer[Tuple2[String, String]]
-    for (i <- 1 to NR_ENTRIES) entries += (i.toString, "data")
-    CassandraStorage.insertMapStorageEntriesFor("test", entries.toList)
-    end = System.currentTimeMillis
-    println("Writes per second - batch: " + NR_ENTRIES / ((end - start).toDouble / 1000))
-
-    println("=================================================")
-    start = System.currentTimeMillis
-    for (i <- 1 to NR_ENTRIES) CassandraStorage.getMapStorageEntryFor("test", i.toString)
-    end = System.currentTimeMillis
-    println("Reads per second: " + NR_ENTRIES / ((end - start).toDouble / 1000))
-
-    System.exit(0)
-  }
 }
-
-
-
-
-/*
-//import voldemort.client.{SocketStoreClientFactory, StoreClient, StoreClientFactory}
-//import voldemort.server.{VoldemortConfig, VoldemortServer}
-//import voldemort.versioning.Versioned
-
-  private[this] var storageFactory: StoreClientFactory = _
-  private[this] var storageServer: VoldemortServer = _
-*/
-
-//  private[akka] def startVoldemort = {
-//  val VOLDEMORT_SERVER_URL = "tcp://" + SERVER_URL
-//  val VOLDEMORT_SERVER_PORT = 6666
-//  val VOLDEMORT_BOOTSTRAP_URL = VOLDEMORT_SERVER_URL + ":" + VOLDEMORT_SERVER_PORT
-//    // Start Voldemort server
-//    val config = VoldemortConfig.loadFromVoldemortHome(Boot.HOME)
-//    storageServer = new VoldemortServer(config)
-//    storageServer.start
-//    log.info("Replicated persistent storage server started at %s", VOLDEMORT_BOOTSTRAP_URL)
-//
-//    // Create Voldemort client factory
-//    val numThreads = 10
-//    val maxQueuedRequests = 10
-//    val maxConnectionsPerNode = 10
-//    val maxTotalConnections = 100
-//    storageFactory = new SocketStoreClientFactory(
-//      numThreads,
-//      numThreads,
-//      maxQueuedRequests,
-//      maxConnectionsPerNode,
-//      maxTotalConnections,
-//      VOLDEMORT_BOOTSTRAP_URL)
-//
-//    val name = this.getClass.getName
-//    val storage = getStorageFor("actors")
-////    val value = storage.get(name)
-//    val value = new Versioned("state")
-//    //value.setObject("state")
-//    storage.put(name, value)
-//  }
-//
-//  private[akka] def getStorageFor(storageName: String): StoreClient[String, String] =
-//    storageFactory.getStoreClient(storageName)
-
-// private[akka] def startZooKeeper = {
-//import org.apache.zookeeper.jmx.ManagedUtil
-//import org.apache.zookeeper.server.persistence.FileTxnSnapLog
-//import org.apache.zookeeper.server.ServerConfig
-//import org.apache.zookeeper.server.NIOServerCnxn
-//  val ZOO_KEEPER_SERVER_URL = SERVER_URL
-//  val ZOO_KEEPER_SERVER_PORT = 9898
-//   try {
-//     ManagedUtil.registerLog4jMBeans
-//     ServerConfig.parse(args)
-//   } catch {
-//     case e: JMException => log.warning("Unable to register log4j JMX control: s%", e)
-//     case e => log.fatal("Error in ZooKeeper config: s%", e)
-//   }
-//   val factory = new ZooKeeperServer.Factory() {
-//     override def createConnectionFactory = new NIOServerCnxn.Factory(ServerConfig.getClientPort)
-//     override def createServer = {
-//       val server = new ZooKeeperServer
-//       val txLog = new FileTxnSnapLog(
-//         new File(ServerConfig.getDataLogDir),
-//         new File(ServerConfig.getDataDir))
-//       server.setTxnLogFactory(txLog)
-//       server
-//     }
-//   }
-//   try {
-//     val zooKeeper = factory.createServer
-//     zooKeeper.startup
-//     log.info("ZooKeeper started")
-//     // TODO: handle clean shutdown as below in separate thread
-//     // val cnxnFactory = serverFactory.createConnectionFactory
-//     // cnxnFactory.setZooKeeperServer(zooKeeper)
-//     // cnxnFactory.join
-//     // if (zooKeeper.isRunning) zooKeeper.shutdown
-//   } catch { case e => log.fatal("Unexpected exception: s%",e) }
-// }
