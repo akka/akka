d037f2a9154ac890e64042c30121d07a50a2d50d
Viktor Klang <viktor.klang@gmail.com>
2009-12-24 14:20:48 +0100

    Cleaned up the code

--- akka-comet/src/main/scala/AkkaClusterBroadcastFilter.scala
+++ akka-comet/src/main/scala/AkkaClusterBroadcastFilter.scala
@@ -4,7 +4,6 @@
  
 package se.scalablesolutions.akka.comet
 
-import se.scalablesolutions.akka.util.{Logging}
 import se.scalablesolutions.akka.actor.{Actor}
 import se.scalablesolutions.akka.remote.{Cluster}
 import org.atmosphere.cpr.{ClusterBroadcastFilter,Broadcaster}
@@ -13,40 +12,30 @@ import scala.reflect.{BeanProperty}
 sealed trait AkkaClusterBroadcastMessage
 case class BroadcastMessage(val name : String, val msg : AnyRef) extends AkkaClusterBroadcastMessage
 
-class AkkaClusterBroadcastFilter extends Actor with ClusterBroadcastFilter[AnyRef] with Logging {
-	@BeanProperty var clusterName = ""
-	@BeanProperty var broadcaster : Broadcaster = null
-	
-	override def init : Unit = ()
-	
-	def destroy : Unit = stop
-	
-	def filter(o : AnyRef) : AnyRef = {
-	    o match {
-	      case bm@BroadcastMessage(_,m) => {
-	        log.info("filter invoked for message [%s], message shouldn't be forwarded to cluster",o)
-	      	m
-	      }
-	      
-	      case _ => {
-		      Cluster.relayMessage(classOf[AkkaClusterBroadcastFilter],BroadcastMessage(clusterName,o))
-		      log.info("filter invoked for message [%s], message was forwarded to cluster",o)
-		      o
-	      }
-	    }
-	}
-	
-	def receive = { 
-	     case bm@BroadcastMessage(c,m) if (c == clusterName) && (broadcaster ne null) => {
-	                     log.info("Receiving remote message, broadcasting it to listeners: [%s]",m)
-	                     broadcaster broadcast bm
-	                     }
-	     case x => log.info("Not a valid message for cluster[%s] = [%s]",clusterName,x)
-	}
-	
-	start //Make sure it gets started when instantiated
-}
+class AkkaClusterBroadcastFilter extends Actor with ClusterBroadcastFilter[AnyRef] {
+  @BeanProperty var clusterName = ""
+  @BeanProperty var broadcaster : Broadcaster = null
 
-/*class AkkaBroadcaster extends JerseyBroadcaster {
-   super.bc.addFilter()
-}*/
+  override def init : Unit = ()
+
+  def destroy : Unit = stop
+
+  def filter(o : AnyRef) : AnyRef = {
+    o match {
+      case BroadcastMessage(_,m) => m
+
+      case _ => {
+        Cluster.relayMessage(classOf[AkkaClusterBroadcastFilter],BroadcastMessage(clusterName,o))
+        o
+        }
+      }
+	}
+
+  def receive = { 
+    case bm@BroadcastMessage(c,m) if (c == clusterName) && (broadcaster ne null) => broadcaster broadcast bm
+    case _ =>
+  }
+
+  //Since this class is instantiated by Atmosphere, we need to make sure it's started
+  start
+}
