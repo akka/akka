8dfc57c4f679e77d96c7e38838bdc4002fb5dfdd
Jonas Bonér <jonasremove@jonasboner.com>
2010-01-05 10:27:55 +0100

    renamed shutdown tests to spec

---
+++ akka-core/src/test/scala/ShutdownSpec.scala
@@ -0,0 +1,38 @@
+package se.scalablesolutions.akka.remote
+
+import se.scalablesolutions.akka.actor.Actor
+
+object ActorShutdownSpec {
+  def main(args: Array[String]) {
+
+    class MyActor extends Actor {
+      def receive = {
+        case "test" => println("received test")
+        case m@_ => println("received unknown message " + m)
+      }
+    }
+
+    val myActor = new MyActor
+    myActor.start
+    myActor.send("test")
+    myActor.stop
+  }
+}
+
+
+// case 2
+
+object RemoteServerAndClusterShutdownSpec {
+  def main(args: Array[String]) {
+    val s1 = new RemoteServer
+    val s2 = new RemoteServer
+    val s3 = new RemoteServer
+    s1.start("localhost", 9999)
+    s2.start("localhost", 9998)
+    s3.start("localhost", 9997)
+    Thread.sleep(5000)
+    s1.shutdown
+    s2.shutdown
+    s3.shutdown
+  }
+}
