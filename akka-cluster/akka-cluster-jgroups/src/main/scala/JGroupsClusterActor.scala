e77b10c6669adae7a25487716da2ed6821754f05
Viktor Klang <viktor.klang@gmail.com>
2010-01-27 01:13:31 +0100

    Compiles... :)

--- akka-cluster/akka-cluster-jgroups/src/main/scala/JGroupsClusterActor.scala
+++ akka-cluster/akka-cluster-jgroups/src/main/scala/JGroupsClusterActor.scala
@@ -1,43 +1,22 @@
 package se.scalablesolutions.akka.remote
 
-import org.jgroups.{JChannel, View, Address, Message, ExtendedMembershipListener, Receiver}
-
-import se.scalablesolutions.akka.config.ScalaConfig._
-import se.scalablesolutions.akka.remote.Cluster.{Node, RelayedMessage}
-import se.scalablesolutions.akka.actor.{ Actor, ActorRegistry}
-
-import scala.collection.immutable.{Map, HashMap}
-
-/**
- * JGroups Internal Cluster messages.
- */
-private[remote] object JGroupsClusterActor {
-  sealed trait JGroupsClusterMessage
-  case object PapersPlease extends JGroupsClusterMessage
-  case class Papers(addresses: List[RemoteAddress]) extends JGroupsClusterMessage
-  case object Block extends JGroupsClusterMessage
-  case object Unblock extends JGroupsClusterMessage
-  case class Zombie(address: Address) extends JGroupsClusterMessage
-  case class RegisterLocalNode(server: RemoteAddress) extends JGroupsClusterMessage
-  case class DeregisterLocalNode(server: RemoteAddress) extends JGroupsClusterMessage
-}
+import org.jgroups.{JChannel, View => JG_VIEW, Address, Message => JG_MSG, ExtendedMembershipListener, Receiver}
 
 /**
  * Clustering support via JGroups.
  */
 class JGroupsClusterActor extends ClusterActor {
-  import JGroupsClusterActor._
-  import org.scala_tools.javautils.Implicits._
+  import ClusterActor._
+  import org.scala_tools.javautils.Imports._
 
+  type ADDR_T = Address
 
   @volatile private var isActive = false
-  @volatile private var local: Node = Node(Nil)
   @volatile private var channel: Option[JChannel] = None
-  @volatile private var remotes: Map[Address, Node] = Map()
 
   override def init = {
+    super.init
     log debug "Initiating JGroups-based cluster actor"
-    remotes = new HashMap[Address, Node]
     val me = this
     isActive = true
 
@@ -48,108 +27,36 @@ class JGroupsClusterActor extends ClusterActor {
 
         def setState(state: Array[Byte]): Unit = ()
 
-        def receive(msg: Message): Unit = if (isActive) me send msg
+        def receive(m: JG_MSG): Unit =
+          if (isActive && m.getSrc != channel.map(_.getAddress).getOrElse(m.getSrc)) me send Message(m.getSrc,m.getRawBuffer)
 
-        def viewAccepted(view: View): Unit = if (isActive) me send view
+        def viewAccepted(view: JG_VIEW): Unit =
+          if (isActive) me send View(Set[ADDR_T]() ++ view.getMembers.asScala - channel.get.getAddress)
 
-        def suspect(a: Address): Unit = if (isActive) me send Zombie(a)
+        def suspect(a: Address): Unit =
+          if (isActive) me send Zombie(a)
 
-        def block: Unit = if (isActive) me send Block
+        def block: Unit =
+          log debug "UNSUPPORTED: JGroupsClusterActor::block" //TODO HotSwap to a buffering body
 
-        def unblock: Unit = if (isActive) me send Unblock
+        def unblock: Unit =
+          log debug "UNSUPPORTED: JGroupsClusterActor::unblock" //TODO HotSwap back and flush the buffer
       })
     })
     channel.map(_.connect(name))
   }
 
-  def lookup[T](handleRemoteAddress: PartialFunction[RemoteAddress, T]): Option[T] =
-    remotes.values.toList.flatMap(_.endpoints).find(handleRemoteAddress isDefinedAt _).map(handleRemoteAddress)
+  protected def toOneNode(dest : Address, msg: Array[Byte]): Unit =
+    for (c <- channel) c.send(new JG_MSG(dest, null, msg))
 
-  def registerLocalNode(hostname: String, port: Int): Unit =
-    send(RegisterLocalNode(RemoteAddress(hostname, port)))
-
-  def deregisterLocalNode(hostname: String, port: Int): Unit =
-    send(DeregisterLocalNode(RemoteAddress(hostname, port)))
-
-  def relayMessage(to: Class[_ <: Actor], msg: AnyRef): Unit =
-    send(RelayedMessage(to.getName, msg))
-
-  private def broadcast[T <: AnyRef](recipients: Iterable[Address], msg: T): Unit = {
-    lazy val m = Cluster.serializer out msg
-    for (c <- channel; r <- recipients) c.send(new Message(r, null, m))
-  }
-
-  private def broadcast[T <: AnyRef](msg: T): Unit =
-    //Don't broadcast if we are not connected anywhere...
-    if (!remotes.isEmpty) channel.map(_.send(new Message(null, null, Cluster.serializer out msg)))
-
-  def receive = {
-    case Zombie(x) => { //Ask the presumed zombie for papers and prematurely treat it as dead
-      log debug ("Killing Zombie Node: %s", x)
-      broadcast(x :: Nil, PapersPlease)
-      remotes = remotes - x
-    }
-
-    case v: View => {
-      // Not present in the cluster anymore = presumably zombies
-      // Nodes we have no prior knowledge existed = unknowns
-      val members = Set[Address]() ++ v.getMembers.asScala - channel.get.getAddress // Exclude ourselves
-      val zombies = Set[Address]() ++ remotes.keySet -- members
-      val unknown = members -- remotes.keySet
-
-      log debug v.printDetails
-
-      // Tell the zombies and unknowns to provide papers and prematurely treat the zombies as dead
-      broadcast(zombies ++ unknown, PapersPlease)
-      remotes = remotes -- zombies
-    }
-
-    case m: Message => {
-      if (m.getSrc != channel.map(_.getAddress).getOrElse(m.getSrc)) // Handle non-own messages only, and only if we're connected
-        (Cluster.serializer in (m.getRawBuffer, None)) match {
-
-          case PapersPlease => {
-            log debug ("Asked for papers by %s", m.getSrc)
-            broadcast(m.getSrc :: Nil, Papers(local.endpoints))
-
-            if (remotes.get(m.getSrc).isEmpty) // If we were asked for papers from someone we don't know, ask them!
-              broadcast(m.getSrc :: Nil, PapersPlease)
-          }
-
-          case Papers(x) => remotes = remotes + (m.getSrc -> Node(x))
-
-          case RelayedMessage(c, m) => ActorRegistry.actorsFor(c).map(_ send m)
-
-          case unknown => log debug ("Unknown message: %s", unknown.toString)
-        }
-    }
-
-    case rm @ RelayedMessage(_, _) => {
-      log debug ("Relaying message: %s", rm)
-      broadcast(rm)
-    }
-
-    case RegisterLocalNode(s) => {
-      log debug ("RegisterLocalNode: %s", s)
-      local = Node(local.endpoints + s)
-      broadcast(Papers(local.endpoints))
-    }
-
-    case DeregisterLocalNode(s) => {
-      log debug ("DeregisterLocalNode: %s", s)
-      local = Node(local.endpoints - s)
-      broadcast(Papers(local.endpoints))
-    }
-
-    case Block => log debug "UNSUPPORTED: JGroupsClusterActor::block" //TODO HotSwap to a buffering body
-    case Unblock => log debug "UNSUPPORTED: JGroupsClusterActor::unblock" //TODO HotSwap back and flush the buffer
-  }
+  protected def toAllNodes(msg : Array[Byte]) : Unit =
+    for (c <- channel) c.send(new JG_MSG(null, null, msg))
 
   override def shutdown = {
+    super.shutdown
     log debug ("Shutting down %s", toString)
     isActive = false
     channel.foreach(_.shutdown)
-    remotes = Map()
     channel = None
   }
 }
---
+++ akka-cluster/akka-cluster-shoal/src/main/scala/ShoalClusterActor.scala
@@ -0,0 +1,92 @@
+/**
+ * Copyright (C) 2009-2010 Scalable Solutions AB <http://scalablesolutions.se>
+ */
+package se.scalablesolutions.akka.remote
+
+import se.scalablesolutions.akka.config.ScalaConfig._
+import com.sun.enterprise.ee.cms.core.{CallBack,
+                                       GMSConstants,
+                                       GMSFactory,
+                                       GroupManagementService,
+                                       MessageSignal,
+                                       Signal,
+                                       GMSException,
+                                       SignalAcquireException,
+                                       SignalReleaseException,
+                                       JoinNotificationSignal,
+                                       FailureSuspectedSignal,
+                                       FailureNotificationSignal }
+import com.sun.enterprise.ee.cms.impl.client.{FailureNotificationActionFactoryImpl,
+                                       FailureSuspectedActionFactoryImpl,
+                                       JoinNotificationActionFactoryImpl,
+                                       MessageActionFactoryImpl,
+                                       PlannedShutdownActionFactoryImpl
+}
+
+class ShoalClusterActor extends ClusterActor {
+
+  type ADDR_T = String
+
+  @volatile protected var gms : Option[GroupManagementService] = None
+            protected val serverName : String = RemoteServer.HOSTNAME + ":" + RemoteServer.PORT
+  @volatile private   var isActive = false
+
+  protected def topic : String = "akka-messages"
+
+  override def init = {
+    super.init
+    gms = createGMS
+    isActive = true
+  }
+
+  override def shutdown = {
+    super.shutdown
+    isActive = false
+    for(g <- gms) g.shutdown(GMSConstants.shutdownType.INSTANCE_SHUTDOWN)
+    gms = None
+  }
+
+  protected def createGMS : Option[GroupManagementService] = {
+    val g = GMSFactory.startGMSModule(serverName,name, GroupManagementService.MemberType.CORE, null).asInstanceOf[GroupManagementService]
+
+    val callback = createCallback
+    g.addActionFactory(new JoinNotificationActionFactoryImpl(callback))
+    g.addActionFactory(new FailureSuspectedActionFactoryImpl(callback))
+    g.addActionFactory(new FailureNotificationActionFactoryImpl(callback))
+    g.addActionFactory(new PlannedShutdownActionFactoryImpl(callback))
+    g.addActionFactory(new MessageActionFactoryImpl(callback), topic)
+    g.join
+    Some(g)
+  }
+
+  protected def createCallback : CallBack = {
+    import org.scala_tools.javautils.Imports._
+    val me = this
+    new CallBack {
+      def processNotification(signal : Signal) {
+        try {
+          signal.acquire()
+          if(isActive) {
+            signal match {
+              case  ms : MessageSignal => me send Message(ms.getMemberToken,ms.getMessage)
+              case  js : JoinNotificationSignal => me send View(Set[ADDR_T]() ++ js.getCurrentCoreMembers.asScala - serverName)
+              case fss : FailureSuspectedSignal => me send Zombie(fss.getMemberToken)
+              case fns : FailureNotificationSignal => me send Zombie(fns.getMemberToken)
+              case _ => log.debug("Unhandled signal: [%s]",signal)
+            }
+          }
+          signal.release()
+        } catch {
+            case e : SignalAcquireException => log.warning(e,"SignalAcquireException")
+            case e : SignalReleaseException => log.warning(e,"SignalReleaseException")
+        }
+      }
+    }
+  }
+
+  protected def toOneNode(dest : ADDR_T, msg : Array[Byte]) : Unit =
+    for(g <- gms) g.getGroupHandle.sendMessage(dest,topic, msg)
+
+  protected def toAllNodes(msg : Array[Byte]) : Unit =
+    for(g <- gms) g.getGroupHandle.sendMessage(topic, msg)
+}
