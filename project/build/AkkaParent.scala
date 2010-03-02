b97b456d26cbc1cb14a454c82dc7745200d43d8e
peter hausel <peter_kovac11@hotmail.com>
2010-03-02 00:30:14 -0500

    second phase

--- project/build/AkkaParent.scala
+++
@@ -1,7 +0,0 @@
-import sbt._
-
-class AkkaParent(info: ProjectInfo) extends ParentProject(info) {
-     lazy val javautil = project("akka-util-java", "akka java util")
-     lazy val util = project("akka-util", "akka util")
-     lazy val core = project("akka-core", "akka core", util,javautil)
-}
