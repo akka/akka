d7fa4a653030237630176883031526657e4b62db
Jan Van Besien <janvanbesien@gmail.com>
2010-03-04 21:22:16 +0100

    Merged with dispatcher improvements. Cleanup unit tests.
    Conflicts:
    	akka-core/src/main/scala/actor/Actor.scala
    	akka-core/src/main/scala/dispatch/ExecutorBasedEventDrivenDispatcher.scala

--- akka-core/src/test/scala/ExecutorBasedEventDrivenDispatcherActorsTest.scala
+++ akka-core/src/test/scala/ExecutorBasedEventDrivenDispatcherActorsTest.scala
@@ -61,27 +61,3 @@ class ExecutorBasedEventDrivenDispatcherActorsTest extends JUnitSuite with MustM
   })
 
 }
-
-trait ActorTestUtil {
-  def handle[T](actors: Actor*)(test: => T): T = {
-    for (a <- actors) a.start
-    try
-    {
-      test
-    }
-    finally
-      {
-        for (a <- actors) a.stop
-      }
-  }
-
-  def verify(actor: TestActor): Unit = handle(actor)
-            {actor.test}
-}
-
-abstract class TestActor extends Actor with ActorTestUtil
-{
-  def test: Unit
-
-  def receive = {case _ =>}
-}
