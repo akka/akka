066deef4c358ef107d5a8c9cae255d6889b694ac
Martin Krasser <krasserm@googlemail.com>
2010-03-14 11:17:34 +0100

    publish/subscribe examples using jms and cometd

--- akka-samples/akka-sample-camel/src/main/scala/Boot.scala
+++ akka-samples/akka-sample-camel/src/main/scala/Boot.scala
@@ -1,7 +1,10 @@
 package sample.camel
 
-import org.apache.camel.builder.RouteBuilder
 import org.apache.camel.{Exchange, Processor}
+import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.impl.DefaultCamelContext
+import org.apache.camel.spring.spi.ApplicationContextRegistry
+import org.springframework.context.support.ClassPathXmlApplicationContext
 
 import se.scalablesolutions.akka.actor.SupervisorFactory
 import se.scalablesolutions.akka.camel.CamelContextManager
@@ -12,8 +15,14 @@ import se.scalablesolutions.akka.config.ScalaConfig._
  */
 class Boot {
 
-  CamelContextManager.init()
+  // Create CamelContext with Spring-based registry and custom route builder
+
+  val context = new ClassPathXmlApplicationContext("/sample-camel-context.xml", getClass)
+  val registry = new ApplicationContextRegistry(context)
+  CamelContextManager.init(new DefaultCamelContext(registry))
   CamelContextManager.context.addRoutes(new CustomRouteBuilder)
+
+  // Basic example
 
   val factory = SupervisorFactory(
     SupervisorConfig(
@@ -21,6 +30,8 @@ class Boot {
       Supervise(new Consumer1, LifeCycle(Permanent)) ::
       Supervise(new Consumer2, LifeCycle(Permanent)) :: Nil))
   factory.newInstance.start
+
+  // Routing example
 
   val producer = new Producer1
   val mediator = new Transformer(producer)
@@ -30,6 +41,20 @@ class Boot {
   mediator.start
   consumer.start
 
+  // Publish subscribe example
+
+  val cometdUri = "cometd://localhost:8111/test/abc?resourceBase=target"
+  val cometdSubscriber = new Subscriber("cometd-subscriber", cometdUri).start
+  val cometdPublisher = new Publisher("cometd-publisher", cometdUri).start
+
+  val jmsUri = "jms:topic:test"
+  val jmsSubscriber1 = new Subscriber("jms-subscriber-1", jmsUri).start
+  val jmsSubscriber2 = new Subscriber("jms-subscriber-2", jmsUri).start
+  val jmsPublisher = new Publisher("jms-publisher", jmsUri).start
+
+  val cometdPublisherBridge = new PublisherBridge("jetty:http://0.0.0.0:8877/camel/pub/cometd", cometdPublisher).start
+  val jmsPublisherBridge = new PublisherBridge("jetty:http://0.0.0.0:8877/camel/pub/jms", jmsPublisher).start
+  
 }
 
 class CustomRouteBuilder extends RouteBuilder {
