32365990b44004267df9239f5aaf573c64dc8924
Michael Kober <michaelkober@mike-laptop.local>
2010-03-17 10:21:29 +0100

    added integration tests

---
+++ akka-spring/akka-spring-test-java/src/main/java/se/scalablesolutions/akka/spring/foo/StatefulPojo.java
@@ -0,0 +1,47 @@
+package se.scalablesolutions.akka.spring.foo;
+
+import se.scalablesolutions.akka.stm.TransactionalMap;
+import se.scalablesolutions.akka.stm.TransactionalVector;
+import se.scalablesolutions.akka.stm.TransactionalRef;
+import se.scalablesolutions.akka.stm.TransactionalState;
+
+public class StatefulPojo {
+    private TransactionalMap<String, String> mapState;
+    private TransactionalVector<String> vectorState;
+    private TransactionalRef<String> refState;
+    private boolean isInitialized = false;
+
+    public void init() {
+      if (!isInitialized) {
+        mapState = TransactionalState.newMap();
+        vectorState = TransactionalState.newVector();
+        refState = TransactionalState.newRef();
+        isInitialized = true;
+      }
+    }
+
+    public String getMapState(String key) {
+      return (String)mapState.get(key).get();
+    }
+
+    public String getVectorState() {
+      return (String)vectorState.last();
+    }
+
+    public String getRefState() {
+      return (String)refState.get().get();
+    }
+
+    public void setMapState(String key, String msg) {
+      mapState.put(key, msg);
+    }
+
+    public void setVectorState(String msg) {
+      vectorState.add(msg);
+    }
+
+    public void setRefState(String msg) {
+      refState.swap(msg);
+    }
+
+}
