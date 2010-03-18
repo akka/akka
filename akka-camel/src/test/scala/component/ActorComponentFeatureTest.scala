0e9be44661ee1e35d6e1d34908d9b632abaae7f2
Martin Krasser <krasserm@googlemail.com>
2010-03-18 16:34:08 +0100

    extension/rewrite of actor component unit and functional tests

---
+++ akka-camel/src/test/scala/component/ActorComponentFeatureTest.scala
@@ -0,0 +1,62 @@
+package se.scalablesolutions.akka.camel.component
+
+import org.apache.camel.RuntimeCamelException
+import org.scalatest.{BeforeAndAfterEach, BeforeAndAfterAll, FeatureSpec}
+
+import se.scalablesolutions.akka.actor.ActorRegistry
+import se.scalablesolutions.akka.camel.CamelContextManager
+import se.scalablesolutions.akka.camel.support.{Respond, Countdown, Tester, Retain}
+
+class ActorComponentFeatureTest extends FeatureSpec with BeforeAndAfterAll with BeforeAndAfterEach {
+  override protected def beforeAll() = {
+    ActorRegistry.shutdownAll
+    CamelContextManager.init
+    CamelContextManager.start 
+  }
+
+  override protected def afterAll() = CamelContextManager.stop
+
+  override protected def afterEach() = ActorRegistry.shutdownAll
+  
+  feature("Communicate with an actor from a Camel application using actor endpoint URIs") {
+    import CamelContextManager.template
+
+    scenario("one-way communication using actor id") {
+      val actor = new Tester with Retain with Countdown
+      actor.start
+      template.sendBody("actor:%s" format actor.getId, "Martin")
+      assert(actor.waitFor)
+      assert(actor.body === "Martin")
+    }
+
+    scenario("one-way communication using actor uuid") {
+      val actor = new Tester with Retain with Countdown
+      actor.start
+      template.sendBody("actor:uuid:%s" format actor.uuid, "Martin")
+      assert(actor.waitFor)
+      assert(actor.body === "Martin")
+    }
+
+    scenario("two-way communication using actor id") {
+      val actor = new Tester with Respond
+      actor.start
+      assert(template.requestBody("actor:%s" format actor.getId, "Martin") === "Hello Martin")
+    }
+
+    scenario("two-way communication using actor uuid") {
+      val actor = new Tester with Respond
+      actor.start
+      assert(template.requestBody("actor:uuid:%s" format actor.uuid, "Martin") === "Hello Martin")
+    }
+
+    scenario("two-way communication with timeout") {
+      val actor = new Tester {
+        timeout = 1
+      }
+      actor.start
+      intercept[RuntimeCamelException] {
+        template.requestBody("actor:uuid:%s" format actor.uuid, "Martin")
+      }
+    }
+  }
+}
--- akka-camel/src/test/scala/component/ActorProducerTest.scala
+++ akka-camel/src/test/scala/component/ActorProducerTest.scala
@@ -1,42 +1,75 @@
 package se.scalablesolutions.akka.camel.component
 
-import org.apache.camel.{CamelContext, ExchangePattern}
-import org.apache.camel.impl.{DefaultCamelContext, DefaultExchange}
-import org.junit.Assert._
-import org.junit.Test
+import ActorComponentTest._
+
+import org.apache.camel.ExchangePattern
+import org.junit.{After, Test}
 import org.scalatest.junit.JUnitSuite
 
-import se.scalablesolutions.akka.actor.Actor
-import se.scalablesolutions.akka.camel.Message
+import se.scalablesolutions.akka.actor.ActorRegistry
+import se.scalablesolutions.akka.camel.support.{Countdown, Retain, Tester, Respond}
+import org.scalatest.BeforeAndAfterAll
+import se.scalablesolutions.akka.camel.{Failure, Message}
+import java.util.concurrent.TimeoutException
 
-class ActorProducerTest extends JUnitSuite {
+class ActorProducerTest extends JUnitSuite with BeforeAndAfterAll {
 
-  //
-  // TODO: extend/rewrite unit tests
-  // These tests currently only ensure proper functioning of basic features.
-  //
+  @After def tearDown = {
+    ActorRegistry.shutdownAll
+  }
 
-  val context = new DefaultCamelContext
-  val endpoint = context.getEndpoint("actor:%s" format classOf[TestActor].getName)
-  val producer = endpoint.createProducer
-
-  @Test def shouldSendAndReceiveMessageBodyAndHeaders = {
-    val exchange = new DefaultExchange(null.asInstanceOf[CamelContext], ExchangePattern.InOut)
-    val actor = new TestActor
+  @Test def shouldSendMessageToActor = {
+    val actor = new Tester with Retain with Countdown
+    val endpoint = mockEndpoint("actor:uuid:%s" format actor.uuid)
+    val exchange = endpoint.createExchange(ExchangePattern.InOnly)
     actor.start
     exchange.getIn.setBody("Martin")
     exchange.getIn.setHeader("k1", "v1")
-    producer.process(exchange)
-    assertEquals("Hello Martin", exchange.getOut.getBody)
-    assertEquals("v1", exchange.getOut.getHeader("k1"))
-    assertEquals("v2", exchange.getOut.getHeader("k2"))
-    actor.stop
+    endpoint.createProducer.process(exchange)
+    actor.waitFor
+    assert(actor.body === "Martin")
+    assert(actor.headers === Map(Message.MessageExchangeId -> exchange.getExchangeId, "k1" -> "v1"))
   }
 
-  class TestActor extends Actor {
-    protected def receive = {
-      case msg: Message => reply(Message("Hello %s" format msg.body, Map("k2" -> "v2") ++ msg.headers))
+  @Test def shouldSendMessageToActorAndReturnResponse = {
+    val actor = new Tester with Respond {
+      override def response(msg: Message) = Message(super.response(msg), Map("k2" -> "v2"))
+    }
+    val endpoint = mockEndpoint("actor:uuid:%s" format actor.uuid)
+    val exchange = endpoint.createExchange(ExchangePattern.InOut)
+    actor.start
+    exchange.getIn.setBody("Martin")
+    exchange.getIn.setHeader("k1", "v1")
+    endpoint.createProducer.process(exchange)
+    assert(exchange.getOut.getBody === "Hello Martin")
+    assert(exchange.getOut.getHeader("k2") === "v2")
+  }
+
+  @Test def shouldSendMessageToActorAndReturnFailure = {
+    val actor = new Tester with Respond {
+      override def response(msg: Message) = Failure(new Exception("testmsg"), Map("k3" -> "v3"))
+    }
+    val endpoint = mockEndpoint("actor:uuid:%s" format actor.uuid)
+    val exchange = endpoint.createExchange(ExchangePattern.InOut)
+    actor.start
+    exchange.getIn.setBody("Martin")
+    exchange.getIn.setHeader("k1", "v1")
+    endpoint.createProducer.process(exchange)
+    assert(exchange.getException.getMessage === "testmsg")
+    assert(exchange.getOut.getBody === null)
+    assert(exchange.getOut.getHeader("k3") === null) // headers from failure message are currently ignored
+  }
+
+  @Test def shouldSendMessageToActorAndTimeout: Unit = {
+    val actor = new Tester {
+      timeout = 1
+    }
+    val endpoint = mockEndpoint("actor:uuid:%s" format actor.uuid)
+    val exchange = endpoint.createExchange(ExchangePattern.InOut)
+    actor.start
+    exchange.getIn.setBody("Martin")
+    intercept[TimeoutException] {
+      endpoint.createProducer.process(exchange)
     }
   }
-
 }
