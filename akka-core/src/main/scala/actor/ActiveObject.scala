68bb4af726d2c6eab2ff86a13320e522a0471b82
Jonas Bonér <jonasremove@jonasboner.com>
2010-04-21 11:23:30 +0200

    Moved ActiveObjectConfiguration to ActiveObject.scala file

--- akka-core/src/main/scala/actor/ActiveObject.scala
+++ akka-core/src/main/scala/actor/ActiveObject.scala
@@ -4,7 +4,7 @@
 
 package se.scalablesolutions.akka.actor
 
-import _root_.se.scalablesolutions.akka.config.FaultHandlingStrategy
+import se.scalablesolutions.akka.config.FaultHandlingStrategy
 import se.scalablesolutions.akka.remote.protobuf.RemoteProtocol.RemoteRequest
 import se.scalablesolutions.akka.remote.{RemoteProtocolBuilder, RemoteClient, RemoteRequestIdFactory}
 import se.scalablesolutions.akka.dispatch.{MessageDispatcher, Future}
@@ -27,6 +27,43 @@ object Annotations {
   val postrestart =            classOf[postrestart]
   val immutable =              classOf[immutable]
   val inittransactionalstate = classOf[inittransactionalstate]
+}
+
+/**
+ * TODO: document
+ * FIXDOC: document ActiveObjectConfiguration
+ */
+final class ActiveObjectConfiguration {
+  private[akka] var _timeout: Long = Actor.TIMEOUT
+  private[akka] var _restartCallbacks: Option[RestartCallbacks] = None
+  private[akka] var _transactionRequired = false
+  private[akka] var _host: Option[InetSocketAddress] = None
+  private[akka] var _messageDispatcher: Option[MessageDispatcher] = None
+
+  def timeout(timeout: Long) : ActiveObjectConfiguration = {
+    _timeout = timeout
+    this
+  }
+
+  def restartCallbacks(pre: String, post: String) : ActiveObjectConfiguration = {
+    _restartCallbacks = Some(new RestartCallbacks(pre, post))
+    this
+  }
+
+  def makeTransactionRequired() : ActiveObjectConfiguration = {
+    _transactionRequired = true;
+    this
+  }
+
+  def makeRemote(hostname: String, port: Int) : ActiveObjectConfiguration = {
+    _host = Some(new InetSocketAddress(hostname, port))
+    this
+  }
+
+  def dispatcher(messageDispatcher: MessageDispatcher) : ActiveObjectConfiguration = {
+    _messageDispatcher = Some(messageDispatcher)
+    this
+  }
 }
 
 /**
