4ffa75937beb3a5130cc537a3ac5223494b80123
Viktor Klang <viktor.klang@gmail.com>
2010-04-24 14:56:50 +0200

    Added Listeners to akka-patterns

--- akka-patterns/src/main/scala/Patterns.scala
+++ akka-patterns/src/main/scala/Patterns.scala
@@ -85,3 +85,21 @@ class SmallestMailboxFirstIterator(items : List[Actor]) extends InfiniteIterator
     items.reduceLeft((actor1, actor2) => actorWithSmallestMailbox(actor1,actor2))
   }
 } 
+
+sealed trait ListenerMessage
+case class Listen(listener : Actor) extends ListenerMessage
+case class Deafen(listener : Actor) extends ListenerMessage 
+case class WithListeners(f : Set[Actor] => Unit) extends ListenerMessage
+
+trait Listeners { self : Actor =>
+  import se.scalablesolutions.akka.actor.Agent
+  private lazy val listeners = Agent(Set[Actor]())
+
+  protected def listenerManagement : PartialFunction[Any,Unit] = {
+    case Listen(l) => listeners( _ + l)
+    case Deafen(l) => listeners( _ - l )
+    case WithListeners(f) => listeners foreach f
+  }
+  
+  protected def gossip(msg : Any) = listeners foreach ( _ foreach ( _ ! msg ) )
+}
