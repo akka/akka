271a8297e4ca58baf7d4c34d19e15bcd8f1abcce
Viktor Klang <viktor.klang@gmail.com>
2010-04-29 11:03:25 +0200

    Consolidated akka-security, akka-rest, akka-comet and akka-servlet into akka-http

---
+++ akka-http/src/main/scala/AkkaBroadcaster.scala
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C) 2009-2010 Scalable Solutions AB <http://scalablesolutions.se>
+ */
+
+package se.scalablesolutions.akka.comet
+
+import org.atmosphere.cpr.{AtmosphereResourceEvent, AtmosphereResource}
+import se.scalablesolutions.akka.actor.Actor
+
+class AkkaBroadcaster extends org.atmosphere.jersey.JerseyBroadcaster {
+  name = classOf[AkkaBroadcaster].getName
+  
+  val caster = new Actor {  
+    def receive = { case f : Function0[_] => f() }
+    start
+  }
+
+  override def destroy {
+    super.destroy
+    caster.stop
+  }
+  
+  protected override def broadcast(r :  AtmosphereResource[_,_], e : AtmosphereResourceEvent[_,_]) = {
+    caster ! (() => super.broadcast(r,e))
+  }
+}
---
+++ akka-http/src/main/scala/ListWriter.scala
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2009-2010 Scalable Solutions AB <http://scalablesolutions.se>
+ */
+package se.scalablesolutions.akka.rest
+
+import java.io.OutputStream
+import se.scalablesolutions.akka.serialization.Serializer
+import javax.ws.rs.core.{MultivaluedMap, MediaType}
+import javax.ws.rs.ext.{MessageBodyWriter, Provider}
+import javax.ws.rs.Produces
+
+/**
+ * writes Lists of JSON serializable objects
+ */
+@Provider
+@Produces(Array("application/json"))
+class ListWriter extends MessageBodyWriter[List[_]] {
+
+  def isWriteable(aClass: Class[_], aType: java.lang.reflect.Type, annotations: Array[java.lang.annotation.Annotation], mediaType: MediaType) = {
+    classOf[List[_]].isAssignableFrom(aClass) || aClass == ::.getClass
+  }
+
+  def getSize(list: List[_], aClass: Class[_], aType: java.lang.reflect.Type, annotations: Array[java.lang.annotation.Annotation], mediaType: MediaType) = -1L
+
+  def writeTo(list: List[_],
+              aClass: Class[_],
+              aType: java.lang.reflect.Type,
+              annotations: Array[java.lang.annotation.Annotation],
+              mediaType: MediaType,
+              stringObjectMultivaluedMap: MultivaluedMap[String, Object],
+              outputStream: OutputStream) : Unit = {
+      if (list.isEmpty)
+        outputStream.write(" ".getBytes)
+      else
+        outputStream.write(Serializer.ScalaJSON.out(list))
+  }
+
+}
