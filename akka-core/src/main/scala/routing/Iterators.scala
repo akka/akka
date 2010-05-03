a6c566eef3cbbeb4f75d6adbc95078e0d67726e6
Viktor Klang <viktor.klang@gmail.com>
2010-05-03 19:32:40 +0200

    Split up Patterns.scala in different files

---
+++ akka-core/src/main/scala/routing/Iterators.scala
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) 2009-2010 Scalable Solutions AB <http://scalablesolutions.se>
+ */
+
+package se.scalablesolutions.akka.patterns
+
+import se.scalablesolutions.akka.actor.Actor
+
+trait InfiniteIterator[T] extends Iterator[T]
+
+class CyclicIterator[T](items: List[T]) extends InfiniteIterator[T] {
+  @volatile private[this] var current: List[T] = items
+
+  def hasNext = items != Nil
+
+  def next = {
+    val nc = if (current == Nil) items else current
+    current = nc.tail
+    nc.head
+  }
+}
+
+class SmallestMailboxFirstIterator(items : List[Actor]) extends InfiniteIterator[Actor] {
+  def hasNext = items != Nil
+
+  def next = items.reduceLeft((a1, a2) => if (a1.mailboxSize < a2.mailboxSize) a1 else a2)
+} 
---
+++ akka-core/src/main/scala/routing/Routers.scala
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C) 2009-2010 Scalable Solutions AB <http://scalablesolutions.se>
+ */
+
+package se.scalablesolutions.akka.patterns
+
+import se.scalablesolutions.akka.actor.Actor
+
+trait Dispatcher { self: Actor =>
+
+  protected def transform(msg: Any): Any = msg
+
+  protected def routes: PartialFunction[Any, Actor]
+
+  protected def dispatch: PartialFunction[Any, Unit] = {
+    case a if routes.isDefinedAt(a) =>
+      if (self.replyTo.isDefined) routes(a) forward transform(a)
+      else routes(a) ! transform(a)
+  }
+
+  def receive = dispatch
+}
+
+trait LoadBalancer extends Dispatcher { self: Actor =>
+  protected def seq: InfiniteIterator[Actor]
+
+  protected def routes = { case x if seq.hasNext => seq.next }
+}
