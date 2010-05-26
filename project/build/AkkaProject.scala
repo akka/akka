718aac2dd09fef508b5d16a27e3704c91e77a598
Peter Vlugter <peter@maka>
2010-05-27 09:12:17 +1200

    Updated to Multiverse 0.5.2

--- project/build/AkkaProject.scala
+++ project/build/AkkaProject.scala
@@ -19,6 +19,7 @@ class AkkaParent(info: ProjectInfo) extends DefaultProject(info) {
   val CASSANDRA_VERSION = "0.6.1"
   val LIFT_VERSION = "2.0-scala280-SNAPSHOT"
   val SCALATEST_VERSION = "1.2-for-scala-2.8.0.RC2-SNAPSHOT"
+  val MULTIVERSE_VERSION = "0.5.2"
 
   // ------------------------------------------------------------
   lazy val deployPath = info.projectPath / "deploy"
@@ -174,7 +175,7 @@ class AkkaParent(info: ProjectInfo) extends DefaultProject(info) {
     val guicey = "org.guiceyfruit" % "guice-all" % "2.0" % "compile"
     val aopalliance = "aopalliance" % "aopalliance" % "1.0" % "compile"
     val protobuf = "com.google.protobuf" % "protobuf-java" % "2.2.0" % "compile"
-    val multiverse = "org.multiverse" % "multiverse-alpha" % "0.5" % "compile"
+    val multiverse = "org.multiverse" % "multiverse-alpha" % MULTIVERSE_VERSION % "compile"
     val jgroups = "jgroups" % "jgroups" % "2.9.0.GA" % "compile"
     
     // testing
