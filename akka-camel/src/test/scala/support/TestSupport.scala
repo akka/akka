9241599e5f4b3cdc912c4568e58212da589557f9
Martin Krasser <krasserm@googlemail.com>
2010-06-03 12:17:04 +0200

    Refactored tests to interact with actors only via message passing

--- akka-camel/src/test/scala/support/TestSupport.scala
+++ akka-camel/src/test/scala/support/TestSupport.scala
@@ -5,45 +5,72 @@ import java.util.concurrent.{TimeUnit, CountDownLatch}
 import se.scalablesolutions.akka.camel.Message
 import se.scalablesolutions.akka.actor.Actor
 
-trait Receive[T] {
-  def onMessage(msg: T): Unit
+import TestSupport._
+
+object TestSupport {
+  type Handler = PartialFunction[Any, Any]
 }
 
-trait Respond extends Receive[Message] { this: Actor =>
-  abstract override def onMessage(msg: Message): Unit = {
-    super.onMessage(msg)
-    this.self.reply(response(msg))
+trait TestActor extends Actor {
+  def receive = {
+    case msg => {
+      val x = msg
+      handler(msg)
+    }
   }
+  
+  def handler: Handler
+}
+
+class Tester1 extends TestActor with Retain with Countdown {
+  def handler = retain andThen countdown
+}
+
+class Tester2 extends TestActor with Respond {
+  def handler = respond
+}
+
+class Tester3 extends TestActor with Noop {
+  self.timeout = 1
+  def handler = noop
+}
+
+trait Countdown { this: Actor =>
+  var latch: CountDownLatch = new CountDownLatch(0)
+  def countdown: Handler = {
+    case SetExpectedMessageCount(num) => {
+      latch = new CountDownLatch(num)
+      self.reply(latch)
+    }
+    case msg => latch.countDown
+  }
+}
+
+trait Respond { this: Actor =>
+  def respond: Handler = {
+    case msg: Message => self.reply(response(msg))
+  }
+
   def response(msg: Message): Any = "Hello %s" format msg.body
 }
 
-trait Retain extends Receive[Message] {
-  var body: Any = _
-  var headers = Map.empty[String, Any]
-  abstract override def onMessage(msg: Message): Unit = {
-    super.onMessage(msg)
-    body = msg.body
-    headers = msg.headers
+trait Retain { this: Actor =>
+  var message: Any = _
+
+  def retain: Handler = {
+    case GetRetainedMessage => self.reply(message)
+    case msg => {
+      message = msg
+      msg
+    }
   }
 }
 
-trait Countdown[T] extends Receive[T] {
-  val count = 1
-  val duration = 5000
-  val latch = new CountDownLatch(count)
-
-  def waitFor = latch.await(duration, TimeUnit.MILLISECONDS)
-  def countDown = latch.countDown
-
-  abstract override def onMessage(msg: T) = {
-    super.onMessage(msg)
-    countDown
+trait Noop  { this: Actor =>
+  def noop: Handler = {
+    case msg => msg
   }
 }
 
-class Tester extends Actor with Receive[Message] {
-  def receive = {
-    case msg: Message => onMessage(msg)
-  }
-  def onMessage(msg: Message): Unit = {}
-}
+case class SetExpectedMessageCount(num: Int)
+case class GetRetainedMessage()
