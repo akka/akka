6911c7629c1757b23ea0fad9744c9b056bf47745
Andreas Kollegger <akollegger@tembopublic.org>
2010-06-06 14:33:10 -0400

    initial changes for OSGification: added bnd4sbt plugin, changed artifact naming to include _osgi

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -5,4 +5,5 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val spdeSbt = "us.technically.spde" % "spde-sbt-plugin" % "0.4.1"
 //  val repo = "GH-pages repo" at "http://mpeltonen.github.com/maven/"
 //  val idea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.1-SNAPSHOT"
+  val bnd4sbt = "com.weiglewilczek" % "bnd4sbt" % "0.4"  
 }
