341ab906de8cb06a1988271b56ef7d4f6c0f03a9
Roman Roelofsen <r.roelofsen@tuxed.de>
2010-06-07 15:59:47 +0200

    Removed Maven projects and added bnd4sbt

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -5,4 +5,6 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val spdeSbt = "us.technically.spde" % "spde-sbt-plugin" % "0.4.1"
 //  val repo = "GH-pages repo" at "http://mpeltonen.github.com/maven/"
 //  val idea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.1-SNAPSHOT"
+
+  lazy val bnd4sbt = "com.weiglewilczek" % "bnd4sbt" % "0.4"
 }
