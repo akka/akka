07ac1d990c8a857e9b5c312e01cd9ffa49398132
Heiko Seeberger <heiko.seeberger@googlemail.com>
2010-06-21 12:47:23 +0200

    re #281: Made all subprojects compile  after breaking changes introduced by removing the type parameter from ActorRef.!!; test-compile still missing!

--- akka-samples/akka-sample-chat/src/main/scala/ChatServer.scala
+++ akka-samples/akka-sample-chat/src/main/scala/ChatServer.scala
@@ -63,10 +63,10 @@ case class ChatMessage(from: String, message: String) extends Event
 class ChatClient(val name: String) {
   val chat = RemoteClient.actorFor("chat:service", "localhost", 9999)
 
-  def login =                 chat ! Login(name)
-  def logout =                chat ! Logout(name)
+  def login                 = chat ! Login(name)
+  def logout                = chat ! Logout(name)
   def post(message: String) = chat ! ChatMessage(name, name + ": " + message)
-  def chatLog: ChatLog =     (chat !! GetChatLog(name)).getOrElse(throw new Exception("Couldn't get the chat log from ChatServer"))
+  def chatLog               = (chat !! GetChatLog(name)).as[ChatLog].getOrElse(throw new Exception("Couldn't get the chat log from ChatServer"))
 }
 
 /**
