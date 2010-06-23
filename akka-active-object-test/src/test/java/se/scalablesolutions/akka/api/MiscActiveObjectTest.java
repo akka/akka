421aec4cfc1a0efc87814f7df318a84dc4d63949
Johan Rask <johan.rask@jayway.com>
2010-06-23 09:50:55 +0200

    Added test for verifying pre/post restart invocations

---
+++ akka-active-object-test/src/test/java/se/scalablesolutions/akka/api/MiscActiveObjectTest.java
@@ -0,0 +1,35 @@
+package se.scalablesolutions.akka.api;
+
+import static se.scalablesolutions.akka.actor.ActiveObject.link;
+import static se.scalablesolutions.akka.actor.ActiveObject.newInstance;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import se.scalablesolutions.akka.config.OneForOneStrategy;
+import junit.framework.TestCase;
+
+/**
+ * <p>Small misc tests that do not fit anywhere else and does not require a separate testcase</p>
+ * 
+ * @author johanrask
+ *
+ */
+public class MiscActiveObjectTest extends TestCase {
+
+	
+	/**
+	 * Verifies that both preRestart and postRestart methods are invoked when
+	 * an actor is restarted
+	 */
+	public void testFailingPostRestartInvocation() throws InterruptedException {
+		SimpleJavaPojo pojo = newInstance(SimpleJavaPojo.class,500);
+		SimpleJavaPojo supervisor = newInstance(SimpleJavaPojo.class,500);
+		link(supervisor,pojo,new OneForOneStrategy(3, 2000),new Class[]{Throwable.class});
+		pojo.throwException();
+		Thread.sleep(500);
+		Assert.assertTrue(pojo.pre);
+		Assert.assertTrue(pojo.post);
+	}
+	
+}
