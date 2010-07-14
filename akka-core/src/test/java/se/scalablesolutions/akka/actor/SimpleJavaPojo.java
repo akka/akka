6ac052be04f1e3371d983f6e5e87892481c282d3
Jonas Bonér <jonasremove@jonasboner.com>
2010-07-14 13:47:24 +0200

    Added tests for ActiveObjectContext

--- akka-core/src/test/java/se/scalablesolutions/akka/actor/SimpleJavaPojo.java
+++ akka-core/src/test/java/se/scalablesolutions/akka/actor/SimpleJavaPojo.java
@@ -2,35 +2,47 @@ package se.scalablesolutions.akka.actor;
 
 import se.scalablesolutions.akka.actor.annotation.prerestart;
 import se.scalablesolutions.akka.actor.annotation.postrestart;
+import se.scalablesolutions.akka.actor.ActiveObjectContext;
+import se.scalablesolutions.akka.dispatch.CompletableFuture;
 
 public class SimpleJavaPojo {
-        
-        public boolean pre = false;
-        public boolean post = false;
-        
-        private String name;
-        
-        public void setName(String name) {
-                this.name = name;
-        }
-        
-        public String getName() {
-                return name;
-        }
-        
-        @prerestart
-        public void pre() {
-                System.out.println("** pre()");
-                pre = true;
-        }
-        
-        @postrestart
-        public void post() {
-                System.out.println("** post()");
-                post = true;
-        }
 
-        public void throwException() {
-                throw new RuntimeException();
-        }
+  ActiveObjectContext context;
+  
+  public boolean pre = false;
+  public boolean post = false;
+
+  private String name;
+
+  public Object getSender() {
+    return context.getSender();
+  }
+
+  public CompletableFuture<Object> getSenderFuture() {
+    return context.getSenderFuture();
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  @prerestart
+  public void pre() {
+    System.out.println("** pre()");
+    pre = true;
+  }
+
+  @postrestart
+  public void post() {
+    System.out.println("** post()");
+    post = true;
+  }
+
+  public void throwException() {
+    throw new RuntimeException();
+  }
 }
