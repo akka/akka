ea5ce25474070038ef625771344de814f49cf894
Jonas Boner <jonas@jonasboner.com>
2010-07-30 00:46:35 +0200

    removed trailing whitespace

--- akka-core/src/test/java/se/scalablesolutions/akka/actor/ReplyUntypedActor.java
+++ akka-core/src/test/java/se/scalablesolutions/akka/actor/ReplyUntypedActor.java
@@ -4,25 +4,25 @@ import se.scalablesolutions.akka.actor.*;
 
 public class ReplyUntypedActor extends UntypedActor {
   public void onReceive(Object message, UntypedActorRef context) throws Exception {
-  	if (message instanceof String) {
+        if (message instanceof String) {
       String str = (String)message;
 
-	    if (str.equals("ReplyToSendOneWayUsingReply")) {
-	      context.replyUnsafe("Reply");
+            if (str.equals("ReplyToSendOneWayUsingReply")) {
+              context.replyUnsafe("Reply");
       } else if (str.equals("ReplyToSendOneWayUsingSender")) {
-      	context.getSender().get().sendOneWay("Reply");
+        context.getSender().get().sendOneWay("Reply");
 
       } else if (str.equals("ReplyToSendRequestReplyUsingReply")) {
-	      context.replyUnsafe("Reply");
+              context.replyUnsafe("Reply");
       } else if (str.equals("ReplyToSendRequestReplyUsingFuture")) {
-      	context.getSenderFuture().get().completeWithResult("Reply");
+        context.getSenderFuture().get().completeWithResult("Reply");
 
       } else if (str.equals("ReplyToSendRequestReplyFutureUsingReply")) {
-	      context.replyUnsafe("Reply");
+              context.replyUnsafe("Reply");
       } else if (str.equals("ReplyToSendRequestReplyFutureUsingFuture")) {
-      	context.getSenderFuture().get().completeWithResult("Reply");
+        context.getSenderFuture().get().completeWithResult("Reply");
 
       } else throw new IllegalArgumentException("Unknown message: " + str);
-  	} else throw new IllegalArgumentException("Unknown message: " + message);
+        } else throw new IllegalArgumentException("Unknown message: " + message);
   }
 }
