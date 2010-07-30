ac77ce525be750c2d5383e68401312072bd2bde5
Jonas Boner <jonas@jonasboner.com>
2010-07-30 18:23:53 +0200

    Restructured test folder structure

---
+++ akka-core/src/test/scala/ticket/Ticket001Spec.scala
@@ -0,0 +1,16 @@
+package se.scalablesolutions.akka.actor.ticket
+
+import org.scalatest.junit.JUnitSuite
+import org.junit.Test
+
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
+
+class Ticket001Spec extends WordSpec with MustMatchers {
+
+  "An XXX" should {
+    "do YYY" in {
+      1 must be (1)
+    }
+  }
+}
