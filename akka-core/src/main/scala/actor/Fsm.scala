0f26ccd42724ebeeb8398d7e1486dc95f5cd9592
momania <irmo.manie@gmail.com>
2010-08-19 17:31:59 +0200

    Dining hakkers on fsm

--- akka-core/src/main/scala/actor/Fsm.scala
+++ akka-core/src/main/scala/actor/Fsm.scala
@@ -1,56 +1,53 @@
 package actor
 
-import java.util.concurrent.TimeUnit
-import se.scalablesolutions.akka.actor.{ActorRef, Scheduler, Actor}
+import se.scalablesolutions.akka.actor.{Scheduler, Actor}
 import se.scalablesolutions.akka.stm.Ref
 import se.scalablesolutions.akka.stm.local._
+import java.util.concurrent.{ScheduledFuture, TimeUnit}
 
-trait Fsm[S] { self: Actor =>
-  
+trait Fsm[S] {
+  this: Actor =>
+
   type StateFunction = scala.PartialFunction[Event, State]
 
-  val stateRef: Ref[State] = Ref(initialState)
-  @volatile var timeoutActor: Option[ActorRef] = None
+  var currentState: State = initialState
+  var timeoutFuture: Option[ScheduledFuture[AnyRef]] = None
 
   def initialState: State
 
   def handleEvent: StateFunction = {
-    case event@Event(value,stateData) =>
-      log.warning("No state for event with value %s - keeping current state %s", value, stateData)
-      val currentState: State = stateRef.getOrElse(initialState)
+    case event@Event(value, stateData) =>
+      log.warning("No state for event with value %s - keeping current state %s at %s", value, stateData, self.id)
       State(NextState, currentState.stateFunction, stateData, currentState.timeout)
   }
 
 
-  override protected def receive: Receive = {
+  override final protected def receive: Receive = {
     case value => {
-      atomic {
-        timeoutActor = timeoutActor.flatMap{ref => Scheduler.unschedule(ref); None}
+      timeoutFuture = timeoutFuture.flatMap {ref => ref.cancel(true); None}
 
-        val currentState: State = stateRef.getOrElse(initialState)
-        val event = Event(value, currentState.stateData)
-        val newState = (currentState.stateFunction orElse handleEvent).apply(event)
+      val event = Event(value, currentState.stateData)
+      val newState = (currentState.stateFunction orElse handleEvent).apply(event)
 
-        newState match {
-          case State(Reply, _, _, _, Some(replyValue)) => this.self.reply(replyValue)
-          case _ => () // ignore for now
-        }
+      currentState = newState
 
-        newState.timeout.foreach{timeout =>
-          timeoutActor = Some(Scheduler.scheduleOnce(this.self, StateTimeout, timeout, TimeUnit.MILLISECONDS))
-        }
+      newState match {
+        case State(Reply, _, _, _, Some(replyValue)) => self.sender.foreach(_ ! replyValue)
+        case _ => () // ignore for now
+      }
 
-        stateRef.swap(newState)
+      newState.timeout.foreach {
+        timeout =>
+          timeoutFuture = Some(Scheduler.scheduleOnce(self, StateTimeout, timeout, TimeUnit.MILLISECONDS))
       }
     }
   }
 
-  
   case class State(stateEvent: StateEvent,
                    stateFunction: StateFunction,
                    stateData: S,
                    timeout: Option[Int] = None,
-                   replyValue: Option[Any] =None)
+                   replyValue: Option[Any] = None)
 
   case class Event(event: Any, stateData: S)
 
