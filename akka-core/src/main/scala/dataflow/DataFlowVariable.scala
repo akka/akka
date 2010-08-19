d825a5882f0c6d1827f1b2e0a35c6c60d33fb718
Viktor Klang <viktor.klang@gmail.com>
2010-08-19 17:34:32 +0200

    Fixing bugs in DataFlowVariable and adding tests

--- akka-core/src/main/scala/dataflow/DataFlowVariable.scala
+++ akka-core/src/main/scala/dataflow/DataFlowVariable.scala
@@ -36,7 +36,7 @@ import se.scalablesolutions.akka.dispatch.CompletableFuture
   private class IsolatedEventBasedThread(body: => Unit) extends Actor {
     def receive = {
       case Start => body
-      case Exit => exit
+      case Exit => self.stop
     }
   }
 
@@ -46,7 +46,7 @@ import se.scalablesolutions.akka.dispatch.CompletableFuture
   private class ReactiveEventBasedThread[A <: AnyRef, T <: AnyRef](body: A => T)
     extends Actor {
     def receive = {
-      case Exit => exit
+      case Exit => self.stop
       case message => self.reply(body(message.asInstanceOf[A]))
     }
   }
@@ -74,7 +74,7 @@ import se.scalablesolutions.akka.dispatch.CompletableFuture
             dataFlow.blockedReaders.clear
           } else throw new DataFlowVariableException(
             "Attempt to change data flow variable (from [" + dataFlow.value.get + "] to [" + v + "])")
-        case Exit =>  exit
+        case Exit =>  self.stop
       }
     }
 
@@ -87,7 +87,7 @@ import se.scalablesolutions.akka.dispatch.CompletableFuture
           if (ref.isDefined) self.reply(ref.get)
           else readerFuture = self.senderFuture.asInstanceOf[Option[CompletableFuture[T]]]
         case Set(v:T) => if (readerFuture.isDefined) readerFuture.get.completeWithResult(v)
-        case Exit =>  exit
+        case Exit =>  self.stop
       }
     }
 
@@ -158,238 +158,3 @@ import se.scalablesolutions.akka.dispatch.CompletableFuture
    */
   class DataFlowVariableException(msg: String) extends AkkaException(msg)
 }
-
-
-// ==========================
-// ======== EXAMPLES ========
-// ==========================
-
-object Test1 extends Application {
-
-  // =======================================
-  // This example is rom Oz wikipedia page: http://en.wikipedia.org/wiki/Oz_(programming_language)
-
-  /*
-  thread
-    Z = X+Y     % will wait until both X and Y are bound to a value.
-    {Browse Z}  % shows the value of Z.
-  end
-  thread X = 40 end
-  thread Y = 2 end
-  */
-
-  import DataFlow._
-  val x, y, z = new DataFlowVariable[Int]
-  thread {
-    z << x() + y()
-    println("z = " + z())
-  }
-  thread { x << 40 }
-  thread { y << 2 }
-}
-
-// =======================================
-object Test2 extends Application {
-
-  /*
-  fun {Ints N Max}
-    if N == Max then nil
-    else
-      {Delay 1000}
-      N|{Ints N+1 Max}
-    end
-  end
-
-  fun {Sum S Stream}
-    case Stream of nil then S
-    [] H|T then S|{Sum H+S T} end
-  end
-
-  local X Y in
-    thread X = {Ints 0 1000} end
-    thread Y = {Sum 0 X} end
-    {Browse Y}
-  end
-  */
-
-  import DataFlow._
-
-  def ints(n: Int, max: Int): List[Int] =
-    if (n == max) Nil
-    else n :: ints(n + 1, max)
-
-  def sum(s: Int, stream: List[Int]): List[Int] = stream match {
-    case Nil => s :: Nil
-    case h :: t => s :: sum(h + s, t)
-  }
-
-  val x = new DataFlowVariable[List[Int]]
-  val y = new DataFlowVariable[List[Int]]
-
-  thread { x << ints(0, 1000) }
-  thread { y << sum(0, x()) }
-  thread { println("List of sums: " + y()) }
-}
-
-// =======================================
-object Test3 extends Application {
-
-  // Using DataFlowStream and foldLeft to calculate sum
-
-  /*
-  fun {Ints N Max}
-    if N == Max then nil
-    else
-      {Delay 1000}
-      N|{Ints N+1 Max}
-    end
-  end
-
-  fun {Sum S Stream}
-    case Stream of nil then S
-    [] H|T then S|{Sum H+S T} end
-  end
-
-  local X Y in
-    thread X = {Ints 0 1000} end
-    thread Y = {Sum 0 X} end
-    {Browse Y}
-  end
-  */
-
-  import DataFlow._
-
-  def ints(n: Int, max: Int, stream: DataFlowStream[Int]): Unit = if (n != max) {
-    println("Generating int: " + n)
-    stream <<< n
-    ints(n + 1, max, stream)
-  }
-
-  def sum(s: Int, in: DataFlowStream[Int], out: DataFlowStream[Int]): Unit = {
-    println("Calculating: " + s)
-    out <<< s
-    sum(in() + s, in, out)
-  }
-
-  def printSum(stream: DataFlowStream[Int]): Unit = {
-    println("Result: " + stream())
-    printSum(stream)
-  }
-
-  val producer = new DataFlowStream[Int]
-  val consumer = new DataFlowStream[Int]
-
-  thread { ints(0, 1000, producer) }
-  thread {
-    Thread.sleep(1000)
-    println("Sum: " + producer.map(x => x * x).foldLeft(0)(_ + _))
-  }
-}
-
-
-// =======================================
-object Test4 extends Application {
-
-  // Using DataFlowStream and recursive function to calculate sum
-
-  /*
-  fun {Ints N Max}
-    if N == Max then nil
-    else
-      {Delay 1000}
-      N|{Ints N+1 Max}
-    end
-  end
-
-  fun {Sum S Stream}
-    case Stream of nil then S
-    [] H|T then S|{Sum H+S T} end
-  end
-
-  local X Y in
-    thread X = {Ints 0 1000} end
-    thread Y = {Sum 0 X} end
-    {Browse Y}
-  end
-  */
-
-  import DataFlow._
-
-  def ints(n: Int, max: Int, stream: DataFlowStream[Int]): Unit = if (n != max) {
-    println("Generating int: " + n)
-    stream <<< n
-    ints(n + 1, max, stream)
-  }
-
-  def sum(s: Int, in: DataFlowStream[Int], out: DataFlowStream[Int]): Unit = {
-    println("Calculating: " + s)
-    out <<< s
-    sum(in() + s, in, out)
-  }
-
-  def printSum(stream: DataFlowStream[Int]): Unit = {
-    println("Result: " + stream())
-    printSum(stream)
-  }
-
-  val producer = new DataFlowStream[Int]
-  val consumer = new DataFlowStream[Int]
-
-  thread { ints(0, 1000, producer) }
-  thread { sum(0, producer, consumer) }
-  thread { printSum(consumer) }
-}
-
-
-// =======================================
-object Test5 extends Application {
-  import DataFlow._
-
-  // create four 'Int' data flow variables
-  val x, y, z, v = new DataFlowVariable[Int]
-
-  val main = thread {
-    println("Thread 'main'")
-
-    x << 1
-    println("'x' set to: " + x())
-
-    println("Waiting for 'y' to be set...")
-
-    if (x() > y()) {
-      z << x
-      println("'z' set to 'x': " + z())
-    } else {
-      z << y
-      println("'z' set to 'y': " + z())
-    }
-
-    // main completed, shut down the data flow variables
-    x.shutdown
-    y.shutdown
-    z.shutdown
-    v.shutdown
-  }
-
-  val setY = thread {
-    println("Thread 'setY', sleeping...")
-    Thread.sleep(5000)
-    y << 2
-    println("'y' set to: " + y())
-  }
-
-  val setV = thread {
-    println("Thread 'setV'")
-    v << y
-    println("'v' set to 'y': " + v())
-  }
-
-  // shut down the threads
-  main ! Exit
-  setY ! Exit
-  setV ! Exit
-
-  //System.gc
-}
-
-
