fb2d77762011dee3413a5cbb257a4f365074d2aa
Viktor Klang <viktor.klang@gmail.com>
2010-08-19 10:55:22 +0200

    Changing Listeners backing store to ConcurrentSkipListSet and changing signature of WithListeners(f) to (ActorRef) => Unit

--- akka-core/src/main/scala/routing/Listeners.scala
+++ akka-core/src/main/scala/routing/Listeners.scala
@@ -5,13 +5,13 @@
 package se.scalablesolutions.akka.routing
 
 import se.scalablesolutions.akka.actor.{Actor, ActorRef}
-
-import java.util.concurrent.CopyOnWriteArraySet
+import java.util.concurrent.ConcurrentSkipListSet
+import scala.collection.JavaConversions._
 
 sealed trait ListenerMessage
 case class Listen(listener: ActorRef) extends ListenerMessage
 case class Deafen(listener: ActorRef) extends ListenerMessage
-case class WithListeners(f: List[ActorRef] => Unit) extends ListenerMessage
+case class WithListeners(f: (ActorRef) => Unit) extends ListenerMessage
 
 /**
  * Listeners is a generic trait to implement listening capability on an Actor.
@@ -25,15 +25,13 @@ case class WithListeners(f: List[ActorRef] => Unit) extends ListenerMessage
  * Send <code>WithListeners(fun)</code> to traverse the current listeners.
  */
 trait Listeners { self: Actor =>
-  private val listeners = new CopyOnWriteArraySet[ActorRef]
+  private val listeners = new ConcurrentSkipListSet[ActorRef]
 
   protected def listenerManagement: Receive = {
     case Listen(l)        => listeners add l
     case Deafen(l)        => listeners remove l
-    case WithListeners(f) => f(listenersAsList)
+    case WithListeners(f) => listeners foreach f
   }
 
-  protected def gossip(msg: Any) = listenersAsList foreach (_ ! msg)
-
-  private def listenersAsList: List[ActorRef] = listeners.toArray.toList.asInstanceOf[List[ActorRef]]
+  protected def gossip(msg: Any) = listeners foreach (_ ! msg)
 }
