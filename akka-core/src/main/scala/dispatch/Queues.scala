0077a14099969a13c1f8387e601e300d6163f73d
Viktor Klang <viktor.klang@gmail.com>
2010-08-27 17:56:09 +0200

    Added boilerplate to improve BoundedTransferQueue performance

--- akka-core/src/main/scala/dispatch/Queues.scala
+++ akka-core/src/main/scala/dispatch/Queues.scala
@@ -20,31 +20,23 @@ class BoundedTransferQueue[E <: AnyRef](
   
   protected val guard = new Semaphore(capacity)
 
-  //Enqueue an item within the push timeout (acquire Semaphore)
-  protected def enq(f: => Boolean): Boolean = {
-    if (guard.tryAcquire(pushTimeout,pushTimeUnit)) {
-      val result = try {
-        f
-      } catch {
-        case e =>
-          guard.release //If something broke, release
-          throw e
-      }
-      if (!result) guard.release //Didn't add anything
-      result
-    } else
-      false
-  }
-
-  //Dequeue an item (release Semaphore)
-  protected def deq(e: E): E = {
-    if (e ne null) guard.release //Signal removal of item
+  override def take(): E = {
+    val e = super.take
+    if (e ne null) guard.release
     e
   }
 
-  override def take(): E = deq(super.take)
-  override def poll(): E = deq(super.poll)
-  override def poll(timeout: Long, unit: TimeUnit): E = deq(super.poll(timeout,unit))
+  override def poll(): E = {
+    val e = super.poll
+    if (e ne null) guard.release
+    e
+  }
+
+  override def poll(timeout: Long, unit: TimeUnit): E = {
+    val e = super.poll(timeout,unit)
+    if (e ne null) guard.release
+    e
+  }
 
   override def remainingCapacity = guard.availablePermits
 
@@ -57,26 +49,90 @@ class BoundedTransferQueue[E <: AnyRef](
     }
   }
 
-  override def offer(e: E): Boolean =
-    enq(super.offer(e))
+  override def offer(e: E): Boolean = {
+    if (guard.tryAcquire(pushTimeout,pushTimeUnit)) {
+      val result = try {
+        super.offer(e)
+      } catch {
+        case e => guard.release; throw e
+      }
+      if (!result) guard.release
+      result
+    } else
+      false
+  }
 
-  override def offer(e: E, timeout: Long, unit: TimeUnit): Boolean =
-    enq(super.offer(e,timeout,unit))
+  override def offer(e: E, timeout: Long, unit: TimeUnit): Boolean = {
+    if (guard.tryAcquire(pushTimeout,pushTimeUnit)) {
+      val result = try {
+        super.offer(e,timeout,unit)
+      } catch {
+        case e => guard.release; throw e
+      }
+      if (!result) guard.release
+      result
+    } else
+      false
+  }
 
-  override def add(e: E): Boolean =
-    enq(super.add(e))
+  override def add(e: E): Boolean = {
+    if (guard.tryAcquire(pushTimeout,pushTimeUnit)) {
+      val result = try {
+        super.add(e)
+      } catch {
+        case e => guard.release; throw e
+      }
+      if (!result) guard.release
+      result
+    } else
+      false
+  }
 
-  override def put(e :E): Unit =
-    enq({ super.put(e); true })
+  override def put(e :E): Unit = {
+    if (guard.tryAcquire(pushTimeout,pushTimeUnit)) {
+      try {
+        super.put(e)
+      } catch {
+        case e => guard.release; throw e
+      }
+    }
+  }
 
-  override def tryTransfer(e: E): Boolean =
-    enq(super.tryTransfer(e))
+  override def tryTransfer(e: E): Boolean = {
+    if (guard.tryAcquire(pushTimeout,pushTimeUnit)) {
+      val result = try {
+        super.tryTransfer(e)
+      } catch {
+        case e => guard.release; throw e
+      }
+      if (!result) guard.release
+      result
+    } else
+      false
+  }
 
-  override def tryTransfer(e: E, timeout: Long, unit: TimeUnit): Boolean =
-    enq(super.tryTransfer(e,timeout,unit))
+  override def tryTransfer(e: E, timeout: Long, unit: TimeUnit): Boolean = {
+    if (guard.tryAcquire(pushTimeout,pushTimeUnit)) {
+      val result = try {
+        super.tryTransfer(e,timeout,unit)
+      } catch {
+        case e => guard.release; throw e
+      }
+      if (!result) guard.release
+      result
+    } else
+      false
+  }
   
-  override def transfer(e: E): Unit =
-    enq({ super.transfer(e); true })
+  override def transfer(e: E): Unit = {
+    if (guard.tryAcquire(pushTimeout,pushTimeUnit)) {
+      try {
+        super.transfer(e)
+      } catch {
+        case e => guard.release; throw e
+      }
+    }
+  }
 
   override def iterator: Iterator[E] = {
     val it = super.iterator
