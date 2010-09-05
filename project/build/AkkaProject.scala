485aebbeac85f1043bd0d03023a8e1cc890e8938
Debasish Ghosh <debasish@debasishg.(none)>
2010-09-05 21:31:31 +0530

    redisclient support for Redis 2.0. Not fully backward compatible, since Redis 2.0 has some differences with 1.x

--- project/build/AkkaProject.scala
+++ project/build/AkkaProject.scala
@@ -176,7 +176,7 @@ class AkkaParentProject(info: ProjectInfo) extends DefaultProject(info) {
 
     lazy val rabbit = "com.rabbitmq" % "amqp-client" % "1.8.1" % "compile"
 
-    lazy val redis = "com.redis" % "redisclient" % "2.8.0-1.4" % "compile"
+    lazy val redis = "com.redis" % "redisclient" % "2.8.0-2.0" % "compile"
 
     lazy val sbinary = "sbinary" % "sbinary" % "2.8.0-0.3.1" % "compile"
 
