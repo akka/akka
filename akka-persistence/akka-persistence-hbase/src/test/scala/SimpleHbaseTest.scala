fb2ba7ec0ccf79c5d451d8ccaee924891f18d114
David Greco <dgreco@family.local>
2010-09-15 16:06:02 +0200

    working on the hbase integration

--- akka-persistence/akka-persistence-hbase/src/test/scala/SimpleHbaseTest.scala
+++ akka-persistence/akka-persistence-hbase/src/test/scala/SimpleHbaseTest.scala
@@ -42,6 +42,22 @@ class PersistenceSpec extends Spec with BeforeAndAfterAll with ShouldMatchers {
       table should not equal(null)
     }
 
+    it("should use the quorum read from the akka configuration") {
+      import se.scalablesolutions.akka.config.Config.config
+      import org.apache.hadoop.hbase.HBaseConfiguration
+      import org.apache.hadoop.hbase.client.HBaseAdmin
+      import org.apache.hadoop.hbase.client.HTable
+
+      val HBASE_ZOOKEEPER_QUORUM = config.getString("akka.storage.hbase.zookeeper.quorum", "0")
+      HBASE_ZOOKEEPER_QUORUM should not equal("0")
+      HBASE_ZOOKEEPER_QUORUM should equal("localhost")
+      
+      val configuration = new HBaseConfiguration
+      configuration.set("hbase.zookeeper.quorum", HBASE_ZOOKEEPER_QUORUM)
+      val admin = new HBaseAdmin(configuration)
+      admin.tableExists("ATable") should equal(true)
+   }
+
   }
 
 }
