a14697e6ca31d99a7cb3862e376a4588e1abe9b1
David Greco <dgreco@family.local>
2010-09-15 15:03:22 +0200

    Added a simple test showing how to use Hbase testing utilities

--- akka-persistence/akka-persistence-hbase/src/test/scala/SimpleHbaseTest.scala
+++ akka-persistence/akka-persistence-hbase/src/test/scala/SimpleHbaseTest.scala
@@ -6,20 +6,42 @@ import org.scalatest.BeforeAndAfterAll
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
 import org.junit.Test
-import org.apache.hadoop.hbase.HBaseClusterTestCase
 
-@Test
-class PersistenceTest extends HBaseClusterTestCase with Spec with BeforeAndAfterAll {
+import org.apache.hadoop.hbase.HBaseTestingUtility
+
+@RunWith(classOf[JUnitRunner])
+class PersistenceSpec extends Spec with BeforeAndAfterAll with ShouldMatchers {
+
+  import org.apache.hadoop.hbase.HBaseTestingUtility
+
+  val testUtil = new HBaseTestingUtility
 
   override def beforeAll {
-    super.setUp
+    testUtil.startMiniCluster
   }
 
-  @Test
-  def testPersistence {}
-
   override def afterAll {
-    super.tearDown
+    testUtil.shutdownMiniCluster
+  }
+
+  describe("simple hbase persistence test") {
+    it("should create a table") {
+      import org.apache.hadoop.hbase.util.Bytes
+      import org.apache.hadoop.hbase.HTableDescriptor
+      import org.apache.hadoop.hbase.HColumnDescriptor
+      import org.apache.hadoop.hbase.client.HBaseAdmin
+      import org.apache.hadoop.hbase.client.HTable
+
+      val descriptor = new HTableDescriptor(Bytes.toBytes("ATable"))
+      descriptor.addFamily(new HColumnDescriptor(Bytes.toBytes("Family1")))
+      descriptor.addFamily(new HColumnDescriptor(Bytes.toBytes("Family2")))
+      val admin = new HBaseAdmin(testUtil.getConfiguration)
+      admin.createTable(descriptor)
+      val table = new HTable(testUtil.getConfiguration, Bytes.toBytes("ATable"))
+ 
+      table should not equal(null)
+    }
+
   }
 
 }
