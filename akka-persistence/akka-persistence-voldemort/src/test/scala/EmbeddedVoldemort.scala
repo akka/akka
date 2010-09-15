e617b1381079c828c568f42967d84916aaa5b08b
ticktock <ticktock@ticktock.com>
2010-09-15 19:37:54 -0400

    Initial tests working with bdb backed voldemort,

--- akka-persistence/akka-persistence-voldemort/src/test/scala/EmbeddedVoldemort.scala
+++ akka-persistence/akka-persistence-voldemort/src/test/scala/EmbeddedVoldemort.scala
@@ -1,29 +1,33 @@
 package se.scalablesolutions.akka.persistence.voldemort
 
 import org.scalatest.matchers.ShouldMatchers
-import se.scalablesolutions.akka.util.UUID
 import voldemort.server.{VoldemortServer, VoldemortConfig}
 import org.scalatest.{Suite, BeforeAndAfterAll, FunSuite}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import voldemort.utils.Utils
 import java.io.File
+import se.scalablesolutions.akka.util.{Logging, UUID}
 
 @RunWith(classOf[JUnitRunner])
-trait EmbeddedVoldemort extends BeforeAndAfterAll {
+trait EmbeddedVoldemort extends BeforeAndAfterAll with Logging {
   this: Suite =>
   var server: VoldemortServer = null
 
   override protected def beforeAll(): Unit = {
     
     try {
-      val dir = "./akka-persistence/akka-persistence-voldemort/src/test/resources"
+      val dir = "./akka-persistence/akka-persistence-voldemort/target/scala_2.8.0/test-resources"
       val home = new File(dir)
+      log.info("Creating Voldemort Config")
       val config = VoldemortConfig.loadFromVoldemortHome(home.getCanonicalPath)
+      log.info("Starting Voldemort")
       server = new VoldemortServer(config)
-      server.start        
+      server.start
+      log.info("Started")
     } catch {
-      case e => e.printStackTrace
+      case e => log.error(e, "Error Starting Voldemort")
+      throw e
     }
   }
 
--- akka-persistence/akka-persistence-voldemort/src/test/scala/VoldemortStorageBackendSuite.scala
+++ akka-persistence/akka-persistence-voldemort/src/test/scala/VoldemortStorageBackendSuite.scala
@@ -7,46 +7,66 @@ import org.scalatest.junit.JUnitRunner
 import se.scalablesolutions.akka.persistence.voldemort.VoldemortStorageBackend._
 import se.scalablesolutions.akka.util.{Logging, UUID}
 import collection.immutable.TreeSet
+import VoldemortStorageBackendSuite._
 
 @RunWith(classOf[JUnitRunner])
 class VoldemortStorageBackendSuite extends FunSuite with ShouldMatchers with EmbeddedVoldemort with Logging {
   test("that ref storage and retrieval works") {
-    refClient.put("testRef", "testRefValue".getBytes("UTF-8"))
-    new String(refClient.getValue("testRef", Array.empty[Byte]), "UTF-8") should be("testRefValue")
+    val key = "testRef"
+    val value = "testRefValue"
+    val valueBytes = bytes(value)
+    refClient.delete(key)
+    refClient.getValue(key, empty) should be(empty)
+    refClient.put(key, valueBytes)
+    refClient.getValue(key) should be(valueBytes)
   }
 
   test("that map key storage and retrieval works") {
-    val mapKeys = new TreeSet[Array[Byte]] + "key1".getBytes
-    mapKeyClient.put("testMapKey", mapKeys)
-    val returned = mapKeyClient.getValue("testMapKey", new TreeSet[Array[Byte]])
-    returned should equal(mapKeys)
+    val key = "testmapKey"
+    val mapKeys = new TreeSet[Array[Byte]] + bytes("key1")
+    mapKeyClient.delete(key)
+    mapKeyClient.getValue(key, emptySet) should equal(emptySet)
+    mapKeyClient.put(key, mapKeys)
+    mapKeyClient.getValue(key, emptySet) should equal(mapKeys)
+
   }
 
   test("that map value storage and retrieval works") {
-    val key = "keyForTestingMapValueClient".getBytes("UTF-8")
-    val value = "value for testing map value client".getBytes("UTF-8")
+    val key = bytes("keyForTestingMapValueClient")
+    val value = bytes("value for testing map value client")
     mapValueClient.put(key, value)
-    mapValueClient.getValue(key) should equal(value)
+    mapValueClient.getValue(key, empty) should equal(value)
   }
 
   test("that vector size storage and retrieval works") {
     val key = "vectorKey"
-    vectorSizeClient.put(key, IntSerializer.toBytes(17))
-    vectorSizeClient.getValue(key) should equal(IntSerializer.toBytes(17))
+    val size = IntSerializer.toBytes(17)
+    vectorSizeClient.delete(key)
+    vectorSizeClient.getValue(key, empty) should equal(empty)
+    vectorSizeClient.put(key, size)
+    vectorSizeClient.getValue(key) should equal(size)
   }
 
   test("that vector value storage and retrieval works") {
     val key = "vectorValueKey"
     val index = 3
-    val value = "some bytes".getBytes("UTF-8")
+    val value = bytes("some bytes")
     val vecKey = getVectorValueKey(key, index)
-    try{
-    val idx = getIndexFromVectorValueKey(key, vecKey)
+    getIndexFromVectorValueKey(key, vecKey) should be(index)
+    vectorValueClient.delete(vecKey)
+    vectorValueClient.getValue(vecKey, empty) should equal(empty)
     vectorValueClient.put(vecKey, value)
-    vectorValueClient.get(vecKey) should equal(value)
-    } catch{
-      case e => e.printStackTrace
-    }
+    vectorValueClient.getValue(vecKey) should equal(value)
+  }
+
+}
+
+object VoldemortStorageBackendSuite {
+  val empty = Array.empty[Byte]
+  val emptySet = new TreeSet[Array[Byte]]
+
+  def bytes(value: String): Array[Byte] = {
+    value.getBytes("UTF-8")
   }
 
 }
