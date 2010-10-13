20007f7355c6e49d2e1d72fda987afa6f91c7c5b
ticktock <ticktock@ticktock.com>
2010-10-13 17:03:27 -0400

    Initial Port of the Voldemort Backend to Riak
    this is using the protobuf interface now, but may have to try to the http interface, since there seems to be some race conditions internally to riak during rapid updates/deletes, which may not happen with http
    
    Basically  single threaded tests that involve rapid updates/deletes fail. When the same tests are run in a debugger, and breakpoints are set such that there is time in between riak requests, the tests pass. :(

--- akka-persistence/akka-persistence-riak/src/test/scala/RiakStorageBackendTestIntegration.scala
+++ akka-persistence/akka-persistence-riak/src/test/scala/RiakStorageBackendTestIntegration.scala
@@ -8,17 +8,46 @@ import se.scalablesolutions.akka.util.{Logging}
 import collection.immutable.TreeSet
 import scala.None
 import org.scalatest.{Spec, FunSuite}
-import com.trifork.riak.RiakClient
+import com.trifork.riak.{RiakObject, RiakClient}
+import collection.JavaConversions
 
 @RunWith(classOf[JUnitRunner])
 class RiakStorageBackendTestIntegration extends Spec with ShouldMatchers  with Logging {
 
+  import se.scalablesolutions.akka.persistence.riak.RiakStorageBackend.RiakAccess._
 
   describe("successfuly configuring the riak pb client"){
     it("should connect to riak, if riak is running"){
       val riakClient = new RiakClient("localhost");
+      val props = riakClient.getServerInfo
+      JavaConversions.asMap(props) foreach {
+        _ match {
+          case (k,v) => log.info("%s -> %s",k,v)
+        }
+      }
+      val maps = riakClient.getBucketProperties("Maps")
+      debug(maps)
       riakClient.listBuckets should not be (null)
+      riakClient.store(new RiakObject("Maps", "testkey", "testvalue"))
+      riakClient.fetch("Maps", "testkey").isEmpty should be (false)
+      riakClient.fetch("Maps", "testkey")(0).getValue.toStringUtf8 should be("testvalue")
+      //riakClient.delete("Maps","testkey")
+      RiakStorageBackend.MapClient.delete("testkey")
+      RiakStorageBackend.VectorClient.quorum
+      riakClient.fetch("Maps", "testkey").isEmpty should be (true)
+      riakClient.store(new RiakObject("Maps", "testkey", "testvalue"))
+      riakClient.fetch("Maps", "testkey").isEmpty should be (false)
+      riakClient.fetch("Maps", "testkey")(0).getValue.toStringUtf8 should be("testvalue")
+      //riakClient.delete("Maps","testkey")
+      RiakStorageBackend.MapClient.delete("testkey")
+
+      riakClient.fetch("Maps", "testkey").isEmpty should be (true)
     }
+  }
+
+
+  def debug(ani:Any){
+    log.debug("ani")
   }
 
 }
