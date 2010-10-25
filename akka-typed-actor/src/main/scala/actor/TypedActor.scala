5c9fab80d357ce94f60a0c454b47aa0243d3805d
Viktor Klang <viktor.klang@gmail.com>
2010-10-25 18:21:22 +0200

    Fixing a cranky compiler whine on a match statement

--- akka-typed-actor/src/main/scala/actor/TypedActor.scala
+++ akka-typed-actor/src/main/scala/actor/TypedActor.scala
@@ -829,7 +829,7 @@ private[akka] abstract class ActorAspect {
       (actorRef.!!(joinPoint, timeout)(senderActorRef)).as[JOption[AnyRef]] match {
         case None => JOption.none[AnyRef]
         case Some(x) if ((x eq null) || x.isEmpty) => JOption.some[AnyRef](null)
-        case Some(x) if(x.isDefined) => x
+        case Some(x) => x
       }
     } else {
       val result = (actorRef.!!(joinPoint, timeout)(senderActorRef)).as[AnyRef]
