ff81cfbf264f4b0d787e5ffad7d7bb81c3995f04
imn <irmo.manie@efgfp.com>
2010-10-26 19:58:14 +0200

    refactoring the FSM part

--- akka-actor/src/test/scala/actor/actor/FSMActorSpec.scala
+++ akka-actor/src/test/scala/actor/actor/FSMActorSpec.scala
@@ -13,34 +13,44 @@ import java.util.concurrent.TimeUnit
 
 object FSMActorSpec {
 
-  class Lock(code: String,
-             timeout: Int,
-             unlockedLatch: StandardLatch,
-             lockedLatch: StandardLatch) extends Actor with FSM[CodeState] {
+  val unlockedLatch = new StandardLatch
+  val lockedLatch = new StandardLatch
+  val unhandledLatch = new StandardLatch
 
-    def initialState = State(NextState, locked, CodeState("", code))
+  class Lock(code: String, timeout: Int) extends Actor with FSM[String, CodeState] {
 
-    def locked: StateFunction = {
+    inState("locked") {
       case Event(digit: Char, CodeState(soFar, code)) => {
         soFar + digit match {
           case incomplete if incomplete.length < code.length =>
-            State(NextState, locked, CodeState(incomplete, code))
+            stay using CodeState(incomplete, code)
           case codeTry if (codeTry == code) => {
             doUnlock
-            State(NextState, open, CodeState("", code), Some(timeout))
+            goto("open") using CodeState("", code) until timeout
           }
           case wrong => {
             log.error("Wrong code %s", wrong)
-            State(NextState, locked, CodeState("", code))
+            stay using CodeState("", code)
           }
         }
       }
+      case Event("hello", _) => stay replying "world"
     }
 
-    def open: StateFunction = {
+    inState("open") {
       case Event(StateTimeout, stateData) => {
         doLock
-        State(NextState, locked, stateData)
+        goto("locked")
+      }
+    }
+
+    setInitialState("locked", CodeState("", code))
+    
+    whenUnhandled {
+      case Event(_, stateData) => {
+        log.info("Unhandled")
+        unhandledLatch.open
+        stay
       }
     }
 
@@ -63,11 +73,9 @@ class FSMActorSpec extends JUnitSuite {
 
   @Test
   def unlockTheLock = {
-    val unlockedLatch = new StandardLatch
-    val lockedLatch = new StandardLatch
 
     // lock that locked after being open for 1 sec
-    val lock = Actor.actorOf(new Lock("33221", 1000, unlockedLatch, lockedLatch)).start
+    val lock = Actor.actorOf(new Lock("33221", 1000)).start
 
     lock ! '3'
     lock ! '3'
@@ -77,6 +85,21 @@ class FSMActorSpec extends JUnitSuite {
 
     assert(unlockedLatch.tryAwait(1, TimeUnit.SECONDS))
     assert(lockedLatch.tryAwait(2, TimeUnit.SECONDS))
+
+    lock ! "not_handled"
+    assert(unhandledLatch.tryAwait(2, TimeUnit.SECONDS))
+
+    val answerLatch = new StandardLatch
+    object Go
+    val tester = Actor.actorOf(new Actor {
+      protected def receive = {
+        case Go => lock ! "hello"
+        case "world" => answerLatch.open
+
+      }
+    }).start
+    tester ! Go
+    assert(answerLatch.tryAwait(2, TimeUnit.SECONDS))
   }
 }
 
