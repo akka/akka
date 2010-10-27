62ccb5609efcd0c1f46b0364bc3b1031b27c9a8f
ticktock <ticktock@ticktock.com>
2010-10-26 20:58:10 -0400

    refactored KVStorageBackend to also work with Cassandra, refactored CassandraStorageBackend to use KVStorageBackend, so Cassandra backend is now fully compliant with the test specs and supports PersistentQueue and Vector.pop

--- akka-persistence/akka-persistence-cassandra/src/test/scala/CassandraStorageBackendCompatibilityTest.scala
+++ akka-persistence/akka-persistence-cassandra/src/test/scala/CassandraStorageBackendCompatibilityTest.scala
@@ -3,12 +3,12 @@ package se.scalablesolutions.akka.persistence.cassandra
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import se.scalablesolutions.akka.persistence.common.{VectorStorageBackendTest, MapStorageBackendTest, RefStorageBackendTest}
+import se.scalablesolutions.akka.persistence.common.{QueueStorageBackendTest, VectorStorageBackendTest, MapStorageBackendTest, RefStorageBackendTest}
 
 @RunWith(classOf[JUnitRunner])
 class CassandraRefStorageBackendTestIntegration extends RefStorageBackendTest {
   def dropRefs = {
-    //
+    CassandraStorageBackend.refAccess.drop
   }
 
 
@@ -18,7 +18,7 @@ class CassandraRefStorageBackendTestIntegration extends RefStorageBackendTest {
 @RunWith(classOf[JUnitRunner])
 class CassandraMapStorageBackendTestIntegration extends MapStorageBackendTest {
   def dropMaps = {
-    //
+    CassandraStorageBackend.mapAccess.drop
   }
 
 
@@ -28,7 +28,17 @@ class CassandraMapStorageBackendTestIntegration extends MapStorageBackendTest {
 @RunWith(classOf[JUnitRunner])
 class CassandraVectorStorageBackendTestIntegration extends VectorStorageBackendTest {
   def dropVectors = {
-    //
+    CassandraStorageBackend.vectorAccess.drop
+  }
+
+
+  def storage = CassandraStorageBackend
+}
+
+@RunWith(classOf[JUnitRunner])
+class CassandraQueueStorageBackendTestIntegration extends QueueStorageBackendTest {
+  def dropQueues = {
+      CassandraStorageBackend.queueAccess.drop
   }
 
 
