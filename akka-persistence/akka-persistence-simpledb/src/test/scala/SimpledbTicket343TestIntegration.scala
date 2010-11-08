9c0908e4d5fd58d32aa2ae35590ae6a32f70a780
ticktock <ticktock@ticktock.com>
2010-11-07 20:04:07 -0500

    wip for simpledb backend

---
+++ akka-persistence/akka-persistence-simpledb/src/test/scala/SimpledbTicket343TestIntegration.scala
@@ -0,0 +1,12 @@
+package
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ticktock
+ * Date: 11/7/10
+ * Time: 7:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class SimpledbTicket343TestIntegration
