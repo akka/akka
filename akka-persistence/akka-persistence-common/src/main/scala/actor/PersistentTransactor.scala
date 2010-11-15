98eb924860e8281b191e7ae4f294e0f964759679
ticktock <ticktock@ticktock.com>
2010-11-15 16:09:52 -0500

    wip

--- akka-persistence/akka-persistence-common/src/main/scala/actor/PersistentTransactor.scala
+++ akka-persistence/akka-persistence-common/src/main/scala/actor/PersistentTransactor.scala
@@ -1,13 +1,49 @@
 package akka.persistence.common.actor
 
 import akka.stm.TransactionalVector
-import akka.actor.{ActorRef, Actor, Transactor}
+import akka.transactor.{Transactor}
+import akka.stm.TransactionManagement.transaction
+import akka.actor.{FSM, ActorRef, Actor}
+import org.multiverse.api.exceptions.TooManyRetriesException
+import akka.persistence.common._
 
-trait PersistentTransactor extends Transactor {
+
+
+trait PersistentTransactor extends Transactor{
   //somehow wrap message loop and catch a final commit failure, send "begin" snapshot + unapplied portion of the tx log to
   //recoverymanager actor
   ///do FSM to do start become recovering become active
+  type ElementType
+
+  def recoveryManager: ActorRef
+
+
+  def atomically = {
+    try
+    {
+      atomicallyWithStorage
+    } catch {
+      case e: TooManyRetriesException => {
+        transaction.get.get.persistentStateMap foreach{
+
+        }
+      }
+    }
+  }
+
+
+  def atomicallyWithStorage
+
+
 }
+
+sealed trait CommitFailure
+case class RefFailure(uuid:String, ref: Array[Byte]) extends CommitFailure
+case class VectorFailure(uuid:String, log:Array[Byte]) extends CommitFailure
+case class MapFailure(uuid:String, log:Array[Byte]) extends CommitFailure
+case class QueueFailure(uuid:String, log:Array[Byte]) extends CommitFailure
+case class SortedSetFailure(uuid:String, log:Array[Byte]) extends CommitFailure
+
 
 
 trait PersistentTransactorSupervisor extends Actor {
@@ -18,6 +54,7 @@ trait PersistentTransactorSupervisor extends Actor {
 sealed trait RecoveryMesage
 
 case class CommitFailed(actorRef: ActorRef, snapshot: Any, unappliedLog: TransactionalVector) extends RecoveryMesage
+
 case class RecoverFailedTransaction extends RecoveryMesage
 
 trait PersistentTransactorRecoveryManager extends Actor {
