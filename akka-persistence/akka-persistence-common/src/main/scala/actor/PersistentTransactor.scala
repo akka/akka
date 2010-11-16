f278780987879f744a5c7a77bf7d9004ac443180
ticktock <ticktock@ticktock.com>
2010-11-15 23:07:01 -0500

    wip

--- akka-persistence/akka-persistence-common/src/main/scala/actor/PersistentTransactor.scala
+++ akka-persistence/akka-persistence-common/src/main/scala/actor/PersistentTransactor.scala
@@ -1,22 +1,26 @@
 package akka.persistence.common.actor
 
-import akka.stm.TransactionalVector
 import akka.transactor.{Transactor}
-import akka.stm.TransactionManagement.transaction
 import akka.actor.{FSM, ActorRef, Actor}
 import org.multiverse.api.exceptions.TooManyRetriesException
 import akka.persistence.common._
+import akka.stm.{Recoverable, TransactionalVector}
+import scala.collection.mutable.HashMap
+
+import akka.util.Logging
 
 
+trait PersistentTransactor extends Transactor with Storage {
 
-trait PersistentTransactor extends Transactor{
-  //somehow wrap message loop and catch a final commit failure, send "begin" snapshot + unapplied portion of the tx log to
-  //recoverymanager actor
-  ///do FSM to do start become recovering become active
   type ElementType
 
-  def recoveryManager: ActorRef
+  val refs = new HashMap[String, PersistentRef[ElementType]]
+  val maps = new HashMap[String, PersistentMap[ElementType]]
+  val queues = new HashMap[String, PersistentQueue[ElementType]]
+  val vectors = new HashMap[String, PersistentVector[ElementType]]
+  val sortedSets = new HashMap[String, PersistentSortedSet[ElementType]]
 
+  def recoveryManager: ActorRef
 
   def atomically = {
     try
@@ -24,27 +28,185 @@ trait PersistentTransactor extends Transactor{
       atomicallyWithStorage
     } catch {
       case e: TooManyRetriesException => {
-
-        transaction.get.get.persistentStateMap foreach{
-
-        }
+        log.error(e, "Caught Too Many Retries Exception, sending uncommitted state to recoveryManager")
+        logRefs
+        logMaps
+        logQueues
+        logVectors
+        logSets
+        throw e
       }
     }
   }
 
 
-  def atomicallyWithStorage:Unit
+  def atomicallyWithStorage: Receive
 
+  def underlyingStorage: Storage
 
+  def logRefs() = {
+    refs.values foreach {
+      ref => {
+        if (ref.ref.isDefined) {
+          recoveryManager ! RefFailure(ref.uuid, ref.getLog)
+        }
+      }
+    }
+  }
+
+  def logMaps() = {
+    maps.values foreach {
+      map => {
+        if (map.appendOnlyTxLog.length > 0) {
+          recoveryManager ! MapFailure(map.uuid, map.getLog)
+        }
+      }
+    }
+  }
+
+  def logVectors() = {
+    vectors.values forech {
+      vec => {
+        if (vec.appendOnlyTxLog.length > 0) {
+          recoveryManager ! VectorFailure(vec.uuid, vec.getLog)
+        }
+      }
+    }
+  }
+
+  def logQueues() = {
+    queues.values foreach {
+      queue => {
+        if (queue.appndenOnlyTxLog.length > 0) {
+          recoveryManager ! QueueFailure(queue.uuid, queue.getLog)
+        }
+      }
+    }
+  }
+
+  def newRef(id: String) = {
+    val ref = underlyingStorage.newRef(id)
+    refs.put(ref.uuid, ref)
+    ref
+  }
+
+  def newVector(id: String) = {
+    val vec = underlyingStorage.newVector(id)
+    vectors.put(vec.uuid, vec)
+    vec
+  }
+
+  def newMap(id: String) = {
+    val map = underlyingStorage.newMap(id)
+    maps.put(map.uuid, map)
+    map
+  }
+
+  def getRef(id: String) = {
+    refs.getOrElseUpdate(id, {
+      val ref = underlyingStorage.getRef(id)
+      recoveryManager !! RefRecovery(id) match {
+        case RefFailure(id, log) => ref.applyLog(log)
+        case _ => ()
+      }
+    })
+  }
+
+  def getVector(id: String) = {
+    val vec = underlyingStorage.getVector(id)
+    vectors.put(vec.uuid, vec)
+    vec
+  }
+
+  def getMap(id: String) = {
+    val map = underlyingStorage.newMap(id)
+    maps.put(map.uuid, map)
+    map
+  }
+
+  def newRef = {
+    val ref = underlyingStorage.newRef
+    refs.put(ref.uuid, ref)
+    ref
+  }
+
+  def newVector = {
+    val vec = underlyingStorage.newVector
+    vectors.put(vec.uuid, vec)
+    vec
+  }
+
+  def newMap = {
+    val map = underlyingStorage.newMap
+    maps.put(map.uuid, map)
+    map
+  }
+
+  override def transactional = underlyingStorage.transactional
+
+  override def newQueue = {
+    val queue = underlyingStorage.newQueue
+    queues.put(queue.uuid, queue)
+    queue
+  }
+
+  override def newSortedSet = {
+    val set = underlyingStorage.newSortedSet
+    sortedSets.put(set.uuid, set)
+    set
+  }
+
+  override def getQueue(id: String) = {
+    val queue = underlyingStorage.getQueue(id)
+    queues.put(queue.uuid, queue)
+    queue
+  }
+
+  override def getSortedSet(id: String) = {
+    val set = underlyingStorage.getSortedSet(id)
+    sortedSets.put(set.uuid, set)
+    set
+  }
+
+  override def newQueue(id: String) = {
+    val queue = underlyingStorage.newQueue(id)
+    queues.put(queue.uuid, queue)
+    queue
+  }
+
+  override def newSortedSet(id: String) = {
+    val set = underlyingStorage.newSortedSet(id)
+    sortedSets.put(set.uuid, set)
+    set
+  }
 }
 
-@serializable sealed trait CommitFailure
-case class RefFailure(uuid:String, ref: Array[Byte], error:String) extends CommitFailure
-case class VectorFailure(uuid:String, log:Array[Byte],error:String) extends CommitFailure
-case class MapFailure(uuid:String, log:Array[Byte], error:String) extends CommitFailure
-case class QueueFailure(uuid:String, log:Array[Byte],error:String) extends CommitFailure
-case class SortedSetFailure(uuid:String, log:Array[Byte],error:String) extends CommitFailure
 
+sealed trait CommitRecovery
+
+case class RefRecovery(uuid: String) extends CommitRecovery
+
+case class MapRecovery(uuid: String) extends CommitRecovery
+
+case class QueueRecovery(uuid: String) extends CommitRecovery
+
+case class VectorRecovery(uuid: String) extends CommitRecovery
+
+case class SortedSetRecovery(uuid: String) extends CommitRecovery
+
+
+@serializable
+sealed trait CommitFailure
+
+case class RefFailure(uuid: String, tlog: Array[Byte]) extends CommitFailure
+
+case class MapFailure(uuid: String, tlog: Array[Byte]) extends CommitFailure
+
+case class QueueFailure(uuid: String, tlog: Array[Byte]) extends CommitFailure
+
+case class VectorFailure(uuid: String, tlog: Array[Byte]) extends CommitFailure
+
+case class SortedSetFailure(uuid: String, tlog: Array[Byte]) extends CommitFailure
 
 
 trait PersistentTransactorSupervisor extends Actor {
@@ -54,18 +216,17 @@ trait PersistentTransactorSupervisor extends Actor {
 
 sealed trait RecoveryMesage
 
-case class CommitFailed(actorRef: ActorRef, snapshot: Any, unappliedLog: TransactionalVector) extends RecoveryMesage
-
-case class RecoverFailedTransaction extends RecoveryMesage
+case class RecoverFailedTransaction() extends RecoveryMesage
 
 trait PersistentTransactorRecoveryManager extends Actor {
   //log the failed transactions in hawtdb
   //send the failed transactions back to the restarted actor
 
   protected def receive = {
-
+    case RefFailure(uuid, tlog, error) => {
+      log.info(uuid + " " + error)
+    }
   }
-
 
 
 }
