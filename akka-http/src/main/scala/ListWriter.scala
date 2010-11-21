13126c494199b9bdacc1118d59638d62f014125f
Garrick Evans <buka37@gmail.com>
2010-11-20 23:41:40 -0800

    fixing a screwy merge from master... readding files git deleted for some unknown reason

---
+++ akka-http/src/main/scala/ListWriter.scala
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2009-2010 Scalable Solutions AB <http://scalablesolutions.se>
+ */
+package akka.rest
+
+import java.io.OutputStream
+import akka.serialization.Serializer
+import javax.ws.rs.core.{MultivaluedMap, MediaType}
+import javax.ws.rs.ext.{MessageBodyWriter, Provider}
+import javax.ws.rs.Produces
+
+/**
+ * Writes Lists of JSON serializable objects.
+ */
+@Provider
+@Produces(Array("application/json"))
+class ListWriter extends MessageBodyWriter[List[_]] {
+
+  def isWriteable(aClass: Class[_],
+                  aType: java.lang.reflect.Type,
+                  annotations: Array[java.lang.annotation.Annotation],
+                  mediaType: MediaType) =
+    classOf[List[_]].isAssignableFrom(aClass) || aClass == ::.getClass
+
+  def getSize(list: List[_],
+              aClass: Class[_],
+              aType: java.lang.reflect.Type,
+              annotations: Array[java.lang.annotation.Annotation],
+              mediaType: MediaType) =
+    -1L
+
+  def writeTo(list: List[_],
+              aClass: Class[_],
+              aType: java.lang.reflect.Type,
+              annotations: Array[java.lang.annotation.Annotation],
+              mediaType: MediaType,
+              stringObjectMultivaluedMap: MultivaluedMap[String, Object],
+              outputStream: OutputStream): Unit =
+    if (list.isEmpty) outputStream.write(" ".getBytes)
+    else outputStream.write(Serializer.ScalaJSON.toBinary(list))
+}
