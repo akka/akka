ec5be9b23b3c8935d11e60a08cfca6ebc8857093
Roland Kuhn <rk@rkuhn.info>
2010-12-19 22:16:15 +0100

    improvements on FSM
    - change to akka.util.Duration
    - add proper implicits to enable timeouts like "5 seconds"
    - add concept of state timeouts, which are actually attached to states
    - add timer handling for conveniently modeling timing irrespective of message "interruptions"
    - add generic Buncher class as usage example and useful utility

--- akka-actor/src/main/scala/akka/actor/FSM.scala
+++ akka-actor/src/main/scala/akka/actor/FSM.scala
@@ -1,8 +1,9 @@
 /**
  * Copyright (C) 2009-2010 Scalable Solutions AB <http://scalablesolutions.se>
  */
-
 package akka.actor
+
+import akka.util._
 
 import scala.collection.mutable
 import java.util.concurrent.{ScheduledFuture, TimeUnit}
@@ -15,6 +16,29 @@ object FSM {
 
   case object StateTimeout
   case class TimeoutMarker(generation: Long)
+  case class Timer(name : String, msg : AnyRef, repeat : Boolean) {
+	  private var ref : Option[ScheduledFuture[AnyRef]] = _
+	  def schedule(actor : ActorRef, timeout : Duration) {
+	 	  if (repeat) {
+	 	 	  ref = Some(Scheduler.schedule(actor, this, timeout.length, timeout.length, timeout.unit))
+	 	  } else {
+	 		  ref = Some(Scheduler.scheduleOnce(actor, this, timeout.length, timeout.unit))
+	 	  }
+	  }
+	  def cancel {
+	 	  ref = ref flatMap {t => t.cancel(true); None}
+	  }
+  }
+  
+  /*
+   * With these implicits in scope, you can write "5 seconds" anywhere a
+   * Duration or Option[Duration] is expected. This is conveniently true
+   * for derived classes.
+   */
+  implicit def d2od(d : Duration) : Option[Duration] = Some(d)
+  implicit def p2od(p : (Long, TimeUnit)) : Duration = new Duration(p._1, p._2)
+  implicit def i2d(i : Int) : DurationInt = new DurationInt(i)
+  implicit def l2d(l : Long) : DurationLong = new DurationLong(l)
 }
 
 trait FSM[S, D] {
@@ -22,20 +46,21 @@ trait FSM[S, D] {
   import FSM._
 
   type StateFunction = scala.PartialFunction[Event, State]
+  type Timeout = Option[Duration]
 
   /** DSL */
   protected final def notifying(transitionHandler: PartialFunction[Transition, Unit]) = {
     transitionEvent = transitionHandler
   }
 
-  protected final def when(stateName: S)(stateFunction: StateFunction) = {
-    register(stateName, stateFunction)
+  protected final def when(stateName: S, stateTimeout: Timeout = None)(stateFunction: StateFunction) = {
+    register(stateName, stateFunction, stateTimeout)
   }
 
   protected final def startWith(stateName: S,
                                 stateData: D,
-                                timeout: Option[(Long, TimeUnit)] = None) = {
-    applyState(State(stateName, stateData, timeout))
+                                timeout: Timeout = None) = {
+    currentState = State(stateName, stateData, timeout)
   }
 
   protected final def goto(nextStateName: S): State = {
@@ -43,6 +68,7 @@ trait FSM[S, D] {
   }
 
   protected final def stay(): State = {
+    // cannot directly use currentState because of the timeout field
     goto(currentState.stateName)
   }
 
@@ -58,6 +84,38 @@ trait FSM[S, D] {
     stay using stateData withStopReason(reason)
   }
 
+  /**
+   * Schedule named timer to deliver message after given delay, possibly repeating.
+   * @param name identifier to be used with cancelTimer()
+   * @param msg message to be delivered
+   * @param timeout delay of first message delivery and between subsequent messages
+   * @param repeat send once if false, scheduleAtFixedRate if true
+   * @return current State
+   */
+  protected final def setTimer(name : String, msg : AnyRef, timeout : Duration, repeat : Boolean):State = {
+    if (timers contains name) {
+    	timers(name).cancel
+    }
+	val timer = Timer(name, msg, repeat)
+    timer.schedule(self, timeout)
+    timers(name) = timer
+    stay
+  }
+  
+  /**
+   * Cancel named timer, ensuring that the message is not subsequently delivered (no race).
+   * @param name
+   * @return
+   */
+  protected final def cancelTimer(name : String) = {
+	  if (timers contains name) {
+	 	  timers(name).cancel
+	 	  timers -= name
+	  }
+  }
+  
+  protected final def timerActive_?(name : String) = timers contains name
+
   def whenUnhandled(stateFunction: StateFunction) = {
     handleEvent = stateFunction
   }
@@ -65,19 +123,29 @@ trait FSM[S, D] {
   def onTermination(terminationHandler: PartialFunction[Reason, Unit]) = {
     terminateEvent = terminationHandler
   }
+  
+  def initialize {
+	  // check existence of initial state and setup timeout
+	  makeTransition(currentState)
+  }
 
   /** FSM State data and default handlers */
   private var currentState: State = _
   private var timeoutFuture: Option[ScheduledFuture[AnyRef]] = None
   private var generation: Long = 0L
 
+  private val timers = mutable.Map[String, Timer]()
 
   private val stateFunctions = mutable.Map[S, StateFunction]()
-  private def register(name: S, function: StateFunction) {
+  private val stateTimeouts = mutable.Map[S, Timeout]()
+  
+  private def register(name: S, function: StateFunction, timeout: Timeout) {
     if (stateFunctions contains name) {
       stateFunctions(name) = stateFunctions(name) orElse function
+      stateTimeouts(name) = timeout orElse stateTimeouts(name)
     } else {
       stateFunctions(name) = function
+      stateTimeouts(name) = timeout
     }
   }
 
@@ -100,6 +168,13 @@ trait FSM[S, D] {
     case TimeoutMarker(gen) =>
       if (generation == gen) {
         processEvent(StateTimeout)
+      }
+    case t @ Timer(name, msg, repeat) =>
+      if (timerActive_?(name)) {
+   	    processEvent(msg)
+   	    if (!repeat) {
+   	      timers -= name
+   	    }
       }
     case value => {
       timeoutFuture = timeoutFuture.flatMap {ref => ref.cancel(true); None}
@@ -124,14 +199,12 @@ trait FSM[S, D] {
       if (currentState.stateName != nextState.stateName) {
         transitionEvent.apply(Transition(currentState.stateName, nextState.stateName))
       }
-      applyState(nextState)
-    }
-  }
-
-  private def applyState(nextState: State) = {
-    currentState = nextState
-    currentState.timeout.foreach { t =>
-      timeoutFuture = Some(Scheduler.scheduleOnce(self, TimeoutMarker(generation), t._1, t._2))
+      currentState = nextState
+      currentState.timeout orElse stateTimeouts(currentState.stateName) foreach { t =>
+      	if (t.length >= 0) {
+      		timeoutFuture = Some(Scheduler.scheduleOnce(self, TimeoutMarker(generation), t.length, t.unit))
+      	}
+      }
     }
   }
 
@@ -143,12 +216,12 @@ trait FSM[S, D] {
 
   case class Event(event: Any, stateData: D)
 
-  case class State(stateName: S, stateData: D, timeout: Option[(Long, TimeUnit)] = None) {
+  case class State(stateName: S, stateData: D, timeout: Timeout = None) {
 
-    def forMax(timeout: (Long, TimeUnit)): State = {
+    def forMax(timeout: Duration): State = {
       copy(timeout = Some(timeout))
     }
-
+    
     def replying(replyValue:Any): State = {
       self.sender match {
         case Some(sender) => sender ! replyValue
