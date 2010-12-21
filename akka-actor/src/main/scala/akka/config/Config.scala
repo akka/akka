2d720610f9a60732a2c82f531b523ba45c422274
Jonas Bonér <jonasremove@jonasboner.com>
2010-12-21 22:59:40 +0100

    added option to turn on/off JMX browsing of the configuration

--- akka-actor/src/main/scala/akka/config/Config.scala
+++ akka-actor/src/main/scala/akka/config/Config.scala
@@ -101,6 +101,7 @@ object Config extends Logging {
       CConfig.fromString("<akka></akka>") // default empty config
     }
   }
+  if (config.getBool("akka.enable-jmx-for-config", true)) config.registerWithJmx("akka")
 
   val CONFIG_VERSION = config.getString("akka.version", VERSION)
   if (VERSION != CONFIG_VERSION) throw new ConfigurationException(
