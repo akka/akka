5c241ae1f33823981312dd8a7e158b87d9d73cd7
Peter Vlugter <peter@maka>
2010-12-22 13:15:23 +1300

    Throw runtime exception for @Coordinated when used with non-void methods

--- akka-typed-actor/src/test/scala/transactor/TypedCoordinatedIncrementSpec.scala
+++ akka-typed-actor/src/test/scala/transactor/TypedCoordinatedIncrementSpec.scala
@@ -6,23 +6,27 @@ import org.scalatest.matchers.MustMatchers
 import akka.actor.TypedActor
 import akka.stm.Ref
 import akka.transactor.annotation.Coordinated
+import akka.transactor.CoordinateException
 import akka.transactor.Coordination._
 
 object TypedCoordinatedIncrement {
   trait Counter {
     @Coordinated def increment: Unit
+    @Coordinated def incrementAndGet: Int
     def get: Int
   }
 
   class CounterImpl extends TypedActor with Counter {
     val ref = Ref(0)
     def increment = ref alter (_ + 1)
+    def incrementAndGet = { ref alter (_ + 1); ref.get }
     def get = ref.get
   }
 
   class FailerImpl extends TypedActor with Counter {
     val ref = Ref(0)
     def increment = throw new RuntimeException("Expected failure")
+    def incrementAndGet = throw new RuntimeException("Expected failure")
     def get = ref.get
   }
 }
@@ -67,6 +71,12 @@ class TypedCoordinatedIncrementSpec extends WordSpec with MustMatchers {
       }
       counters foreach (TypedActor.stop)
       TypedActor.stop(failer)
+    }
+
+    "fail when used with non-void methods" in {
+      val counter = TypedActor.newInstance(classOf[Counter], classOf[CounterImpl])
+      evaluating { counter.incrementAndGet } must produce [CoordinateException]
+      TypedActor.stop(counter)
     }
   }
 }
