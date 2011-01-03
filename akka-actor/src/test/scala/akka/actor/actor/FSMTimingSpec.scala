9dbfad331fd7032cb0528de584a3b351a6f26359
Roland Kuhn <rk@rkuhn.info>
2011-01-03 18:50:42 +0100

    change indentation to 2 spaces

--- akka-actor/src/test/scala/akka/actor/actor/FSMTimingSpec.scala
+++ akka-actor/src/test/scala/akka/actor/actor/FSMTimingSpec.scala
@@ -7,114 +7,114 @@ import org.scalatest.WordSpec
 import org.scalatest.matchers.MustMatchers
 
 class FSMTimingSpec
-        extends WordSpec
-        with MustMatchers
-        with TestKit {
+    extends WordSpec
+    with MustMatchers
+    with TestKit {
 
-    import FSMTimingSpec._
-    import FSM._
+  import FSMTimingSpec._
+  import FSM._
 
-    val fsm = Actor.actorOf(new StateMachine(testActor)).start
-    fsm ! SubscribeTransitionCallBack(testActor)
-    expectMsg(50 millis, Initial)
+  val fsm = Actor.actorOf(new StateMachine(testActor)).start
+  fsm ! SubscribeTransitionCallBack(testActor)
+  expectMsg(50 millis, Initial)
 
-    ignoreMsg {
-        case Transition(Initial, _) => true
+  ignoreMsg {
+    case Transition(Initial, _) => true
+  }
+
+  "A Finite State Machine" must {
+
+    "receive StateTimeout" in {
+      within (50 millis, 150 millis) {
+        fsm ! TestStateTimeout
+        expectMsg(Transition(TestStateTimeout, Initial))
+        expectNoMsg
+      }
     }
 
-    "A Finite State Machine" must {
-
-        "receive StateTimeout" in {
-            within (50 millis, 150 millis) {
-                fsm ! TestStateTimeout
-                expectMsg(Transition(TestStateTimeout, Initial))
-                expectNoMsg
-            }
-        }
-
-        "receive single-shot timer" in {
-            within (50 millis, 150 millis) {
-                fsm ! TestSingleTimer
-                expectMsg(Tick)
-                expectMsg(Transition(TestSingleTimer, Initial))
-                expectNoMsg
-            }
-        }
-
-        "receive and cancel a repeated timer" in {
-            fsm ! TestRepeatedTimer
-            val seq = receiveWhile(550 millis) {
-                case Tick => Tick
-            }
-            seq must have length (5)
-            within(250 millis) {
-                fsm ! Cancel
-                expectMsg(Transition(TestRepeatedTimer, Initial))
-                expectNoMsg
-            }
-        }
-
-        "notify unhandled messages" in {
-            within(200 millis) {
-                fsm ! Cancel
-                expectMsg(Unhandled(Cancel))
-                expectNoMsg
-            }
-        }
-
+    "receive single-shot timer" in {
+      within (50 millis, 150 millis) {
+        fsm ! TestSingleTimer
+        expectMsg(Tick)
+        expectMsg(Transition(TestSingleTimer, Initial))
+        expectNoMsg
+      }
     }
+
+    "receive and cancel a repeated timer" in {
+      fsm ! TestRepeatedTimer
+      val seq = receiveWhile(550 millis) {
+        case Tick => Tick
+      }
+      seq must have length (5)
+      within(250 millis) {
+        fsm ! Cancel
+        expectMsg(Transition(TestRepeatedTimer, Initial))
+        expectNoMsg
+      }
+    }
+
+    "notify unhandled messages" in {
+      within(200 millis) {
+        fsm ! Cancel
+        expectMsg(Unhandled(Cancel))
+        expectNoMsg
+      }
+    }
+
+  }
 
 }
 
 object FSMTimingSpec {
 
-    trait State
-    case object Initial extends State
-    case object TestStateTimeout extends State
-    case object TestSingleTimer extends State
-    case object TestRepeatedTimer extends State
+  trait State
+  case object Initial extends State
+  case object TestStateTimeout extends State
+  case object TestSingleTimer extends State
+  case object TestRepeatedTimer extends State
 
-    case object Tick
-    case object Cancel
+  case object Tick
+  case object Cancel
 
-    case class Unhandled(msg : AnyRef)
+  case class Unhandled(msg : AnyRef)
 
-    class StateMachine(tester : ActorRef) extends Actor with FSM[State, Unit] {
-        import FSM._
+  class StateMachine(tester : ActorRef) extends Actor with FSM[State, Unit] {
+    import FSM._
 
-        startWith(Initial, ())
-        when(Initial) {
-            case Ev(TestStateTimeout) => goto(TestStateTimeout)
-            case Ev(TestSingleTimer) =>
-                setTimer("tester", Tick, 100 millis, false)
-                goto(TestSingleTimer)
-            case Ev(TestRepeatedTimer) =>
-                setTimer("tester", Tick, 100 millis, true)
-                goto(TestRepeatedTimer)
-        }
-        when(TestStateTimeout, stateTimeout = 100 millis) {
-            case Ev(StateTimeout) => goto(Initial)
-        }
-        when(TestSingleTimer) {
-            case Ev(Tick) =>
-                tester ! Tick
-                goto(Initial)
-        }
-        when(TestRepeatedTimer) {
-            case Ev(Tick) =>
-                tester ! Tick
-                stay
-            case Ev(Cancel) =>
-                cancelTimer("tester")
-                goto(Initial)
-        }
-
-        whenUnhandled {
-            case Ev(msg : AnyRef) =>
-                tester ! Unhandled(msg)
-                stay
-        }
+    startWith(Initial, ())
+    when(Initial) {
+      case Ev(TestStateTimeout) => goto(TestStateTimeout)
+      case Ev(TestSingleTimer) =>
+        setTimer("tester", Tick, 100 millis, false)
+        goto(TestSingleTimer)
+      case Ev(TestRepeatedTimer) =>
+        setTimer("tester", Tick, 100 millis, true)
+        goto(TestRepeatedTimer)
     }
+    when(TestStateTimeout, stateTimeout = 100 millis) {
+      case Ev(StateTimeout) => goto(Initial)
+    }
+    when(TestSingleTimer) {
+      case Ev(Tick) =>
+        tester ! Tick
+        goto(Initial)
+    }
+    when(TestRepeatedTimer) {
+      case Ev(Tick) =>
+        tester ! Tick
+        stay
+      case Ev(Cancel) =>
+        cancelTimer("tester")
+        goto(Initial)
+    }
+
+    whenUnhandled {
+      case Ev(msg : AnyRef) =>
+        tester ! Unhandled(msg)
+        stay
+    }
+  }
 
 }
 
