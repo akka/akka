dba6c1bf1c1b191db4138e46b0e45702478724ee
Peter Vlugter <peter@maka>
2011-01-21 13:25:55 +1300

    Add scripts for release

---
+++ project/scripts/push-release.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+VERSION=$1
+
+if [ -z "$VERSION" ]; then
+    echo "Usage: push-release.sh VERSION"
+    exit 1
+fi
+
+source ~/.akka-release
+
+if [ -z "$AKKA_RELEASE_SERVER" ]; then
+    echo "Need AKKA_RELEASE_SERVER to be specified"
+    exit 1
+fi
+
+if [ -z "$AKKA_RELEASE_PATH" ]; then
+    echo "Need AKKA_RELEASE_PATH to be specified"
+    exit 1
+fi
+
+ref=$(git symbolic-ref HEAD 2> /dev/null)
+branch=${ref#refs/heads/}
+
+git push origin $branch
+git push origin --tags
+
+release="target/release/${VERSION}"
+tmp="/tmp/akka-release-${VERSION}"
+
+ssh -t ${AKKA_RELEASE_SERVER} sudo -v
+rsync -avz ${release}/ ${AKKA_RELEASE_SERVER}:${tmp}/
+ssh -t ${AKKA_RELEASE_SERVER} sudo rsync -rpt ${tmp}/ ${AKKA_RELEASE_PATH}
+ssh -t rm -rf ${tmp}
