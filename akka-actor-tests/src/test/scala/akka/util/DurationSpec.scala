e6f28704496f116319b2f856ed38e73e1b98577b
Roland <rk@rkuhn.info>
2011-05-16 21:12:10 +0200

    add some tests for Duration

---
+++ akka-actor-tests/src/test/scala/akka/util/DurationSpec.scala
@@ -0,0 +1,42 @@
+package akka.util
+
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
+import duration._
+
+class DurationSpec extends WordSpec with MustMatchers {
+
+  "Duration" must {
+
+    "form a one-dimensional vector field" in {
+      val zero = 0.seconds
+      val one = 1.second
+      val two = one + one
+      val three = 3 * one
+      (0 * one) must be (zero)
+      (2 * one) must be (two)
+      (three - two) must be (one)
+      (three / 3) must be (one)
+      (two / one) must be (2)
+      (one + zero) must be (one)
+      (one / 1000000) must be (1.micro)
+    }
+
+    "respect correct treatment of infinities" in {
+      val one = 1.second
+      val inf = Duration.Inf
+      val minf = Duration.MinusInf
+      (-inf) must be (minf)
+      intercept[IllegalArgumentException] { minf + inf }
+      intercept[IllegalArgumentException] { inf - inf }
+      intercept[IllegalArgumentException] { inf + minf }
+      intercept[IllegalArgumentException] { minf - minf }
+      (inf + inf) must be (inf)
+      (inf - minf) must be (inf)
+      (minf - inf) must be (minf)
+      (minf + minf) must be (minf)
+    }
+
+  }
+
+}
