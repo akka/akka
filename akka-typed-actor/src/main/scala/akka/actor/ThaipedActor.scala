1f05440b6ef1d5d671aa5de92c9cd051c82aa6f3
Viktor Klang <viktor.klang@gmail.com>
2011-05-18 13:43:23 +0200

    ThaipedActor seems to come along nicely

--- akka-typed-actor/src/main/scala/akka/actor/ThaipedActor.scala
+++ akka-typed-actor/src/main/scala/akka/actor/ThaipedActor.scala
@@ -1,103 +1,152 @@
-package akka.actor
+package akka.thaipedactor
 
 import java.lang.reflect.{Method, InvocationHandler, Proxy}
 import akka.japi.{Creator, Option => JOption}
 import akka.actor.Actor.{actorOf, futureToAnyOptionAsTypedOption}
-import akka.dispatch.{AlreadyCompletedFuture, Future}
+import akka.transactor.annotation.{Coordinated => CoordinatedAnnotation}
+import akka.util.Duration
+import akka.dispatch.{MessageDispatcher, Dispatchers, AlreadyCompletedFuture, Future}
+import akka.actor.{ActorRef, Actor}
 
-trait Foo {
-  def pigdog(): String
+object MethodCall {
+  private[akka] def isOneWay(method: Method): Boolean =
+      method.getReturnType == java.lang.Void.TYPE
 
-  def futurePigdog(): Future[String]
-  def futurePigdog(delay: Long): Future[String]
+  private[akka] def isCoordinated(method: Method): Boolean =
+    method.isAnnotationPresent(classOf[CoordinatedAnnotation])
 
-  def optionPigdog(): Option[String]
+  private[akka] def returnsFuture_?(method: Method): Boolean =
+    classOf[Future[_]].isAssignableFrom(method.getReturnType)
+
+  private[akka] def returnsJOption_?(method: Method): Boolean =
+    classOf[akka.japi.Option[_]].isAssignableFrom(method.getReturnType)
+
+  private[akka] def returnsOption_?(method: Method): Boolean =
+    classOf[scala.Option[_]].isAssignableFrom(method.getReturnType)
+
+  //Note to self: move resolveMethod from NettyRemoteSupport to here for remote typed actors
 }
 
-class Bar extends Foo {
-  def pigdog = "Bar"
+case class MethodCall(method: Method, parameters: Array[AnyRef]) {
+  def isOneWay = MethodCall.isOneWay(method)
+  def isCoordinated = MethodCall.isCoordinated(method)
+  def returnsFuture_? = MethodCall.returnsFuture_?(method)
+  def returnsJOption_? = MethodCall.returnsJOption_?(method)
+  def returnsOption_? = MethodCall.returnsOption_?(method)
 
-  def futurePigdog(): Future[String] = new AlreadyCompletedFuture(Right(pigdog))
-  def futurePigdog(delay: Long): Future[String] = {
-    Thread.sleep(delay)
-    futurePigdog
+  def callMethodOn(instance: AnyRef): AnyRef = {
+
+    /*if (parameters ne null)
+      println("### CALLING " + method.getName + "(" + parameters.mkString(", ") + ") owned by [" + method.getDeclaringClass.getName + "] on " + instance + " of class " + instance.getClass.getName)
+    else
+      println("### CALLING " + method.getName + "() owned by [" + method.getDeclaringClass.getName + "] on " + instance + " of class " + instance.getClass.getName)*/
+
+    //We do not yet obey Actor.SERIALIZE_MESSAGES
+    parameters match {
+      case null                     => method.invoke(instance)
+      case args if args.length == 0 => method.invoke(instance)
+      case args                     => method.invoke(instance, args:_*)
+    }
   }
-
-  def optionPigdog(): Option[String] = Some(pigdog)
-
 }
 
 object ThaipedActor {
 
-  class ThaipedActor[TI](createInstance: => TI) extends Actor {
-    val currentInstance = createInstance
+  class ThaipedActor[TI <: AnyRef](createInstance: => TI) extends Actor {
+    val me = createInstance
     def receive = {
-      case Invocation(method, args) =>
+      case m: MethodCall => m match {
+        case m if m.isOneWay => m.callMethodOn(me)
 
-        def invokeMethod(): AnyRef = args match {
-          case null => method.invoke(currentInstance)
-          case some => method.invoke(currentInstance, some:_*)
-        }
+        case m if m.returnsFuture_? => self.senderFuture.get completeWith m.callMethodOn(me).asInstanceOf[Future[Any]]
 
-
-      method.getReturnType match {
-        case c if c.isAssignableFrom(Void.TYPE) => //Handle sendOneWay
-          invokeMethod()
-        case c if c.isAssignableFrom(classOf[Future[_]]) => //Handle non-blocking sends
-          val r = invokeMethod().asInstanceOf[Future[Any]]
-          self.senderFuture.get completeWith r
-        case c if c.isAssignableFrom(classOf[JOption[_]]) => //Handle Java options returns
-          self reply invokeMethod()
-        case c if c.isAssignableFrom(classOf[Option[_]]) => //Handle Scala options returns
-          self reply invokeMethod()
-        case _ => //Handle blocking request-reply sends
-          self reply invokeMethod()
+        case m => self reply m.callMethodOn(me)
       }
     }
   }
 
-  case class Invocation(method: Method, args: Array[AnyRef])
-
-  protected[akka] def createInvocationHandler(actor: ActorRef): InvocationHandler = new InvocationHandler {
-
+  case class ThaipedActorInvocationHandler(actor: ActorRef) extends InvocationHandler {
     def invoke(proxy: AnyRef, method: Method, args: Array[AnyRef]): AnyRef = method.getName match {
       case "toString" => actor.toString
       case "equals" => if (proxy eq args(0)) java.lang.Boolean.TRUE else java.lang.Boolean.FALSE
+      case "hashCode" => actor.hashCode.asInstanceOf[AnyRef]
       case _ =>
-        method.getReturnType match {
-          case c if c.isAssignableFrom(Void.TYPE) => //Handle sendOneWay
-            actor ! Invocation(method, args)
-            null
-          case c if c.isAssignableFrom(classOf[Future[_]]) => //Handle non-blocking sends
-            actor !!! Invocation(method, args)
-          case c if c.isAssignableFrom(classOf[JOption[_]]) => //Handle Java options returns
-            (actor !!! Invocation(method, args)).as[JOption[Any]] match {
-              case Some(null) => JOption.none[Any]
-              case Some(joption) => joption
-              case None => JOption.none[Any]
-            }
-          case c if c.isAssignableFrom(classOf[Option[_]]) => //Handle Scala options returns
-            (actor !!! Invocation(method, args)).as[AnyRef] match {
-              case Some(null) => None
-              case Some(option) => option
-              case None => None
-            }
-          case _ => //Handle blocking request-reply sends
-            (actor !!! Invocation(method, args)).get
-        }
+         MethodCall(method, args) match {
+           case m if m.isOneWay =>
+             actor ! m
+             null
+
+           case m if m.returnsJOption_? =>
+             (actor !!! m).as[JOption[Any]] match {
+               case Some(null) => JOption.none[Any]
+               case Some(joption) => joption
+               case None => JOption.none[Any]
+             }
+
+           case m if m.returnsOption_? =>
+             (actor !!! m).as[AnyRef] match {
+               case Some(null) => None
+               case Some(option) => option
+               case None => None
+             }
+
+           case m if m.returnsFuture_? =>
+             actor !!! m
+
+           case m =>
+             (actor !!! m).get
+         }
     }
   }
 
-  def thaipedActorOf[T,TI <: T](interface: Class[T], impl: Class[TI], loader: ClassLoader): T =
-    newThaipedActor(interface, impl.newInstance.asInstanceOf[TI],loader)
+  val defaultTimeout = Duration(Actor.TIMEOUT, "millis")
 
-  def thaipedActorOf[T,TI <: T](interface: Class[T], impl: Creator[TI], loader: ClassLoader): T =
-    newThaipedActor(interface, impl.create, loader)
+  case class Configuration(timeout: Duration = defaultTimeout, dispatcher: MessageDispatcher = Dispatchers.defaultGlobalDispatcher)
 
-  def thaipedActorOf[T : Manifest, TI <: T](impl: Creator[TI], loader: ClassLoader): T =
-    newThaipedActor[T,TI](implicitly[Manifest[T]].erasure.asInstanceOf[Class[T]], impl, loader)
+  def thaipedActorOf[T <: AnyRef,TI <: T](interface: Class[T], impl: Class[TI], config: Configuration, loader: ClassLoader): T =
+    newThaipedActor(interface, impl.newInstance.asInstanceOf[TI], config, loader)
 
+  def thaipedActorOf[T <: AnyRef,TI <: T](interface: Class[T], impl: Creator[TI], config: Configuration, loader: ClassLoader): T =
+    newThaipedActor(interface, impl.create, config, loader)
 
-  protected def newThaipedActor[T, TI <: T](interface: Class[T], impl: => TI, loader: ClassLoader): T =
-    Proxy.newProxyInstance(loader, Array[Class[_]](interface), createInvocationHandler(actorOf(new ThaipedActor[TI](impl)).start)).asInstanceOf[T]
+  def thaipedActorOf[T <: AnyRef : Manifest, TI <: T](impl: Creator[TI], config: Configuration, loader: ClassLoader): T =
+    newThaipedActor[T,TI](implicitly[Manifest[T]].erasure.asInstanceOf[Class[T]], impl.create, config, loader)
+
+  protected def newThaipedActor[T <: AnyRef, TI <: T](interface: Class[T], impl: => TI, config: Configuration, loader: ClassLoader): T = {
+    val actor = actorOf(new ThaipedActor[TI](impl))
+
+    actor.timeout = config.timeout.toMillis
+    actor.dispatcher = config.dispatcher
+
+    val handler = new ThaipedActorInvocationHandler(actor)
+    val proxy: T = Proxy.newProxyInstance(loader, Array[Class[_]](interface), handler).asInstanceOf[T]
+    actor.start
+    proxy
+  }
+
+  def stop(thaipedActor: AnyRef): Boolean = getActorFor(thaipedActor) match {
+    case Some(ref) =>
+      ref.stop
+      true
+    case _ => false
+  }
+
+  def getActorFor(thaipedActor: AnyRef): Option[ActorRef] = thaipedActor match {
+    case null => None
+    case other =>
+      Proxy.getInvocationHandler(other) match {
+        case null => None
+        case handler: ThaipedActorInvocationHandler => Option(handler.actor)
+        case _ => None
+      }
+  }
+
+  def isThaipedActor(thaipedActor_? : AnyRef): Boolean = thaipedActor_? match {
+    case null => false
+    case some => Proxy.getInvocationHandler(some) match {
+      case null => false
+      case handler: ThaipedActorInvocationHandler => true
+      case _ => false
+    }
+  }
 }
---
+++ akka-typed-actor/src/test/scala/actor/typed-actor/ThaipedActorSpec.scala
@@ -0,0 +1,185 @@
+package akka.thaipedactor
+
+/**
+ * Copyright (C) 2009-2011 Scalable Solutions AB <http://scalablesolutions.se>
+ */
+
+import org.scalatest.matchers.MustMatchers
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+import org.scalatest.{BeforeAndAfterAll, WordSpec, BeforeAndAfterEach}
+import akka.thaipedactor.ThaipedActor._
+import akka.japi.{Option => JOption}
+import akka.util.Duration
+import akka.dispatch.{Dispatchers, Future, AlreadyCompletedFuture}
+import akka.routing.CyclicIterator
+
+object ThaipedActorSpec {
+  trait Foo {
+    def pigdog(): String
+
+    def futurePigdog(): Future[String]
+    def futurePigdog(delay: Long): Future[String]
+    def futurePigdog(delay: Long, numbered: Int): Future[String]
+
+    def optionPigdog(): Option[String]
+    def optionPigdog(delay: Long): Option[String]
+    def joptionPigdog(delay: Long): JOption[String]
+
+
+    def incr()
+    def read(): Int
+  }
+
+  class Bar extends Foo {
+    def pigdog = "Pigdog"
+
+    def futurePigdog(): Future[String] = new AlreadyCompletedFuture(Right(pigdog))
+    def futurePigdog(delay: Long): Future[String] = {
+      Thread.sleep(delay)
+      futurePigdog
+    }
+
+    def futurePigdog(delay: Long, numbered: Int): Future[String] ={
+      Thread.sleep(delay)
+      new AlreadyCompletedFuture(Right(pigdog + numbered))
+    }
+
+    def optionPigdog(): Option[String] = Some(pigdog)
+
+    def optionPigdog(delay: Long): Option[String] = {
+      Thread.sleep(delay)
+      Some(pigdog)
+    }
+
+    def joptionPigdog(delay: Long): JOption[String] = {
+      Thread.sleep(delay)
+      JOption.some(pigdog)
+    }
+
+    var internalNumber = 0
+
+    def incr() {
+      internalNumber += 1
+    }
+
+    def read() = internalNumber
+  }
+}
+
+@RunWith(classOf[JUnitRunner])
+class ThaipedActorSpec extends
+  WordSpec with
+  MustMatchers with
+  BeforeAndAfterEach with
+  BeforeAndAfterAll {
+  import akka.thaipedactor.ThaipedActorSpec._
+
+  def newFooBar: Foo = newFooBar(Duration(2, "seconds"))
+
+  def newFooBar(timeout: Duration): Foo =
+    newFooBar(Configuration(timeout))
+
+  def newFooBar(config: Configuration): Foo =
+    thaipedActorOf(classOf[Foo],
+                   classOf[Bar],
+                   config,
+                   classOf[Foo].getClassLoader)
+
+  def mustStop(foo: Foo) = stop(foo) must be (true)
+
+  "ThaipedActors" must {
+
+    "be able to instantiate" in {
+      val t = newFooBar
+      isThaipedActor(t) must be(true)
+      mustStop(t)
+    }
+
+    "be able to stop" in {
+      val t = newFooBar
+      mustStop(t)
+    }
+
+    "not stop non-started ones" in {
+      stop(null) must be (false)
+    }
+
+    "be able to call toString" in {
+      val t = newFooBar
+      t.toString must be (getActorFor(t).get.toString)
+      mustStop(t)
+    }
+
+    "be able to call equals" in {
+      val t = newFooBar
+      t must equal (t)
+      t must not equal (null)
+      mustStop(t)
+    }
+
+    "be able to call hashCode" in {
+      val t = newFooBar
+      t.hashCode must be (getActorFor(t).get.hashCode)
+      mustStop(t)
+    }
+
+    "be able to call user-defined void-methods" in {
+      val t = newFooBar
+      t.incr()
+      t.read() must be (1)
+      mustStop(t)
+    }
+
+    "be able to call Future-returning methods non-blockingly" in {
+      val t = newFooBar
+      val f = t.futurePigdog(200)
+      f.isCompleted must be (false)
+      f.get must be ("Pigdog")
+      mustStop(t)
+    }
+
+    "be able to call multiple Future-returning methods non-blockingly" in {
+      val t = newFooBar
+      val futures = for(i <- 1 to 20) yield (i, t.futurePigdog(20,i))
+      for((i,f) <- futures) {
+        f.get must be ("Pigdog"+i)
+      }
+      mustStop(t)
+    }
+
+    "be able to call methods returning Java Options" in {
+      val t = newFooBar(Duration(500,"ms"))
+      t.joptionPigdog(200).get must be ("Pigdog")
+      t.joptionPigdog(700) must be (JOption.none[String])
+      mustStop(t)
+    }
+
+    "be able to call methods returning Scala Options" in {
+      val t = newFooBar(Duration(500,"ms"))
+      t.optionPigdog(200).get must be ("Pigdog")
+      t.optionPigdog(700) must be (None)
+      mustStop(t)
+    }
+
+    "be able to use work-stealing dispatcher" in {
+      val config = Configuration(
+                     Duration(6600,"ms"),
+                     Dispatchers.newExecutorBasedEventDrivenWorkStealingDispatcher("pooled-dispatcher")
+                          .withNewThreadPoolWithLinkedBlockingQueueWithUnboundedCapacity
+                          .setCorePoolSize(60)
+                          .setMaxPoolSize(60)
+                          .build
+                   )
+
+      val thais    = for(i <- 1 to 60) yield newFooBar(config)
+      val iterator = new CyclicIterator(thais)
+
+      val results = for(i <- 1 to 120) yield (i, iterator.next.futurePigdog(200L,i))
+
+      for((i,r) <- results) r.get must be ("Pigdog"+i)
+
+      for(t <- thais) mustStop(t)
+    }
+  }
+}
