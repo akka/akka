8d4622bcd370250c0be8a59460806c8c9c488e3c
Derek Williams <derek@nebvin.ca>
2011-05-22 22:00:04 -0600

    IO Actor initial rewrite

--- akka-actor-tests/src/test/scala/akka/actor/actor/NIOActor.scala
+++ akka-actor-tests/src/test/scala/akka/actor/actor/NIOActor.scala
@@ -8,81 +8,45 @@ import org.scalatest.WordSpec
 import org.scalatest.matchers.MustMatchers
 import org.scalatest.BeforeAndAfterEach
 
-import akka.config.Supervision.Temporary
+import akka.config.Supervision.{ Permanent, Temporary }
 
-import java.nio.ByteBuffer
-import java.nio.channels.SocketChannel
+object IOActorSpec {
 
-import scala.annotation.tailrec
-import scala.collection.immutable.Queue
+  class EchoServer(val host: String, val port: Int) extends Actor {
 
-object NIOActorSpec {
+    self.lifeCycle = Permanent
 
-  class EchoServer(val host: String, val port: Int) extends Actor with NIOSimpleServer {
+    var ioManager: ActorRef = _
 
-    def createWorker = new EchoServerWorker(nioHandler)
+    override def preStart = {
+      ioManager = self startLink (Actor.actorOf(new IOManager))
+      ioManager ! IO.CreateServer(self, host, port)
+    }
 
     def receive = {
-      case "Ping" ⇒ self reply_? "Pong"
+      case IO.NewConnection ⇒
+        self reply IO.AcceptConnection(self, self startLink (Actor.actorOf(new EchoServerWorker(ioManager))))
     }
 
   }
 
-  class EchoServerWorker(val nioHandler: NIOHandler) extends Actor with NIO {
-    val readBuffer = ByteBuffer allocate 8192
-    var writeBuffers = Queue.empty[ByteBuffer]
+  class EchoServerWorker(val ioManager: ActorRef) extends Actor {
 
     self.lifeCycle = Temporary
 
     def receive = {
-      case "Ping" ⇒ self reply_? "Pong"
+      case IO.Read(bytes) ⇒ self reply IO.Write(self, bytes)
+      case IO.Closed      ⇒ self.stop
     }
 
-    def receiveIO = {
-      case NIO.Read(client: SocketChannel)  ⇒ read(client)
-      case NIO.Write(client: SocketChannel) ⇒ write(client)
-    }
-
-    @tailrec
-    final def read(client: SocketChannel): Unit = {
-      readBuffer clear ()
-      val readLen = client read readBuffer
-      if (readLen == -1) {
-        nioHandler unregister client
-        client close ()
-        self stop ()
-      } else {
-        val bytes = new Array[Byte](readLen)
-        readBuffer flip ()
-        readBuffer get bytes
-        writeBuffers = writeBuffers enqueue (ByteBuffer wrap bytes)
-        nioHandler interestOps (client, NIO.Read, NIO.Write)
-        if (readLen == readBuffer.capacity) read(client)
-      }
-    }
-
-    @tailrec
-    final def write(client: SocketChannel): Unit = {
-      if (writeBuffers.nonEmpty) {
-        val (buf, bufs) = writeBuffers.dequeue
-        val writeLen = client write buf
-        if (buf.remaining == 0) {
-          writeBuffers = bufs
-          if (writeBuffers.isEmpty) {
-            nioHandler interestOps (client, NIO.Read)
-          } else {
-            write(client)
-          }
-        }
-      }
-    }
   }
+
 }
 
-class NIOActorSpec extends WordSpec with MustMatchers with BeforeAndAfterEach {
-  import NIOActorSpec._
+class IOActorSpec extends WordSpec with MustMatchers with BeforeAndAfterEach {
+  import IOActorSpec._
 
-  "an NIO Actor" must {
+  "an IO Actor" must {
     "run" in {
       val actor = Actor.actorOf(new EchoServer("localhost", 8064)).start
       Thread.sleep(600000)
--- akka-actor/src/main/scala/akka/actor/NIO.scala
+++ akka-actor/src/main/scala/akka/actor/NIO.scala
@@ -4,213 +4,185 @@
 package akka.actor
 
 import akka.config.Supervision.Permanent
+import akka.util.ByteString
 
-import java.nio.channels.{ SelectableChannel, ServerSocketChannel, Selector, SelectionKey, CancelledKeyException }
+import java.nio.ByteBuffer
+import java.nio.channels.{ SelectableChannel, ReadableByteChannel, WritableByteChannel, ServerSocketChannel, Selector, SelectionKey, CancelledKeyException }
 import java.net.InetSocketAddress
 import java.io.IOException
 import java.util.concurrent.atomic.AtomicReference
 import java.util.Collections.synchronizedSet
 import java.util.HashSet
 
+import scala.collection.immutable.Queue
 import scala.annotation.tailrec
 
-object NIO {
+object IO {
 
-  object Ops {
-    def apply(op: Op): Int = op.value
-    def apply(op1: Op, op2: Op, ops: Op*): Int = op1.value | op2.value | apply(ops)
-    def apply(ops: Traversable[Op]): Int = (0 /: ops)(_ | _.value)
-  }
+  case class CreateServer(owner: ActorRef, host: String, port: Int)
+  case object NewConnection
+  case class AcceptConnection(server: ActorRef, owner: ActorRef)
+  case object Close
+  case object Closed
 
-  sealed trait Op { def value: Int }
-  object Connect extends Op { val value = SelectionKey.OP_CONNECT }
-  object Accept extends Op { val value = SelectionKey.OP_ACCEPT }
-  object Read extends Op { val value = SelectionKey.OP_READ }
-  object Write extends Op { val value = SelectionKey.OP_WRITE }
-
-  sealed trait SelectedOp { def channel: SelectableChannel; def value: Int }
-  case class Connect(channel: SelectableChannel) extends SelectedOp { def value = Connect.value }
-  case class Accept(channel: SelectableChannel) extends SelectedOp { def value = Accept.value }
-  case class Read(channel: SelectableChannel) extends SelectedOp { def value = Read.value }
-  case class Write(channel: SelectableChannel) extends SelectedOp { def value = Write.value }
+  case class Read(bytes: ByteString)
+  case class Write(owner: ActorRef, bytes: ByteString)
 
 }
 
-trait NIO {
-  this: Actor ⇒
+class IOManager extends Actor {
 
-  def nioHandler: NIOHandler
+  var worker: IOWorker = _
+  var workerThread: Thread = _
 
-  val originalReceive = receive
-
-  become {
-    case selectedOp: NIO.SelectedOp ⇒
-      try {
-        receiveIO(selectedOp)
-      } catch {
-        case e: IOException ⇒ // Any reason why this is bad?
-          nioHandler unregister selectedOp.channel
-          selectedOp.channel.close
-          throw e
-      }
-      finally {
-        nioHandler received selectedOp
-      }
-    case other if originalReceive.isDefinedAt(other) ⇒ originalReceive(other)
+  override def preStart: Unit = {
+    worker = new IOWorker(self)
+    workerThread = new Thread(worker)
+    workerThread.start
   }
 
-  def receiveIO: PartialFunction[NIO.SelectedOp, Unit]
+  def receive = {
+    case IO.CreateServer(owner, host, port) ⇒ worker.createServer(owner, host, port)
+    case IO.AcceptConnection(server, owner) ⇒ worker.acceptConnection(server, owner)
+    case IO.Write(owner, data)              ⇒ worker.write(owner, data)
+  }
+
 }
 
-trait NIOSimpleServer extends NIO {
-  this: Actor ⇒
+private[akka] object IOWorker {
+  sealed trait ChangeRequest
+  case class Register(owner: ActorRef, channel: SelectableChannel, ops: Int) extends ChangeRequest
+  case class Accepted(server: ActorRef, owner: ActorRef) extends ChangeRequest
+  case class QueueWrite(owner: ActorRef, data: ByteBuffer) extends ChangeRequest
+  case class InterestOps(channel: SelectableChannel, interestOps: Int) extends ChangeRequest
+}
 
-  def host: String
+private[akka] class IOWorker(ioManager: ActorRef) extends Runnable {
+  import SelectionKey.{ OP_READ, OP_WRITE, OP_ACCEPT, OP_CONNECT }
+  import IOWorker._
 
-  def port: Int
+  type ReadChannel = ReadableByteChannel with SelectableChannel
+  type WriteChannel = WritableByteChannel with SelectableChannel
 
-  def createWorker: Actor with NIO
+  implicit val optionIOManager: Some[ActorRef] = Some(ioManager)
 
-  self.lifeCycle = Permanent
+  val bufferSize = 8192 // make configurable
 
-  var nioHandler: NIOHandler = _
-
-  override def preStart = {
-    nioHandler = new NIOHandler
+  def createServer(owner: ActorRef, host: String, port: Int): Unit = {
     val inetAddress = new InetSocketAddress(host: String, port: Int)
     val server = ServerSocketChannel open ()
     server configureBlocking false
     server.socket bind inetAddress
-    nioHandler register (server, NIO.Accept)
+    addChangeRequest(Register(owner, server, OP_ACCEPT))
   }
 
-  override def preRestart(reason: scala.Throwable) = {
-    nioHandler stop ()
-  }
+  def acceptConnection(server: ActorRef, owner: ActorRef): Unit =
+    addChangeRequest(Accepted(server, owner))
 
-  def receiveIO = {
-    case NIO.Accept(server: ServerSocketChannel) ⇒ accept(server)
-  }
+  def write(owner: ActorRef, data: ByteString): Unit =
+    addChangeRequest(QueueWrite(owner, data.asByteBuffer))
 
-  @tailrec
-  final def accept(server: ServerSocketChannel): Unit = {
-    val client = server accept ()
-    if (client ne null) {
-      client configureBlocking false
-      nioHandler.register(client, NIO.Read)(Some(self startLink (Actor.actorOf(createWorker))))
-      accept(server)
-    }
-  }
-}
-
-object NIOHandler {
-  sealed trait ChangeRequest { def channel: SelectableChannel }
-  case class Register(channel: SelectableChannel, interestOps: Int, owner: ActorRef) extends ChangeRequest
-  case class InterestOps(channel: SelectableChannel, interestOps: Int) extends ChangeRequest
-}
-
-class NIOHandler {
-  import NIOHandler._
-
-  private val _activeOps = new AtomicReference(Map.empty[SelectableChannel, Int].withDefaultValue(0))
-
-  private val _changeRequests = new AtomicReference(List.empty[ChangeRequest])
+  // private
 
   private val selector: Selector = Selector open ()
 
-  private val select = new Runnable {
-    def run(): Unit = {
-      while (selector.isOpen) {
-        val keys = selector.selectedKeys.iterator
-        while (keys.hasNext) {
-          val key = keys next ()
-          keys remove ()
-          if (key.isValid) { notifyOwner(key) }
+  private val _changeRequests = new AtomicReference(List.empty[ChangeRequest])
+
+  private var acceptedChannels = Map.empty[ActorRef, Queue[SelectableChannel]].withDefaultValue(Queue.empty)
+
+  private var channels = Map.empty[ActorRef, SelectableChannel]
+
+  private var writeQueues = Map.empty[ActorRef, Queue[ByteBuffer]].withDefaultValue(Queue.empty)
+
+  private val buffer = ByteBuffer.allocate(bufferSize)
+
+  def run(): Unit = {
+    while (selector.isOpen) {
+      val keys = selector.selectedKeys.iterator
+      while (keys.hasNext) {
+        val key = keys next ()
+        keys remove ()
+        if (key.isValid) { process(key) }
+      }
+      _changeRequests.getAndSet(Nil).reverse foreach {
+        case Register(owner, channel, ops) ⇒ channel register (selector, ops, owner)
+        case Accepted(server, owner) ⇒
+          val (channel, rest) = acceptedChannels(server).dequeue
+          if (rest.isEmpty) acceptedChannels -= server
+          else acceptedChannels += (server -> rest)
+          channels += (owner -> channel)
+          channel register (selector, OP_READ, owner)
+        case QueueWrite(owner, data) ⇒
+          if (channels contains owner) {
+            val queue = writeQueues(owner)
+            if (queue.isEmpty) channels(owner) keyFor selector interestOps OP_WRITE
+            writeQueues += (owner -> queue.enqueue(data))
+          }
+        case InterestOps(channel, ops) ⇒ channel keyFor selector interestOps ops
+      }
+      selector select ()
+    }
+  }
+
+  private def process(key: SelectionKey): Unit = try {
+    val owner = key.attachment.asInstanceOf[ActorRef]
+    //if (key.isConnectable) connected(key)
+    if (key.isAcceptable) key.channel match {
+      case server: ServerSocketChannel ⇒ accept(owner, server)
+    }
+    if (key.isReadable) key.channel match {
+      case channel: ReadChannel ⇒ read(owner, channel)
+    }
+    if (key.isWritable) key.channel match {
+      case channel: WriteChannel ⇒ write(owner, channel)
+    }
+  } catch {
+    case e: CancelledKeyException ⇒ println("Got " + e)
+  }
+
+  @tailrec
+  final def accept(owner: ActorRef, server: ServerSocketChannel): Unit = {
+    val client = server.accept
+    if (client ne null) {
+      client configureBlocking false
+      acceptedChannels += (owner -> (acceptedChannels(owner) enqueue client))
+      owner ! IO.NewConnection
+      accept(owner, server)
+    }
+  }
+
+  @tailrec
+  final def read(owner: ActorRef, channel: ReadChannel): Unit = {
+    buffer.clear
+    val readLen = channel read buffer
+    if (readLen == -1) {
+      channel.close
+      channels -= owner
+      owner ! IO.Closed
+    } else if (readLen > 0) {
+      buffer.flip
+      owner ! IO.Read(ByteString(buffer))
+      if (readLen == buffer.capacity) read(owner, channel)
+    }
+  }
+
+  @tailrec
+  final def write(owner: ActorRef, channel: WriteChannel): Unit = {
+    val queue = writeQueues(owner)
+    if (queue.nonEmpty) {
+      val (buf, bufs) = queue.dequeue
+      val writeLen = channel write buf
+      if (buf.remaining == 0) {
+        if (bufs.isEmpty) {
+          writeQueues -= owner
+          addChangeRequest(InterestOps(channel, OP_READ))
+        } else {
+          writeQueues += (owner -> bufs)
+          write(owner, channel)
         }
-        _changeRequests.getAndSet(Nil).reverse foreach {
-          case Register(channel, ops, owner) ⇒ channel register (selector, ops, owner)
-          case InterestOps(channel, ops) ⇒
-            try {
-              channel keyFor selector interestOps ops
-            } catch {
-              case e: CancelledKeyException ⇒ resetActiveOps(channel)
-            }
-        }
-        selector select ()
       }
     }
   }
-
-  private val thread = new Thread(select)
-  thread.start
-
-  private def notifyOwner(key: SelectionKey): Unit = {
-    val channel = key.channel
-
-    @tailrec
-    def updateActiveOps(): Int = {
-      val activeOps = _activeOps.get
-      val ops = activeOps(channel)
-      val diffOps = (key.readyOps | ops) - (key.readyOps & ops)
-      val newOps = diffOps - (diffOps & ops)
-      if (newOps > 0) {
-        if (_activeOps.compareAndSet(activeOps, activeOps.updated(channel, ops | newOps))) newOps
-        else updateActiveOps()
-      } else 0
-    }
-
-    try {
-      val ops = updateActiveOps()
-      if (ops > 0) {
-        val owner = key.attachment.asInstanceOf[ActorRef]
-        if ((ops & NIO.Connect.value) > 0) owner ! NIO.Connect(channel)
-        if ((ops & NIO.Accept.value) > 0) owner ! NIO.Accept(channel)
-        if ((ops & NIO.Read.value) > 0) owner ! NIO.Read(channel)
-        if ((ops & NIO.Write.value) > 0) owner ! NIO.Write(channel)
-      }
-    } catch {
-      case e: CancelledKeyException        ⇒ resetActiveOps(channel)
-      case e: ActorInitializationException ⇒ resetActiveOps(channel)
-    }
-  }
-
-  private def resetActiveOps(channel: SelectableChannel): Unit = {
-    val activeOps = _activeOps.get
-    if (!_activeOps.compareAndSet(activeOps, activeOps - channel)) resetActiveOps(channel)
-  }
-
-  final def register(channel: SelectableChannel)(implicit owner: Some[ActorRef]): Unit =
-    register(channel, 0)(owner)
-
-  final def register(channel: SelectableChannel, op: NIO.Op)(implicit owner: Some[ActorRef]): Unit =
-    register(channel, NIO.Ops(op))(owner)
-
-  final def register(channel: SelectableChannel, op1: NIO.Op, op2: NIO.Op, ops: NIO.Op*)(implicit owner: Some[ActorRef]): Unit =
-    register(channel, NIO.Ops(op1, op2, ops: _*))(owner)
-
-  final def register(channel: SelectableChannel, ops: Traversable[NIO.Op])(implicit owner: Some[ActorRef]): Unit =
-    register(channel, NIO.Ops(ops))(owner)
-
-  final def register(channel: SelectableChannel, ops: Int)(implicit owner: Some[ActorRef]): Unit =
-    addChangeRequest(Register(channel, ops, owner.get))
-
-  final def unregister(channel: SelectableChannel): Unit =
-    channel keyFor selector cancel ()
-
-  final def interestOps(channel: SelectableChannel): Unit =
-    interestOps(channel, 0)
-
-  final def interestOps(channel: SelectableChannel, op: NIO.Op): Unit =
-    interestOps(channel, NIO.Ops(op))
-
-  final def interestOps(channel: SelectableChannel, op1: NIO.Op, op2: NIO.Op, ops: NIO.Op*): Unit =
-    interestOps(channel, NIO.Ops(op1, op2, ops: _*))
-
-  final def interestOps(channel: SelectableChannel, ops: Traversable[NIO.Op]): Unit =
-    interestOps(channel, NIO.Ops(ops))
-
-  final def interestOps(channel: SelectableChannel, ops: Int): Unit =
-    addChangeRequest(InterestOps(channel, ops))
 
   @tailrec
   private def addChangeRequest(req: ChangeRequest): Unit = {
@@ -220,21 +192,4 @@ class NIOHandler {
     else
       addChangeRequest(req)
   }
-
-  @tailrec
-  private[akka] final def received(op: NIO.SelectedOp): Unit = {
-    val activeOps = _activeOps.get
-    val ops = activeOps(op.channel)
-    if ((ops & op.value) > 0) {
-      val newOps = ops - op.value
-      if (newOps == 0) {
-        if (!_activeOps.compareAndSet(activeOps, activeOps - op.channel)) received(op)
-      } else {
-        if (!_activeOps.compareAndSet(activeOps, activeOps.updated(op.channel, newOps))) received(op)
-      }
-    }
-  }
-
-  final def stop(): Unit = selector close ()
-
 }
