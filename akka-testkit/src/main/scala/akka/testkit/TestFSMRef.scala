4cf73809cf7a9a1d65d25cfdf21924099b1ca839
Roland <rk@rkuhn.info>
2011-06-04 17:37:10 +0200

    actually really add TestFSMRef

---
+++ akka-testkit/src/main/scala/akka/testkit/TestFSMRef.scala
@@ -0,0 +1,77 @@
+/**
+ * Copyright (C) 2009-2011 Scalable Solutions AB <http://scalablesolutions.se>
+ */
+
+package akka.testkit
+
+import akka.actor._
+import akka.util.Duration
+
+/**
+ * This is a specialised form of the TestActorRef with support for querying and
+ * setting the state of a FSM. Use a LoggingFSM with this class if you also
+ * need to inspect event traces.
+ *
+ * <pre><code>
+ * val fsm = TestFSMRef(new Actor with LoggingFSM[Int, Null] {
+ *     override def logDepth = 12
+ *     startWith(1, null)
+ *     when(1) {
+ *       case Ev("hello") =&gt; goto(2)
+ *     }
+ *     when(2) {
+ *       case Ev("world") =&gt; goto(1)
+ *     }
+ *   }
+ * assert (fsm.stateName == 1)
+ * fsm ! "hallo"
+ * assert (fsm.stateName == 2)
+ * assert (fsm.underlyingActor.getLog == IndexedSeq(FSMLogEntry(1, null, "hallo")))
+ * </code></pre>
+ *
+ * @author Roland Kuhn
+ * @since 1.2
+ */
+class TestFSMRef[S, D, T <: Actor with FSM[S, D]](factory: () => T) extends TestActorRef(factory) {
+
+  private def fsm = underlyingActor
+
+  /**
+   * Get current state name of this FSM.
+   */
+  def stateName: S = fsm.stateName
+
+  /**
+   * Get current state data of this FSM.
+   */
+  def stateData: D = fsm.stateData
+
+  /**
+   * Change FSM state; any value left out defaults to the current FSM state
+   * (timeout defaults to None). This method is directly equivalent to a
+   * corresponding transition initiated from within the FSM, including timeout
+   * and stop handling.
+   */
+  def setState(stateName: S = fsm.stateName, stateData: D = fsm.stateData, timeout: Option[Duration] = None) {
+    val f = fsm // needed to make the following type-check
+    f.applyState(f.State(stateName, stateData, timeout))
+  }
+
+  /**
+   * Proxy for FSM.setTimer.
+   */
+  def setTimer(name: String, msg: Any, timeout: Duration, repeat: Boolean) {
+    fsm.setTimer(name, msg, timeout, repeat)
+  }
+
+  /**
+   * Proxy for FSM.cancelTimer.
+   */
+  def cancelTimer(name: String) { fsm.cancelTimer(name) }
+
+  /**
+   * Proxy for FSM.timerActive_?.
+   */
+  def timerActive_?(name: String) = fsm.timerActive_?(name)
+
+}
