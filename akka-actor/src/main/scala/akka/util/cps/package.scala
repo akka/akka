b559c115be57ea614b84664c99ad02cd3ac7eda9
Derek Williams <derek@nebvin.ca>
2011-06-04 22:17:16 -0600

    Manually transform CPS in Future loops in order to optimize

--- akka-actor/src/main/scala/akka/util/cps/package.scala
+++ akka-actor/src/main/scala/akka/util/cps/package.scala
@@ -17,50 +17,39 @@ package object cps {
 
 package cps {
   object CPSLoop extends DefaultCPSLoop {
-    import akka.dispatch.{ Future, Promise }
 
-    implicit val futureCPSLoop: CPSLoop[Future[Any]] = new CPSLoop[Future[Any]] {
-
-      def loopC(block: ⇒ Any @cps[Future[Any]]): Unit @cps[Future[Any]] = {
-        shift { c: (Unit ⇒ Future[Any]) ⇒
-          Future.flow {
-            block
-            Future(()).apply
-            loopC(block)
-          } flatMap c
-        }
-      }
-
-      def whileC(test: ⇒ Boolean)(block: ⇒ Unit @cps[Future[Any]]): Unit @cps[Future[Any]] = {
-        shift { c: (Unit ⇒ Future[Any]) ⇒
-          Future.flow {
-            if (test) {
-              block
-              Future(()).apply
-              whileC(test)(block)
-            }
-          } flatMap c
-        }
-      }
-
-      def repeatC(times: Int)(block: ⇒ Any @cps[Future[Any]]): Unit @cps[Future[Any]] = {
-        shift { c: (Unit ⇒ Future[Any]) ⇒
-          Future.flow {
-            if (times > 0) {
-              block
-              Future(()).apply
-              repeatC(times - 1)(block)
-            }
-          } flatMap c
-        }
-      }
-    }
+    implicit object FutureCPSLoop extends FutureCPSLoop
   }
 
   trait CPSLoop[A] {
     def loopC(block: ⇒ Any @cps[A]): Unit @cps[A]
     def whileC(test: ⇒ Boolean)(block: ⇒ Unit @cps[A]): Unit @cps[A]
     def repeatC(times: Int)(block: ⇒ Any @cps[A]): Unit @cps[A]
+  }
+
+  import akka.dispatch.{ Future, Promise }
+  class FutureCPSLoop extends CPSLoop[Future[Any]] {
+
+    def loopC(block: ⇒ Any @cps[Future[Any]]): Unit @cps[Future[Any]] =
+      shift { c: (Unit ⇒ Future[Any]) ⇒
+        Future(reify(block) flatMap (_ ⇒ reify(loopC(block))) foreach c)
+      }
+
+    def whileC(test: ⇒ Boolean)(block: ⇒ Unit @cps[Future[Any]]): Unit @cps[Future[Any]] =
+      shift { c: (Unit ⇒ Future[Any]) ⇒
+        if (test)
+          Future(reify(block) flatMap (_ ⇒ reify(whileC(test)(block))) foreach c)
+        else
+          Promise() completeWithResult (shiftUnitR[Unit, Future[Any]](()) foreach c)
+      }
+
+    def repeatC(times: Int)(block: ⇒ Any @cps[Future[Any]]): Unit @cps[Future[Any]] =
+      shift { c: (Unit ⇒ Future[Any]) ⇒
+        if (times > 0)
+          Future(reify(block) flatMap (_ ⇒ reify(repeatC(times - 1)(block))) foreach c)
+        else
+          Promise() completeWithResult (shiftUnitR[Unit, Future[Any]](()) foreach c)
+      }
   }
 
   trait DefaultCPSLoop {
