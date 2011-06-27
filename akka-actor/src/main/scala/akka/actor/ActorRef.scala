10256991cbd1e52fbebab5d426dee7331e786652
Roland <rk@rkuhn.info>
2011-06-27 19:09:09 +0200

    add Actor.freshInstance hook, test #955

--- akka-actor/src/main/scala/akka/actor/ActorRef.scala
+++ akka-actor/src/main/scala/akka/actor/ActorRef.scala
@@ -845,7 +845,20 @@ class LocalActorRef private[akka] (
     val stackBefore = refStack.get
     refStack.set(stackBefore.push(this))
     try {
-      actorFactory()
+      if (_status == ActorRefInternals.BEING_RESTARTED) {
+        val a = actor
+        val fresh = try a.freshInstance catch {
+          case e ⇒
+            EventHandler.error(e, a, "freshInstance() failed, falling back to initial actor factory")
+            None
+        }
+        fresh match {
+          case Some(ref) ⇒ ref
+          case None      ⇒ actorFactory()
+        }
+      } else {
+        actorFactory()
+      }
     } finally {
       val stackAfter = refStack.get
       if (stackAfter.nonEmpty)
