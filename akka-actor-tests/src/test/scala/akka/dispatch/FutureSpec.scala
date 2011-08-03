e565f9cb0407c80d63a22e71a9a1c9663357e720
Viktor Klang <viktor.klang@gmail.com>
2011-08-03 15:43:40 +0200

    Reenabling mutable zeroes test

--- akka-actor-tests/src/test/scala/akka/dispatch/FutureSpec.scala
+++ akka-actor-tests/src/test/scala/akka/dispatch/FutureSpec.scala
@@ -315,21 +315,20 @@ class FutureSpec extends WordSpec with MustMatchers with Checkers with BeforeAnd
         }
       }
 
-      /*  @Test
-  def shouldFoldMutableZeroes {
-    import scala.collection.mutable.ArrayBuffer
-    def test(testNumber: Int) {
-      val fs = (0 to 1000) map (i ⇒ Future(i, 10000))
-      val result = Futures.fold(ArrayBuffer.empty[AnyRef], 10000)(fs) {
-        case (l, i) if i % 2 == 0 ⇒ l += i.asInstanceOf[AnyRef]
-        case (l, _)               ⇒ l
-      }.get.asInstanceOf[ArrayBuffer[Int]].sum
+      "fold mutable zeroes safely" in {
+        import scala.collection.mutable.ArrayBuffer
+        def test(testNumber: Int) {
+          val fs = (0 to 1000) map (i ⇒ Future(i, 10000))
+          val result = Futures.fold(ArrayBuffer.empty[AnyRef], 10000)(fs) {
+            case (l, i) if i % 2 == 0 ⇒ l += i.asInstanceOf[AnyRef]
+            case (l, _)               ⇒ l
+          }.get.asInstanceOf[ArrayBuffer[Int]].sum
 
-      assert(result === 250500)
-    }
+          assert(result === 250500)
+        }
 
-    (1 to 100) foreach test //Make sure it tries to provoke the problem
-  }*/
+        (1 to 100) foreach test //Make sure it tries to provoke the problem
+      }
 
       "return zero value if folding empty list" in {
         Futures.fold(0)(List[Future[Int]]())(_ + _).get must be(0)
