5c7e0cd45d94a2466d4881a598c5d8c948e32cc1
Jonas Bonér <jonas@jonasboner.com>
2011-08-19 08:31:47 +0200

    Renamed and rewrote DirectRoutingFailoverMultiJvmSpec.
    Signed-off-by: Jonas Bonér <jonas@jonasboner.com>

---
+++ akka-cluster/src/multi-jvm/scala/akka/cluster/routing/direct/failover/DirectRoutingFailoverMultiJvmSpec.scala
@@ -0,0 +1,86 @@
+package akka.cluster.routing.direct.failover
+
+import akka.config.Config
+import scala.Predef._
+import akka.cluster.{ ClusterActorRef, Cluster, MasterClusterTestNode, ClusterTestNode }
+import akka.actor.{ ActorInitializationException, Actor, ActorRef }
+import akka.event.EventHandler
+import akka.testkit.{ EventFilter, TestEvent }
+import java.net.ConnectException
+import java.nio.channels.NotYetConnectedException
+
+object DirectRoutingFailoverMultiJvmSpec {
+
+  val NrOfNodes = 2
+
+  class SomeActor extends Actor with Serializable {
+
+    def receive = {
+      case "identify" ⇒
+        self.reply(Config.nodename)
+    }
+  }
+}
+
+class DirectRoutingFailoverMultiJvmNode1 extends MasterClusterTestNode {
+
+  import DirectRoutingFailoverMultiJvmSpec._
+
+  val testNodes = NrOfNodes
+
+  "Direct Router" must {
+    "throw exception [ActorInitializationException] upon fail-over" ignore {
+
+      val ignoreExceptions = Seq(EventFilter[NotYetConnectedException], EventFilter[ConnectException])
+      EventHandler.notify(TestEvent.Mute(ignoreExceptions))
+
+      var actor: ActorRef = null
+
+      Cluster.barrier("node-start", NrOfNodes) {
+        Cluster.node
+      }
+
+      Cluster.barrier("actor-creation", NrOfNodes) {
+        actor = Actor.actorOf[SomeActor]("service-hello")
+      }
+
+      Cluster.barrier("verify-actor", NrOfNodes) {
+        (actor ? "identify").get must equal("node2")
+      }
+
+      Thread.sleep(5000) // wait for fail-over from node2
+
+      Cluster.barrier("verify-fail-over", NrOfNodes - 1) {
+        actor ! "identify" // trigger failure and removal of connection to node2
+        intercept[Exception] {
+          actor ! "identify" // trigger exception since no more connections
+        }
+      }
+
+      Cluster.node.shutdown()
+    }
+  }
+}
+
+class DirectRoutingFailoverMultiJvmNode2 extends ClusterTestNode {
+
+  import DirectRoutingFailoverMultiJvmSpec._
+
+  "___" must {
+    "___" ignore {
+      Cluster.barrier("node-start", NrOfNodes) {
+        Cluster.node
+      }
+
+      Cluster.barrier("actor-creation", NrOfNodes) {
+      }
+
+      Cluster.barrier("verify-actor", NrOfNodes) {
+        Cluster.node.isInUseOnNode("service-hello") must be(true)
+      }
+
+      Cluster.node.shutdown()
+    }
+  }
+}
+
