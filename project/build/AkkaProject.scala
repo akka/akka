46ca3c3c30994b2210bd019bb454f7026a6b5043
Viktor Klang <viktor.klang@gmail.com>
2011-08-24 14:06:58 +0200

    Adding akka-durable-mailboxes to the akkaCoreDist

--- project/build/AkkaProject.scala
+++ project/build/AkkaProject.scala
@@ -576,7 +576,7 @@ class AkkaParentProject(info: ProjectInfo) extends ParentProject(info) with Exec
     lazy val akkaActorsDist = project("actors", "akka-dist-actors", new AkkaActorsDistProject(_), akka_actor)
 
     lazy val akkaCoreDist = project("core", "akka-dist-core", new AkkaCoreDistProject(_),
-                                    akkaActorsDist, akka_remote, akka_http, akka_slf4j, akka_testkit, akka_actor_tests)
+                                    akkaActorsDist, akka_remote, akka_http, akka_slf4j, akka_testkit, akka_actor_tests, akka_durable_mailboxes)
 
     def doNothing = task { None }
     override def publishLocalAction = doNothing
