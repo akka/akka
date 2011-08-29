bbb9bc2c5e0ca0cbcc0a417c09243e2610a511f7
Peter Vlugter <peter@maka>
2011-08-29 15:43:55 +0200

    Manually fix protocols for scaladoc generation. See #1017

--- akka-cluster/src/main/java/akka/cluster/ClusterProtocol.java
+++ akka-cluster/src/main/java/akka/cluster/ClusterProtocol.java
@@ -431,7 +431,7 @@ public final class ClusterProtocol {
         maybeForceBuilderInitialization();
       }
       
-      private Builder(BuilderParent parent) {
+      private Builder(com.google.protobuf.GeneratedMessage.BuilderParent parent) {
         super(parent);
         maybeForceBuilderInitialization();
       }
@@ -1244,7 +1244,7 @@ public final class ClusterProtocol {
         maybeForceBuilderInitialization();
       }
       
-      private Builder(BuilderParent parent) {
+      private Builder(com.google.protobuf.GeneratedMessage.BuilderParent parent) {
         super(parent);
         maybeForceBuilderInitialization();
       }
@@ -1846,7 +1846,7 @@ public final class ClusterProtocol {
         maybeForceBuilderInitialization();
       }
       
-      private Builder(BuilderParent parent) {
+      private Builder(com.google.protobuf.GeneratedMessage.BuilderParent parent) {
         super(parent);
         maybeForceBuilderInitialization();
       }
