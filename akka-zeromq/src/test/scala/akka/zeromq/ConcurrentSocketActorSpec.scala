a911a1ffe5f863e052807d8d8866b3b21369c5fe
Karim Osman <karim@iki.fi>
2011-10-12 09:40:23 +0300

    akka-zeromq: Add test case for zero-length message frames
    While at it, use "5 seconds" instead of "5000 milliseconds" when passing
    the duration for #within. Also, add two probes, for each side of the
    connection, and invoke #term for the context in the finally block.
    
    Signed-off-by: Karim Osman <karim@iki.fi>

--- akka-zeromq/src/test/scala/akka/zeromq/ConcurrentSocketActorSpec.scala
+++ akka-zeromq/src/test/scala/akka/zeromq/ConcurrentSocketActorSpec.scala
@@ -4,45 +4,63 @@
 package akka.zeromq
 
 import akka.actor.{Actor, ActorRef}
-import akka.testkit.TestKit
+import akka.testkit.{TestKit, TestProbe}
 import akka.util.Duration
 import akka.util.duration._
 import java.util.Arrays
-import org.scalatest.matchers.MustMatchers
 import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 
 class ConcurrentSocketActorSpec extends WordSpec with MustMatchers with TestKit {
   val endpoint = "inproc://PubSubConnectionSpec"
   "ConcurrentSocketActor" should {
     "support pub-sub connections" in {
+      val (publisherProbe, subscriberProbe) = (TestProbe(), TestProbe())
       val message = ZMQMessage("hello".getBytes)
       var context: Option[Context] = None
       var publisher: Option[ActorRef] = None
       var subscriber: Option[ActorRef] = None
       try {
         context = Some(ZeroMQ.newContext)
-        publisher = newPublisher(context.get)
-        subscriber = newSubscriber(context.get)
-        within (5000 millis) {
-          expectMsg(Connected)
+        publisher = newPublisher(context.get, publisherProbe.ref)
+        subscriber = newSubscriber(context.get, subscriberProbe.ref)
+        subscriberProbe.within(5 seconds) {
+          subscriberProbe.expectMsg(Connected)
           publisher ! message
-          expectMsg(message)
+          subscriberProbe.expectMsg(message)
         }
       } finally {
         subscriber ! Close
         publisher ! Close
-        within (5000 millis) {
-          expectMsg(Closed)
+        subscriberProbe.within(5 seconds) {
+          subscriberProbe.expectMsg(Closed)
         }
+        context.foreach(_.term)
       }
     }
-    def newPublisher(context: Context) = {
-      val publisher = ZeroMQ.newSocket(context, SocketType.Pub)
+    "support zero-length message frames" in {
+      val publisherProbe = TestProbe()
+      var publisher: Option[ActorRef] = None
+      var context: Option[Context] = None
+      try {
+        context = Some(ZeroMQ.newContext)
+        publisher = newPublisher(context.get, publisherProbe.ref)
+        publisher ! ZMQMessage(Seq[Frame]())
+      } finally {
+        publisher ! Close
+        publisherProbe.within (5 seconds) {
+          publisherProbe.expectMsg(Closed)
+        }
+        context.foreach(_.term)
+      }
+    }
+    def newPublisher(context: Context, listener: ActorRef) = {
+      val publisher = ZeroMQ.newSocket(context, SocketType.Pub, Some(listener))
       publisher ! Bind(endpoint)
       Some(publisher)
     }
-    def newSubscriber(context: Context) = {
-      val subscriber = ZeroMQ.newSocket(context, SocketType.Sub, Some(testActor))
+    def newSubscriber(context: Context, listener: ActorRef) = {
+      val subscriber = ZeroMQ.newSocket(context, SocketType.Sub, Some(listener))
       subscriber ! Connect(endpoint)
       subscriber ! Subscribe(Seq())
       Some(subscriber)
