88edec35ef79c0ef81f9bbac7af91d9d7457591d
Jonas Bonér <jonas@jonasboner.com>
2011-10-14 15:11:31 +0200

    Vector clock implementation, including tests. To be used in Gossip protocol.
    Signed-off-by: Jonas Bonér <jonas@jonasboner.com>

---
+++ akka-remote/src/test/scala/akka/remote/VectorClockSpec.scala
@@ -0,0 +1,126 @@
+package akka.remote
+
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
+
+import java.net.InetSocketAddress
+
+class VectorClockSpec extends WordSpec with MustMatchers {
+  import VectorClock._
+
+  "An VectorClock" must {
+
+    "have zero versions when created" in {
+      val clock = VectorClock()
+      clock.versions must be(Vector())
+    }
+
+    "be able to add Entry if non-existing" in {
+      val clock1 = VectorClock()
+      clock1.versions must be(Vector())
+      val clock2 = clock1.incrementVersionForNode(1, System.currentTimeMillis)
+      val clock3 = clock2.incrementVersionForNode(2, System.currentTimeMillis)
+
+      clock3.versions must be(Vector(Entry(1, 1), Entry(2, 1)))
+    }
+
+    "be able to increment version of existing Entry" in {
+      val clock1 = VectorClock()
+      val clock2 = clock1.incrementVersionForNode(1, System.currentTimeMillis)
+      val clock3 = clock2.incrementVersionForNode(2, System.currentTimeMillis)
+      val clock4 = clock3.incrementVersionForNode(1, System.currentTimeMillis)
+      val clock5 = clock4.incrementVersionForNode(2, System.currentTimeMillis)
+      val clock6 = clock5.incrementVersionForNode(2, System.currentTimeMillis)
+
+      clock6.versions must be(Vector(Entry(1, 2), Entry(2, 3)))
+    }
+
+    "The empty clock should not happen before itself" in {
+      val clock1 = VectorClock()
+      val clock2 = VectorClock()
+
+      clock1.compare(clock2) must not be (Concurrently)
+    }
+
+    "A clock should not happen before an identical clock" in {
+      val clock1_1 = VectorClock()
+      val clock2_1 = clock1_1.incrementVersionForNode(1, System.currentTimeMillis)
+      val clock3_1 = clock2_1.incrementVersionForNode(2, System.currentTimeMillis)
+      val clock4_1 = clock3_1.incrementVersionForNode(1, System.currentTimeMillis)
+
+      val clock1_2 = VectorClock()
+      val clock2_2 = clock1_2.incrementVersionForNode(1, System.currentTimeMillis)
+      val clock3_2 = clock2_2.incrementVersionForNode(2, System.currentTimeMillis)
+      val clock4_2 = clock3_2.incrementVersionForNode(1, System.currentTimeMillis)
+
+      clock4_1.compare(clock4_2) must not be (Concurrently)
+    }
+
+    "A clock should happen before an identical clock with a single additional event" in {
+      val clock1_1 = VectorClock()
+      val clock2_1 = clock1_1.incrementVersionForNode(1, System.currentTimeMillis)
+      val clock3_1 = clock2_1.incrementVersionForNode(2, System.currentTimeMillis)
+      val clock4_1 = clock3_1.incrementVersionForNode(1, System.currentTimeMillis)
+
+      val clock1_2 = VectorClock()
+      val clock2_2 = clock1_2.incrementVersionForNode(1, System.currentTimeMillis)
+      val clock3_2 = clock2_2.incrementVersionForNode(2, System.currentTimeMillis)
+      val clock4_2 = clock3_2.incrementVersionForNode(1, System.currentTimeMillis)
+      val clock5_2 = clock4_2.incrementVersionForNode(3, System.currentTimeMillis)
+
+      clock4_1.compare(clock5_2) must be(Before)
+    }
+
+    "Two clocks with different events should be concurrent: 1" in {
+      var clock1_1 = VectorClock()
+      val clock2_1 = clock1_1.incrementVersionForNode(1, System.currentTimeMillis)
+
+      val clock1_2 = VectorClock()
+      val clock2_2 = clock1_2.incrementVersionForNode(2, System.currentTimeMillis)
+
+      clock2_1.compare(clock2_2) must be(Concurrently)
+    }
+
+    "Two clocks with different events should be concurrent: 2" in {
+      val clock1_3 = VectorClock()
+      val clock2_3 = clock1_3.incrementVersionForNode(1, System.currentTimeMillis)
+      val clock3_3 = clock2_3.incrementVersionForNode(2, System.currentTimeMillis)
+      val clock4_3 = clock3_3.incrementVersionForNode(1, System.currentTimeMillis)
+
+      val clock1_4 = VectorClock()
+      val clock2_4 = clock1_4.incrementVersionForNode(1, System.currentTimeMillis)
+      val clock3_4 = clock2_4.incrementVersionForNode(1, System.currentTimeMillis)
+      val clock4_4 = clock3_4.incrementVersionForNode(3, System.currentTimeMillis)
+
+      clock4_3.compare(clock4_4) must be(Concurrently)
+    }
+
+    ".." in {
+      val clock1_1 = VectorClock()
+      val clock2_1 = clock1_1.incrementVersionForNode(2, System.currentTimeMillis)
+      val clock3_1 = clock2_1.incrementVersionForNode(2, System.currentTimeMillis)
+
+      val clock1_2 = VectorClock()
+      val clock2_2 = clock1_2.incrementVersionForNode(1, System.currentTimeMillis)
+      val clock3_2 = clock2_2.incrementVersionForNode(2, System.currentTimeMillis)
+      val clock4_2 = clock3_2.incrementVersionForNode(2, System.currentTimeMillis)
+      val clock5_2 = clock4_2.incrementVersionForNode(3, System.currentTimeMillis)
+
+      clock3_1.compare(clock5_2) must be(Before)
+    }
+
+    "..." in {
+      val clock1_1 = VectorClock()
+      val clock2_1 = clock1_1.incrementVersionForNode(1, System.currentTimeMillis)
+      val clock3_1 = clock2_1.incrementVersionForNode(2, System.currentTimeMillis)
+      val clock4_1 = clock3_1.incrementVersionForNode(2, System.currentTimeMillis)
+      val clock5_1 = clock4_1.incrementVersionForNode(3, System.currentTimeMillis)
+
+      val clock1_2 = VectorClock()
+      val clock2_2 = clock1_2.incrementVersionForNode(2, System.currentTimeMillis)
+      val clock3_2 = clock2_2.incrementVersionForNode(2, System.currentTimeMillis)
+
+      clock5_1.compare(clock3_2) must be(After)
+    }
+  }
+}
