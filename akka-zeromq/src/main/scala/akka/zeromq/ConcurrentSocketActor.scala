7bf7007002f8a9750ff2ec8d584e12c996808ba0
Karim Osman <karim@iki.fi>
2011-10-19 11:14:29 +0300

    akka-zeromq: Revise ConcurrentSocketActor using ReceiveTimeout notificatinos
    As pointed out by Viktor Klang, the code is simplified even further when
    polling for messages upon an actor message receive timeout.
    
    Signed-off-by: Karim Osman <karim@iki.fi>

--- akka-zeromq/src/main/scala/akka/zeromq/ConcurrentSocketActor.scala
+++ akka-zeromq/src/main/scala/akka/zeromq/ConcurrentSocketActor.scala
@@ -3,106 +3,86 @@
  */
 package akka.zeromq
 
-import akka.actor.{Actor, ActorRef}
+import akka.actor.{Actor, ReceiveTimeout}
 import akka.dispatch.MessageDispatcher
-import akka.util.Duration
-import akka.zeromq.SocketType._
-import java.util.concurrent.atomic.AtomicReference
 import org.zeromq.ZMQ.{Socket, Poller}
 import org.zeromq.{ZMQ => JZMQ}
-import scala.annotation.tailrec
-import scala.collection.mutable.MutableList
 
 private[zeromq] class ConcurrentSocketActor(params: SocketParameters, dispatcher: MessageDispatcher) extends Actor {
   private val noBytes = Array[Byte]()
-  private val requests = new AtomicReference(Vector.empty[Request])
   private val socket: Socket = params.context.socket(params.socketType)
   private val poller: Poller = params.context.poller
+  self.receiveTimeout = Some(params.pollTimeoutDuration.toMillis)
   self.dispatcher = dispatcher
-  poller.register(socket, Poller.POLLIN)
-  private val selectTask = { () =>
-    def connect(endpoint: String) {
+  override def receive: Receive = {
+    case Send(frames) =>
+      sendFrames(frames)
+      pollAndReceiveFrames()
+    case ZMQMessage(frames) => 
+      sendFrames(frames)
+      pollAndReceiveFrames()
+    case Connect(endpoint) =>
       socket.connect(endpoint)
-      params.listener.foreach { listener =>
-        if (!listener.isShutdown)
-          listener ! Connecting
-      }
-    }
-    def bind(endpoint: String) {
+      notifyListener(Connecting)
+    case Bind(endpoint) => 
       socket.bind(endpoint)
+    case Subscribe(topic) => 
+      socket.subscribe(topic.toArray)
+    case Unsubscribe(topic) => 
+      socket.unsubscribe(topic.toArray)
+    case ReceiveTimeout =>
+      pollAndReceiveFrames()
+  }
+  override def preStart {
+    poller.register(socket, Poller.POLLIN)
+  }
+  override def postStop {
+    poller.unregister(socket)
+    socket.close
+    notifyListener(Closed)
+  }
+  private def sendFrames(frames: Seq[Frame]) {
+    def sendBytes(bytes: Seq[Byte], flags: Int) {
+      socket.send(bytes.toArray, flags)
     }
-    def sendFrames(frames: Seq[Frame]) = for (i <- 0 until frames.length) {
+    for (i <- 0 until frames.length) {
       val flags = if (i < frames.length - 1) JZMQ.SNDMORE else 0
       sendBytes(frames(i).payload, flags)
     }
-    def sendBytes(bytes: Seq[Byte], flags: Int) = {
-      socket.send(bytes.toArray, flags)
+  }
+  private def pollAndReceiveFrames() {
+    def pollSocket: Boolean = {
+      poller.poll(params.pollTimeoutDuration.toMillis) > 0 && poller.pollin(0)
     }
-    def closeSocket = {
-      poller.unregister(socket)
-      socket.close
-      params.listener.foreach { listener =>
-        if (!listener.isShutdown)
-          listener ! Closed
+    if (pollSocket) {
+      receiveFrames match {
+        case frames if (frames.length > 0) => notifyListener(params.deserializer(frames))
       }
     }
-    def receiveFrames: Seq[Frame] = receiveBytes() match {
+  }
+  private def receiveFrames(): Seq[Frame] = {
+    @inline def receiveBytes(): Array[Byte] = socket.recv(0) match {
+      case null => noBytes
+      case bytes: Array[Byte] if bytes.length > 0 => bytes
+      case _ => noBytes
+    }
+    receiveBytes() match {
       case `noBytes` => Vector.empty
-      case someBytes => {
+      case someBytes =>
         var frames = Vector(Frame(someBytes))
         while (socket.hasReceiveMore) receiveBytes() match {
           case `noBytes` =>
           case someBytes => frames :+= Frame(someBytes)
         }
         frames
-      }
     }
-    @inline def receiveBytes(): Array[Byte] = socket.recv(0) match {
-      case null => noBytes
-      case bytes: Array[Byte] if bytes.length > 0 => bytes
-      case _ => noBytes
+  }
+  private def notifyListener(message: Any) {
+    params.listener.foreach { listener =>
+      if (listener.isShutdown)
+        self.stop
+      else
+        listener ! message 
     }
-    val self = this.self
-    if (poller.poll(params.pollTimeoutDuration.toMillis) > 0) {
-      if (poller.pollin(0)) {
-        receiveFrames match {
-          case frames if (frames.length > 0) => params.listener.foreach { listener => 
-            if (!listener.isShutdown)
-              listener ! params.deserializer(frames)
-            else if (self != null && !self.isShutdown)
-              self.stop
-          }
-        }
-      }
-    }
-    requests.getAndSet(Vector.empty).foreach {
-      case Connect(endpoint) => connect(endpoint)
-      case Bind(endpoint) => bind(endpoint)
-      case Close => closeSocket
-      case Send(frames) => sendFrames(frames)
-      case Subscribe(topic) => socket.subscribe(topic.toArray)
-      case Unsubscribe(topic) => socket.unsubscribe(topic.toArray)
-    }
-    if (self != null && !self.isShutdown) 
-      select()
-  }
-  override def preStart {
-    select
-  }
-  override def postStop {
-    addRequest(Close)
-  }
-  override def receive: Receive = {
-    case ZMQMessage(frames) => addRequest(Send(frames))
-    case request: Request => addRequest(request)
-  }
-  private def select() {
-    self.dispatcher.dispatchTask(selectTask)
-  }
-  @tailrec private def addRequest(request: Request) {
-    val oldRequests = requests.get
-    val newRequests = oldRequests :+ request 
-    if (!requests.compareAndSet(oldRequests, newRequests))
-      addRequest(request)
   }
 }
