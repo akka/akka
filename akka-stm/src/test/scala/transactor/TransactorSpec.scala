d2e7a3276114d20d9c701dc09614ff2bfaf42a70
Viktor Klang <viktor.klang@gmail.com>
2011-10-27 14:52:48 +0200

    Making sure that akka.transactor.test.TransactorSpec works with machines with less than 4 cores

--- akka-stm/src/test/scala/transactor/TransactorSpec.scala
+++ akka-stm/src/test/scala/transactor/TransactorSpec.scala
@@ -82,7 +82,7 @@ class TransactorSpec extends AkkaSpec {
 
   implicit val timeout = Timeout(5.seconds.dilated)
 
-  val numCounters = 5
+  val numCounters = 3
 
   def createTransactors = {
     def createCounter(i: Int) = app.actorOf(Props(new Counter("counter" + i)))
