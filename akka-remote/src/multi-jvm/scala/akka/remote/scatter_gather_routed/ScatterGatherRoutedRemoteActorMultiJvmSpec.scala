a9049ec9e5b32958a48d576fae76b0c52da1b903
Jonas Bonér <jonas@jonasboner.com>
2011-11-11 14:53:45 +0100

    Added test for ScatterGatherFirstCompletedRouter. Fixes #1275.
    Signed-off-by: Jonas Bonér <jonas@jonasboner.com>

---
+++ akka-remote/src/multi-jvm/scala/akka/remote/scatter_gather_routed/ScatterGatherRoutedRemoteActorMultiJvmSpec.scala
@@ -0,0 +1,102 @@
+package akka.remote.scatter_gather_routed
+
+import akka.actor.Actor
+import akka.remote._
+import akka.routing._
+import akka.routing.Routing.Broadcast
+
+object ScatterGatherRoutedRemoteActorMultiJvmSpec {
+  val NrOfNodes = 4
+  class SomeActor extends Actor with Serializable {
+    def receive = {
+      case "hit" ⇒ sender ! app.nodename
+      case "end" ⇒ self.stop()
+    }
+  }
+}
+
+class ScatterGatherRoutedRemoteActorMultiJvmNode1 extends AkkaRemoteSpec {
+  import ScatterGatherRoutedRemoteActorMultiJvmSpec._
+  val nodes = NrOfNodes
+  "___" must {
+    "___" in {
+      barrier("setup")
+      remote.start()
+      barrier("start")
+      barrier("broadcast-end")
+      barrier("end")
+      barrier("done")
+    }
+  }
+}
+
+class ScatterGatherRoutedRemoteActorMultiJvmNode2 extends AkkaRemoteSpec {
+  import ScatterGatherRoutedRemoteActorMultiJvmSpec._
+  val nodes = NrOfNodes
+  "___" must {
+    "___" in {
+      barrier("setup")
+      remote.start()
+      barrier("start")
+      barrier("broadcast-end")
+      barrier("end")
+      barrier("done")
+    }
+  }
+}
+
+class ScatterGatherRoutedRemoteActorMultiJvmNode3 extends AkkaRemoteSpec {
+  import ScatterGatherRoutedRemoteActorMultiJvmSpec._
+  val nodes = NrOfNodes
+  "___" must {
+    "___" in {
+      barrier("setup")
+      remote.start()
+      barrier("start")
+      barrier("broadcast-end")
+      barrier("end")
+      barrier("done")
+    }
+  }
+}
+
+class ScatterGatherRoutedRemoteActorMultiJvmNode4 extends AkkaRemoteSpec {
+  import ScatterGatherRoutedRemoteActorMultiJvmSpec._
+  val nodes = NrOfNodes
+  "A new remote actor configured with a ScatterGather router" must {
+    "be locally instantiated on a remote node and be able to communicate through its RemoteActorRef" in {
+
+      barrier("setup")
+      remote.start()
+
+      barrier("start")
+      val actor = app.actorOf[SomeActor]("service-hello")
+      actor.isInstanceOf[RoutedActorRef] must be(true)
+      actor.asInstanceOf[RoutedActorRef].router.isInstanceOf[ScatterGatherFirstCompletedRouter] must be(true)
+
+      val connectionCount = NrOfNodes - 1
+      val iterationCount = 10
+
+      var replies = Map(
+        "node1" -> 0,
+        "node2" -> 0,
+        "node3" -> 0)
+
+      for (i ← 0 until iterationCount) {
+        for (k ← 0 until connectionCount) {
+          val nodeName = (actor ? "hit").as[String].getOrElse(fail("No id returned by actor"))
+          replies = replies + (nodeName -> (replies(nodeName) + 1))
+        }
+      }
+
+      barrier("broadcast-end")
+      actor ! Broadcast("end")
+
+      barrier("end")
+      replies.values foreach { _ must be(10) }
+
+      barrier("done")
+    }
+  }
+}
+
