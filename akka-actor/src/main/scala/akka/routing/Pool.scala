78022f4a1cdf3a54f769af13aea9f322bf69430b
Viktor Klang <viktor.klang@gmail.com>
2011-11-14 14:43:40 +0100

    Removing nonsensical check for current message in ActiveActorsPressureCapacitor

--- akka-actor/src/main/scala/akka/routing/Pool.scala
+++ akka-actor/src/main/scala/akka/routing/Pool.scala
@@ -296,10 +296,8 @@ trait MailboxPressureCapacitor {
 trait ActiveActorsPressureCapacitor {
   def pressure(delegates: Seq[ActorRef]): Int =
     delegates count {
-      case a: LocalActorRef ⇒
-        val cell = a.underlying
-        cell.mailbox.isScheduled && cell.currentMessage != null
-      case _ ⇒ false
+      case a: LocalActorRef ⇒ a.underlying.mailbox.isScheduled
+      case _                ⇒ false
     }
 }
 
