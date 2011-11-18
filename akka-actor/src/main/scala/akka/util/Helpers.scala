8f944f54f192c8dfcfa3cfd0b9992ccb5ef03b11
Viktor Klang <viktor.klang@gmail.com>
2011-11-18 12:52:58 +0100

    Removing unused utilities

--- akka-actor/src/main/scala/akka/util/Helpers.scala
+++ akka-actor/src/main/scala/akka/util/Helpers.scala
@@ -12,8 +12,6 @@ import scala.annotation.tailrec
  */
 object Helpers {
 
-  implicit def null2Option[T](t: T): Option[T] = Option(t)
-
   def compareIdentityHash(a: AnyRef, b: AnyRef): Int = {
     /*
      * make sure that there is no overflow or underflow in comparisons, so 
@@ -71,78 +69,5 @@ object Helpers {
         System.err.println(sw.toString)
         throw e
     }
-  }
-
-  /**
-   * Convenience helper to cast the given Option of Any to an Option of the given type. Will throw a ClassCastException
-   * if the actual type is not assignable from the given one.
-   */
-  def narrow[T](o: Option[Any]): Option[T] = {
-    require((o ne null), "Option to be narrowed must not be null!")
-    o.asInstanceOf[Option[T]]
-  }
-
-  /**
-   * Convenience helper to cast the given Option of Any to an Option of the given type. Will swallow a possible
-   * ClassCastException and return None in that case.
-   */
-  def narrowSilently[T: Manifest](o: Option[Any]): Option[T] =
-    try {
-      narrow(o)
-    } catch {
-      case e: ClassCastException ⇒
-        None
-    }
-
-  /**
-   * Reference that can hold either a typed value or an exception.
-   *
-   * Usage:
-   * <pre>
-   * scala> ResultOrError(1)
-   * res0: ResultOrError[Int] = ResultOrError@a96606
-   *
-   * scala> res0()
-   * res1: Int = 1
-   *
-   * scala> res0() = 3
-   *
-   * scala> res0()
-   * res3: Int = 3
-   *
-   * scala> res0() = { println("Hello world"); 3}
-   * Hello world
-   *
-   * scala> res0()
-   * res5: Int = 3
-   *
-   * scala> res0() = error("Lets see what happens here...")
-   *
-   * scala> res0()
-   * java.lang.RuntimeException: Lets see what happens here...
-   *    at ResultOrError.apply(Helper.scala:11)
-   *    at .<init>(<console>:6)
-   *    at .<clinit>(<console>)
-   *    at Re...
-   * </pre>
-   */
-  class ResultOrError[R](result: R) {
-    private[this] var contents: Either[R, Throwable] = Left(result)
-
-    def update(value: ⇒ R) {
-      contents = try {
-        Left(value)
-      } catch {
-        case (error: Throwable) ⇒ Right(error)
-      }
-    }
-
-    def apply() = contents match {
-      case Left(result) ⇒ result
-      case Right(error) ⇒ throw error.fillInStackTrace
-    }
-  }
-  object ResultOrError {
-    def apply[R](result: R) = new ResultOrError(result)
   }
 }
