44b6c73b816a20c5a8d06339f7670fac3ca20cd3
Ivan Porto Carrero <ivan@flanders.co.nz>
2011-12-01 17:27:28 +0100

    Allow configuration of the socket

--- akka-zeromq/src/main/scala/akka/zeromq/ConcurrentSocketActor.scala
+++ akka-zeromq/src/main/scala/akka/zeromq/ConcurrentSocketActor.scala
@@ -7,85 +7,112 @@ import akka.actor.{Actor, ReceiveTimeout}
 import akka.dispatch.MessageDispatcher
 import org.zeromq.ZMQ.{Socket, Poller}
 import org.zeromq.{ZMQ => JZMQ}
+import java.nio.charset.Charset
 
 private[zeromq] class ConcurrentSocketActor(params: SocketParameters, dispatcher: MessageDispatcher) extends Actor {
+
   private val noBytes = Array[Byte]()
   private val socket: Socket = params.context.socket(params.socketType)
   private val poller: Poller = params.context.poller
+
   self.receiveTimeout = Some(params.pollTimeoutDuration.toMillis)
   self.dispatcher = dispatcher
-  override def receive: Receive = {
-    case Send(frames) =>
+
+  protected def receive: Receive = {
+    case Send(frames) ⇒
       sendFrames(frames)
       pollAndReceiveFrames()
-    case ZMQMessage(frames) => 
+    case ZMQMessage(frames) ⇒
       sendFrames(frames)
       pollAndReceiveFrames()
-    case Connect(endpoint) =>
+    case Connect(endpoint) ⇒
       socket.connect(endpoint)
       notifyListener(Connecting)
-    case Bind(endpoint) => 
+    case Bind(endpoint) ⇒
       socket.bind(endpoint)
-    case Subscribe(topic) => 
+    case Subscribe(topic) ⇒
       socket.subscribe(topic.toArray)
-    case Unsubscribe(topic) => 
+    case Unsubscribe(topic) ⇒
       socket.unsubscribe(topic.toArray)
-    case ReceiveTimeout =>
+    case ReceiveTimeout ⇒
       pollAndReceiveFrames()
   }
+
   override def preStart {
+    configureSocket(socket)
     poller.register(socket, Poller.POLLIN)
   }
+
   override def postStop {
     poller.unregister(socket)
     socket.close
     notifyListener(Closed)
   }
+
   private def sendFrames(frames: Seq[Frame]) {
     def sendBytes(bytes: Seq[Byte], flags: Int) {
       socket.send(bytes.toArray, flags)
     }
-    val iter = frames.iterator  
+    val iter = frames.iterator
     while (iter.hasNext) {
       val payload = iter.next.payload
       val flags = if (iter.hasNext) JZMQ.SNDMORE else 0
       sendBytes(payload, flags)
     }
   }
+
   private def pollAndReceiveFrames() {
     def pollSocket: Boolean = {
       poller.poll(params.pollTimeoutDuration.toMillis) > 0 && poller.pollin(0)
     }
     if (pollSocket) {
       receiveFrames() match {
-        case Seq() =>
-        case frames => notifyListener(params.deserializer(frames))
+        case Seq() ⇒
+        case frames ⇒ {
+          notifyListener(params.deserializer(frames))
+        }
       }
     }
   }
+
   private def receiveFrames(): Seq[Frame] = {
     @inline def receiveBytes(): Array[Byte] = socket.recv(0) match {
-      case null => noBytes
-      case bytes: Array[Byte] if bytes.length > 0 => bytes
-      case _ => noBytes
+      case null | `noBytes`   ⇒ noBytes
+      case bytes: Array[Byte] ⇒ bytes
     }
     receiveBytes() match {
-      case `noBytes` => Vector.empty
-      case someBytes =>
+      case `noBytes` ⇒ Vector.empty
+      case someBytes ⇒
         var frames = Vector(Frame(someBytes))
         while (socket.hasReceiveMore) receiveBytes() match {
-          case `noBytes` =>
-          case someBytes => frames :+= Frame(someBytes)
+          case `noBytes` ⇒
+          case someBytes ⇒ frames :+= Frame(someBytes)
         }
         frames
     }
   }
+
   private def notifyListener(message: Any) {
-    params.listener.foreach { listener =>
+    params.listener foreach { listener ⇒
       if (listener.isShutdown)
         self.stop
       else
-        listener ! message 
+        listener ! message
+    }
+  }
+
+  val Utf8 = Charset.forName("UTF-8")
+  private def configureSocket(sock: Socket) {
+    params.options foreach {
+      case lo: LingerOption   ⇒ sock setLinger lo.value
+      case HWM(value)         ⇒ sock setHWM value
+      case Affinity(value)    ⇒ sock setAffinity value
+      case Rate(value)        ⇒ sock setRate value
+      case RecoveryIVL(value) ⇒ sock setReconnectIVL value
+      case SndBuf(value)      ⇒ sock setSendBufferSize value
+      case RcvBuf(value)      ⇒ sock setReceiveBufferSize value
+      case Identity(value)    ⇒ sock setIdentity value.getBytes(Utf8)
+      case McastLoop(value)   ⇒ sock setMulticastLoop value
     }
   }
 }
--- akka-zeromq/src/main/scala/akka/zeromq/ZeroMQ.scala
+++ akka-zeromq/src/main/scala/akka/zeromq/ZeroMQ.scala
@@ -10,12 +10,43 @@ import akka.util.Duration
 import akka.util.duration._
 
 case class SocketParameters(
-  context: Context, 
-  socketType: SocketType, 
-  listener: Option[ActorRef] = None, 
+  context: Context,
+  socketType: SocketType,
+  listener: Option[ActorRef] = None,
   deserializer: Deserializer = new ZMQMessageDeserializer,
-  pollTimeoutDuration: Duration = 100 millis
-)
+  pollTimeoutDuration: Duration = 100 millis,
+  options: Seq[SocketOption] = Seq.empty)
+
+trait SocketOption {
+  type OptionType
+  def value: OptionType
+}
+
+trait IntSocketOption extends SocketOption { type OptionType = Int }
+trait LongSocketOption extends SocketOption { type OptionType = Long }
+trait StringSocketOption extends SocketOption { type OptionType = String }
+trait BoolSocketOption extends SocketOption { type OptionType = Boolean }
+trait DeserializerSocketOption extends SocketOption { type OptionType = Deserializer }
+trait ActorRefSocketOption extends SocketOption { type OptionType = ActorRef }
+
+abstract class LingerOption(val value: Long) extends LongSocketOption
+case class Linger(override val value: Long) extends LingerOption(value)
+case object NoLinger extends LingerOption(0)
+case class HWM(value: Long) extends LongSocketOption
+case class Affinity(value: Long) extends LongSocketOption
+case class Rate(value: Long) extends LongSocketOption
+case class RecoveryIVL(value: Long) extends LongSocketOption
+case class SndBuf(value: Long) extends LongSocketOption
+case class RcvBuf(value: Long) extends LongSocketOption
+case class Identity(value: String) extends StringSocketOption
+case class McastLoop(value: Boolean) extends BoolSocketOption
+
+object Timeout {
+  def apply(value: Duration): Timeout = new Timeout(value.toMillis)
+}
+case class Timeout(value: Long) extends LongSocketOption
+case class MessageDeserializer(value: Deserializer) extends DeserializerSocketOption
+case class SocketListener(value: ActorRef) extends ActorRefSocketOption
 
 object ZeroMQ {
   def newContext(numIoThreads: Int = 1) = {
