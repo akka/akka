d695ef52c0705b13a50560d472cf6b30ba8dcddc
Patrik Nordwall <patrik.nordwall@gmail.com>
2011-12-23 21:01:33 +0100

    System property for parallelExecution, use -Dakka.parallelExecution=false in sbt launch script to run build/tests sequential

--- project/AkkaBuild.scala
+++ project/AkkaBuild.scala
@@ -25,7 +25,7 @@ object AkkaBuild extends Build {
     id = "akka",
     base = file("."),
     settings = parentSettings ++ Release.settings ++ Unidoc.settings ++ Rstdoc.settings ++ Publish.versionSettings ++ Dist.settings ++ Seq(
-      parallelExecution in GlobalScope := true,
+      parallelExecution in GlobalScope := System.getProperty("akka.parallelExecution", "true").toBoolean,
       Publish.defaultPublishTo in ThisBuild <<= crossTarget / "repository",
       Unidoc.unidocExclude := Seq(samples.id, tutorials.id),
       Dist.distExclude := Seq(actorTests.id, akkaSbtPlugin.id, docs.id)
@@ -318,7 +318,7 @@ object AkkaBuild extends Build {
     unmanagedClasspath in Runtime <+= (baseDirectory in LocalProject("akka")) map { base => Attributed.blank(base / "config") },
     unmanagedClasspath in Test    <+= (baseDirectory in LocalProject("akka")) map { base => Attributed.blank(base / "config") },
 
-    parallelExecution in Test := true,
+    parallelExecution in Test := System.getProperty("akka.parallelExecution", "true").toBoolean,
 
     // for excluding tests by name (or use system property: -Dakka.test.names.exclude=TimingSpec)
     excludeTestNames := {
