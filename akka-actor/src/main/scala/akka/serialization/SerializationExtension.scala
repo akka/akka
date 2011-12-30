4387b0c27f75708de067ae85dc125daa871aab9b
Viktor Klang <viktor.klang@gmail.com>
2011-12-30 19:15:35 +0100

    Adding minor ScalaDoc to SerializationExtension

--- akka-actor/src/main/scala/akka/serialization/SerializationExtension.scala
+++ akka-actor/src/main/scala/akka/serialization/SerializationExtension.scala
@@ -5,6 +5,10 @@ package akka.serialization
 
 import akka.actor.{ ActorSystem, ExtensionId, ExtensionIdProvider, ActorSystemImpl }
 
+/**
+ * SerializationExtension is an Akka Extension to interact with the Serialization
+ * that is built into Akka
+ */
 object SerializationExtension extends ExtensionId[Serialization] with ExtensionIdProvider {
   override def get(system: ActorSystem): Serialization = super.get(system)
   override def lookup = SerializationExtension
