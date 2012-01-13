3c23b3c23360d60380edd426b84eb1ff9bbf7b2a
Piotr Gabryanczyk <piotrga@gmail.com>
2012-01-13 17:05:18 +0000

    - Made overrides really thread-safe - renamed stop to shutdown to be consistent with Akka

--- src/main/scala/akka/camel/Camel.scala
+++ src/main/scala/akka/camel/Camel.scala
@@ -4,11 +4,11 @@ import internal.component.{BlockingOrNotTypeConverter, DurationTypeConverter, Ac
 import internal._
 import org.apache.camel.impl.DefaultCamelContext
 import org.apache.camel.{ProducerTemplate, CamelContext}
-import collection.mutable.HashMap
 import akka.event.Logging.Info
 import akka.util.Duration
 import akka.util.ErrorUtils._
 import akka.actor.{ExtensionIdProvider, ActorSystemImpl, ExtensionId, Extension, Props, ActorSystem, ActorRef}
+import java.util.concurrent.ConcurrentHashMap
 
 trait Camel extends ConsumerRegistry with Extension with Activation{
   def context : CamelContext
@@ -48,21 +48,21 @@ class DefaultCamel(val actorSystem : ActorSystem) extends Camel{
    * @see akka.camel.DefaultCamel#stop()
    */
   def start = {
-    context.start
-    template.start
+    context.start()
+    template.start()
     actorSystem.eventStream.publish(Info("Camel",String.format("Started CamelContext %s for ActorSystem %s",context.getName, actorSystem.name)))
     this
   }
 
   /**
    * Stops camel and underlying camel context and template.
-   * Only the creator of Camel should stop it.
+   * Only the creator of Camel should shut it down.
    * There is no need to stop Camel instance, which you get from the CamelExtension, as its lifecycle is bound to the actor system.
    *
    * @see akka.camel.DefaultCamel#start()
    */
 
-  def stop {
+  def shutdown() {
     tryAll(
       context.stop(),
       template.stop()
@@ -72,13 +72,14 @@ class DefaultCamel(val actorSystem : ActorSystem) extends Camel{
 }
 
 object CamelExtension extends ExtensionId[Camel] with ExtensionIdProvider{
-  val overrides = synchronized(new HashMap[ActorSystem, Camel])
+  val overrides = new ConcurrentHashMap[ActorSystem, Camel]
   /**
    * If you need to start Camel context outside of extension you can use this method
    * to tell the actor system which camel instance it should use.
    * The user is responsible for stopping such a Camel instance.
+   * The override is only valid until the actor system is terminated.
    */
-  def setCamelFor(system: ActorSystem, camel: Camel) { overrides(system) = camel } //TODO: putIfAbsent maybe?
+  def setCamelFor(system: ActorSystem, camel: Camel) { overrides.put(system, camel) }
 
   /**
    * Creates new instance of Camel and makes sure it gets stopped when actor system is shutdown.
@@ -90,13 +91,14 @@ object CamelExtension extends ExtensionId[Camel] with ExtensionIdProvider{
   def createExtension(system: ActorSystemImpl) = {
 
     def useOverride(system: ActorSystemImpl) = {
+      val camel = overrides.get(system)
       system.registerOnTermination(overrides.remove(system))
-      overrides(system)
+      camel
     }
 
     def createNew(system: ActorSystemImpl) = {
       val camel = new DefaultCamel(system).start;
-      system.registerOnTermination(camel.stop)
+      system.registerOnTermination(camel.shutdown())
       camel
     }
 
