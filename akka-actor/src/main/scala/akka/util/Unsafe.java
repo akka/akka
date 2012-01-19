69196a998f6d699c54183fd11187a45f180a237c
Viktor Klang <viktor.klang@gmail.com>
2012-01-19 16:35:36 +0100

    Switching to semi-dynamic discovery of sun.misc.Unsafe entity

--- akka-actor/src/main/scala/akka/util/Unsafe.java
+++ akka-actor/src/main/scala/akka/util/Unsafe.java
@@ -11,9 +11,16 @@ public final class Unsafe {
     public final static sun.misc.Unsafe instance;
     static {
         try {
-            Field field = sun.misc.Unsafe.class.getDeclaredField("theUnsafe");
-            field.setAccessible(true);
-            instance = (sun.misc.Unsafe) field.get(null);
+            sun.misc.Unsafe found = null;
+            for(Field field : sun.misc.Unsafe.class.getDeclaredFields()) {
+              if (field.getType() == sun.misc.Unsafe.class) {
+                field.setAccessible(true);
+                found = (sun.misc.Unsafe) field.get(null);
+                break;
+              }
+            }
+            if (found == null) throw new IllegalStateException("Can't find instance of sun.misc.Unsafe");
+            else instance = found;
         } catch(Throwable t) {
           throw new ExceptionInInitializerError(t);
         }
