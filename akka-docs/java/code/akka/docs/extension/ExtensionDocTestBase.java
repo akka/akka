05d4a6ed38e91954f29a4c4af7c745b8ae1cd084
Patrik Nordwall <patrik.nordwall@gmail.com>
2012-01-20 11:30:33 +0100

    DOC: Extension sample for application specific settings. See #1702
    * Added sample and documentation for how to create extension for application specific config
    * Simplified java usage of ExtensionIdProvider, more aligned with how it is done in scala

--- akka-docs/java/code/akka/docs/extension/ExtensionDocTestBase.java
+++ akka-docs/java/code/akka/docs/extension/ExtensionDocTestBase.java
@@ -9,63 +9,63 @@ import java.util.concurrent.atomic.AtomicLong;
 
 //#imports
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class ExtensionDocTestBase {
 
-    //#extension
-    public static class CountExtensionImpl implements Extension {
-      //Since this Extension is a shared instance
-      // per ActorSystem we need to be threadsafe
-      private final AtomicLong counter = new AtomicLong(0);
+  //#extension
+  public static class CountExtensionImpl implements Extension {
+    //Since this Extension is a shared instance
+    // per ActorSystem we need to be threadsafe
+    private final AtomicLong counter = new AtomicLong(0);
 
-      //This is the operation this Extension provides
-      public long increment()  {
-          return counter.incrementAndGet();
-      }
+    //This is the operation this Extension provides
+    public long increment() {
+      return counter.incrementAndGet();
     }
-    //#extension
+  }
 
-    //#extensionid
-    static class CountExtensionId extends AbstractExtensionId<CountExtensionImpl> {
-        //This method will be called by Akka
-        // to instantiate our Extension
-        public CountExtensionImpl createExtension(ActorSystemImpl i) {
-          return new CountExtensionImpl();
-        }
-    }
+  //#extension
 
+  //#extensionid
+  public static class CountExtension extends AbstractExtensionId<CountExtensionImpl> implements ExtensionIdProvider {
     //This will be the identifier of our CountExtension
-    public final static CountExtensionId CountExtension = new CountExtensionId();
-    //#extensionid
+    public final static CountExtension instance = new CountExtension();
 
-    //#extensionid-provider
-    static class CountExtensionIdProvider implements ExtensionIdProvider {
-        public CountExtensionId lookup() {
-          return CountExtension; //The public static final
-        }
-      }
-    //#extensionid-provider
-
-    //#extension-usage-actor
-    static class MyActor extends UntypedActor {
-      public void onReceive(Object msg) {
-        CountExtension.get(getContext().system()).increment();
-      }
+    //The lookup method is required by ExtensionIdProvider,
+    // so we return ourselves here, this allows us
+    // to configure our extension to be loaded when
+    // the ActorSystem starts up
+    public CountExtension lookup() {
+      return CountExtension.instance; //The public static final
     }
-    //#extension-usage-actor
 
+    //This method will be called by Akka
+    // to instantiate our Extension
+    public CountExtensionImpl createExtension(ActorSystemImpl system) {
+      return new CountExtensionImpl();
+    }
+  }
 
-  @Test public void demonstrateHowToCreateAndUseAnAkkaExtensionInJava() {
+  //#extensionid
+
+  //#extension-usage-actor
+  public static class MyActor extends UntypedActor {
+    public void onReceive(Object msg) {
+      CountExtension.instance.get(getContext().system()).increment();
+    }
+  }
+
+  //#extension-usage-actor
+
+  @Test
+  public void demonstrateHowToCreateAndUseAnAkkaExtensionInJava() {
     final ActorSystem system = null;
     try {
       //#extension-usage
-      CountExtension.get(system).increment();
+      CountExtension.instance.get(system).increment();
       //#extension-usage
-    } catch(Exception e) {
+    } catch (Exception e) {
       //do nothing
     }
   }
---
+++ akka-docs/scala/code/akka/docs/extension/SettingsExtensionDocSpec.scala
@@ -0,0 +1,77 @@
+/**
+ * Copyright (C) 2009-2012 Typesafe Inc. <http://www.typesafe.com>
+ */
+package akka.docs.extension
+
+//#imports
+import akka.actor.Extension
+import akka.actor.ExtensionId
+import akka.actor.ExtensionIdProvider
+import akka.actor.ActorSystemImpl
+import akka.util.Duration
+import com.typesafe.config.Config
+import java.util.concurrent.TimeUnit
+
+//#imports
+
+import akka.actor.Actor
+import akka.testkit.AkkaSpec
+
+//#extension
+class SettingsImpl(config: Config) extends Extension {
+  val DbUri: String = config.getString("myapp.db.uri")
+  val CircuitBreakerTimeout: Duration = Duration(config.getMilliseconds("myapp.circuit-breaker.timeout"), TimeUnit.MILLISECONDS)
+}
+//#extension
+
+//#extensionid
+object Settings extends ExtensionId[SettingsImpl] with ExtensionIdProvider {
+
+  override def lookup = Settings
+
+  override def createExtension(system: ActorSystemImpl) = new SettingsImpl(system.settings.config)
+}
+//#extensionid
+
+object SettingsExtensionDocSpec {
+
+  val config = """
+    //#config
+    myapp {
+      db {
+        uri = "mongodb://example1.com:27017,example2.com:27017"
+      }
+      circuit-breaker {
+        timeout = 30 seconds
+      }
+    }
+    //#config
+    """
+
+  //#extension-usage-actor
+
+  class MyActor extends Actor {
+    val connection = connect(Settings(context.system).DbUri, Settings(context.system).CircuitBreakerTimeout)
+
+    //#extension-usage-actor
+    def receive = {
+      case someMessage ⇒
+    }
+
+    def connect(dbUri: String, circuitBreakerTimeout: Duration) = {
+      "dummy"
+    }
+  }
+
+}
+
+class SettingsExtensionDocSpec extends AkkaSpec(SettingsExtensionDocSpec.config) {
+
+  "demonstrate how to create application specific settings extension in Scala" in {
+    //#extension-usage
+    val dbUri = Settings(system).DbUri
+    val circuitBreakerTimeout = Settings(system).CircuitBreakerTimeout
+    //#extension-usage
+  }
+
+}
