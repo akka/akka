d677d3b8e2d37e22dac4bf393cecf3c227340826
Piotr Gabryanczyk <piotrga@gmail.com>
2012-01-24 21:08:03 +0000

    autoreformat

--- akka-camel/src/main/scala/akka/camel/ActorRouteDefinition.scala
+++ akka-camel/src/main/scala/akka/camel/ActorRouteDefinition.scala
@@ -8,7 +8,6 @@ import internal.component.ActorEndpointPath
 import akka.actor.ActorRef
 import org.apache.camel.model.ProcessorDefinition
 
-
 class ActorRouteDefinition(definition: ProcessorDefinition[_]) {
   def to(actorRef: ActorRef) = definition.to(ActorEndpointPath(actorRef).toCamelPath())
   def to(actorRef: ActorRef, consumerConfig: ConsumerConfig) = definition.to(ActorEndpointPath(actorRef).toCamelPath(consumerConfig))
