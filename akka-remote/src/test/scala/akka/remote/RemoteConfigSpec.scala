91a63484978d11b82dcf0e300639044199be619d
Viktor Klang <viktor.klang@gmail.com>
2012-01-25 16:50:06 +0100

    Fixing so that RemoteConfigSpec actually tests what is used by Akka

--- akka-remote/src/test/scala/akka/remote/RemoteConfigSpec.scala
+++ akka-remote/src/test/scala/akka/remote/RemoteConfigSpec.scala
@@ -1,50 +1,64 @@
 package akka.remote
 
 import akka.testkit.AkkaSpec
+import akka.actor.ExtendedActorSystem
+import akka.util.duration._
 
 @org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
-class RemoteConfigSpec extends AkkaSpec("") {
+class RemoteConfigSpec extends AkkaSpec(
+  """
+  akka {
+    actor {
+      provider = "akka.remote.RemoteActorRefProvider"
+    }
+  }
+  """) {
 
   "RemoteExtension" must {
     "be able to parse remote and cluster config elements" in {
+      val settings = system.asInstanceOf[ExtendedActorSystem].provider.asInstanceOf[RemoteActorRefProvider].remoteSettings
 
-      val config = system.settings.config
-      import config._
+      //SharedSettings
 
-      //akka.remote
-      getString("akka.remote.transport") must equal("akka.remote.netty.NettyRemoteSupport")
-      getString("akka.remote.secure-cookie") must equal("")
-      getBoolean("akka.remote.use-passive-connections") must equal(true)
-      getMilliseconds("akka.remote.backoff-timeout") must equal(0)
-      // getMilliseconds("akka.remote.remote-daemon-ack-timeout") must equal(30 * 1000)
+      {
+        import settings._
 
-      //akka.remote.server
-      getInt("akka.remote.server.port") must equal(2552)
-      getBytes("akka.remote.server.message-frame-size") must equal(1048576L)
+        RemoteTransport must equal("akka.remote.netty.NettyRemoteSupport")
+        BackoffTimeout must equal(0 seconds)
+        LogReceivedMessages must equal(false)
+        LogSentMessages must equal(false)
+      }
 
-      getBoolean("akka.remote.server.require-cookie") must equal(false)
-      getBoolean("akka.remote.server.untrusted-mode") must equal(false)
-      getInt("akka.remote.server.backlog") must equal(4096)
+      //ServerSettings
 
-      getBoolean("akka.remote.log-received-messages") must equal(false)
-      getBoolean("akka.remote.log-sent-messages") must equal(false)
+      {
+        import settings.serverSettings._
+        SecureCookie must be(None)
+        UsePassiveConnections must equal(true)
+        Port must equal(2552)
+        MessageFrameSize must equal(1048576L)
+        RequireCookie must equal(false)
+        UntrustedMode must equal(false)
+        Backlog must equal(4096)
+        ExecutionPoolKeepAlive must equal(1 minute)
+        ExecutionPoolSize must equal(4)
+        MaxChannelMemorySize must equal(0)
+        MaxTotalMemorySize must equal(0)
+      }
 
-      getMilliseconds("akka.remote.server.execution-pool-keepalive") must equal(60 * 1000)
+      //ClientSettings
 
-      getInt("akka.remote.server.execution-pool-size") must equal(4)
-
-      getBytes("akka.remote.server.max-channel-memory-size") must equal(0)
-      getBytes("akka.remote.server.max-total-memory-size") must equal(0)
-
-      //akka.remote.client
-      getMilliseconds("akka.remote.client.reconnect-delay") must equal(5 * 1000)
-      getMilliseconds("akka.remote.client.read-timeout") must equal(3600 * 1000)
-      getMilliseconds("akka.remote.client.reconnection-time-window") must equal(600 * 1000)
-      getMilliseconds("akka.remote.client.connection-timeout") must equal(10000)
+      {
+        import settings.clientSettings._
+        SecureCookie must be(None)
+        ReconnectDelay must equal(5 seconds)
+        ReadTimeout must equal(1 hour)
+        ReconnectionTimeWindow must equal(10 minutes)
+        ConnectionTimeout must equal(10 seconds)
+      }
 
       // TODO cluster config will go into akka-cluster/reference.conf when we enable that module
-      //akka.cluster
-      getStringList("akka.cluster.seed-nodes") must equal(new java.util.ArrayList[String])
+      settings.SeedNodes must be('empty)
     }
   }
 }
