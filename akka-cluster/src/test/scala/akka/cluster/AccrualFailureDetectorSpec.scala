4162372024e90e395d4cd92fd204fb24283053bf
Jonas Bonér <jonas@jonasboner.com>
2012-01-31 15:48:01 +0100

    Cleaned up AccrualFailureDetectorSpec after changing order of arguments in AccrualFailureDetector

--- akka-cluster/src/test/scala/akka/cluster/AccrualFailureDetectorSpec.scala
+++ akka-cluster/src/test/scala/akka/cluster/AccrualFailureDetectorSpec.scala
@@ -12,7 +12,7 @@ class AccrualFailureDetectorSpec extends AkkaSpec("""
     val conn = Address("akka", "", Some("localhost"), Some(2552))
 
     "mark node as available after a series of successful heartbeats" in {
-      val fd = new AccrualFailureDetector(system = system)
+      val fd = new AccrualFailureDetector(system)
 
       fd.heartbeat(conn)
 
@@ -27,7 +27,7 @@ class AccrualFailureDetectorSpec extends AkkaSpec("""
 
     // FIXME how should we deal with explicit removal of connection? - if triggered as failure then we have a problem in boostrap - see line 142 in AccrualFailureDetector
     "mark node as dead after explicit removal of connection" ignore {
-      val fd = new AccrualFailureDetector(system = system)
+      val fd = new AccrualFailureDetector(system)
 
       fd.heartbeat(conn)
 
@@ -45,7 +45,7 @@ class AccrualFailureDetectorSpec extends AkkaSpec("""
     }
 
     "mark node as dead if heartbeat are missed" in {
-      val fd = new AccrualFailureDetector(threshold = 3, system = system)
+      val fd = new AccrualFailureDetector(system, threshold = 3)
 
       fd.heartbeat(conn)
 
@@ -63,7 +63,7 @@ class AccrualFailureDetectorSpec extends AkkaSpec("""
     }
 
     "mark node as available if it starts heartbeat again after being marked dead due to detection of failure" in {
-      val fd = new AccrualFailureDetector(threshold = 3, system = system)
+      val fd = new AccrualFailureDetector(system, threshold = 3)
 
       fd.heartbeat(conn)
 
