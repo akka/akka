c43b024cacdc6889e338cd907b2536e7f4c2a859
Patrik Nordwall <patrik.nordwall@gmail.com>
2012-02-08 13:37:00 +0100

    Fixed the failing doc test

--- akka-docs/scala/code/akka/docs/actor/ActorDocSpec.scala
+++ akka-docs/scala/code/akka/docs/actor/ActorDocSpec.scala
@@ -37,6 +37,12 @@ case class Message(s: String)
 class FirstActor extends Actor {
   val myActor = context.actorOf(Props[MyActor], name = "myactor")
   //#context-actorOf
+  def receive = {
+    case x ⇒ sender ! x
+  }
+}
+
+class AnonymousActor extends Actor {
   //#anonymous-actor
   def receive = {
     case m: DoIt ⇒
@@ -233,27 +239,27 @@ class ActorDocSpec extends AkkaSpec(Map("akka.loglevel" -> "INFO")) {
     system.stop(myActor)
   }
 
-  "using implicit timeout" taggedAs TimingTest in {
+  "using implicit timeout" in {
     val myActor = system.actorOf(Props(new FirstActor))
     //#using-implicit-timeout
     import akka.util.duration._
     import akka.util.Timeout
     import akka.pattern.ask
-    implicit val timeout = Timeout(500 millis)
+    implicit val timeout = Timeout(5 seconds)
     val future = myActor ? "hello"
     //#using-implicit-timeout
     Await.result(future, timeout.duration) must be("hello")
 
   }
 
-  "using explicit timeout" taggedAs TimingTest in {
+  "using explicit timeout" in {
     val myActor = system.actorOf(Props(new FirstActor))
     //#using-explicit-timeout
     import akka.util.duration._
     import akka.pattern.ask
-    val future = myActor.ask("hello")(500 millis)
+    val future = myActor.ask("hello")(5 seconds)
     //#using-explicit-timeout
-    Await.result(future, 500 millis) must be("hello")
+    Await.result(future, 5 seconds) must be("hello")
   }
 
   "using receiveTimeout" in {
