8b5da5d4db7367327f205b65274a6145191a09ea
Viktor Klang <viktor.klang@gmail.com>
2012-02-27 13:56:49 +0100

    Removing playMini as to avoid the cyclic dependency, please look at the play-mini docs for examples

--- akka-docs/modules/code/Global.scala
+++
@@ -1,6 +0,0 @@
-/**
- * Copyright (C) 2009-2012 Typesafe Inc. <http://www.typesafe.com>
- */
-//#global
-object Global extends com.typesafe.play.mini.Setup(akka.docs.http.PlayMiniApplication)
-//#global
