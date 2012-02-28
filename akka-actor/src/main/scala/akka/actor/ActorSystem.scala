bbc5498d8180f3f772f915dce900065c38ddfae0
Viktor Klang <viktor.klang@gmail.com>
2012-02-28 16:24:48 +0100

    Update version for release 2.0-RC3

--- akka-actor/src/main/scala/akka/actor/ActorSystem.scala
+++ akka-actor/src/main/scala/akka/actor/ActorSystem.scala
@@ -23,7 +23,7 @@ import collection.immutable.Stack
 
 object ActorSystem {
 
-  val Version = "2.0-SNAPSHOT"
+  val Version = "2.0-RC3"
 
   val EnvHome = System.getenv("AKKA_HOME") match {
     case null | "" | "." ⇒ None
