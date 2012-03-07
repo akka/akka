364cb63415e6241c1ff12726df62c6ffc1f93de1
Florian Hopf <hopf@synyx.de>
2012-03-07 13:16:55 +0100

    added test that validates that String message is ignored

--- akka-tutorials/akka-tutorial-first/src/test/java/akka/tutorial/first/java/WorkerTest.java
+++ akka-tutorials/akka-tutorial-first/src/test/java/akka/tutorial/first/java/WorkerTest.java
@@ -4,6 +4,8 @@ import akka.actor.ActorSystem;
 import akka.testkit.TestActor;
 import akka.testkit.TestActorRef;
 import akka.testkit.TestProbe;
+import akka.util.Duration;
+import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,13 +15,14 @@ import static org.junit.Assert.assertEquals;
 
 /**
  * Tests the Worker actor using the akka-testkit.
+ *
  * @author Florian Hopf
  */
 public class WorkerTest {
 
     private TestActorRef<Pi.Worker> actorRef;
     private ActorSystem actorSystem;
-    
+
     @Before
     public void initActor() {
         actorSystem = ActorSystem.apply();
@@ -29,34 +32,41 @@ public class WorkerTest {
             public Pi.Worker apply() {
                 return new Pi.Worker();
             }
-            
         }, actorSystem);
     }
-    
+
+    @Test
+    public void doNothingForString() {
+        TestProbe testProbe = TestProbe.apply(actorSystem);
+        actorRef.tell("Hello", testProbe.ref());
+
+        testProbe.expectNoMsg(Duration.apply(100, TimeUnit.MILLISECONDS));
+    }
+
     @Test
     public void calculatePiFor0() {
         TestProbe testProbe = TestProbe.apply(actorSystem);
-        Pi.Work work = new Pi.Work(0, 0);        
+        Pi.Work work = new Pi.Work(0, 0);
         actorRef.tell(work, testProbe.ref());
 
-        testProbe.expectMsgClass(Pi.Result.class);     
+        testProbe.expectMsgClass(Pi.Result.class);
         TestActor.Message message = testProbe.lastMessage();
         Pi.Result resultMsg = (Pi.Result) message.msg();
         assertEquals(0.0, resultMsg.getValue(), 0.0000000001);
     }
-    
+
     @Test
     public void calculatePiFor1() {
         TestProbe testProbe = TestProbe.apply(actorSystem);
-        Pi.Work work = new Pi.Work(1, 1);        
+        Pi.Work work = new Pi.Work(1, 1);
         actorRef.tell(work, testProbe.ref());
 
-        testProbe.expectMsgClass(Pi.Result.class);     
+        testProbe.expectMsgClass(Pi.Result.class);
         TestActor.Message message = testProbe.lastMessage();
         Pi.Result resultMsg = (Pi.Result) message.msg();
         assertEquals(-1.3333333333333333, resultMsg.getValue(), 0.0000000001);
     }
-    
+
     @After
     public void shutdownActorSystem() {
         actorSystem.shutdown();
