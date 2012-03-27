32f8f94db2ae22352a7b00298efc98349d5eb9fb
phaller <hallerp@gmail.com>
2012-03-27 20:32:24 +0200

    Support nested receives in thread actors. Add self to ActorDSL.

--- akka-actor-tests/src/test/scala/akka/actor/ActorDSLSpec.scala
+++ akka-actor-tests/src/test/scala/akka/actor/ActorDSLSpec.scala
@@ -6,15 +6,19 @@ package akka.actor
 import akka.testkit._
 import akka.testkit.DefaultTimeout
 import akka.testkit.TestEvent._
+
 import akka.dispatch.{ Await, MessageQueueAppendFailedException, BoundedDequeBasedMailbox }
 import akka.pattern.ask
 import akka.util.duration._
+
 import akka.actor.ActorSystem.Settings
+import akka.actor.ActorDSL._
+
 import com.typesafe.config.{ Config, ConfigFactory }
 import org.scalatest.BeforeAndAfterEach
-import akka.actor.AkkaDSL._
+import org.scalatest.junit.JUnitRunner
 
-@org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
+@org.junit.runner.RunWith(classOf[JUnitRunner])
 class ActorDSLSpec extends AkkaSpec with DefaultTimeout with BeforeAndAfterEach {
   import ActorWithBoundedStashSpec._
 
@@ -26,11 +30,10 @@ class ActorDSLSpec extends AkkaSpec with DefaultTimeout with BeforeAndAfterEach 
 
   def myProps(creator: ⇒ Actor): Props = Props(creator).withDispatcher("my-dispatcher")
 
-  "An Actor created with the DSL " must {
-
-    "have context, respond and die" in {
-      var in1 = false
-      var in2 = false
+  "An actor created with the DSL" must {
+    "have a working context" in {
+      @volatile var in1 = false
+      @volatile var in2 = false
 
       val actor = actorOf { context ⇒
         {
@@ -43,7 +46,6 @@ class ActorDSLSpec extends AkkaSpec with DefaultTimeout with BeforeAndAfterEach 
                 m must be("hello")
             }
         }
-
       }
 
       actor ! "hello"
@@ -57,10 +59,9 @@ class ActorDSLSpec extends AkkaSpec with DefaultTimeout with BeforeAndAfterEach 
     }
   }
 
-  "An Actor created with the DSL " must {
-
-    "be able to react from a regular thread " in {
-      val tA = threadSender
+  "An actor created with the DSL" must {
+    "be able to exchange messages with a regular thread " in {
+      val tA = self
       val actor = actorOf { context ⇒
         {
           case m ⇒
@@ -77,7 +78,82 @@ class ActorDSLSpec extends AkkaSpec with DefaultTimeout with BeforeAndAfterEach 
           m must be("hello")
       }
     }
-
   }
+
+  "A thread actor" must {
+    "support nested receives" in {
+      val tA = self
+      val actor = actorOf { context ⇒
+        {
+          case m ⇒
+            context.sender must be(tA)
+            context.sender ! m
+        }
+      }
+
+      actor ! "hello"
+      actor ! "world"
+
+      receive {
+        case m @ "hello" ⇒
+          m must be("hello")
+          receive {
+            case next @ "world" ⇒
+              next must be("world")
+          }
+      }
+    }
+  }
+
+  "A thread actor" must {
+    "only receive user messages" in {
+      val tA = self
+      val actor = actorOf { context ⇒
+        {
+          case m ⇒
+            context.sender must be(tA)
+            for (_ ← 0 until 10000)
+              context.sender ! m
+        }
+      }
+
+      actor ! "hello"
+
+      var i = 0
+      while (i < 10000) {
+        i += 1
+        receive {
+          case m ⇒
+            m must be("hello")
+        }
+      }
+    }
+  }
+
+  /*
+  "A thread actor" must {
+    "support the ask pattern" in {
+      val tA = self
+      val actor = actorOf { context ⇒
+        {
+          case m ⇒
+            // send message and wait for response
+            println("context.sender: " + context.sender)
+            val f = context.sender ? "hello"
+            val res = Await.result(f, 1 seconds)
+            res must be("response")
+        }
+      }
+
+      actor.tell("whatever", tA)
+
+      receive {
+        case resp ⇒
+          println("tA received " + resp)
+          actor ! "response"
+      }
+    }
+  }
+  */
 
 }
---
+++ akka-actor/src/main/scala/akka/actor/ActorDSL.scala
@@ -0,0 +1,158 @@
+package akka.actor
+
+import akka.dispatch.SystemMessage
+import collection.mutable.Queue
+
+/**
+ * The ActorDSL trait provides factory methods which allow creating actors
+ * using a very lightweight syntax which simplifies programming with actors
+ * in the interactive interpreter, for example.
+ *
+ * Moreover, the ActorDSL trait provides an actor interface for the current
+ * thread. By importing the `self` member, the current thread is treated
+ * as an implicit sender when sending messages outside of a regular actor.
+ * Using the `receive` method, the current thread can receive messages
+ * sent to its actor reference.
+ *
+ * @tparam A  The type of the actor trait
+ * @tparam AR The type of the actor reference returned by factory methods
+ * @tparam CT The type of the internal actor context
+ */
+trait ActorDSL[A, AR, CT] {
+
+  /**
+   * Factory method for creating and starting an actor.
+   *
+   * @param  creator  The code block that creates the new actor
+   * @return          The newly created actor. Note that it is automatically started.
+   */
+  def actorOf[T <: A](creator: ⇒ T): AR
+
+  /**
+   * Factory method for creating and starting an actor that loops trying to receive
+   * messages using a provided partial function. Note that the actor's behavior can be
+   * changed after the first message has been received.
+   *
+   * The parameter (of type `CT`) of the argument function (`body`) is used to provide
+   * access to the internal functionality of the actor (e.g., the underlying actor
+   * instance or the actor's context).
+   *
+   * @param  body  The function which returns the top-level message handler
+   * @return       The newly created actor. Note that it is automatically started.
+   */
+  def actorOf(body: CT ⇒ PartialFunction[Any, Unit]): AR
+
+  /**
+   * Receives a message from the mailbox of the current thread. Calling this method
+   * will block the current thread, until a matching message is received.
+   *
+   * @example {{{
+   * receive {
+   *   case "exit" => println("exiting")
+   *   case 42 => println("got the answer")
+   *   case x: Int => println("got an answer")
+   * }
+   * }}}
+   *
+   * @param  pf A partial function specifying patterns and actions
+   * @return    The result of processing the received message
+   */
+  def receive[T](f: PartialFunction[Any, T]): T
+
+  /**
+   * The actor reference of the current thread.
+   */
+  def self: AR
+
+}
+
+object ActorDSL extends ActorDSL[Actor, ActorRef, ActorContext] {
+
+  private val system = ActorSystem("ActorDSLSystem")
+
+  /* The ActorRef of the current thread */
+  private val tl = new ThreadLocal[ActorRef]
+  /* The message queue of the current thread */
+  private val tlQueue = new ThreadLocal[Queue[Any]]
+
+  def actorOf(body: ActorContext ⇒ PartialFunction[Any, Unit]): ActorRef =
+    system actorOf Props(new Actor {
+      def receive: Receive = body(context)
+    })
+
+  def actorOf[T <: Actor](creator: ⇒ T): ActorRef =
+    if (creator.isInstanceOf[Stash])
+      system actorOf Props(creator = () ⇒ creator, dispatcher = "akka.actor.default-stash-dispatcher")
+    else
+      system actorOf Props(creator)
+
+  def receive[T](pf: PartialFunction[Any, T]): T = {
+    val queue = tlQueue.get
+    var done = false // guarded by queue
+    var msgOpt: Option[Any] = None // guarded by queue
+
+    while (!done) {
+      queue.synchronized {
+        // find (and remove) first message that matches any of the patterns in pf
+        msgOpt = queue.dequeueFirst(m ⇒ pf.isDefinedAt(m))
+
+        if (msgOpt.isEmpty) queue.wait()
+        else done = true
+      }
+    }
+
+    // apply partial function to message
+    pf(msgOpt.get)
+  }
+
+  implicit def self: ActorRef = {
+    val s = tl.get
+    if (s eq null) {
+      // initialize thread-local message queue
+      val queue = Queue[Any]()
+      tlQueue set queue
+
+      // initialize thread-local ActorRef
+      val ref = new ThreadActorRef(queue)
+      tl set ref
+
+      ref
+    } else
+      s
+  }
+
+  private class ThreadActorRef(queue: Queue[Any]) extends InternalActorRef with LocalRef {
+    /* Since we are not providing an ActorContext for the current thread, the following
+       methods should not be invoked internally.
+     */
+    def resume(): Unit = throw new RuntimeException
+    def suspend(): Unit = throw new RuntimeException
+    def restart(cause: Throwable): Unit = throw new RuntimeException
+    def stop(): Unit = throw new RuntimeException
+    def sendSystemMessage(message: SystemMessage): Unit = throw new RuntimeException
+
+    def provider: ActorRefProvider = null //ActorSystem("ThreadActorSystem").asInstanceOf[ActorSystemImpl].provider
+
+    def getParent: InternalActorRef = null
+
+    def getChild(name: Iterator[String]): InternalActorRef = null
+
+    def !(message: Any)(implicit sender: ActorRef = null): Unit = {
+      /* put message into queue and notify receiving thread
+         
+         note that we are ignoring the sender, because the current thread
+         does not have a context anyway
+       */
+      queue.synchronized {
+        queue += message
+        // notify the thread that's waiting
+        queue.notifyAll()
+      }
+    }
+
+    def isTerminated: Boolean = false
+
+    def path: ActorPath = null
+  }
+
+}
