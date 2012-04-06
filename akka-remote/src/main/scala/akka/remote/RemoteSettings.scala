2f16b0dc905ecc990829450e32d12e574b61d31f
Scott Clasen <scott@heroku.com>
2012-04-06 12:08:39 -0700

    add nat-friendly remote support option

--- akka-remote/src/main/scala/akka/remote/RemoteSettings.scala
+++ akka-remote/src/main/scala/akka/remote/RemoteSettings.scala
@@ -6,17 +6,22 @@ package akka.remote
 import com.typesafe.config.Config
 import akka.util.Duration
 import java.util.concurrent.TimeUnit.MILLISECONDS
-import java.net.InetAddress
-import akka.config.ConfigurationException
-import scala.collection.JavaConverters._
-import akka.actor.Address
-import akka.actor.AddressFromURIString
+import collection.JavaConverters._
 
 class RemoteSettings(val config: Config, val systemName: String) {
+
   import config._
+
   val RemoteTransport = getString("akka.remote.transport")
   val LogReceive = getBoolean("akka.remote.log-received-messages")
   val LogSend = getBoolean("akka.remote.log-sent-messages")
   val RemoteSystemDaemonAckTimeout = Duration(getMilliseconds("akka.remote.remote-daemon-ack-timeout"), MILLISECONDS)
   val UntrustedMode = getBoolean("akka.remote.untrusted-mode")
+  val NATFriendly = getBoolean("akka.remote.nat-friendly")
+  val NATWhitelist = {
+    if (hasPath("akka.remote.nat-whitelist"))
+      getStringList("akka.remote.nat-whitelist").asScala.toList
+    else
+      List.empty[String]
+  }
 }
