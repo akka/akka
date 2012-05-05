c68df0635f2213d397649533db24a0a01ffe17c5
Roland <rk@rkuhn.info>
2012-05-05 15:16:21 +0200

    add previously forgotten TestConductorTransport

---
+++ akka-remote/src/main/scala/akka/remote/testconductor/TestConductorTransport.scala
@@ -0,0 +1,21 @@
+/**
+ *  Copyright (C) 2009-2012 Typesafe Inc. <http://www.typesafe.com>
+ */
+package akka.remote.testconductor
+
+import akka.remote.netty.NettyRemoteTransport
+import akka.remote.RemoteSettings
+import akka.actor.ActorSystemImpl
+import akka.remote.RemoteActorRefProvider
+import org.jboss.netty.channel.ChannelHandler
+import org.jboss.netty.channel.ChannelPipelineFactory
+
+class TestConductorTransport(_remoteSettings: RemoteSettings, _system: ActorSystemImpl, _provider: RemoteActorRefProvider)
+  extends NettyRemoteTransport(_remoteSettings, _system, _provider) {
+
+  override def mkPipeline(endpoint: ⇒ ChannelHandler, withTimeout: Boolean): ChannelPipelineFactory =
+    new ChannelPipelineFactory {
+      def getPipeline = PipelineFactory(new NetworkFailureInjector(system) +: PipelineFactory.defaultStack(withTimeout) :+ endpoint)
+    }
+
+}
