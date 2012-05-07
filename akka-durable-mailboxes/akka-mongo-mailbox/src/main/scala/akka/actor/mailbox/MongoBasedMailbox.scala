1d77ba311975849dd97a2eb6429978d7af2f2628
Brian Scully <scullduggery@gmail.com>
2012-05-06 22:39:21 -0400

    Add circuit breaker facility to DurableMailboxQueue base class for use by subclasses.  Circuit breaker implementation as described by Michael T. Nygard in Release It!.  Fixes #1734
      * Create finite state machine for three states: Closed, Open, Half-Open
        * Closed state allows calls through, and on sequential failures exceeding the max# set - transitions to Open state.  Intervening successes cause the failure count to reset to 0
         * Open state throws a CircuitOpenException on every call until the reset timeout is reached which causes a transition to Half-Open state
         * Half-Open state will allow the next single call through, if it succeeds - transition to Closed state, if it fails - transition back to Open state, starting the reset timer again
      * Allow configuration for the call and reset timeouts, as well as the maximum number of sequential failures before opening
      * Failures can be either thrown exceptions or call timeouts (internally throws an exception)
      * Uses akka scheduler for timer events5
    
    Fixed async on mongo; Upgrade redis-client to 2.4.2 to avoid OOM on server disconnect

--- akka-durable-mailboxes/akka-mongo-mailbox/src/main/scala/akka/actor/mailbox/MongoBasedMailbox.scala
+++ akka-durable-mailboxes/akka-mongo-mailbox/src/main/scala/akka/actor/mailbox/MongoBasedMailbox.scala
@@ -10,13 +10,12 @@ import org.bson.collection._
 import akka.actor.ActorContext
 import akka.event.Logging
 import akka.actor.ActorRef
-import akka.dispatch.{ Await, Promise, Envelope }
 import java.util.concurrent.TimeoutException
-import akka.dispatch.MailboxType
 import com.typesafe.config.Config
 import akka.config.ConfigurationException
-import akka.dispatch.MessageQueue
 import akka.actor.ActorSystem
+import akka.dispatch._
+import scala.{ Some, None }
 
 class MongoBasedMailboxException(message: String) extends AkkaException(message)
 
@@ -57,13 +56,15 @@ class MongoBasedMessageQueue(_owner: ActorContext, val settings: MongoBasedMailb
     val durableMessage = MongoDurableMessage(ownerPathString, envelope.message, envelope.sender)
     // todo - do we need to filter the actor name at all for safe collection naming?
     val result = Promise[Boolean]()(dispatcher)
-    mongo.insert(durableMessage, false)(RequestFutures.write { wr: Either[Throwable, (Option[AnyRef], WriteResult)] ⇒
-      wr match {
-        case Right((oid, wr)) ⇒ asyncHandle.onAsyncSuccess(); result.success(true)
-        case Left(t)          ⇒ asyncHandle.onAsyncFailure(); result.failure(t)
-      }
-    })
-    Await.ready(result, settings.WriteTimeout)
+    asyncHandle.withCircuitBreaker {
+      mongo.insert(durableMessage, false)(RequestFutures.write { wr: Either[Throwable, (Option[AnyRef], WriteResult)] ⇒
+        wr match {
+          case Right((oid, wr)) ⇒ asyncHandle.onAsyncSuccess(); result.success(true)
+          case Left(t)          ⇒ asyncHandle.onAsyncFailure(); result.failure(t)
+        }
+      })
+      Await.ready(result, settings.WriteTimeout)
+    }
   }
 
   def dequeue(): Envelope = {
@@ -76,15 +77,17 @@ class MongoBasedMessageQueue(_owner: ActorContext, val settings: MongoBasedMailb
      */
     val asyncHandle = newAsyncCircuitBreakerHandle()
     val envelopePromise = Promise[Envelope]()(dispatcher)
-    mongo.findAndRemove(Document.empty) { doc: Option[MongoDurableMessage] ⇒
-      doc match {
-        case Some(msg) ⇒
-          envelopePromise.success(msg.envelope(system))
-          ()
-        case None ⇒
-          log.info("No matching document found. Not an error, just an empty queue.")
-          envelopePromise.success(null)
-          ()
+    asyncHandle.withCircuitBreaker {
+      mongo.findAndRemove(Document.empty) { doc: Option[MongoDurableMessage] ⇒
+        doc match {
+          case Some(msg) ⇒
+            envelopePromise.success(msg.envelope(system))
+            ()
+          case None ⇒
+            log.info("No matching document found. Not an error, just an empty queue.")
+            envelopePromise.success(null)
+            ()
+        }
       }
     }
     try {
@@ -97,7 +100,7 @@ class MongoBasedMessageQueue(_owner: ActorContext, val settings: MongoBasedMailb
   def numberOfMessages: Int = {
     val asyncHandle = newAsyncCircuitBreakerHandle()
     val count = Promise[Int]()(dispatcher)
-    mongo.count()(count.success)
+    asyncHandle.withCircuitBreaker(mongo.count()(count.success))
     try {
       val result = Await.result(count, settings.ReadTimeout).asInstanceOf[Int]
       asyncHandle.onAsyncSuccess()
