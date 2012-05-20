4e2c4955b34095781729b733cf3db296eb41643e
Patrik Nordwall <patrik.nordwall@gmail.com>
2012-05-20 20:16:06 +0200

    Changed package to akka.jmx, for OSGi purposes. See #2079

--- akka-kernel/src/main/java/akka/jmx/Client.java
+++ akka-kernel/src/main/java/akka/jmx/Client.java
@@ -23,7 +23,7 @@
  * along with Heritrix; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-package org.archive.jmx;
+package akka.jmx;
 
 import java.io.IOException;
 import java.io.PrintWriter;
