b9bf0fec80c02149b935be1f4f90ee09f06d19fd
Viktor Klang <viktor.klang@gmail.com>
2012-05-25 17:15:35 +0200

    #2130 - Fixing unforunate naming in DiningHakkers

--- akka-samples/akka-sample-fsm/src/main/scala/DiningHakkersOnBecome.scala
+++ akka-samples/akka-sample-fsm/src/main/scala/DiningHakkersOnBecome.scala
@@ -131,17 +131,15 @@ class Hakker(name: String, left: ActorRef, right: ActorRef) extends Actor {
 object DiningHakkers {
   val system = ActorSystem()
 
-  def main(args: Array[String]): Unit = {
-    run
-  }
+  def main(args: Array[String]): Unit = run
 
   def run {
     //Create 5 chopsticks
-    val chopsticks = for (i ← 1 to 5) yield system.actorOf(Props[Chopstick], "Chopstick " + i)
+    val chopsticks = for (i ← 1 to 5) yield system.actorOf(Props[Chopstick], "Chopstick" + i)
 
     //Create 5 awesome hakkers and assign them their left and right chopstick
     val hakkers = for {
-      (name, i) ← List("Ghosh", "Bonér", "Klang", "Krasser", "Manie").zipWithIndex
+      (name, i) ← List("Ghosh", "Boner", "Klang", "Krasser", "Manie").zipWithIndex
     } yield system.actorOf(Props(new Hakker(name, chopsticks(i), chopsticks((i + 1) % 5))))
 
     //Signal all hakkers that they should start thinking, and watch the show
