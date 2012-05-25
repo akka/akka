0ac7f967dd6d6e70ee713127abbc349e4f66586a
Patrik Nordwall <patrik.nordwall@gmail.com>
2012-05-25 08:32:42 +0200

    Change node naming. See 2111

--- akka-cluster/src/multi-jvm/scala/akka/cluster/JoinTwoClustersSpec.scala
+++ akka-cluster/src/multi-jvm/scala/akka/cluster/JoinTwoClustersSpec.scala
@@ -40,20 +40,20 @@ abstract class JoinTwoClustersSpec extends MultiNodeSpec(JoinTwoClustersMultiJvm
 
   override def initialParticipants = 6
 
-  def node(): Cluster = Cluster(system)
+  def cluster: Cluster = Cluster(system)
 
   after {
     testConductor.enter("after")
   }
 
-  val a1Address = testConductor.getAddressFor(a1).await
-  val b1Address = testConductor.getAddressFor(b1).await
-  val c1Address = testConductor.getAddressFor(c1).await
+  val a1Address = node(a1).address
+  val b1Address = node(b1).address
+  val c1Address = node(c1).address
 
   def awaitUpConvergence(numberOfMembers: Int): Unit = {
-    awaitCond(node().latestGossip.members.size == numberOfMembers)
-    awaitCond(node().latestGossip.members.forall(_.status == MemberStatus.Up))
-    awaitCond(node().convergence.isDefined)
+    awaitCond(cluster.latestGossip.members.size == numberOfMembers)
+    awaitCond(cluster.latestGossip.members.forall(_.status == MemberStatus.Up))
+    awaitCond(cluster.convergence.isDefined)
   }
 
   "Three different clusters (A, B and C)" must {
@@ -61,40 +61,40 @@ abstract class JoinTwoClustersSpec extends MultiNodeSpec(JoinTwoClustersMultiJvm
     "be able to 'elect' a single leader after joining (A -> B)" in {
 
       runOn(a1, a2) {
-        node().join(a1Address)
+        cluster.join(a1Address)
       }
       runOn(b1, b2) {
-        node().join(b1Address)
+        cluster.join(b1Address)
       }
       runOn(c1, c2) {
-        node().join(c1Address)
+        cluster.join(c1Address)
       }
 
       awaitUpConvergence(numberOfMembers = 2)
 
-      node().isLeader must be(ifNode(a1, b1, c1)(true)(false))
+      cluster.isLeader must be(ifNode(a1, b1, c1)(true)(false))
 
       runOn(b2) {
-        node().join(a1Address)
+        cluster.join(a1Address)
       }
 
       runOn(a1, a2, b1, b2) {
         awaitUpConvergence(numberOfMembers = 4)
       }
 
-      node().isLeader must be(ifNode(a1, c1)(true)(false))
+      cluster.isLeader must be(ifNode(a1, c1)(true)(false))
 
     }
 
     "be able to 'elect' a single leader after joining (C -> A + B)" in {
 
       runOn(b2) {
-        node().join(c1Address)
+        cluster.join(c1Address)
       }
 
       awaitUpConvergence(numberOfMembers = 6)
 
-      node().isLeader must be(ifNode(a1)(true)(false))
+      cluster.isLeader must be(ifNode(a1)(true)(false))
     }
   }
 
