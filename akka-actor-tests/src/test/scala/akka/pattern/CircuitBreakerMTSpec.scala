6a415f0e9bf915a366e36ab0bc6d4cb43cb3f15e
Brian Scully <scullduggery@gmail.com>
2012-06-01 08:24:47 -0400

    * Add Circuit Breaker to akka.pattern for general use. Circuit breaker implementation as described by Michael T. Nygard in Release It!. Fixes #1734
      * Uses finite state machine for three states: Closed, Open, Half-Open
        * Closed state allows calls through, and on sequential failures exceeding the max# set - transitions to Open state. Intervening successes cause the failure count to reset to 0
        * Open state throws a CircuitOpenException on every call until the reset timeout is reached which causes a transition to Half-Open state
        * Half-Open state will allow the next single call through, if it succeeds - transition to Closed state, if it fails - transition back to Open state, starting the reset timer again
      * Allow configuration for the call and reset timeouts, as well as the maximum number of sequential failures before opening
      * Supports async or synchronous call protection
      * Callbacks are supported for state entry into Closed, Open, Half-Open.  These are run in the supplied execution context
      * Both thrown exceptions and calls exceeding max call time are considered failures
      * Uses akka scheduler for timer events
      * Integrated into File-Based durable mailbox
      * Sample documented for other durable mailboxes

---
+++ akka-actor-tests/src/test/scala/akka/pattern/CircuitBreakerMTSpec.scala
@@ -0,0 +1,121 @@
+/**
+ * Copyright (C) 2009-2012 Typesafe Inc. <http://www.typesafe.com>
+ */
+package akka.pattern
+
+import akka.testkit._
+import akka.util.duration._
+import org.scalatest.BeforeAndAfter
+import akka.dispatch.{Promise, Await, Future}
+
+class CircuitBreakerMTSpec extends AkkaSpec with BeforeAndAfter {
+
+  @volatile
+  var breakers: BreakerState = null
+
+  class BreakerState {
+
+    val halfOpenLatch = new TestLatch(1)
+
+    val breaker = new CircuitBreaker(system.scheduler,5,100.millis.dilated,500.millis.dilated)
+        .onHalfOpen(halfOpenLatch.countDown())
+
+  }
+
+  before {
+    breakers = new BreakerState()
+  }
+
+  def unreliableCall(param: String) = {
+    param match {
+      case "fail" => throw new RuntimeException("FAIL")
+      case _ => param
+    }
+  }
+
+  def openBreaker: Unit = {
+    for (i <- 1 to 5)
+      Await.result(breakers.breaker.withCircuitBreaker(Future(unreliableCall("fail"))) recoverWith {
+        case _ => Promise.successful("OK")
+      }, 1.second.dilated)
+  }
+
+  "A circuit breaker being called by many threads" must {
+    "allow many calls while in closed state with no errors" in {
+
+      val futures = for (i <- 1 to 100) yield breakers.breaker.withCircuitBreaker(Future {Thread.sleep(10); unreliableCall("succeed")})
+
+      val futureList = Future.sequence(futures)
+
+      val result = Await.result(futureList, 1.second.dilated)
+
+      result.size must be (100)
+      result.distinct.size must be (1)
+      result.distinct must contain ("succeed")
+
+    }
+
+    "transition to open state upon reaching failure limit and fail-fast" in {
+
+      openBreaker
+
+      val futures = for (i <- 1 to 100) yield breakers.breaker.withCircuitBreaker(Future {
+          Thread.sleep(10); unreliableCall("success")
+        }) recoverWith {
+          case _: CircuitBreakerOpenException => Promise.successful("CBO")
+        }
+
+      val futureList = Future.sequence(futures)
+
+      val result = Await.result(futureList, 1.second.dilated)
+
+      result.size must be (100)
+      result.distinct.size must be (1)
+      result.distinct must contain ("CBO")
+    }
+
+    "allow a single call through in half-open state" in {
+      openBreaker
+
+      Await.ready(breakers.halfOpenLatch, 2.seconds.dilated)
+
+      val futures = for (i <- 1 to 100) yield breakers.breaker.withCircuitBreaker(Future {
+          Thread.sleep(10); unreliableCall("succeed")
+        }) recoverWith {
+          case _: CircuitBreakerOpenException => Promise.successful("CBO")
+        }
+
+      val futureList = Future.sequence(futures)
+
+      val result = Await.result(futureList, 1.second.dilated)
+
+      result.size must be (100)
+      result.distinct.size must be (2)
+      result.distinct must contain ("succeed")
+      result.distinct must contain ("CBO")
+    }
+
+    "recover and reset the breaker after the reset timeout" in {
+      openBreaker
+
+      Await.ready(breakers.halfOpenLatch, 2.seconds.dilated)
+
+      Await.ready(breakers.breaker.withCircuitBreaker(Future(unreliableCall("succeed"))), 1.second.dilated)
+
+      val futures = for (i <- 1 to 100) yield breakers.breaker.withCircuitBreaker(Future {
+          Thread.sleep(10); unreliableCall("succeed")
+        }) recoverWith {
+          case _: CircuitBreakerOpenException => Promise.successful("CBO")
+        }
+
+      val futureList = Future.sequence(futures)
+
+      val result = Await.result(futureList, 1.second.dilated)
+
+      result.size must be (100)
+      result.distinct.size must be (1)
+      result.distinct must contain ("succeed")
+    }
+  }
+
+}
