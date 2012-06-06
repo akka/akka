1c5d0bdf42e489dc60d9f28432ef5038defec38c
Viktor Klang <viktor.klang@gmail.com>
2012-06-06 14:04:02 +0200

    Adding a FIXME

--- akka-remote/src/main/scala/akka/remote/netty/Client.scala
+++ akka-remote/src/main/scala/akka/remote/netty/Client.scala
@@ -65,6 +65,7 @@ private[akka] abstract class RemoteClient private[akka] (val netty: NettyRemoteT
         new ChannelFutureListener {
           def operationComplete(future: ChannelFuture) {
             if (future.isCancelled || !future.isSuccess) {
+              //FIXME Should we just _not_ notifyListeners here and just assume that the other error reporting is sufficient?
               netty.notifyListeners(RemoteClientError(future.getCause, netty, remoteAddress))
               val (message, sender, recipient) = request
               netty.system.deadLetters ! DeadLetter(message, sender.getOrElse(netty.system.deadLetters), recipient)
