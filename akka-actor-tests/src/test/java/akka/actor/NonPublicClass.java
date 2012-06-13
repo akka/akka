1eb6681b3c9c23d60dce31c17acaf58cafd09643
Viktor Klang <viktor.klang@gmail.com>
2012-06-13 15:11:19 +0200

    Removing horrid IDEA autogenerated section and adding Typesafe copyright to NonPublicClass.java

--- akka-actor-tests/src/test/java/akka/actor/NonPublicClass.java
+++ akka-actor-tests/src/test/java/akka/actor/NonPublicClass.java
@@ -1,14 +1,9 @@
+/**
+ *  Copyright (C) 2009-2012 Typesafe Inc. <http://www.typesafe.com>
+ */
+
 package akka.actor;
 
-import com.sun.xml.internal.ws.api.PropertySet;
-
-/**
- * Created by IntelliJ IDEA.
- * User: viktorklang
- * Date: 6/13/12
- * Time: 12:12 PM
- * To change this template use File | Settings | File Templates.
- */
 public class NonPublicClass {
     public static Props createProps() {
         return new Props(MyNonPublicActorClass.class);
