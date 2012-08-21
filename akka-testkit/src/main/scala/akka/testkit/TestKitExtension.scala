8211fd6e8f38b1a9724045ea5eb3c8971c913f63
Roland <rk@rkuhn.info>
2012-08-21 09:22:09 +0200

    change settings to FiniteDuration where applicable

--- akka-testkit/src/main/scala/akka/testkit/TestKitExtension.scala
+++ akka-testkit/src/main/scala/akka/testkit/TestKitExtension.scala
@@ -8,6 +8,7 @@ import scala.concurrent.util.Duration
 import akka.util.Timeout
 import java.util.concurrent.TimeUnit.MILLISECONDS
 import akka.actor.{ ExtensionId, ActorSystem, Extension, ExtendedActorSystem }
+import scala.concurrent.util.FiniteDuration
 
 object TestKitExtension extends ExtensionId[TestKitSettings] {
   override def get(system: ActorSystem): TestKitSettings = super.get(system)
@@ -19,7 +20,7 @@ class TestKitSettings(val config: Config) extends Extension {
   import config._
 
   val TestTimeFactor = getDouble("akka.test.timefactor")
-  val SingleExpectDefaultTimeout = Duration(getMilliseconds("akka.test.single-expect-default"), MILLISECONDS)
-  val TestEventFilterLeeway = Duration(getMilliseconds("akka.test.filter-leeway"), MILLISECONDS)
-  val DefaultTimeout = Timeout(Duration(getMilliseconds("akka.test.default-timeout"), MILLISECONDS))
+  val SingleExpectDefaultTimeout: FiniteDuration = Duration(getMilliseconds("akka.test.single-expect-default"), MILLISECONDS)
+  val TestEventFilterLeeway: FiniteDuration = Duration(getMilliseconds("akka.test.filter-leeway"), MILLISECONDS)
+  val DefaultTimeout: Timeout = Timeout(Duration(getMilliseconds("akka.test.default-timeout"), MILLISECONDS))
 }
