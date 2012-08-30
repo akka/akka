87352f0754e20105ef45973065991a324515f16d
Endre Sándor Varga <vendre@hit.bme.hu>
2012-08-30 13:05:33 +0200

    Settings now use absolute path

--- akka-remote/src/main/scala/akka/remote/actmote/ActorManagedRemotingSettings.scala
+++ akka-remote/src/main/scala/akka/remote/actmote/ActorManagedRemotingSettings.scala
@@ -6,11 +6,11 @@ import concurrent.util.Duration
 private[akka] class ActorManagedRemotingSettings(config: Config) {
 
   import config._
-  val Connector: String = getString("connector")
-  val UsePassiveConnections: Boolean = getBoolean("use-passive-connections")
-  val StartupTimeout: Duration = Duration.fromNanos(getNanoseconds("startup-timeout"))
-  val ShutdownTimeout: Duration = Duration.fromNanos(getNanoseconds("shutdown-timeout"))
-  val PreConnectBufferSize: Int = getInt("preconnect-buffer-size")
-  val RetryLatchClosedFor: Duration = Duration.fromNanos(getNanoseconds("retry-latch-closed-for"))
+  val Connector: String = getString("akka.remote.managed.connector")
+  val UsePassiveConnections: Boolean = getBoolean("akka.remote.managed.use-passive-connections")
+  val StartupTimeout: Duration = Duration.fromNanos(getNanoseconds("akka.remote.managed.startup-timeout"))
+  val ShutdownTimeout: Duration = Duration.fromNanos(getNanoseconds("akka.remote.managed.shutdown-timeout"))
+  val PreConnectBufferSize: Int = getInt("akka.remote.managed.preconnect-buffer-size") //TODO: rename
+  val RetryLatchClosedFor: Duration = Duration.fromNanos(getNanoseconds("akka.remote.managed.retry-latch-closed-for"))
 
 }
