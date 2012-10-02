aa0d4f4cfb567ac0c2012e68a6ce385b02145909
Björn Antonsson <ban@rhsh.net>
2012-10-02 11:09:38 +0200

    Three more tweaks. #2413

--- akka-docs/rst/scala/code/docs/dispatcher/DispatcherDocSpec.scala
+++ akka-docs/rst/scala/code/docs/dispatcher/DispatcherDocSpec.scala
@@ -112,21 +112,22 @@ object DispatcherDocSpec {
 
   // We inherit, in this case, from UnboundedPriorityMailbox
   // and seed it with the priority generator
-  class MyPrioMailbox(settings: ActorSystem.Settings, config: Config) extends UnboundedPriorityMailbox(
-    // Create a new PriorityGenerator, lower prio means more important
-    PriorityGenerator {
-      // 'highpriority messages should be treated first if possible
-      case 'highpriority ⇒ 0
+  class MyPrioMailbox(settings: ActorSystem.Settings, config: Config)
+    extends UnboundedPriorityMailbox(
+      // Create a new PriorityGenerator, lower prio means more important
+      PriorityGenerator {
+        // 'highpriority messages should be treated first if possible
+        case 'highpriority ⇒ 0
 
-      // 'lowpriority messages should be treated last if possible
-      case 'lowpriority  ⇒ 2
+        // 'lowpriority messages should be treated last if possible
+        case 'lowpriority  ⇒ 2
 
-      // PoisonPill when no other left
-      case PoisonPill    ⇒ 3
+        // PoisonPill when no other left
+        case PoisonPill    ⇒ 3
 
-      // We default to 1, which is in between high and low
-      case otherwise     ⇒ 1
-    })
+        // We default to 1, which is in between high and low
+        case otherwise     ⇒ 1
+      })
   //#prio-mailbox
 
   class MyActor extends Actor {
