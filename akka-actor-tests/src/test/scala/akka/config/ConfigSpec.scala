ab3b2749ffd39ed20764fb0b562e1406c7ffb930
Björn Antonsson <ban@rhsh.net>
2012-11-08 14:01:04 +0100

    Update version for release 2.1.0-RC2

--- akka-actor-tests/src/test/scala/akka/config/ConfigSpec.scala
+++ akka-actor-tests/src/test/scala/akka/config/ConfigSpec.scala
@@ -24,8 +24,8 @@ class ConfigSpec extends AkkaSpec(ConfigFactory.defaultReference(ActorSystem.fin
       {
         import config._
 
-        getString("akka.version") must equal("2.1-SNAPSHOT")
-        settings.ConfigVersion must equal("2.1-SNAPSHOT")
+        getString("akka.version") must equal("2.1.0-RC2")
+        settings.ConfigVersion must equal("2.1.0-RC2")
 
         getBoolean("akka.daemonic") must equal(false)
         getBoolean("akka.actor.serialize-messages") must equal(false)
--- akka-actor/src/main/scala/akka/actor/ActorSystem.scala
+++ akka-actor/src/main/scala/akka/actor/ActorSystem.scala
@@ -23,7 +23,7 @@ import util.{ Failure, Success }
 
 object ActorSystem {
 
-  val Version: String = "2.1-SNAPSHOT"
+  val Version: String = "2.1.0-RC2"
 
   val EnvHome: Option[String] = System.getenv("AKKA_HOME") match {
     case null | "" | "." ⇒ None
