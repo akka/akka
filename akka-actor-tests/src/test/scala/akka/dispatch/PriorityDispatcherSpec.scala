623672cd89accf022f29cb291dab241f7ea95c56
Viktor Klang <viktor.klang@gmail.com>
2012-11-15 10:42:14 +0100

    Rewriting PriorityDispatcherSpec not to rely on suspension

--- akka-actor-tests/src/test/scala/akka/dispatch/PriorityDispatcherSpec.scala
+++ akka-actor-tests/src/test/scala/akka/dispatch/PriorityDispatcherSpec.scala
@@ -6,10 +6,9 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import com.typesafe.config.Config
 
-import akka.actor.{ Props, InternalActorRef, ActorSystem, Actor }
+import akka.actor.{ Props, ActorSystem, Actor }
 import akka.pattern.ask
 import akka.testkit.{ DefaultTimeout, AkkaSpec }
-import scala.concurrent.Await
 import scala.concurrent.duration._
 
 object PriorityDispatcherSpec {
@@ -50,24 +49,27 @@ class PriorityDispatcherSpec extends AkkaSpec(PriorityDispatcherSpec.config) wit
   }
 
   def testOrdering(dispatcherKey: String) {
+    val msgs = (1 to 100) toList
 
     val actor = system.actorOf(Props(new Actor {
-      var acc: List[Int] = Nil
+
+      val acc = scala.collection.mutable.ListBuffer[Int]()
+
+      {
+        val l = java.util.Arrays.asList(msgs.toArray: _*)
+        java.util.Collections.shuffle(l)
+        scala.collection.JavaConverters.iterableAsScalaIterableConverter(l).asScala foreach { m ⇒ self ! m }
+      }
+
+      self.tell('Result, testActor)
 
       def receive = {
-        case i: Int  ⇒ acc = i :: acc
-        case 'Result ⇒ sender ! acc
+        case i: Int  ⇒ acc += i
+        case 'Result ⇒ sender ! acc.toList
       }
-    }).withDispatcher(dispatcherKey)).asInstanceOf[InternalActorRef]
+    }).withDispatcher(dispatcherKey))
 
-    actor.suspend //Make sure the actor isn't treating any messages, let it buffer the incoming messages
-
-    val msgs = (1 to 100).toList
-    for (m ← msgs) actor ! m
-
-    actor.resume(causedByFailure = null) //Signal the actor to start treating it's message backlog
-
-    Await.result(actor.?('Result).mapTo[List[Int]], timeout.duration) must be === msgs.reverse
+    expectMsgType[List[_]] must be === msgs
   }
 
 }
