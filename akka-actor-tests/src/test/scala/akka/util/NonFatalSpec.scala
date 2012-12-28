16fe032ac0013e2091ebe372c53c74dd20d17839
Viktor Klang <viktor.klang@gmail.com>
2012-12-28 15:48:58 +0100

    #2860 - fix rethrow + add tests to avoid regression.

--- akka-actor-tests/src/test/scala/akka/util/NonFatalSpec.scala
+++ akka-actor-tests/src/test/scala/akka/util/NonFatalSpec.scala
@@ -5,6 +5,7 @@ package akka.util
 
 import org.scalatest.matchers.MustMatchers
 import akka.testkit.AkkaSpec
+import java.io.IOException
 
 @org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
 class NonFatalSpec extends AkkaSpec with MustMatchers {
@@ -55,6 +56,15 @@ class NonFatalSpec extends AkkaSpec with MustMatchers {
       } catch {
         case NonFatal(_) | _: InterruptedException ⇒ // as expected
       }
+    }
+
+    "be able to rethrow non-RuntimeException" in {
+      val ioe = new IOException
+      val e = new Exception
+      val re = new RuntimeException
+      intercept[IOException] { akka.jsr166y.ForkJoinPool.rethrow(ioe) } must be(ioe)
+      intercept[Exception] { akka.jsr166y.ForkJoinPool.rethrow(e) } must be(e)
+      intercept[RuntimeException] { akka.jsr166y.ForkJoinPool.rethrow(re) } must be(re)
     }
 
   }
