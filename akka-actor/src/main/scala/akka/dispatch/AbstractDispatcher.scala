7b3ec79c0cc4a964878328b7dacc109695ef5753
Rich Dougherty <rich@rd.gen.nz>
2013-01-25 14:24:23 +1300

    Make it easer to override SystemMessage serialization. Fixes #2940
      * Make SystemMessage extend Serializable to avoid ambiguity when
        setting serialization-bindings.
      * Set serialVersionUID in SystemMessages and create tests to
        ensure binary formats remain unchanged.
      * Add tests for reference.conf's serialization settings.
      * Make some existing serialization tests more robust.
    
    Removed boilerplate from serialization tests
    
    Use actual reference.conf; tidy up
    
    Make serialization compatible

--- akka-actor/src/main/scala/akka/dispatch/AbstractDispatcher.scala
+++ akka-actor/src/main/scala/akka/dispatch/AbstractDispatcher.scala
@@ -72,7 +72,7 @@ private[akka] object SystemMessage {
  *
  * ➡➡➡ NEVER SEND THE SAME SYSTEM MESSAGE OBJECT TO TWO ACTORS ⬅⬅⬅
  */
-private[akka] sealed trait SystemMessage extends PossiblyHarmful {
+private[akka] sealed trait SystemMessage extends PossiblyHarmful with Serializable {
   @transient
   var next: SystemMessage = _
 }
@@ -80,42 +80,52 @@ private[akka] sealed trait SystemMessage extends PossiblyHarmful {
 /**
  * INTERNAL API
  */
+@SerialVersionUID(-4836972106317757555L)
 private[akka] case class Create(uid: Int) extends SystemMessage // send to self from Dispatcher.register
 /**
  * INTERNAL API
  */
+@SerialVersionUID(686735569005808256L)
 private[akka] case class Recreate(cause: Throwable) extends SystemMessage // sent to self from ActorCell.restart
 /**
  * INTERNAL API
  */
+@SerialVersionUID(7270271967867221401L)
 private[akka] case class Suspend() extends SystemMessage // sent to self from ActorCell.suspend
 /**
  * INTERNAL API
  */
+@SerialVersionUID(-2567504317093262591L)
 private[akka] case class Resume(causedByFailure: Throwable) extends SystemMessage // sent to self from ActorCell.resume
 /**
  * INTERNAL API
  */
+@SerialVersionUID(708873453777219599L)
 private[akka] case class Terminate() extends SystemMessage // sent to self from ActorCell.stop
 /**
  * INTERNAL API
  */
+@SerialVersionUID(3245747602115485675L)
 private[akka] case class Supervise(child: ActorRef, async: Boolean, uid: Int) extends SystemMessage // sent to supervisor ActorRef from ActorCell.start
 /**
  * INTERNAL API
  */
+@SerialVersionUID(5513569382760799668L)
 private[akka] case class ChildTerminated(child: ActorRef) extends SystemMessage // sent to supervisor from ActorCell.doTerminate
 /**
  * INTERNAL API
  */
+@SerialVersionUID(3323205435124174788L)
 private[akka] case class Watch(watchee: ActorRef, watcher: ActorRef) extends SystemMessage // sent to establish a DeathWatch
 /**
  * INTERNAL API
  */
+@SerialVersionUID(6363620903363658256L)
 private[akka] case class Unwatch(watchee: ActorRef, watcher: ActorRef) extends SystemMessage // sent to tear down a DeathWatch
 /**
  * INTERNAL API
  */
+@SerialVersionUID(-5475916034683997987L)
 private[akka] case object NoMessage extends SystemMessage // switched into the mailbox to signal termination
 
 final case class TaskInvocation(eventStream: EventStream, runnable: Runnable, cleanup: () ⇒ Unit) extends Batchable {
