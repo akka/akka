92bd87c66bb08ce27d7f6f14408c8a39c45f3e1a
Roland <rk@rkuhn.info>
2013-02-25 20:52:49 +0100

    Update version for release 2.1.1

--- akka-actor/src/main/scala/akka/actor/ActorSystem.scala
+++ akka-actor/src/main/scala/akka/actor/ActorSystem.scala
@@ -22,7 +22,7 @@ import scala.util.{ Failure, Success }
 
 object ActorSystem {
 
-  val Version: String = "2.1-SNAPSHOT"
+  val Version: String = "2.1.1"
 
   val EnvHome: Option[String] = System.getenv("AKKA_HOME") match {
     case null | "" | "." ⇒ None
