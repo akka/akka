f3a47f19872b54f4af5135aaca276e45a4041c84
Björn Antonsson <ban@rhsh.net>
2013-04-11 08:39:09 +0200

    Make karaf osgi-sample extract to target directory. See #3216

--- akka-samples/akka-sample-osgi-dining-hakkers/core/src/main/scala/akka/sample/osgi/internal/Table.scala
+++ akka-samples/akka-sample-osgi-dining-hakkers/core/src/main/scala/akka/sample/osgi/internal/Table.scala
@@ -21,6 +21,6 @@ class Table extends Actor {
   val chopsticks = for (i ← 1 to 5) yield context.actorOf(Props[Chopstick], "Chopstick" + i)
 
   def receive = {
-    case x: Int ⇒ sender ! (chopsticks(x), chopsticks((x + 1) % 5))
+    case x: Int ⇒ sender ! ((chopsticks(x), chopsticks((x + 1) % 5)))
   }
 }
