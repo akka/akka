40955bb87f8c329238c6839364d6c0b52d1f18d0
Roland <rk@rkuhn.info>
2013-05-22 17:36:04 +0200

    Update version for release 2.2-M4

--- project/AkkaBuild.scala
+++ project/AkkaBuild.scala
@@ -38,7 +38,7 @@ object AkkaBuild extends Build {
 
   lazy val buildSettings = Seq(
     organization := "com.typesafe.akka",
-    version      := "2.2-SNAPSHOT",
+    version      := "2.2-M4",
     // Also change ScalaVersion in akka-sbt-plugin/sample/project/Build.scala
     scalaVersion := System.getProperty("akka.scalaVersion", "2.11.0-M3"),
     scalaBinaryVersion <<= (scalaVersion, scalaBinaryVersion)((v, bv) => System.getProperty("akka.scalaBinaryVersion", if (v contains "-") v else bv))
