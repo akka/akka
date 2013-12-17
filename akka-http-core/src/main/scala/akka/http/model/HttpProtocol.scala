7a4dbd6026665ac9fd9a73239ab4a2e6fd0a9abd
Johannes Rudolph <johannes.rudolph@gmail.com>
2013-12-17 10:22:34 +0100

    +hco import HttpProtocols

--- akka-http-core/src/main/scala/akka/http/model/HttpProtocol.scala
+++ akka-http-core/src/main/scala/akka/http/model/HttpProtocol.scala
@@ -1,8 +1,13 @@
 package akka.http.model
 
-/** The protocol of an HTTP message */
-trait HttpProtocol
+import akka.http.rendering.SingletonValueRenderable
 
-object HttpProtocols {
-  def `HTTP/1.1`: HttpProtocol = ???
+/** The protocol of an HTTP message */
+case class HttpProtocol private[http] (override val value: String) extends SingletonValueRenderable
+
+object HttpProtocols extends ObjectRegistry[String, HttpProtocol] {
+  private def register(p: HttpProtocol): HttpProtocol = register(p.value, p)
+
+  val `HTTP/1.0` = register(HttpProtocol("HTTP/1.0"))
+  val `HTTP/1.1` = register(HttpProtocol("HTTP/1.1"))
 }
