55c44fc2eb68ec7cad23d94ceb84b0dbc57b726a
Johannes Rudolph <johannes.rudolph@gmail.com>
2013-12-20 17:46:34 +0100

    +hco port new Allow header model from current spray master

--- akka-http-core/src/main/scala/akka/http/model/headers/headers.scala
+++ akka-http-core/src/main/scala/akka/http/model/headers/headers.scala
@@ -136,6 +136,15 @@ case class `Access-Control-Max-Age`(deltaSeconds: Long) extends ModeledHeader {
   protected def companion = `Access-Control-Max-Age`
 }
 
+object Allow extends ModeledCompanion {
+  implicit val methodsRenderer = Renderer.defaultSeqRenderer[HttpMethod]
+}
+case class Allow(methods: HttpMethod*) extends ModeledHeader {
+  import Allow.methodsRenderer
+  def renderValue[R <: Rendering](r: R): r.type = r ~~ methods
+  protected def companion = Allow
+}
+
 object Authorization extends ModeledCompanion
 case class Authorization(credentials: HttpCredentials) extends ModeledHeader {
   def renderValue[R <: Rendering](r: R): r.type = r ~~ credentials
