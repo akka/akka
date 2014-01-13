e420a5bbeb632241f93c72f04d684508c47a3af4
Patrik Nordwall <patrik.nordwall@gmail.com>
2014-01-13 08:12:28 +0100

    =act #3605 Make defaultDecider a val

--- akka-actor/src/main/scala/akka/actor/FaultHandling.scala
+++ akka-actor/src/main/scala/akka/actor/FaultHandling.scala
@@ -145,26 +145,26 @@ object SupervisorStrategy extends SupervisorStrategyLowPriorityImplicits {
 
   /**
    * When supervisorStrategy is not specified for an actor this
-   * is used by default. OneForOneStrategy with decider defined in
-   * [[#defaultDecider]].
-   */
-  final val defaultStrategy: SupervisorStrategy = {
-    OneForOneStrategy()(defaultDecider)
-  }
-
-  /**
-   * When supervisorStrategy is not specified for an actor this
    * [[Decider]] is used by default in the supervisor strategy.
    * The child will be stopped when [[akka.actor.ActorInitializationException]],
    * [[akka.actor.ActorKilledException]], or [[akka.actor.DeathPactException]] is
    * thrown. It will be restarted for other `Exception` types.
    * The error is escalated if it's a `Throwable`, i.e. `Error`.
    */
-  final def defaultDecider: Decider = {
+  final val defaultDecider: Decider = {
     case _: ActorInitializationException ⇒ Stop
     case _: ActorKilledException         ⇒ Stop
     case _: DeathPactException           ⇒ Stop
     case _: Exception                    ⇒ Restart
+  }
+
+  /**
+   * When supervisorStrategy is not specified for an actor this
+   * is used by default. OneForOneStrategy with decider defined in
+   * [[#defaultDecider]].
+   */
+  final val defaultStrategy: SupervisorStrategy = {
+    OneForOneStrategy()(defaultDecider)
   }
 
   /**
