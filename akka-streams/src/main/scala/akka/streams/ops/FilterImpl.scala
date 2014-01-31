e2f5c724d4b5b43a9065ed5cfa6cc71386048919
Johannes Rudolph <johannes.rudolph@gmail.com>
2014-01-31 11:59:12 +0100

    optimize EmitMany in FilterImpl

--- akka-streams/src/main/scala/akka/streams/ops/FilterImpl.scala
+++ akka-streams/src/main/scala/akka/streams/ops/FilterImpl.scala
@@ -7,9 +7,20 @@ object FilterImpl {
       def handle(result: SimpleResult[I]): Result[I] = result match {
         case r: RequestMore ⇒ r /* + heuristic of previously filtered? */
         case Emit(i)        ⇒ if (filter.pred(i)) Emit(i) else Continue ~ RequestMore(1) // FIXME: error handling
-        case e: EmitMany[I] ⇒ handleMany(e)
-        case Complete       ⇒ Complete
-        case e: Error       ⇒ e
+        case EmitMany(is) ⇒
+          val (matching, skipped) = is.partition(filter.pred)
+
+          val result =
+            if (matching.size == 1) Emit(matching.head)
+            else if (matching.isEmpty) Continue
+            else EmitMany(matching)
+
+          val requestRemaining =
+            if (skipped.size > 0) RequestMore(skipped.size) else Continue
+
+          result ~ requestRemaining
+        case Complete ⇒ Complete
+        case e: Error ⇒ e
       }
     }
 }
