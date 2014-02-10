dcd2773c83e51f7f8349dc242603b895485046f6
Johannes Rudolph <johannes.rudolph@gmail.com>
2014-02-10 09:32:18 +0100

    update TcpEchoServer example

--- akka-streams/src/test/scala/akka/streams/io/TcpEchoServer.scala
+++ akka-streams/src/test/scala/akka/streams/io/TcpEchoServer.scala
@@ -1,19 +1,24 @@
-package akka.streams.io
+package akka.streams
+package io
 
 import akka.actor.ActorSystem
 import java.net.InetSocketAddress
+import akka.streams.io.TcpStream.IOStream
+import akka.util.ByteString
 
 object TcpEchoServer {
   //import akka.streams.Combinators._
+  import akka.streams.Operation._
   import scala.concurrent.ExecutionContext.Implicits.global
 
   def main(args: Array[String]) {
     implicit val system = ActorSystem("tcpdemo")
-    /*TcpStream.listen(new InetSocketAddress("localhost", 1111)) foreach {
+    implicit val settings = ProcessorSettings(system, () ⇒ new RaceTrack(0))
+    TcpStream.listen(new InetSocketAddress("localhost", 1111)).foreach {
       case (address, (in, out)) ⇒
         println(s"Client connected: $address")
-        in /*.onComplete { println(s"Client disconnected $address") }*/ .getPublisher.subscribe(out.getSubscriber)
-    }*/
+        in.fold(0)(_ + _.map(_.toInt).sum).map(i ⇒ ByteString(i.toString)).finish(FromConsumerSink(out)).run()
+    }.run()
     /*// alternative
     TcpStream.listenAndHandle(new InetSocketAddress("localhost", 1111)) { peer ⇒
       identity
