4ecb0987358591f1e8d22ced5f0f37e7df15d84a
Johannes Rudolph <johannes.rudolph@gmail.com>
2014-02-14 14:41:45 +0100

    fix enhancements to use correct types

--- akka-streams/src/main/scala/akka/streams/package.scala
+++ akka-streams/src/main/scala/akka/streams/package.scala
@@ -1,10 +1,10 @@
 package akka
 
 import rx.async.api.{ Processor, Consumer, Producer }
-import akka.streams.Operation
-import Operation.Pipeline
 
 package object streams {
+  import Operation.{ Sink, Source, Pipeline }
+
   implicit class LinkProducer[I](val producer: Producer[I]) extends AnyVal {
     // this probably belongs somewhere in the rx.streams.API but is probably not figured out under which name
     def link(consumer: Consumer[I]): Unit = producer.getPublisher.subscribe(consumer.getSubscriber)
@@ -12,10 +12,10 @@ package object streams {
   implicit class CreateProcessor[I, O](operation: Operation[I, O])(implicit factory: ImplementationFactory) {
     def create(): Processor[I, O] = factory.processor(operation)
   }
-  implicit class CreateProducer[O](producer: Producer[O])(implicit factory: ImplementationFactory) {
+  implicit class CreateProducer[O](producer: Source[O])(implicit factory: ImplementationFactory) {
     def create(): Producer[O] = factory.producer(producer)
   }
-  implicit class CreateConsumer[I](consumer: Consumer[I])(implicit factory: ImplementationFactory) {
+  implicit class CreateConsumer[I](consumer: Sink[I])(implicit factory: ImplementationFactory) {
     def create(): Consumer[I] = factory.consumer(consumer)
   }
   implicit class RunPipeline(val pipeline: Pipeline[_])(implicit factory: ImplementationFactory) {
