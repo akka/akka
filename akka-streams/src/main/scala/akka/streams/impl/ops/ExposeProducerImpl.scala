2a88c7d8f34394588c7fbf1ee6a4c688bd966612
Johannes Rudolph <johannes.rudolph@gmail.com>
2014-02-18 11:09:53 +0100

    extract MapLikeImpl to make it easier to provide specialized map-like operations

--- akka-streams/src/main/scala/akka/streams/impl/ops/ExposeProducerImpl.scala
+++ akka-streams/src/main/scala/akka/streams/impl/ops/ExposeProducerImpl.scala
@@ -5,11 +5,7 @@ import rx.async.api.Producer
 import akka.streams.Operation.Source
 import rx.async.spi.{ Subscription, Subscriber, Publisher }
 
-class ExposeProducerImpl[T](upstream: Upstream, downstream: Downstream[Producer[T]], ctx: ContextEffects) extends SyncOperation[Source[T]] {
-  def handleRequestMore(n: Int): Effect = upstream.requestMore(n)
-  def handleCancel(): Effect = upstream.cancel
-
-  def handleNext(element: Source[T]): Effect = downstream.next(ctx.expose(element))
-  def handleComplete(): Effect = downstream.complete
-  def handleError(cause: Throwable): Effect = downstream.error(cause)
+class ExposeProducerImpl[T](val upstream: Upstream, val downstream: Downstream[Producer[T]], ctx: ContextEffects)
+  extends MapLikeImpl[Source[T], Producer[T]] {
+  def map(element: Source[T]): Producer[T] = ctx.expose(element)
 }
--- akka-streams/src/main/scala/akka/streams/impl/ops/MapImpl.scala
+++ akka-streams/src/main/scala/akka/streams/impl/ops/MapImpl.scala
@@ -3,43 +3,7 @@ package akka.streams.impl.ops
 import akka.streams.impl._
 import scala.util.control.NonFatal
 
-class MapImpl[I, O](upstream: Upstream, downstream: Downstream[O], f: I ⇒ O) extends DynamicSyncOperation[I] {
-
-  def initial = Running
-
-  def Running = new State {
-    def handleRequestMore(n: Int): Effect = upstream.requestMore(n)
-    def handleCancel(): Effect = {
-      become(Stopped)
-      upstream.cancel
-    }
-
-    def handleNext(element: I): Effect =
-      try {
-        downstream.next(f(element))
-      } catch {
-        case NonFatal(ex) ⇒
-          become(Stopped)
-          downstream.error(ex) ~ upstream.cancel
-      }
-
-    def handleComplete(): Effect = {
-      become(Stopped)
-      downstream.complete
-    }
-    def handleError(cause: Throwable): Effect = {
-      become(Stopped)
-      downstream.error(cause)
-    }
-  }
-  def Stopped = new State {
-    def handleRequestMore(n: Int): Effect = errOut()
-    def handleCancel(): Effect = errOut()
-
-    def handleNext(element: I): Effect = errOut()
-    def handleComplete(): Effect = errOut()
-    def handleError(cause: Throwable): Effect = errOut()
-
-    def errOut(): Nothing = throw new IllegalStateException("No events expected after complete/error/cancelled")
-  }
+class MapImpl[I, O](val upstream: Upstream, val downstream: Downstream[O], f: I ⇒ O)
+  extends MapLikeImpl[I, O] {
+  def map(i: I): O = f(i)
 }
