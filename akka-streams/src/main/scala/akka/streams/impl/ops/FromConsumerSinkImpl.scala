d15a2d512edfc7e9800c83c7df7e6e082167c807
Johannes Rudolph <johannes.rudolph@gmail.com>
2014-03-19 17:56:35 +0100

    fix FromConsumerSinkImpl which failed to take responsibility for actually subscribing and instead looped infinitely
    This happened since 42c225b07a094964d112eec0c7701da61a63d36b.

--- akka-streams/src/main/scala/akka/streams/impl/ops/FromConsumerSinkImpl.scala
+++ akka-streams/src/main/scala/akka/streams/impl/ops/FromConsumerSinkImpl.scala
@@ -1,23 +1,24 @@
 package akka.streams.impl.ops
 
-import akka.streams.Operation
+import asyncrx.api.Consumer
+import asyncrx.spi.Subscription
+
 import akka.streams.impl._
-import Operation.Sink
 
-class FromConsumerSinkImpl[I](upstream: Upstream, ctx: ContextEffects, sink: Sink[I]) extends SyncSink[I] {
+class FromConsumerSinkImpl[I](upstream: Upstream, ctx: ContextEffects, consumer: Consumer[I]) extends SyncSink[I] {
   var downstream: Downstream[I] = _
-  override def start(): Effect = ctx.subscribeFrom(sink)(handleSubscribe)
-  def handleSubscribe(downstream: Downstream[I]): SyncSource = {
-    this.downstream = downstream
-    internalSource
+  override def start(): Effect = {
+    consumer.getSubscriber.onSubscribe(internalSubscription)
+    Continue
   }
 
-  val internalSource = new SyncSource {
-    def handleRequestMore(n: Int): Effect = upstream.requestMore(n)
-    def handleCancel(): Effect = upstream.cancel
+  val internalSubscription = new Subscription {
+    def requestMore(elements: Int): Unit = ctx.runStrictInContext(upstream.requestMore(elements))
+    def cancel(): Unit = ctx.runStrictInContext(upstream.cancel)
   }
+  val subscriberEffects = BasicEffects.forSubscriber(consumer.getSubscriber)
 
-  def handleNext(element: I): Effect = downstream.next(element)
-  def handleComplete(): Effect = downstream.complete
-  def handleError(cause: Throwable): Effect = downstream.error(cause)
+  def handleNext(element: I): Effect = subscriberEffects.next(element)
+  def handleComplete(): Effect = subscriberEffects.complete
+  def handleError(cause: Throwable): Effect = subscriberEffects.error(cause)
 }
