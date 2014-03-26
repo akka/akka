82a5c47a7f68220623ce21e2cd7f9c02716e7d09
Johannes Rudolph <johannes.rudolph@gmail.com>
2014-03-26 14:48:28 +0100

    =pro use shapeless with CrossVersion.full for easier 2.11 support

--- project/AkkaBuild.scala
+++ project/AkkaBuild.scala
@@ -1079,8 +1079,8 @@ object Dependencies {
     val genjavadoc    = compilerPlugin("com.typesafe.genjavadoc" %% "genjavadoc-plugin" % genJavaDocVersion cross CrossVersion.full) // ApacheV2
 
     // http-core (temporary, will be removed by internalizing)
-    val parboiled2    = "org.parboiled"               %% "parboiled"                    % "2.0-SNAPSHOT" changing()   // ApacheV2
-    val shapeless     = "com.chuusai"                 %  "shapeless_2.10.3"             % "2.0.0-M1"  // ApacheV2
+    val parboiled2    = "org.parboiled"               %% "parboiled"                    % "2.0-SNAPSHOT" changing()          // ApacheV2
+    val shapeless     = "com.chuusai"                 %% "shapeless"                    % "2.0.0-M1" cross CrossVersion.full // ApacheV2
 
     // Test
 
