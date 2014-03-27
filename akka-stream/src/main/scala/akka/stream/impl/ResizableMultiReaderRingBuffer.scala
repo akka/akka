763feaeaceb99f149425fa2304239e47aac98d5e
Roland Kuhn <rk@rkuhn.info>
2014-03-27 17:56:03 +0100

    !str step 1: add impl part that compiles

---
+++ akka-stream/src/main/scala/akka/stream/impl/ResizableMultiReaderRingBuffer.scala
@@ -0,0 +1,172 @@
+package akka.stream.impl
+
+import scala.annotation.tailrec
+import scala.util.control.NoStackTrace
+import ResizableMultiReaderRingBuffer._
+
+// FIXME move to impl
+
+/**
+ * A mutable RingBuffer that can grow in size and supports multiple readers.
+ * Contrary to many other ring buffer implementations this one does not automatically overwrite the oldest
+ * elements, rather, if full, the buffer tries to grow and rejects further writes if max capacity is reached.
+ */
+class ResizableMultiReaderRingBuffer[T](initialSize: Int, // constructor param, not field
+                                        maxSize: Int, // constructor param, not field
+                                        val cursors: Cursors) {
+  require(Integer.lowestOneBit(maxSize) == maxSize && 0 < maxSize && maxSize <= Int.MaxValue / 2,
+    "maxSize must be a power of 2 that is > 0 and < Int.MaxValue/2")
+  require(Integer.lowestOneBit(initialSize) == initialSize && 0 < initialSize && initialSize <= maxSize,
+    "initialSize must be a power of 2 that is > 0 and <= maxSize")
+
+  private[this] val maxSizeBit = Integer.numberOfTrailingZeros(maxSize)
+  private[this] var array = new Array[Any](initialSize)
+
+  // Usual ring buffer implementations keep two pointers into the array which are wrapped around
+  // (mod array.length) upon increase. However, there is an ambiguity when writeIx == readIx since this state
+  // will be reached when the buffer is completely empty as well as when the buffer is completely full.
+  // An easy fix is to add another field (like 'count') that serves as additional data point resolving the
+  // ambiguity. However, adding another field adds more overhead than required and is especially inconvenient
+  // when supporting multiple readers.
+  // Therefore the approach we take here does not add another field, rather we don't wrap around the pointers
+  // at all but simply rebase them from time to time when we have to loop through the cursors anyway.
+  private[this] var writeIx = 0
+  private[this] var readIx = 0 // the "oldest" of all read cursor indices, i.e. the one that is most behind
+
+  // current array.length log2, we don't keep it as an extra field because `Integer.numberOfTrailingZeros`
+  // is a JVM intrinsic compiling down to a `BSF` instruction on x86, which is very fast on modern CPUs
+  private def lenBit: Int = Integer.numberOfTrailingZeros(array.length)
+
+  // bit mask for converting a cursor into an array index
+  private def mask: Int = Int.MaxValue >> (31 - lenBit)
+
+  /**
+   * The number of elements currently in the buffer.
+   */
+  def size: Int = writeIx - readIx
+
+  def isEmpty: Boolean = size == 0
+
+  def nonEmpty: Boolean = !isEmpty
+
+  /**
+   * The number of elements the buffer can still take without having to be resized.
+   */
+  def immediatelyAvailable: Int = array.length - size
+
+  /**
+   * The maximum number of elements the buffer can still take.
+   */
+  def maxAvailable: Int = (1 << maxSizeBit) - size
+
+  /**
+   * Applies availability bounds to the given element number.
+   * Equivalent to `min(maxAvailable, max(immediatelyAvailable, elements))`.
+   */
+  def potentiallyAvailable(elements: Int): Int =
+    math.min(maxAvailable, math.max(immediatelyAvailable, elements))
+
+  /**
+   * Returns the number of elements that the buffer currently contains for the given cursor.
+   */
+  def count(cursor: Cursor): Int = writeIx - cursor.cursor
+
+  /**
+   * Initializes the given Cursor to the oldest buffer entry that is still available.
+   */
+  def initCursor(cursor: Cursor): Unit = cursor.cursor = readIx
+
+  /**
+   * Tries to write the given value into the buffer thereby potentially growing the backing array.
+   * Returns `true` if the write was successful and false if the buffer is full and cannot grow anymore.
+   */
+  def write(value: T): Boolean =
+    if (size < array.length) { // if we have space left we can simply write and be done
+      array(writeIx & mask) = value
+      writeIx = writeIx + 1
+      true
+    } else if (lenBit < maxSizeBit) { // if we are full but can grow we do so
+      // the growing logic is quite simple: we assemble all current buffer entries in the new array
+      // in their natural order (removing potential wrap around) and rebase all indices to zero
+      val r = readIx & mask
+      val newArray = new Array[Any](array.length << 1)
+      System.arraycopy(array, r, newArray, 0, array.length - r)
+      System.arraycopy(array, 0, newArray, array.length - r, r)
+      @tailrec def rebaseCursors(remaining: List[Cursor]): Unit = remaining match {
+        case head :: tail ⇒
+          head.cursor -= readIx
+          rebaseCursors(tail)
+        case _ ⇒ // done
+      }
+      rebaseCursors(cursors.cursors)
+      array = newArray
+      val w = size
+      array(w & mask) = value
+      writeIx = w + 1
+      readIx = 0
+      true
+    } else false
+
+  /**
+   * Tries to read from the buffer using the given Cursor.
+   * If there are no more data to be read (i.e. the cursor is already
+   * at writeIx) the method throws ResizableMultiReaderRingBuffer.NothingToReadException!
+   */
+  def read(cursor: Cursor): T = {
+    val c = cursor.cursor
+    if (c < writeIx) {
+      cursor.cursor += 1
+      if (c == readIx) updateReadIxAndPotentiallyRebaseCursors()
+      array(c & mask).asInstanceOf[T]
+    } else throw NothingToReadException
+  }
+
+  def onCursorRemoved(cursor: Cursor): Unit =
+    if (cursor.cursor == readIx) // if this cursor is the last one it must be at readIx
+      updateReadIxAndPotentiallyRebaseCursors()
+
+  private def updateReadIxAndPotentiallyRebaseCursors(): Unit = {
+    val threshold = rebaseThreshold
+    if (readIx > threshold) {
+      @tailrec def rebaseCursorsAndReturnMin(remaining: List[Cursor], result: Int): Int =
+        remaining match {
+          case head :: tail ⇒
+            head.cursor -= threshold
+            rebaseCursorsAndReturnMin(tail, math.min(head.cursor, result))
+          case _ ⇒ result
+        }
+      writeIx -= threshold
+      readIx = rebaseCursorsAndReturnMin(cursors.cursors, writeIx)
+    } else {
+      @tailrec def minCursor(remaining: List[Cursor], result: Int): Int =
+        remaining match {
+          case head :: tail ⇒ minCursor(tail, math.min(head.cursor, result))
+          case _            ⇒ result
+        }
+      readIx = minCursor(cursors.cursors, writeIx)
+      // TODO: is not nulling-out the now unused buffer cells acceptable here?
+    }
+  }
+
+  // from time to time we need to rebase all pointers and cursors so they don't overflow,
+  // rebasing is performed when `readIx` is greater than this threshold which *must* be a multiple of array.length!
+  // default: the largest safe threshold which is the largest multiple of array.length that is < Int.MaxValue/2
+  protected def rebaseThreshold: Int = (Int.MaxValue / 2) & ~mask
+
+  protected def underlyingArray: Array[Any] = array
+
+  override def toString: String =
+    s"ResizableMultiReaderRingBuffer(size=$size, writeIx=$writeIx, readIx=$readIx, cursors=${cursors.cursors.size})"
+}
+
+object ResizableMultiReaderRingBuffer {
+  object NothingToReadException extends RuntimeException with NoStackTrace
+
+  trait Cursors {
+    def cursors: List[Cursor]
+  }
+  trait Cursor {
+    def cursor: Int
+    def cursor_=(ix: Int): Unit
+  }
+}
---
+++ akka-stream/src/main/scala/akka/stream/impl/StreamImpl.scala
@@ -0,0 +1,405 @@
+package akka.stream.impl
+
+import asyncrx.api.{ Producer, Consumer, Processor }
+import akka.actor.{ Actor, ActorRef, ActorRefFactory, Props, PoisonPill }
+import asyncrx.spi.{ Publisher, Subscription, Subscriber }
+import java.util.concurrent.atomic.AtomicReference
+import scala.annotation.tailrec
+import scala.util.control.NonFatal
+import akka.stream.{ Stream, GeneratorSettings, ProcessorGenerator }
+import scala.collection.immutable
+
+/**
+ * INTERNAL API
+ */
+private[akka] object Ast {
+  trait AstNode
+
+  case class Transform(zero: Any, f: (Any, Any) ⇒ (Any, immutable.Seq[Any]), onComplete: Any ⇒ immutable.Seq[Any]) extends AstNode
+}
+
+/**
+ * INTERNAL API
+ */
+private[akka] case class StreamImpl[I, O](producer: Producer[I], ops: List[Ast.AstNode]) extends Stream[O] {
+  import Ast._
+  // Storing ops in reverse order
+  private def andThen[U](op: AstNode): Stream[U] = this.copy(ops = op :: ops)
+
+  def map[U](f: O ⇒ U): Stream[U] = transform(())((_, in) ⇒ ((), List(f(in))))
+
+  def filter(p: O ⇒ Boolean): Stream[O] = transform(())((_, in) ⇒ if (p(in)) ((), List(in)) else ((), Nil))
+
+  def grouped(n: Int): Stream[immutable.Seq[O]] =
+    transform[immutable.Seq[O], immutable.Seq[O]](Nil, (x: immutable.Seq[O]) ⇒ List(x)) { (buf: immutable.Seq[O], in: O) ⇒
+      val group = buf :+ in
+      if (group.size == n) (Nil, List(group))
+      else (group, Nil)
+    }
+
+  def mapSeq[U](f: O ⇒ immutable.Seq[U]): Stream[U] = transform(())((_, in) ⇒ ((), f(in)))
+
+  def transform[S, U](zero: S)(f: (S, O) ⇒ (S, immutable.Seq[U])): Stream[U] = transform(zero, (_: S) ⇒ Nil)(f)
+
+  def transform[S, U](zero: S, onComplete: S ⇒ immutable.Seq[U])(f: (S, O) ⇒ (S, immutable.Seq[U])): Stream[U] =
+    andThen(Transform(
+      zero,
+      f.asInstanceOf[(Any, Any) ⇒ (Any, immutable.Seq[Any])],
+      onComplete.asInstanceOf[Any ⇒ immutable.Seq[Any]]))
+
+  def toProducer(generator: ProcessorGenerator): Producer[O] = generator.toProducer(producer, ops)
+}
+
+/**
+ * INTERNAL API
+ */
+private[akka] class ActorBasedProcessorGenerator(settings: GeneratorSettings, context: ActorRefFactory) extends ProcessorGenerator {
+  import Ast._
+
+  // Ops come in reverse order
+  override def toProducer[I, O](producerToExtend: Producer[I], ops: List[AstNode]): Producer[O] = {
+    @tailrec def processorChain(topConsumer: Consumer[Any], ops: immutable.Seq[AstNode], bottomProducer: Producer[Any]): (Consumer[Any], Producer[Any]) = {
+      ops match {
+        case op :: tail ⇒
+          val opProcessor: Processor[Any, Any] = processorForNode(op)
+          opProcessor.getPublisher.subscribe(topConsumer.getSubscriber)
+          processorChain(opProcessor, tail, bottomProducer)
+        case _ ⇒ (topConsumer, bottomProducer)
+      }
+    }
+
+    if (ops.isEmpty) producerToExtend.asInstanceOf[Producer[O]]
+    else {
+      val opProcessor: Processor[Any, Any] = processorForNode(ops.head)
+      val (topConsumer, bottomProducer) = processorChain(opProcessor, ops.tail, opProcessor)
+      producerToExtend.getPublisher.subscribe(topConsumer.getSubscriber.asInstanceOf[Subscriber[I]])
+      bottomProducer.asInstanceOf[Producer[O]]
+    }
+  }
+
+  private def processorForNode(op: AstNode): Processor[Any, Any] = op match {
+    case t: Transform ⇒ ActorProcessor(context.actorOf(TransformProcessorImpl.props(settings, t)))
+    // FIXME implement the other operations as specialized ActorProcessors instead of using transform
+  }
+
+}
+
+/**
+ * INTERNAL API
+ */
+private[akka] object ActorProcessor {
+  case class OnSubscribe(subscription: Subscription)
+  // TODO performance improvement: skip wrapping ordinary elements in OnNext
+  case class OnNext(element: Any)
+  case object OnComplete
+  case class OnError(cause: Throwable)
+
+  case object SubscribePending
+
+  case class RequestMore(subscription: ActorSubscription, demand: Int)
+  case class Cancel(subscriptions: ActorSubscription)
+
+  case class ExposedPublisher(publisher: ActorPublisher[Any])
+
+  def apply[I, O](ref: ActorRef): Processor[I, O] = new ActorProcessor[I, O](ref)
+
+  trait ActorConsumer[T] extends Consumer[T] {
+    def impl: ActorRef
+    override val getSubscriber: Subscriber[T] = new ActorSubscriber[T](impl)
+  }
+
+  trait ActorProducer[T] extends Producer[T] {
+    def impl: ActorRef
+    override val getPublisher: Publisher[T] = {
+      val a = new ActorPublisher[T](impl)
+      // Resolve cyclic dependency with actor. This MUST be the first message no matter what.
+      impl ! ExposedPublisher(a.asInstanceOf[ActorPublisher[Any]])
+      a
+    }
+  }
+
+  class ActorProcessor[I, O]( final val impl: ActorRef) extends Processor[I, O] with ActorConsumer[I] with ActorProducer[O]
+
+  class ActorSubscriber[T]( final val impl: ActorRef) extends Subscriber[T] {
+    override def onError(cause: Throwable): Unit = impl ! OnError(cause)
+    override def onComplete(): Unit = impl ! OnComplete
+    override def onNext(element: T): Unit = impl ! OnNext(element)
+    override def onSubscribe(subscription: Subscription): Unit = impl ! OnSubscribe(subscription)
+  }
+
+  final class ActorPublisher[T](val impl: ActorRef) extends Publisher[T] {
+
+    // The subscriber of an subscription attempt is first placed in this list of pending subscribers.
+    // The actor will call takePendingSubscribers to remove it from the list when it has received the 
+    // SubscribePending message. The AtomicReference is set to null by the shutdown method, which is
+    // called by the actor from postStop. Pending (unregistered) subscription attempts are denied by
+    // the shutdown method. Subscription attempts after shutdown can be denied immediately.
+    private val pendingSubscribers = new AtomicReference[List[Subscriber[T]]](Nil)
+
+    override def subscribe(subscriber: Subscriber[T]): Unit = {
+      @tailrec def doSubscribe(subscriber: Subscriber[T]): Unit = {
+        val current = pendingSubscribers.get
+        if (current eq null)
+          reportShutdownError(subscriber)
+        else {
+          if (pendingSubscribers.compareAndSet(current, subscriber :: current))
+            impl ! SubscribePending
+          else
+            doSubscribe(subscriber) // CAS retry
+        }
+      }
+
+      doSubscribe(subscriber)
+    }
+
+    def takePendingSubscribers(): List[Subscriber[T]] =
+      pendingSubscribers.getAndSet(Nil)
+
+    def shutdown(): Unit =
+      pendingSubscribers.getAndSet(null) foreach reportShutdownError
+
+    private def reportShutdownError(subscriber: Subscriber[T]): Unit =
+      subscriber.onError(new IllegalStateException("Cannot subscribe to shut-down spi.Publisher"))
+
+  }
+
+  class ActorSubscription( final val impl: ActorRef, final val _subscriber: Subscriber[Any]) extends SubscriptionWithCursor {
+    override def subscriber[T]: Subscriber[T] = _subscriber.asInstanceOf[Subscriber[T]]
+    override def requestMore(elements: Int): Unit =
+      if (elements <= 0) throw new IllegalArgumentException("The number of requested elements must be > 0")
+      else impl ! RequestMore(this, elements)
+    override def cancel(): Unit = impl ! Cancel(this)
+    override def toString = "ActorSubscription"
+  }
+}
+
+/**
+ * INTERNAL API
+ */
+private[akka] abstract class ActorProcessorImpl(val settings: GeneratorSettings) extends Actor with SubscriberManagement[Any] {
+  import ActorProcessor._
+  type S = ActorSubscription
+
+  override def maxBufferSize: Int = settings.maxFanOutBufferSize
+  override def initialBufferSize: Int = settings.initialFanOutBufferSize
+  override def createSubscription(subscriber: Subscriber[Any]): S = new ActorSubscription(self, subscriber)
+
+  override def receive = waitingExposedPublisher
+
+  //////////////////////  Startup phases //////////////////////
+
+  var upstream: Subscription = _
+  var exposedPublisher: ActorPublisher[Any] = _
+
+  def waitingExposedPublisher: Receive = {
+    case ExposedPublisher(publisher) ⇒
+      exposedPublisher = publisher
+      context.become(waitingForUpstream)
+    case _ ⇒ throw new IllegalStateException("The first message must be ExposedPublisher")
+  }
+
+  def waitingForUpstream: Receive = downstreamManagement orElse {
+    case OnComplete ⇒ shutdown() // There is nothing to flush here
+    case OnSubscribe(subscription) ⇒
+      assert(subscription != null)
+      upstream = subscription
+      // Prime up input buffer
+      upstream.requestMore(inputBuffer.length)
+      context.become(running)
+    case OnError(cause) ⇒ fail(cause)
+  }
+
+  //////////////////////  Management of subscribers //////////////////////
+
+  // All methods called here are implemented by SubscriberManagement
+  val downstreamManagement: Receive = {
+    case SubscribePending                    ⇒ subscribePending()
+    case RequestMore(subscription, elements) ⇒ moreRequested(subscription, elements)
+    case Cancel(subscription)                ⇒ unregisterSubscription(subscription)
+  }
+
+  private def subscribePending(): Unit =
+    exposedPublisher.takePendingSubscribers() foreach registerSubscriber
+
+  //////////////////////  Active state //////////////////////
+
+  def running: Receive = downstreamManagement orElse {
+    case OnNext(element) ⇒
+      enqueueInputElement(element)
+      pump()
+    case OnComplete     ⇒ flushAndComplete()
+    case OnError(cause) ⇒ fail(cause)
+  }
+
+  // Called by SubscriberManagement when all subscribers are gone.
+  // The method shutdown() is called automatically by SubscriberManagement after it called this method.
+  override def cancelUpstream(): Unit = if (upstream ne null) upstream.cancel()
+
+  // Called by SubscriberManagement whenever the output buffer is ready to accept additional elements
+  override protected def requestFromUpstream(elements: Int): Unit = {
+    downstreamBufferSpace += elements
+    pump()
+  }
+
+  def fail(e: Throwable): Unit = {
+    abortDownstream(e)
+    if (upstream ne null) upstream.cancel()
+    context.stop(self)
+  }
+
+  var downstreamBufferSpace = 0
+  var inputBuffer = Array.ofDim[Any](settings.initialInputBufferSize)
+  var inputBufferElements = 0
+  var nextInputElementCursor = 0
+  val IndexMask = settings.initialInputBufferSize - 1
+
+  // TODO: buffer and batch sizing heuristics
+  def requestBatchSize = math.max(1, inputBuffer.length / 2)
+  var batchRemaining = requestBatchSize
+
+  def dequeueInputElement(): Any = {
+    val elem = inputBuffer(nextInputElementCursor & IndexMask)
+
+    batchRemaining -= 1
+    if (batchRemaining == 0 && !upstreamCompleted) {
+      upstream.requestMore(requestBatchSize)
+      batchRemaining = requestBatchSize
+    }
+
+    inputBufferElements -= 1
+    nextInputElementCursor += 1
+    nextInputElementCursor &= IndexMask
+    elem
+  }
+
+  def enqueueInputElement(elem: Any): Unit = {
+    inputBuffer((nextInputElementCursor + inputBufferElements) & IndexMask) = elem
+    inputBufferElements += 1
+  }
+
+  def enqueueOutputElement(elem: Any): Unit = {
+    downstreamBufferSpace -= 1
+    pushToDownstream(elem)
+  }
+
+  // States of the operation that is executed by this processor
+  trait TransferState {
+    def isReady: Boolean
+    def isCompleted: Boolean
+    def isExecutable = isReady && !isCompleted
+    def inputsAvailable = inputBufferElements > 0
+    def demandAvailable = downstreamBufferSpace > 0
+    def inputsDepleted = upstreamCompleted && inputBufferElements == 0
+  }
+  object NeedsInput extends TransferState {
+    def isReady = inputsAvailable
+    def isCompleted = inputsDepleted
+  }
+  object NeedsDemand extends TransferState {
+    def isReady = demandAvailable
+    def isCompleted = false
+  }
+  object NeedsInputAndDemand extends TransferState {
+    def isReady = inputsAvailable && demandAvailable
+    def isCompleted = inputsDepleted
+  }
+
+  var transferState: TransferState = NeedsInputAndDemand
+
+  // Exchange input buffer elements and output buffer "requests" until one of them becomes empty.
+  // Generate upstream requestMore for every Nth consumed input element
+  var pumping = false
+  def pump(): Unit = {
+    if (!pumping) {
+      // Prevent reentry from SubscriberManagement
+      pumping = true
+      try while (transferState.isExecutable)
+        transferState = transfer()
+      catch { case NonFatal(e) ⇒ fail(e) }
+      pumping = false
+
+      if (transferState.isCompleted) {
+        isShuttingDown = true
+        completeDownstream()
+      }
+
+    }
+  }
+
+  // Needs to be implemented by Processor implementations. Transfers elements from the input buffer to the output
+  // buffer.
+  def transfer(): TransferState
+
+  //////////////////////  Completing and Flushing  //////////////////////
+
+  var upstreamCompleted = false
+
+  def flushAndComplete(): Unit = {
+    upstreamCompleted = true
+    context.become(flushing)
+    pump()
+  }
+
+  def flushing: Receive = downstreamManagement orElse {
+    case OnSubscribe(subscription) ⇒ throw new IllegalStateException("Cannot subscribe shutdown subscriber")
+    case _                         ⇒ // ignore everything else
+  }
+
+  //////////////////////  Shutdown and cleanup (graceful and abort) //////////////////////
+
+  var isShuttingDown = false
+
+  // Called by SubscriberManagement to signal that output buffer finished (flushed or aborted)
+  override def shutdown(): Unit = {
+    isShuttingDown = true
+    context.stop(self)
+  }
+
+  override def postStop(): Unit = {
+    if (exposedPublisher ne null)
+      exposedPublisher.shutdown()
+    // Non-gracefully stopped, do our best here
+    if (!isShuttingDown)
+      abortDownstream(new IllegalStateException("Processor actor terminated abruptly"))
+
+    // FIXME what about upstream subscription before we got 
+    // case OnSubscribe(subscription) ⇒ subscription.cancel()  
+  }
+
+  override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+    super.preRestart(reason, message)
+    throw new IllegalStateException("This actor cannot be restarted")
+  }
+
+}
+
+/**
+ * INTERNAL API
+ */
+private[akka] object TransformProcessorImpl {
+  def props(settings: GeneratorSettings, op: Ast.Transform): Props =
+    Props(new TransformProcessorImpl(settings, op))
+}
+
+/**
+ * INTERNAL API
+ */
+private[akka] class TransformProcessorImpl(_settings: GeneratorSettings, op: Ast.Transform) extends ActorProcessorImpl(_settings) {
+  var state = op.zero
+  // TODO performance improvement: mutable buffer?
+  var emits = immutable.Seq.empty[Any]
+
+  override def transfer(): TransferState = {
+    if (emits.isEmpty) {
+      val (nextState, newEmits) = op.f(state, dequeueInputElement())
+      state = nextState
+      emits = newEmits
+    } else {
+      enqueueOutputElement(emits.head)
+      emits = emits.tail
+    }
+
+    if (emits.nonEmpty) NeedsDemand
+    else NeedsInputAndDemand
+  }
+}
