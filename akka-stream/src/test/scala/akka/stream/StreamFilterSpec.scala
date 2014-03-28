a4229fb1c820637f22c8d692e97543ec0be9fbcf
Roland Kuhn <rk@rkuhn.info>
2014-03-28 22:52:26 +0100

    !str add test for Filter and Map

---
+++ akka-stream/src/test/scala/akka/stream/StreamFilterSpec.scala
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) 2014 Typesafe Inc. <http://www.typesafe.com>
+ */
+package akka.stream
+
+import akka.testkit.AkkaSpec
+import akka.stream.testkit.ScriptedTest
+import scala.concurrent.forkjoin.ThreadLocalRandom.{ current ⇒ random }
+
+class StreamFilterSpec extends AkkaSpec with ScriptedTest {
+
+  val genSettings = GeneratorSettings(
+    initialInputBufferSize = 2,
+    maximumInputBufferSize = 16,
+    initialFanOutBufferSize = 1,
+    maxFanOutBufferSize = 16)
+
+  "A Filter" must {
+
+    "filter" in {
+      def script = Script((1 to 50) map { _ ⇒ val x = random.nextInt(); Seq(x) -> (if ((x & 1) == 0) Seq(x) else Seq()) }: _*)
+      (1 to 50) foreach (_ ⇒ runScript(script, genSettings)(_.filter(_ % 2 == 0)))
+    }
+
+  }
+
+}
