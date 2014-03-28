65d4223b3111f347c854c141c26eb4f7da1b5847
Patrik Nordwall <patrik.nordwall@gmail.com>
2014-03-28 12:13:57 +0100

    !str Add tests

---
+++ akka-stream/src/test/scala/akka/stream/IteratorProducerTest.scala
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C) 2014 Typesafe Inc. <http://www.typesafe.com>
+ */
+package akka.stream
+
+import org.scalatest.testng.TestNGSuiteLike
+import asyncrx.spi.Publisher
+import asyncrx.tck.PublisherVerification
+import akka.stream.testkit.TestProducer
+
+class IteratorProducerTest extends PublisherVerification[Int] with WithActorSystem with TestNGSuiteLike {
+  import system.dispatcher
+
+  def createPublisher(elements: Int): Publisher[Int] = {
+    val iter = Iterator from 1000
+    TestProducer(if (elements > 0) iter take elements else iter).getPublisher
+  }
+
+  override def createCompletedStatePublisher(): Publisher[Int] =
+    TestProducer(Nil).getPublisher
+}
