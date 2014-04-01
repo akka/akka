b1e28698887485e28edf6bb417e9c303184195ab
Roland Kuhn <rk@rkuhn.info>
2014-04-01 19:35:56 +0200

    add ScalaDocs

--- akka-stream/src/main/scala/akka/stream/Support.scala
+++ akka-stream/src/main/scala/akka/stream/Support.scala
@@ -5,4 +5,8 @@ package akka.stream
 
 import scala.util.control.NoStackTrace
 
+/**
+ * This exception can be thrown from a callback-based stream producer to
+ * signal the end of stream.
+ */
 case object Stop extends RuntimeException("Stop this flow") with NoStackTrace
