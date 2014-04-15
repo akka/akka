27a314057dedc9dc3b21af087a407f88c890b715
Konrad Malawski <ktoso@typesafe.com>
2014-04-15 13:24:20 +0200

    =str updated stream tests to use Java TCK
    A bit more hoop jumping in the constructors due to using plain java tck

--- project/AkkaBuild.scala
+++ project/AkkaBuild.scala
@@ -1152,7 +1152,7 @@ object Dependencies {
     // mirrored in OSGi sample
     val levelDBNative = "org.fusesource.leveldbjni"   % "leveldbjni-all"               % "1.7"         // New BSD
 
-    val reactiveStreams = "org.reactivestreams"      %% "reactive-streams-spi"         % "0.1-SNAPSHOT" // CC0
+    val reactiveStreams = "org.reactivestreams"       % "reactive-streams-spi"         % "0.3"         // CC0
 
     // Camel Sample
     val camelJetty  = "org.apache.camel"              % "camel-jetty"                  % camelCore.revision // ApacheV2
@@ -1186,7 +1186,7 @@ object Dependencies {
       // mirrored in OSGi sample
       val paxExam      = "org.ops4j.pax.exam"          % "pax-exam-junit4"              % "2.6.0"            % "test" // ApacheV2
 
-      val reactiveStreams = "org.reactivestreams"     %% "reactive-streams-tck"         % "0.1-SNAPSHOT"     % "test" // CC0
+      val reactiveStreams = "org.reactivestreams"      % "reactive-streams-tck"         % "0.3"              % "test" // CC0
     }
   }
 
