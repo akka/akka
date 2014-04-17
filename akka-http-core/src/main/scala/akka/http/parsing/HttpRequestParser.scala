a5fded695afe1361d1be762ca8f6bd0a29d3a3bd
Mathias <mathias@decodified.com>
2014-04-17 17:42:20 +0200

    =hco Finish high-level server-side pipeline setup

--- akka-http-core/src/main/scala/akka/http/parsing/HttpRequestParser.scala
+++ akka-http-core/src/main/scala/akka/http/parsing/HttpRequestParser.scala
@@ -5,19 +5,19 @@
 package akka.http.parsing
 
 import scala.collection.immutable
-import akka.http.model.{ HttpRequest, ErrorInfo }
+import akka.http.model.{ HttpResponse, HttpRequest, ErrorInfo }
 import akka.util.ByteString
 import akka.stream2.Operation
 
 private[http] class HttpRequestParser(settings: ParserSettings, rawRequestUriHeader: Boolean = false)(headerParser: HttpHeaderParser = HttpHeaderParser(settings))
-  extends Operation.Transformer[ByteString, HttpRequest] {
+  extends Operation.Transformer[ByteString, Either[HttpResponse, HttpRequest]] {
 
   def copyWith(warnOnIllegalHeader: ErrorInfo ⇒ Unit): HttpRequestParser =
     new HttpRequestParser(settings, rawRequestUriHeader)(headerParser.copyWith(warnOnIllegalHeader))
 
-  def onNext(elem: ByteString): immutable.Seq[HttpRequest] = ???
+  def onNext(elem: ByteString): immutable.Seq[Either[HttpResponse, HttpRequest]] = ???
 
   override def isComplete: Boolean = ???
 
-  override def onComplete: immutable.Seq[HttpRequest] = ???
+  override def onComplete: immutable.Seq[Either[HttpResponse, HttpRequest]] = ???
 }
--- akka-http-core/src/main/scala/akka/http/server/HttpServerPipeline.scala
+++ akka-http-core/src/main/scala/akka/http/server/HttpServerPipeline.scala
@@ -4,10 +4,11 @@
 
 package akka.http.server
 
+import scala.collection.immutable
 import akka.event.LoggingAdapter
 import akka.stream.io.StreamTcp
 import akka.actor.ActorRefFactory
-import akka.http.model.{ HttpResponse, ErrorInfo }
+import akka.http.model.{ HttpRequest, HttpResponse, ErrorInfo }
 import akka.http.parsing.HttpRequestParser
 import akka.http.rendering.HttpResponseRendererFactory
 import akka.http.Http
@@ -24,16 +25,43 @@ private[http] class HttpServerPipeline(settings: ServerSettings, log: LoggingAda
   val responseRendererFactory = new HttpResponseRendererFactory(settings.serverHeader, settings.chunklessStreaming)
 
   def apply(tcpConn: StreamTcp.IncomingTcpConnection): Http.IncomingConnection = {
+    val errorResponseBypass =
+      Operation[Either[HttpResponse, HttpRequest]]
+        .map(_.left.toOption)
+        .toProcessor
+
     val requestStream =
       Flow(tcpConn.inputStream)
         .transform(rootParser.copyWith(warnOnIllegalHeader))
+        .tee(errorResponseBypass)
+        .collect { case Right(request) ⇒ request }
         .toProducer
 
     val responseStream =
       Operation[HttpResponse]
+        .fanIn(errorResponseBypass, new ErrorResponseBypassFanIn)
         .mapConcat(responseRendererFactory.newRenderer)
         .produceTo(tcpConn.outputStream)
 
     Http.IncomingConnection(tcpConn.remoteAddress, requestStream, responseStream)
   }
+}
+
+/**
+ * a specialized fan-in which implements the following logic:
+ * - when a response comes in on the primary channel it is buffered until a `None` is received from the secondary
+ *   input (the error bypass), only then it is dispatched to downstream
+ * - when a `None` is received from the secondary input it clears the buffered (or next) primary response
+ *   for dispatch to downstream
+ * - when a `Some` is received from the secondary input it is immediately dispatched to downstream, a potentially
+ *   buffered regular response from the primary input is left untouched
+ */
+private[server] class ErrorResponseBypassFanIn extends Operation.FanIn[HttpResponse, Option[HttpResponse], HttpResponse] {
+  def next(): immutable.Seq[HttpResponse] = ???
+  def primaryDemand: Int = ???
+  def secondaryDemand: Int = ???
+  def primaryOnNext(elem: HttpResponse): Unit = ???
+  def secondaryOnNext(elem: Option[HttpResponse]): Unit = ???
+  def primaryOnComplete(): Unit = ???
+  def secondaryOnComplete(): Unit = ???
 }
