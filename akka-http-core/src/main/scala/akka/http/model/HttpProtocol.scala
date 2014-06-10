be8ffb8f6171df5b6eb5f4af06fac532d507350c
Mathias <mathias@decodified.com>
2014-06-10 14:15:28 +0200

    +hco Add `akka-http-core` module with initial HTTP model

---
+++ akka-http-core/src/main/scala/akka/http/model/HttpProtocol.scala
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) 2009-2014 Typesafe Inc. <http://www.typesafe.com>
+ */
+
+package akka.http.model
+
+import akka.http.util.{ SingletonValueRenderable, ObjectRegistry }
+
+/** The protocol of an HTTP message */
+final case class HttpProtocol private[http] (override val value: String) extends SingletonValueRenderable
+
+object HttpProtocols extends ObjectRegistry[String, HttpProtocol] {
+  private def register(p: HttpProtocol): HttpProtocol = register(p.value, p)
+
+  val `HTTP/1.0` = register(HttpProtocol("HTTP/1.0"))
+  val `HTTP/1.1` = register(HttpProtocol("HTTP/1.1"))
+}
