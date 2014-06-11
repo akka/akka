6ced957834533a2dd855cfb87a2ff69f7bf3eb0b
Johannes Rudolph <johannes.rudolph@gmail.com>
2014-06-11 09:57:59 +0200

    !hco more stringent following of plurally named container object
     - ContentDispositionType => ContentDispositionTypes
     - TransferEncoding => TransferEncodings
     - RangeUnit => RangeUnits

--- akka-http-core/src/main/scala/akka/http/model/headers/HttpOrigin.scala
+++ akka-http-core/src/main/scala/akka/http/model/headers/HttpOrigin.scala
@@ -15,8 +15,6 @@ abstract class HttpOriginRange extends ValueRenderable {
   def matches(origin: HttpOrigin): Boolean
 }
 object HttpOriginRange {
-  implicit val originsRenderer: Renderer[immutable.Seq[HttpOrigin]] = Renderer.seqRenderer(" ", "null")
-
   case object `*` extends HttpOriginRange {
     def matches(origin: HttpOrigin) = true
     def render[R <: Rendering](r: R): r.type = r ~~ '*'
@@ -34,6 +32,8 @@ final case class HttpOrigin(scheme: String, host: Host) extends ValueRenderable 
   def render[R <: Rendering](r: R): r.type = host.renderValue(r ~~ scheme ~~ "://")
 }
 object HttpOrigin {
+  implicit val originsRenderer: Renderer[immutable.Seq[HttpOrigin]] = Renderer.seqRenderer(" ", "null")
+
   implicit def apply(str: String): HttpOrigin = {
     val parser = new UriParser(str, UTF8, Uri.ParsingMode.Relaxed)
     parser.parseOrigin()
