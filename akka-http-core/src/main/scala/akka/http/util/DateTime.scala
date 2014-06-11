bf05139677dc14597660c652c12180ce9a883586
Johannes Rudolph <johannes.rudolph@gmail.com>
2014-06-11 09:26:07 +0200

    =hco,str small cleanups

--- akka-http-core/src/main/scala/akka/http/util/DateTime.scala
+++ akka-http-core/src/main/scala/akka/http/util/DateTime.scala
@@ -25,7 +25,7 @@ final case class DateTime private (year: Int, // the year
   def weekdayStr: String = DateTime.weekday(weekday)
 
   /**
-   * The day of the month as a 3 letter abbreviation:
+   * The month as a 3 letter abbreviation:
    * `Jan`, `Feb`, `Mar`, `Apr`, `May`, `Jun`, `Jul`, `Aug`, `Sep`, `Oct`, `Nov` or `Dec`
    */
   def monthStr: String = DateTime.month(month - 1)
