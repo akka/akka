5cd2a34fba6a3a56a34790b13eb173f52f5ad435
Konrad 'ktoso' Malawski <ktoso@typesafe.com>
2014-07-23 12:15:07 +0200

    =per #15559 use the same config as all other peristence tests
    * this includes the default expect timeout of 10s (instead of 3s)

--- akka-persistence-tck/src/test/scala/akka/persistence/journal/leveldb/LeveldbJournalNativePerfSpec.scala
+++ akka-persistence-tck/src/test/scala/akka/persistence/journal/leveldb/LeveldbJournalNativePerfSpec.scala
@@ -3,19 +3,15 @@
  */
 package akka.persistence.journal.leveldb
 
-import com.typesafe.config.ConfigFactory
-
-import akka.persistence.journal.{ JournalPerfSpec, JournalSpec }
-import akka.persistence.PluginCleanup
+import akka.persistence.journal.{JournalPerfSpec, JournalSpec}
+import akka.persistence.{PersistenceSpec, PluginCleanup}
 import org.scalatest.DoNotDiscover
 
 @DoNotDiscover // because only checking that compilation is OK with JournalPerfSpec
 class LeveldbJournalNativePerfSpec extends JournalSpec with JournalPerfSpec with PluginCleanup {
-  lazy val config = ConfigFactory.parseString(
-    """
-      |akka.persistence.journal.plugin = "akka.persistence.journal.leveldb"
-      |akka.persistence.journal.leveldb.native = on
-      |akka.persistence.journal.leveldb.dir = "target/journal-native"
-      |akka.persistence.snapshot-store.local.dir = "target/snapshots-native/"
-    """.stripMargin)
+  lazy val config = PersistenceSpec.config(
+    "leveldb",
+    "LeveldbJournalNativePerfSpec",
+    extraConfig = Some("akka.persistence.journal.leveldb.native = on")
+  )
 }
