9a8b6bb0c276289517fd1819511bec73b9d8d8d9
Johannes Rudolph <johannes.rudolph@gmail.com>
2014-07-31 11:52:38 +0200

    reenable HostDirectiveSpecs

--- akka-http-tests/src/test/scala/akka/http/routing/directives/HostDirectivesSpec.scala
+++ akka-http-tests/src/test/scala/akka/http/routing/directives/HostDirectivesSpec.scala
@@ -17,55 +17,50 @@
 package akka.http.routing.directives
 
 import akka.http.model.headers.Host
+import org.scalatest.FreeSpec
 
-class HostDirectivesSpec extends RoutingSpec {
-
-  "The 'host' directive" should {
-
-    "in its simple String form" in pendingUntilFixed {
-
-      "block requests to unmatched hosts" in pendingUntilFixed {
+class HostDirectivesSpec extends FreeSpec with GenericRoutingSpec {
+  "The 'host' directive" - {
+    "in its simple String form should" - {
+      "block requests to unmatched hosts" in {
         Get() ~> Host("spray.io") ~> {
           host("spray.com") { completeOk }
         } ~> check { handled mustEqual false }
       }
 
-      "let requests to matching hosts pass" in pendingUntilFixed {
+      "let requests to matching hosts pass" in {
         Get() ~> Host("spray.io") ~> {
           host("spray.com", "spray.io") { completeOk }
         } ~> check { response mustEqual Ok }
       }
     }
 
-    "in its simple RegEx form" in pendingUntilFixed {
-
-      "block requests to unmatched hosts" in pendingUntilFixed {
+    "in its simple RegEx form" - {
+      "block requests to unmatched hosts" in {
         Get() ~> Host("spray.io") ~> {
           host("hairspray.*".r) { echoComplete }
         } ~> check { handled mustEqual false }
       }
 
-      "let requests to matching hosts pass and extract the full host" in pendingUntilFixed {
+      "let requests to matching hosts pass and extract the full host" in {
         Get() ~> Host("spray.io") ~> {
           host("spra.*".r) { echoComplete }
         } ~> check { responseAs[String] mustEqual "spray.io" }
       }
     }
 
-    "in its group RegEx form" in pendingUntilFixed {
-
-      "block requests to unmatched hosts" in pendingUntilFixed {
+    "in its group RegEx form" - {
+      "block requests to unmatched hosts" in {
         Get() ~> Host("spray.io") ~> {
           host("hairspray(.*)".r) { echoComplete }
         } ~> check { handled mustEqual false }
       }
 
-      "let requests to matching hosts pass and extract the full host" in pendingUntilFixed {
+      "let requests to matching hosts pass and extract the full host" in {
         Get() ~> Host("spray.io") ~> {
           host("spra(.*)".r) { echoComplete }
         } ~> check { responseAs[String] mustEqual "y.io" }
       }
     }
   }
-
 }
