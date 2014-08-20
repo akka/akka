6d165d28f9ab1bb291c5da7ae2438650803a00d2
Martynas Mickevicius <mmartynas@gmail.com>
2014-08-20 09:06:46 +0300

    !str #15672 added all combinators from current Flow API
    * combinator tests

--- akka-stream/src/main/scala/akka/stream/dsl/Graph.scala
+++ akka-stream/src/main/scala/akka/stream/dsl/Graph.scala
@@ -4,18 +4,16 @@ package akka.stream.dsl
  * Dummy implementation needed for runtime tests.
  */
 trait Graph {
-  def broadcast[T](source: HasOpenOutput[_, T], destinations: Seq[HasOpenInput[T, _]]) = ()
-  def zip[T, U](source1: HasOpenOutput[_, T], source2: HasOpenOutput[_, U], destination: HasOpenInput[(T, U), _]) = ()
 
-  /**
-   * Whenever a new K value is returned from `f`, a new `dest` flow will be materialized.
-   *
-   * The discriminating value `K` of the output flow can be known by applying `f` on any
-   * element in the output flows.
-   */
-  def groupBy[T, K](source: HasOpenOutput[_, T], f: T ⇒ K, dest: HasOpenInput[T, _]) = ()
+  def merge[T](source1: HasOpenOutput[_, T], source2: HasOpenOutput[_, T], destination: HasOpenInput[T, _]) = ()
+  def zip[T, U](source1: HasOpenOutput[_, T], source2: HasOpenOutput[_, U], destination: HasOpenInput[(T, U), _]) = ()
+  def concat[T](source1: HasOpenOutput[_, T], source2: HasOpenOutput[_, T], destination: HasOpenInput[T, _]) = ()
+  def broadcast[T](source: HasOpenOutput[_, T], destinations: Seq[HasOpenInput[T, _]]) = ()
 }
 
 object Graph {
   def apply(): Graph = new Graph {}
 }
+
+final case class Broadcast[T]() extends Input[T] with Output[T]
+final case class Zip[T]() extends Input[T] with Output[T]
