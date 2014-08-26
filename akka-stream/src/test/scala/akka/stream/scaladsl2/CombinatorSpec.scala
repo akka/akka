11936c43618311d41cf05facf526cbf92cfcf2d2
Patrik Nordwall <patrik.nordwall@gmail.com>
2014-08-26 18:11:11 +0200

    =str #15746 Rename flow classes

--- akka-stream/src/test/scala/akka/stream/scaladsl2/CombinatorSpec.scala
+++ akka-stream/src/test/scala/akka/stream/scaladsl2/CombinatorSpec.scala
@@ -15,66 +15,66 @@ class CombinatorSpec extends WordSpec with Matchers {
 
   "Linear simple combinators in Flow" should {
     "map" in {
-      val t: OpenFlow[Int, Int] = f.map(_ * 2)
+      val t: ProcessorFlow[Int, Int] = f.map(_ * 2)
     }
     "mapFuture" in {
       import scala.concurrent.ExecutionContext.Implicits.global
-      val t: OpenFlow[Int, Int] = f.mapFuture(Future(_))
+      val t: ProcessorFlow[Int, Int] = f.mapFuture(Future(_))
     }
     "filter" in {
-      val t: OpenFlow[Int, Int] = f.filter(_ != 2)
+      val t: ProcessorFlow[Int, Int] = f.filter(_ != 2)
     }
     "collect" in {
-      val t: OpenFlow[Int, String] = f.collect {
+      val t: ProcessorFlow[Int, String] = f.collect {
         case i: Int if i == 2 ⇒ "two"
       }
     }
     "fold" in {
       val fo = FoldOut("elements:") { (soFar, element: Int) ⇒ soFar + element }
-      val t: OpenInputFlow[Int, Int] = f.withOutput(fo)
+      val t: SubscriberFlow[Int, Int] = f.withOutput(fo)
     }
     "drop" in {
-      val t: OpenFlow[Int, Int] = f.drop(2)
+      val t: ProcessorFlow[Int, Int] = f.drop(2)
     }
     "dropWithin" in {
-      val t: OpenFlow[Int, Int] = f.dropWithin(2.seconds)
+      val t: ProcessorFlow[Int, Int] = f.dropWithin(2.seconds)
     }
     "take" in {
-      val t: OpenFlow[Int, Int] = f.take(2)
+      val t: ProcessorFlow[Int, Int] = f.take(2)
     }
     "takeWithin" in {
-      val t: OpenFlow[Int, Int] = f.takeWithin(2.seconds)
+      val t: ProcessorFlow[Int, Int] = f.takeWithin(2.seconds)
     }
     "grouped" in {
-      val t: OpenFlow[Int, immutable.Seq[Int]] = f.grouped(2)
+      val t: ProcessorFlow[Int, immutable.Seq[Int]] = f.grouped(2)
     }
     "groupedWithin" in {
-      val t: OpenFlow[Int, immutable.Seq[Int]] = f.groupedWithin(2, 2.seconds)
+      val t: ProcessorFlow[Int, immutable.Seq[Int]] = f.groupedWithin(2, 2.seconds)
     }
     "mapConcat" in {
-      val t: OpenFlow[Int, Int] = f.mapConcat { i ⇒ immutable.Seq(i, i, i) }
+      val t: ProcessorFlow[Int, Int] = f.mapConcat { i ⇒ immutable.Seq(i, i, i) }
     }
     "conflate" in {
-      val t: OpenFlow[Int, String] = f.conflate(_.toString, (soFar: String, i) ⇒ soFar + i)
+      val t: ProcessorFlow[Int, String] = f.conflate(_.toString, (soFar: String, i) ⇒ soFar + i)
     }
     "expand" in {
-      val t: OpenFlow[Int, String] = f.expand(_.toString, (soFar: String) ⇒ (soFar, "_"))
+      val t: ProcessorFlow[Int, String] = f.expand(_.toString, (soFar: String) ⇒ (soFar, "_"))
     }
     "buffer" in {
-      val t: OpenFlow[Int, Int] = f.buffer(100, OverflowStrategy.DropHead)
+      val t: ProcessorFlow[Int, Int] = f.buffer(100, OverflowStrategy.DropHead)
     }
   }
 
   "Linear combinators which produce multiple flows" should {
     "prefixAndTail" in {
-      val t: OpenFlow[Int, (immutable.Seq[String], OpenOutputFlow[String, String])] =
+      val t: ProcessorFlow[Int, (immutable.Seq[String], PublisherFlow[String, String])] =
         f.map(_.toString).prefixAndTail(10)
     }
     "groupBy" in {
-      val grouped: OpenOutputFlow[Int, (String, OpenOutputFlow[Int, Int])] =
+      val grouped: PublisherFlow[Int, (String, PublisherFlow[Int, Int])] =
         From(immutable.Seq(1, 2, 3)).map(_ * 2).groupBy((o: Int) ⇒ o.toString)
 
-      val closedInner: OpenOutputFlow[Int, (String, ClosedFlow[Int, Int])] = grouped.map {
+      val closedInner: PublisherFlow[Int, (String, RunnableFlow[Int, Int])] = grouped.map {
         case (key, openFlow) ⇒ (key, openFlow.withOutput(PublisherOut()))
       }
 
@@ -83,14 +83,14 @@ class CombinatorSpec extends WordSpec with Matchers {
       closedInner.withOutput(PublisherOut()).run
     }
     "splitWhen" in {
-      val t: OpenFlow[Int, OpenOutputFlow[String, String]] = f.map(_.toString).splitWhen(_.length > 2)
+      val t: ProcessorFlow[Int, PublisherFlow[String, String]] = f.map(_.toString).splitWhen(_.length > 2)
     }
   }
 
   "Linear combinators which consume multiple flows" should {
     "flatten" in {
-      val split: OpenFlow[Int, OpenOutputFlow[String, String]] = f.map(_.toString).splitWhen(_.length > 2)
-      val flattened: OpenFlow[Int, String] = split.flatten(FlattenStrategy.concatOpenOutputFlow)
+      val split: ProcessorFlow[Int, PublisherFlow[String, String]] = f.map(_.toString).splitWhen(_.length > 2)
+      val flattened: ProcessorFlow[Int, String] = split.flatten(FlattenStrategy.concatPublisherFlow)
     }
   }
 
