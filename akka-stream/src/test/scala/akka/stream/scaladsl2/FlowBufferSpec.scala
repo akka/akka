e3fc05b0f3c54df1a2e4f2c858e460de83816e6c
Patrik Nordwall <patrik.nordwall@gmail.com>
2014-09-09 14:39:45 +0200

    =str #15741 Hook-up conflate, expand, buffer, and FoldSink

---
+++ akka-stream/src/test/scala/akka/stream/scaladsl2/FlowBufferSpec.scala
@@ -0,0 +1,190 @@
+/**
+ * Copyright (C) 2009-2014 Typesafe Inc. <http://www.typesafe.com>
+ */
+package akka.stream.scaladsl2
+
+import akka.stream.testkit.{ AkkaSpec, StreamTestKit }
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import akka.stream.MaterializerSettings
+import akka.stream.OverflowStrategy
+
+class FlowBufferSpec extends AkkaSpec {
+
+  val settings = MaterializerSettings(system)
+    .withInputBuffer(initialSize = 1, maxSize = 1)
+    .withFanOutBuffer(initialSize = 1, maxSize = 1)
+
+  implicit val materializer = FlowMaterializer(settings)
+
+  "Buffer" must {
+
+    "pass elements through normally in backpressured mode" in {
+      val futureSink = FutureSink[Seq[Int]]
+      val mf = FlowFrom((1 to 1000).iterator).buffer(100, overflowStrategy = OverflowStrategy.backpressure).grouped(1001).
+        withSink(futureSink).run()
+      val future = futureSink.future(mf)
+      Await.result(future, 3.seconds) should be(1 to 1000)
+    }
+
+    "pass elements through normally in backpressured mode with buffer size one" in {
+      val futureSink = FutureSink[Seq[Int]]
+      val mf = FlowFrom((1 to 1000).iterator).buffer(1, overflowStrategy = OverflowStrategy.backpressure).grouped(1001).
+        withSink(futureSink).run()
+      val future = futureSink.future(mf)
+      Await.result(future, 3.seconds) should be(1 to 1000)
+    }
+
+    "pass elements through a chain of backpressured buffers of different size" in {
+      val futureSink = FutureSink[Seq[Int]]
+      val mf = FlowFrom((1 to 1000).iterator)
+        .buffer(1, overflowStrategy = OverflowStrategy.backpressure)
+        .buffer(10, overflowStrategy = OverflowStrategy.backpressure)
+        .buffer(256, overflowStrategy = OverflowStrategy.backpressure)
+        .buffer(1, overflowStrategy = OverflowStrategy.backpressure)
+        .buffer(5, overflowStrategy = OverflowStrategy.backpressure)
+        .buffer(128, overflowStrategy = OverflowStrategy.backpressure)
+        .grouped(1001)
+        .withSink(futureSink).run()
+      val future = futureSink.future(mf)
+      Await.result(future, 3.seconds) should be(1 to 1000)
+    }
+
+    "accept elements that fit in the buffer while downstream is silent" in {
+      val publisher = StreamTestKit.PublisherProbe[Int]()
+      val subscriber = StreamTestKit.SubscriberProbe[Int]()
+
+      FlowFrom(publisher).buffer(100, overflowStrategy = OverflowStrategy.backpressure).publishTo(subscriber)
+
+      val autoPublisher = new StreamTestKit.AutoPublisher(publisher)
+      val sub = subscriber.expectSubscription()
+
+      // Fill up buffer
+      for (i ← 1 to 100) autoPublisher.sendNext(i)
+
+      // drain
+      for (i ← 1 to 100) {
+        sub.request(1)
+        subscriber.expectNext(i)
+      }
+      sub.cancel()
+    }
+
+    "drop head elements if buffer is full and configured so" in {
+      val publisher = StreamTestKit.PublisherProbe[Int]()
+      val subscriber = StreamTestKit.SubscriberProbe[Int]()
+
+      FlowFrom(publisher).buffer(100, overflowStrategy = OverflowStrategy.dropHead).publishTo(subscriber)
+
+      val autoPublisher = new StreamTestKit.AutoPublisher(publisher)
+      val sub = subscriber.expectSubscription()
+
+      // Fill up buffer
+      for (i ← 1 to 200) autoPublisher.sendNext(i)
+
+      // drain
+      for (i ← 101 to 200) {
+        sub.request(1)
+        subscriber.expectNext(i)
+      }
+
+      sub.request(1)
+      subscriber.expectNoMsg(1.seconds)
+
+      autoPublisher.sendNext(-1)
+      sub.request(1)
+      subscriber.expectNext(-1)
+
+      sub.cancel()
+    }
+
+    "drop tail elements if buffer is full and configured so" in {
+      val publisher = StreamTestKit.PublisherProbe[Int]()
+      val subscriber = StreamTestKit.SubscriberProbe[Int]()
+
+      FlowFrom(publisher).buffer(100, overflowStrategy = OverflowStrategy.dropTail).publishTo(subscriber)
+
+      val autoPublisher = new StreamTestKit.AutoPublisher(publisher)
+      val sub = subscriber.expectSubscription()
+
+      // Fill up buffer
+      for (i ← 1 to 200) autoPublisher.sendNext(i)
+
+      // drain
+      for (i ← 1 to 99) {
+        sub.request(1)
+        subscriber.expectNext(i)
+      }
+
+      sub.request(1)
+      subscriber.expectNext(200)
+
+      sub.request(1)
+      subscriber.expectNoMsg(1.seconds)
+
+      autoPublisher.sendNext(-1)
+      sub.request(1)
+      subscriber.expectNext(-1)
+
+      sub.cancel()
+    }
+
+    "drop all elements if buffer is full and configured so" in {
+      val publisher = StreamTestKit.PublisherProbe[Int]
+      val subscriber = StreamTestKit.SubscriberProbe[Int]()
+
+      FlowFrom(publisher).buffer(100, overflowStrategy = OverflowStrategy.dropBuffer).publishTo(subscriber)
+
+      val autoPublisher = new StreamTestKit.AutoPublisher(publisher)
+      val sub = subscriber.expectSubscription()
+
+      // Fill up buffer
+      for (i ← 1 to 150) autoPublisher.sendNext(i)
+
+      // drain
+      for (i ← 101 to 150) {
+        sub.request(1)
+        subscriber.expectNext(i)
+      }
+
+      sub.request(1)
+      subscriber.expectNoMsg(1.seconds)
+
+      autoPublisher.sendNext(-1)
+      sub.request(1)
+      subscriber.expectNext(-1)
+
+      sub.cancel()
+    }
+
+    for (strategy ← List(OverflowStrategy.dropHead, OverflowStrategy.dropTail, OverflowStrategy.dropBuffer)) {
+
+      s"work with $strategy if buffer size of one" in {
+
+        val publisher = StreamTestKit.PublisherProbe[Int]
+        val subscriber = StreamTestKit.SubscriberProbe[Int]()
+
+        FlowFrom(publisher).buffer(1, overflowStrategy = strategy).publishTo(subscriber)
+
+        val autoPublisher = new StreamTestKit.AutoPublisher(publisher)
+        val sub = subscriber.expectSubscription()
+
+        // Fill up buffer
+        for (i ← 1 to 200) autoPublisher.sendNext(i)
+
+        sub.request(1)
+        subscriber.expectNext(200)
+
+        sub.request(1)
+        subscriber.expectNoMsg(1.seconds)
+
+        autoPublisher.sendNext(-1)
+        sub.request(1)
+        subscriber.expectNext(-1)
+
+        sub.cancel()
+      }
+    }
+
+  }
+}
