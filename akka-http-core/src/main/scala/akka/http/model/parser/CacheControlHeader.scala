176dc395f279363cb953f9d0e576809834220b91
Mathias <mathias@decodified.com>
2014-10-02 14:40:07 +0200

    =htc fix parsing of `Cache-Control` header

--- akka-http-core/src/main/scala/akka/http/model/parser/CacheControlHeader.scala
+++ akka-http-core/src/main/scala/akka/http/model/parser/CacheControlHeader.scala
@@ -9,7 +9,7 @@ import akka.parboiled2.Parser
 import headers._
 import CacheDirectives._
 
-private[parser] trait CacheControlHeader { this: Parser with CommonRules with CommonActions ⇒
+private[parser] trait CacheControlHeader { this: Parser with CommonRules with CommonActions with StringBuilding ⇒
 
   // http://tools.ietf.org/html/rfc7234#section-5.2
   def `cache-control` = rule {
@@ -17,8 +17,7 @@ private[parser] trait CacheControlHeader { this: Parser with CommonRules with Co
   }
 
   def `cache-directive` = rule(
-    "no-cache" ~ push(`no-cache`)
-      | "no-store" ~ push(`no-store`)
+    "no-store" ~ push(`no-store`)
       | "no-transform" ~ push(`no-transform`)
       | "max-age=" ~ `delta-seconds` ~> (`max-age`(_))
       | "max-stale" ~ optional(ws('=') ~ `delta-seconds`) ~> (`max-stale`(_))
@@ -26,11 +25,17 @@ private[parser] trait CacheControlHeader { this: Parser with CommonRules with Co
       | "only-if-cached" ~ push(`only-if-cached`)
       | "public" ~ push(`public`)
       | "private" ~ (ws('=') ~ `field-names` ~> (`private`(_: _*)) | push(`private`(Nil: _*)))
-      | "no-cache" ~ (ws('=') ~ `field-names` ~> (`no-cache`(_: _*)) | push(`no-cache`(Nil: _*)))
+      | "no-cache" ~ (ws('=') ~ `field-names` ~> (`no-cache`(_: _*)) | push(`no-cache`))
       | "must-revalidate" ~ push(`must-revalidate`)
       | "proxy-revalidate" ~ push(`proxy-revalidate`)
       | "s-maxage=" ~ `delta-seconds` ~> (`s-maxage`(_))
       | token ~ optional(ws('=') ~ word) ~> (CacheDirective.custom(_, _)))
 
-  def `field-names` = rule { oneOrMore(word).separatedBy(listSep) }
+  def `field-names` = rule { `quoted-tokens` | token ~> (Seq(_)) }
+
+  def `quoted-tokens` = rule { '"' ~ zeroOrMore(`quoted-tokens-elem`).separatedBy(listSep) ~ '"' }
+
+  def `quoted-tokens-elem` = rule {
+    clearSB() ~ zeroOrMore(!'"' ~ !',' ~ qdtext ~ appendSB() | `quoted-pair`) ~ push(sb.toString)
+  }
 }
