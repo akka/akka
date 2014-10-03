5785803eb733f6fd582b4255b61af0d5e4ab125b
Mathias <mathias@decodified.com>
2014-10-03 12:43:25 +0200

    =htc allow `0` as special value `If-Modified-Since` and similar headers
    Port of https://github.com/spray/spray/issues/942

--- akka-http-core/src/test/scala/akka/http/model/parser/HttpHeaderSpec.scala
+++ akka-http-core/src/test/scala/akka/http/model/parser/HttpHeaderSpec.scala
@@ -254,6 +254,7 @@ class HttpHeaderSpec extends FreeSpec with Matchers {
 
     "If-Modified-Since" in {
       "If-Modified-Since: Wed, 13 Jul 2011 08:12:31 GMT" =!= `If-Modified-Since`(DateTime(2011, 7, 13, 8, 12, 31))
+      "If-Modified-Since: 0" =!= `If-Modified-Since`(DateTime.MinValue).renderedTo("Wed, 01 Jan 1800 00:00:00 GMT")
     }
 
     "If-None-Match" in {
