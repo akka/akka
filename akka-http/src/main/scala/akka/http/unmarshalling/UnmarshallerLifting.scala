6dc20da339e8b3eb25ccc508917310439b74f81c
Mathias <mathias@decodified.com>
2014-10-06 10:35:39 +0200

    =htp fix `UnmarshallerLifting` to not obscure missing Unmarshaller errors with implicit diversions
    Unfortunately implicit resolution evaluates type boundaries only quite late in the resolution process which can lead to fake implicit diversions errors. Getting rid of the type boundary here make debugging Unmarshaller type class problem easier.

--- akka-http/src/main/scala/akka/http/unmarshalling/UnmarshallerLifting.scala
+++ akka-http/src/main/scala/akka/http/unmarshalling/UnmarshallerLifting.scala
@@ -4,12 +4,12 @@
 
 package akka.http.unmarshalling
 
-import akka.http.model._
-
 trait UnmarshallerLifting {
+  implicit def entity2request[T](implicit um: FromEntityUnmarshaller[T]): FromRequestUnmarshaller[T] =
+    Unmarshaller { request ⇒ um(request.entity) }
 
-  implicit def entity2message[A <: HttpMessage, B](implicit um: FromEntityUnmarshaller[B]): Unmarshaller[A, B] =
-    Unmarshaller { message ⇒ um(message.entity) }
+  implicit def entity2response[T](implicit um: FromEntityUnmarshaller[T]): FromResponseUnmarshaller[T] =
+    Unmarshaller { response ⇒ um(response.entity) }
 
   implicit def message2request[T](implicit um: FromMessageUnmarshaller[T]): FromRequestUnmarshaller[T] =
     Unmarshaller { request ⇒ um(request) }
