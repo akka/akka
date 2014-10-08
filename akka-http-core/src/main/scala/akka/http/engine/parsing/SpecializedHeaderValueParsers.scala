d4ddc740a6fd31ca4fdc04125fdb12dfd16b1e3c
Mathias <mathias@decodified.com>
2014-10-08 13:41:48 +0200

    =htc small improvement to `ContentLengthParser`

--- akka-http-core/src/main/scala/akka/http/engine/parsing/SpecializedHeaderValueParsers.scala
+++ akka-http-core/src/main/scala/akka/http/engine/parsing/SpecializedHeaderValueParsers.scala
@@ -7,7 +7,7 @@ package akka.http.engine.parsing
 import scala.annotation.tailrec
 import akka.util.ByteString
 import akka.http.model.{ HttpHeader, ErrorInfo }
-import akka.http.model.headers.`Content-Length`
+import akka.http.model.headers.{ RawHeader, `Content-Length` }
 import akka.http.model.parser.CharacterClasses._
 
 /**
@@ -22,9 +22,10 @@ private object SpecializedHeaderValueParsers {
     def apply(input: ByteString, valueStart: Int, warnOnIllegalHeader: ErrorInfo ⇒ Unit): (HttpHeader, Int) = {
       @tailrec def recurse(ix: Int = valueStart, result: Long = 0): (HttpHeader, Int) = {
         val c = byteChar(input, ix)
-        if (DIGIT(c) && result >= 0) recurse(ix + 1, result * 10 + c - '0')
+        if (result < 0) fail("`Content-Length` header value must not exceed 63-bit integer range")
+        else if (DIGIT(c)) recurse(ix + 1, result * 10 + c - '0')
         else if (WSP(c)) recurse(ix + 1, result)
-        else if (c == '\r' && byteChar(input, ix + 1) == '\n' && result >= 0) (`Content-Length`(result), ix + 2)
+        else if (c == '\r' && byteChar(input, ix + 1) == '\n') (`Content-Length`(result), ix + 2)
         else fail("Illegal `Content-Length` header value")
       }
       recurse()
