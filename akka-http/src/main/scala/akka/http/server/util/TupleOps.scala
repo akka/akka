33ddabbc9fc46a78685e147461e80c74a462b706
Mathias <mathias@decodified.com>
2014-10-15 10:06:36 +0200

    =htp refactor tuple join to foldLeft-based implementation, unify tuple operations

--- akka-http/src/main/scala/akka/http/server/util/TupleOps.scala
+++ akka-http/src/main/scala/akka/http/server/util/TupleOps.scala
@@ -7,48 +7,58 @@ package akka.http.server.util
 class TupleOps[T](val tuple: T) extends AnyVal {
   import TupleOps._
 
-  def foldLeft[In](zero: In)(op: Poly2)(implicit fold: FoldLeft[In, T, op.type]): fold.Out = fold(zero, tuple)
+  /**
+   * Appends the given value to the tuple producing a tuple of arity n + 1.
+   */
+  def append[S](value: S)(implicit ao: AppendOne[T, S]): ao.Out = ao(tuple, value)
+
+  /**
+   * Left-Folds over the tuple using the given binary poly-function.
+   */
+  def foldLeft[In](zero: In)(op: BinaryPolyFunc)(implicit fold: FoldLeft[In, T, op.type]): fold.Out = fold(zero, tuple)
+
+  /**
+   * Appends the given tuple to the underlying tuple producing a tuple of arity n + m.
+   */
+  def join[S](suffixTuple: S)(implicit join: Join[T, S]): join.Out = join(tuple, suffixTuple)
 }
 
 object TupleOps {
   implicit def enhanceTuple[T: Tuple](tuple: T) = new TupleOps(tuple)
 
+  trait AppendOne[P, S] {
+    type Out
+    def apply(prefix: P, last: S): Out
+  }
+  object AppendOne extends TupleAppendOneInstances
+
   trait FoldLeft[In, T, Op] {
     type Out
     def apply(zero: In, tuple: T): Out
   }
-  object FoldLeft extends TupleFoldInstances {
-    import Poly2.Case
+  object FoldLeft extends TupleFoldInstances
 
-    type Aux[In, T, Op, Out0] = FoldLeft[In, T, Op] { type Out = Out0 }
-
-    implicit def t0[In, Op]: Aux[In, Unit, Op, In] =
-      new FoldLeft[In, Unit, Op] {
-        type Out = In
-        def apply(zero: In, tuple: Unit) = zero
-      }
-
-    implicit def t1[In, A, Op](implicit f: Case[In, A, Op]): Aux[In, Tuple1[A], Op, f.Out] =
-      new FoldLeft[In, Tuple1[A], Op] {
-        type Out = f.Out
-        def apply(zero: In, tuple: Tuple1[A]) = f(zero, tuple._1)
-      }
+  trait Join[P, S] {
+    type Out
+    def apply(prefix: P, suffix: S): Out
   }
-}
-
-trait Poly2 {
-  def at[A, B] = new CaseBuilder[A, B]
-  class CaseBuilder[A, B] {
-    def apply[R](f: (A, B) ⇒ R) = new Poly2.Case[A, B, Poly2.this.type] {
-      type Out = R
-      def apply(a: A, b: B) = f(a, b)
+  object Join extends LowLevelJoinImplicits {
+    // O(1) shortcut for the Join[Unit, T] case to avoid O(n) runtime in this case
+    implicit def join0P[T] =
+      new Join[Unit, T] {
+        type Out = T
+        def apply(prefix: Unit, suffix: T): Out = suffix
+      }
+    // we implement the join by folding over the suffix with the prefix as growing accumulator
+    object Fold extends BinaryPolyFunc {
+      implicit def step[T, A](implicit append: AppendOne[T, A]) = at[T, A](append(_, _))
     }
   }
-}
-object Poly2 {
-  trait Case[A, B, Op] {
-    type Out
-    def apply(a: A, b: B): Out
+  sealed abstract class LowLevelJoinImplicits {
+    implicit def join[P, S](implicit fold: FoldLeft[P, S, Join.Fold.type]) =
+      new Join[P, S] {
+        type Out = fold.Out
+        def apply(prefix: P, suffix: S): Out = fold(prefix, suffix)
+      }
   }
 }
-
