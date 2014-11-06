d6ad67150a16764e0f20d35e55db4e5350a0e961
Patrik Nordwall <patrik.nordwall@gmail.com>
2014-11-06 14:03:01 +0100

    =str Fix scaladoc warnings

--- akka-stream/src/main/scala/akka/stream/javadsl/Source.scala
+++ akka-stream/src/main/scala/akka/stream/javadsl/Source.scala
@@ -29,10 +29,6 @@ object Source {
   def adapt[O](source: scaladsl.Source[O]): Source[O] =
     new Source(source)
 
-  /** Adapt [[scaladsl.SourcePipe]] for use within JavaDSL */
-  def adapt[O](source: scaladsl.SourcePipe[O]): Source[O] =
-    new Source(source)
-
   /**
    * Create a `Source` with no elements, i.e. an empty stream that is completed immediately
    * for every connected `Sink`.
@@ -380,8 +376,8 @@ class Source[+Out](delegate: scaladsl.Source[Out]) {
 
   /**
    * Adds a fixed size buffer in the flow that allows to store elements from a faster upstream until it becomes full.
-   * Depending on the defined [[OverflowStrategy]] it might drop elements or backpressure the upstream if there is no
-   * space available
+   * Depending on the defined [[akka.stream.OverflowStrategy]] it might drop elements or backpressure the upstream if
+   * there is no space available
    *
    * @param size The size of the buffer in element count
    * @param overflowStrategy Strategy that is used when incoming elements cannot fit inside the buffer
@@ -397,7 +393,7 @@ class Source[+Out](delegate: scaladsl.Source[Out]) {
    * subscribers, the [[akka.stream.Transformer#isComplete]] predicate determines whether to end
    * stream processing at this point; in that case the upstream subscription is
    * canceled. Before signaling normal completion to the downstream subscribers,
-   * the [[akka.stream.Transformer#onComplete]] function is invoked to produce a (possibly empty)
+   * the [[akka.stream.Transformer#onTermination]] function is invoked to produce a (possibly empty)
    * sequence of elements in response to the end-of-stream event.
    *
    * [[akka.stream.Transformer#onError]] is called when failure is signaled from upstream.
@@ -424,7 +420,7 @@ class Source[+Out](delegate: scaladsl.Source[Out]) {
    * subscribers, the [[akka.stream.Transformer#isComplete]] predicate determines whether to end
    * stream processing at this point; in that case the upstream subscription is
    * canceled. Before signaling normal completion to the downstream subscribers,
-   * the [[akka.stream.Transformer#onComplete]] function is invoked to produce a (possibly empty)
+   * the [[akka.stream.Transformer#onTermination]] function is invoked to produce a (possibly empty)
    * sequence of elements in response to the end-of-stream event.
    *
    * [[akka.stream.Transformer#onError]] is called when failure is signaled from upstream.
