2bd1230ca90e6396d94980a364e46b5cc544f2cb
Viktor Klang <viktor.klang@gmail.com>
2014-11-20 21:59:33 +0100

    +str - adds ZipWith and makes Zip use that to create tuples/pairs

--- akka-stream/src/main/scala/akka/stream/scaladsl/OperationAttributes.scala
+++ akka-stream/src/main/scala/akka/stream/scaladsl/OperationAttributes.scala
@@ -18,13 +18,15 @@ case class OperationAttributes private (private val attributes: List[OperationAt
    * Adds given attributes to the end of these attributes.
    */
   def and(other: OperationAttributes): OperationAttributes = {
+    // FIXME should return `this` if other.attributes is empty
+    // FIXME should return `other` if this is `none`
     OperationAttributes(attributes ::: other.attributes)
   }
 
   private[akka] def nameLifted: Option[String] =
     attributes.collect {
       case Name(name) ⇒ name
-    }.reduceOption(_ + "-" + _)
+    }.reduceOption(_ + "-" + _) // FIXME don't do a double-traversal, use a fold instead
 
   private[akka] def name: String = nameLifted match {
     case Some(name) ⇒ name
@@ -36,7 +38,7 @@ case class OperationAttributes private (private val attributes: List[OperationAt
       case InputBuffer(initial, max) ⇒ (s: MaterializerSettings) ⇒ s.withInputBuffer(initial, max)
       case FanOutBuffer(initial, max) ⇒ (s: MaterializerSettings) ⇒ s.withFanOutBuffer(initial, max)
       case Dispatcher(dispatcher) ⇒ (s: MaterializerSettings) ⇒ s.withDispatcher(dispatcher)
-    }.reduceOption(_ andThen _).getOrElse(identity)
+    }.reduceOption(_ andThen _).getOrElse(identity) // FIXME is this the optimal way of encoding this?
 
   private[akka] def transform(node: AstNode): AstNode =
     if ((this eq OperationAttributes.none) || (this eq node.attributes)) node
@@ -49,8 +51,8 @@ case class OperationAttributes private (private val attributes: List[OperationAt
    *
    * https://github.com/akka/akka/issues/16392
    */
-  private[akka] def withoutName = this.copy(
-    attributes = attributes.filterNot {
+  private[akka] def withoutName = this.copy( // FIXME should return OperationAttributes.none if empty
+    attributes = attributes.filterNot { // FIXME should return the same instance if didn't have any Name
       case attr: Name ⇒ true
     })
 }
