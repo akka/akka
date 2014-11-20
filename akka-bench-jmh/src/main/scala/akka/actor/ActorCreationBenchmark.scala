398e6c235bdb79bcb23cc9a6e0d7d6b6d8b2a9c0
Konrad 'ktoso' Malawski <ktoso@typesafe.com>
2014-11-20 14:07:18 +0100

    =act #16344 ActorPath validation now without regex, safer and faster

---
+++ akka-bench-jmh/src/main/scala/akka/actor/ActorCreationBenchmark.scala
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2014 Typesafe Inc. <http://www.typesafe.com>
+ */
+package akka.actor
+
+import java.util.concurrent.TimeUnit
+
+import org.openjdk.jmh.annotations._
+
+/*
+regex checking:
+[info] a.a.ActorCreationBenchmark.synchronousStarting       ss    120000       28.285        0.481       us
+
+hand checking:
+[info] a.a.ActorCreationBenchmark.synchronousStarting       ss    120000       21.496        0.502       us
+
+
+*/
+@State(Scope.Benchmark)
+@BenchmarkMode(Array(Mode.SingleShotTime))
+@Fork(5)
+@Warmup(iterations = 1000)
+@Measurement(iterations = 4000)
+class ActorCreationBenchmark {
+  implicit val system: ActorSystem = ActorSystem()
+
+  final val props = Props[MyActor]
+
+  var i = 1
+  def name = {
+    i +=1
+    "some-rather-long-actor-name-actor-" + i
+  }
+
+  @TearDown(Level.Trial)
+  def shutdown() {
+    system.shutdown()
+    system.awaitTermination()
+  }
+
+  @Benchmark
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def synchronousStarting =
+    system.actorOf(props, name)
+}
+
+class MyActor extends Actor {
+  override def receive: Receive = {
+    case _ ⇒
+  }
+}
