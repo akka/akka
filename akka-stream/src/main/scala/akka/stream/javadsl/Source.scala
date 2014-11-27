16f53ea5f94ace4cabe895e327173283684b377c
Konrad 'ktoso' Malawski <ktoso@typesafe.com>
2014-11-27 10:55:40 +0100

    -str #16393 removes timerTransform mentions from scaladocs

--- akka-stream/src/main/scala/akka/stream/javadsl/Source.scala
+++ akka-stream/src/main/scala/akka/stream/javadsl/Source.scala
@@ -389,8 +389,6 @@ class Source[+Out](delegate: scaladsl.Source[Out]) {
    * Generic transformation of a stream with a custom processing [[akka.stream.stage.Stage]].
    * This operator makes it possible to extend the `Flow` API when there is no specialized
    * operator that performs the transformation.
-   *
-   * Note that you can use [[#timerTransform]] if you need support for scheduled events in the transformer.
    */
   def transform[U](name: String, mkStage: japi.Creator[Stage[Out, U]]): javadsl.Source[U] =
     new Source(delegate.transform(name, () ⇒ mkStage.create()))
