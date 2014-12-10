29d7a041f6eb5a47d835040bc92ef3cd2748593e
Johannes Rudolph <johannes.rudolph@gmail.com>
2014-12-10 16:55:50 +0100

    +htp #16516 rewrite Deflate/GzipDecompressor as StatefulStage to defuse zip bomb
    Also, the tests have been DRY'd up.

--- akka-http/src/main/scala/akka/http/coding/Decoder.scala
+++ akka-http/src/main/scala/akka/http/coding/Decoder.scala
@@ -6,6 +6,7 @@ package akka.http.coding
 
 import akka.http.model._
 import akka.http.util.StreamUtils
+import akka.stream.stage.Stage
 import akka.util.ByteString
 import headers.HttpEncoding
 import akka.stream.scaladsl.Flow
@@ -18,31 +19,38 @@ trait Decoder {
       decodeData(message).withHeaders(message.headers filterNot Encoder.isContentEncodingHeader)
     else message.self
 
-  def decodeData[T](t: T)(implicit mapper: DataMapper[T]): T =
-    mapper.transformDataBytes(t, newDecodeTransfomer)
+  def decodeData[T](t: T)(implicit mapper: DataMapper[T]): T = mapper.transformDataBytes(t, decoderFlow)
 
-  def decode(input: ByteString): ByteString = newDecompressor.decompressAndFinish(input)
+  def maxBytesPerChunk: Int
+  def withMaxBytesPerChunk(maxBytesPerChunk: Int): Decoder
 
-  def newDecompressor: Decompressor
-
-  def newDecodeTransfomer(): Flow[ByteString, ByteString] = {
-    val decompressor = newDecompressor
-
-    def decodeChunk(bytes: ByteString): ByteString = decompressor.decompress(bytes)
-    def finish(): ByteString = decompressor.finish()
-
-    StreamUtils.byteStringTransformer(decodeChunk, finish)
-  }
+  def decoderFlow: Flow[ByteString, ByteString]
+  def decode(input: ByteString): ByteString
+}
+object Decoder {
+  val MaxBytesPerChunkDefault: Int = 65536
 }
 
-/** A stateful object representing ongoing decompression. */
-abstract class Decompressor {
-  /** Decompress the buffer and return decompressed data. */
-  def decompress(input: ByteString): ByteString
+/** A decoder that is implemented in terms of a [[Stage]] */
+trait StreamDecoder extends Decoder { outer ⇒
+  protected def newDecompressorStage(maxBytesPerChunk: Int): () ⇒ Stage[ByteString, ByteString]
 
-  /** Flushes potential remaining data from any internal buffers and may report on truncation errors */
-  def finish(): ByteString
+  def maxBytesPerChunk: Int = Decoder.MaxBytesPerChunkDefault
+  def withMaxBytesPerChunk(newMaxBytesPerChunk: Int): Decoder =
+    new StreamDecoder {
+      def encoding: HttpEncoding = outer.encoding
+      override def maxBytesPerChunk: Int = newMaxBytesPerChunk
 
-  /** Combines decompress and finish */
-  def decompressAndFinish(input: ByteString): ByteString = decompress(input) ++ finish()
+      def newDecompressorStage(maxBytesPerChunk: Int): () ⇒ Stage[ByteString, ByteString] =
+        outer.newDecompressorStage(maxBytesPerChunk)
+    }
+
+  def decoderFlow: Flow[ByteString, ByteString] =
+    Flow[ByteString].transform(newDecompressorStage(maxBytesPerChunk))
+
+  def decode(input: ByteString): ByteString = decodeWithLimits(input)
+  def decodeWithLimits(input: ByteString, maxBytesSize: Int = Int.MaxValue, maxIterations: Int = 1000): ByteString =
+    StreamUtils.runStrict(input, decoderFlow, maxBytesSize, maxIterations).get.get
+  def decodeFromIterator(input: Iterator[ByteString], maxBytesSize: Int = Int.MaxValue, maxIterations: Int = 1000): ByteString =
+    StreamUtils.runStrict(input, decoderFlow, maxBytesSize, maxIterations).get.get
 }
