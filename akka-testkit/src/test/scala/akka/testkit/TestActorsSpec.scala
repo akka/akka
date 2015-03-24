96183821e53819abfa0d6eb85ea7bf49c3c1f765
Ekin Gecikligun <ekin@vngrs.com>
2015-03-24 18:39:43 +0200

    +tes #17023 Adding ForwardActor among test-kit actors

--- akka-testkit/src/test/scala/akka/testkit/TestActorsSpec.scala
+++ akka-testkit/src/test/scala/akka/testkit/TestActorsSpec.scala
@@ -5,7 +5,7 @@ package akka.testkit
 
 class TestActorsSpec extends AkkaSpec with ImplicitSender {
 
-  import TestActors.echoActorProps
+  import TestActors.{ echoActorProps, forwardActorProps }
 
   "A EchoActor" must {
     "send back messages unchanged" in {
@@ -13,6 +13,17 @@ class TestActorsSpec extends AkkaSpec with ImplicitSender {
       val echo = system.actorOf(echoActorProps)
 
       echo ! message
+
+      expectMsg(message)
+    }
+  }
+
+  "A ForwardActor" must {
+    "forward messages to target actor" in {
+      val message = "forward me"
+      val forward = system.actorOf(forwardActorProps(testActor))
+
+      forward ! message
 
       expectMsg(message)
     }
