91a4c6580cfd6394e49e8668cb2f0afc532a2b3a
Vikraman Choudhury <git@vikraman.org>
2015-04-11 20:51:16 +0530

    Add spec for extractUri directive

--- akka-http-tests/src/test/scala/akka/http/server/directives/BasicDirectivesSpec.scala
+++ akka-http-tests/src/test/scala/akka/http/server/directives/BasicDirectivesSpec.scala
@@ -5,6 +5,9 @@
 package akka.http.server
 package directives
 
+import akka.http.model._
+import headers._
+
 class BasicDirectivesSpec extends RoutingSpec {
 
   "The `mapUnmatchedPath` directive" should {
@@ -24,6 +27,24 @@ class BasicDirectivesSpec extends RoutingSpec {
           echoComplete
         }
       } ~> check { responseAs[String] shouldEqual "GET" }
+    }
+  }
+
+  "The `extractUri` directive" should {
+    "extract from the request url" in {
+      Get("https://example.com/foo") ~> {
+        extractUri { echoComplete }
+      } ~> check { responseAs[String] shouldEqual "https://example.com/foo" }
+    }
+    "extract from a X-Request-URI header" in {
+      Get() ~> addHeader(RawHeader("x-request-uri", "http://example.com/foo")) ~> {
+        extractUri { echoComplete }
+      } ~> check { responseAs[String] shouldEqual "http://example.com/" }
+    }
+    "extract from request path and a Host header" in {
+      Get("/foo/bar") ~> addHeader(`Host`("example.com")) ~> {
+        extractUri { echoComplete }
+      } ~> check { responseAs[String] shouldEqual "http://example.com/foo/bar" }
     }
   }
 }
