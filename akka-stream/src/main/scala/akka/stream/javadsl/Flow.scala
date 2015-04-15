fadcb33fccfaad4cef08e71d3af1b01909c33395
Konrad Malawski <konrad.malawski@typesafe.com>
2015-04-15 17:41:41 +0200

    =str #16608 adds scaladoc about null in col returned from mapConcat

--- akka-stream/src/main/scala/akka/stream/javadsl/Flow.scala
+++ akka-stream/src/main/scala/akka/stream/javadsl/Flow.scala
@@ -154,6 +154,9 @@ class Flow[-In, +Out, +Mat](delegate: scaladsl.Flow[In, Out, Mat]) extends Graph
   /**
    * Transform each input element into a sequence of output elements that is
    * then flattened into the output stream.
+   *
+   * The returned list MUST NOT contain `null` values,
+   * as they are illegal as stream elements - according to the Reactive Streams specification.
    */
   def mapConcat[T](f: japi.Function[Out, java.util.List[T]]): javadsl.Flow[In, T, Mat] =
     new Flow(delegate.mapConcat(elem ⇒ Util.immutableSeq(f.apply(elem))))
