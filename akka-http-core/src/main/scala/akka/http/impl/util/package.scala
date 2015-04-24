57a175d122d0ade3583ed98f37018b84f58a37a1
Mathias <mathias@decodified.com>
2015-04-24 16:33:46 +0200

    !ht* #17279 scala side: refactor package structure to new layout

--- akka-http-core/src/main/scala/akka/http/impl/util/package.scala
+++ akka-http-core/src/main/scala/akka/http/impl/util/package.scala
@@ -121,6 +121,15 @@ package object util {
   private[this] val _identityFunc: Any ⇒ Any = x ⇒ x
   /** Returns a constant identity function to avoid allocating the closure */
   private[http] def identityFunc[T]: T ⇒ T = _identityFunc.asInstanceOf[T ⇒ T]
+
+  private[http] def humanReadableByteCount(bytes: Long, si: Boolean): String = {
+    val unit = if (si) 1000 else 1024
+    if (bytes >= unit) {
+      val exp = (math.log(bytes) / math.log(unit)).toInt
+      val pre = if (si) "kMGTPE".charAt(exp - 1).toString else "KMGTPE".charAt(exp - 1).toString + 'i'
+      "%.1f %sB" format (bytes / math.pow(unit, exp), pre)
+    } else bytes.toString + "  B"
+  }
 }
 
 package util {
