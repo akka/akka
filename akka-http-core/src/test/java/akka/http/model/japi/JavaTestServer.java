0860187dfb2ce570b53b451bd55e0bbc34757490
Johannes Rudolph <johannes.rudolph@gmail.com>
2015-04-24 14:40:27 +0200

    +htc #16887 the Java side of the Websocket server-side API

--- akka-http-core/src/test/java/akka/http/model/japi/JavaTestServer.java
+++ akka-http-core/src/test/java/akka/http/model/japi/JavaTestServer.java
@@ -8,13 +8,19 @@ import akka.actor.ActorSystem;
 import akka.japi.function.Function;
 import akka.japi.function.Procedure;
 import akka.http.engine.server.ServerSettings;
+import akka.http.javadsl.model.ws.Message;
+import akka.http.javadsl.model.ws.TextMessage;
+import akka.http.javadsl.model.ws.Websocket;
+import akka.japi.JavaPartialFunction;
 import akka.stream.ActorFlowMaterializer;
 import akka.stream.FlowMaterializer;
+import akka.stream.javadsl.Flow;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 import scala.concurrent.Await;
 import scala.concurrent.Future;
 import scala.concurrent.duration.FiniteDuration;
+import scala.runtime.BoxedUnit;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -32,7 +38,13 @@ public class JavaTestServer {
                             new Function<HttpRequest, HttpResponse>() {
                                 public HttpResponse apply(HttpRequest request) throws Exception {
                                     System.out.println("Handling request to " + request.getUri());
-                                    return JavaApiTestCases.handleRequest(request);
+
+                                    if (request.getUri().path().equals("/"))
+                                        return Websocket.handleWebsocketRequestWith(request, echoMessages(), materializer);
+                                    else if (request.getUri().path().equals("/greeter"))
+                                        return Websocket.handleWebsocketRequestWith(request, greeter(), materializer);
+                                    else
+                                        return JavaApiTestCases.handleRequest(request);
                                 }
                             }, "localhost", 8080, materializer);
 
@@ -42,5 +54,30 @@ public class JavaTestServer {
         } finally {
             system.shutdown();
         }
+    }
+
+    public static Flow<Message, Message, BoxedUnit> echoMessages() {
+        return Flow.create(); // the identity operation
+    }
+
+    public static Flow<Message, Message, BoxedUnit> greeter() {
+        return
+            Flow.<Message>create()
+                .collect(new JavaPartialFunction<Message, Message>() {
+                    @Override
+                    public Message apply(Message msg, boolean isCheck) throws Exception {
+                        if (isCheck)
+                            if (msg.isText()) return null;
+                            else throw noMatch();
+                        else
+                            return handleTextMessage(msg.asTextMessage());
+                    }
+                });
+    }
+    public static TextMessage handleTextMessage(TextMessage msg) {
+        if (msg.isStrict())
+            return TextMessage.create("Hello "+msg.getStrictText());
+        else
+            return TextMessage.create(Source.single("Hello ").concat(msg.getStreamedText()));
     }
 }
