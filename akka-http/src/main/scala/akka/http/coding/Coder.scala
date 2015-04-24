dba609e6541b6f3c03e8d99dc19e14673b177cbd
Mathias <mathias@decodified.com>
2015-04-24 11:49:53 +0200

    =ht* #17279 rename akka-http-* modules where agreed

--- akka-http/src/main/scala/akka/http/coding/Coder.scala
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (C) 2009-2014 Typesafe Inc. <http://www.typesafe.com>
- */
-
-package akka.http.coding
-
-/** Marker trait for A combined Encoder and Decoder */
-trait Coder extends Encoder with Decoder
