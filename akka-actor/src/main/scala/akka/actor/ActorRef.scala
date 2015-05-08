0e8264856b37fffa82e1d9adb70350f3c7ec0435
Patrik Nordwall <patrik.nordwall@gmail.com>
2015-05-08 10:37:41 +0200

    !act #13919 Make ActorRef.isTerminated internal
    * until we have replaced all internal usages of it,
      or we could decide that it is good to keep as an
      internal facility and then we can remove the deprecation
      annotations

--- akka-actor/src/main/scala/akka/actor/ActorRef.scala
+++ akka-actor/src/main/scala/akka/actor/ActorRef.scala
@@ -127,11 +127,13 @@ abstract class ActorRef extends java.lang.Comparable[ActorRef] with Serializable
   def forward(message: Any)(implicit context: ActorContext) = tell(message, context.sender())
 
   /**
+   * INTERNAL API
    * Is the actor shut down?
    * The contract is that if this method returns true, then it will never be false again.
    * But you cannot rely on that it is alive if it returns false, since this by nature is a racy method.
    */
-  @deprecated("Use context.watch(actor) and receive Terminated(actor)", "2.2") def isTerminated: Boolean
+  @deprecated("Use context.watch(actor) and receive Terminated(actor)", "2.2")
+  private[akka] def isTerminated: Boolean
 
   final override def hashCode: Int = {
     if (path.uid == ActorCell.undefinedUid) path.hashCode
@@ -247,10 +249,10 @@ private[akka] abstract class InternalActorRef extends ActorRef with ScalaActorRe
   def isLocal: Boolean
 
   /**
-   * Returns “true” if the actor is locally known to be terminated, “false” if
+   * INTERNAL API: Returns “true” if the actor is locally known to be terminated, “false” if
    * alive or uncertain.
    */
-  def isTerminated: Boolean
+  private[akka] def isTerminated: Boolean
 }
 
 /**
@@ -298,7 +300,7 @@ private[akka] class LocalActorRef private[akka] (
   _mailboxType: MailboxType,
   _supervisor: InternalActorRef,
   override val path: ActorPath)
-  extends ActorRefWithCell with LocalRef {
+    extends ActorRefWithCell with LocalRef {
 
   /*
    * Safe publication of this class’s fields is guaranteed by mailbox.setActor()
@@ -319,11 +321,11 @@ private[akka] class LocalActorRef private[akka] (
   protected def actorContext: ActorContext = actorCell
 
   /**
-   * Is the actor terminated?
+   * INTERNAL API: Is the actor terminated?
    * If this method returns true, it will never return false again, but if it
    * returns false, you cannot be sure if it's alive still (race condition)
    */
-  override def isTerminated: Boolean = actorCell.isTerminated
+  override private[akka] def isTerminated: Boolean = actorCell.isTerminated
 
   /**
    * Starts the actor after initialization.
@@ -445,7 +447,8 @@ private[akka] trait MinimalActorRef extends InternalActorRef with LocalRef {
   override def suspend(): Unit = ()
   override def resume(causedByFailure: Throwable): Unit = ()
   override def stop(): Unit = ()
-  @deprecated("Use context.watch(actor) and receive Terminated(actor)", "2.2") override def isTerminated = false
+  @deprecated("Use context.watch(actor) and receive Terminated(actor)", "2.2")
+  override private[akka] def isTerminated = false
 
   override def !(message: Any)(implicit sender: ActorRef = Actor.noSender): Unit = ()
 
@@ -511,7 +514,8 @@ private[akka] class EmptyLocalActorRef(override val provider: ActorRefProvider,
                                        override val path: ActorPath,
                                        val eventStream: EventStream) extends MinimalActorRef {
 
-  @deprecated("Use context.watch(actor) and receive Terminated(actor)", "2.2") override def isTerminated = true
+  @deprecated("Use context.watch(actor) and receive Terminated(actor)", "2.2")
+  override private[akka] def isTerminated = true
 
   override def sendSystemMessage(message: SystemMessage): Unit = {
     if (Mailbox.debug) println(s"ELAR $path having enqueued $message")
@@ -589,10 +593,10 @@ private[akka] class DeadLetterActorRef(_provider: ActorRefProvider,
  * INTERNAL API
  */
 private[akka] class VirtualPathContainer(
-  override val provider: ActorRefProvider,
-  override val path: ActorPath,
-  override val getParent: InternalActorRef,
-  val log: LoggingAdapter) extends MinimalActorRef {
+    override val provider: ActorRefProvider,
+    override val path: ActorPath,
+    override val getParent: InternalActorRef,
+    val log: LoggingAdapter) extends MinimalActorRef {
 
   private val children = new ConcurrentHashMap[String, InternalActorRef]
 
