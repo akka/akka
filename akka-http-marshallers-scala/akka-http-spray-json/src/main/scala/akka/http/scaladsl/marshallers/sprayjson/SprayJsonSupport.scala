d3d297b19ff15854390493f55ee6aac450be14bf
Mathias <mathias@decodified.com>
2015-05-11 23:00:45 +0200

    !htp Change marshalling and unmarshalling infrastructure to use ExecutionContext from RequestContext
    ... instead of an ad-hoc implicit one.
    This has the advantages that implicit (un)marshaller construction doesn't require an implicit EC anymore (a point that often tripped up users because the errors are mostly unhelpful or even misleading) and the ExecutionContext for all of the routing DSL is now configured in one central point (when constructing the `RequestContext`).

--- akka-http-marshallers-scala/akka-http-spray-json/src/main/scala/akka/http/scaladsl/marshallers/sprayjson/SprayJsonSupport.scala
+++ akka-http-marshallers-scala/akka-http-spray-json/src/main/scala/akka/http/scaladsl/marshallers/sprayjson/SprayJsonSupport.scala
@@ -5,7 +5,6 @@
 package akka.http.scaladsl.marshallers.sprayjson
 
 import scala.language.implicitConversions
-import scala.concurrent.ExecutionContext
 import akka.stream.FlowMaterializer
 import akka.http.scaladsl.marshalling.{ ToEntityMarshaller, Marshaller }
 import akka.http.scaladsl.unmarshalling.{ FromEntityUnmarshaller, Unmarshaller }
@@ -17,11 +16,11 @@ import spray.json._
  * A trait providing automatic to and from JSON marshalling/unmarshalling using an in-scope *spray-json* protocol.
  */
 trait SprayJsonSupport {
-  implicit def sprayJsonUnmarshallerConverter[T](reader: RootJsonReader[T])(implicit ec: ExecutionContext, mat: FlowMaterializer): FromEntityUnmarshaller[T] =
-    sprayJsonUnmarshaller(reader, ec, mat)
-  implicit def sprayJsonUnmarshaller[T](implicit reader: RootJsonReader[T], ec: ExecutionContext, mat: FlowMaterializer): FromEntityUnmarshaller[T] =
+  implicit def sprayJsonUnmarshallerConverter[T](reader: RootJsonReader[T])(implicit mat: FlowMaterializer): FromEntityUnmarshaller[T] =
+    sprayJsonUnmarshaller(reader, mat)
+  implicit def sprayJsonUnmarshaller[T](implicit reader: RootJsonReader[T], mat: FlowMaterializer): FromEntityUnmarshaller[T] =
     sprayJsValueUnmarshaller.map(jsonReader[T].read)
-  implicit def sprayJsValueUnmarshaller(implicit ec: ExecutionContext, mat: FlowMaterializer): FromEntityUnmarshaller[JsValue] =
+  implicit def sprayJsValueUnmarshaller(implicit mat: FlowMaterializer): FromEntityUnmarshaller[JsValue] =
     Unmarshaller.byteStringUnmarshaller.forContentTypes(`application/json`).mapWithCharset { (data, charset) ⇒
       val input =
         if (charset == HttpCharsets.`UTF-8`) ParserInput(data.toArray)
@@ -29,11 +28,11 @@ trait SprayJsonSupport {
       JsonParser(input)
     }
 
-  implicit def sprayJsonMarshallerConverter[T](writer: RootJsonWriter[T])(implicit printer: JsonPrinter = PrettyPrinter, ec: ExecutionContext): ToEntityMarshaller[T] =
-    sprayJsonMarshaller[T](writer, printer, ec)
-  implicit def sprayJsonMarshaller[T](implicit writer: RootJsonWriter[T], printer: JsonPrinter = PrettyPrinter, ec: ExecutionContext): ToEntityMarshaller[T] =
-    sprayJsValueMarshaller[T].compose(writer.write)
-  implicit def sprayJsValueMarshaller[T](implicit writer: RootJsonWriter[T], printer: JsonPrinter = PrettyPrinter, ec: ExecutionContext): ToEntityMarshaller[JsValue] =
+  implicit def sprayJsonMarshallerConverter[T](writer: RootJsonWriter[T])(implicit printer: JsonPrinter = PrettyPrinter): ToEntityMarshaller[T] =
+    sprayJsonMarshaller[T](writer, printer)
+  implicit def sprayJsonMarshaller[T](implicit writer: RootJsonWriter[T], printer: JsonPrinter = PrettyPrinter): ToEntityMarshaller[T] =
+    sprayJsValueMarshaller[T] compose writer.write
+  implicit def sprayJsValueMarshaller[T](implicit writer: RootJsonWriter[T], printer: JsonPrinter = PrettyPrinter): ToEntityMarshaller[JsValue] =
     Marshaller.StringMarshaller.wrap(ContentTypes.`application/json`)(printer.apply)
 }
 object SprayJsonSupport extends SprayJsonSupport
