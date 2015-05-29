0800a7c1a63f1ae8335b90b6b14a8085e804aaa1
Patrik Nordwall <patrik.nordwall@gmail.com>
2015-05-29 07:57:14 +0200

    =rem #17567 Adjust parameters for DeadlineFailureDetector
    To be more aligned with PhiAccrualFailureDetector the
    DeadlineFailureDetector should trigger after
    heartbeat-interval + acceptable-heartbeat-pause

--- akka-remote/src/test/scala/akka/remote/DeadlineFailureDetectorSpec.scala
+++ akka-remote/src/test/scala/akka/remote/DeadlineFailureDetectorSpec.scala
@@ -26,11 +26,11 @@ class DeadlineFailureDetectorSpec extends AkkaSpec {
     def createFailureDetector(
       acceptableLostDuration: FiniteDuration,
       clock: Clock = FailureDetector.defaultClock) =
-      new DeadlineFailureDetector(acceptableLostDuration)(clock = clock)
+      new DeadlineFailureDetector(acceptableLostDuration, heartbeatInterval = 1.second)(clock = clock)
 
     "mark node as monitored after a series of successful heartbeats" in {
       val timeInterval = List[Long](0, 1000, 100, 100)
-      val fd = createFailureDetector(acceptableLostDuration = 5.seconds, clock = fakeTimeGenerator(timeInterval))
+      val fd = createFailureDetector(acceptableLostDuration = 4.seconds, clock = fakeTimeGenerator(timeInterval))
       fd.isMonitoring should ===(false)
 
       fd.heartbeat()
@@ -43,7 +43,7 @@ class DeadlineFailureDetectorSpec extends AkkaSpec {
 
     "mark node as dead if heartbeat are missed" in {
       val timeInterval = List[Long](0, 1000, 100, 100, 7000)
-      val fd = createFailureDetector(acceptableLostDuration = 5.seconds, clock = fakeTimeGenerator(timeInterval))
+      val fd = createFailureDetector(acceptableLostDuration = 4.seconds, clock = fakeTimeGenerator(timeInterval))
 
       fd.heartbeat() //0
       fd.heartbeat() //1000
@@ -57,7 +57,7 @@ class DeadlineFailureDetectorSpec extends AkkaSpec {
       // 1000 regular intervals, 5 minute pause, and then a short pause again that should trigger unreachable again
       val regularIntervals = 0L +: Vector.fill(999)(1000L)
       val timeIntervals = regularIntervals :+ (5 * 60 * 1000L) :+ 100L :+ 900L :+ 100L :+ 7000L :+ 100L :+ 900L :+ 100L :+ 900L
-      val fd = createFailureDetector(acceptableLostDuration = 7.seconds, clock = fakeTimeGenerator(timeIntervals))
+      val fd = createFailureDetector(acceptableLostDuration = 4.seconds, clock = fakeTimeGenerator(timeIntervals))
 
       for (_ ← 0 until 1000) fd.heartbeat()
       fd.isAvailable should ===(false) // after the long pause
@@ -73,7 +73,7 @@ class DeadlineFailureDetectorSpec extends AkkaSpec {
 
     "accept some configured missing heartbeats" in {
       val timeInterval = List[Long](0, 1000, 1000, 1000, 4000, 1000, 1000)
-      val fd = createFailureDetector(acceptableLostDuration = 5.seconds, clock = fakeTimeGenerator(timeInterval))
+      val fd = createFailureDetector(acceptableLostDuration = 4.seconds, clock = fakeTimeGenerator(timeInterval))
 
       fd.heartbeat()
       fd.heartbeat()
@@ -86,7 +86,7 @@ class DeadlineFailureDetectorSpec extends AkkaSpec {
 
     "fail after configured acceptable missing heartbeats" in {
       val timeInterval = List[Long](0, 1000, 1000, 1000, 1000, 1000, 500, 500, 5000)
-      val fd = createFailureDetector(acceptableLostDuration = 5.seconds, clock = fakeTimeGenerator(timeInterval))
+      val fd = createFailureDetector(acceptableLostDuration = 4.seconds, clock = fakeTimeGenerator(timeInterval))
 
       fd.heartbeat()
       fd.heartbeat()
