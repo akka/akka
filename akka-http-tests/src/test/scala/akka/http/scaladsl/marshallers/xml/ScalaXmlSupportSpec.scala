6d8b01defc9b6c4d55ee736591ba71ffad5e32ee
Johannes Rudolph <johannes.rudolph@gmail.com>
2015-06-05 17:05:27 +0200

    =htp #17067 port XXE fixes and tests from spray/spray#1024

--- akka-http-tests/src/test/scala/akka/http/scaladsl/marshallers/xml/ScalaXmlSupportSpec.scala
+++ akka-http-tests/src/test/scala/akka/http/scaladsl/marshallers/xml/ScalaXmlSupportSpec.scala
@@ -4,28 +4,102 @@
 
 package akka.http.scaladsl.marshallers.xml
 
+import java.io.File
+
+import akka.http.scaladsl.TestUtils
+import scala.concurrent.duration._
+import org.xml.sax.SAXParseException
+
+import scala.concurrent.{ Future, Await }
 import scala.xml.NodeSeq
-import org.scalatest.{ Matchers, WordSpec }
+import org.scalatest.{ Inside, FreeSpec, Matchers }
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.http.scaladsl.unmarshalling.{ Unmarshaller, Unmarshal }
 import akka.http.scaladsl.model._
 import HttpCharsets._
 import MediaTypes._
 
-class ScalaXmlSupportSpec extends WordSpec with Matchers with ScalatestRouteTest {
+class ScalaXmlSupportSpec extends FreeSpec with Matchers with ScalatestRouteTest with Inside {
   import ScalaXmlSupport._
 
-  "ScalaXmlSupport" should {
-    "NodeSeqMarshaller should marshal xml snippets to `text/xml` content in UTF-8" in {
+  "NodeSeqMarshaller should" - {
+    "marshal xml snippets to `text/xml` content in UTF-8" in {
       marshal(<employee><nr>Ha“llo</nr></employee>) shouldEqual
         HttpEntity(ContentType(`text/xml`, `UTF-8`), "<employee><nr>Ha“llo</nr></employee>")
     }
-    "nodeSeqUnmarshaller should unmarshal `text/xml` content in UTF-8 to NodeSeqs" in {
+    "unmarshal `text/xml` content in UTF-8 to NodeSeqs" in {
       Unmarshal(HttpEntity(`text/xml`, "<int>Hällö</int>")).to[NodeSeq].map(_.text) should evaluateTo("Hällö")
     }
-    "nodeSeqUnmarshaller should reject `application/octet-stream`" in {
+    "reject `application/octet-stream`" in {
       Unmarshal(HttpEntity(`application/octet-stream`, "<int>Hällö</int>")).to[NodeSeq].map(_.text) should
         haveFailedWith(Unmarshaller.UnsupportedContentTypeException(nodeSeqContentTypeRanges: _*))
+    }
+
+    "don't be vulnerable to XXE attacks" - {
+      "parse XML bodies without loading in a related schema" in {
+        withTempFile("I shouldn't be there!") { f ⇒
+          val xml = s"""<?xml version="1.0" encoding="ISO-8859-1"?>
+                     | <!DOCTYPE foo [
+                     |   <!ELEMENT foo ANY >
+                     |   <!ENTITY xxe SYSTEM "${f.toURI}">]><foo>hello&xxe;</foo>""".stripMargin
+
+          shouldHaveFailedWithSAXParseException(Unmarshal(HttpEntity(`text/xml`, xml)).to[NodeSeq])
+        }
+      }
+      "parse XML bodies without loading in a related schema from a parameter" in {
+        withTempFile("I shouldnt be there!") { generalEntityFile ⇒
+          withTempFile {
+            s"""<!ENTITY % xge SYSTEM "${generalEntityFile.toURI}">
+             |<!ENTITY % pe "<!ENTITY xxe '%xge;'>">""".stripMargin
+          } { parameterEntityFile ⇒
+            val xml = s"""<?xml version="1.0" encoding="ISO-8859-1"?>
+                       | <!DOCTYPE foo [
+                       |   <!ENTITY % xpe SYSTEM "${parameterEntityFile.toURI}">
+                       |   %xpe;
+                       |   %pe;
+                       |   ]><foo>hello&xxe;</foo>""".stripMargin
+            shouldHaveFailedWithSAXParseException(Unmarshal(HttpEntity(`text/xml`, xml)).to[NodeSeq])
+          }
+        }
+      }
+      "gracefully fail when there are too many nested entities" in {
+        val nested = for (x ← 1 to 30) yield "<!ENTITY laugh" + x + " \"&laugh" + (x - 1) + ";&laugh" + (x - 1) + ";\">"
+        val xml =
+          s"""<?xml version="1.0"?>
+           | <!DOCTYPE billion [
+           | <!ELEMENT billion (#PCDATA)>
+           | <!ENTITY laugh0 "ha">
+           | ${nested.mkString("\n")}
+           | ]>
+           | <billion>&laugh30;</billion>""".stripMargin
+
+        shouldHaveFailedWithSAXParseException(Unmarshal(HttpEntity(`text/xml`, xml)).to[NodeSeq])
+      }
+      "gracefully fail when an entity expands to be very large" in {
+        val as = "a" * 50000
+        val entities = "&a;" * 50000
+        val xml = s"""<?xml version="1.0"?>
+                  | <!DOCTYPE kaboom [
+                  | <!ENTITY a "$as">
+                  | ]>
+                  | <kaboom>$entities</kaboom>""".stripMargin
+        shouldHaveFailedWithSAXParseException(Unmarshal(HttpEntity(`text/xml`, xml)).to[NodeSeq])
+      }
+    }
+  }
+
+  def shouldHaveFailedWithSAXParseException(result: Future[NodeSeq]) =
+    inside(Await.result(result.failed, 1.second)) {
+      case _: SAXParseException ⇒
+    }
+
+  def withTempFile[T](content: String)(f: File ⇒ T): T = {
+    val file = File.createTempFile("xxe", ".txt")
+    try {
+      TestUtils.writeAllText(content, file)
+      f(file)
+    } finally {
+      file.delete()
     }
   }
 }
