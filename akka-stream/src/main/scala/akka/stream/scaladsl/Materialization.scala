3eae14aec723e3ec6b15948633f2bbcb6d580656
Endre Sándor Varga <endre.varga@typesafe.com>
2015-06-15 13:28:50 +0200

    +str #17702: Add Keep.none

--- akka-stream/src/main/scala/akka/stream/scaladsl/Materialization.scala
+++ akka-stream/src/main/scala/akka/stream/scaladsl/Materialization.scala
@@ -13,8 +13,10 @@ object Keep {
   private val _left = (l: Any, r: Any) ⇒ l
   private val _right = (l: Any, r: Any) ⇒ r
   private val _both = (l: Any, r: Any) ⇒ (l, r)
+  private val _none = (l: Any, r: Any) ⇒ ()
 
   def left[L, R]: (L, R) ⇒ L = _left.asInstanceOf[(L, R) ⇒ L]
   def right[L, R]: (L, R) ⇒ R = _right.asInstanceOf[(L, R) ⇒ R]
   def both[L, R]: (L, R) ⇒ (L, R) = _both.asInstanceOf[(L, R) ⇒ (L, R)]
+  def none[L, R]: (L, R) ⇒ Unit = _none.asInstanceOf[(L, R) ⇒ Unit]
 }
