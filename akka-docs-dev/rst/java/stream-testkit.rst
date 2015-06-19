7910e62a619f3296c9ed8b1a0fe66e83774f6aea
Martynas Mickevičius <mmartynas@gmail.com>
2015-06-19 14:42:43 +0300

    =doc #17291 java docs code

--- akka-docs-dev/rst/java/stream-testkit.rst
+++ akka-docs-dev/rst/java/stream-testkit.rst
@@ -4,22 +4,67 @@
 Testing streams
 ###############
 
-Akka Streams comes with an :mod:`akka-stream-testkit` module that provides tools which can be used for controlling and asserting various parts of the stream pipeline.
+Verifying behaviour of Akka Stream sources, flows and sinks can be done using various code patterns and libraries. Here we will discuss testing these elements using:
 
-Probe Sink
-==========
+- simple sources, sinks and flows;
+- sources and sinks in combination with :class:`TestProbe` from the :mod:`akka-testkit` module;
+- sources and sinks specifically crafted for writing tests from the :mod:`akka-stream-testkit` module.
 
-Using probe as a `Sink` allows manual control over demand and assertions over elements coming downstream. Streams testkit provides a sink that materializes to a :class:`TestSubscriber.Probe`.
+It is important to keep your data processing pipeline as separate sources, flows and sinks. This makes them easily testable by wiring them up to other sources or sinks, or some test harnesses that :mod:`akka-testkit` or :mod:`akka-stream-testkit` provide.
+
+Built in sources, sinks and combinators
+=======================================
+
+Testing a custom sink can be as simple as attaching a source that emits elements from a predefined collection, running a constructed test flow and asserting on the results that sink produced. Here is an example of a test for a sink:
+
+.. includecode:: ../../../akka-samples/akka-docs-java-lambda/src/test/java/docs/stream/StreamTestKitDocTest.java#strict-collection
+
+The same strategy can be applied for sources as well. In the next example we have a source that produces an infinite stream of elements. Such source can be tested by asserting that first arbitrary number of elements hold some condition. Here :code:`grouped` combinator and :code:`Sink.head` are very useful.
+
+.. includecode:: ../../../akka-samples/akka-docs-java-lambda/src/test/java/docs/stream/StreamTestKitDocTest.java#grouped-infinite
+
+When testing a flow we need to attach a source and a sink. As both stream ends are under our control, we can choose sources that tests various edge cases of the flow and sinks that eases assertions.
+
+.. includecode:: ../../../akka-samples/akka-docs-java-lambda/src/test/java/docs/stream/StreamTestKitDocTest.java#folded-stream
+
+TestKit
+=======
+
+Akka Stream offers integration with Actors out of the box. This support can be used for writing stream tests that use familiar :class:`TestProbe` from the :mod:`akka-testkit` API.
+
+One of the more straightforward tests would be to materialize stream to a :class:`Future` and then use :code:`pipe` pattern to pipe the result of that future to the probe.
+
+.. includecode:: ../../../akka-samples/akka-docs-java-lambda/src/test/java/docs/stream/StreamTestKitDocTest.java#pipeto-testprobe
+
+Instead of materializing to a future, we can use a :class:`Sink.actorRef` that sends all incoming elements to the given :class:`ActorRef`. Now we can use assertion methods on :class:`TestProbe` and expect elements one by one as they arrive. We can also assert stream completion by expecting for :code:`onCompleteMessage` which was given to :class:`Sink.actorRef`.
+
+.. includecode:: ../../../akka-samples/akka-docs-java-lambda/src/test/java/docs/stream/StreamTestKitDocTest.java#sink-actorref
+
+Similarly to :class:`Sink.actorRef` that provides control over received elements, we can use :class:`Source.actorRef` and have full control over elements to be sent.
+
+.. includecode:: ../../../akka-samples/akka-docs-java-lambda/src/test/java/docs/stream/StreamTestKitDocTest.java#source-actorref
+
+Streams TestKit
+===============
+
+You may have noticed various code patterns that emerge when testing stream pipelines. Akka Stream has a separate :mod:`akka-stream-testkit` module that provides tools specifically for writing stream tests. This module comes with two main components that are :class:`TestSource` and :class:`TestSink` which provide sources and sinks that materialize to probes that allow fluent API.
+
+.. note::
+
+   Be sure to add the module :mod:`akka-stream-testkit` to your dependencies.
+
+A sink returned by :code:`TestSink.probe` allows manual control over demand and assertions over elements coming downstream.
 
 .. includecode:: ../../../akka-samples/akka-docs-java-lambda/src/test/java/docs/stream/StreamTestKitDocTest.java#test-sink-probe
 
-Probe Source
-============
-
-A source that materializes to :class:`TestPublisher.Probe` can be used for asserting demand or controlling when stream is completed or ended with an error.
+A source returned by :code:`TestSource.probe` can be used for asserting demand or controlling when stream is completed or ended with an error.
 
 .. includecode:: ../../../akka-samples/akka-docs-java-lambda/src/test/java/docs/stream/StreamTestKitDocTest.java#test-source-probe
 
-*TODO*
+You can also inject exceptions and test sink behaviour on error conditions.
 
-List by example various operations on probes. Using probes without a sink.
+.. includecode:: ../../../akka-samples/akka-docs-java-lambda/src/test/java/docs/stream/StreamTestKitDocTest.java#injecting-failure
+
+Test source and sink can be used together in combination when testing flows.
+
+.. includecode:: ../../../akka-samples/akka-docs-java-lambda/src/test/java/docs/stream/StreamTestKitDocTest.java#test-source-and-sink
--- akka-samples/akka-docs-java-lambda/src/test/java/docs/stream/StreamTestKitDocTest.java
+++ akka-samples/akka-docs-java-lambda/src/test/java/docs/stream/StreamTestKitDocTest.java
@@ -4,14 +4,27 @@
 package docs.stream;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.*;
+import static org.junit.Assert.assertEquals;
 
 import akka.actor.*;
+import akka.dispatch.Futures;
 import akka.testkit.*;
+import akka.japi.Pair;
 import akka.stream.*;
 import akka.stream.javadsl.*;
 import akka.stream.testkit.*;
 import akka.stream.testkit.javadsl.*;
+import akka.testkit.TestProbe;
+import scala.util.*;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
+import scala.concurrent.duration.FiniteDuration;
 
 import scala.runtime.BoxedUnit;
 
@@ -33,24 +46,121 @@ public class StreamTestKitDocTest {
   final FlowMaterializer mat = ActorFlowMaterializer.create(system);
 
   @Test
-  public void demonstrateTestSourceProbe() {
+  public void strictCollection() throws Exception {
+    //#strict-collection
+    final Sink<Integer, Future<Integer>> sinkUnderTest = Flow.of(Integer.class)
+      .map(i -> i * 2)
+      .to(Sink.fold(0, (agg, next) -> agg + next), Keep.right());
 
-    //#test-source-probe
-    TestSource.<Integer>probe(system)
-      .to(Sink.cancelled(), Keep.left())
-      .run(mat)
-      .expectCancellation();
-    //#test-source-probe
+    final Future<Integer> future = Source.from(Arrays.asList(1, 2, 3, 4))
+      .runWith(sinkUnderTest, mat);
+    Await.ready(future, Duration.create(100, TimeUnit.MILLISECONDS));
+    final Integer result = future.value().get().get();
+    assert(result == 20);
+    //#strict-collection
   }
 
   @Test
-  public void demonstrateTestSinkProbe() {
+  public void groupedPartOfInfiniteStream() throws Exception {
+    //#grouped-infinite
+    final Source<Integer, BoxedUnit> sourceUnderTest = Source.repeat(1)
+      .map(i -> i * 2);
 
-    //#test-sink-probe
-    Source
+    final Future<List<Integer>> future = sourceUnderTest
+      .grouped(10)
+      .runWith(Sink.head(), mat);
+    Await.result(future, Duration.create(100, TimeUnit.MILLISECONDS));
+    final List<Integer> result = future.value().get().get();
+    assertEquals(result, Collections.nCopies(10, 2));
+    //#grouped-infinite
+  }
+
+  @Test
+  public void foldedStream() throws Exception {
+    //#folded-stream
+    final Flow<Integer, Integer, BoxedUnit> flowUnderTest = Flow.of(Integer.class)
+      .takeWhile(i -> i < 5);
+
+    final Future<Integer> future = Source.from(Arrays.asList(1, 2, 3, 4, 5, 6))
+      .via(flowUnderTest).runWith(Sink.fold(0, (agg, next) -> agg + next), mat);
+    Await.result(future, Duration.create(100, TimeUnit.MILLISECONDS));
+    final Integer result = future.value().get().get();
+    assert(result == 10);
+    //#folded-stream
+  }
+
+  @Test
+  public void pipeToTestProbe() throws Exception {
+    //#pipeto-testprobe
+    final Source<List<Integer>, BoxedUnit> sourceUnderTest = Source
       .from(Arrays.asList(1, 2, 3, 4))
+      .grouped(2);
+
+    final TestProbe probe = new TestProbe(system);
+    final Future<List<List<Integer>>> future = sourceUnderTest
+      .grouped(2)
+      .runWith(Sink.head(), mat);
+    akka.pattern.Patterns.pipe(future, system.dispatcher()).to(probe.ref());
+    probe.expectMsg(Duration.create(100, TimeUnit.MILLISECONDS),
+      Arrays.asList(Arrays.asList(1, 2), Arrays.asList(3, 4))
+    );
+    //#pipeto-testprobe
+  }
+
+  public enum Tick { TOCK, COMPLETED };
+
+  @Test
+  public void sinkActorRef() throws Exception {
+    //#sink-actorref
+    final Source<Tick, Cancellable> sourceUnderTest = Source.from(
+      FiniteDuration.create(0, TimeUnit.MILLISECONDS),
+      FiniteDuration.create(200, TimeUnit.MILLISECONDS),
+      Tick.TOCK);
+
+    final TestProbe probe = new TestProbe(system);
+    final Cancellable cancellable = sourceUnderTest
+      .to(Sink.actorRef(probe.ref(), Tick.COMPLETED)).run(mat);
+    probe.expectMsg(Duration.create(1, TimeUnit.SECONDS), Tick.TOCK);
+    probe.expectNoMsg(Duration.create(100, TimeUnit.MILLISECONDS));
+    probe.expectMsg(Duration.create(200, TimeUnit.MILLISECONDS), Tick.TOCK);
+    cancellable.cancel();
+    probe.expectMsg(Duration.create(200, TimeUnit.MILLISECONDS), Tick.COMPLETED);
+    //#sink-actorref
+  }
+
+  @Test
+  public void sourceActorRef() throws Exception {
+    //#source-actorref
+    final Sink<Integer, Future<String>> sinkUnderTest = Flow.of(Integer.class)
+      .map(i -> i.toString())
+      .to(Sink.fold("", (agg, next) -> agg + next), Keep.right());
+
+    final Pair<ActorRef, Future<String>> refAndFuture =
+      Source.<Integer>actorRef(8, OverflowStrategy.fail())
+        .to(sinkUnderTest, Keep.both())
+        .run(mat);
+    final ActorRef ref = refAndFuture.first();
+    final Future<String> future = refAndFuture.second();
+
+    ref.tell(1, ActorRef.noSender());
+    ref.tell(2, ActorRef.noSender());
+    ref.tell(3, ActorRef.noSender());
+    ref.tell(new akka.actor.Status.Success("done"), ActorRef.noSender());
+
+    Await.result(future, Duration.create(100, TimeUnit.MILLISECONDS));
+    final String result = future.value().get().get();
+    assertEquals(result, "123");
+    //#source-actorref
+  }
+
+  @Test
+  public void testSinkProbe() {
+    //#test-sink-probe
+    final Source<Integer, BoxedUnit> sourceUnderTest = Source.from(Arrays.asList(1, 2, 3, 4))
       .filter(elem -> elem % 2 == 0)
-      .map(elem -> elem * 2)
+      .map(elem -> elem * 2);
+
+    sourceUnderTest
       .runWith(TestSink.probe(system), mat)
       .request(2)
       .expectNext(4, 8)
@@ -58,4 +168,65 @@ public class StreamTestKitDocTest {
     //#test-sink-probe
   }
 
+  @Test
+  public void testSourceProbe() {
+    //#test-source-probe
+    final Sink<Integer, BoxedUnit> sinkUnderTest = Sink.cancelled();
+
+    TestSource.<Integer>probe(system)
+      .to(sinkUnderTest, Keep.left())
+      .run(mat)
+      .expectCancellation();
+    //#test-source-probe
+  }
+
+  @Test
+  public void injectingFailure() throws Exception {
+    //#injecting-failure
+    final Sink<Integer, Future<Integer>> sinkUnderTest = Sink.head();
+
+    final Pair<TestPublisher.Probe<Integer>, Future<Integer>> probeAndFuture =
+      TestSource.<Integer>probe(system)
+        .to(sinkUnderTest, Keep.both())
+        .run(mat);
+    final TestPublisher.Probe<Integer> probe = probeAndFuture.first();
+    final Future<Integer> future = probeAndFuture.second();
+    probe.sendError(new Exception("boom"));
+
+    Await.ready(future, Duration.create(100, TimeUnit.MILLISECONDS));
+    final Throwable exception = ((Failure)future.value().get()).exception();
+    assertEquals(exception.getMessage(), "boom");
+    //#injecting-failure
+  }
+
+  @Test
+  public void testSourceAndTestSink() throws Exception {
+    //#test-source-and-sink
+    final Flow<Integer, Integer, BoxedUnit> flowUnderTest = Flow.of(Integer.class)
+      .mapAsyncUnordered(2, sleep -> akka.pattern.Patterns.after(
+        Duration.create(10, TimeUnit.MILLISECONDS),
+        system.scheduler(),
+        system.dispatcher(),
+        Futures.successful(sleep)
+      ));
+
+    final Pair<TestPublisher.Probe<Integer>, TestSubscriber.Probe<Integer>> pubAndSub =
+      TestSource.<Integer>probe(system)
+        .via(flowUnderTest)
+        .to(TestSink.<Integer>probe(system), Keep.both())
+        .run(mat);
+    final TestPublisher.Probe<Integer> pub = pubAndSub.first();
+    final TestSubscriber.Probe<Integer> sub = pubAndSub.second();
+
+    sub.request(3);
+    pub.sendNext(3);
+    pub.sendNext(2);
+    pub.sendNext(1);
+    sub.expectNextUnordered(1, 2, 3);
+
+    pub.sendError(new Exception("Power surge in the linear subroutine C-47!"));
+    final Throwable ex = sub.expectError();
+    assert(ex.getMessage().contains("C-47"));
+    //#test-source-and-sink
+  }
 }
