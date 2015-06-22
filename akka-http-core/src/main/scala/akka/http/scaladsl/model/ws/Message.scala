f50a3ffc4b62d580d6527cfba201ddb7fd9d6f06
Johannes Rudolph <johannes.rudolph@gmail.com>
2015-06-22 08:58:45 +0200

    !htc remove `Streamed` subclasses to signify that streamed is the default and add convenience Message constructors

--- akka-http-core/src/main/scala/akka/http/scaladsl/model/ws/Message.scala
+++ akka-http-core/src/main/scala/akka/http/scaladsl/model/ws/Message.scala
@@ -8,22 +8,50 @@ import akka.stream.scaladsl.Source
 import akka.util.ByteString
 
 /**
- * The ADT for Websocket messages. A message can either be binary or a text message. Each of
- * those can either be strict or streamed.
+ * The ADT for Websocket messages. A message can either be a binary or a text message.
  */
 sealed trait Message
-sealed trait TextMessage extends Message
+
+/**
+ * A binary
+ */
+trait TextMessage extends Message {
+  /**
+   * The contents of this message as a stream.
+   */
+  def textStream: Source[String, _]
+}
 object TextMessage {
+  def apply(text: String): Strict = Strict(text)
+  def apply(textStream: Source[String, Any]): TextMessage =
+    Streamed(textStream)
+
+  /**
+   * A strict [[TextMessage]] that contains the complete data as a [[String]].
+   */
   final case class Strict(text: String) extends TextMessage {
+    def textStream: Source[String, _] = Source.single(text)
     override def toString: String = s"TextMessage.Strict($text)"
   }
-  final case class Streamed(textStream: Source[String, _]) extends TextMessage
+  final private case class Streamed(textStream: Source[String, _]) extends TextMessage
 }
-
-sealed trait BinaryMessage extends Message
+trait BinaryMessage extends Message {
+  /**
+   * The contents of this message as a stream.
+   */
+  def dataStream: Source[ByteString, _]
+}
 object BinaryMessage {
+  def apply(data: ByteString): Strict = Strict(data)
+  def apply(dataStream: Source[ByteString, Any]): BinaryMessage =
+    Streamed(dataStream)
+
+  /**
+   * A strict [[BinaryMessage]] that contains the complete data as a [[ByteString]].
+   */
   final case class Strict(data: ByteString) extends BinaryMessage {
+    def dataStream: Source[ByteString, _] = Source.single(data)
     override def toString: String = s"BinaryMessage.Strict($data)"
   }
-  final case class Streamed(dataStream: Source[ByteString, _]) extends BinaryMessage
+  final private case class Streamed(dataStream: Source[ByteString, _]) extends BinaryMessage
 }
