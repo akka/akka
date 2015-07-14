4f13be0589093e4add5e700cac3fc06b7c55c431
Johannes Rudolph <johannes.rudolph@gmail.com>
2015-07-14 14:42:34 +0200

    =htp #17984 #17985 fix ContentNegotiationSpec

--- akka-http-tests/src/test/scala/akka/http/scaladsl/marshalling/ContentNegotiationSpec.scala
+++ akka-http-tests/src/test/scala/akka/http/scaladsl/marshalling/ContentNegotiationSpec.scala
@@ -4,6 +4,9 @@
 
 package akka.http.scaladsl.marshalling
 
+import akka.http.scaladsl.model.MediaType.Encoding
+import akka.http.scaladsl.unmarshalling.FromResponseUnmarshaller
+
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import org.scalatest.{ Matchers, FreeSpec }
@@ -16,7 +19,7 @@ class ContentNegotiationSpec extends FreeSpec with Matchers {
   "Content Negotiation should work properly for requests with header(s)" - {
 
     "(without headers)" test { accept ⇒
-      accept(`text/plain`) should select(`text/plain`, `UTF-8`)
+      //accept(`text/plain`) should select(`text/plain`, `UTF-8`)
       accept(`text/plain` withCharset `UTF-16`) should select(`text/plain`, `UTF-16`)
     }
 
@@ -55,9 +58,7 @@ class ContentNegotiationSpec extends FreeSpec with Matchers {
 
     "manually created Accept-Charset: UTF-16" in testHeaders(headers.`Accept-Charset`(Vector(HttpCharsets.`UTF-16`.toRange))) { accept ⇒
       accept(`text/plain`) should select(`text/plain`, `UTF-16`)
-
-      // FIXME: reenable, currently fails because of #17984
-      // accept(`text/plain` withCharset `UTF-8`) should reject
+      accept(`text/plain` withCharset `UTF-8`) should reject
     }
 
     "Accept-Charset: UTF-16, UTF-8" test { accept ⇒
@@ -70,9 +71,9 @@ class ContentNegotiationSpec extends FreeSpec with Matchers {
       accept(`text/plain` withCharset `UTF-8`) should select(`text/plain`, `UTF-8`)
     }
 
-    "Accept-Charset: UTF-8;q=.2" test { accept ⇒
+    "Accept-Charset: ISO-8859-1;q=.2" test { accept ⇒
       accept(`text/plain`) should select(`text/plain`, `ISO-8859-1`)
-      accept(`text/plain` withCharset `UTF-8`) should select(`text/plain`, `UTF-8`)
+      accept(`text/plain` withCharset `UTF-8`) should reject
     }
 
     "Accept-Charset: latin1;q=.1, UTF-8;q=.2" test { accept ⇒
@@ -111,7 +112,7 @@ class ContentNegotiationSpec extends FreeSpec with Matchers {
       accept(`text/plain`, `text/html` withCharset `UTF-8`, `audio/ogg`) should select(`text/plain`, `UTF-16`)
       accept(`audio/ogg`, `application/javascript`, `text/plain` withCharset `UTF-8`) should select(`application/javascript`, `UTF-16`)
       accept(`image/gif`, `application/javascript`) should select(`application/javascript`, `UTF-16`)
-      accept(`image/gif`, `audio/ogg`) should select(`image/gif`, `UTF-16`)
+      accept(`image/gif`, `audio/ogg`) should select(`image/gif`)
     }
   }
 
@@ -119,11 +120,22 @@ class ContentNegotiationSpec extends FreeSpec with Matchers {
     val request = HttpRequest(headers = headers.toVector)
     body { contentTypes ⇒
       import scala.concurrent.ExecutionContext.Implicits.global
-      implicit val marshallers = contentTypes map {
-        case ct @ ContentType(mt, Some(cs)) ⇒ Marshaller.withFixedCharset(mt, cs)((s: String) ⇒ HttpEntity(ct, s))
-        case ContentType(mt, None)          ⇒ Marshaller.withOpenCharset(mt)((s: String, cs) ⇒ HttpEntity(ContentType(mt, cs), s))
-      }
-      Await.result(Marshal("foo").toResponseFor(request)
+
+      // creates a pseudo marshaller for X, that applies for all the given content types
+      trait X
+      object X extends X
+      implicit val marshallers: ToEntityMarshaller[X] =
+        Marshaller.oneOf(contentTypes map {
+          case ct @ ContentType(mt, Some(cs)) ⇒
+            Marshaller.withFixedCharset(mt, cs)((s: X) ⇒ HttpEntity(ct, "The X"))
+          case ContentType(mt, None) ⇒
+            mt.encoding match {
+              case Encoding.Open ⇒ Marshaller.withOpenCharset(mt)((s: X, cs) ⇒ HttpEntity(ContentType(mt, cs), "The X"))
+              case _             ⇒ Marshaller.withOpenCharset(mt)((s: X, _) ⇒ HttpEntity(ContentType(mt, None), "The X"))
+            }
+        }: _*)
+
+      Await.result(Marshal(X).toResponseFor(request)
         .fast.map(response ⇒ Some(response.entity.contentType))
         .fast.recover { case _: Marshal.UnacceptableResponseContentTypeException ⇒ None }, 1.second)
     }
@@ -131,6 +143,7 @@ class ContentNegotiationSpec extends FreeSpec with Matchers {
 
   def reject = equal(None)
   def select(mediaType: MediaType, charset: HttpCharset) = equal(Some(ContentType(mediaType, charset)))
+  def select(mediaType: MediaType) = equal(Some(ContentType(mediaType, None)))
 
   implicit class AddStringToIn(example: String) {
     def test(body: ((ContentType*) ⇒ Option[ContentType]) ⇒ Unit): Unit = example in {
