c401d8a2fe61823418896340615500b04e5ffe41
Johannes Rudolph <johannes.rudolph@gmail.com>
2015-07-14 12:21:54 +0200

    +htp #16439 allow creation of custom marshallers on the java side

--- akka-http/src/main/scala/akka/http/impl/server/Util.scala
+++ akka-http/src/main/scala/akka/http/impl/server/Util.scala
@@ -6,16 +6,24 @@ package akka.http.impl.server
 
 import akka.http.scaladsl.unmarshalling.{ Unmarshaller, FromStringUnmarshaller }
 import akka.http.scaladsl.util.FastFuture
-import akka.japi.function.Function
+import akka.japi.function
 
 import scala.concurrent.{ Future, ExecutionContext }
 import scala.util.Try
 
 object Util {
-  def fromStringUnmarshallerFromFunction[T](convert: Function[String, T]): FromStringUnmarshaller[T] =
+  def fromStringUnmarshallerFromFunction[T](convert: function.Function[String, T]): FromStringUnmarshaller[T] =
     scalaUnmarshallerFromFunction(convert)
-  def scalaUnmarshallerFromFunction[T, U](convert: Function[T, U]): Unmarshaller[T, U] =
+
+  def scalaUnmarshallerFromFunction[T, U](convert: function.Function[T, U]): Unmarshaller[T, U] =
     new Unmarshaller[T, U] {
       def apply(value: T)(implicit ec: ExecutionContext): Future[U] = FastFuture(Try(convert(value)))
     }
+
+  implicit class JApiFunctionAndThen[T, U](f1: function.Function[T, U]) {
+    def andThen[V](f2: U ⇒ V): function.Function[T, V] =
+      new function.Function[T, V] {
+        def apply(param: T): V = f2(f1(param))
+      }
+  }
 }
