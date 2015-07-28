64718c0013a1ccb0a869b632dc01568aef552117
Konrad Malawski <konrad.malawski@typesafe.com>
2015-07-28 14:15:23 +0200

    +per #18076 makes AtomicWrite more user friendly

---
+++ akka-persistence/src/test/scala/akka/persistence/AtomicWriteSpec.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2009-2015 Typesafe Inc. <http://www.typesafe.com>
+ */
+
+package akka.persistence
+
+import org.scalatest.{ Matchers, WordSpec }
+
+class AtomicWriteSpec extends WordSpec with Matchers {
+
+  "AtomicWrite" must {
+    "only contain messages for the same persistence id" in {
+      AtomicWrite(
+        PersistentRepr("", 1, "p1") ::
+          PersistentRepr("", 2, "p1") :: Nil).persistenceId should ===("p1")
+
+      intercept[IllegalArgumentException] {
+        AtomicWrite(
+          PersistentRepr("", 1, "p1") ::
+            PersistentRepr("", 2, "p1") ::
+            PersistentRepr("", 3, "p2") :: Nil)
+      }
+    }
+
+    "have highestSequenceNr" in {
+      AtomicWrite(
+        PersistentRepr("", 1, "p1") ::
+          PersistentRepr("", 2, "p1") ::
+          PersistentRepr("", 3, "p1") :: Nil).highestSequenceNr should ===(3)
+    }
+
+    "have lowestSequenceNr" in {
+      AtomicWrite(
+        PersistentRepr("", 2, "p1") ::
+          PersistentRepr("", 3, "p1") ::
+          PersistentRepr("", 4, "p1") :: Nil).lowestSequenceNr should ===(2)
+    }
+  }
+
+}
