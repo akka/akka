81058380f9fe81dcd994a89256996ef1ea192b98
Konrad Malawski <konrad.malawski@typesafe.com>
2015-07-28 14:15:23 +0200

    +per #18076 makes AtomicWrite more user friendly

--- akka-persistence/src/main/scala/akka/persistence/Persistent.scala
+++ akka-persistence/src/main/scala/akka/persistence/Persistent.scala
@@ -38,6 +38,20 @@ object AtomicWrite {
 }
 
 final case class AtomicWrite(payload: immutable.Seq[PersistentRepr]) extends PersistentEnvelope with Message {
+
+  // only check that all persistenceIds are equal when there's more than one in the Seq
+  if (payload match {
+    case l: List[PersistentRepr]   => l.tail.nonEmpty
+    case v: Vector[PersistentRepr] => v.size > 1
+    case _                         => true // some other collection type, let's just check
+  }) require(payload.forall(_.persistenceId == payload.head.persistenceId),
+      "AtomicWrite must contain messages for the same persistenceId, " +
+        s"yet different persistenceIds found: ${payload.map(_.persistenceId).toSet}")
+
+  def persistenceId = payload.head.persistenceId
+  def lowestSequenceNr = payload.head.sequenceNr // this assumes they're gapless; they should be (it is only our code creating AWs)
+  def highestSequenceNr = payload.last.sequenceNr // TODO: could be optimised, since above require traverses already
+
   override def sender: ActorRef = ActorRef.noSender
   override def size: Int = payload.size
 }
