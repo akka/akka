33e05ea59ac2c45bd7a6204b3269b0d37fa6e60e
Patrik Nordwall <patrik.nordwall@gmail.com>
2015-08-17 16:46:26 +0200

    =clt #18232 Serializer for ClusterSingleton

---
+++ akka-cluster-tools/src/test/scala/akka/cluster/singleton/protobuf/ClusterSingletonMessageSerializerSpec.scala
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) 2015 Typesafe Inc. <http://www.typesafe.com>
+ */
+package akka.cluster.singleton.protobuf
+
+import akka.actor.ExtendedActorSystem
+import akka.testkit.AkkaSpec
+import akka.cluster.singleton.ClusterSingletonManager.Internal.HandOverDone
+import akka.cluster.singleton.ClusterSingletonManager.Internal.HandOverInProgress
+import akka.cluster.singleton.ClusterSingletonManager.Internal.HandOverToMe
+import akka.cluster.singleton.ClusterSingletonManager.Internal.TakeOverFromMe
+
+class ClusterSingletonMessageSerializerSpec extends AkkaSpec {
+
+  val serializer = new ClusterSingletonMessageSerializer(system.asInstanceOf[ExtendedActorSystem])
+
+  def checkSerialization(obj: AnyRef): Unit = {
+    val blob = serializer.toBinary(obj)
+    val ref = serializer.fromBinary(blob, serializer.manifest(obj))
+    ref should ===(obj)
+  }
+
+  "ClusterSingletonMessages" must {
+
+    "be serializable" in {
+      checkSerialization(HandOverDone)
+      checkSerialization(HandOverInProgress)
+      checkSerialization(HandOverToMe)
+      checkSerialization(TakeOverFromMe)
+    }
+  }
+}
