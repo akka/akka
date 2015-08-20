f167301ead44b2a4660edd89be7ffd88f53ea5f7
Alexander Golubev <alexander.golubev@gmail.com>
2015-08-19 23:04:20 -0400

    +str #17693 add Source.queue and Sink.queue

---
+++ akka-stream/src/main/scala/akka/stream/impl/AcknowledgePublisher.scala
@@ -0,0 +1,86 @@
+/**
+ * Copyright (C) 2015 Typesafe Inc. <http://www.typesafe.com>
+ */
+package akka.stream.impl
+
+import akka.actor.{ ActorRef, Props }
+import akka.stream.OverflowStrategy
+import akka.stream.OverflowStrategy._
+import akka.stream.actor.ActorPublisherMessage.Request
+import akka.stream.impl.AcknowledgePublisher.{ Rejected, Ok }
+
+/**
+ * INTERNAL API
+ */
+private[akka] object AcknowledgePublisher {
+  def props(bufferSize: Int, overflowStrategy: OverflowStrategy) =
+    Props(new AcknowledgePublisher(bufferSize, overflowStrategy))
+
+  case class Ok()
+  case class Rejected()
+}
+
+/**
+ * INTERNAL API
+ */
+private[akka] class AcknowledgePublisher(bufferSize: Int, overflowStrategy: OverflowStrategy)
+  extends ActorRefSourceActor(bufferSize, overflowStrategy) {
+
+  var backpressedElem: Option[ActorRef] = None
+
+  override def requestElem: Receive = {
+    case _: Request ⇒
+      // totalDemand is tracked by super
+      if (bufferSize != 0)
+        while (totalDemand > 0L && !buffer.isEmpty) {
+          //if buffer is full - sent ack message to sender in case of Backpressure mode
+          if (buffer.isFull) backpressedElem match {
+            case Some(ref) ⇒
+              ref ! Ok(); backpressedElem = None
+            case None ⇒ //do nothing
+          }
+          onNext(buffer.dequeue())
+        }
+  }
+
+  override def receiveElem: Receive = {
+    case elem if isActive ⇒
+      if (totalDemand > 0L) {
+        onNext(elem)
+        sendAck(true)
+      } else if (bufferSize == 0) {
+        log.debug("Dropping element because there is no downstream demand: [{}]", elem)
+        sendAck(false)
+      } else if (!buffer.isFull)
+        enqueueAndSendAck(elem)
+      else overflowStrategy match {
+        case DropHead ⇒
+          buffer.dropHead()
+          enqueueAndSendAck(elem)
+        case DropTail ⇒
+          buffer.dropTail()
+          enqueueAndSendAck(elem)
+        case DropBuffer ⇒
+          buffer.clear()
+          enqueueAndSendAck(elem)
+        case DropNew ⇒
+          sendAck(false)
+        case Fail ⇒
+          onErrorThenStop(new Fail.BufferOverflowException(s"Buffer overflow (max capacity was: $bufferSize)!"))
+        case Backpressure ⇒
+          sendAck(false) //does not allow to send more than buffer size
+      }
+  }
+
+  def enqueueAndSendAck(elem: Any): Unit = {
+    buffer.enqueue(elem)
+    if (buffer.isFull && overflowStrategy == Backpressure) backpressedElem = Some(sender)
+    else sendAck(true)
+  }
+
+  def sendAck(isOk: Boolean): Unit = {
+    val msg = if (isOk) Ok() else Rejected()
+    context.sender() ! msg
+  }
+
+}
