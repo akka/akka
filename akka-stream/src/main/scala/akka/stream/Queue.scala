48f99ae4d98268bce68abde2a7b4b1ebc6ce0f06
Alexander Golubev <alexander.golubev@gmail.com>
2015-08-19 23:04:20 -0400

    +str #17693 add Source.queue and Sink.queue

---
+++ akka-stream/src/main/scala/akka/stream/Queue.scala
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) 2015 Typesafe Inc. <http://www.typesafe.com>
+ */
+package akka.stream
+
+import scala.concurrent.Future
+
+/**
+ * This trait allows to have the queue as a data source for some stream.
+ */
+trait SourceQueue[T] {
+
+  /**
+   * Method offers next element to a stream and returns future that:
+   * - competes with true if element is consumed by a stream
+   * - competes with false when stream dropped offered element
+   * - fails if stream is completed or cancelled.
+   *
+   * @param elem element to send to a stream
+   */
+  def offer(elem: T): Future[Boolean]
+}
+
+/**
+ * Trait allows to have the queue as a sink for some stream.
+ * "SinkQueue" pulls data from stream with backpressure mechanism.
+ */
+trait SinkQueue[T] {
+
+  /**
+   * Method pulls elements from stream and returns future that:
+   * - fails if stream is finished
+   * - completes with None in case if stream is completed after we got future
+   * - completes with `Some(element)` in case next element is available from stream.
+   */
+  def pull(): Future[Option[T]]
+}
