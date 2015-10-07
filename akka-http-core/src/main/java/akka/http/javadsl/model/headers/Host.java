3081893bfd2f6504aa853c68a765e6b2fc59bd76
Patrik Nordwall <patrik.nordwall@gmail.com>
2015-10-07 12:07:26 +0200

    =doc #18657 Doc host and path directives
    * also including docs of RequestVals.matchAndExtractHost
      and RequestVals.matchAndExtractHost
    * and add Host.create factory methods
    * add missing HttpRequest PATCH and OPTIONS
    * change to val in matchAndExtractHost for
      fail fast exception

--- akka-http-core/src/main/java/akka/http/javadsl/model/headers/Host.java
+++ akka-http-core/src/main/java/akka/http/javadsl/model/headers/Host.java
@@ -4,7 +4,22 @@
 
 package akka.http.javadsl.model.headers;
 
+import java.net.InetSocketAddress;
+
 public abstract class Host extends akka.http.scaladsl.model.HttpHeader {
+  
+    public static Host create(InetSocketAddress address) {
+        return akka.http.scaladsl.model.headers.Host.apply(address);
+    }
+    
+    public static Host create(String host) {
+        return akka.http.scaladsl.model.headers.Host.apply(host);
+    }
+    
+    public static Host create(String host, int port) {
+      return akka.http.scaladsl.model.headers.Host.apply(host, port);
+    }
+  
     public abstract akka.http.javadsl.model.Host host();
     public abstract int port();
 }
