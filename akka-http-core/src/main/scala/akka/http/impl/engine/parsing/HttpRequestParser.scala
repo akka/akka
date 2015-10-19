7b9f7e6a0804b10505fded18d15997fbcfca2506
Johannes Rudolph <johannes.rudolph@gmail.com>
2015-10-19 09:08:36 +0200

    =htc server side handshake cleanup and clarifications

--- akka-http-core/src/main/scala/akka/http/impl/engine/parsing/HttpRequestParser.scala
+++ akka-http-core/src/main/scala/akka/http/impl/engine/parsing/HttpRequestParser.scala
@@ -129,7 +129,7 @@ private[http] class HttpRequestParser(_settings: ParserSettings,
 
         val allHeaders =
           if (method == HttpMethods.GET) {
-            Handshake.Server.isWebsocketUpgrade(headers, hostHeaderPresent) match {
+            Handshake.Server.websocketUpgrade(headers, hostHeaderPresent) match {
               case Some(upgrade) ⇒ upgrade :: allHeaders0
               case None          ⇒ allHeaders0
             }
