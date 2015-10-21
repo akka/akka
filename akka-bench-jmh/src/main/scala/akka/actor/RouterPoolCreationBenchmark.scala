99828d41482d1c7426cb46bae7d77347c97b00c3
Patrik Nordwall <patrik.nordwall@gmail.com>
2015-10-21 14:47:49 +0200

    =act #18613 Optimize RepointableActorRef for sys msg
    * Before the cell is started the RepointableActorRef queue the messages
      (both system and non-system messages) into a queue. System messages
      are inserted right before the last non-system message by iterating
      the queue, i.e. becomes slow when there are many system messages already
      enqueued.
    * This is a problem for pools with many routees.
    * The fix is to keep system messages in a separate queue
    
    JMH benchmark before:
    
        [info] Benchmark                                 (size)  Mode  Cnt       Score      Error  Units
        [info] RouterPoolCreationBenchmark.testCreation    1000    ss  300   13204.048 ± 3081.576  us/op
        [info] RouterPoolCreationBenchmark.testCreation    2000    ss  300   41939.524 ± 6178.087  us/op
        [info] RouterPoolCreationBenchmark.testCreation    3000    ss  300   70752.881 ± 4344.992  us/op
        [info] RouterPoolCreationBenchmark.testCreation    4000    ss  300  120620.885 ± 3296.342  us/op
    
    JMH benchmark after:
    
        [info] Benchmark                                 (size)  Mode  Cnt      Score      Error  Units
        [info] RouterPoolCreationBenchmark.testCreation    1000    ss  300   7738.721 ± 1806.297  us/op
        [info] RouterPoolCreationBenchmark.testCreation    2000    ss  300  15497.588 ± 4532.852  us/op
        [info] RouterPoolCreationBenchmark.testCreation    3000    ss  300  28704.005 ± 6322.458  us/op
        [info] RouterPoolCreationBenchmark.testCreation    4000    ss  300  37783.516 ± 6778.437  us/op
    
    fully drain sysmsgQueue when swap cell

---
+++ akka-bench-jmh/src/main/scala/akka/actor/RouterPoolCreationBenchmark.scala
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2015 Typesafe Inc. <http://www.typesafe.com>
+ */
+package akka.actor
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import akka.routing.RoundRobinPool
+import akka.testkit.TestActors
+import akka.testkit.TestProbe
+import org.openjdk.jmh.annotations._
+import java.util.concurrent.TimeUnit
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Array(Mode.SingleShotTime))
+@Fork(3)
+@Warmup(iterations = 20)
+@Measurement(iterations = 100)
+class RouterPoolCreationBenchmark {
+  implicit val system: ActorSystem = ActorSystem()
+  val probe = TestProbe()
+
+  Props[TestActors.EchoActor]
+
+  @Param(Array("1000", "2000", "3000", "4000"))
+  var size = 0
+
+  @TearDown(Level.Trial)
+  def shutdown() {
+    system.terminate()
+    Await.ready(system.whenTerminated, 15.seconds)
+  }
+
+  @Benchmark
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def testCreation: Boolean = {
+    val pool = system.actorOf(RoundRobinPool(size).props(TestActors.echoActorProps))
+    pool.tell("hello", probe.ref)
+    probe.expectMsg(5.seconds, "hello")
+    true
+  }
+}
+
