467ef94e12b36d974e08f4c156d03a27a278c519
Michal Lacko <michal@springdom.com>
2015-10-30 14:51:56 +1300

    +cam #11214 add attachments to CamelMessage
    add support for attachments in CamelMessage, taken from JCamelMessage
    
    add a extension class CamelMessageWithAttachments base on CamelMessage that contains attachments based on JCamelMessage
    
    add newline CamelMessageTest
    
    replace for-cycle attachment adding with one putAll
    
    remove CamelMessageWithAttachments and update CamelMessage to contain attachments
    
    fix the binary compatibility issues on CamelMessage
    
    add documentation for new methods in CamelMessage, add return type for method withBody

--- akka-camel/src/test/scala/akka/camel/CamelMessageTest.scala
+++ akka-camel/src/test/scala/akka/camel/CamelMessageTest.scala
@@ -4,6 +4,9 @@
 
 package akka.camel
 
+import java.net.URL
+import javax.activation.DataHandler
+
 import org.apache.camel.impl.{ DefaultExchange, DefaultMessage }
 import akka.camel.TestSupport.SharedCamelSystem
 import org.scalatest.Matchers
@@ -12,22 +15,23 @@ import org.scalatest.WordSpecLike
 //TODO merge it with MessageScalaTest
 class CamelMessageTest extends Matchers with WordSpecLike with SharedCamelSystem {
 
-  "CamelMessage" must {
+  "CamelMessage copyContent" must {
+    "create a new CamelMessage with additional headers, attachments and new body" in {
+      val attachment = new DataHandler(new URL("https://foo.bar"))
+      val message = new DefaultMessage
+      message.setBody("test")
+      message.setHeader("foo", "bar")
+      message.addAttachment("foo", attachment)
+      message.setExchange(new DefaultExchange(camel.context))
 
-    "overwrite body and add header" in {
-      val msg = sampleMessage
-      CamelMessage.copyContent(CamelMessage("blah", Map("key" -> "baz")), msg)
-      assert(msg.getBody === "blah")
-      assert(msg.getHeader("foo") === "bar")
-      assert(msg.getHeader("key") === "baz")
+      val attachmentToAdd = new DataHandler(new URL("https://another.url"))
+      CamelMessage.copyContent(new CamelMessage("body", Map("key" -> "baz"), Map("key" -> attachmentToAdd)), message)
+
+      assert(message.getBody === "body")
+      assert(message.getHeader("foo") === "bar")
+      assert(message.getHeader("key") === "baz")
+      assert(message.getAttachment("key") === attachmentToAdd)
+      assert(message.getAttachment("foo") === attachment)
     }
-  }
-
-  private[camel] def sampleMessage = {
-    val message = new DefaultMessage
-    message.setBody("test")
-    message.setHeader("foo", "bar")
-    message.setExchange(new DefaultExchange(camel.context))
-    message
   }
 }
