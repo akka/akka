faba9e35dbe387dbe831e0ec2ed125f5f99915b6
Mathias <mathias@decodified.com>
2015-11-11 21:35:16 +0100

    +htp #16468 add `withSizeLimit` modifier to `HttpEntity` to flexibilize entity size verification

--- akka-http-core/src/test/scala/akka/http/impl/engine/server/HttpServerTestSetupBase.scala
+++ akka-http-core/src/test/scala/akka/http/impl/engine/server/HttpServerTestSetupBase.scala
@@ -5,23 +5,16 @@
 package akka.http.impl.engine.server
 
 import java.net.InetSocketAddress
-
 import akka.http.impl.engine.ws.ByteStringSinkProbe
 import akka.stream.io.{ SendBytes, SslTlsOutbound, SessionBytes }
-
 import scala.concurrent.duration.FiniteDuration
-import scala.concurrent.duration._
-
 import akka.actor.ActorSystem
 import akka.event.NoLogging
 import akka.util.ByteString
-
 import akka.stream.{ ClosedShape, Materializer }
 import akka.stream.scaladsl._
 import akka.stream.testkit.{ TestPublisher, TestSubscriber }
-
 import akka.http.impl.util._
-
 import akka.http.ServerSettings
 import akka.http.scaladsl.model.headers.{ ProductVersion, Server }
 import akka.http.scaladsl.model.{ HttpResponse, HttpRequest }
@@ -56,7 +49,7 @@ abstract class HttpServerTestSetupBase {
 
   def expectResponseWithWipedDate(expected: String): Unit = {
     val trimmed = expected.stripMarginWithNewline("\r\n")
-    // XXXX = 4 bytes, ISO Date Time String = 29 bytes => need to request 15 bytes more than expected string
+    // XXXX = 4 bytes, ISO Date Time String = 29 bytes => need to request 25 bytes more than expected string
     val expectedSize = ByteString(trimmed, "utf8").length + 25
     val received = wipeDate(netOut.expectBytes(expectedSize).utf8String)
     assert(received == trimmed, s"Expected request '$trimmed' but got '$received'")
