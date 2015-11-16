3f3642337eeaf90c66e550ff6848723ec3527145
Patrik Nordwall <patrik.nordwall@gmail.com>
2015-11-16 17:42:24 +0100

    +act #18487 Support custom supervision strategy in BackoffSupervisor
    * and forward messages from child to parent
      (needed for sharding passivation)

--- akka-actor/src/main/scala/akka/actor/AbstractFSM.scala
+++ akka-actor/src/main/scala/akka/actor/AbstractFSM.scala
@@ -112,7 +112,7 @@ abstract class AbstractFSM[S, D] extends FSM[S, D] {
    * called, not only the first one matching.</b>
    */
   final def onTransition(transitionHandler: UnitApply2[S, S]): Unit =
-    onTransition(transitionHandler(_: S, _: S)) 
+    onTransition(transitionHandler(_: S, _: S))
 
   /**
    * Set handler which is called upon reception of unhandled messages. Calling
