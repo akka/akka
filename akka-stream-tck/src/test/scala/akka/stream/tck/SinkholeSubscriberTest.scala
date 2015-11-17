75b112e0eea0f033ac10f5033f7a5f4a2af83b2b
Viktor Klang <viktor.klang@gmail.com>
2015-11-18 00:09:04 +0100

    +str - Adds Sink.last and Sink.lastOption to mirror Sink.head and Sink.headOption
    * Renames BlackholeSubscriber to SinkholeSunbscriber
    * Makes SinkholeSubscriber request Long.MaxValue
    * SinkholeSink seems like the best name ever

---
+++ akka-stream-tck/src/test/scala/akka/stream/tck/SinkholeSubscriberTest.scala
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C) 2014 Typesafe Inc. <http://www.typesafe.com>
+ */
+package akka.stream.tck
+
+import akka.stream.impl.SinkholeSubscriber
+import org.reactivestreams.tck.{ TestEnvironment, SubscriberWhiteboxVerification }
+import org.reactivestreams.tck.SubscriberWhiteboxVerification.{ SubscriberPuppet, WhiteboxSubscriberProbe }
+import org.scalatest.testng.{ TestNGSuiteLike }
+import java.lang.{ Integer ⇒ JInt }
+import scala.concurrent.Promise
+import org.reactivestreams.{ Subscription, Subscriber }
+
+class SinkholeSubscriberTest extends SubscriberWhiteboxVerification[JInt](new TestEnvironment()) with TestNGSuiteLike {
+  override def createSubscriber(probe: WhiteboxSubscriberProbe[JInt]): Subscriber[JInt] = {
+    new Subscriber[JInt] {
+      val hole = new SinkholeSubscriber[JInt](Promise[Unit]())
+
+      override def onError(t: Throwable): Unit = {
+        hole.onError(t)
+        probe.registerOnError(t)
+      }
+
+      override def onSubscribe(s: Subscription): Unit = {
+        probe.registerOnSubscribe(new SubscriberPuppet() {
+          override def triggerRequest(elements: Long): Unit = s.request(elements)
+          override def signalCancel(): Unit = s.cancel()
+        })
+        hole.onSubscribe(s)
+      }
+
+      override def onComplete(): Unit = {
+        hole.onComplete()
+        probe.registerOnComplete()
+      }
+
+      override def onNext(t: JInt): Unit = {
+        hole.onNext(t)
+        probe.registerOnNext(t)
+      }
+    }
+  }
+
+  override def createElement(element: Int): JInt = element
+}
+
