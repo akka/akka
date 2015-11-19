79b9d120a035503f704ac87385710ec30488c432
Konrad Malawski <konrad.malawski@typesafe.com>
2015-11-19 17:19:45 +0100

    =htp Fixes early error also being signalled as error via event

--- akka-http-tests/src/test/scala/akka/http/scaladsl/TestUtils.scala
+++ akka-http-tests/src/test/scala/akka/http/scaladsl/TestUtils.scala
@@ -5,6 +5,8 @@
 package akka.http.scaladsl
 
 import java.io.{ FileOutputStream, File }
+import java.net.InetSocketAddress
+import java.nio.channels.ServerSocketChannel
 
 object TestUtils {
   def writeAllText(text: String, file: File): Unit = {
@@ -12,5 +14,21 @@ object TestUtils {
     try {
       fos.write(text.getBytes("UTF-8"))
     } finally fos.close()
+  }
+
+  // TODO duplicated code from akka-http-core-tests
+  def temporaryServerAddress(interface: String = "127.0.0.1"): InetSocketAddress = {
+    val serverSocket = ServerSocketChannel.open()
+    try {
+      serverSocket.socket.bind(new InetSocketAddress(interface, 0))
+      val port = serverSocket.socket.getLocalPort
+      new InetSocketAddress(interface, port)
+    } finally serverSocket.close()
+  }
+
+  // TODO duplicated code from akka-http-core-tests
+  def temporaryServerHostnameAndPort(interface: String = "127.0.0.1"): (InetSocketAddress, String, Int) = {
+    val socketAddress = temporaryServerAddress(interface)
+    (socketAddress, socketAddress.getHostName, socketAddress.getPort)
   }
 }
