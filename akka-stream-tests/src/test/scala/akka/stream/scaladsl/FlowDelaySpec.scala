b200ded859cdfb5199e387fcb3d2a9b1776998c5
Alexander Golubev <alexander.golubev@gmail.com>
2015-11-25 21:29:35 -0500

    +str #18556 add delay combinator

--- akka-stream-tests/src/test/scala/akka/stream/scaladsl/FlowDelaySpec.scala
+++ akka-stream-tests/src/test/scala/akka/stream/scaladsl/FlowDelaySpec.scala
@@ -3,40 +3,116 @@
  */
 package akka.stream.scaladsl
 
-import akka.stream.ActorMaterializer
+import akka.stream.Attributes._
+import akka.stream.testkit.Utils._
 import akka.stream.testkit.scaladsl.TestSink
 import akka.stream.testkit.{ AkkaSpec, TestPublisher, TestSubscriber }
-import akka.stream.testkit.Utils._
+import akka.stream.{ DelayOverflowStrategy, ActorMaterializer }
 
+import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.util.control.NoStackTrace
 
 class FlowDelaySpec extends AkkaSpec {
 
   implicit val materializer = ActorMaterializer()
 
   "A Delay" must {
+    "deliver elements with some time shift" in {
+      Await.result(
+        Source(1 to 10).delay(1.seconds).grouped(100).runWith(Sink.head),
+        1200.millis) should ===(1 to 10)
+    }
 
-    "deliver element after time passed" in {
+    "deliver element after time passed from actual receiving element" in {
       Source(1 to 3).delay(300.millis).runWith(TestSink.probe[Int])
-        .request(3)
-        .expectNoMsg(100.millis)
-        .expectNext(1)
-        .expectNoMsg(100.millis)
-        .expectNext(2)
-        .expectNoMsg(100.millis)
-        .expectNext(3)
+        .request(2)
+        .expectNoMsg(200.millis) //delay
+        .expectNext(200.millis, 1) //delayed element
+        .expectNext(100.millis, 2) //buffered element
+        .expectNoMsg(200.millis)
+        .request(1)
+        .expectNext(3) //buffered element
         .expectComplete()
     }
 
-    "deliver buffered elements onComplete before the timeout" in assertAllStagesStopped {
+    "deliver elements with delay for slow stream" in assertAllStagesStopped {
       val c = TestSubscriber.manualProbe[Int]()
-      Source(1 to 3).delay(300.millis).to(Sink(c)).run()
+      val p = TestPublisher.manualProbe[Int]()
+
+      Source(p).delay(300.millis).to(Sink(c)).run()
       val cSub = c.expectSubscription()
-      c.expectNoMsg(200.millis)
+      val pSub = p.expectSubscription()
       cSub.request(100)
-      (1 to 3) foreach { n ⇒ c.expectNext(n) }
-      c.expectComplete()
+      pSub.sendNext(1)
       c.expectNoMsg(200.millis)
+      c.expectNext(1)
+      pSub.sendNext(2)
+      c.expectNoMsg(200.millis)
+      c.expectNext(2)
+      pSub.sendComplete()
+      c.expectComplete()
+    }
+
+    "drop tail for internal buffer if it's full in DropTail mode" in assertAllStagesStopped {
+      Await.result(
+        Source(1 to 20).delay(1.seconds, DelayOverflowStrategy.dropTail).withAttributes(inputBuffer(16, 16))
+          .grouped(100)
+          .runWith(Sink.head),
+        1200.millis) should ===((1 to 15).toList :+ 20)
+    }
+
+    "drop head for internal buffer if it's full in DropHead mode" in assertAllStagesStopped {
+      Await.result(
+        Source(1 to 20).delay(1.seconds, DelayOverflowStrategy.dropHead).withAttributes(inputBuffer(16, 16))
+          .grouped(100)
+          .runWith(Sink.head),
+        1200.millis) should ===(5 to 20)
+    }
+
+    "clear all for internal buffer if it's full in DropBuffer mode" in assertAllStagesStopped {
+      Await.result(
+        Source(1 to 20).delay(1.seconds, DelayOverflowStrategy.dropBuffer).withAttributes(inputBuffer(16, 16))
+          .grouped(100)
+          .runWith(Sink.head),
+        1200.millis) should ===(17 to 20)
+    }
+
+    "pass elements with delay through normally in backpressured mode" in assertAllStagesStopped {
+      Source(1 to 3).delay(300.millis, DelayOverflowStrategy.backpressure).runWith(TestSink.probe[Int])
+        .request(5)
+        .expectNoMsg(200.millis)
+        .expectNext(200.millis, 1)
+        .expectNoMsg(200.millis)
+        .expectNext(200.millis, 2)
+        .expectNoMsg(200.millis)
+        .expectNext(200.millis, 3)
+    }
+
+    "fail on overflow in Fail mode" in assertAllStagesStopped {
+      Source(1 to 20).delay(300.millis, DelayOverflowStrategy.fail)
+        .withAttributes(inputBuffer(16, 16))
+        .runWith(TestSink.probe[Int])
+        .request(100)
+        .expectError(new DelayOverflowStrategy.Fail.BufferOverflowException("Buffer overflow for delay combinator (max capacity was: 16)!"))
+
+    }
+
+    "emit early when buffer is full and in EmitEarly mode" in assertAllStagesStopped {
+      val c = TestSubscriber.manualProbe[Int]()
+      val p = TestPublisher.manualProbe[Int]()
+
+      Source(p).delay(10.seconds, DelayOverflowStrategy.emitEarly).withAttributes(inputBuffer(16, 16)).to(Sink(c)).run()
+      val cSub = c.expectSubscription()
+      val pSub = p.expectSubscription()
+      cSub.request(20)
+
+      for (i ← 1 to 16) pSub.sendNext(i)
+      c.expectNoMsg(300.millis)
+      pSub.sendNext(17)
+      c.expectNext(100.millis, 1)
+      pSub.sendError(new RuntimeException() with NoStackTrace)
+      c.expectError()
     }
   }
 }
