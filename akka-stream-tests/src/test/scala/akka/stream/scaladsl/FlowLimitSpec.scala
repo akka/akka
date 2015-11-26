55e2f51c89e92224e61896630fb96f08fe61acb1
lolski <soulmagic13@gmail.com>
2015-11-27 00:05:20 +0800

    adding limit unit tests

--- akka-stream-tests/src/test/scala/akka/stream/scaladsl/FlowLimitSpec.scala
+++ akka-stream-tests/src/test/scala/akka/stream/scaladsl/FlowLimitSpec.scala
@@ -1,14 +1,48 @@
 package akka.stream.scaladsl
 
+import akka.stream.testkit.Utils._
+import akka.stream.{ StreamLimitReachedException, ActorMaterializer, ActorMaterializerSettings }
 import akka.stream.testkit.AkkaSpec
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.util.{ Failure, Try }
 
-/**
-  * Created by lolski on 11/25/15.
-  */
 class FlowLimitSpec extends AkkaSpec {
-  // Source(size = n).limit(n) == ???
-  "???" must {
+  import system.dispatcher
 
+  val settings = ActorMaterializerSettings(system)
+    .withInputBuffer(initialSize = 2, maxSize = 16)
+
+  implicit val mat = ActorMaterializer(settings)
+
+  // Source(size = n).limit(n) == ???
+  "Limit" must {
+    "produce output that is identical to the input when n = input.length" in assertAllStagesStopped {
+      val input = (1 to 6)
+      val n = input.length
+      val future = Source(input).limit(n).grouped(Integer.MAX_VALUE).runWith(Sink.head)
+      val result = Await.result(future, 300.millis)
+      result == input.toSeq
+    }
+  }
+
+  "produce output that is identical to the input when n > input.length" in assertAllStagesStopped {
+    val input = (1 to 6)
+    val n = input.length + 2 // n > input.length
+    val future = Source(input).limit(n).grouped(Integer.MAX_VALUE).runWith(Sink.head)
+    val result = Await.result(future, 300.millis)
+    result == input.toSeq
+  }
+
+  "produce n messages before throwing a StreamLimitReachedException when n < input.size" in {
+    val input = (1 to 6)
+    val n = input.length - 2 // n < input.length
+    val future = Source(input).limit(n).grouped(Integer.MAX_VALUE).runWith(Sink.head)
+    val result = Try(Await.result(future, 300.millis))
+    result match {
+      case Failure(t: StreamLimitReachedException) if t.n == n ⇒ true
+      case _ ⇒ false
+    }
   }
 
   // Source(size = m).limit(n) where m < n == ???
---
+++ akka-stream-tests/src/test/scala/akka/stream/scaladsl/ToSeqSinkSpec.scala
@@ -0,0 +1,8 @@
+package akka.stream.scaladsl
+
+/**
+  * Created by lolski on 11/27/15.
+  */
+class ToSeqSinkSpec {
+
+}
