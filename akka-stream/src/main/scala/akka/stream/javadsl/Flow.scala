46ef6c548f9707c391d7e69d85ec1a1dc7234341
Alexander Golubev <alexander.golubev@gmail.com>
2015-12-02 14:58:30 -0500

    +str #18556 add delay combinator

--- akka-stream/src/main/scala/akka/stream/javadsl/Flow.scala
+++ akka-stream/src/main/scala/akka/stream/javadsl/Flow.scala
@@ -509,6 +509,8 @@ final class Flow[-In, +Out, +Mat](delegate: scaladsl.Flow[In, Out, Mat]) extends
    * [[akka.stream.DelayOverflowStrategy]] it might drop elements or backpressure the upstream if
    * there is no space available in the buffer.
    *
+   * Delay precession is 10ms to avoid unnecessary timer scheduling cycles
+   *
    * Internal buffer has default capacity 16. You can set buffer size by calling `withAttributes(inputBuffer)`
    *
    * '''Emits when''' there is a pending element in the buffer and configured time for this element elapsed
