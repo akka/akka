1097256c5c1da3eb24df7c3f733b51be466e44af
Viktor Klang <viktor.klang@gmail.com>
2015-12-05 23:09:09 +0100

    =str - Adds finals and reorganizes some StreamOfStreams code

--- akka-stream/src/main/scala/akka/stream/impl/fusing/StreamOfStreams.scala
+++ akka-stream/src/main/scala/akka/stream/impl/fusing/StreamOfStreams.scala
@@ -10,10 +10,8 @@ import akka.stream.actor.ActorSubscriberMessage
 import akka.stream.actor.ActorSubscriberMessage._
 import akka.stream.actor.ActorPublisherMessage
 import akka.stream.actor.ActorPublisherMessage._
-import scala.concurrent.forkjoin.ThreadLocalRandom
 import java.{ util ⇒ ju }
 import scala.concurrent._
-import akka.dispatch.ExecutionContexts.sameThreadExecutionContext
 
 final class FlattenMerge[T, M](breadth: Int) extends GraphStage[FlowShape[Source[T, M], T]] {
   private val in = Inlet[Source[T, M]]("flatten.in")
@@ -22,16 +20,18 @@ final class FlattenMerge[T, M](breadth: Int) extends GraphStage[FlowShape[Source
 
   override def createLogic(attr: Attributes) = new GraphStageLogic(shape) {
 
+    import StreamOfStreams.{ LocalSink, LocalSource }
+
     var sources = Set.empty[LocalSource[T]]
     def activeSources = sources.size
 
-    private trait Queue {
+    private sealed trait Queue {
       def hasData: Boolean
       def enqueue(src: LocalSource[T]): Unit
       def dequeue(): LocalSource[T]
     }
 
-    private class FixedQueue extends Queue {
+    private final class FixedQueue extends Queue {
       final val Size = 16
       final val Mask = 15
 
@@ -59,7 +59,7 @@ final class FlattenMerge[T, M](breadth: Int) extends GraphStage[FlowShape[Source
       }
     }
 
-    private class DynamicQueue extends ju.LinkedList[LocalSource[T]] with Queue {
+    private final class DynamicQueue extends ju.LinkedList[LocalSource[T]] with Queue {
       def hasData = !isEmpty()
       def enqueue(src: LocalSource[T]): Unit = add(src)
       def dequeue(): LocalSource[T] = remove()
@@ -131,83 +131,84 @@ final class FlattenMerge[T, M](breadth: Int) extends GraphStage[FlowShape[Source
   }
 }
 
-// TODO possibly place the Local* classes in a companion object depending on where they are reused
-
 /**
  * INTERNAL API
  */
-private[fusing] final class LocalSinkSubscription[T](sub: ActorPublisherMessage ⇒ Unit) {
-  def pull(): Unit = sub(Request(1))
-  def cancel(): Unit = sub(Cancel)
-}
+private[fusing] object StreamOfStreams {
+  import akka.dispatch.ExecutionContexts.sameThreadExecutionContext
+  private val RequestOne = Request(1) // No need to frivolously allocate these
+  private type LocalSinkSubscription = ActorPublisherMessage ⇒ Unit
+  /**
+   * INTERNAL API
+   */
+  private[fusing] final class LocalSource[T] {
+    private var subF: Future[LocalSinkSubscription] = _
+    private var sub: LocalSinkSubscription = _
 
-/**
- * INTERNAL API
- */
-private[fusing] final class LocalSource[T] {
-  private var subF: Future[LocalSinkSubscription[T]] = _
-  private var sub: LocalSinkSubscription[T] = _
+    var elem: T = null.asInstanceOf[T]
 
-  var elem: T = null.asInstanceOf[T]
+    def isActive: Boolean = sub ne null
 
-  def isActive: Boolean = sub ne null
-  def deactivate(): Unit = {
-    sub = null
-    subF = null
-  }
-  def activate(f: Future[LocalSinkSubscription[T]]): Unit = {
-    subF = f
-    /*
-     * The subscription is communicated to the FlattenMerge stage by way of completing
-     * the future. Encoding it like this means that the `sub` field will be written
-     * either by us (if the future has already been completed) or by the LocalSink (when
-     * it eventually completes the future in its `preStart`). The important part is that
-     * either way the `sub` field is populated before we get the first `OnNext` message
-     * and the value is safely published in either case as well (since AsyncCallback is
-     * based on an Actor message send).
-     */
-    f.foreach(s ⇒ sub = s)(sameThreadExecutionContext)
-  }
-
-  def pull(): Unit = {
-    if (sub eq null)
-      throw new IllegalStateException("not yet initialized, subscription future has " + subF.value)
-    sub.pull()
-  }
-
-  def cancel(): Unit =
-    if (subF ne null)
-      subF.foreach(_.cancel())(sameThreadExecutionContext)
-}
-
-/**
- * INTERNAL API
- */
-private[fusing] final class LocalSink[T](notifier: ActorSubscriberMessage ⇒ Unit)
-  extends GraphStageWithMaterializedValue[SinkShape[T], Future[LocalSinkSubscription[T]]] {
-
-  private val in = Inlet[T]("LocalSink.in")
-  override val shape = SinkShape(in)
-
-  override def createLogicAndMaterializedValue(attr: Attributes): (GraphStageLogic, Future[LocalSinkSubscription[T]]) = {
-    val sub = Promise[LocalSinkSubscription[T]]
-    val logic = new GraphStageLogic(shape) {
-      setHandler(in, new InHandler {
-        override def onPush(): Unit = notifier(OnNext(grab(in)))
-        override def onUpstreamFinish(): Unit = notifier(OnComplete)
-        override def onUpstreamFailure(ex: Throwable): Unit = notifier(OnError(ex))
-      })
-
-      override def preStart(): Unit = {
-        pull(in)
-        sub.success(
-          new LocalSinkSubscription[T](getAsyncCallback[ActorPublisherMessage] {
-            case Request(1) ⇒ tryPull(in)
-            case Cancel     ⇒ completeStage()
-            case _          ⇒
-          }.invoke))
-      }
+    def deactivate(): Unit = {
+      sub = null
+      subF = null
     }
-    logic -> sub.future
+
+    def activate(f: Future[LocalSinkSubscription]): Unit = {
+      subF = f
+      /*
+       * The subscription is communicated to the FlattenMerge stage by way of completing
+       * the future. Encoding it like this means that the `sub` field will be written
+       * either by us (if the future has already been completed) or by the LocalSink (when
+       * it eventually completes the future in its `preStart`). The important part is that
+       * either way the `sub` field is populated before we get the first `OnNext` message
+       * and the value is safely published in either case as well (since AsyncCallback is
+       * based on an Actor message send).
+       */
+      f.foreach(s ⇒ sub = s)(sameThreadExecutionContext)
+    }
+
+    def pull(): Unit = {
+      if (sub ne null) sub(RequestOne)
+      else throw new IllegalStateException("not yet initialized, subscription future has " + subF.value)
+    }
+
+    def cancel(): Unit =
+      if (subF ne null)
+        subF.foreach(_(Cancel))(sameThreadExecutionContext)
+  }
+
+  /**
+   * INTERNAL API
+   */
+  private[fusing] final class LocalSink[T](notifier: ActorSubscriberMessage ⇒ Unit)
+    extends GraphStageWithMaterializedValue[SinkShape[T], Future[LocalSinkSubscription]] {
+
+    private val in = Inlet[T]("LocalSink.in")
+    override val shape = SinkShape(in)
+
+    override def createLogicAndMaterializedValue(attr: Attributes): (GraphStageLogic, Future[LocalSinkSubscription]) = {
+      val sub = Promise[LocalSinkSubscription]
+      val logic = new GraphStageLogic(shape) {
+        setHandler(in, new InHandler {
+          override def onPush(): Unit = notifier(OnNext(grab(in)))
+
+          override def onUpstreamFinish(): Unit = notifier(OnComplete)
+
+          override def onUpstreamFailure(ex: Throwable): Unit = notifier(OnError(ex))
+        })
+
+        override def preStart(): Unit = {
+          pull(in)
+          sub.success(
+            getAsyncCallback[ActorPublisherMessage] {
+              case RequestOne ⇒ tryPull(in)
+              case Cancel     ⇒ completeStage()
+              case _          ⇒ throw new IllegalStateException("Bug")
+            }.invoke)
+        }
+      }
+      logic -> sub.future
+    }
   }
 }
