7cc4cf63b0856daf0d6a71696ec64a3e84405483
Konrad Malawski <konrad.malawski@typesafe.com>
2015-12-22 17:54:35 +0100

    =htc,str #19256 add TCK test for InputStreamPublisher

---
+++ akka-stream-tck/src/test/scala/akka/stream/tck/InputStreamSourceTest.scala
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) 2015 Typesafe Inc. <http://www.typesafe.com>
+ */
+package akka.stream.tck
+
+import java.io.InputStream
+
+import akka.stream.ActorAttributes
+import akka.stream.scaladsl.{ Sink, StreamConverters }
+import akka.util.ByteString
+import org.reactivestreams.Publisher
+
+class InputStreamSourceTest extends AkkaPublisherVerification[ByteString] {
+
+  def createPublisher(elements: Long): Publisher[ByteString] = {
+    StreamConverters.fromInputStream(() ⇒ new InputStream {
+      @volatile var num = 0
+      override def read(): Int = {
+        num += 1
+        num
+      }
+    }).withAttributes(ActorAttributes.dispatcher("akka.test.stream-dispatcher"))
+      .take(elements)
+      .runWith(Sink.asPublisher(false))
+  }
+}
+
