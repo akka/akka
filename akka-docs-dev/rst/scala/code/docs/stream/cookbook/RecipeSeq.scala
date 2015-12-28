b9025ea9b7cb7dc710708a3c723a26605a7644f9
lolski <soulmagic13@gmail.com>
2015-12-28 21:34:06 +0800

    stream-cookbook WIP

--- akka-docs-dev/rst/scala/code/docs/stream/cookbook/RecipeSeq.scala
+++ akka-docs-dev/rst/scala/code/docs/stream/cookbook/RecipeSeq.scala
@@ -14,15 +14,34 @@ class RecipeSeq extends RecipeSpec {
       val myData = Source(List("1", "2", "3"))
       val MaxAllowedSeqSize = 100
 
-      //#draining-to-seq
+      //#draining-to-seq-unsafe
       val strictUnsafe: Future[immutable.Seq[Message]] = myData.runWith(Sink.seq) // dangerous! if myData is unbounded, will cause OutOfMemory exception
+      //#draining-to-seq-unsafe
+
+      Await.result(strictUnsafe, 3.seconds) should be(List("1", "2", "3"))
+    }
+
+    "work together with limit(n)" in {
+      val myData = Source(List("1", "2", "3"))
+      val MaxAllowedSeqSize = 100
+
+      //#draining-to-seq-limit
       val strictSafe1: Future[immutable.Seq[Message]] = myData.limit(MaxAllowedSeqSize).runWith(Sink.seq) // ok. Future will fail with a `StreamLimitReachedException` if the number of incoming elements is larger than MaxAllowedSeqSize
-      val strictSafe2: Future[immutable.Seq[Message]] = myData.take(MaxAllowedSeqSize).runWith(Sink.seq) // ok. Collect up until `MaxAllowedSeqSize`-th elements only (drop the rest if any)
-      //#draining-to-seq
+      //#draining-to-seq-limit
 
       Await.result(strictSafe1, 3.seconds) should be(List("1", "2", "3"))
     }
 
+    "work together with take(n)" in {
+      val myData = Source(List("1", "2", "3"))
+      val MaxAllowedSeqSize = 100
+
+      //#draining-to-seq-take
+      val strictSafe2: Future[immutable.Seq[Message]] = myData.take(MaxAllowedSeqSize).runWith(Sink.seq) // ok. Collect up until `MaxAllowedSeqSize`-th elements only (drop the rest if any)
+      //#draining-to-seq-take
+
+      Await.result(strictSafe2, 3.seconds) should be(List("1", "2", "3"))
+    }
   }
 
 }
