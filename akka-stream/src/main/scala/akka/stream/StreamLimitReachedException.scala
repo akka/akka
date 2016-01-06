02ac985625835bb92b651a9c0e81c5da65fdbb18
Ganeshwara <ganesh@dropmysite.com>
2016-01-06 08:33:09 +0000

    fixing more formatting issues

--- akka-stream/src/main/scala/akka/stream/StreamLimitReachedException.scala
+++ akka-stream/src/main/scala/akka/stream/StreamLimitReachedException.scala
@@ -1,6 +1,7 @@
-package akka.stream
-
 /**
  * Copyright (C) 2015 Typesafe Inc. <http://www.typesafe.com>
  */
+
+package akka.stream
+
 class StreamLimitReachedException(val n: Long) extends RuntimeException(s"limit of $n reached")
