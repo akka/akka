d7a403885bc7da2ab4bbf38e0458a5e424bc3927
Konrad Malawski <konrad.malawski@typesafe.com>
2016-01-14 14:25:43 +0100

    =htc #19418 add compile test for accessing javadsl content/media type

--- akka-http-core/src/test/java/akka/http/javadsl/model/JavaApiTestCases.java
+++ akka-http-core/src/test/java/akka/http/javadsl/model/JavaApiTestCases.java
@@ -5,66 +5,96 @@
 package akka.http.javadsl.model;
 
 import akka.http.impl.util.Util;
-import akka.http.javadsl.model.headers.Authorization;
-import akka.http.javadsl.model.headers.HttpCredentials;
+import akka.http.javadsl.model.headers.*;
 import akka.japi.Pair;
 
 public class JavaApiTestCases {
-    /** Builds a request for use on the client side */
-    public static HttpRequest buildRequest() {
+  /**
+   * Builds a request for use on the client side
+   */
+  public static HttpRequest buildRequest() {
+    return
+      HttpRequest.create()
+                 .withMethod(HttpMethods.POST)
+                 .withUri("/send");
+  }
+
+  /**
+   * A simple handler for an Http server
+   */
+  public static HttpResponse handleRequest(HttpRequest request) {
+    if (request.method() == HttpMethods.GET) {
+      Uri uri = request.getUri();
+      if (uri.path().equals("/hello")) {
+        String name = Util.getOrElse(uri.query().get("name"), "Mister X");
+
         return
-            HttpRequest.create()
-                .withMethod(HttpMethods.POST)
-                .withUri("/send");
-    }
-
-    /** A simple handler for an Http server */
-    public static HttpResponse handleRequest(HttpRequest request) {
-        if (request.method() == HttpMethods.GET) {
-            Uri uri = request.getUri();
-            if (uri.path().equals("/hello")) {
-                String name = Util.getOrElse(uri.query().get("name"), "Mister X");
-
-                return
-                    HttpResponse.create()
-                        .withEntity("Hello " + name + "!");
-            } else
-                return
-                    HttpResponse.create()
-                        .withStatus(404)
-                        .withEntity("Not found");
-        } else
-            return
-                HttpResponse.create()
+          HttpResponse.create()
+                      .withEntity("Hello " + name + "!");
+      } else {
+        return
+          HttpResponse.create()
+                      .withStatus(404)
+                      .withEntity("Not found");
+      }
+    } else {
+      return
+        HttpResponse.create()
                     .withStatus(StatusCodes.METHOD_NOT_ALLOWED)
                     .withEntity("Unsupported method");
     }
+  }
 
-    /** Adds authentication to an existing request */
-    public static HttpRequest addAuthentication(HttpRequest request) {
-        // unused here but just to show the shortcut
-        request.addHeader(Authorization.basic("username", "password"));
+  /**
+   * Adds authentication to an existing request
+   */
+  public static HttpRequest addAuthentication(HttpRequest request) {
+    // unused here but just to show the shortcut
+    request.addHeader(Authorization.basic("username", "password"));
 
-        return request
-            .addHeader(Authorization.create(HttpCredentials.createBasicHttpCredentials("username", "password")));
+    return request
+      .addHeader(Authorization.create(HttpCredentials.createBasicHttpCredentials("username", "password")));
 
-    }
+  }
 
-    /** Removes cookies from an existing request */
-    public static HttpRequest removeCookies(HttpRequest request) {
-        return request.removeHeader("Cookie");
-    }
+  /**
+   * Removes cookies from an existing request
+   */
+  public static HttpRequest removeCookies(HttpRequest request) {
+    return request.removeHeader("Cookie");
+  }
 
-    /** Build a uri to send a form */
-    public static Uri createUriForOrder(String orderId, String price, String amount) {
-        return Uri.create("/order").query(
-                Query.create(
-                        Pair.create("orderId", orderId),
-                        Pair.create("price", price),
-                        Pair.create("amount", amount)));
-    }
+  /**
+   * Build a uri to send a form
+   */
+  public static Uri createUriForOrder(String orderId, String price, String amount) {
+    return Uri.create("/order").query(
+      Query.create(
+        Pair.create("orderId", orderId),
+        Pair.create("price", price),
+        Pair.create("amount", amount)));
+  }
 
-    public static Query addSessionId(Query query) {
-        return query.withParam("session", "abcdefghijkl");
-    }
+  public static Query addSessionId(Query query) {
+    return query.withParam("session", "abcdefghijkl");
+  }
+
+  public static Object accessScalaDefinedJavadslContentTypeAndMediaType(ContentType type) {
+    Object anything = null;
+
+    akka.http.javadsl.model.MediaType mediaType = type.mediaType();
+
+    // just for the sake of explicitly touching the interfaces
+    if (mediaType.binary()) anything = (akka.http.javadsl.model.MediaType.Binary) mediaType;
+    if (1 == 2) anything = (akka.http.javadsl.model.MediaType.Multipart) mediaType;
+    if (1 == 2) anything = (akka.http.javadsl.model.MediaType.WithOpenCharset) mediaType;
+    if (1 == 2) anything = (akka.http.javadsl.model.MediaType.WithFixedCharset) mediaType;
+
+    if (type.binary()) anything = (akka.http.javadsl.model.ContentType.Binary) type;
+    if (1 == 2) anything = (akka.http.javadsl.model.ContentType.NonBinary) type;
+    if (1 == 2) anything = (akka.http.javadsl.model.ContentType.WithCharset) type;
+    if (1 == 2) anything = (akka.http.javadsl.model.ContentType.WithFixedCharset) type;
+
+    return anything;
+  }
 }
