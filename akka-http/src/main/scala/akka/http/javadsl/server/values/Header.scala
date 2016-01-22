a8abbaf36bdae84f9eca2346a58c5f126c64a98c
Martynas Mickevičius <mmartynas@gmail.com>
2016-01-22 17:01:22 +0200

    wip #19441 convert from Option to Optional in javadsl

--- akka-http/src/main/scala/akka/http/javadsl/server/values/Header.scala
+++ akka-http/src/main/scala/akka/http/javadsl/server/values/Header.scala
@@ -4,6 +4,8 @@
 
 package akka.http.javadsl.server.values
 
+import java.util.Optional
+
 import akka.http.impl.server.HeaderImpl
 import akka.http.javadsl.model.HttpHeader
 import akka.http.javadsl.server.RequestVal
@@ -11,27 +13,28 @@ import akka.http.scaladsl.model
 import akka.http.scaladsl.server.Directive1
 import akka.http.scaladsl.server.util.ClassMagnet
 
+import scala.compat.java8.OptionConverters._
 import scala.reflect.{ ClassTag, classTag }
 
 trait Header[T <: HttpHeader] {
   def instance(): RequestVal[T]
-  def optionalInstance(): RequestVal[Option[T]]
+  def optionalInstance(): RequestVal[Optional[T]]
 
   def value(): RequestVal[String]
-  def optionalValue(): RequestVal[Option[String]]
+  def optionalValue(): RequestVal[Optional[String]]
 }
 object Headers {
   import akka.http.scaladsl.server.directives.BasicDirectives._
   import akka.http.scaladsl.server.directives.HeaderDirectives._
 
   def byName(name: String): Header[HttpHeader] =
-    HeaderImpl[HttpHeader](name, _ ⇒ optionalHeaderInstanceByName(name.toLowerCase()), classTag[HttpHeader])
+    HeaderImpl[HttpHeader](name, _ ⇒ optionalHeaderInstanceByName(name.toLowerCase()).map(_.asScala), classTag[HttpHeader])
 
   def byClass[T <: HttpHeader](clazz: Class[T]): Header[T] =
     HeaderImpl[T](clazz.getSimpleName, ct ⇒ optionalHeaderValueByType(ClassMagnet(ct)), ClassTag(clazz))
 
-  private def optionalHeaderInstanceByName(lowercaseName: String): Directive1[Option[model.HttpHeader]] =
+  private def optionalHeaderInstanceByName(lowercaseName: String): Directive1[Optional[model.HttpHeader]] =
     extract(_.request.headers.collectFirst {
       case h @ model.HttpHeader(`lowercaseName`, _) ⇒ h
-    })
+    }.asJava)
 }
