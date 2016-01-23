3fb54da0aa9747c845d48c15ec68a81deb5427b4
Matthias Sperl <sperl.matthias0@gmail.com>
2016-01-23 14:28:16 +0100

    +str #19589 Add autoFlush option to  OutputStream Sinks

--- akka-stream/src/main/scala/akka/stream/impl/io/IOSinks.scala
+++ akka-stream/src/main/scala/akka/stream/impl/io/IOSinks.scala
@@ -45,7 +45,7 @@ private[akka] final class FileSink(f: File, append: Boolean, val attributes: Att
  * Creates simple synchronous (Java 6 compatible) Sink which writes all incoming elements to the given file
  * (creating it before hand if necessary).
  */
-private[akka] final class OutputStreamSink(createOutput: () ⇒ OutputStream, val attributes: Attributes, shape: SinkShape[ByteString])
+private[akka] final class OutputStreamSink(createOutput: () ⇒ OutputStream, val attributes: Attributes, shape: SinkShape[ByteString], autoFlush: Boolean)
   extends SinkModule[ByteString, Future[IOResult]](shape) {
 
   override def create(context: MaterializationContext) = {
@@ -55,16 +55,16 @@ private[akka] final class OutputStreamSink(createOutput: () ⇒ OutputStream, va
 
     val os = createOutput() // if it fails, we fail the materialization
 
-    val props = OutputStreamSubscriber.props(os, ioResultPromise, settings.maxInputBufferSize)
+    val props = OutputStreamSubscriber.props(os, ioResultPromise, settings.maxInputBufferSize, autoFlush)
 
     val ref = materializer.actorOf(context, props)
     (akka.stream.actor.ActorSubscriber[ByteString](ref), ioResultPromise.future)
   }
 
   override protected def newInstance(shape: SinkShape[ByteString]): SinkModule[ByteString, Future[IOResult]] =
-    new OutputStreamSink(createOutput, attributes, shape)
+    new OutputStreamSink(createOutput, attributes, shape, autoFlush)
 
   override def withAttributes(attr: Attributes): Module =
-    new OutputStreamSink(createOutput, attr, amendShape(attr))
+    new OutputStreamSink(createOutput, attr, amendShape(attr), autoFlush)
 }
 
