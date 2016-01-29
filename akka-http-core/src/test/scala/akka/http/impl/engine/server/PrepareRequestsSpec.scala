314451a78cf525b4c4083861d125523aa37885f1
Johan Andrén <johan@markatta.com>
2016-01-29 09:39:17 +0100

    =htt #19623 optimizations and more careful handling of complete and failure in PrepareRequest

--- akka-http-core/src/test/scala/akka/http/impl/engine/server/PrepareRequestsSpec.scala
+++ akka-http-core/src/test/scala/akka/http/impl/engine/server/PrepareRequestsSpec.scala
@@ -18,6 +18,38 @@ import scala.concurrent.duration._
 
 class PrepareRequestsSpec extends AkkaSpec {
 
+  val chunkedStart =
+    ParserOutput.RequestStart(
+      HttpMethods.GET,
+      Uri("http://example.com/"),
+      HttpProtocols.`HTTP/1.1`,
+      List(),
+      StreamedEntityCreator[ParserOutput, RequestEntity] { entityChunks ⇒
+        val chunks = entityChunks.collect {
+          case EntityChunk(chunk)      ⇒ chunk
+          case EntityStreamError(info) ⇒ throw EntityStreamException(info)
+        }
+        HttpEntity.Chunked(ContentTypes.`application/octet-stream`, HttpEntity.limitableChunkSource(chunks))
+      },
+      true,
+      false)
+
+  val chunkPart =
+    ParserOutput.EntityChunk(HttpEntity.ChunkStreamPart(ByteString("abc")))
+
+  val chunkRequestComplete =
+    ParserOutput.MessageEnd
+
+  val strictRequest =
+    ParserOutput.RequestStart(
+      HttpMethods.GET,
+      Uri("http://example.com/"),
+      HttpProtocols.`HTTP/1.1`,
+      List(),
+      StrictEntityCreator(HttpEntity.Strict(ContentTypes.`application/octet-stream`, ByteString("body"))),
+      true,
+      false)
+
   "The PrepareRequest stage" should {
 
     "not fail when there is demand from both streamed entity consumption and regular flow" in {
@@ -41,20 +73,7 @@ class PrepareRequestsSpec extends AkkaSpec {
       // let request with streamed entity through
       upstreamSub.request(1)
       inSub.expectRequest(1)
-      inSub.sendNext(ParserOutput.RequestStart(
-        HttpMethods.GET,
-        Uri("http://example.com/"),
-        HttpProtocols.`HTTP/1.1`,
-        List(),
-        StreamedEntityCreator[ParserOutput, RequestEntity] { entityChunks ⇒
-          val chunks = entityChunks.collect {
-            case EntityChunk(chunk)      ⇒ chunk
-            case EntityStreamError(info) ⇒ throw EntityStreamException(info)
-          }
-          HttpEntity.Chunked(ContentTypes.`application/octet-stream`, HttpEntity.limitableChunkSource(chunks))
-        },
-        true,
-        false))
+      inSub.sendNext(chunkedStart)
 
       val request = upstreamProbe.expectNext()
 
@@ -87,16 +106,101 @@ class PrepareRequestsSpec extends AkkaSpec {
       // that should go downstream when the streamed entity
       // has reached it's end
       inSub.expectRequest(1)
-      inSub.sendNext(ParserOutput.RequestStart(
-        HttpMethods.GET,
-        Uri("http://example.com/"),
-        HttpProtocols.`HTTP/1.1`,
-        List(),
-        StrictEntityCreator(HttpEntity.Strict(ContentTypes.`application/octet-stream`, ByteString("body"))),
-        true,
-        false))
+      inSub.sendNext(strictRequest)
 
       upstreamProbe.expectNext()
+
+    }
+
+    "not complete running entity stream when upstream cancels" in {
+      implicit val materializer = ActorMaterializer()
+
+      val inProbe = TestPublisher.manualProbe[ParserOutput.RequestOutput]()
+      val upstreamProbe = TestSubscriber.manualProbe[HttpRequest]()
+
+      val stage = Flow.fromGraph(new PrepareRequests(ServerSettings(system)))
+
+      Source.fromPublisher(inProbe)
+        .via(stage)
+        .to(Sink.fromSubscriber(upstreamProbe))
+        .withAttributes(Attributes.inputBuffer(1, 1))
+        .run()
+
+      val upstreamSub = upstreamProbe.expectSubscription()
+      val inSub = inProbe.expectSubscription()
+
+      // let request with streamed entity through
+      upstreamSub.request(1)
+      inSub.expectRequest(1)
+      inSub.sendNext(chunkedStart)
+
+      val request = upstreamProbe.expectNext()
+
+      // and subscribe to it's streamed entity
+      val entityProbe = TestSubscriber.manualProbe[ByteString]()
+      request.entity.dataBytes.to(Sink.fromSubscriber(entityProbe))
+        .withAttributes(Attributes.inputBuffer(1, 1))
+        .run()
+
+      val entitySub = entityProbe.expectSubscription()
+
+      // user logic cancels flow
+      upstreamSub.cancel()
+
+      // but incoming chunks should still end up in entity
+      entitySub.request(1)
+      inSub.expectRequest(1)
+      inSub.sendNext(chunkPart)
+      entityProbe.expectNext()
+
+      entitySub.request(1)
+      inSub.expectRequest(1)
+      inSub.sendNext(chunkRequestComplete)
+
+      // and then when entity is complete, the stage should complete
+      entityProbe.expectComplete()
+
+    }
+
+    "complete stage if chunked stream is completed without reaching end of chunks" in {
+      // a bit unsure about this, but to document the assumption
+      implicit val materializer = ActorMaterializer()
+
+      val inProbe = TestPublisher.manualProbe[ParserOutput.RequestOutput]()
+      val upstreamProbe = TestSubscriber.manualProbe[HttpRequest]()
+
+      val stage = Flow.fromGraph(new PrepareRequests(ServerSettings(system)))
+
+      Source.fromPublisher(inProbe)
+        .via(stage)
+        .to(Sink.fromSubscriber(upstreamProbe))
+        .withAttributes(Attributes.inputBuffer(1, 1))
+        .run()
+
+      val upstreamSub = upstreamProbe.expectSubscription()
+      val inSub = inProbe.expectSubscription()
+
+      // let request with streamed entity through
+      upstreamSub.request(1)
+      inSub.expectRequest(1)
+      inSub.sendNext(chunkedStart)
+
+      val request = upstreamProbe.expectNext()
+
+      // and subscribe to it's streamed entity
+      val entityProbe = TestSubscriber.manualProbe[ByteString]()
+      request.entity.dataBytes.to(Sink.fromSubscriber(entityProbe))
+        .withAttributes(Attributes.inputBuffer(1, 1))
+        .run()
+
+      val entitySub = entityProbe.expectSubscription()
+
+      // incoming stream is completed, but we never got the chunk end
+      inSub.sendComplete()
+
+      // assumption: should cause stage to complete
+      entityProbe.expectComplete()
+      upstreamProbe.expectComplete()
 
     }
   }
