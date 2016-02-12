f11e413e724e2da8d911c7e39261dadb7e0345c2
Roland Kuhn <rk@rkuhn.info>
2016-02-12 16:41:47 +0100

    include build information for Scala 2.12.0-M3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,11 +12,15 @@ object Dependencies {
   val junitVersion = "4.12"
 
   val Versions = Seq(
-    crossScalaVersions := Seq("2.11.7"), //"2.12.0-M2"
+    crossScalaVersions := Seq("2.11.7"), // "2.12.0-M3"
     scalaVersion := crossScalaVersions.value.head,
     scalaStmVersion := sys.props.get("akka.build.scalaStmVersion").getOrElse("0.7"),
     scalaCheckVersion := sys.props.get("akka.build.scalaCheckVersion").getOrElse("1.11.6"),
-    scalaTestVersion := (if (scalaVersion.value == "2.12.0-M2") "2.2.5-M2" else "2.2.4")
+    scalaTestVersion := (
+      if (scalaVersion.value == "2.12.0-M2") "2.2.5-M2"
+      else if (scalaVersion.value == "2.12.0-M3") "2.2.5-M3"
+      else "2.2.4"
+    )
   )
 
   object Compile {
