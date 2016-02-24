d2a45e046c1f82e4cf2a07138600522a381af754
Konrad Malawski <konrad.malawski@typesafe.com>
2016-02-24 18:22:03 +0100

    =htc #19827 avoid double-push when request timeout triggers

---
+++ akka-http-core/src/test/scala/akka/http/scaladsl/TightRequestTimeoutSpec.scala
@@ -0,0 +1,71 @@
+/**
+ * Copyright (C) 2009-2016 Lightbend Inc. <http://www.lightbend.com>
+ */
+
+package akka.http.scaladsl
+
+import java.io.{ BufferedReader, BufferedWriter, InputStreamReader, OutputStreamWriter }
+import java.net.{ BindException, Socket }
+import java.util.concurrent.TimeoutException
+
+import akka.actor.ActorSystem
+import akka.event.Logging
+import akka.event.Logging.LogEvent
+import akka.http.impl.util._
+import akka.http.scaladsl.Http.ServerBinding
+import akka.http.scaladsl.model.HttpEntity._
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers._
+import akka.http.scaladsl.settings.{ ClientConnectionSettings, ConnectionPoolSettings, ServerSettings }
+import akka.stream.scaladsl._
+import akka.stream.testkit._
+import akka.stream.{ OverflowStrategy, ActorMaterializer, BindFailedException, StreamTcpException }
+import akka.testkit.{ TestProbe, EventFilter }
+import akka.util.ByteString
+import com.typesafe.config.{ Config, ConfigFactory }
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpec }
+
+import scala.annotation.tailrec
+import scala.concurrent.duration._
+import scala.concurrent.{ Await, Future, Promise }
+import scala.util.{ Success, Try }
+
+class TightRequestTimeoutSpec extends WordSpec with Matchers with BeforeAndAfterAll with ScalaFutures {
+  val testConf: Config = ConfigFactory.parseString("""
+    akka.loggers = ["akka.testkit.TestEventListener"]
+    akka.loglevel = ERROR
+    akka.stdout-loglevel = ERROR
+    windows-connection-abort-workaround-enabled = auto
+    akka.log-dead-letters = OFF
+    akka.http.server.request-timeout = 10ms""")
+
+  implicit val system = ActorSystem(getClass.getSimpleName, testConf)
+  import system.dispatcher
+  implicit val materializer = ActorMaterializer()
+  implicit val patience = PatienceConfig(3.seconds)
+
+  "Tight request timeout" should {
+
+    "not cause double push error caused by the late response attemting to push" in {
+      val (_, hostname, port) = TestUtils.temporaryServerHostnameAndPort()
+      val slowHandler = Flow[HttpRequest].map(_ ⇒ HttpResponse()).delay(500.millis, OverflowStrategy.backpressure)
+      val binding = Http().bindAndHandle(slowHandler, hostname, port)
+
+      val p = TestProbe()
+      system.eventStream.subscribe(p.ref, classOf[Logging.Error])
+
+      val response = Http().singleRequest(HttpRequest(uri = s"http://$hostname:$port/")).futureValue
+      response.status should ===(StatusCodes.ServiceUnavailable) // the timeout response
+
+      p.expectMsgPF(hint = "Expected truncation error") {
+        case Logging.Error(_, _, _, msg: String) if msg contains "Inner stream finished before inputs completed." ⇒ ()
+      }
+      p.expectNoMsg(1.second) // here the double push might happen
+
+      binding.flatMap(_.unbind()).futureValue
+    }
+
+  }
+}
