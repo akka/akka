d815ed1b5e68bc6097fbfabd48cedfc74b209853
Endre Sándor Varga <endre.varga@typesafe.com>
2016-02-25 11:19:52 +0100

    19862 Token bucket reimplemented

---
+++ akka-actor/src/main/scala/akka/util/TokenBucket.scala
@@ -0,0 +1,88 @@
+/**
+ * Copyright (C) 2015-2016 Lightbend Inc. <http://www.lightbend.com>
+ */
+package akka.util
+
+/**
+ * INTERNAL API
+ */
+private[akka] abstract class TokenBucket(capacity: Long, nanosBetweenTokens: Long) {
+  require(capacity >= 0, "Capacity must be non-negative.")
+  require(nanosBetweenTokens > 0, "Time between tokens must be larger than zero nanoseconds.")
+
+  private[this] var availableTokens: Long = _
+  private[this] var lastUpdate: Long = _
+
+  /**
+   * This method must be called before the token bucket can be used.
+   */
+  def init(): Unit = {
+    availableTokens = capacity
+    lastUpdate = currentTime
+  }
+
+  /**
+   * The current time in nanos. The returned value is monotonic, might wrap over and has no relationship with wall-clock.
+   *
+   * @return return the current time in nanos as a Long.
+   */
+  def currentTime: Long
+
+  /**
+   * Call this (side-effecting) method whenever an element should be passed through the token-bucket. This method
+   * will return the number of nanoseconds the element needs to be delayed to conform with the token bucket parameters.
+   * Returns zero if the element can be emitted immediately. The method does not handle overflow, if an element is to
+   * be delayed longer in nanoseconds than what can be represented as a positive Long then an undefined value is returned.
+   *
+   * If a non-zero value is returned, it is the responsibility of the caller to not call this method before the
+   * returned delay has been elapsed (but can be called later). This class does not check or protect against early
+   * calls.
+   *
+   * @param cost How many tokens the element costs. Can be larger than the capacity of the bucket.
+   * @return
+   */
+  def offer(cost: Long): Long = {
+    if (cost < 0) throw new IllegalArgumentException("Cost must be non-negative")
+
+    val now = currentTime
+    val timeElapsed = now - lastUpdate
+
+    val tokensArrived =
+      // Was there even a tick since last time?
+      if (timeElapsed >= nanosBetweenTokens) {
+        // only one tick elapsed
+        if (timeElapsed < nanosBetweenTokens * 2) {
+          lastUpdate += nanosBetweenTokens
+          1
+        } else {
+          // Ok, no choice, do the slow integer division
+          val tokensArrived = timeElapsed / nanosBetweenTokens
+          lastUpdate += tokensArrived * nanosBetweenTokens
+          tokensArrived
+        }
+      } else 0
+
+    availableTokens = math.min(availableTokens + tokensArrived, capacity)
+
+    if (cost <= availableTokens) {
+      availableTokens -= cost
+      0
+    } else {
+      val remainingCost = cost - availableTokens
+      // Tokens always arrive at exact multiples of the token generation period, we must account for that
+      val timeSinceTokenArrival = now - lastUpdate
+      val delay = remainingCost * nanosBetweenTokens - timeSinceTokenArrival
+      availableTokens = 0
+      lastUpdate = now + delay
+      delay
+    }
+  }
+
+}
+
+/**
+ * Default implementation of [[TokenBucket]] that uses `System.nanoTime` as the time source.
+ */
+final class NanoTimeTokenBucket(_cap: Long, _period: Long) extends TokenBucket(_cap, _period) {
+  override def currentTime: Long = System.nanoTime()
+}
--- akka-stream-tests/src/test/scala/akka/stream/scaladsl/FlowThrottleSpec.scala
+++ akka-stream-tests/src/test/scala/akka/stream/scaladsl/FlowThrottleSpec.scala
@@ -29,6 +29,23 @@ class FlowThrottleSpec extends AkkaSpec {
         .expectComplete()
     }
 
+    "accept very high rates" in Utils.assertAllStagesStopped {
+      Source(1 to 5).throttle(1, 1 nanos, 0, ThrottleMode.Shaping)
+        .runWith(TestSink.probe[Int])
+        .request(5)
+        .expectNext(1, 2, 3, 4, 5)
+        .expectComplete()
+    }
+
+    "accept very low rates" in Utils.assertAllStagesStopped {
+      Source(1 to 5).throttle(1, 100.days, 1, ThrottleMode.Shaping)
+        .runWith(TestSink.probe[Int])
+        .request(5)
+        .expectNext(1)
+        .expectNoMsg(100.millis)
+        .cancel() // We won't wait 100 days, sorry
+    }
+
     "emit single element per tick" in Utils.assertAllStagesStopped {
       val upstream = TestPublisher.probe[Int]()
       val downstream = TestSubscriber.probe[Int]()
