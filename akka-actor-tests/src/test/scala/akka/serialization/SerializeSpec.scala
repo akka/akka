ac69051f9a5c5a41867a3a3ad36fb6235a11690e
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-03-07 20:51:26 +0100

    =act #19789 do not log serialization warnings for java.lang.*

--- akka-actor-tests/src/test/scala/akka/serialization/SerializeSpec.scala
+++ akka-actor-tests/src/test/scala/akka/serialization/SerializeSpec.scala
@@ -420,12 +420,19 @@ class DefaultSerializationWarningSpec extends AkkaSpec(
   ConfigFactory.parseString("akka.actor.warn-about-java-serializer-usage = on")) {
 
   val ser = SerializationExtension(system)
+  val messagePrefix = "Using the default Java serializer for class.*"
 
   "Using the default Java serializer" must {
 
-    "log a warning" in {
-      EventFilter.warning(message = "Using the default Java serializer for class.*") intercept {
-        ser.serializerFor(classOf[java.lang.Integer])
+    "log a warning when serializing classes outside of java.lang package" in {
+      EventFilter.warning(message = messagePrefix) intercept {
+        ser.serializerFor(classOf[java.math.BigDecimal])
+      }
+    }
+
+    "not log warning when serializing classes from java.lang package" in {
+      EventFilter.warning(message = messagePrefix, occurrences = 0) intercept {
+        ser.serializerFor(classOf[java.lang.String])
       }
     }
 
