3e5c5ec855d73db2330148ad6496e211b77fb579
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-03-10 10:10:44 +0100

    =doc fixed part of scaladoc warnings

--- akka-actor/src/main/scala/akka/routing/Resizer.scala
+++ akka-actor/src/main/scala/akka/routing/Resizer.scala
@@ -96,20 +96,9 @@ case object DefaultResizer {
 /**
  * Implementation of [[Resizer]] that adjust the [[Pool]] based on specified
  * thresholds.
- */
-@SerialVersionUID(1L)
-case class DefaultResizer(
-  /**
-   * The fewest number of routees the router should ever have.
-   */
-  val lowerBound: Int = 1,
-  /**
- * The most number of routees the router should ever have.
- * Must be greater than or equal to `lowerBound`.
- */
-  val upperBound: Int = 10,
-  /**
- * Threshold to evaluate if routee is considered to be busy (under pressure).
+ * @param lowerBound The fewest number of routees the router should ever have.
+ * @param upperBound The most number of routees the router should ever have. Must be greater than or equal to `lowerBound`.
+ * @param pressureThreshold Threshold to evaluate if routee is considered to be busy (under pressure).
  * Implementation depends on this value (default is 1).
  * <ul>
  * <li> 0:   number of routees currently processing a message.</li>
@@ -119,35 +108,30 @@ case class DefaultResizer(
  *           messages in their mailbox. Note that estimating mailbox size of
  *           default UnboundedMailbox is O(N) operation.</li>
  * </ul>
- */
-  val pressureThreshold: Int = 1,
-  /**
- * Percentage to increase capacity whenever all routees are busy.
+ * @param rampupRate  Percentage to increase capacity whenever all routees are busy.
  * For example, 0.2 would increase 20% (rounded up), i.e. if current
  * capacity is 6 it will request an increase of 2 more routees.
- */
-  val rampupRate: Double = 0.2,
-  /**
- * Minimum fraction of busy routees before backing off.
+ * @param backoffThreshold Minimum fraction of busy routees before backing off.
  * For example, if this is 0.3, then we'll remove some routees only when
  * less than 30% of routees are busy, i.e. if current capacity is 10 and
  * 3 are busy then the capacity is unchanged, but if 2 or less are busy
  * the capacity is decreased.
- *
  * Use 0.0 or negative to avoid removal of routees.
- */
-  val backoffThreshold: Double = 0.3,
-  /**
- * Fraction of routees to be removed when the resizer reaches the
+ * @param backoffRate  Fraction of routees to be removed when the resizer reaches the
  * backoffThreshold.
  * For example, 0.1 would decrease 10% (rounded up), i.e. if current
  * capacity is 9 it will request an decrease of 1 routee.
- */
-  val backoffRate: Double = 0.1,
-  /**
- * Number of messages between resize operation.
+ * @param messagesPerResize Number of messages between resize operation.
  * Use 1 to resize before each message.
  */
+@SerialVersionUID(1L)
+case class DefaultResizer(
+  val lowerBound: Int = 1,
+  val upperBound: Int = 10,
+  val pressureThreshold: Int = 1,
+  val rampupRate: Double = 0.2,
+  val backoffThreshold: Double = 0.3,
+  val backoffRate: Double = 0.1,
   val messagesPerResize: Int = 10) extends Resizer {
 
   /**
