1b20790d056b6e896219c12174454f1e7a1739e1
Konrad Malawski <konrad.malawski@project13.pl>
2016-04-10 16:51:41 +0200

    +htc #20273 EitherUnmarshaller

--- akka-http/src/main/scala/akka/http/scaladsl/unmarshalling/GenericUnmarshallers.scala
+++ akka-http/src/main/scala/akka/http/scaladsl/unmarshalling/GenericUnmarshallers.scala
@@ -4,7 +4,12 @@
 
 package akka.http.scaladsl.unmarshalling
 
+import akka.http.scaladsl.unmarshalling.Unmarshaller.EitherUnmarshallingException
 import akka.http.scaladsl.util.FastFuture
+import akka.stream.impl.ConstantFun
+
+import scala.concurrent.Future
+import scala.reflect.ClassTag
 
 trait GenericUnmarshallers extends LowerPriorityGenericUnmarshallers {
 
@@ -26,4 +31,34 @@ sealed trait LowerPriorityGenericUnmarshallers {
       case Some(a) ⇒ um(a)
       case None    ⇒ FastFuture.failed(Unmarshaller.NoContentException)
     })
+
+  /**
+   * Enables using [[Either]] to encode the following unmarshalling logic:
+   * Attempt unmarshalling the entity as as `R` first (yielding `R`),
+   * and if it fails attempt unmarshalling as `L` (yielding `Left`).
+   *
+   * Note that the Either's "R" type will be attempted first (as Left is often considered as the "failed case" in Either).
+   */
+  // format: OFF
+  implicit def eitherUnmarshaller[L, R](implicit ua: FromEntityUnmarshaller[L], rightTag: ClassTag[R],
+                                                 ub: FromEntityUnmarshaller[R], leftTag: ClassTag[L]): FromEntityUnmarshaller[Either[L, R]] =
+    Unmarshaller.withMaterializer { implicit ex ⇒ implicit mat ⇒ value ⇒
+      import akka.http.scaladsl.util.FastFuture._
+      @inline def right = ub(value).fast.map(Right(_))
+      @inline def fallbackLeft: PartialFunction[Throwable, Future[Either[L, R]]] = { case rightFirstEx ⇒
+        val left = ua(value).fast.map(Left(_))
+
+        // combine EitherUnmarshallingException by carring both exceptions
+        left.transform(
+          s = ConstantFun.scalaIdentityFunction,
+          f = leftSecondEx => new EitherUnmarshallingException(
+            rightClass = rightTag.runtimeClass, right = rightFirstEx,
+            leftClass = leftTag.runtimeClass, left = leftSecondEx)
+          )
+      }
+
+      right.recoverWith(fallbackLeft)
+    }
+  // format: ON
+
 }
