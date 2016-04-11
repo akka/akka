6740af9d9c8692827b4b42d77a359309ec5ad286
Johan Andrén <johan@markatta.com>
2016-04-11 17:56:10 +0200

    doc,io #16262 UDP multicast tests work on all platforms

--- akka-docs/rst/java/code/docs/io/JavaUdpMulticastTest.java
+++ akka-docs/rst/java/code/docs/io/JavaUdpMulticastTest.java
@@ -17,13 +17,10 @@ import org.junit.Test;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
-import java.util.Enumeration;
-import java.util.Random;
+import java.util.*;
 
-// not part of the test suite because we have not figured out
-// a way to find an interface that is sure to work on all platforms
-// to listen for udp on
-public class JavaUdpMulticastTest {
+
+public class JavaUdpMulticastTest extends AbstractJavaTest {
 
     static ActorSystem system;
 
@@ -35,43 +32,61 @@ public class JavaUdpMulticastTest {
     @Test
     public void testUdpMulticast() throws Exception {
         new JavaTestKit(system) {{
-            NetworkInterface ipv6Iface = null;
-            for (Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces(); interfaces.hasMoreElements() && ipv6Iface == null;) {
+            List<NetworkInterface> ipv6Ifaces = new ArrayList<>();
+            for (Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces(); interfaces.hasMoreElements(); ) {
                 NetworkInterface interf = interfaces.nextElement();
-                // awdl0 is a special interface on OSX that we cannot use
-                if (!interf.getDisplayName().equals("awdl0") &&
-                        // we do not want to use virtual docker interfaces
-                        !interf.getDisplayName().contains("docker")) {
-                    for (Enumeration<InetAddress> addresses = interf.getInetAddresses(); addresses.hasMoreElements() && ipv6Iface == null; ) {
+                if (interf.isUp() && interf.supportsMulticast()) {
+                    for (Enumeration<InetAddress> addresses = interf.getInetAddresses(); addresses.hasMoreElements(); ) {
                         InetAddress address = addresses.nextElement();
                         if (address instanceof Inet6Address) {
-                            ipv6Iface = interf;
+                            ipv6Ifaces.add(interf);
                         }
                     }
                 }
             }
+            if (ipv6Ifaces.isEmpty()) {
+                system.log().info("JavaUdpMulticastTest skipped since no ipv6 interface supporting multicast could be found");
+            } else {
+                // lots of problems with choosing the wrong interface for this test depending
+                // on the platform (awsdl0 can't be used on OSX, docker[0-9] can't be used in a docker machine etc.)
+                // therefore: try hard to find an interface that _does_ work, and only fail if there was any potentially
+                // working interfaces but all failed
+                for (Iterator<NetworkInterface> interfaceIterator = ipv6Ifaces.iterator(); interfaceIterator.hasNext(); ) {
+                    NetworkInterface ipv6Iface = interfaceIterator.next();
+                    // host assigned link local multicast address http://tools.ietf.org/html/rfc3307#section-4.3.2
+                    // generate a random 32 bit multicast address with the high order bit set
+                    final String randomAddress = Long.toHexString(((long) Math.abs(new Random().nextInt())) | (1L << 31)).toUpperCase();
+                    final StringBuilder groupBuilder = new StringBuilder("FF02:");
+                    for (int i = 0; i < 2; i += 1) {
+                        groupBuilder.append(":");
+                        groupBuilder.append(randomAddress.subSequence(i * 4, i * 4 + 4));
+                    }
+                    final String group = groupBuilder.toString();
+                    final Integer port = TestUtils.temporaryUdpIpv6Port(ipv6Iface);
+                    final String msg = "ohi";
+                    final ActorRef sink = getRef();
+                    final String iface = ipv6Iface.getName();
 
-            // host assigned link local multicast address http://tools.ietf.org/html/rfc3307#section-4.3.2
-            // generate a random 32 bit multicast address with the high order bit set
-            final String randomAddress = Long.toHexString(((long) Math.abs(new Random().nextInt())) | (1L << 31)).toUpperCase();
-            final StringBuilder groupBuilder = new StringBuilder("FF02:");
-            for (int i = 0; i < 2; i += 1) {
-                groupBuilder.append(":");
-                groupBuilder.append(randomAddress.subSequence(i * 4, i * 4 + 4));
+                    final ActorRef listener = system.actorOf(Props.create(JavaUdpMulticast.Listener.class, iface, group, port, sink));
+
+                    try {
+                        expectMsgClass(Udp.Bound.class);
+                        final ActorRef sender = system.actorOf(Props.create(JavaUdpMulticast.Sender.class, iface, group, port, msg));
+                        expectMsgEquals(msg);
+                        // success with one interface is enough
+                        break;
+
+                    } catch (AssertionError ex) {
+                        if (!interfaceIterator.hasNext()) throw ex;
+                        else {
+                            system.log().info("Failed to run test on interface {}", ipv6Iface.getDisplayName());
+                        }
+                    } finally {
+                        // unbind
+                        system.stop(listener);
+                    }
+                }
             }
-            final String group = groupBuilder.toString();
-            final Integer port = TestUtils.temporaryUdpIpv6Port(ipv6Iface);
-            final String msg = "ohi";
-            final ActorRef sink = getRef();
-            final String iface = ipv6Iface.getName();
-
-            final ActorRef listener = system.actorOf(Props.create(JavaUdpMulticast.Listener.class, iface, group, port, sink));
-            expectMsgClass(Udp.Bound.class);
-            final ActorRef sender = system.actorOf(Props.create(JavaUdpMulticast.Sender.class, iface, group, port, msg));
-            expectMsgEquals(msg);
-
-            // unbind
-            system.stop(listener);
         }};
     }
 
