dd4efc483bbe9d6594e99d640109a001805d4143
leonidb <leonid.bakaleynik@gmail.com>
2016-04-18 23:58:42 +0300

    Added deprecation warning

--- akka-persistence/src/main/scala/akka/persistence/fsm/PersistentFSMBase.scala
+++ akka-persistence/src/main/scala/akka/persistence/fsm/PersistentFSMBase.scala
@@ -308,6 +308,7 @@ trait PersistentFSMBase[S, D, E] extends Actor with Listeners with ActorLogging 
    *
    * @see [[akka.persistence.fsm.PersistentFSM#receiveRecover]]
    */
+  @deprecated("Removed from API, called internally", "2.4.5")
   private[akka] final def initialize(): Unit =
     if (currentState != null) makeTransition(currentState)
     else throw new IllegalStateException("You must call `startWith` before calling `initialize`")
