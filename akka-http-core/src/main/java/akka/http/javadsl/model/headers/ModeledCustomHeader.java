8e4369bec77c0c08d5946a5f2d2b30b6e2d56a3a
Johan Andrén <johan@markatta.com>
2016-04-28 12:58:11 +0200

    Custom headers in Java DSL

---
+++ akka-http-core/src/main/java/akka/http/javadsl/model/headers/ModeledCustomHeader.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2009-2016 Lightbend Inc. <http://www.lightbend.com>
+ */
+package akka.http.javadsl.model.headers;
+
+import akka.http.impl.util.Rendering;
+
+public abstract class ModeledCustomHeader extends CustomHeader {
+
+  @Override
+  public boolean renderInRequests() {
+    return true;
+  }
+
+  @Override
+  public boolean renderInResponses() {
+    return true;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  final public <R extends Rendering> R render(R r) {
+    return (R) r.$tilde$tilde(name()).$tilde$tilde(':').$tilde$tilde(' ').$tilde$tilde(value());
+  }
+
+  @Override
+  public String lowercaseName() {
+    return name().toLowerCase();
+  }
+}
