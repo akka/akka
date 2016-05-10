9e66e2f5d9cd3ea6b9acab3f5b0570702e4bf8de
Johan Andrén <johan@markatta.com>
2016-05-10 14:46:05 +0200

    wip re-enabling to see how/if it still is racy

--- project/AkkaBuild.scala
+++ project/AkkaBuild.scala
@@ -271,7 +271,7 @@ object AkkaBuild extends Build {
   lazy val httpTests = Project(
     id = "akka-http-tests",
     base = file("akka-http-tests"),
-    dependencies = Seq(httpTestkit % "test", testkit % "test->test", httpSprayJson, httpXml, httpJackson)
+    dependencies = Seq(httpTestkit % "test", streamTestkit % "test->test", testkit % "test->test", httpSprayJson, httpXml, httpJackson)
   )
 
   lazy val httpMarshallersScala = Project(
