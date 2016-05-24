1a6dbe42086ec859293408387a70d6a1ae0a3bb7
Nikolay Donets <nd.startup@gmail.com>
2016-05-24 17:10:08 +0300

    fix types and conversions in the InvalidOriginHeaderRejection

--- akka-http/src/main/scala/akka/http/scaladsl/server/Rejection.scala
+++ akka-http/src/main/scala/akka/http/scaladsl/server/Rejection.scala
@@ -20,7 +20,8 @@ import akka.http.impl.util.JavaMapping.Implicits._
 import akka.http.javadsl.server.RoutingJavaMapping
 import RoutingJavaMapping._
 import akka.pattern.CircuitBreakerOpenException
-import akka.http.javadsl.model.headers.HttpOrigin
+import akka.http.javadsl.model.headers.{ HttpOrigin ⇒ JHttpOrigin }
+import akka.http.scaladsl.model.headers.{ HttpOrigin ⇒ SHttpOrigin }
 
 import scala.collection.JavaConverters._
 import scala.compat.java8.OptionConverters
@@ -97,9 +98,9 @@ final case class MalformedHeaderRejection(headerName: String, errorMsg: String, 
  * Rejection created by [[akka.http.scaladsl.server.directives.HeaderDirectives.checkSameOrigin]].
  * Signals that the request was rejected because `Origin` header value is invalid.
  */
-final case class InvalidOriginHeaderRejection(invalidOrigins: immutable.Seq[HttpOrigin])
+final case class InvalidOriginHeaderRejection(invalidOrigins: immutable.Seq[SHttpOrigin])
   extends jserver.InvalidOriginHeaderRejection with Rejection {
-  override def getInvalidOrigins: java.util.List[HttpOrigin] = invalidOrigins.asJava
+  override def getInvalidOrigins: java.util.List[JHttpOrigin] = invalidOrigins.map(_.asJava).asJava
 }
 
 /**
