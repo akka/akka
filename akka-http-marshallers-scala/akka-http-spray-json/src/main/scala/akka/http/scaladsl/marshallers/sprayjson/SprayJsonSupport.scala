b52236c153221c91bbf4c595896d69254fd01b40
Konrad Malawski <konrad.malawski@project13.pl>
2016-07-29 15:21:19 +0200

    =htp introduce ToByteStringMarshaller alias and fix fixme

--- akka-http-marshallers-scala/akka-http-spray-json/src/main/scala/akka/http/scaladsl/marshallers/sprayjson/SprayJsonSupport.scala
+++ akka-http-marshallers-scala/akka-http-spray-json/src/main/scala/akka/http/scaladsl/marshallers/sprayjson/SprayJsonSupport.scala
@@ -8,9 +8,9 @@ import akka.http.scaladsl.util.FastFuture
 import akka.util.ByteString
 
 import scala.language.implicitConversions
-import akka.http.scaladsl.marshalling.{ ToEntityMarshaller, Marshaller }
-import akka.http.scaladsl.unmarshalling.{ FromEntityUnmarshaller, Unmarshaller }
-import akka.http.scaladsl.model.{ ContentTypes, MediaTypes, HttpCharsets }
+import akka.http.scaladsl.marshalling.{Marshaller, ToByteStringMarshaller, ToEntityMarshaller}
+import akka.http.scaladsl.unmarshalling.{FromEntityUnmarshaller, Unmarshaller}
+import akka.http.scaladsl.model.{ContentTypes, HttpCharsets, MediaTypes}
 import akka.http.scaladsl.model.MediaTypes.`application/json`
 import spray.json._
 
@@ -30,7 +30,7 @@ trait SprayJsonSupport {
     Unmarshaller.byteStringUnmarshaller.forContentTypes(`application/json`).mapWithCharset { (data, charset) ⇒
       val input =
         if (charset == HttpCharsets.`UTF-8`) ParserInput(data.toArray)
-        else ParserInput(data.decodeString(charset.nioCharset.name)) // FIXME: identify charset by instance, not by name!
+        else ParserInput(data.decodeString(charset.nioCharset))
       JsonParser(input)
     }
 
@@ -40,7 +40,7 @@ trait SprayJsonSupport {
     sprayJsValueMarshaller compose writer.write
   implicit def sprayJsValueMarshaller(implicit printer: JsonPrinter = CompactPrinter): ToEntityMarshaller[JsValue] =
     Marshaller.StringMarshaller.wrap(MediaTypes.`application/json`)(printer)
-  implicit def sprayByteStringMarshaller[T](implicit writer: RootJsonFormat[T], printer: JsonPrinter = CompactPrinter): Marshaller[T, ByteString] =
+  implicit def sprayByteStringMarshaller[T](implicit writer: RootJsonFormat[T], printer: JsonPrinter = CompactPrinter): ToByteStringMarshaller[T] =
     sprayJsValueMarshaller.map(s ⇒ ByteString(s.toString)) compose writer.write
 }
 object SprayJsonSupport extends SprayJsonSupport
