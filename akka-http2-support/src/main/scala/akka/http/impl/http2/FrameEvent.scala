d7cc5e6aedde9fe96ffce7f210efc28ecf12fbde
Johannes Rudolph <johannes.rudolph@gmail.com>
2016-09-14 12:21:24 +0200

    =ht2 define some FrameEvents and add parser for them

--- akka-http2-support/src/main/scala/akka/http/impl/http2/FrameEvent.scala
+++ akka-http2-support/src/main/scala/akka/http/impl/http2/FrameEvent.scala
@@ -1,4 +1,31 @@
 package akka.http.impl.http2
 
+import akka.http.impl.http2.Protocol.FrameType
+import akka.http.impl.http2.Protocol.SettingIdentifier
+import akka.util.ByteString
+
 sealed trait FrameEvent
-sealed trait StreamEvent extends FrameEvent
+sealed trait StreamFrameEvent extends FrameEvent {
+  def streamId: Int
+}
+
+case class GenericEvent(tpe: FrameType, flags: Int, streamId: Int, payload: ByteString) extends StreamFrameEvent
+
+case class DataFrame(streamId: Int /* TODO: finish */ ) extends StreamFrameEvent
+case class HeadersFrame(
+  streamId:            Int,
+  endStream:           Boolean,
+  endHeaders:          Boolean,
+  headerBlockFragment: ByteString) extends FrameEvent
+
+//case class PriorityFrame(streamId: Int, streamDependency: Int, weight: Int) extends StreamFrameEvent
+//case class RstStreamFrame(streamId: Int, errorCode: Int) extends StreamFrameEvent
+case class SettingsFrame(settings: Seq[Setting]) extends FrameEvent
+case object SettingsAckFrame extends FrameEvent
+//case class PushPromiseFrame(streamId: Int) extends StreamFrameEvent
+//case class PingFrame(streamId: Int) extends StreamFrameEvent
+//case class GoAwayFrame(streamId: Int) extends StreamFrameEvent
+case class WindowUpdateFrame(streamId: Int, windowSizeIncrement: Int) extends StreamFrameEvent
+case class ContinuationFrame(streamId: Int, endHeaders: Boolean, payload: ByteString) extends StreamFrameEvent
+
+case class Setting(identifier: SettingIdentifier, value: Int)
--- akka-http2-support/src/main/scala/akka/http/impl/http2/FrameParser.scala
+++ akka-http2-support/src/main/scala/akka/http/impl/http2/FrameParser.scala
@@ -1,5 +1,7 @@
 package akka.http.impl.http2
 
+import akka.http.impl.http2.Protocol._
+import FrameType._
 import akka.stream.Attributes
 import akka.stream.impl.io.ByteStringParser
 import akka.stream.stage.GraphStageLogic
@@ -11,13 +13,91 @@ object FrameParser extends ByteStringParser[FrameEvent] {
 
   def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
     new ParsingLogic {
-      startWith(new ReadFrame)
+      startWith(ReadPreface)
 
-      class ReadFrame extends Step {
+      object ReadPreface extends Step {
+        def parse(reader: ByteReader): ParseResult[FrameEvent] =
+          if (reader.remainingSize < 24) throw NeedMoreData
+          else if (reader.take(24) == Protocol.ConnectionPreface)
+            ParseResult(None, ReadFrame, false)
+          else
+            throw new RuntimeException("Expected ConnectionPreface!")
+
+      }
+
+      object ReadFrame extends Step {
         def parse(reader: ByteReader): ParseResult[FrameEvent] = {
-          println(s"Have ${reader.remainingData} remaining")
-          throw NeedMoreData
+          val length = reader.readShortBE() << 8 | reader.readByte()
+          val tpe = reader.readByte() // TODO: make sure it's valid
+          val flags = reader.readByte()
+          val streamId = reader.readIntBE()
+          // TODO: assert that reserved bit is 0 by checking if streamId > 0
+          val payload = reader.take(length)
+          val frame = parseFrame(FrameType.byId(tpe), flags, streamId, new ByteReader(payload))
+
+          ParseResult(Some(frame), ReadFrame, false)
         }
       }
     }
+
+  def parseFrame(tpe: FrameType, flags: Int, streamId: Int, payload: ByteReader): FrameEvent = {
+    def isSet(flag: Int): Boolean = (flags & flag) != 0
+
+    tpe match {
+      case HEADERS ⇒
+        val pad = isSet(Flags.PADDED)
+        val endStream = isSet(Flags.END_STREAM)
+        val endHeaders = isSet(Flags.END_HEADERS)
+        val priority = isSet(Flags.PRIORITY)
+
+        val paddingLength =
+          if (pad) payload.readByte() & 0xff
+          else 0
+        val dependencyAndE =
+          if (priority) payload.readLongBE()
+          else 0
+        val weight =
+          if (priority) payload.readByte() & 0xff
+          else 0
+
+        // TODO: check that streamId != 0
+        // TODO: also write out Priority frame if priority was set
+        HeadersFrame(streamId, endStream, endHeaders, payload.take(payload.remainingSize - paddingLength))
+
+      case SETTINGS ⇒
+        val ack = isSet(Flags.ACK)
+
+        // TODO: validate that streamId = 0
+
+        if (ack) SettingsAckFrame // TODO: validate that payload is empty
+        else {
+          def readSettings(read: List[Setting]): Seq[Setting] =
+            if (payload.hasRemaining) {
+              // TODO: fail if remaining size isn't exactly 3
+              val id = payload.readShortBE()
+              val value = payload.readIntBE()
+              Setting(SettingIdentifier.byId(id), value) :: read
+            } else read.reverse
+
+          SettingsFrame(readSettings(Nil))
+        }
+
+      case WINDOW_UPDATE ⇒
+        // TODO: check frame size
+        // TODO: check flags
+        // TODO: check reserved flag
+        // TODO: check that increment is > 0
+        val increment = payload.readIntBE()
+        WindowUpdateFrame(streamId, increment)
+
+      case CONTINUATION ⇒
+        val endHeaders = isSet(Flags.END_HEADERS)
+        // TODO: check that streamId > 0
+
+        ContinuationFrame(streamId, endHeaders, payload.remainingData)
+
+      case tpe ⇒ // TODO: remove once all stream types are defined
+        GenericEvent(tpe, flags, streamId, payload.remainingData)
+    }
+  }
 }
