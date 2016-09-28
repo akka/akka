e61ffd1132092514c1c3fda8f53fc04711227b7b
Patrik Nordwall <patrik.nordwall@gmail.com>
2016-09-28 11:14:33 +0200

    protobuf serialization of Status.Succes, Status.Failure, #21202 (#21565)
    * also support for serialization of exceptions, see
      comment in reference.conf
    
    * extract Throwable and Payload methods to helper classes
    
    * add security checks before creating instance from class name

---
+++ akka-remote/src/main/scala/akka/remote/serialization/WrappedPayloadSupport.scala
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2016 Lightbend Inc. <http://www.lightbend.com>
+ */
+package akka.remote.serialization
+
+import akka.actor.ExtendedActorSystem
+import akka.remote.ContainerFormats
+import akka.serialization.SerializationExtension
+import akka.serialization.SerializerWithStringManifest
+import akka.protobuf.ByteString
+
+/**
+ * INTERNAL API
+ */
+private[akka] class WrappedPayloadSupport(system: ExtendedActorSystem) {
+
+  private lazy val serialization = SerializationExtension(system)
+
+  def payloadBuilder(input: Any): ContainerFormats.Payload.Builder = {
+    val payload = input.asInstanceOf[AnyRef]
+    val builder = ContainerFormats.Payload.newBuilder()
+    val serializer = serialization.findSerializerFor(payload)
+
+    builder
+      .setEnclosedMessage(ByteString.copyFrom(serializer.toBinary(payload)))
+      .setSerializerId(serializer.identifier)
+
+    serializer match {
+      case ser2: SerializerWithStringManifest ⇒
+        val manifest = ser2.manifest(payload)
+        if (manifest != "")
+          builder.setMessageManifest(ByteString.copyFromUtf8(manifest))
+      case _ ⇒
+        if (serializer.includeManifest)
+          builder.setMessageManifest(ByteString.copyFromUtf8(payload.getClass.getName))
+    }
+
+    builder
+  }
+
+  def deserializePayload(payload: ContainerFormats.Payload): Any = {
+    val manifest = if (payload.hasMessageManifest) payload.getMessageManifest.toStringUtf8 else ""
+    serialization.deserialize(
+      payload.getEnclosedMessage.toByteArray,
+      payload.getSerializerId,
+      manifest).get
+  }
+
+}
