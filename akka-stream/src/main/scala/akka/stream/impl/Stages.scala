d3accc251863a664efaf85332d4ef88a4f3ffeac
Patrik Nordwall <patrik.nordwall@gmail.com>
2016-11-30 20:43:07 +0100

    make IODispatcher attribute public
    (cherry picked from commit c16e12ffeb4b1292159bb606203682716c48aea8)

--- akka-stream/src/main/scala/akka/stream/impl/Stages.scala
+++ akka-stream/src/main/scala/akka/stream/impl/Stages.scala
@@ -16,7 +16,7 @@ import akka.stream.stage.Stage
 object Stages {
 
   object DefaultAttributes {
-    val IODispatcher = ActorAttributes.Dispatcher("akka.stream.default-blocking-io-dispatcher")
+    val IODispatcher = ActorAttributes.IODispatcher
     val inputBufferOne = inputBuffer(initial = 1, max = 1)
 
     val fused = name("fused")
