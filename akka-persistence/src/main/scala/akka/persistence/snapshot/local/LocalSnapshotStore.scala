0b1171a17ea30b1dd9d7503485872fed829c311f
Konrad `ktoso` Malawski <ktoso@project13.pl>
2017-01-04 15:04:29 +0100

    Update LocalSnapshotStore.scala

--- akka-persistence/src/main/scala/akka/persistence/snapshot/local/LocalSnapshotStore.scala
+++ akka-persistence/src/main/scala/akka/persistence/snapshot/local/LocalSnapshotStore.scala
@@ -164,16 +164,18 @@ private[persistence] class LocalSnapshotStore(config: Config) extends SnapshotSt
   }
 
   private def extractMetadata(filename: String): Option[(String, Long, Long)] = {
-    val sequenceNumberEndIdx = filename.lastIndexOf('-')
-    val persistenceIdEndIdx = filename.lastIndexOf('-', sequenceNumberEndIdx - 1)
-    val timestampString = filename.substring(sequenceNumberEndIdx + 1)
-    if (persistenceIdStartIdx >= persistenceIdEndIdx || timestampString.exists(!_.isDigit)) None
-    else {
-      val persistenceId = filename.substring(persistenceIdStartIdx, persistenceIdEndIdx)
-      val sequenceNumber = filename.substring(persistenceIdEndIdx + 1, sequenceNumberEndIdx).toLong
-      val timestamp = filename.substring(sequenceNumberEndIdx + 1).toLong
-      Some((persistenceId, sequenceNumber, timestamp))
-    }
+    Try {
+      val sequenceNumberEndIdx = filename.lastIndexOf('-')
+      val persistenceIdEndIdx = filename.lastIndexOf('-', sequenceNumberEndIdx - 1)
+      val timestampString = filename.substring(sequenceNumberEndIdx + 1)
+      if (persistenceIdStartIdx >= persistenceIdEndIdx || timestampString.exists(!_.isDigit)) None
+      else {
+        val persistenceId = filename.substring(persistenceIdStartIdx, persistenceIdEndIdx)
+        val sequenceNumber = filename.substring(persistenceIdEndIdx + 1, sequenceNumberEndIdx).toLong
+        val timestamp = filename.substring(sequenceNumberEndIdx + 1).toLong
+        Some((persistenceId, sequenceNumber, timestamp))
+      }
+    }.toOption
   }
 }
 
