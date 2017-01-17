70e2cd71e1af1c712507731fbe2557d308cbce34
Konrad Malawski <konrad.malawski@project13.pl>
2017-01-17 18:18:51 +0100

    =ht2 add more test infrastructure in Http2ServerSpec
    added:
    
     * expectSettingsAck
     * expectFrame
     * sendFrame
     * sendSETTING

--- akka-http2-support/src/test/scala/akka/http/impl/engine/http2/Http2ServerSpec.scala
+++ akka-http2-support/src/test/scala/akka/http/impl/engine/http2/Http2ServerSpec.scala
@@ -5,9 +5,7 @@ import java.io.ByteArrayOutputStream
 import java.nio.ByteOrder
 
 import akka.NotUsed
-import akka.http.impl.engine.http2.Http2Protocol.ErrorCode
-import akka.http.impl.engine.http2.Http2Protocol.Flags
-import akka.http.impl.engine.http2.Http2Protocol.FrameType
+import akka.http.impl.engine.http2.Http2Protocol.{ ErrorCode, Flags, FrameType, SettingIdentifier }
 import akka.http.impl.engine.http2.framing.FrameRenderer
 import akka.http.impl.engine.http2.hpack.HeaderDecompression
 import akka.http.impl.engine.ws.ByteStringSinkProbe
@@ -640,6 +638,11 @@ class Http2ServerSpec extends AkkaSpec with WithInPendingUntilFixed with Eventua
       (lastStreamId, ErrorCode.byId(reader.readIntBE()))
     }
 
+    def expectSettingsAck() = expectFrame(FrameType.SETTINGS, Flags.ACK, 0, ByteString.empty)
+
+    def expectFrame(frameType: FrameType, expectedFlags: ByteFlag, streamId: Int, payload: ByteString) =
+      expectFramePayload(frameType, expectedFlags, streamId) should be === payload
+
     @tailrec
     final def expectFrameFlagsAndPayload(frameType: FrameType, streamId: Int): (ByteFlag, ByteString) = {
       val header = expectFrameHeader()
@@ -677,11 +680,17 @@ class Http2ServerSpec extends AkkaSpec with WithInPendingUntilFixed with Eventua
       // FIXME: also collect CONTINUATION frames as long as END_HEADERS is not set
       expectFramePayload(FrameType.HEADERS, Flags.END_STREAM.ifSet(endStream) | Flags.END_HEADERS, streamId)
 
+    def sendFrame(frame: FrameEvent): Unit =
+      sendBytes(FrameRenderer.render(frame))
+
     def sendFrame(frameType: FrameType, flags: ByteFlag, streamId: Int, payload: ByteString): Unit =
       sendBytes(FrameRenderer.renderFrame(frameType, flags, streamId, payload))
 
     def sendDATA(streamId: Int, endStream: Boolean, data: ByteString): Unit =
       sendFrame(FrameType.DATA, Flags.END_STREAM.ifSet(endStream), streamId, data)
+
+    def sendSETTING(identifier: SettingIdentifier, value: Int): Unit =
+      sendFrame(SettingsFrame(Setting(identifier, value) :: Nil))
 
     def sendHEADERS(streamId: Int, endStream: Boolean, endHeaders: Boolean, headerBlockFragment: ByteString): Unit =
       sendBytes(FrameRenderer.render(HeadersFrame(streamId, endStream, endHeaders, headerBlockFragment, None)))
