6189b6ae6c1b5ec209b4cc128814007eb40e20d9
Konrad Malawski <konrad.malawski@project13.pl>
2017-01-17 17:58:19 +0100

    =ht2 move parseFrame into GraphStageLogic

--- akka-http2-support/src/main/scala/akka/http/impl/engine/http2/framing/Http2FrameParsing.scala
+++ akka-http2-support/src/main/scala/akka/http/impl/engine/http2/framing/Http2FrameParsing.scala
@@ -49,117 +49,117 @@ private[http2] class Http2FrameParsing(shouldReadPreface: Boolean) extends ByteS
           ParseResult(Some(frame), ReadFrame, acceptUpstreamFinish = true)
         }
       }
-    }
 
-  def parseFrame(tpe: FrameType, flags: ByteFlag, streamId: Int, payload: ByteReader): FrameEvent = {
-    // TODO: add @switch? seems non-trivial for now
-    tpe match {
-      case GOAWAY ⇒
-        Http2Compliance.requireZeroStreamId(streamId)
-        GoAwayFrame(payload.readIntBE(), ErrorCode.byId(payload.readIntBE()), payload.takeAll())
+      def parseFrame(tpe: FrameType, flags: ByteFlag, streamId: Int, payload: ByteReader): FrameEvent = {
+        // TODO: add @switch? seems non-trivial for now
+        tpe match {
+          case GOAWAY ⇒
+            Http2Compliance.requireZeroStreamId(streamId)
+            GoAwayFrame(payload.readIntBE(), ErrorCode.byId(payload.readIntBE()), payload.takeAll())
 
-      case HEADERS ⇒
-        val pad = Flags.PADDED.isSet(flags)
-        val endStream = Flags.END_STREAM.isSet(flags)
-        val endHeaders = Flags.END_HEADERS.isSet(flags)
-        val priority = Flags.PRIORITY.isSet(flags)
+          case HEADERS ⇒
+            val pad = Flags.PADDED.isSet(flags)
+            val endStream = Flags.END_STREAM.isSet(flags)
+            val endHeaders = Flags.END_HEADERS.isSet(flags)
+            val priority = Flags.PRIORITY.isSet(flags)
 
-        val paddingLength =
-          if (pad) payload.readByte() & 0xff
-          else 0
+            val paddingLength =
+              if (pad) payload.readByte() & 0xff
+              else 0
 
-        val priorityInfo =
-          if (priority) {
-            val dependencyAndE = payload.readIntBE()
-            val weight = payload.readByte() & 0xff
+            val priorityInfo =
+              if (priority) {
+                val dependencyAndE = payload.readIntBE()
+                val weight = payload.readByte() & 0xff
 
-            val exclusiveFlag = (dependencyAndE >>> 31) == 1 // most significant bit for exclusive flag
-            val dependencyId = dependencyAndE & 0x7fffffff // remaining 31 bits for the dependency part
+                val exclusiveFlag = (dependencyAndE >>> 31) == 1 // most significant bit for exclusive flag
+                val dependencyId = dependencyAndE & 0x7fffffff // remaining 31 bits for the dependency part
+                Http2Compliance.requireNoSelfDependency(streamId, dependencyId)
+                Some(PriorityFrame(streamId, exclusiveFlag, dependencyId, weight))
+              } else
+                None
+
+            HeadersFrame(streamId, endStream, endHeaders, payload.take(payload.remainingSize - paddingLength), priorityInfo)
+
+          case DATA ⇒
+            val pad = Flags.PADDED.isSet(flags)
+            val endStream = Flags.END_STREAM.isSet(flags)
+
+            val paddingLength =
+              if (pad) payload.readByte() & 0xff
+              else 0
+
+            DataFrame(streamId, endStream, payload.take(payload.remainingSize - paddingLength))
+
+          case SETTINGS ⇒
+            val ack = Flags.ACK.isSet(flags)
+            Http2Compliance.requireZeroStreamId(streamId)
+
+            if (ack) {
+              // validate that payload is empty: (6.5)
+              if (payload.hasRemaining)
+                throw new Http2Compliance.IllegalPayloadInSettingsAckFrame(payload.remainingSize, s"SETTINGS ACK frame MUST NOT contain payload (spec 6.5)!")
+
+              SettingsAckFrame
+            } else {
+              def readSettings(read: List[Setting]): immutable.Seq[Setting] =
+                if (payload.hasRemaining) {
+                  val id = payload.readShortBE()
+                  val value = payload.readIntBE()
+                  readSettings(Setting(SettingIdentifier.byId(id), value) :: read)
+                } else read.reverse
+
+              if (payload.remainingSize % 6 != 0) throw new Http2Compliance.IllegalPayloadLengthInSettingsFrame(payload.remainingSize, "SETTINGS payload MUDT be a multiple of multiple of 6 octets")
+              SettingsFrame(readSettings(Nil))
+            }
+
+          case WINDOW_UPDATE ⇒
+            // TODO: check frame size
+            // TODO: check flags
+            // TODO: check reserved flag
+            // TODO: check that increment is > 0
+            val increment = payload.readIntBE()
+            WindowUpdateFrame(streamId, increment)
+
+          case CONTINUATION ⇒
+            val endHeaders = Flags.END_HEADERS.isSet(flags)
+
+            ContinuationFrame(streamId, endHeaders, payload.remainingData)
+
+          case PING ⇒
+            Http2Compliance.requireFrameSize(payload.remainingSize, 8)
+            Http2Compliance.requireZeroStreamId(streamId)
+            val ack = Flags.ACK.isSet(flags)
+            PingFrame(ack, payload.remainingData)
+
+          case RST_STREAM ⇒
+            Http2Compliance.requireFrameSize(payload.remainingSize, 4)
+            RstStreamFrame(streamId, ErrorCode.byId(payload.readIntBE()))
+
+          case PRIORITY ⇒
+            Http2Compliance.requireFrameSize(payload.remainingSize, 5)
+            val streamDependency = payload.readIntBE() // whole word
+            val exclusiveFlag = (streamDependency >>> 31) == 1 // most significant bit for exclusive flag
+            val dependencyId = streamDependency & 0x7fffffff // remaining 31 bits for the dependency part
+            val priority = payload.readByte() & 0xff
             Http2Compliance.requireNoSelfDependency(streamId, dependencyId)
-            Some(PriorityFrame(streamId, exclusiveFlag, dependencyId, weight))
-          } else
-            None
+            PriorityFrame(streamId, exclusiveFlag, dependencyId, priority)
 
-        HeadersFrame(streamId, endStream, endHeaders, payload.take(payload.remainingSize - paddingLength), priorityInfo)
+          case PUSH_PROMISE ⇒
+            val pad = Flags.PADDED.isSet(flags)
+            val endHeaders = Flags.END_HEADERS.isSet(flags)
 
-      case DATA ⇒
-        val pad = Flags.PADDED.isSet(flags)
-        val endStream = Flags.END_STREAM.isSet(flags)
+            val paddingLength =
+              if (pad) payload.readByte() & 0xff
+              else 0
 
-        val paddingLength =
-          if (pad) payload.readByte() & 0xff
-          else 0
+            val promisedStreamId = payload.readIntBE()
 
-        DataFrame(streamId, endStream, payload.take(payload.remainingSize - paddingLength))
+            PushPromiseFrame(streamId, endHeaders, promisedStreamId, payload.take(payload.remainingSize - paddingLength))
 
-      case SETTINGS ⇒
-        val ack = Flags.ACK.isSet(flags)
-        Http2Compliance.requireZeroStreamId(streamId)
-
-        if (ack) {
-          // validate that payload is empty: (6.5)
-          if (payload.hasRemaining)
-            throw new Http2Compliance.IllegalPayloadInSettingsAckFrame(payload.remainingSize, s"SETTINGS ACK frame MUST NOT contain payload (spec 6.5)!")
-
-          SettingsAckFrame
-        } else {
-          def readSettings(read: List[Setting]): immutable.Seq[Setting] =
-            if (payload.hasRemaining) {
-              val id = payload.readShortBE()
-              val value = payload.readIntBE()
-              readSettings(Setting(SettingIdentifier.byId(id), value) :: read)
-            } else read.reverse
-
-          if (payload.remainingSize % 6 != 0) throw new Http2Compliance.IllegalPayloadLengthInSettingsFrame(payload.remainingSize, "SETTINGS payload MUDT be a multiple of multiple of 6 octets")
-          SettingsFrame(readSettings(Nil))
+          case tpe ⇒ // TODO: remove once all stream types are defined
+            UnknownFrameEvent(tpe, flags, streamId, payload.remainingData)
         }
-
-      case WINDOW_UPDATE ⇒
-        // TODO: check frame size
-        // TODO: check flags
-        // TODO: check reserved flag
-        // TODO: check that increment is > 0
-        val increment = payload.readIntBE()
-        WindowUpdateFrame(streamId, increment)
-
-      case CONTINUATION ⇒
-        val endHeaders = Flags.END_HEADERS.isSet(flags)
-
-        ContinuationFrame(streamId, endHeaders, payload.remainingData)
-
-      case PING ⇒
-        Http2Compliance.requireFrameSize(payload.remainingSize, 8)
-        Http2Compliance.requireZeroStreamId(streamId)
-        val ack = Flags.ACK.isSet(flags)
-        PingFrame(ack, payload.remainingData)
-
-      case RST_STREAM ⇒
-        Http2Compliance.requireFrameSize(payload.remainingSize, 4)
-        RstStreamFrame(streamId, ErrorCode.byId(payload.readIntBE()))
-
-      case PRIORITY ⇒
-        Http2Compliance.requireFrameSize(payload.remainingSize, 5)
-        val streamDependency = payload.readIntBE() // whole word
-        val exclusiveFlag = (streamDependency >>> 31) == 1 // most significant bit for exclusive flag
-        val dependencyId = streamDependency & 0x7fffffff // remaining 31 bits for the dependency part
-        val priority = payload.readByte() & 0xff
-        Http2Compliance.requireNoSelfDependency(streamId, dependencyId)
-        PriorityFrame(streamId, exclusiveFlag, dependencyId, priority)
-
-      case PUSH_PROMISE ⇒
-        val pad = Flags.PADDED.isSet(flags)
-        val endHeaders = Flags.END_HEADERS.isSet(flags)
-
-        val paddingLength =
-          if (pad) payload.readByte() & 0xff
-          else 0
-
-        val promisedStreamId = payload.readIntBE()
-
-        PushPromiseFrame(streamId, endHeaders, promisedStreamId, payload.take(payload.remainingSize - paddingLength))
-
-      case tpe ⇒ // TODO: remove once all stream types are defined
-        UnknownFrameEvent(tpe, flags, streamId, payload.remainingData)
+      }
     }
-  }
 }
