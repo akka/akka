ac6786632047fe6e53cbdd0cf224e48d35576caa
snaef <sebastian.naefe@esailors.de>
2017-01-23 12:37:09 +0100

    Include debug for Some where entity already being processed

--- akka-cluster-sharding/src/main/scala/akka/cluster/sharding/Shard.scala
+++ akka-cluster-sharding/src/main/scala/akka/cluster/sharding/Shard.scala
@@ -220,14 +220,16 @@ private[akka] class Shard(
 
   def passivate(entity: ActorRef, stopMessage: Any): Unit = {
     idByRef.get(entity) match {
-      case Some(id) if !messageBuffers.contains(id) ⇒
+      case Some(id) => if (!messageBuffers.contains(id)) {
         log.debug("Passivating started on entity {}", id)
 
         passivating = passivating + entity
         messageBuffers = messageBuffers.updated(id, Vector.empty)
         entity ! stopMessage
-
-      case None => log.debug("Unknown entity {}. Not sending stopMessage back to entity.", entity)
+      } else {
+        log.debug("Passivation already in progress for {}. Not sending stopMessage back to entity.", entity)
+      }
+      case None    => log.debug("Unknown entity {}. Not sending stopMessage back to entity.", entity)
     }
   }
 
