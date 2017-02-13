0ec31a4fb6d87e42e84e44736fd396ff784c7dd3
Johan Andrén <johan@markatta.com>
2017-02-13 14:14:34 +0100

    Fail fast and docs around mixing major Scala versions and serialization #22004

--- akka-actor/src/main/scala/akka/actor/Actor.scala
+++ akka-actor/src/main/scala/akka/actor/Actor.scala
@@ -73,6 +73,9 @@ final case class Identify(messageId: Any) extends AutoReceivedMessage with NotIn
  */
 @SerialVersionUID(1L)
 final case class ActorIdentity(correlationId: Any, ref: Option[ActorRef]) {
+  require(ref.isEmpty || ref.get != null, "ActorIdentity created with ref = Some(null) is not allowed, " +
+    "this could happen when serializing with Scala 2.12 and deserializing with Scala 2.11 which is not supported.")
+
   /**
    * Java API: `ActorRef` of the actor replying to the request or
    * null if no actor matched the request.
