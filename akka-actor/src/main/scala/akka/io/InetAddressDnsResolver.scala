e9fdd47ac33a7b801332a2920cfa398c86f6f83c
ortigali <ortigalibazarov@gmail.com>
2017-02-17 03:05:46 +0500

    add default-blocking-io-dispatcher and make TTL configurable with Java DNS properties #21170
    make default-blocking-io-dispatcher use a fixed pool #21170
    
    reword configuration comment #21170

--- akka-actor/src/main/scala/akka/io/InetAddressDnsResolver.scala
+++ akka-actor/src/main/scala/akka/io/InetAddressDnsResolver.scala
@@ -7,10 +7,28 @@ import akka.actor.Actor
 import com.typesafe.config.Config
 
 import scala.collection.immutable
+import sun.net.{ InetAddressCachePolicy ⇒ IACP }
+import akka.util.Helpers.Requiring
 
 class InetAddressDnsResolver(cache: SimpleDnsCache, config: Config) extends Actor {
-  val positiveTtl = config.getDuration("positive-ttl", TimeUnit.MILLISECONDS)
-  val negativeTtl = config.getDuration("negative-ttl", TimeUnit.MILLISECONDS)
+
+  import IACP.NEVER // 0 constant
+
+  private def getTtl(path: String, positive: Boolean): Long =
+    config.getString(path) match {
+      case "default" ⇒
+        (if (positive) IACP.get else IACP.getNegative) match {
+          case NEVER      ⇒ NEVER
+          case n if n > 0 ⇒ TimeUnit.SECONDS.toMillis(n)
+          case _          ⇒ Long.MaxValue // forever if negative
+        }
+      case "forever" ⇒ Long.MaxValue
+      case "never"   ⇒ NEVER
+      case _ ⇒ config.getDuration(path, TimeUnit.MILLISECONDS)
+        .requiring(_ > 0, s"akka.io.dns.$path must be 'default', 'forever', 'never' or positive duration")
+    }
+  val positiveTtl = getTtl("positive-ttl", true)
+  val negativeTtl = getTtl("negative-ttl", false)
 
   override def receive = {
     case Dns.Resolve(name) ⇒
@@ -19,12 +37,12 @@ class InetAddressDnsResolver(cache: SimpleDnsCache, config: Config) extends Acto
         case None ⇒
           try {
             val answer = Dns.Resolved(name, InetAddress.getAllByName(name))
-            cache.put(answer, positiveTtl)
+            if (positiveTtl != NEVER) cache.put(answer, positiveTtl)
             answer
           } catch {
             case e: UnknownHostException ⇒
               val answer = Dns.Resolved(name, immutable.Seq.empty, immutable.Seq.empty)
-              cache.put(answer, negativeTtl)
+              if (negativeTtl != NEVER) cache.put(answer, negativeTtl)
               answer
           }
       }
