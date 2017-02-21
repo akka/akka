f88c5f59f16d157658607898c7d1099f157f2017
Josep Prat <jlprat@gmail.com>
2017-02-21 15:14:43 +0100

    Transform HttpApp to abstract class
    Easy binary compatibility for HttpApp
    Add exclusion to MiMa

--- akka-http/src/main/scala/akka/http/scaladsl/server/HttpApp.scala
+++ akka-http/src/main/scala/akka/http/scaladsl/server/HttpApp.scala
@@ -26,7 +26,7 @@ import scala.util.{ Failure, Success, Try }
  * It offers additional hooks to modify the default behavior.
  */
 // @akka.annotation.ApiMayChange // FIXME replace with real annotation once Akka dependency bumped
-trait HttpApp extends Directives {
+abstract class HttpApp extends Directives {
 
   private val serverBinding = new AtomicReference[ServerBinding]()
   /**
