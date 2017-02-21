57e19fc394d538265b773330a3a424f341818a99
Johan Andrén <johan@markatta.com>
2017-02-21 16:08:16 +0100

    Fix DaemonMsgCreateSerializer #22225

--- akka-contrib/src/multi-jvm/scala/akka/contrib/pattern/ReliableProxySpec.scala
+++ akka-contrib/src/multi-jvm/scala/akka/contrib/pattern/ReliableProxySpec.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2009-2016 Lightbend Inc. <http://www.lightbend.com>
+ * Copyright (C) 2009-2017 Lightbend Inc. <http://www.lightbend.com>
  */
 
 package akka.contrib.pattern
@@ -12,6 +12,7 @@ import org.scalatest.BeforeAndAfterEach
 import akka.remote.transport.ThrottlerTransportAdapter.Direction
 import akka.actor._
 import akka.testkit.ImplicitSender
+
 import scala.concurrent.duration._
 import akka.actor.FSM
 import akka.actor.ActorRef
