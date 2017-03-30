656a4d0608705a3e35b184f4e61b3c543da5d6d5
Patrik Nordwall <patrik.nordwall@gmail.com>
2017-03-30 16:28:28 +0200

    Update version for release 2.5.0-RC2

--- project/AkkaBuild.scala
+++ project/AkkaBuild.scala
@@ -30,7 +30,7 @@ object AkkaBuild extends Build {
 
   lazy val buildSettings = Dependencies.Versions ++ Seq(
     organization        := "com.typesafe.akka",
-    version             := "2.5-SNAPSHOT"
+    version             := "2.5.0-RC2"
   )
 
   lazy val rootSettings = parentSettings ++ Release.settings ++
