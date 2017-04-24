akka/akka

commit 64a3a9c028f6b81c733c359922a90aa535fc4919

Refactor SimpleDateFormat to the new DateTimeFormatter of JDK 8 which is thread safe.

akka-actor/src/main/scala/akka/event/Logging.scala
@@ -3,6 +3,8 @@
  */
 package akka.event
 
+import java.time.Instant
+import java.time.format.DateTimeFormatter
 import java.util.concurrent.TimeoutException
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -12,13 +14,13 @@ import akka.dispatch.RequiresMessageQueue
 import akka.event.Logging._
 import akka.util.ReentrantGuard
 import akka.util.Helpers.toRootLowerCase
-import akka.{ AkkaException, ConfigurationException }
+import akka.{AkkaException, ConfigurationException}
 
 import scala.annotation.implicitNotFound
 import scala.collection.immutable
 import scala.concurrent.Await
 import scala.language.existentials
-import scala.util.control.{ NoStackTrace, NonFatal }
+import scala.util.control.{NoStackTrace, NonFatal}
 
 /**
  * This trait brings log level handling to the EventStream: it reads the log
@@ -861,14 +863,11 @@ object Logging {
    */
   class LoggerInitializationException(msg: String) extends AkkaException(msg)
 
+  private val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss.SSS")
+
   trait StdOutLogger {
 
     import StdOutLogger._
-    import java.text.SimpleDateFormat
-    import java.util.Date
-
-    private val date = new Date()
-    private val dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss.SSS")
 
     // format: OFF
     // FIXME: remove those when we have the chance to break binary compatibility
@@ -879,10 +878,9 @@ object Logging {
     private val debugFormat             = DebugFormat
     // format: ON
 
-    def timestamp(event: LogEvent): String = synchronized {
-      date.setTime(event.timestamp)
-      dateFormat.format(date)
-    } // SDF isn't threadsafe
+    def timestamp(event: LogEvent): String = {
+      formatter.format(Instant.ofEpochMilli(event.timestamp))
+    }
 
     def print(event: Any): Unit = event match {
       case e: Error   ⇒ error(e)

akka-remote-tests/src/multi-jvm/scala/akka/remote/artery/BenchmarkFileReporter.scala
@@ -5,8 +5,8 @@ package akka.remote.artery
 
 import java.io.File
 import java.nio.file.Files
-import java.text.SimpleDateFormat
-import java.util.Date
+import java.time.Instant
+import java.time.format.DateTimeFormatter
 
 import akka.actor.ActorSystem
 
@@ -27,6 +27,8 @@ object BenchmarkFileReporter {
     target
   }
 
+  val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss")
+
   def apply(testName: String, system: ActorSystem): BenchmarkFileReporter =
     new BenchmarkFileReporter {
       val gitCommit = {
@@ -34,8 +36,7 @@ object BenchmarkFileReporter {
         Try("git describe".!!.trim).getOrElse("[unknown]")
       }
       val testResultFile: File = {
-        val format = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss")
-        val fileName = s"${format.format(new Date())}-Artery-$testName-$gitCommit-results.txt"
+        val fileName = s"${formatter.format(Instant.now())}-Artery-$testName-$gitCommit-results.txt"
         new File(targetDirectory, fileName)
       }
       val config = system.settings.config

akka-remote/src/main/java/akka/remote/artery/AeronErrorLog.java
@@ -25,8 +25,8 @@ import akka.event.LoggingAdapter;
 
 import java.io.File;
 import java.nio.MappedByteBuffer;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -41,7 +41,7 @@ public class AeronErrorLog
     final DirectBuffer cncMetaDataBuffer;
     final int cncVersion;
     final AtomicBuffer buffer;
-    final SimpleDateFormat dateFormat;
+    final DateTimeFormatter formatter;
 
     public AeronErrorLog(File cncFile)
     {
@@ -50,8 +50,7 @@ public class AeronErrorLog
       cncMetaDataBuffer = CncFileDescriptor.createMetaDataBuffer(cncByteBuffer);
       cncVersion = cncMetaDataBuffer.getInt(CncFileDescriptor.cncVersionOffset(0));
       buffer = CncFileDescriptor.createErrorLogBuffer(cncByteBuffer, cncMetaDataBuffer);
-      dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ");
-
+      formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSZ");
 
       if (CncFileDescriptor.CNC_VERSION != cncVersion)
       {
@@ -62,7 +61,7 @@ public class AeronErrorLog
 
     public long logErrors(LoggingAdapter log, long sinceTimestamp)
     {
-        // using AtomicLong because access from lambda, not because of currency
+        // using AtomicLong because access from lambda, not because of concurrency
         final AtomicLong lastTimestamp = new AtomicLong(sinceTimestamp);
 
         ErrorLogReader.read(
@@ -71,8 +70,8 @@ public class AeronErrorLog
                   log.error(String.format(
                       "Aeron error: %d observations from %s to %s for:%n %s",
                       observationCount,
-                      dateFormat.format(new Date(firstObservationTimestamp)),
-                      dateFormat.format(new Date(lastObservationTimestamp)),
+                      formatter.format(Instant.ofEpochMilli(firstObservationTimestamp)),
+                      formatter.format(Instant.ofEpochMilli(lastObservationTimestamp)),
                       encodedException));
                   lastTimestamp.set(Math.max(lastTimestamp.get(), lastObservationTimestamp));
                 }, sinceTimestamp);

project/TimeStampede.scala
@@ -3,6 +3,9 @@
  */
 package akka
 
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+
 import sbt._
 import sbt.Keys._
 
@@ -26,8 +29,9 @@ object TimeStampede extends AutoPlugin {
     else version
   }
 
+  val formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss")
+
   def timestamp(time: Long): String = {
-    val format = new java.text.SimpleDateFormat("yyyyMMdd-HHmmss")
-    format.format(new java.util.Date(time))
+    formatter.format(Instant.ofEpochMilli(time))
   }
 }

commit 6caeaf1788057c3b6f96a1ddc64a19c03c57a5f7

fix log timestamp, #22774
* regression introduced by https://github.com/akka/akka/pull/22716
  (never released)

akka-actor/src/main/scala/akka/event/Logging.scala
@@ -14,13 +14,15 @@ import akka.dispatch.RequiresMessageQueue
 import akka.event.Logging._
 import akka.util.ReentrantGuard
 import akka.util.Helpers.toRootLowerCase
-import akka.{AkkaException, ConfigurationException}
+import akka.{ AkkaException, ConfigurationException }
 
 import scala.annotation.implicitNotFound
 import scala.collection.immutable
 import scala.concurrent.Await
 import scala.language.existentials
-import scala.util.control.{NoStackTrace, NonFatal}
+import scala.util.control.{ NoStackTrace, NonFatal }
+import java.time.ZoneId
+import java.time.LocalDateTime
 
 /**
  * This trait brings log level handling to the EventStream: it reads the log
@@ -864,6 +866,7 @@ object Logging {
   class LoggerInitializationException(msg: String) extends AkkaException(msg)
 
   private val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss.SSS")
+  private val timeZone = ZoneId.systemDefault()
 
   trait StdOutLogger {
 
@@ -879,7 +882,7 @@ object Logging {
     // format: ON
 
     def timestamp(event: LogEvent): String = {
-      formatter.format(Instant.ofEpochMilli(event.timestamp))
+      formatter.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(event.timestamp), timeZone))
     }
 
     def print(event: Any): Unit = event match {

akka-actor-tests/src/test/scala/akka/event/LoggerSpec.scala
@@ -15,6 +15,7 @@ import akka.event.Logging._
 import akka.util.Helpers
 import akka.event.Logging.InitializeLogger
 import akka.event.Logging.Warning
+import java.text.SimpleDateFormat
 
 object LoggerSpec {
 
@@ -270,6 +271,17 @@ class LoggerSpec extends WordSpec with Matchers {
       val seconds = c.get(Calendar.SECOND)
       val ms = c.get(Calendar.MILLISECOND)
       Helpers.currentTimeMillisToUTCString(timestamp) should ===(f"$hours%02d:$minutes%02d:$seconds%02d.$ms%03dUTC")
+    }
+  }
+
+  "StdOutLogger" must {
+    "format timestamp to with system default TimeZone" in {
+      val log = new StdOutLogger {}
+      val event = Info("test", classOf[String], "test")
+      // this was the format in Akka 2.4 and earlier
+      val dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss.SSS")
+      val expected = dateFormat.format(new Date(event.timestamp))
+      log.timestamp(event) should ===(expected)
     }
   }
 