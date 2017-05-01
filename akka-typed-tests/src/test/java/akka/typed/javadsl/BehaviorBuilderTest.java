b9fd7afda4762607d4ec7459b3e31c1ca418099b
Arnout Engelen <arnouten@bzzt.net>
2017-05-01 14:44:52 +0200

    akka.japi.function, docs, construction

--- akka-typed-tests/src/test/java/akka/typed/javadsl/BehaviorBuilderTest.java
+++ akka-typed-tests/src/test/java/akka/typed/javadsl/BehaviorBuilderTest.java
@@ -17,39 +17,25 @@ public class BehaviorBuilderTest extends JUnitSuite {
     interface Message {
     }
 
-    static class One implements Message {
+    static final class One implements Message {
         public String foo() {
           return "Bar";
         }
     }
-    static class MyList<T> extends ArrayList<T> implements Message {
-
+    static final class MyList<T> extends ArrayList<T> implements Message {
     };
 
     @Test
     public void shouldCompile() {
-        Behavior<Message> b = BehaviorBuilder.<Message>create()
-                .message(One.class, o -> o.foo().startsWith("a"), (ctx, o) -> same())
-                .message(One.class, (ctx, o) -> {
-                    o.foo();
-                    return same();
-                })
-                .messageUnchecked(MyList.class, (ActorContext<Message> ctx, MyList<String> l) -> {
-                  String first = l.get(0);
-                  return Actor.<Message>same();
-                })
-                .signal(Terminated.class, (ctx, t) -> {
-                    System.out.println("Terminating along with " + t.ref());
-                    return stopped();
-                });
-    }
-
-    @Test
-    public void shouldBeUsableToCreateFromActor() {
       Behavior<Message> b = Actor.<Message>immutable()
               .message(One.class, (ctx, o) -> {
                 o.foo();
                 return same();
+              })
+              .message(One.class, o -> o.foo().startsWith("a"), (ctx, o) -> same())
+              .messageUnchecked(MyList.class, (ActorContext<Message> ctx, MyList<String> l) -> {
+                String first = l.get(0);
+                return Actor.<Message>same();
               })
               .signal(Terminated.class, (ctx, t) -> {
                 System.out.println("Terminating along with " + t.ref());
--- akka-typed/src/main/scala/akka/typed/javadsl/BehaviorBuilder.scala
+++ akka-typed/src/main/scala/akka/typed/javadsl/BehaviorBuilder.scala
@@ -5,7 +5,7 @@
 package akka.typed.javadsl
 
 import scala.annotation.tailrec
-import akka.japi.pf.FI
+import akka.japi.function.{ Function, Function2, Predicate }
 import akka.typed
 import akka.typed.{ Behavior, ExtensibleBehavior, Signal }
 import BehaviorBuilder._
@@ -41,36 +41,36 @@ case class BehaviorBuilder[T](
   /**
    * Add a new case to the message handling.
    *
-   * @param `type` type of messages to match
+   * @param `type` type of message to match
    * @param handler action to apply if the type matches
-   * @tparam M
+   * @tparam M type of message to match
    * @return a new behavior with the specified handling appended
    */
-  def message[M <: T](`type`: Class[M], handler: FI.Apply2[ActorContext[T], M, Behavior[T]]): BehaviorBuilder[T] =
+  def message[M <: T](`type`: Class[M], handler: Function2[ActorContext[T], M, Behavior[T]]): BehaviorBuilder[T] =
     copy[T](messageHandlers = messageHandlers :+ Case[T, T](`type`, None, (i1: ActorContext[T], msg: T) ⇒ handler.apply(i1, msg.asInstanceOf[M])))
 
   /**
    * Add a new predicated case to the message handling.
    *
-   * @param `type` type of messages to match
-   * @param predicate a predicate that will be evaluated on the argument if the type matches
+   * @param `type` type of message to match
+   * @param test a predicate that will be evaluated on the argument if the type matches
    * @param handler action to apply if the type matches and the predicate returns true
-   * @tparam M
+   * @tparam M type of message to match
    * @return a new behavior with the specified handling appended
    */
-  def message[M <: T](`type`: Class[M], predicate: FI.TypedPredicate[M], handler: FI.Apply2[ActorContext[T], M, Behavior[T]]): BehaviorBuilder[T] =
-    copy[T](messageHandlers = messageHandlers :+ Case[T, T](`type`, Some((t: T) ⇒ predicate.defined(t.asInstanceOf[M])), (i1: ActorContext[T], msg: T) ⇒ handler.apply(i1, msg.asInstanceOf[M])))
+  def message[M <: T](`type`: Class[M], test: Predicate[M], handler: Function2[ActorContext[T], M, Behavior[T]]): BehaviorBuilder[T] =
+    copy[T](messageHandlers = messageHandlers :+ Case[T, T](`type`, Some((t: T) ⇒ test.test(t.asInstanceOf[M])), (i1: ActorContext[T], msg: T) ⇒ handler.apply(i1, msg.asInstanceOf[M])))
 
   /**
    * Add a new case to the message handling without compile time type check.
    * Should normally not be used, but when matching on class with generic type
    * argument it can be useful, e.g. <code>List.class</code> and <code>(List&lt;String&gt; list) -> {...}</code>
    *
-   * @param `type` type of messages to match
+   * @param `type` type of message to match
    * @param handler action to apply when the type matches
    * @return a new behavior with the specified handling appended
    */
-  def messageUnchecked[M <: T](`type`: Class[_ <: T], handler: FI.Apply2[ActorContext[T], M, Behavior[T]]): BehaviorBuilder[T] =
+  def messageUnchecked[M <: T](`type`: Class[_ <: T], handler: Function2[ActorContext[T], M, Behavior[T]]): BehaviorBuilder[T] =
     copy[T](messageHandlers = messageHandlers :+ Case[T, T](`type`, None, (i1: ActorContext[T], msg: T) ⇒ handler.apply(i1, msg.asInstanceOf[M])))
 
   /**
@@ -80,7 +80,7 @@ case class BehaviorBuilder[T](
    * @param handler action to apply when the message matches
    * @return a new behavior with the specified handling appended
    */
-  def messageEquals(msg: T, handler: FI.Apply[ActorContext[T], Behavior[T]]): BehaviorBuilder[T] =
+  def messageEquals(msg: T, handler: Function[ActorContext[T], Behavior[T]]): BehaviorBuilder[T] =
     copy[T](messageHandlers = messageHandlers :+ Case[T, T](msg.getClass, Some(_.equals(msg)), (ctx: ActorContext[T], _: T) ⇒ handler.apply(ctx)))
 
   /**
@@ -88,23 +88,23 @@ case class BehaviorBuilder[T](
    *
    * @param `type` type of signal to match
    * @param handler action to apply if the type matches
-   * @tparam M
+   * @tparam M type of signal to match
    * @return a new behavior with the specified handling appended
    */
-  def signal[M <: Signal](`type`: Class[M], handler: FI.Apply2[ActorContext[T], M, Behavior[T]]): BehaviorBuilder[T] =
+  def signal[M <: Signal](`type`: Class[M], handler: Function2[ActorContext[T], M, Behavior[T]]): BehaviorBuilder[T] =
     copy[T](signalHandlers = signalHandlers :+ Case[T, Signal](`type`, None, (ctx: ActorContext[T], signal: Signal) ⇒ handler.apply(ctx, signal.asInstanceOf[M])))
 
   /**
    * Add a new predicated case to the signal handling.
    *
    * @param `type` type of signals to match
-   * @param predicate a predicate that will be evaluated on the argument if the type matches
+   * @param test a predicate that will be evaluated on the argument if the type matches
    * @param handler action to apply if the type matches and the predicate returns true
-   * @tparam M
+   * @tparam M type of signal to match
    * @return a new behavior with the specified handling appended
    */
-  def signal[M <: Signal](`type`: Class[M], predicate: FI.TypedPredicate[M], handler: FI.Apply2[ActorContext[T], M, Behavior[T]]): BehaviorBuilder[T] =
-    copy[T](signalHandlers = signalHandlers :+ Case[T, Signal](`type`, Some((t: Signal) ⇒ predicate.defined(t.asInstanceOf[M])), (ctx: ActorContext[T], signal: Signal) ⇒ handler.apply(ctx, signal.asInstanceOf[M])))
+  def signal[M <: Signal](`type`: Class[M], test: Predicate[M], handler: Function2[ActorContext[T], M, Behavior[T]]): BehaviorBuilder[T] =
+    copy[T](signalHandlers = signalHandlers :+ Case[T, Signal](`type`, Some((t: Signal) ⇒ test.test(t.asInstanceOf[M])), (ctx: ActorContext[T], signal: Signal) ⇒ handler.apply(ctx, signal.asInstanceOf[M])))
 
   /**
    * Add a new case to the signal handling without compile time type check.
@@ -115,18 +115,18 @@ case class BehaviorBuilder[T](
    * @param handler action to apply when the type matches
    * @return a new behavior with the specified handling appended
    */
-  def signalUnchecked[M <: Signal](`type`: Class[_ <: Signal], handler: FI.Apply2[ActorContext[T], M, Behavior[T]]): BehaviorBuilder[T] =
+  def signalUnchecked[M <: Signal](`type`: Class[_ <: Signal], handler: Function2[ActorContext[T], M, Behavior[T]]): BehaviorBuilder[T] =
     copy[T](signalHandlers = signalHandlers :+ Case[T, Signal](`type`, None, (ctx: ActorContext[T], signal: Signal) ⇒ handler.apply(ctx, signal.asInstanceOf[M])))
 
   /**
    * Add a new case to the signal handling matching equal signals.
    *
-   * @param msg the signal to compare to
+   * @param signal the signal to compare to
    * @param handler action to apply when the message matches
    * @return a new behavior with the specified handling appended
    */
-  def signalEquals(msg: Signal, handler: FI.Apply[ActorContext[T], Behavior[T]]): BehaviorBuilder[T] =
-    copy[T](signalHandlers = signalHandlers :+ Case[T, Signal](msg.getClass, Some(_.equals(msg)), (ctx: ActorContext[T], _: Signal) ⇒ handler.apply(ctx)))
+  def signalEquals(signal: Signal, handler: Function[ActorContext[T], Behavior[T]]): BehaviorBuilder[T] =
+    copy[T](signalHandlers = signalHandlers :+ Case[T, Signal](signal.getClass, Some(_.equals(signal)), (ctx: ActorContext[T], _: Signal) ⇒ handler.apply(ctx)))
 
 }
 
@@ -136,6 +136,6 @@ object BehaviorBuilder {
   import scala.language.existentials
 
   @InternalApi
-  case class Case[BT, MT](`type`: Class[_ <: MT], predicate: Option[MT ⇒ Boolean], handler: (ActorContext[BT], MT) ⇒ Behavior[BT])
+  private[javadsl] final case class Case[BT, MT](`type`: Class[_ <: MT], predicate: Option[MT ⇒ Boolean], handler: (ActorContext[BT], MT) ⇒ Behavior[BT])
 
 }
