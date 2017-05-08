7618e3126cf11b67192b65c853170e88ac5a15c8
Johannes Rudolph <johannes.rudolph@gmail.com>
2017-05-08 17:13:48 +0200

    =pro don't exclude mima file changes from PR validation
    The filter was added here before in akka/akka to make sure that mima
    rules could be added without triggering PR validation for all subprojects.
    
    With the external files this doesn't apply to akka-http any more. As a
    side-effect we now rerun all tests even when changing Mima.scala. That
    shouldn't be too much of a burden because Mima.scala changes are seldom
    enough.

--- project/ValidatePullRequest.scala
+++ project/ValidatePullRequest.scala
@@ -158,7 +158,7 @@ object ValidatePullRequest extends AutoPlugin {
           .filter(l =>
             l.startsWith("akka-") ||
               l.startsWith("docs") ||
-              (l.startsWith("project") && l != "project/MiMa.scala") ||
+              l.startsWith("project") ||
               (l == "build.sbt")
           )
           .map(l ⇒ l.takeWhile(_ != '/'))
