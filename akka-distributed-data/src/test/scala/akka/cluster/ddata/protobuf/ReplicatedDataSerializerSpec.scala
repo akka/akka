edef9e34c7d7983eb2829f00d8511cce3382b60f
Patrik Nordwall <patrik.nordwall@gmail.com>
2017-05-22 20:11:03 +0200

    serialize-creators=off in tests, #23003

--- akka-distributed-data/src/test/scala/akka/cluster/ddata/protobuf/ReplicatedDataSerializerSpec.scala
+++ akka-distributed-data/src/test/scala/akka/cluster/ddata/protobuf/ReplicatedDataSerializerSpec.scala
@@ -27,7 +27,6 @@ class ReplicatedDataSerializerSpec extends TestKit(ActorSystem(
     akka.remote.artery.canonical.port = 0
     akka.actor {
       serialize-messages = off
-      serialize-creators = off
       allow-java-serialization = off
     }
     """))) with WordSpecLike with Matchers with BeforeAndAfterAll {
