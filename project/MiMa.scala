fc76b87ce29ddcc40423b618ef110013ffd9aed1
Johannes Rudolph <johannes.rudolph@gmail.com>
2017-05-30 10:03:10 +0200

    =rem #23023 log reasons for disassociations with debug level

--- project/MiMa.scala
+++ project/MiMa.scala
@@ -1220,6 +1220,11 @@ object MiMa extends AutoPlugin {
         
         // #23144 recoverWithRetries cleanup
         ProblemFilters.exclude[DirectMissingMethodProblem]("akka.stream.impl.fusing.RecoverWith.InfiniteRetries")
+      ),
+      "2.5.2" -> Seq(
+        // #23023 added a new overload with implementation to trait, so old transport implementations compiled against
+        // older versions will be missing the method. We accept that incompatibility for now.
+        ProblemFilters.exclude[ReversedMissingMethodProblem]("akka.remote.transport.AssociationHandle.disassociate")
       )
     )
 
