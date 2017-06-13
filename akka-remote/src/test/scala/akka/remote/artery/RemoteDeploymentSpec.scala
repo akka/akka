6b41c80f9b9bca6c2d7cc631e9eb57d231908f58
Patrik Nordwall <patrik.nordwall@gmail.com>
2017-06-13 15:53:12 +0200

    Simplify Artery remote deployment and make inbound-lanes=4 default, #21422
    * DaemonMsgCreate is not a system message. We send it over the control
      stream because remote deployment process depends on message ordering
      for DaemonMsgCreate and Watch messages. That is all good.
    * We also send DaemonMsgCreate over the ordinary message stream (all
      outbound lanes) so that the first ordinary message that is sent to
      the ref does not arrive before the actor is created. This is not needed,
      since the retried resolve in the Decoder will take care of that anyway.
    * Inbound lanes were not covered, but not needed.
    * Then the deduplication of DaemonMsgCreate messages in  RemoteSystemDaemon
      is not needed.
    * Added some more tests for these things.
    * describe lanes in reference docs

--- akka-remote/src/test/scala/akka/remote/artery/RemoteDeploymentSpec.scala
+++ akka-remote/src/test/scala/akka/remote/artery/RemoteDeploymentSpec.scala
@@ -30,9 +30,42 @@ object RemoteDeploymentSpec {
       target ! "postStop"
     }
   }
+
+  def parentProps(probe: ActorRef): Props =
+    Props(new Parent(probe))
+
+  class Parent(probe: ActorRef) extends Actor {
+    var target: ActorRef = context.system.deadLetters
+
+    override val supervisorStrategy = OneForOneStrategy() {
+      case e: Exception ⇒
+        probe ! e
+        SupervisorStrategy.stop
+    }
+
+    def receive = {
+      case p: Props ⇒
+        sender() ! context.actorOf(p)
+
+      case (p: Props, firstMsg: Int) ⇒
+        val child = context.actorOf(p)
+        sender() ! child
+        child.tell(firstMsg, sender())
+    }
+  }
+
+  class DeadOnArrival extends Actor {
+    throw new Exception("init-crash")
+
+    def receive = Actor.emptyBehavior
+  }
 }
 
-class RemoteDeploymentSpec extends ArteryMultiNodeSpec(ArterySpecSupport.defaultConfig) {
+class RemoteDeploymentSpec extends ArteryMultiNodeSpec(
+  ConfigFactory.parseString("""
+    akka.remote.artery.advanced.inbound-lanes = 10
+    akka.remote.artery.advanced.outbound-lanes = 3
+    """).withFallback(ArterySpecSupport.defaultConfig)) {
 
   import RemoteDeploymentSpec._
 
@@ -41,7 +74,10 @@ class RemoteDeploymentSpec extends ArteryMultiNodeSpec(ArterySpecSupport.default
     s"""
     akka.actor.deployment {
       /blub.remote = "akka://${system.name}@localhost:$port"
+      "/parent*/*".remote = "akka://${system.name}@localhost:$port"
     }
+    akka.remote.artery.advanced.inbound-lanes = 10
+    akka.remote.artery.advanced.outbound-lanes = 3
     """
 
   val masterSystem = newRemoteSystem(name = Some("Master" + system.name), extraConfig = Some(conf))
@@ -64,6 +100,48 @@ class RemoteDeploymentSpec extends ArteryMultiNodeSpec(ArterySpecSupport.default
       senderProbe.expectMsg(43)
       system.stop(r)
       senderProbe.expectMsg("postStop")
+    }
+
+    "notice immediate death" in {
+      val parent = masterSystem.actorOf(parentProps(testActor), "parent")
+      EventFilter[ActorInitializationException](occurrences = 1).intercept {
+        parent.tell(Props[DeadOnArrival], testActor)
+        val child = expectMsgType[ActorRef]
+        expectMsgType[ActorInitializationException]
+
+        watch(child)
+        expectTerminated(child)
+      }(masterSystem)
+    }
+
+    "deliver all messages" in {
+      val numParents = 10
+      val numChildren = 20
+      val numMessages = 5
+
+      val parents = (0 until numParents).map { i ⇒
+        masterSystem.actorOf(parentProps(testActor), s"parent-$i")
+      }.toVector
+
+      val probes = Vector.fill(numParents, numChildren)(TestProbe()(masterSystem))
+      val childProps = Props[Echo1]
+      for (p ← (0 until numParents); c ← (0 until numChildren)) {
+        parents(p).tell((childProps, 0), probes(p)(c).ref)
+      }
+
+      for (p ← (0 until numParents); c ← (0 until numChildren)) {
+        val probe = probes(p)(c)
+        val child = probe.expectMsgType[ActorRef]
+        // message 0 was sent by parent when child was created (stress as quick send as possible)
+        (1 until numMessages).foreach(n ⇒ child.tell(n, probe.ref))
+      }
+
+      val expectedMessages = (0 until numMessages).toVector
+      for (p ← (0 until numParents); c ← (0 until numChildren)) {
+        val probe = probes(p)(c)
+        probe.receiveN(numMessages) should equal(expectedMessages)
+      }
+
     }
 
   }
