e002238aa27d5608a7e8aac43d4caed5bd5b143a
Arnout Engelen <github@bzzt.net>
2017-06-19 03:22:43 -0700

    Add whitesource (#23193)
    for 2.4 branch

---
+++ project/Whitesource.scala
@@ -0,0 +1,24 @@
+import sbt._
+import sbt.Keys._
+import sbtwhitesource.WhiteSourcePlugin.autoImport._
+import sbtwhitesource._
+import com.typesafe.sbt.SbtGit.GitKeys._
+
+object Whitesource extends AutoPlugin {
+  override def requires = WhiteSourcePlugin
+
+  override def trigger = allRequirements
+
+  override lazy val projectSettings = Seq(
+    // do not change the value of whitesourceProduct
+    whitesourceProduct := "Lightbend Reactive Platform",
+    whitesourceAggregateProjectName := {
+      (moduleName in LocalRootProject).value + "-" + (
+        if (isSnapshot.value)
+          if (gitCurrentBranch.value == "master") "master"
+          else "adhoc"
+        else "stable"
+      )
+    }
+  )
+}
