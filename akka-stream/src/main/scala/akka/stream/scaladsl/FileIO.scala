aec17be037796656307813d1d3e7a828596ac4fe
Alexander Golubev <alexander.golubev@gmail.com>
2017-06-22 22:56:00 +0300

    =str JDK7 AsynchronousFileChannel backed FileSource

--- akka-stream/src/main/scala/akka/stream/scaladsl/FileIO.scala
+++ akka-stream/src/main/scala/akka/stream/scaladsl/FileIO.scala
@@ -62,9 +62,6 @@ object FileIO {
    * Emitted elements are `chunkSize` sized [[akka.util.ByteString]] elements,
    * except the final element, which will be up to `chunkSize` in size.
    *
-   * You can configure the default dispatcher for this Source by changing the `akka.stream.blocking-io-dispatcher` or
-   * set it for a given Source by using [[ActorAttributes]].
-   *
    * It materializes a [[Future]] of [[IOResult]] containing the number of bytes read from the source file upon completion,
    * and a possible exception if IO operation was not completed successfully.
    *
@@ -73,7 +70,7 @@ object FileIO {
    * @param startPosition the start position to read from
    */
   def fromPath(f: Path, chunkSize: Int, startPosition: Long): Source[ByteString, Future[IOResult]] =
-    Source.fromGraph(new FileSource(f, chunkSize, startPosition, DefaultAttributes.fileSource, sourceShape("FileSource")))
+    Source.fromGraph(new FileSource(f, chunkSize, startPosition))
 
   /**
    * Creates a Sink which writes incoming [[ByteString]] elements to the given file. Overwrites existing files
