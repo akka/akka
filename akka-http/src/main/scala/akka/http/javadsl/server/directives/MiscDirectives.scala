e7301c8128f666730e865b35a3e75b022a615bba
Josep Prat <jlprat@gmail.com>
2017-06-22 17:02:19 +0200

    =docs add discouragement to use withoutSizeLimit in docs #814
    Issue: #814
    Add Warning and note on Scala/Java Doc so people see the risks of using withoutSizeLimit

--- akka-http/src/main/scala/akka/http/javadsl/server/directives/MiscDirectives.scala
+++ akka-http/src/main/scala/akka/http/javadsl/server/directives/MiscDirectives.scala
@@ -75,6 +75,10 @@ abstract class MiscDirectives extends MethodDirectives {
    * Disables the size limit (configured by `akka.http.parsing.max-content-length` by default) checking on the incoming
    * [[akka.http.javadsl.model.HttpRequest]] entity.
    * Can be useful when handling arbitrarily large data uploads in specific parts of your routes.
+   *
+   * @note  Usage of `withoutSizeLimit` is not recommended as it turns off the too large payload protection. Therefore,
+   *        we highly encourage using `withSizeLimit` instead, providing it with a value high enough to successfully
+   *        handle the route in need of big entities.
    */
   def withoutSizeLimit(inner: Supplier[Route]): Route = RouteAdapter {
     D.withoutSizeLimit { inner.get.delegate }
