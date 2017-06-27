8a52917c230071c8ba97b45d26c765db517a8628
Richard Imaoka <richard.e.imaoka@gmail.com>
2017-06-27 16:16:55 +0900

    Clarify that expectTerminated requires watch(target) beforehand

--- akka-testkit/src/main/scala/akka/testkit/TestKit.scala
+++ akka-testkit/src/main/scala/akka/testkit/TestKit.scala
@@ -408,6 +408,7 @@ trait TestKitBase {
 
   /**
    * Receive one message from the test actor and assert that it is the Terminated message of the given ActorRef.
+   * Before calling this method, you have to `watch` the target actor ref.
    * Wait time is bounded by the given duration, with an AssertionFailure being thrown in case of timeout.
    *
    * @param target the actor ref expected to be Terminated
