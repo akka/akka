cd1c90f3847307d237cbf900568292351fcd0df2
Arnout Engelen <arnouten@bzzt.net>
2017-08-17 19:02:52 +0200

    Whitesource 'product' is always 'Lightbend Reactive Platform' again

--- project/Whitesource.scala
+++ project/Whitesource.scala
@@ -11,10 +11,7 @@ object Whitesource extends AutoPlugin {
 
   override lazy val projectSettings = Seq(
     // do not change the value of whitesourceProduct
-    whitesourceProduct := {
-      if (isSnapshot.value) "Not Distributed"
-      else "Lightbend Reactive Platform"
-    },
+    whitesourceProduct := "Lightbend Reactive Platform",
     whitesourceAggregateProjectName := {
       (moduleName in LocalRootProject).value + "-" + (
         if (isSnapshot.value)
