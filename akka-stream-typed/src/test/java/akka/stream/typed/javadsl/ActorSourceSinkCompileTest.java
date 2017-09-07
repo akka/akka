cdb92f5ae9012de10373f3aa4dd38a10b280de2a
Konrad `ktoso` Malawski <ktoso@project13.pl>
2017-09-07 21:07:41 +0200

    WIP initial typed streams adapters
    Adapt ref source and sink for typed
    
     * do not use the typed teskit temporarily

---
+++ akka-stream-typed/src/test/java/akka/stream/typed/javadsl/ActorSourceSinkCompileTest.java
@@ -0,0 +1,87 @@
+package akka.stream.typed.javadsl;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.ActorSystem;
+import akka.stream.ActorMaterializer;
+import akka.stream.OverflowStrategy;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import scala.PartialFunction$;
+import scala.runtime.AbstractPartialFunction;
+import scala.runtime.BoxedUnit;
+
+public class ActorSourceSinkCompileTest {
+
+  interface Protocol {}
+  class Init implements Protocol {}
+  class Msg implements Protocol {}
+  class Complete implements Protocol {}
+  class Failure implements Protocol {
+    public Exception ex;
+  }
+
+  {
+    final ActorSystem<String> system = null;
+    final ActorMaterializer mat = akka.stream.typed.ActorMaterializer.create(system);
+  }
+
+  {
+    final ActorRef<String> ref = null;
+
+    Source.<String>queue(10, OverflowStrategy.dropBuffer())
+      .map(s -> s + "!")
+      .to(ActorSink.actorRef(ref, "DONE", ex -> "FAILED: " + ex.getMessage()));
+  }
+
+  {
+    final ActorRef<Protocol> ref = null;
+
+    Source.<String>queue(10, OverflowStrategy.dropBuffer())
+      .to(ActorSink.actorRefWithAck(
+        ref,
+        (sender, msg) -> new Init(),
+        (sender) -> new Msg(),
+        "ACK",
+        new Complete(),
+        (f) -> new Failure()));
+  }
+
+  {
+    final AbstractPartialFunction<String, BoxedUnit> completionMatcher = new AbstractPartialFunction<String, BoxedUnit>() {
+      @Override
+      public boolean isDefinedAt(String s) {
+        return s == "complete";
+      }
+    };
+
+    ActorSource
+      .actorRef(
+        completionMatcher,
+        PartialFunction$.MODULE$.empty(), // FIXME make the API nicer
+        10,
+        OverflowStrategy.dropBuffer())
+      .to(Sink.seq());
+  }
+
+  {
+    final AbstractPartialFunction<Protocol, Throwable> failureMatcher = new AbstractPartialFunction<Protocol, Throwable>() {
+      @Override
+      public boolean isDefinedAt(Protocol p) {
+        return p instanceof Failure;
+      }
+
+      @Override
+      public Throwable apply(Protocol p) {
+        return ((Failure)p).ex;
+      }
+    };
+
+    ActorSource
+      .actorRef(
+        PartialFunction$.MODULE$.empty(), // FIXME make the API nicer
+        failureMatcher, 10,
+        OverflowStrategy.dropBuffer())
+      .to(Sink.seq());
+  }
+
+}
