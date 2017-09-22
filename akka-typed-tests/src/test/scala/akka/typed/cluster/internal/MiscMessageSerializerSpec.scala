6f3323a7664caeb80e7bfbcd44d97a6511a497a1
Johan Andrén <johan@markatta.com>
2017-09-22 11:07:22 +0200

    typed actorref serializer to allow for java serialization of messages

---
+++ akka-typed-tests/src/test/scala/akka/typed/cluster/internal/MiscMessageSerializerSpec.scala
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2009-2017 Lightbend Inc. <http://www.lightbend.com>
+ */
+package akka.typed.cluster.internal
+
+import akka.serialization.{ SerializationExtension, SerializerWithStringManifest }
+import akka.typed.{ ActorRef, TypedSpec }
+import akka.typed.TypedSpec.Create
+import akka.typed.internal.adapter.ActorSystemAdapter
+import akka.typed.scaladsl.Actor
+import akka.typed.scaladsl.AskPattern._
+import com.typesafe.config.ConfigFactory
+
+object MiscMessageSerializerSpec {
+  def config = ConfigFactory.parseString(
+    """
+      akka.actor {
+        provider = cluster
+        serialize-messages = off
+        allow-java-serialization = true
+      }
+    """)
+}
+
+class MiscMessageSerializerSpec extends TypedSpec(MiscMessageSerializerSpec.config) {
+
+  object `The typed MiscMessageSerializer` {
+
+    def `must serialize and deserialize typed actor refs `(): Unit = {
+
+      val ref = (adaptedSystem ? Create(Actor.empty[Unit], "some-actor")).futureValue
+
+      val serialization = SerializationExtension(ActorSystemAdapter.toUntyped(adaptedSystem))
+
+      val serializer = serialization.findSerializerFor(ref) match {
+        case s: SerializerWithStringManifest ⇒ s
+      }
+
+      val manifest = serializer.manifest(ref)
+      val serialized = serializer.toBinary(ref)
+
+      val result = serializer.fromBinary(serialized, manifest)
+
+      result should ===(ref)
+
+    }
+  }
+
+}
