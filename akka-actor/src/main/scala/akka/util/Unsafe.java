a8f0aa3e60beae101312759c29b99e24ecc1fd69
Guido Medina <lic_gma@hotmail.com>
2017-09-25 15:22:49 +0100

    Java 9 Unsafe access to String value type mismatch. (#23710)
    It needs to account for both Java 8 and 9 where the types are char[] and byte[] respectively.

--- akka-actor/src/main/scala/akka/util/Unsafe.java
+++ akka-actor/src/main/scala/akka/util/Unsafe.java
@@ -4,12 +4,16 @@
 package akka.util;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 
 /**
  * INTERNAL API
  */
 public final class Unsafe {
-    public final static sun.misc.Unsafe instance;
+    public static final sun.misc.Unsafe instance;
+
+    private static final long stringValueFieldOffset;
+    private static final boolean isJavaVersion9Plus;
 
     static {
         try {
@@ -23,8 +27,67 @@ public final class Unsafe {
             }
             if (found == null) throw new IllegalStateException("Can't find instance of sun.misc.Unsafe");
             else instance = found;
+            stringValueFieldOffset = instance.objectFieldOffset(String.class.getDeclaredField("value"));
+
+            // See Oracle section 1.5.3 at:
+            // https://docs.oracle.com/javase/8/docs/technotes/guides/versioning/spec/versioning2.html
+            final int[] version = Arrays.
+                stream(System.getProperty("java.specification.version").split("\\.")).
+                mapToInt(Integer::parseInt).
+                toArray();
+            final int javaVersion = version[0] == 1 ? version[1] : version[0];
+            isJavaVersion9Plus = javaVersion > 8;
         } catch (Throwable t) {
             throw new ExceptionInInitializerError(t);
         }
+    }
+
+    public static void copyUSAsciiStrToBytes(String str, byte[] bytes) {
+        if (isJavaVersion9Plus) {
+            final byte[] chars = (byte[]) instance.getObject(str, stringValueFieldOffset);
+            System.arraycopy(chars, 0, bytes, 0, chars.length);
+        } else {
+            final char[] chars = (char[]) instance.getObject(str, stringValueFieldOffset);
+            int i = 0;
+            while (i < chars.length) {
+                bytes[i] = (byte) chars[i++];
+            }
+        }
+    }
+
+    public static int fastHash(String str) {
+        long s0 = 391408;
+        long s1 = 601258;
+        int i = 0;
+
+        if (isJavaVersion9Plus) {
+            final byte[] chars = (byte[]) instance.getObject(str, stringValueFieldOffset);
+            while (i < chars.length) {
+                long x = s0 ^ (long)chars[i++]; // Mix character into PRNG state
+                long y = s1;
+
+                // Xorshift128+ round
+                s0 = y;
+                x ^= x << 23;
+                y ^= y >>> 26;
+                x ^= x >>> 17;
+                s1 = x ^ y;
+            }
+        } else {
+            final char[] chars = (char[]) instance.getObject(str, stringValueFieldOffset);
+            while (i < chars.length) {
+                long x = s0 ^ (long)chars[i++]; // Mix character into PRNG state
+                long y = s1;
+
+                // Xorshift128+ round
+                s0 = y;
+                x ^= x << 23;
+                y ^= y >>> 26;
+                x ^= x >>> 17;
+                s1 = x ^ y;
+            }
+        }
+
+        return (int)(s0 + s1);
     }
 }
