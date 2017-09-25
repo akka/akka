9df5d80268775ade87b05274fe25315b227460b7
Guido Medina <lic_gma@hotmail.com>
2017-09-25 15:22:49 +0100

    Java 9 Unsafe access to String value type mismatch. (#23710)
    It needs to account for both Java 8 and 9 where the types are char[] and byte[] respectively.

--- akka-remote/src/main/scala/akka/remote/artery/LruBoundedCache.scala
+++ akka-remote/src/main/scala/akka/remote/artery/LruBoundedCache.scala
@@ -3,41 +3,8 @@
  */
 package akka.remote.artery
 
-import akka.util.{ OptionVal, Unsafe }
-
 import scala.annotation.tailrec
 import scala.reflect.ClassTag
-
-/**
- * INTERNAL API
- */
-private[remote] object FastHash {
-
-  // Fast hash based on the 128 bit Xorshift128+ PRNG. Mixes in character bits into the random generator state.
-  def ofString(s: String): Int = {
-    val chars = Unsafe.instance.getObject(s, EnvelopeBuffer.StringValueFieldOffset).asInstanceOf[Array[Char]]
-    var s0: Long = 391408
-    var s1: Long = 601258
-    var i = 0
-
-    while (i < chars.length) {
-      var x = s0 ^ chars(i).toLong // Mix character into PRNG state
-      var y = s1
-
-      // Xorshift128+ round
-      s0 = y
-      x ^= x << 23
-      y ^= (y >>> 26)
-      x ^= (x >>> 17)
-      s1 = x ^ y
-
-      i += 1
-    }
-
-    (s0 + s1).toInt
-  }
-
-}
 
 /**
  * INTERNAL API
@@ -219,6 +186,6 @@ private[akka] abstract class LruBoundedCache[K: ClassTag, V <: AnyRef: ClassTag]
       s" values = ${values.mkString("[", ",", "]")}," +
       s" hashes = ${hashes.map(_ & Mask).mkString("[", ",", "]")}," +
       s" epochs = ${epochs.mkString("[", ",", "]")}," +
-      s" distances = ${(0 until hashes.length).map(probeDistanceOf).mkString("[", ",", "]")}," +
+      s" distances = ${hashes.indices.map(probeDistanceOf).mkString("[", ",", "]")}," +
       s" $epoch)"
 }
