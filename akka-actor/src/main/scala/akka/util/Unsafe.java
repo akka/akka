8a5575aefc81ec941287976d55cc95404e0eebb2
Konrad `ktoso` Malawski <ktoso@project13.pl>
2017-09-28 19:10:45 +0900

    Revert "=act,build #23719 multi-release-jar akka.util.Unsafe for JDK9 (#23738)"
    This reverts commit cade0ea9f7bf7052ff55058d30d8d11a873b77a5.

--- akka-actor/src/main/scala/akka/util/Unsafe.java
+++ akka-actor/src/main/scala/akka/util/Unsafe.java
@@ -4,6 +4,7 @@
 package akka.util;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 
 /**
  * INTERNAL API
@@ -12,6 +13,7 @@ public final class Unsafe {
     public static final sun.misc.Unsafe instance;
 
     private static final long stringValueFieldOffset;
+    private static final boolean isJavaVersion9Plus;
 
     static {
         try {
@@ -26,16 +28,30 @@ public final class Unsafe {
             if (found == null) throw new IllegalStateException("Can't find instance of sun.misc.Unsafe");
             else instance = found;
             stringValueFieldOffset = instance.objectFieldOffset(String.class.getDeclaredField("value"));
+
+            // See Oracle section 1.5.3 at:
+            // https://docs.oracle.com/javase/8/docs/technotes/guides/versioning/spec/versioning2.html
+            final int[] version = Arrays.
+                stream(System.getProperty("java.specification.version").split("\\.")).
+                mapToInt(Integer::parseInt).
+                toArray();
+            final int javaVersion = version[0] == 1 ? version[1] : version[0];
+            isJavaVersion9Plus = javaVersion > 8;
         } catch (Throwable t) {
             throw new ExceptionInInitializerError(t);
         }
     }
 
     public static void copyUSAsciiStrToBytes(String str, byte[] bytes) {
-        final char[] chars = (char[]) instance.getObject(str, stringValueFieldOffset);
-        int i = 0;
-        while (i < chars.length) {
-            bytes[i] = (byte) chars[i++];
+        if (isJavaVersion9Plus) {
+            final byte[] chars = (byte[]) instance.getObject(str, stringValueFieldOffset);
+            System.arraycopy(chars, 0, bytes, 0, chars.length);
+        } else {
+            final char[] chars = (char[]) instance.getObject(str, stringValueFieldOffset);
+            int i = 0;
+            while (i < chars.length) {
+                bytes[i] = (byte) chars[i++];
+            }
         }
     }
 
@@ -44,17 +60,32 @@ public final class Unsafe {
         long s1 = 601258;
         int i = 0;
 
-        final char[] chars = (char[]) instance.getObject(str, stringValueFieldOffset);
-        while (i < chars.length) {
-            long x = s0 ^ (long)chars[i++]; // Mix character into PRNG state
-            long y = s1;
+        if (isJavaVersion9Plus) {
+            final byte[] chars = (byte[]) instance.getObject(str, stringValueFieldOffset);
+            while (i < chars.length) {
+                long x = s0 ^ (long)chars[i++]; // Mix character into PRNG state
+                long y = s1;
 
-            // Xorshift128+ round
-            s0 = y;
-            x ^= x << 23;
-            y ^= y >>> 26;
-            x ^= x >>> 17;
-            s1 = x ^ y;
+                // Xorshift128+ round
+                s0 = y;
+                x ^= x << 23;
+                y ^= y >>> 26;
+                x ^= x >>> 17;
+                s1 = x ^ y;
+            }
+        } else {
+            final char[] chars = (char[]) instance.getObject(str, stringValueFieldOffset);
+            while (i < chars.length) {
+                long x = s0 ^ (long)chars[i++]; // Mix character into PRNG state
+                long y = s1;
+
+                // Xorshift128+ round
+                s0 = y;
+                x ^= x << 23;
+                y ^= y >>> 26;
+                x ^= x >>> 17;
+                s1 = x ^ y;
+            }
         }
 
         return (int)(s0 + s1);
