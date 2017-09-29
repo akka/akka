484262d73e7f9624776b6b166f954faa934ce896
Andrew Kerr <andrew.kerr@neotechnology.com>
2017-09-29 17:05:59 +0100

    Add some factory methods

--- akka-remote/src/main/scala/akka/remote/security/setup/CryptoServiceProviderSetup.scala
+++ akka-remote/src/main/scala/akka/remote/security/setup/CryptoServiceProviderSetup.scala
@@ -1,9 +1,13 @@
 package akka.remote.security.setup
 
-import java.security.Provider
-import javax.net.ssl.ManagerFactoryParameters
+import java.security.{ AccessController, PrivilegedAction, Provider }
+import java.util.Collections.{ emptyList, emptyMap }
+import javax.net.ssl._
 
 import akka.actor.setup.Setup
+import akka.remote.security.provider._
+
+import scala.reflect.ClassTag
 
 abstract class CryptoServiceProviderSetup extends Setup {
   def provider: Provider
@@ -23,6 +27,23 @@ object KeyManagerFactorySetup {
    */
   def create(keyManagerFactoryProvider: Provider, keyManagerFactoryParameters: ManagerFactoryParameters): KeyManagerFactorySetup =
     KeyManagerFactorySetup(keyManagerFactoryProvider, Some(keyManagerFactoryParameters))
+
+  // TODO Java version
+  def providing[T <: KeyManagerFactorySpi](managerFactoryParameters: Option[ManagerFactoryParameters] = None)(implicit tag: ClassTag[T]): KeyManagerFactorySetup = {
+    val provider = new Provider(s"$tag-provider", 1.0d, s"KeyManagerFactory providing $tag") { outer ⇒
+      AccessController.doPrivileged(new PrivilegedAction[Unit] {
+        override def run(): Unit = {
+          putService(new Provider.Service(
+            outer, "KeyManagerFactory", KeyManagerFactory.getDefaultAlgorithm, tag.runtimeClass.getCanonicalName, emptyList(), emptyMap()
+          ))
+        }
+      })
+    }
+    KeyManagerFactorySetup(provider, managerFactoryParameters)
+  }
+
+  def delegatingTo(keyManagerFactory: KeyManagerFactory): KeyManagerFactorySetup =
+    KeyManagerFactorySetup(DelegatingKeyManagerFactoryProvider, Some(DelegatingKeyManagerFactoryParameters(keyManagerFactory)))
 }
 
 case class TrustManagerFactorySetup(provider: Provider, trustManagerFactoryParameters: Option[ManagerFactoryParameters])
@@ -39,4 +60,21 @@ object TrustManagerFactorySetup {
    */
   def create(trustManagerFactoryProvider: Provider, trustManagerFactoryParameters: ManagerFactoryParameters): TrustManagerFactorySetup =
     TrustManagerFactorySetup(trustManagerFactoryProvider, Some(trustManagerFactoryParameters))
+
+  // TODO Java version
+  def providing[T <: TrustManagerFactorySpi](managerFactoryParameters: Option[ManagerFactoryParameters] = None)(implicit tag: ClassTag[T]): TrustManagerFactorySetup = {
+    val provider = new Provider(s"$tag-provider", 1.0d, s"TrustManagerFactory providing $tag") { outer ⇒
+      AccessController.doPrivileged(new PrivilegedAction[Unit] {
+        override def run(): Unit = {
+          putService(new Provider.Service(
+            outer, "TrustManagerFactory", TrustManagerFactory.getDefaultAlgorithm, tag.runtimeClass.getCanonicalName, emptyList(), emptyMap()
+          ))
+        }
+      })
+    }
+    TrustManagerFactorySetup(provider, managerFactoryParameters)
+  }
+
+  def delegatingTo(trustManagerFactory: TrustManagerFactory): TrustManagerFactorySetup =
+    TrustManagerFactorySetup(DelegatingTrustManagerFactoryProvider, Some(DelegatingTrustManagerFactoryParameters(trustManagerFactory)))
 }
