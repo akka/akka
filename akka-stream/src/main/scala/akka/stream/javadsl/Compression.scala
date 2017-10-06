450ace7c79524d87bbfbc34d33ac3fa57c42e44c
Alexander Golubev <alexander.golubev@gmail.com>
2017-10-06 01:33:07 -0400

    +str add java Compression.gzip and deflate (#23779)

--- akka-stream/src/main/scala/akka/stream/javadsl/Compression.scala
+++ akka-stream/src/main/scala/akka/stream/javadsl/Compression.scala
@@ -23,4 +23,22 @@ object Compression {
    */
   def inflate(maxBytesPerChunk: Int): Flow[ByteString, ByteString, NotUsed] =
     scaladsl.Compression.inflate(maxBytesPerChunk).asJava
+
+  /**
+   * Creates a flow that gzip-compresses a stream of ByteStrings. Note that the compressor
+   * will SYNC_FLUSH after every [[ByteString]] so that it is guaranteed that every [[ByteString]]
+   * coming out of the flow can be fully decompressed without waiting for additional data. This may
+   * come at a compression performance cost for very small chunks.
+   */
+  def gzip: Flow[ByteString, ByteString, NotUsed] =
+    scaladsl.Compression.gzip.asJava
+
+  /**
+   * Creates a flow that deflate-compresses a stream of ByteString. Note that the compressor
+   * will SYNC_FLUSH after every [[ByteString]] so that it is guaranteed that every [[ByteString]]
+   * coming out of the flow can be fully decompressed without waiting for additional data. This may
+   * come at a compression performance cost for very small chunks.
+   */
+  def deflate: Flow[ByteString, ByteString, NotUsed] =
+    scaladsl.Compression.deflate.asJava
 }
