2837ebba6ebf5ef1c7379317f173e497aaa2ee64
Elijah Rippeth <elijah.rippeth@gmail.com>
2017-11-01 19:22:09 -0400

    adds serializer for akka.Done. #23854
    (cherry picked from commit 53a543788494d7b6457f129b1ad4ff57ff530584)

--- akka-actor/src/main/scala/akka/Done.scala
+++ akka-actor/src/main/scala/akka/Done.scala
@@ -3,12 +3,15 @@
  */
 package akka
 
+import java.io.Serializable
+import akka.annotation.DoNotInherit
+
 /**
  * Typically used together with `Future` to signal completion
  * but there is no actual value completed. More clearly signals intent
  * than `Unit` and is available both from Scala and Java (which `Unit` is not).
  */
-sealed abstract class Done
+@DoNotInherit sealed abstract class Done extends Serializable
 
 case object Done extends Done {
   /**
