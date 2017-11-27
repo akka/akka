e9ca9525fa695540e3f554f9bba3944d04224059
Merlijn Boogerd <merlijn.boogerd@trivento.nl>
2017-11-27 07:59:07 +0100

    Cleaned up tests in line with comments; more DRY

--- akka-remote/src/test/scala/akka/remote/artery/OutboundIdleShutdownSpec.scala
+++ akka-remote/src/test/scala/akka/remote/artery/OutboundIdleShutdownSpec.scala
@@ -3,12 +3,11 @@
  */
 package akka.remote.artery
 
-import akka.actor.RootActorPath
+import akka.actor.{ ActorRef, Address, RootActorPath }
 import akka.remote.RARP
-import akka.testkit.ImplicitSender
-import akka.testkit.TestActors
-import akka.testkit.TestProbe
+import akka.testkit.{ ImplicitSender, TestActors, TestProbe }
 import org.scalatest.concurrent.Eventually
+
 import scala.concurrent.duration._
 
 class OutboundIdleShutdownSpec extends ArteryMultiNodeSpec("""
@@ -16,139 +15,118 @@ class OutboundIdleShutdownSpec extends ArteryMultiNodeSpec("""
   akka.remote.artery.advanced.stop-idle-outbound-after = 1 s
   """) with ImplicitSender with Eventually {
 
-  "Outbound lanes" should {
+  "Outbound streams" should {
 
-    "eliminate an association when all of them are idle" in {
-      val remoteSystem = newRemoteSystem()
-      remoteSystem.actorOf(TestActors.echoActorProps, "echo")
-      val remoteAddress = RARP(remoteSystem).provider.getDefaultAddress
+    "eliminate an association when all streams within are idle" in withAssociation {
+      (remoteAddress, remoteEcho, localArtery, localProbe) ⇒
 
-      def remoteEcho = system.actorSelection(RootActorPath(remoteAddress) / "user" / "echo")
-      val localProbe = new TestProbe(localSystem)
-      remoteEcho.tell("ping", localProbe.ref)
-      localProbe.expectMsg("ping")
-
-      val artery = RARP(system).provider.transport.asInstanceOf[ArteryTransport]
-      val association = artery.association(remoteAddress)
-      withClue("When initiating a connection, both the control - and ordinary lanes are opened (regardless of which one was used)") {
-        // TODO: Is this still desirable? Should lanes perhaps be opened opportunistically always?
-        association.isStreamActive(Association.ControlQueueIndex) shouldBe true
-        association.isStreamActive(Association.OrdinaryQueueIndex) shouldBe true
-      }
-
-      eventually {
-        association.isStreamActive(Association.ControlQueueIndex) shouldBe false
-        association.isStreamActive(Association.OrdinaryQueueIndex) shouldBe false
-
-        // FIXME: Currently we have a memory leak in that associations are kept around even though the outbound channels are inactive
-        artery.remoteAddresses shouldBe 'empty
-      }
-    }
-
-    "have individual (in)active cycles" in {
-      val remoteSystem = newRemoteSystem()
-      remoteSystem.actorOf(TestActors.echoActorProps, "echo")
-      val remoteAddress = RARP(remoteSystem).provider.getDefaultAddress
-
-      def remoteEcho = system.actorSelection(RootActorPath(remoteAddress) / "user" / "echo")
-      val echoRef = remoteEcho.resolveOne(3.seconds).futureValue
-      val localProbe = new TestProbe(localSystem)
-      remoteEcho.tell("ping", localProbe.ref)
-      localProbe.expectMsg("ping")
-
-      val artery = RARP(system).provider.transport.asInstanceOf[ArteryTransport]
-      val association = artery.association(remoteAddress)
-
-      withClue("When the ordinary lane is used and the control lane is not, the former should be active and the latter inactive") {
-        eventually {
-          remoteEcho.tell("ping", localProbe.ref)
-
-          association.isStreamActive(Association.ControlQueueIndex) shouldBe false
+        val association = localArtery.association(remoteAddress)
+        withClue("When initiating a connection, both the control - and ordinary streams are opened (regardless of which one was used)") {
+          association.isStreamActive(Association.ControlQueueIndex) shouldBe true
           association.isStreamActive(Association.OrdinaryQueueIndex) shouldBe true
         }
-      }
 
-      withClue("When the control lane is used and the ordinary lane is not, the former should be active and the latter inactive") {
-        localProbe.watch(echoRef)
         eventually {
-          association.isStreamActive(Association.ControlQueueIndex) shouldBe true
+          association.isStreamActive(Association.ControlQueueIndex) shouldBe false
           association.isStreamActive(Association.OrdinaryQueueIndex) shouldBe false
+
+          // FIXME: Currently we have a memory leak in that associations are kept around even though the outbound channels are inactive
+          localArtery.remoteAddresses shouldBe 'empty
         }
-      }
     }
 
-    // FIXME test pending system messages
+    "have individual (in)active cycles" in withAssociation {
+      (remoteAddress, remoteEcho, localArtery, localProbe) ⇒
 
-    "not deactivate if there are unacknowledged system messages" in {
+        val association = localArtery.association(remoteAddress)
+
+        withClue("When the ordinary stream is used and the control stream is not, the former should be active and the latter inactive") {
+          eventually {
+            remoteEcho.tell("ping", localProbe.ref)
+            association.isStreamActive(Association.ControlQueueIndex) shouldBe false
+            association.isStreamActive(Association.OrdinaryQueueIndex) shouldBe true
+          }
+        }
+
+        withClue("When the control stream is used and the ordinary stream is not, the former should be active and the latter inactive") {
+          localProbe.watch(remoteEcho)
+          eventually {
+            association.isStreamActive(Association.ControlQueueIndex) shouldBe true
+            association.isStreamActive(Association.OrdinaryQueueIndex) shouldBe false
+          }
+        }
+    }
+
+    "not deactivate if there are unacknowledged system messages" in withAssociation {
+      (remoteAddress, remoteEcho, localArtery, localProbe) ⇒
+        val association = localArtery.association(remoteAddress)
+
+        val associationState = association.associationState
+        associationState.pendingSystemMessagesCount.incrementAndGet()
+        association.isStreamActive(Association.ControlQueueIndex) shouldBe true
+
+        Thread.sleep(3.seconds.toMillis)
+        association.isStreamActive(Association.ControlQueueIndex) shouldBe true
+    }
+
+    "be dropped after the last outbound system message is acknowledged and the idle period has passed" in withAssociation {
+      (remoteAddress, remoteEcho, localArtery, localProbe) ⇒
+
+        val association = RARP(system).provider.transport.asInstanceOf[ArteryTransport].association(remoteAddress)
+
+        association.associationState.pendingSystemMessagesCount.incrementAndGet()
+        Thread.sleep(3.seconds.toMillis)
+        association.isStreamActive(Association.ControlQueueIndex) shouldBe true
+
+        association.associationState.pendingSystemMessagesCount.decrementAndGet()
+        Thread.sleep(3.seconds.toMillis)
+        eventually(association.isStreamActive(Association.ControlQueueIndex) shouldBe false)
+    }
+
+    "still be resumable after the association has been cleaned" in withAssociation {
+      (remoteAddress, remoteEcho, localArtery, localProbe) ⇒
+        val firstAssociation = localArtery.association(remoteAddress)
+
+        eventually {
+          firstAssociation.isActive() shouldBe false
+          // FIXME: The memory leak actually plays up as the tests are now leaking as well (without the assertion below success ensues)
+          localArtery.remoteAddresses shouldBe 'empty
+        }
+
+        withClue("re-initiating the connection should be the same as starting it the first time") {
+          remoteEcho.tell("ping", localProbe.ref)
+          localProbe.expectMsg("ping")
+          val secondAssociation = localArtery.association(remoteAddress)
+
+          secondAssociation.isStreamActive(Association.ControlQueueIndex) shouldBe true
+          secondAssociation.isStreamActive(Association.OrdinaryQueueIndex) shouldBe true
+        }
+    }
+
+    /**
+     * Test setup fixture:
+     * 1. A 'remote' ActorSystem is created to spawn an Echo actor,
+     * 2. A TestProbe is spawned locally to initiate communication with the Echo actor
+     * 3. Details (remoteAddress, remoteEcho, localArtery, localProbe) are supplied to the test
+     */
+    def withAssociation(test: (Address, ActorRef, ArteryTransport, TestProbe) ⇒ Any): Unit = {
       val remoteSystem = newRemoteSystem()
       remoteSystem.actorOf(TestActors.echoActorProps, "echo")
       val remoteAddress = RARP(remoteSystem).provider.getDefaultAddress
 
       def remoteEcho = system.actorSelection(RootActorPath(remoteAddress) / "user" / "echo")
-      val localProbe = new TestProbe(localSystem)
-      remoteEcho.tell("ping", localProbe.ref)
-      localProbe.expectMsg("ping")
-
-      val artery = RARP(system).provider.transport.asInstanceOf[ArteryTransport]
-      val association = artery.association(remoteAddress)
-
-      val associationState = association.associationState
-      associationState.pendingSystemMessagesCount.incrementAndGet()
-      association.isStreamActive(Association.ControlQueueIndex) shouldBe true
-
-      Thread.sleep(3.seconds.toMillis)
-
-      association.isStreamActive(Association.ControlQueueIndex) shouldBe true
-    }
-
-    "be dropped after the last outbound system message is acknowledged and the idle period has passed" in {
-      val remoteSystem = newRemoteSystem()
-      remoteSystem.actorOf(TestActors.echoActorProps, "echo")
-      val remoteAddress = RARP(remoteSystem).provider.getDefaultAddress
-
-      val remoteEcho = system.actorSelection(RootActorPath(remoteAddress) / "user" / "echo")
-      val localProbe = new TestProbe(localSystem)
-
-      val association = RARP(system).provider.transport.asInstanceOf[ArteryTransport].association(remoteAddress)
 
       val echoRef = remoteEcho.resolveOne(3.seconds).futureValue
-      localProbe.watch(echoRef)
-      Thread.sleep(3.seconds.toMillis)
-      association.isStreamActive(Association.ControlQueueIndex) shouldBe true
-
-      localProbe.unwatch(echoRef)
-      Thread.sleep(2000)
-      eventually(association.associationState.pendingSystemMessagesCount.get() shouldBe 0)
-      eventually(association.isStreamActive(Association.ControlQueueIndex) shouldBe false)
-    }
-
-    "still be resumable after the association has been cleaned" in {
-      val remoteSystem = newRemoteSystem()
-      remoteSystem.actorOf(TestActors.echoActorProps, "echo")
-      val remoteAddress = RARP(remoteSystem).provider.getDefaultAddress
-
-      def remoteEcho = system.actorSelection(RootActorPath(remoteAddress) / "user" / "echo")
       val localProbe = new TestProbe(localSystem)
+
       remoteEcho.tell("ping", localProbe.ref)
       localProbe.expectMsg("ping")
 
       val artery = RARP(system).provider.transport.asInstanceOf[ArteryTransport]
-      val firstAssociation = artery.association(remoteAddress)
 
-      eventually {
-        firstAssociation.isActive() shouldBe false
-        // FIXME: The memory leak actually plays up as the tests are now leaking as well (without the assertion below success ensues)
-        artery.remoteAddresses shouldBe 'empty
-      }
+      test(remoteAddress, echoRef, artery, localProbe)
 
-      withClue("re-initiating the connection should be the same as starting it the first time") {
-        remoteEcho.tell("ping", localProbe.ref)
-        localProbe.expectMsg("ping")
-        val secondAssociation = artery.association(remoteAddress)
-
-        secondAssociation.isStreamActive(Association.ControlQueueIndex) shouldBe true
-        secondAssociation.isStreamActive(Association.OrdinaryQueueIndex) shouldBe true
-      }
+      remoteSystem.terminate()
     }
   }
 }
