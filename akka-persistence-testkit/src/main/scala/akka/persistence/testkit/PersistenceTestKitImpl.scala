59e9cdea4b5118fb1155513a5c739f1555f8bd8b
Kirill Yankov <myworkpostbox@gmail.com>
2017-11-30 20:39:49 +0300

    added storage extension

--- akka-persistence-testkit/src/main/scala/akka/persistence/testkit/PersistenceTestKitImpl.scala
+++ akka-persistence-testkit/src/main/scala/akka/persistence/testkit/PersistenceTestKitImpl.scala
@@ -4,7 +4,7 @@ import java.util.UUID
 import java.util.concurrent.{ BlockingDeque, ConcurrentHashMap, LinkedBlockingDeque }
 import java.util.function.BiFunction
 
-import akka.actor.{ ActorSystem, Props }
+import akka.actor.{ ActorSystem, ExtendedActorSystem, Extension, ExtensionId, ExtensionIdProvider, Props }
 import akka.persistence.{ AtomicWrite, Persistence, PersistentRepr }
 import akka.testkit.TestKitBase
 import com.typesafe.config.ConfigFactory
@@ -14,10 +14,9 @@ import akka.util.Timeout
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.collection.immutable
+import scala.util.{ Failure, Success, Try }
 
 trait PersistenceTestKit extends TestKitBase with PersistentTestKitOps {
-
-  implicit def timeout: Timeout = Timeout(10.seconds)
 
   override implicit lazy val system = {
     //todo implement method for setting plugin in Persistence for testing purposes
@@ -26,19 +25,14 @@ trait PersistenceTestKit extends TestKitBase with PersistentTestKitOps {
 
   }
 
+  private final lazy val storage = InMemStorageExtension(system)
+
   implicit val ec = system.dispatcher
 
-  final val map: ConcurrentHashMap[String, BlockingDeque[PersistentRepr]] = new ConcurrentHashMap()
-
-  //todo this should be atomicref
-  final val decider: RejectionDecider = new RejectionDecider(new RejectionPolicy {
-    override def rejectOrPass(msg: Any) = PassMessage
-  })
-
-  val journal = Persistence(system).addNewJournal(() ⇒ system.actorOf(Props(classOf[PersistenceTestKitPlugin], map, decider)), PersistenceTestKitPlugin.PluginId)
+  val journal = Persistence(system).addNewJournal(() ⇒ system.actorOf(Props(classOf[PersistenceTestKitPlugin])), PersistenceTestKitPlugin.PluginId)
 
   override def expectNextPersisted(persistenceId: String, msg: Any): Unit = {
-    val actual = receiveOnePersisted(persistenceId)
+    val actual = storage.receiveOnePersisted(persistenceId)
     assert(actual == msg, s"Failed to persist $msg, got $actual instead")
   }
 
@@ -50,11 +44,21 @@ trait PersistenceTestKit extends TestKitBase with PersistentTestKitOps {
 
   override def withRejectionPolicy(rej: RejectionPolicy) = ???
 
-  override def clearTheJournal() = {
-    map.clear()
-  }
+  override def clearTheJournal(): Unit = storage.clearTheJournal()
 
-  private def receiveOnePersisted(persistenceId: String): Any = {
+}
+
+class InMemStorage extends Extension {
+
+  implicit def timeout: Timeout = Timeout(10.seconds)
+
+  private final val decider: RejectionDecider = new RejectionDecider(new RejectionPolicy {
+    override def rejectOrPass(msg: Any) = PassMessage
+  })
+
+  private final val map: ConcurrentHashMap[String, BlockingDeque[PersistentRepr]] = new ConcurrentHashMap()
+
+  def receiveOnePersisted(persistenceId: String): Any = {
     val msg = map.computeIfAbsent(persistenceId, new java.util.function.Function[String, BlockingDeque[PersistentRepr]] {
       override def apply(v1: String) = {
         new LinkedBlockingDeque[PersistentRepr]()
@@ -66,6 +70,50 @@ trait PersistenceTestKit extends TestKitBase with PersistentTestKitOps {
     msg
   }
 
+  def clearTheJournal() = {
+    map.clear()
+  }
+
+  def add(p: PersistentRepr): Try[Unit] = {
+    map.compute(p.persistenceId, new BiFunction[String, BlockingDeque[PersistentRepr], BlockingDeque[PersistentRepr]] {
+      override def apply(t: String, u: BlockingDeque[PersistentRepr]) = u match {
+        case null ⇒
+          val q = new LinkedBlockingDeque[PersistentRepr]()
+          q.add(p)
+          q
+        case existing ⇒
+          existing.add(p)
+          existing
+      }
+    })
+    Success(())
+  }
+
+  def addWithRejection(p: PersistentRepr) = {
+    decider.policy.rejectOrPass(p) match {
+      case PassMessage ⇒
+        add(p)
+      case Reject(e) ⇒
+        Failure(e)
+    }
+  }
+
+  def readNum(persistenceId: String) = {
+    Option(
+      map.computeIfAbsent(persistenceId, new java.util.function.Function[String, BlockingDeque[PersistentRepr]] {
+        override def apply(v1: String) = {
+          new LinkedBlockingDeque[PersistentRepr]()
+        }
+      }).pollFirst()).map(_.sequenceNr).getOrElse(0L)
+  }
+
+}
+
+object InMemStorageExtension extends ExtensionId[InMemStorage] with ExtensionIdProvider {
+
+  override def createExtension(system: ExtendedActorSystem) = new InMemStorage
+
+  override def lookup() = InMemStorageExtension
 }
 
 trait PersistentTestKitOps {
@@ -85,16 +133,16 @@ trait PersistentTestKitOps {
 
 }
 
-class PersistenceTestKitPlugin(val map: ConcurrentHashMap[String, BlockingDeque[PersistentRepr]], val decider: RejectionDecider) extends AsyncWriteJournal {
+class PersistenceTestKitPlugin extends AsyncWriteJournal {
+
+  private final val storage = InMemStorageExtension(context.system)
 
   override def asyncWriteMessages(messages: immutable.Seq[AtomicWrite]) = {
     var failed: Option[Throwable] = None
     for (w ← messages; p ← w.payload if failed.isEmpty) {
-      decider.policy.rejectOrPass(p) match {
-        case PassMessage ⇒
-          add(p)
-        case Reject(e) ⇒
-          failed = Some(e)
+      storage.addWithRejection(p) match {
+        case Failure(e) ⇒ failed = Some(e)
+        case _          ⇒
       }
     }
     failed match {
@@ -103,29 +151,12 @@ class PersistenceTestKitPlugin(val map: ConcurrentHashMap[String, BlockingDeque[
     }
   }
 
-  def add(p: PersistentRepr): Unit = map.compute(p.persistenceId, new BiFunction[String, BlockingDeque[PersistentRepr], BlockingDeque[PersistentRepr]] {
-    override def apply(t: String, u: BlockingDeque[PersistentRepr]) = u match {
-      case null ⇒
-        val q = new LinkedBlockingDeque[PersistentRepr]()
-        q.add(p)
-        q
-      case existing ⇒
-        existing.add(p)
-        existing
-    }
-  })
-
   override def asyncDeleteMessagesTo(persistenceId: String, toSequenceNr: Long) = ???
 
   override def asyncReplayMessages(persistenceId: String, fromSequenceNr: Long, toSequenceNr: Long, max: Long)(recoveryCallback: (PersistentRepr) ⇒ Unit) = ???
 
   override def asyncReadHighestSequenceNr(persistenceId: String, fromSequenceNr: Long) = {
-    val result = Option(
-      map.computeIfAbsent(persistenceId, new java.util.function.Function[String, BlockingDeque[PersistentRepr]] {
-        override def apply(v1: String) = {
-          new LinkedBlockingDeque[PersistentRepr]()
-        }
-      }).pollFirst()).map(_.sequenceNr).getOrElse(0L)
+    val result = storage.readNum(persistenceId)
     Future.successful(result)
   }
 
