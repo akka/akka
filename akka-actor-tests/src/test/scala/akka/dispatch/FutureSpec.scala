741b208072a76f97fce0b092cca542df255a26e7
Catalin Ursachi <catalin.ursachi@softwire.com>
2017-12-09 16:39:11 +0000

    Cleaned up tests

--- akka-actor-tests/src/test/scala/akka/dispatch/FutureSpec.scala
+++ akka-actor-tests/src/test/scala/akka/dispatch/FutureSpec.scala
@@ -1,45 +1,22 @@
 package akka.dispatch
 
-import java.util.concurrent._
-
 import akka.actor._
 import akka.pattern.ask
-import akka.testkit.{ AkkaSpec, DefaultTimeout, TestLatch, filterException }
+import akka.testkit.{ AkkaSpec, DefaultTimeout, filterException }
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.junit.JUnitSuiteLike
 import org.scalatest.prop.Checkers
 
-import scala.concurrent.duration._
-import scala.concurrent.{ Await, Awaitable, ExecutionContext, Future }
+import scala.concurrent.{ Await, ExecutionContext, Future }
 import scala.language.postfixOps
 import scala.reflect.ClassTag
-import scala.util.control.NonFatal
 import scala.util.{ Failure, Success }
 
 object FutureSpec {
-
-  def ready[T](awaitable: Awaitable[T], atMost: Duration): awaitable.type =
-    try Await.ready(awaitable, atMost) catch {
-      case t: TimeoutException ⇒ throw t
-      case e if NonFatal(e)    ⇒ awaitable //swallow
-    }
-
   class TestActor extends Actor {
     def receive = {
       case "Hello" ⇒ sender() ! "World"
       case "Failure" ⇒
-        sender() ! Status.Failure(new RuntimeException("Expected exception; to test fault-tolerance"))
-      case "NoReply" ⇒
-    }
-  }
-
-  class TestDelayActor(await: TestLatch) extends Actor {
-    def receive = {
-      case "Hello" ⇒
-        FutureSpec.ready(await, TestLatch.DefaultTimeout); sender() ! "World"
-      case "NoReply" ⇒ FutureSpec.ready(await, TestLatch.DefaultTimeout)
-      case "Failure" ⇒
-        FutureSpec.ready(await, TestLatch.DefaultTimeout)
         sender() ! Status.Failure(new RuntimeException("Expected exception; to test fault-tolerance"))
     }
   }
@@ -57,7 +34,7 @@ class FutureSpec extends AkkaSpec with Checkers with BeforeAndAfterAll with Defa
         behave like futureWithResult { test ⇒
           val actor = system.actorOf(Props[TestActor]())
           val future = actor ? "Hello"
-          FutureSpec.ready(future, timeout.duration)
+          Await.ready(future, timeout.duration)
           test(future, "World")
           system.stop(actor)
         }
@@ -67,7 +44,7 @@ class FutureSpec extends AkkaSpec with Checkers with BeforeAndAfterAll with Defa
           filterException[RuntimeException] {
             val actor = system.actorOf(Props[TestActor]())
             val future = actor ? "Failure"
-            FutureSpec.ready(future, timeout.duration)
+            Await.ready(future, timeout.duration)
             test(future, "Expected exception; to test fault-tolerance")
             system.stop(actor)
           }
@@ -81,7 +58,7 @@ class FutureSpec extends AkkaSpec with Checkers with BeforeAndAfterAll with Defa
           val actor1 = system.actorOf(Props[TestActor]())
           val actor2 = system.actorOf(Props(new Actor { def receive = { case s: String ⇒ sender() ! s.toUpperCase } }))
           val future = actor1 ? "Hello" flatMap { case s: String ⇒ actor2 ? s }
-          FutureSpec.ready(future, timeout.duration)
+          Await.ready(future, timeout.duration)
           test(future, "WORLD")
           system.stop(actor1)
           system.stop(actor2)
@@ -93,7 +70,7 @@ class FutureSpec extends AkkaSpec with Checkers with BeforeAndAfterAll with Defa
             val actor1 = system.actorOf(Props[TestActor]())
             val actor2 = system.actorOf(Props(new Actor { def receive = { case s: String ⇒ sender() ! Status.Failure(new ArithmeticException("/ by zero")) } }))
             val future = actor1 ? "Hello" flatMap { case s: String ⇒ actor2 ? s }
-            FutureSpec.ready(future, timeout.duration)
+            Await.ready(future, timeout.duration)
             test(future, "/ by zero")
             system.stop(actor1)
             system.stop(actor2)
