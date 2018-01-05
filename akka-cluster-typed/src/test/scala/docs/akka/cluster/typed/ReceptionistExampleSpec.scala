43dcaf4b1533cda509cfedb392992e1cf56430b5
Christopher Batey <christopher.batey@gmail.com>
2018-01-05 15:21:52 +0000

    Receptionist docs and examples

---
+++ akka-cluster-typed/src/test/scala/docs/akka/cluster/typed/ReceptionistExampleSpec.scala
@@ -0,0 +1,209 @@
+package docs.akka.cluster.typed
+
+import java.util.concurrent.ThreadLocalRandom
+
+import akka.actor.Address
+import akka.actor.typed._
+import akka.actor.typed.receptionist.Receptionist
+import akka.actor.typed.receptionist.Receptionist.{Find, Listing, ServiceKey}
+import akka.actor.typed.scaladsl._
+import akka.cluster.ClusterEvent._
+import akka.cluster.typed.{Cluster, Join, Subscribe}
+import com.typesafe.config.ConfigFactory
+import org.scalatest.WordSpec
+import org.scalatest.concurrent.ScalaFutures
+
+import scala.collection.immutable.Set
+
+object RandomRouter {
+
+  def router[T](serviceKey: ServiceKey[T]): Behavior[T] =
+    Actor.deferred[Any] { ctx ⇒
+      ctx.system.receptionist ! Receptionist.Subscribe(serviceKey, ctx.self)
+
+      def routingBehavior(routees: Vector[ActorRef[T]]): Behavior[Any] =
+        Actor.immutable { (_, msg) ⇒
+          msg match {
+            case Listing(_, services: Set[ActorRef[T]]) ⇒
+              routingBehavior(services.toVector)
+            case other: T@unchecked ⇒
+              if (routees.isEmpty)
+                Actor.unhandled
+              else {
+                val i = ThreadLocalRandom.current.nextInt(routees.size)
+                routees(i) ! other
+                Actor.same
+              }
+          }
+        }
+
+      routingBehavior(Vector.empty)
+    }.narrow[T]
+
+  private final case class WrappedReachabilityEvent(event: ReachabilityEvent)
+
+  // same as above, but also subscribes to cluster reachability events and
+  // avoids routees that are unreachable
+  def clusterRouter[T](serviceKey: ServiceKey[T]): Behavior[T] =
+    Actor.deferred[Any] { ctx ⇒
+      ctx.system.receptionist ! Receptionist.Subscribe(serviceKey, ctx.self)
+
+      val cluster = Cluster(ctx.system)
+      // typically you have to map such external messages into this
+      // actor's protocol with a message adapter
+      val reachabilityAdapter: ActorRef[ReachabilityEvent] = ctx.spawnAdapter(WrappedReachabilityEvent.apply)
+      cluster.subscriptions ! Subscribe(reachabilityAdapter, classOf[ReachabilityEvent])
+
+      def routingBehavior(routees: Vector[ActorRef[T]], unreachable: Set[Address]): Behavior[Any] =
+        Actor.immutable { (_, msg) ⇒
+          msg match {
+            case Listing(_, services: Set[ActorRef[T]]) ⇒
+              routingBehavior(services.toVector, unreachable)
+            case WrappedReachabilityEvent(event) => event match {
+              case UnreachableMember(m) =>
+                routingBehavior(routees, unreachable + m.address)
+              case ReachableMember(m) =>
+                routingBehavior(routees, unreachable - m.address)
+            }
+
+            case other: T@unchecked ⇒
+              if (routees.isEmpty)
+                Actor.unhandled
+              else {
+                val reachableRoutes =
+                  if (unreachable.isEmpty) routees
+                  else routees.filterNot { r => unreachable(r.path.address) }
+
+                val i = ThreadLocalRandom.current.nextInt(reachableRoutes.size)
+                reachableRoutes(i) ! other
+                Actor.same
+              }
+          }
+        }
+
+      routingBehavior(Vector.empty, Set.empty)
+    }.narrow[T]
+}
+
+object PingPongExample {
+  //#ping-service
+  val PingServiceKey = Receptionist.ServiceKey[Ping]("pingService")
+
+  final case class Ping(replyTo: ActorRef[Pong.type])
+  final case object Pong
+
+  val pingService: Behavior[Ping] =
+    Actor.deferred { ctx ⇒
+      ctx.system.receptionist ! Receptionist.Register(PingServiceKey, ctx.self, ctx.system.deadLetters)
+      Actor.immutable[Ping] { (_, msg) ⇒
+        msg match {
+          case Ping(replyTo) ⇒
+            replyTo ! Pong
+            Actor.stopped
+        }
+      }
+    }
+  //#ping-service
+
+  //#pinger
+  def pinger(pingService: ActorRef[Ping]) = Actor.deferred[Pong.type] { ctx =>
+    pingService ! Ping(ctx.self)
+    Actor.immutable { (_, msg) =>
+      println("I was ponged!!" + msg)
+      Actor.same
+    }
+  }
+  //#pinger
+
+  //#pinger-guardian
+  val guardian: Behavior[Listing[Ping]] = Actor.deferred { ctx =>
+    ctx.system.receptionist ! Receptionist.Subscribe(PingServiceKey, ctx.self)
+    val ps = ctx.spawnAnonymous(pingService)
+    ctx.watch(ps)
+    Actor.immutablePartial[Listing[Ping]] {
+      case (c, Listing(PingServiceKey, listings)) if listings.nonEmpty =>
+        listings.foreach(ps => ctx.spawnAnonymous(pinger(ps)))
+        Actor.same
+    } onSignal {
+      case (_, Terminated(`ps`)) =>
+        println("Ping service has shut down")
+        Actor.stopped
+    }
+  }
+  //#pinger-guardian
+
+  //#pinger-guardian-pinger-service
+  val guardianJustPingService: Behavior[Listing[Ping]] = Actor.deferred { ctx =>
+    val ps = ctx.spawnAnonymous(pingService)
+    ctx.watch(ps)
+    Actor.immutablePartial[Listing[Ping]] {
+      case (c, Listing(PingServiceKey, listings)) if listings.nonEmpty =>
+        listings.foreach(ps => ctx.spawnAnonymous(pinger(ps)))
+        Actor.same
+    } onSignal {
+      case (_, Terminated(`ps`)) =>
+        println("Ping service has shut down")
+        Actor.stopped
+    }
+  }
+  //#pinger-guardian-pinger-service
+
+  //#pinger-guardian-just-pinger
+  val guardianJustPinger: Behavior[Listing[Ping]] = Actor.deferred { ctx =>
+    ctx.system.receptionist ! Receptionist.Subscribe(PingServiceKey, ctx.self)
+    Actor.immutablePartial[Listing[Ping]] {
+      case (c, Listing(PingServiceKey, listings)) if listings.nonEmpty =>
+        listings.foreach(ps => ctx.spawnAnonymous(pinger(ps)))
+        Actor.same
+    }
+  }
+  //#pinger-guardian-just-pinger
+
+}
+
+object ReceptionistExampleSpec {
+  val clusterConfig = ConfigFactory.parseString(
+    s"""
+#config
+akka {
+  actor {
+    provider = "cluster"
+  }
+  cluster.jmx.multi-mbeans-in-same-jvm = on
+  remote {
+    netty.tcp {
+      hostname = "127.0.0.1"
+      port = 0
+    }
+  }
+}
+#config
+     """)
+
+}
+
+class ReceptionistExampleSpec extends WordSpec with ScalaFutures {
+
+  import ReceptionistExampleSpec._
+  import PingPongExample._
+
+  "A local basic example" must {
+    "show register" in {
+      val system = ActorSystem(guardian, "PingPongExample")
+      system.whenTerminated.futureValue
+    }
+  }
+
+  "A remote basic example" must {
+    "show register" in {
+      val system1 = ActorSystem(guardianJustPingService, "PingPongExample", clusterConfig)
+      val system2 = ActorSystem(guardianJustPinger, "PingPongExample", clusterConfig)
+
+      val cluster1 = Cluster(system1)
+      val cluster2 = Cluster(system2)
+
+      cluster1.manager ! Join(cluster1.selfMember.address)
+      cluster1.manager ! Join(cluster2.selfMember.address)
+    }
+  }
+}
--- akka-docs/src/main/paradox/cluster-typed.md
+++ akka-docs/src/main/paradox/cluster-typed.md
@@ -1,30 +1,88 @@
 # Cluster
 
-sbt
-:   @@@vars
-    ```
-    "com.typesafe.akka" %% "akka-cluster-typed" % "$akka.version$"
-    ```
-    @@@
+@@@ warning
 
-Gradle
-:   @@@vars
-    ```
-    dependencies {
-      compile group: 'com.typesafe.akka', name: 'akka-cluster-typed_2.11', version: '$akka.version$'
-    }
-    ```
-    @@@
+This module is currently marked as @ref:[may change](common/may-change.md) in the sense
+  of being the subject of active research. This means that API or semantics can
+  change without warning or deprecation period and it is not recommended to use
+  this module in production just yet—you have been warned.
+  
+@@@
 
-Maven
-:   @@@vars
-    ```
-    <dependency>
-      <groupId>com.typesafe.akka</groupId>
-      <artifactId>akka-cluster-typed_$scala.binary_version$</artifactId>
-      <version>$akka.version$</version>
-    </dependency>
-    ```
-    @@@
+To use the testkit add the following dependency:
 
-TODO    
+@@dependency [sbt,Maven,Gradle] {
+  group=com.typesafe.akka
+  artifact=akka-cluster-typed_2.12
+  version=$akka.version$
+}
+
+For an introduction to Akka Cluster concepts see @ref:[Cluster Specification](common/cluster.md). This documentation shows how to use the typed
+Cluster API. All of the examples below assume the following imports:
+
+Scala
+:  @@snip [BasicClusterExampleSpec.scala]($akka$/akka-cluster-typed/src/test/scala/docs/akka/cluster/typed/BasicClusterExampleSpec.scala) { #cluster-imports }
+
+And the minimum configuration required is to set a host/port for remoting and the `cluster` 
+
+Scala
+:  @@snip [BasicClusterExampleSpec.scala]($akka$/akka-cluster-typed/src/test/scala/docs/akka/cluster/typed/BasicClusterExampleSpec.scala) { #config }
+
+## Cluster API extension 
+
+The typed Cluster extension gives access to management tasks (Joining, Leaving, Downing, …) and subscription of 
+cluster membership events (MemberUp, MemberRemoved, UnreachableMember, etc). Those are exposed as two different actor 
+references, i.e. it’s a message based API. 
+
+The references are on the `Cluster` extension:
+
+Scala
+:  @@snip [BasicClusterExampleSpec.scala]($akka$/akka-cluster-typed/src/test/scala/docs/akka/cluster/typed/BasicClusterExampleSpec.scala) { #cluster-create }
+
+
+The Cluster extensions gives you access to:
+
+* manager: An `ActorRef[ClusterCommand]` where a `ClusterCommand` is a command such as: `Join`, `Leave` and `Down`
+* subscriptions: An `ActorRef[ClusterStateSubscription]` where a `ClusterStateSubscription` is one of `GetCurrentState` or `Subscribe` and `Unsubscribe` to cluster events like `MemberRemoved`
+* state: The current `CurrentClusterState`
+
+
+### Cluster Management 
+
+If not using configuration to specify seeds joining the cluster can be done programmatically via the `manager`.
+
+Scala
+:  @@snip [BasicClusterExampleSpec.scala]($akka$/akka-cluster-typed/src/test/scala/docs/akka/cluster/typed/BasicClusterExampleSpec.scala) { #cluster-join }
+
+Leaving and downing are similar e.g.
+
+Scala
+:  @@snip [BasicClusterExampleSpec.scala]($akka$/akka-cluster-typed/src/test/scala/docs/akka/cluster/typed/BasicClusterExampleSpec.scala) { #cluster-leave }
+
+### Cluster subscriptions
+
+Cluster `subscriptions` can be used to receive messages when cluster state changes. For example, registering
+for all `MemberEvent`s, then using the `manager` to have a node leave the cluster will result in events
+for the node going through the lifecycle described in @ref:[Cluster Specification](common/cluster.md).
+
+This example subscribes with a `TestProbe` but in a real application it would be an Actor:
+
+Scala
+:  @@snip [BasicClusterExampleSpec.scala]($akka$/akka-cluster-typed/src/test/scala/docs/akka/cluster/typed/BasicClusterExampleSpec.scala) { #cluster-subscribe }
+
+Then asking a node to leave:
+
+Scala
+:  @@snip [BasicClusterExampleSpec.scala]($akka$/akka-cluster-typed/src/test/scala/docs/akka/cluster/typed/BasicClusterExampleSpec.scala) { #cluster-leave-example }
+
+## Serialization
+
+See [serialization](https://doc.akka.io/docs/akka/current/scala/serialization.html) for how messages are sent between 
+ActorSystems. Actor references are typically included in the messages, 
+since there is no `sender`. To serialize actor references to/from string representation you will use the `ActorRefResolver`.
+For example here's how a serializer could look for the `Ping` and `Pong` messages above: 
+
+Scala
+:  @@snip [PingSerializer.scala]($akka$/akka-cluster-typed/src/test/scala/docs/akka/cluster/typed/PingSerializer.scala) { #serializer }
+
+
