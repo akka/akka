545eaf039d54b6da968d5591d9b2f0de48e13a40
Johan Andrén <johan@markatta.com>
2018-01-08 14:35:15 +0100

    Typed actor context is AC that means scaladsl is SAC

--- akka-actor-typed/src/main/scala/akka/actor/typed/internal/BehaviorImpl.scala
+++ akka-actor-typed/src/main/scala/akka/actor/typed/internal/BehaviorImpl.scala
@@ -7,10 +7,9 @@ package internal
 import akka.util.LineNumbers
 import akka.annotation.InternalApi
 import akka.actor.typed.{ ActorContext ⇒ AC }
-import akka.actor.typed.scaladsl.{ ActorContext ⇒ TAC }
+import akka.actor.typed.scaladsl.{ ActorContext ⇒ SAC }
 
 import scala.reflect.ClassTag
-import scala.annotation.tailrec
 
 /**
  * INTERNAL API
@@ -56,19 +55,19 @@ import scala.annotation.tailrec
   }
 
   class ImmutableBehavior[T](
-    val onMessage: (TAC[T], T) ⇒ Behavior[T],
-    onSignal:      PartialFunction[(TAC[T], Signal), Behavior[T]] = Behavior.unhandledSignal.asInstanceOf[PartialFunction[(TAC[T], Signal), Behavior[T]]])
+    val onMessage: (SAC[T], T) ⇒ Behavior[T],
+    onSignal:      PartialFunction[(SAC[T], Signal), Behavior[T]] = Behavior.unhandledSignal.asInstanceOf[PartialFunction[(SAC[T], Signal), Behavior[T]]])
     extends ExtensibleBehavior[T] {
 
     override def receiveSignal(ctx: AC[T], msg: Signal): Behavior[T] =
-      onSignal.applyOrElse((ctx.asScala, msg), Behavior.unhandledSignal.asInstanceOf[PartialFunction[(TAC[T], Signal), Behavior[T]]])
+      onSignal.applyOrElse((ctx.asScala, msg), Behavior.unhandledSignal.asInstanceOf[PartialFunction[(SAC[T], Signal), Behavior[T]]])
     override def receiveMessage(ctx: AC[T], msg: T) = onMessage(ctx.asScala, msg)
     override def toString = s"Immutable(${LineNumbers(onMessage)})"
   }
 
   def tap[T](
-    onMessage: (TAC[T], T) ⇒ _,
-    onSignal:  (TAC[T], Signal) ⇒ _,
+    onMessage: (SAC[T], T) ⇒ _,
+    onSignal:  (SAC[T], Signal) ⇒ _,
     behavior:  Behavior[T]): Behavior[T] = {
     intercept[T, T](
       beforeMessage = (ctx, msg) ⇒ {
@@ -100,10 +99,10 @@ import scala.annotation.tailrec
    * different than the incoming message).
    */
   def intercept[T, U <: Any: ClassTag](
-    beforeMessage:  (TAC[U], U) ⇒ T,
-    beforeSignal:   (TAC[T], Signal) ⇒ Boolean,
-    afterMessage:   (TAC[T], T, Behavior[T]) ⇒ Behavior[T],
-    afterSignal:    (TAC[T], Signal, Behavior[T]) ⇒ Behavior[T],
+    beforeMessage:  (SAC[U], U) ⇒ T,
+    beforeSignal:   (SAC[T], Signal) ⇒ Boolean,
+    afterMessage:   (SAC[T], T, Behavior[T]) ⇒ Behavior[T],
+    afterSignal:    (SAC[T], Signal, Behavior[T]) ⇒ Behavior[T],
     behavior:       Behavior[T],
     toStringPrefix: String                                      = "Intercept"): Behavior[T] = {
     behavior match {
@@ -119,10 +118,10 @@ import scala.annotation.tailrec
   }
 
   private final case class Intercept[T, U <: Any: ClassTag](
-    beforeOnMessage: (TAC[U], U) ⇒ T,
-    beforeOnSignal:  (TAC[T], Signal) ⇒ Boolean,
-    afterMessage:    (TAC[T], T, Behavior[T]) ⇒ Behavior[T],
-    afterSignal:     (TAC[T], Signal, Behavior[T]) ⇒ Behavior[T],
+    beforeOnMessage: (SAC[U], U) ⇒ T,
+    beforeOnSignal:  (SAC[T], Signal) ⇒ Boolean,
+    afterMessage:    (SAC[T], T, Behavior[T]) ⇒ Behavior[T],
+    afterSignal:     (SAC[T], Signal, Behavior[T]) ⇒ Behavior[T],
     behavior:        Behavior[T],
     toStringPrefix:  String                                      = "Intercept") extends ExtensibleBehavior[T] {
 
@@ -142,7 +141,7 @@ import scala.annotation.tailrec
     override def receiveMessage(ctx: AC[T], msg: T): Behavior[T] = {
       msg match {
         case m: U ⇒
-          val msg2 = beforeOnMessage(ctx.asScala.asInstanceOf[TAC[U]], m)
+          val msg2 = beforeOnMessage(ctx.asScala.asInstanceOf[SAC[U]], m)
           val next: Behavior[T] =
             if (msg2 == null)
               same
