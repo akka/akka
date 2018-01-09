c58b2216f25169ca0b793df700a3e361e8a7f417
Andrew Kerr <andrew.kerr@neotechnology.com>
2018-01-09 16:23:11 +0000

    Javadoc

--- akka-remote/src/main/scala/akka/remote/security/setup/CryptoServiceProviderSetup.scala
+++ akka-remote/src/main/scala/akka/remote/security/setup/CryptoServiceProviderSetup.scala
@@ -23,25 +23,52 @@ private[setup] object CryptoServiceProviderSetup {
   }
 }
 
+/**
+ * Allows programmatic control of [[javax.net.ssl.KeyManagerFactory]] used by Akka Remote SSL.
+ *
+ * @param provider Provider that will be used to instantiate a KeyManagerFactory for the default algorithm.
+ * @param keyManagerFactoryParameters If supplied will be used to initialise the KeyManagerFactory instead of any key store specified in Config.
+ */
 case class KeyManagerFactorySetup(provider: Provider, keyManagerFactoryParameters: Option[ManagerFactoryParameters])
   extends CryptoServiceProviderSetup
 
+/**
+ * Factory for [[akka.remote.security.setup.KeyManagerFactorySetup]] instances.
+ */
 object KeyManagerFactorySetup {
   /**
-   * Java API
+   * Java API factory method to create a KeyManagerFactorySetup.
+   *
+   * @param provider Provider that will be used to instantiate a [[javax.net.ssl.KeyManagerFactory]] for the default algorithm.
+   * Will be initialised using the key store specified in Config.
    */
   def create(provider: Provider): KeyManagerFactorySetup = KeyManagerFactorySetup(provider, None)
 
   /**
-   * Java API
+   * Java API factory method to create a KeyManagerFactorySetup.
+   *
+   * @param provider Provider that will be used to instantiate a [[javax.net.ssl.KeyManagerFactory]] for the default algorithm.
+   * @param managerFactoryParameters Will be used to initialise the [[javax.net.ssl.KeyManagerFactory]] instead of Config settings.
    */
-  def create(keyManagerFactoryProvider: Provider, keyManagerFactoryParameters: ManagerFactoryParameters): KeyManagerFactorySetup =
-    KeyManagerFactorySetup(keyManagerFactoryProvider, Some(keyManagerFactoryParameters))
+  def create(provider: Provider, managerFactoryParameters: ManagerFactoryParameters): KeyManagerFactorySetup =
+    KeyManagerFactorySetup(provider, Some(managerFactoryParameters))
 
+  /**
+   * Factory to create a KeyManagerFactorySetup to instantiate a [[javax.net.ssl.KeyManagerFactory]] based on the given KeyManagerFactorySpi for the default
+   * algorithm. This will be initialised using the key store specified in Config.
+   *
+   * @tparam T Class on which to base a KeyManagerFactory. Must be public and top level.
+   */
   def providing[T <: KeyManagerFactorySpi](implicit tag: ClassTag[T]): KeyManagerFactorySetup = {
     providing[T](None)(tag)
   }
 
+  /**
+   * Factory to create a KeyManagerFactorySetup to instantiate a [[javax.net.ssl.KeyManagerFactory]] based on the given KeyManagerFactorySpi for the default algorithm.
+   *
+   * @tparam T Class on which to base a KeyManagerFactory. Must be public and top level.
+   * @param managerFactoryParameters Used to initialise the KeyManagerFactory instead of any key store specified in Config.
+   */
   def providing[T <: KeyManagerFactorySpi](managerFactoryParameters: ManagerFactoryParameters)(implicit tag: ClassTag[T]): KeyManagerFactorySetup = {
     providing[T](Some(managerFactoryParameters))(tag)
   }
@@ -62,42 +89,82 @@ object KeyManagerFactorySetup {
   }
 
   /**
-   * Java API
+   * Java API factory to create a KeyManagerFactorySetup to instantiate a [[javax.net.ssl.KeyManagerFactory]] based on the given KeyManagerFactorySpi for the
+   * default algorithm. This will be initialised using the key store specified in Config.
+   *
+   * @param clazz Class on which to base a KeyManagerFactory. Must be public and top level.
    */
   def createProviding[T <: KeyManagerFactorySpi](clazz: Class[T]): KeyManagerFactorySetup = {
     providing[T](None)(ClassTag(clazz))
   }
 
   /**
-   * Java API
+   * Java API factory to create a KeyManagerFactorySetup to instantiate a [[javax.net.ssl.KeyManagerFactory]] based on the given KeyManagerFactorySpi
+   * for the default algorithm.
+   *
+   * @param clazz Class on which to base a KeyManagerFactory. Must be public and top level.
+   * @param managerFactoryParameters Used to initialise the KeyManagerFactory instead of any key store specified in Config.
    */
   def createProviding[T <: KeyManagerFactorySpi](clazz: Class[T], managerFactoryParameters: ManagerFactoryParameters): KeyManagerFactorySetup = {
     providing[T](Some(managerFactoryParameters))(ClassTag(clazz))
   }
 
+  /**
+   * Factory to create a KeyManagerFactorySetup to instantiate a [[javax.net.ssl.KeyManagerFactory]] that delegates to the given KeyManagerFactory for the
+   * default algorithm. The delegate is assumed to be already initialised.
+   *
+   * @param keyManagerFactory Initialised delegate KeyManagerFactory.
+   */
   def delegatingTo(keyManagerFactory: KeyManagerFactory): KeyManagerFactorySetup =
     KeyManagerFactorySetup(DelegatingKeyManagerFactoryProvider, Some(DelegatingKeyManagerFactoryParameters(keyManagerFactory)))
 }
 
+/**
+ * Allows programmatic control of [[javax.net.ssl.TrustManagerFactory]] used by Akka Remote SSL.
+ *
+ * @param provider Provider that will be used to instantiate a TrustManagerFactory for the default algorithm.
+ * @param trustManagerFactoryParameters If supplied will be used to initialise the TrustManagerFactory instead of any trust store specified in Config.
+ */
 case class TrustManagerFactorySetup(provider: Provider, trustManagerFactoryParameters: Option[ManagerFactoryParameters])
   extends CryptoServiceProviderSetup
 
+/**
+ * Factory for [[akka.remote.security.setup.TrustManagerFactorySetup]] instances.
+ */
 object TrustManagerFactorySetup {
   /**
-   * Java API
+   * Java API factory method to create a TrustManagerFactorySetup.
+   *
+   * @param provider Provider that will be used to instantiate a [[javax.net.ssl.TrustManagerFactory]] for the default algorithm.
+   * Will be initialised using the trust store specified in Config.
    */
   def create(provider: Provider): TrustManagerFactorySetup = TrustManagerFactorySetup(provider, None)
 
   /**
-   * Java API
+   * Java API factory method to create a TrustManagerFactorySetup.
+   *
+   * @param provider Provider that will be used to instantiate a [[javax.net.ssl.TrustManagerFactory]] for the default algorithm.
+   * @param managerFactoryParameters Will be used to initialise the [[javax.net.ssl.TrustManagerFactory]] instead of Config settings.
    */
-  def create(trustManagerFactoryProvider: Provider, trustManagerFactoryParameters: ManagerFactoryParameters): TrustManagerFactorySetup =
-    TrustManagerFactorySetup(trustManagerFactoryProvider, Some(trustManagerFactoryParameters))
+  def create(provider: Provider, managerFactoryParameters: ManagerFactoryParameters): TrustManagerFactorySetup =
+    TrustManagerFactorySetup(provider, Some(managerFactoryParameters))
 
+  /**
+   * Factory to create a TrustManagerFactorySetup to instantiate a [[javax.net.ssl.TrustManagerFactory]] based on the given TrustManagerFactorySpi for the default
+   * algorithm. This will be initialised using the trust store specified in Config.
+   *
+   * @tparam T Class on which to base a TrustManagerFactory. Must be public and top level.
+   */
   def providing[T <: TrustManagerFactorySpi](implicit tag: ClassTag[T]): TrustManagerFactorySetup = {
     providing[T](None)(tag)
   }
 
+  /**
+   * Factory to create a TrustManagerFactorySetup to instantiate a [[javax.net.ssl.TrustManagerFactory]] based on the given TrustManagerFactorySpi for the default algorithm.
+   *
+   * @tparam T Class on which to base a TrustManagerFactory. Must be public and top level.
+   * @param managerFactoryParameters Used to initialise the TrustManagerFactory instead of any trust store specified in Config.
+   */
   def providing[T <: TrustManagerFactorySpi](managerFactoryParameters: ManagerFactoryParameters)(implicit tag: ClassTag[T]): TrustManagerFactorySetup = {
     providing[T](Some(managerFactoryParameters))(tag)
   }
@@ -118,19 +185,32 @@ object TrustManagerFactorySetup {
   }
 
   /**
-   * Java api
+   * Java API factory to create a TrustManagerFactorySetup to instantiate a [[javax.net.ssl.TrustManagerFactory]] based on the given TrustManagerFactorySpi for the
+   * default algorithm. This will be initialised using the trust store specified in Config.
+   *
+   * @param clazz Class on which to base a TrustManagerFactory. Must be public and top level.
    */
   def createProviding[T <: TrustManagerFactorySpi](clazz: Class[T]): TrustManagerFactorySetup = {
     providing[T](None)(ClassTag(clazz))
   }
 
   /**
-   * Java api
+   * Java API factory to create a TrustManagerFactorySetup to instantiate a [[javax.net.ssl.TrustManagerFactory]] based on the given TrustManagerFactorySpi
+   * for the default algorithm.
+   *
+   * @param clazz Class on which to base a TrustManagerFactory. Must be public and top level.
+   * @param managerFactoryParameters Used to initialise the TrustManagerFactory instead of any trust store specified in Config.
    */
   def createProviding[T <: TrustManagerFactorySpi](clazz: Class[T], managerFactoryParameters: ManagerFactoryParameters): TrustManagerFactorySetup = {
     providing[T](Some(managerFactoryParameters))(ClassTag(clazz))
   }
 
+  /**
+   * Factory to create a TrustManagerFactorySetup to instantiate a [[javax.net.ssl.TrustManagerFactory]] that delegates to the given TrustManagerFactory for the
+   * default algorithm. The delegate is assumed to be already initialised.
+   *
+   * @param trustManagerFactory Initialised delegate TrustManagerFactory.
+   */
   def delegatingTo(trustManagerFactory: TrustManagerFactory): TrustManagerFactorySetup =
     TrustManagerFactorySetup(DelegatingTrustManagerFactoryProvider, Some(DelegatingTrustManagerFactoryParameters(trustManagerFactory)))
 }
