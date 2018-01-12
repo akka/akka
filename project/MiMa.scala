de5377e3993da04ef3ee9fdd18d9bccca28404c3
Martynas Mickevičius <self@2m.lt>
2018-01-12 14:25:10 +0700

    Update the latest versions for MiMa checks

--- project/MiMa.scala
+++ project/MiMa.scala
@@ -10,8 +10,8 @@ import com.typesafe.tools.mima.plugin.MimaPlugin.autoImport._
 
 object MiMa extends AutoPlugin {
 
-  private val latestMinorOf25 = 8
-  private val latestMinorOf24 = 18
+  private val latestMinorOf25 = 9
+  private val latestMinorOf24 = 20
 
   override def requires = MimaPlugin
   override def trigger = allRequirements
