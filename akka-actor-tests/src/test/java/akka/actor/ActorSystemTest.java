94acdd2483caa55ce2e8a045eb6f4a475914389d
Evgeny Veretennikov <vere10@yandex-team.ru>
2018-01-18 15:34:54 +0300

    #24303 test just !isDone() in no-termination getWhenTerminated() test

--- akka-actor-tests/src/test/java/akka/actor/ActorSystemTest.java
+++ akka-actor-tests/src/test/java/akka/actor/ActorSystemTest.java
@@ -4,16 +4,15 @@
 package akka.actor;
 
 import akka.testkit.AkkaJUnitActorSystemResource;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.scalatest.junit.JUnitSuite;
 
 import java.util.concurrent.CompletionStage;
-import java.util.concurrent.TimeoutException;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertFalse;
 
 public class ActorSystemTest extends JUnitSuite {
 
@@ -36,15 +35,7 @@ public class ActorSystemTest extends JUnitSuite {
   }
 
   @Test
-  public void testGetWhenTerminatedWithoutTermination() throws Exception {
-    final CompletionStage<Terminated> cs = system.getWhenTerminated();
-    try {
-      cs.toCompletableFuture().get(2, SECONDS);
-      Assert.fail("System wasn't terminated, but getWhenTerminated() unexpectedly completed");
-    } catch (TimeoutException e) {
-      // we expect this to be thrown, because system wasn't terminated
-    } finally {
-      system.terminate();
-    }
+  public void testGetWhenTerminatedWithoutTermination() {
+    assertFalse(system.getWhenTerminated().toCompletableFuture().isDone());
   }
 }
--- akka-actor-typed-tests/src/test/java/akka/actor/typed/ActorSystemTest.java
+++ akka-actor-typed-tests/src/test/java/akka/actor/typed/ActorSystemTest.java
@@ -3,14 +3,13 @@
   */
 package akka.actor.typed;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.scalatest.junit.JUnitSuite;
 
 import java.util.concurrent.CompletionStage;
-import java.util.concurrent.TimeoutException;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertFalse;
 
 public class ActorSystemTest extends JUnitSuite {
 
@@ -23,16 +22,8 @@ public class ActorSystemTest extends JUnitSuite {
   }
 
   @Test
-  public void testGetWhenTerminatedWithoutTermination() throws Exception {
+  public void testGetWhenTerminatedWithoutTermination() {
     final ActorSystem system = ActorSystem.create(Behavior.empty(), "GetWhenTerminatedWithoutTermination");
-    final CompletionStage<Terminated> cs = system.getWhenTerminated();
-    try {
-      cs.toCompletableFuture().get(2, SECONDS);
-      Assert.fail("System wasn't terminated, but getWhenTerminated() unexpectedly completed");
-    } catch (TimeoutException e) {
-      // we expect this to be thrown, because system wasn't terminated
-    } finally {
-      system.terminate();
-    }
+    assertFalse(system.getWhenTerminated().toCompletableFuture().isDone());
   }
 }
