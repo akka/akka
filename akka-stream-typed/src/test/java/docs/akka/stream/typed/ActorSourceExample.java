6b396739165391a73f1305e3e87d4ba06a022c21
Martynas Mickevičius <self@2m.lt>
2018-01-22 21:35:56 +0700

    Add docs

---
+++ akka-stream-typed/src/test/java/docs/akka/stream/typed/ActorSourceExample.java
@@ -0,0 +1,86 @@
+package docs.akka.stream.typed;
+
+// #actor-source-ref
+import akka.actor.typed.ActorRef;
+import akka.japi.JavaPartialFunction;
+import akka.stream.ActorMaterializer;
+import akka.stream.OverflowStrategy;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import akka.stream.typed.javadsl.ActorSource;
+import scala.PartialFunction;
+import scala.runtime.BoxedUnit;
+// #actor-source-ref
+
+public class ActorSourceExample {
+
+  // #actor-source-ref
+
+  interface Protocol {}
+  class Message implements Protocol {
+    private final String msg;
+    public Message(String msg) {
+      this.msg = msg;
+    }
+  }
+  class Complete implements Protocol {}
+  class Fail implements Protocol {
+    private final Exception ex;
+    public Fail(Exception ex) {
+      this.ex = ex;
+    }
+  }
+  // #actor-source-ref
+
+  final ActorMaterializer mat = null;
+
+  {
+    // #actor-source-ref
+
+    final PartialFunction<Protocol, BoxedUnit> completionMatcher =
+      new JavaPartialFunction<Protocol, BoxedUnit>() {
+        public BoxedUnit apply(Protocol p, boolean isCheck) {
+          if (p instanceof Complete) {
+            return BoxedUnit.UNIT;
+          }
+          else {
+            throw noMatch();
+          }
+        }
+      };
+
+    final PartialFunction<Protocol, Throwable> failureMatcher =
+      new JavaPartialFunction<Protocol, Throwable>() {
+        public Throwable apply(Protocol p, boolean isCheck) {
+          if (p instanceof Fail) {
+            return ((Fail)p).ex;
+          }
+          else {
+            throw noMatch();
+          }
+        }
+      };
+
+    final Source<Protocol, ActorRef<Protocol>> source = ActorSource.actorRef(
+      completionMatcher,
+      failureMatcher,
+      8,
+      OverflowStrategy.fail()
+    );
+
+    final ActorRef<Protocol> ref = source.collect(new JavaPartialFunction<Protocol, String>() {
+      public String apply(Protocol p, boolean isCheck) {
+        if (p instanceof Message) {
+          return ((Message)p).msg;
+       }
+       else {
+          throw noMatch();
+       }
+     }
+    }).to(Sink.foreach(System.out::println)).run(mat);
+
+    ref.tell(new Message("msg1"));
+    // ref.tell("msg2"); Does not compile
+    // #actor-source-ref
+  }
+}
