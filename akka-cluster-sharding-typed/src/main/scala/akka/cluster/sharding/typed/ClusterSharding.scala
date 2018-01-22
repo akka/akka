31660cf9a8a4da2112444d11690d9b473ebda9b5
Christopher Batey <christopher.batey@gmail.com>
2018-01-22 13:47:18 +0000

    Typed docs for sharding and singleton (#24364)

--- akka-cluster-sharding-typed/src/main/scala/akka/cluster/sharding/typed/ClusterSharding.scala
+++ akka-cluster-sharding-typed/src/main/scala/akka/cluster/sharding/typed/ClusterSharding.scala
@@ -283,7 +283,7 @@ final class AdaptedClusterShardingImpl(system: ActorSystem[_]) extends ClusterSh
 }
 
 @DoNotInherit
-sealed trait ClusterSharding extends Extension {
+sealed abstract class ClusterSharding extends Extension {
 
   /**
    * Spawn a shard region or a proxy depending on if the settings require role and if this node has such a role.
