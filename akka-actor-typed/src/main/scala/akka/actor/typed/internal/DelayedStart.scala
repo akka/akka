9dda9a04d6b2633082dc54900a0e10f2f219e075
Johan Andrén <johan@markatta.com>
2018-01-23 16:36:04 +0100

    minor review fixes

--- akka-actor-typed/src/main/scala/akka/actor/typed/internal/DelayedStart.scala
+++ akka-actor-typed/src/main/scala/akka/actor/typed/internal/DelayedStart.scala
@@ -18,10 +18,9 @@ private[akka] object DelayedStart {
 
   case object Start
 
-  // note that we cannot use StashBuffer here as we need to buffer signals as well as messages
   def delayStart[T](delayedBehavior: Behavior[T]): Behavior[Any] =
     scaladsl.Behaviors.deferred[Any] { _ ⇒
-      val buffer = MutableStashBuffer[Any](100)
+      val buffer = MutableStashBuffer[Any](1000)
 
       scaladsl.Behaviors.immutable[Any] { (sctx, msg) ⇒
         // delay actual initialization until the actor system is started as the actor may touch
@@ -30,7 +29,7 @@ private[akka] object DelayedStart {
           case Start ⇒
             buffer.unstashAll(sctx, delayedBehavior.asInstanceOf[Behavior[Any]])
           case t ⇒
-            buffer.stash(t)
+            if (!buffer.isFull) buffer.stash(t)
             Behavior.same
         }
       }.onSignal {
