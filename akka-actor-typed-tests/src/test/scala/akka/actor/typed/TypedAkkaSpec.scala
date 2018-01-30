5b2a4edd2c2e0bbd49e453c25a62040e194b5778
Christopher Batey <christopher.batey@gmail.com>
2018-01-30 15:23:49 +0000

    Increase timeout for system termination in BasicClusterConfigSpec (#24446)
    Refs #24444

--- akka-actor-typed-tests/src/test/scala/akka/actor/typed/TypedAkkaSpec.scala
+++ akka-actor-typed-tests/src/test/scala/akka/actor/typed/TypedAkkaSpec.scala
@@ -3,17 +3,20 @@ package akka.actor.typed
 import akka.testkit.typed.{ TestInbox, TestKit }
 import akka.util.Timeout
 import org.scalactic.TypeCheckedTripleEquals
-import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.concurrent.{ Eventually, ScalaFutures }
 import org.scalatest.time.Span
 import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpecLike }
+
 import scala.concurrent.duration._
 import scala.util.control.NoStackTrace
 
 /**
  * Helper trait to include standard traits for typed tests
  */
-trait TypedAkkaSpec extends WordSpecLike with Matchers with BeforeAndAfterAll with ScalaFutures with TypeCheckedTripleEquals {
-  implicit val akkaPatience = PatienceConfig(3.seconds, Span(100, org.scalatest.time.Millis))
+trait TypedAkkaSpec extends WordSpecLike with Matchers with BeforeAndAfterAll with ScalaFutures
+  with TypeCheckedTripleEquals with Eventually {
+
+  implicit override val patienceConfig: PatienceConfig = PatienceConfig(3.seconds, Span(100, org.scalatest.time.Millis))
   implicit val timeout = Timeout(3.seconds)
 
   def assertEmpty(inboxes: TestInbox[_]*): Unit = {
