06b36db458792d4e4923b0ef6e736b7a91beffb6
Johan Andrén <johan@markatta.com>
2018-02-02 17:49:03 +0100

    Actor interaction patterns docs for typed (#24404)
    * Request response and actor-to-actor-ask
    
    * Outside of actor ask
    
    * Review feedback plus simplification of tell sample
    
    * ask and mention symbolic
    
    * Final sample and some proof reading feedback adressed
    
    * Tests should pass in InteractionPatternSpec
    
    * Incorrect comment removed
    
    * Made sure all scala samples execute, to protect from regression, added forward refereces in text
    
    * Use context logger
    
    * Some minor rewording
    
    * Feedback applied
    
    * Rebase fix
    
    * expectMessageType

--- akka-actor-typed-tests/src/test/java/jdocs/akka/typed/InteractionPatternsTest.java
+++ akka-actor-typed-tests/src/test/java/jdocs/akka/typed/InteractionPatternsTest.java
@@ -3,15 +3,14 @@
  */
 package jdocs.akka.typed;
 
+import akka.NotUsed;
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.Props;
-import akka.actor.typed.javadsl.ActorContext;
-import akka.actor.typed.javadsl.BehaviorBuilder;
-import akka.actor.typed.javadsl.Behaviors;
-import akka.actor.typed.javadsl.TimerScheduler;
+import akka.actor.typed.javadsl.*;
 import akka.testkit.typed.scaladsl.TestProbe;
+import akka.util.Timeout;
 import org.junit.Test;
 import org.scalatest.junit.JUnitSuite;
 import scala.concurrent.Await;
@@ -20,36 +19,64 @@ import scala.concurrent.duration.FiniteDuration;
 
 import java.net.URI;
 import java.util.*;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.TimeUnit;
 
 public class InteractionPatternsTest extends JUnitSuite {
 
-  // #fire-and-forget
-  interface PrinterProtocol { }
-  class DisableOutput implements PrinterProtocol { }
-  class EnableOutput implements PrinterProtocol { }
-  class PrintMe implements PrinterProtocol {
+  // #fire-and-forget-definition
+  class PrintMe {
     public final String message;
     public PrintMe(String message) {
       this.message = message;
     }
   }
 
-  public Behavior<PrinterProtocol> enabledPrinterBehavior() {
-    return BehaviorBuilder.<PrinterProtocol>create()
-      .onMessage(DisableOutput.class, (ctx, disableOutput) -> disabledPrinterBehavior())
-      .onMessage(PrintMe.class, (ctx, printMe) -> {
-        System.out.println(printMe.message);
-        return Behaviors.same();
-      }).build();
+  static final Behavior<PrintMe> printerBehavior = Behaviors.immutable(PrintMe.class)
+    .onMessage(PrintMe.class, (ctx, printMe) -> {
+      ctx.getLog().info(printMe.message);
+      return Behaviors.same();
+    }).build();
+  // #fire-and-forget-definition
+
+  // #request-response-protocol
+  class Request {
+    public final String query;
+    public final ActorRef<Response> respondTo;
+    public Request(String query, ActorRef<Response> respondTo) {
+      this.query = query;
+      this.respondTo = respondTo;
+    }
   }
 
-  public Behavior<PrinterProtocol> disabledPrinterBehavior() {
-    return BehaviorBuilder.<PrinterProtocol>create()
-      .onMessage(EnableOutput.class, (ctx, enableOutput) -> enabledPrinterBehavior())
-      .build();
+  class Response {
+    public final String result;
+    public Response(String result) {
+      this.result = result;
+    }
   }
-  // #fire-and-forget
+  // #request-response-protocol
+
+  public void compileOnlyRequestResponse() {
+
+    // #request-response-respond
+    // actor behavior
+    Behaviors.immutable(Request.class)
+      .onMessage(Request.class, (ctx, request) -> {
+        // ... process request ...
+        request.respondTo.tell(new Response("Here's your response!"));
+        return Behaviors.same();
+      }).build();
+    // #request-response-respond
+
+    ActorRef<Request> otherActor = null;
+    ActorContext<Response> ctx = null;
+
+    // #request-response-send
+    otherActor.tell(new Request("give me cookies", ctx.getSelf()));
+    // #request-response-send
+  }
+
 
   // #adapted-response
 
@@ -101,7 +128,7 @@ public class InteractionPatternsTest extends JUnitSuite {
   public static class Frontend {
 
     interface Command {}
-    public static class  Translate implements Command {
+    public static class Translate implements Command {
       public final URI site;
       public final ActorRef<URI> replyTo;
 
@@ -177,15 +204,17 @@ public class InteractionPatternsTest extends JUnitSuite {
             return this;
           })
           .onMessage(WrappedJobStarted.class, wrapped -> {
-            System.out.println("Started " + wrapped.response.taskId);
+            ctx.getLog().info("Started {}", wrapped.response.taskId);
             return this;
           })
           .onMessage(WrappedJobProgress.class, wrapped -> {
-            System.out.println("Progress " + wrapped.response.taskId + ": " + wrapped.response.progress);
+            ctx.getLog().info("Progress {}: {}", wrapped.response.taskId,
+              wrapped.response.progress);
             return this;
           })
           .onMessage(WrappedJobCompleted.class, wrapped -> {
-            System.out.println("Completed " + wrapped.response.taskId + ": " + wrapped.response.result);
+            ctx.getLog().info("Completed {}: {}", wrapped.response.taskId,
+              wrapped.response.result);
             return this;
           })
           .onMessage(OtherResponse.class, other -> Behaviors.unhandled())
@@ -198,20 +227,17 @@ public class InteractionPatternsTest extends JUnitSuite {
 
   @Test
   public void fireAndForgetSample() throws Exception {
-    // #fire-and-forget
-    final ActorSystem<PrinterProtocol> system =
-      ActorSystem.create(enabledPrinterBehavior(), "printer-sample-system");
+    // #fire-and-forget-doit
+    final ActorSystem<PrintMe> system =
+      ActorSystem.create(printerBehavior, "printer-sample-system");
 
     // note that system is also the ActorRef to the guardian actor
-    final ActorRef<PrinterProtocol> ref = system;
+    final ActorRef<PrintMe> ref = system;
 
     // these are all fire and forget
-    ref.tell(new PrintMe("message"));
-    ref.tell(new DisableOutput());
-    ref.tell(new PrintMe("message"));
-    ref.tell(new EnableOutput());
-
-    // #fire-and-forget
+    ref.tell(new PrintMe("message 1"));
+    ref.tell(new PrintMe("message 2"));
+    // #fire-and-forget-doit
 
     Await.ready(system.terminate(), Duration.create(3, TimeUnit.SECONDS));
   }
@@ -255,7 +281,7 @@ public class InteractionPatternsTest extends JUnitSuite {
 
   private static final Object TIMER_KEY = new Object();
 
-  private static class Timeout implements Msg {
+  private static class TimeoutMsg implements Msg {
   }
 
   public static Behavior<Msg> behavior(ActorRef<Batch> target, FiniteDuration after, int maxSize) {
@@ -266,7 +292,7 @@ public class InteractionPatternsTest extends JUnitSuite {
                                     FiniteDuration after, int maxSize) {
     return Behaviors.immutable(Msg.class)
       .onMessage(Msg.class, (ctx, msg) -> {
-        timers.startSingleTimer(TIMER_KEY, new Timeout(), after);
+        timers.startSingleTimer(TIMER_KEY, new TimeoutMsg(), after);
         List<Msg> buffer = new ArrayList<>();
         buffer.add(msg);
         return active(buffer, timers, target, after, maxSize);
@@ -277,7 +303,7 @@ public class InteractionPatternsTest extends JUnitSuite {
   private static Behavior<Msg> active(List<Msg> buffer, TimerScheduler<Msg> timers,
                                       ActorRef<Batch> target, FiniteDuration after, int maxSize) {
     return Behaviors.immutable(Msg.class)
-      .onMessage(Timeout.class, (ctx, msg) -> {
+      .onMessage(TimeoutMsg.class, (ctx, msg) -> {
         target.tell(new Batch(buffer));
         return idle(timers, target, after, maxSize);
       })
@@ -301,7 +327,8 @@ public class InteractionPatternsTest extends JUnitSuite {
     TestProbe<Batch> probe = new TestProbe<>("batcher", system);
     ActorRef<Msg> bufferer = Await.result(system.systemActorOf(
       behavior(probe.ref(), new FiniteDuration(1, TimeUnit.SECONDS), 10),
-      "batcher", Props.empty(), akka.util.Timeout.apply(1, TimeUnit.SECONDS)), new FiniteDuration(1, TimeUnit.SECONDS));
+      "batcher", Props.empty(), akka.util.Timeout.apply(1, TimeUnit.SECONDS)),
+      new FiniteDuration(1, TimeUnit.SECONDS));
 
     ExcitingMessage msgOne = new ExcitingMessage("one");
     ExcitingMessage msgTwo = new ExcitingMessage("two");
@@ -313,6 +340,223 @@ public class InteractionPatternsTest extends JUnitSuite {
 
     Await.ready(system.terminate(), Duration.create(3, TimeUnit.SECONDS));
   }
+
+
+
+  // #actor-ask
+  interface HalCommand {}
+  static final class OpenThePodBayDoorsPlease implements HalCommand {
+    public final ActorRef<HalResponse> respondTo;
+    OpenThePodBayDoorsPlease(ActorRef<HalResponse> respondTo) {
+      this.respondTo = respondTo;
+    }
+  }
+  static final class HalResponse {
+    public final String message;
+    HalResponse(String message) {
+      this.message = message;
+    }
+  }
+
+  static final Behavior<HalCommand> halBehavior =
+    Behaviors.immutable(HalCommand.class)
+      .onMessage(OpenThePodBayDoorsPlease.class, (ctx, msg) -> {
+        msg.respondTo.tell(new HalResponse("I'm sorry, Dave. I'm afraid I can't do that."));
+        return Behaviors.same();
+      }).build();
+
+  interface DaveProtocol {}
+
+  // this is a part of the protocol that is internal to the actor itself
+  private static final class AdaptedResponse implements DaveProtocol {
+    public final String message;
+    public AdaptedResponse(String message) {
+      this.message = message;
+    }
+  }
+
+  public static Behavior<DaveProtocol> daveBehavior(final ActorRef<HalCommand> hal) {
+    return Behaviors.deferred((ActorContext<DaveProtocol> ctx) -> {
+
+      // asking someone requires a timeout, if the timeout hits without response
+      // the ask is failed with a TimeoutException
+      final Timeout timeout = Timeout.apply(3, TimeUnit.SECONDS);
+
+      ctx.ask(
+        HalResponse.class,
+        hal,
+        timeout,
+        // construct the outgoing message
+        (ActorRef<HalResponse> ref) -> new OpenThePodBayDoorsPlease(ref),
+        // adapt the response (or failure to respond)
+        (response, throwable) -> {
+          if (response != null) {
+            return new AdaptedResponse(response.message);
+          } else {
+            return new AdaptedResponse("Request failed");
+          }
+      });
+
+      // we can also tie in request context into an interaction, it is safe to look at
+      // actor internal state from the transformation function, but remember that it may have
+      // changed at the time the response arrives and the transformation is done, best is to
+      // use immutable state we have closed over like here.
+      final int requestId = 1;
+      ctx.ask(
+        HalResponse.class,
+        hal,
+        timeout,
+        // construct the outgoing message
+        (ActorRef<HalResponse> ref) -> new OpenThePodBayDoorsPlease(ref),
+        // adapt the response (or failure to respond)
+        (response, throwable) -> {
+          if (response != null) {
+            return new AdaptedResponse(requestId + ": " + response.message);
+          } else {
+            return new AdaptedResponse(requestId + ": Request failed");
+          }
+        });
+
+      return Behaviors.immutable(DaveProtocol.class)
+        // the adapted message ends up being processed like any other
+        // message sent to the actor
+        .onMessage(AdaptedResponse.class, (innerCtx, response) -> {
+          innerCtx.getLog().info("Got response from HAL: {}", response.message);
+          return Behaviors.same();
+        }).build();
+    });
+  }
+  // #actor-ask
+
+
+  // #standalone-ask
+  interface CookieCommand {}
+  static class GiveMeCookies implements CookieCommand {
+    public final ActorRef<Cookies> cookies;
+    GiveMeCookies(ActorRef<Cookies> cookies) {
+      this.cookies = cookies;
+    }
+  };
+  static class Cookies {}
+
+
+  public void askAndPrint(ActorSystem<Object> system, ActorRef<CookieCommand> cookieActorRef) {
+    CompletionStage<Cookies> result = AskPattern.ask(
+      cookieActorRef,
+      GiveMeCookies::new,
+      Timeout.apply(3, TimeUnit.SECONDS),
+      system.scheduler());
+
+    result.whenComplete((cookies, failure) -> {
+      if (cookies != null) System.out.println("Yay, cookies!");
+      else System.out.println("Boo! didn't get cookies in time.");
+    });
+  }
+  // #standalone-ask
+
+
+  // #per-session-child
+  // dummy data types just for this sample
+  interface Keys {}
+  interface Wallet {}
+  // #per-session-child
+
+  static final Behavior<GetKeys> keyCabinetBehavior = null;
+  static final Behavior<GetWallet> drawerBehavior = null;
+  // #per-session-child
+  // messages for the two services we interact with
+  class GetKeys {
+    public final String whoseKeys;
+    public final ActorRef<Keys> respondTo;
+    public GetKeys(String whoseKeys, ActorRef<Keys> respondTo) {
+      this.whoseKeys = whoseKeys;
+      this.respondTo = respondTo;
+    }
+  }
+  class GetWallet {
+    public final String whoseWallet;
+    public final ActorRef<Wallet> respondTo;
+    public GetWallet(String whoseWallet, ActorRef<Wallet> respondTo) {
+      this.whoseWallet = whoseWallet;
+      this.respondTo = respondTo;
+    }
+  }
+
+  interface HomeCommand {}
+  class LeaveHome implements HomeCommand {
+    public final String who;
+    public final ActorRef<ReadyToLeaveHome> respondTo;
+    public LeaveHome(String who, ActorRef<ReadyToLeaveHome> respondTo) {
+      this.who = who;
+      this.respondTo = respondTo;
+    }
+  }
+
+  class ReadyToLeaveHome {
+    public final String who;
+    public final Keys keys;
+    public final Wallet wallet;
+    public ReadyToLeaveHome(String who, Keys keys, Wallet wallet) {
+      this.who = who;
+      this.keys = keys;
+      this.wallet = wallet;
+    }
+  }
+
+  // actor behavior
+  public Behavior<HomeCommand> homeBehavior() {
+    return Behaviors.deferred((ctx) -> {
+      final ActorRef<GetKeys> keyCabinet = ctx.spawn(keyCabinetBehavior, "key-cabinet");
+      final ActorRef<GetWallet> drawer = ctx.spawn(drawerBehavior, "drawer");
+
+      return Behaviors.immutable(HomeCommand.class)
+        .onMessage(LeaveHome.class, (innerCtx, msg) -> {
+          ctx.spawn(new PrepareToLeaveHome(msg.who, msg.respondTo, keyCabinet, drawer), "leaving" + msg.who);
+          return Behavior.same();
+        }).build();
+    });
+  }
+
+  // per session actor behavior
+  class PrepareToLeaveHome extends Behaviors.MutableBehavior<Object> {
+    private final String whoIsLeaving;
+    private final ActorRef<ReadyToLeaveHome> respondTo;
+    private final ActorRef<GetKeys> keyCabinet;
+    private final ActorRef<GetWallet> drawer;
+    private Optional<Wallet> wallet = Optional.empty();
+    private Optional<Keys> keys = Optional.empty();
+    public PrepareToLeaveHome(String whoIsLeaving, ActorRef<ReadyToLeaveHome> respondTo, ActorRef<GetKeys> keyCabinet, ActorRef<GetWallet> drawer) {
+      this.whoIsLeaving = whoIsLeaving;
+      this.respondTo = respondTo;
+      this.keyCabinet = keyCabinet;
+      this.drawer = drawer;
+    }
+
+    @Override
+    public Behaviors.Receive<Object> createReceive() {
+      return receiveBuilder()
+        .onMessage(Wallet.class, (wallet) -> {
+          this.wallet = Optional.of(wallet);
+          return completeOrContinue();
+        }).onMessage(Keys.class, (keys) -> {
+          this.keys = Optional.of(keys);
+          return completeOrContinue();
+        }).build();
+    }
+
+
+    private Behavior<Object> completeOrContinue() {
+      if (wallet.isPresent() && keys.isPresent()) {
+        respondTo.tell(new ReadyToLeaveHome(whoIsLeaving, keys.get(), wallet.get()));
+        return Behaviors.stopped();
+      } else {
+        return this;
+      }
+    }
+  }
+  // #per-session-child
+
+
 
 }
 
