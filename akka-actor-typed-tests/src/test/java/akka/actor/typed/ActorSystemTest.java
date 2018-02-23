e8235b7e63ece032b42836a3aadb657b703e796d
kelebra <kelebra20@gmail.com>
2018-02-22 23:19:15 -0600

    Fixed duplication in formatting

--- akka-actor-typed-tests/src/test/java/akka/actor/typed/ActorSystemTest.java
+++ akka-actor-typed-tests/src/test/java/akka/actor/typed/ActorSystemTest.java
@@ -1,10 +1,7 @@
-/*
- * Copyright (C) 2018 Lightbend Inc. <https://www.lightbend.com>
+/**
+ * Copyright (C) 2009-2018 Lightbend Inc. <https://www.lightbend.com>
  */
 
-/**
-  * Copyright (C) 2009-2018 Lightbend Inc. <https://www.lightbend.com>
-  */
 package akka.actor.typed;
 
 import org.junit.Test;
