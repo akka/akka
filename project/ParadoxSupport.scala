ad4ec77fef97b3a92a84c2595bf5aba9ffb2f474
Richard Imaoka <richard.e.imaoka@gmail.com>
2018-03-01 05:55:25 +0900

    Copy from akka/akka-http#1890

--- project/ParadoxSupport.scala
+++ project/ParadoxSupport.scala
@@ -6,7 +6,6 @@ package akka
 
 import _root_.io.github.lukehutch.fastclasspathscanner.FastClasspathScanner
 import com.lightbend.paradox.markdown._
-import com.lightbend.paradox.markdown.ExternalLinkDirective.LinkException
 import com.lightbend.paradox.sbt.ParadoxPlugin.autoImport._
 import org.pegdown.Printer
 import org.pegdown.ast._
@@ -23,75 +22,65 @@ object ParadoxSupport {
       lazy val scanner = new FastClasspathScanner("akka").addClassLoader(classloader).scan()
       val allClasses = scanner.getNamesOfAllClasses.asScala.toVector
       Def.task { Seq(
-        { context: Writer.Context ⇒ new UnidocDirective(allClasses, context.location.tree.label, context.properties) }
+        { _: Writer.Context ⇒ new UnidocDirective(allClasses) }
       )}
     }.value
   )
 
-  class UnidocDirective(allClasses: IndexedSeq[String], page: Page, variables: Map[String, String]) extends InlineDirective("unidoc") {
+  class UnidocDirective(allClasses: IndexedSeq[String]) extends InlineDirective("unidoc") {
     def render(node: DirectiveNode, visitor: Visitor, printer: Printer): Unit = {
-      if (node.label.contains('.')) {
+      if (node.label.split('[')(0).contains('.')) {
         val fqcn = node.label
-        val label = fqcn.split('.').last
-        val source = new DirectiveNode.Source.Direct(fqcn)
-        val attributes = new org.pegdown.ast.DirectiveAttributes.AttributeMap()
-        val updatedNode = new DirectiveNode(DirectiveNode.Format.Inline, node.name, label, source, attributes, null,  new TextNode(label))
-        renderByFqcn(updatedNode, visitor, printer)
+        if (allClasses.contains(fqcn)) {
+          val label = fqcn.split('.').last
+          syntheticNode("java", javaLabel(label), fqcn, node).accept(visitor)
+          syntheticNode("scala", label, fqcn, node).accept(visitor)
+        } else {
+          throw new java.lang.IllegalStateException(s"fqcn not found by @unidoc[$fqcn]")
+        }
       }
       else {
         renderByClassName(node.label, node, visitor, printer)
       }
     }
 
-    def renderByClassName(label: String, node: DirectiveNode, visitor: Visitor, printer: Printer): Unit = {
-      def syntheticNode(group: String, label: String, c: String): DirectiveNode = {
-        val syntheticSource = new DirectiveNode.Source.Direct(c)
-        val attributes = new org.pegdown.ast.DirectiveAttributes.AttributeMap()
-        new DirectiveNode(DirectiveNode.Format.Inline, group, null, null, attributes, null,
-          new DirectiveNode(DirectiveNode.Format.Inline, group + "doc", label, syntheticSource, node.attributes, c,
-            new TextNode(label)
-          ))
-      }
+    def javaLabel(label: String): String =
+      label.replaceAll("\\[", "&lt;").replaceAll("\\]", "&gt;").replace('_', '?')
 
-      val scalaLabel = label.replaceAll("\\\\_", "_")
-      val labelWithoutGenericParameters = scalaLabel.split("\\[")(0)
-      val javaLabel = scalaLabel.replaceAll("\\[", "&lt;").replaceAll("\\]", "&gt;").replace('_', '?')
+    def syntheticNode(group: String, label: String, fqcn: String, node: DirectiveNode): DirectiveNode = {
+      val syntheticSource = new DirectiveNode.Source.Direct(fqcn)
+      val attributes = new org.pegdown.ast.DirectiveAttributes.AttributeMap()
+      new DirectiveNode(DirectiveNode.Format.Inline, group, null, null, attributes, null,
+        new DirectiveNode(DirectiveNode.Format.Inline, group + "doc", label, syntheticSource, node.attributes, fqcn,
+          new TextNode(label)
+        ))
+    }
+
+    def renderByClassName(label: String, node: DirectiveNode, visitor: Visitor, printer: Printer): Unit = {
+      val label = node.label.replaceAll("\\\\_", "_")
+      val labelWithoutGenericParameters = label.split("\\[")(0)
+      val labelWithJavaGenerics = javaLabel(label)
       val matches = allClasses.filter(_.endsWith('.' + labelWithoutGenericParameters))
       matches.size match {
         case 0 =>
-          throw new java.lang.IllegalStateException(s"No matches found for $label.")
+          throw new java.lang.IllegalStateException(s"No matches found for $label")
         case 1 if matches(0).contains("adsl") =>
           throw new java.lang.IllegalStateException(s"Match for $label only found in one language: ${matches(0)}")
         case 1 =>
-          syntheticNode("scala", scalaLabel, matches(0)).accept(visitor)
-          syntheticNode("java", javaLabel, matches(0)).accept(visitor)
+          syntheticNode("scala", label, matches(0), node).accept(visitor)
+          syntheticNode("java", labelWithJavaGenerics, matches(0), node).accept(visitor)
         case 2 if matches.forall(_.contains("adsl")) =>
           matches.foreach(m => {
             if (!m.contains("javadsl"))
-              syntheticNode("scala", scalaLabel, m).accept(visitor)
+              syntheticNode("scala", label, m, node).accept(visitor)
             if (!m.contains("scaladsl"))
-              syntheticNode("java", javaLabel, m).accept(visitor)
+              syntheticNode("java", labelWithJavaGenerics, m, node).accept(visitor)
           })
         case n =>
           throw new java.lang.IllegalStateException(
             s"$n matches found for @unidoc[$label], but not javadsl/scaladsl: ${matches.mkString(", ")}. " +
               s"You may want to use the fully qualified class name as @unidoc[fqcn] instead of @unidoc[${label}]."
           )
-      }
-    }
-
-    def renderByFqcn(node: DirectiveNode, visitor: Visitor, printer: Printer): Unit = {
-      try{
-        printer.print(s"""<span class="group-scala">""")
-        ScaladocDirective(page, variables).render(node, visitor, printer)
-        printer.print(s"</span>")
-
-        printer.print(s"""<span class="group-java">""")
-        JavadocDirective(page, variables).render(node, visitor, printer)
-        printer.print(s"</span>")
-      } catch {
-        case e: LinkException =>
-          throw new LinkException(s"${e.getMessage}. You may want to use the fully qualified class name as `@unidoc[fqcn]`.")
       }
     }
   }
