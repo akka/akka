80f68ca710c02a323c09312d943f6c8fb8b85e40
Johan Andrén <johan@markatta.com>
2018-03-15 12:16:04 +0100

    Split up receptionist implementations #24615

---
+++ akka-actor-typed/src/main/scala/akka/actor/typed/internal/receptionist/ServiceKey.scala
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) 2009-2018 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.actor.typed.internal.receptionist
+
+import akka.actor.typed.receptionist.ServiceKey
+import akka.annotation.InternalApi
+
+/**
+ * Internal representation of [[ServiceKey]] which is needed
+ * in order to use a TypedMultiMap (using keys with a type parameter does not
+ * work in Scala 2.x).
+ *
+ * Internal API
+ */
+@InternalApi
+private[akka] abstract class AbstractServiceKey {
+  type Protocol
+
+  /** Type-safe down-cast */
+  def asServiceKey: ServiceKey[Protocol]
+}
+
+/**
+ * This is the only actual concrete service key type
+ *
+ * Internal API
+ */
+final case class DefaultServiceKey[T](id: String, typeName: String) extends ServiceKey[T] {
+  override def toString: String = s"ServiceKey[$typeName]($id)"
+}
