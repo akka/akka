4211d1312bf3f2c9f5f96e4557ffd09c46b3b8eb
Hawstein <tomhawstein@gmail.com>
2018-03-28 11:58:16 +0800

    add java support and since to deprecated

--- akka-stream/src/main/scala/akka/stream/scaladsl/Graph.scala
+++ akka-stream/src/main/scala/akka/stream/scaladsl/Graph.scala
@@ -858,7 +858,8 @@ final class Balance[T](val outputPorts: Int, val waitForAllDownstreams: Boolean,
   // one output might seem counter intuitive but saves us from special handling in other places
   require(outputPorts >= 1, "A Balance must have one or more output ports")
 
-  @deprecated("Use the constructor which also specifies the `eagerCancel` parameter")
+  @Deprecated
+  @deprecated("Use the constructor which also specifies the `eagerCancel` parameter", since = "2.5.12")
   def this(outputPorts: Int, waitForAllDownstreams: Boolean) = this(outputPorts, waitForAllDownstreams, false)
 
   val in: Inlet[T] = Inlet[T]("Balance.in")
