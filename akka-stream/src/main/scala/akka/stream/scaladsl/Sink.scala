4ccc025d56010601a41e6764f3f3473f73451a16
Konrad `ktoso` Malawski <konrad.malawski@project13.pl>
2018-04-23 10:44:49 +0900

    fix trailing space, fixes pr validation (#24945)

--- akka-stream/src/main/scala/akka/stream/scaladsl/Sink.scala
+++ akka-stream/src/main/scala/akka/stream/scaladsl/Sink.scala
@@ -253,7 +253,7 @@ object Sink {
    * A `Sink` that will invoke the given procedure for each received element. The sink is materialized
    * into a [[scala.concurrent.Future]] which will be completed with `Success` when reaching the
    * normal end of the stream, or completed with `Failure` if there is a failure signaled in
-   * the stream. 
+   * the stream.
    */
   def foreach[T](f: T ⇒ Unit): Sink[T, Future[Done]] =
     Flow[T].map(f).toMat(Sink.ignore)(Keep.right).named("foreachSink")
