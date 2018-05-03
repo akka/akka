dfd8d8aa818d3031891c81cb241e7a70e6fbcf1a
Johan Andrén <johan@markatta.com>
2018-05-03 13:24:51 +0200

    Additional tests for async boundaries and GroupBy #24676

--- akka-stream-tests/src/test/scala/akka/stream/scaladsl/FlowGroupBySpec.scala
+++ akka-stream-tests/src/test/scala/akka/stream/scaladsl/FlowGroupBySpec.scala
@@ -6,7 +6,7 @@ package akka.stream.scaladsl
 
 import java.util
 
-import akka.NotUsed
+import akka.{ Done, NotUsed }
 import akka.actor.ActorSystem
 import akka.stream.Attributes._
 import akka.stream.impl.SinkModule
@@ -25,6 +25,8 @@ import org.scalatest.concurrent.PatienceConfiguration.Timeout
 import akka.stream.testkit.scaladsl.TestSource
 import akka.stream.testkit.scaladsl.TestSink
 import java.util.concurrent.ThreadLocalRandom
+
+import akka.testkit.TestLatch
 
 object FlowGroupBySpec {
 
@@ -615,6 +617,35 @@ class FlowGroupBySpec extends StreamSpec {
         }
       }
       upstreamSubscription.sendComplete()
+    }
+
+    "not block all substreams when one is blocked but has a buffer in front" in assertAllStagesStopped {
+      case class Elem(id: Int, substream: Int, f: () ⇒ Any)
+      val queue = Source.queue[Elem](3, OverflowStrategy.backpressure)
+        .groupBy(2, _.substream)
+        .buffer(2, OverflowStrategy.backpressure)
+        .map { _.f() }.async
+        .to(Sink.ignore)
+        .run()
+
+      val threeProcessed = Promise[Done]()
+      val blockSubStream1 = TestLatch()
+      List(
+        Elem(1, 1, () ⇒ {
+          // timeout just to not wait forever if something is wrong, not really relevant for test
+          Await.result(blockSubStream1, 10.seconds)
+          1
+        }),
+        Elem(2, 1, () ⇒ 2),
+        Elem(3, 2, () ⇒ {
+          threeProcessed.success(Done)
+          3
+        })).foreach(queue.offer)
+      // two and three are processed as fast as possible, not blocked by substream 1 being clogged
+      threeProcessed.future.futureValue should ===(Done)
+      // let 1 pass so stream can complete
+      blockSubStream1.open()
+      queue.complete()
     }
 
   }
