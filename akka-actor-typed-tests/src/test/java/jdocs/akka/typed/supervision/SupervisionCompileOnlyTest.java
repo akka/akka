c838f83c06deba484773076eb468c54a81fad4cb
Christopher Batey <christopher.batey@gmail.com>
2018-05-31 13:07:15 +0100

    Clarify typed supervision when returning a new behavior (#25163)

--- akka-actor-typed-tests/src/test/java/jdocs/akka/typed/supervision/SupervisionCompileOnlyTest.java
+++ akka-actor-typed-tests/src/test/java/jdocs/akka/typed/supervision/SupervisionCompileOnlyTest.java
@@ -4,6 +4,7 @@
 
 package jdocs.akka.typed.supervision;
 
+import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.SupervisorStrategy;
 import akka.actor.typed.javadsl.Behaviors;
@@ -12,6 +13,40 @@ import scala.concurrent.duration.FiniteDuration;
 import java.util.concurrent.TimeUnit;
 
 public class SupervisionCompileOnlyTest {
+  //#wrap
+  interface CounterMessage { }
+
+  public static final class Increase implements CounterMessage { }
+
+  public static final class Get implements CounterMessage {
+    final ActorRef<Got> sender;
+
+    public Get(ActorRef<Got> sender) {
+      this.sender = sender;
+    }
+  }
+
+  public static final class Got {
+    final int n;
+
+    public Got(int n) {
+      this.n = n;
+    }
+  }
+
+  public static Behavior<CounterMessage> counter(int currentValue) {
+    return Behaviors.receive(CounterMessage.class)
+      .onMessage(Increase.class, (ctx, o) -> {
+        return counter(currentValue + 1);
+      })
+      .onMessage(Get.class, (ctx, o) -> {
+        o.sender.tell(new Got(currentValue));
+        return Behaviors.same();
+      })
+      .build();
+  }
+  //#wrap
+
   public static Behavior<String> behavior = Behaviors.empty();
 
   public void supervision() {
@@ -28,14 +63,19 @@ public class SupervisionCompileOnlyTest {
     //#restart-limit
     Behaviors.supervise(behavior)
       .onFailure(IllegalStateException.class, SupervisorStrategy.restartWithLimit(
-      10, FiniteDuration.apply(10, TimeUnit.SECONDS)
-    ));
+        10, FiniteDuration.apply(10, TimeUnit.SECONDS)
+      ));
     //#restart-limit
 
     //#multiple
     Behaviors.supervise(Behaviors.supervise(behavior)
       .onFailure(IllegalStateException.class, SupervisorStrategy.restart()))
-    .onFailure(IllegalArgumentException.class, SupervisorStrategy.stop());
+      .onFailure(IllegalArgumentException.class, SupervisorStrategy.stop());
     //#multiple
+
+
+    //#top-level
+    Behaviors.supervise(counter(1));
+    //#top-level
   }
 }
