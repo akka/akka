1d0757cb69bcd4a91460396cb5a0fdd4a4cdb306
Christopher Batey <christopher.batey@gmail.com>
2018-06-25 06:42:37 +0100

    Typed FSM docs page
    Show how the example for untyped FSM can be ported to typed

---
+++ akka-actor-typed-tests/src/test/java/jdocs/akka/typed/FSMDocTest.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright (C) 2018 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package jdocs.akka.typed;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.Behaviors;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class FSMDocTest {
+
+    //#simple-events
+    interface Event { }
+    //#simple-events
+
+    static
+    //#simple-events
+    public final class SetTarget implements Event {
+        private final ActorRef<Batch> ref;
+
+        public SetTarget(ActorRef<Batch> ref) {
+            this.ref = ref;
+        }
+
+        public ActorRef<Batch> getRef() {
+            return ref;
+        }
+    }
+    //#simple-events
+
+    static
+    //#simple-events
+    final class Timeout implements Event {}
+    //#simple-events
+
+    //#simple-events
+    public enum Flush implements Event {
+        Flush
+    }
+    //#simple-events
+
+    static
+    //#simple-events
+    public final class Queue implements Event {
+        private final Object obj;
+
+        public Queue(Object obj) {
+            this.obj = obj;
+        }
+
+        public Object getObj() {
+            return obj;
+        }
+    }
+
+    //#simple-events
+    static public final class Batch {
+        private final List<Object> list;
+
+        public Batch(List<Object> list) {
+            this.list = list;
+        }
+
+        public List<Object> getList() {
+            return list;
+        }
+        //#boilerplate
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Batch batch = (Batch) o;
+
+            return list.equals(batch.list);
+        }
+
+        @Override
+        public int hashCode() {
+            return list.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder builder = new StringBuilder();
+            builder.append("Batch{list=");
+            list.stream().forEachOrdered(e -> {
+                builder.append(e);
+                builder.append(",");
+            });
+            int len = builder.length();
+            builder.replace(len, len, "}");
+            return builder.toString();
+        }
+        //#boilerplate
+    }
+
+    //#storing-state
+    interface Data {
+    }
+    //#storing-state
+
+    static
+    //#storing-state
+    final class Todo implements Data {
+        private final ActorRef<Batch> target;
+        private final List<Object> queue;
+
+        public Todo(ActorRef<Batch> target, List<Object> queue) {
+            this.target = target;
+            this.queue = queue;
+        }
+
+        public ActorRef<Batch> getTarget() {
+            return target;
+        }
+
+        public List<Object> getQueue() {
+            return queue;
+        }
+        //#boilerplate
+
+        @Override
+        public String toString() {
+            return "Todo{" +
+                    "target=" + target +
+                    ", queue=" + queue +
+                    '}';
+        }
+
+        public Todo addElement(Object element) {
+            List<Object> nQueue = new LinkedList<>(queue);
+            nQueue.add(element);
+            return new Todo(this.target, nQueue);
+        }
+
+        public Todo copy(List<Object> queue) {
+            return new Todo(this.target, queue);
+        }
+
+        public Todo copy(ActorRef<Batch> target) {
+            return new Todo(target, this.queue);
+        }
+        //#boilerplate
+    }
+    //#storing-state
+
+    //#simple-state
+    // FSM states represented as behaviors
+    private static Behavior<Event> uninitialized() {
+        return Behaviors.receive(Event.class)
+                .onMessage(SetTarget.class, (ctx, msg) -> idle(new Todo(msg.getRef(), Collections.emptyList())))
+                .build();
+    }
+
+    private static Behavior<Event> idle(Todo data) {
+        return Behaviors.receive(Event.class)
+                .onMessage(Queue.class, (ctx, msg) -> active(data.addElement(msg)))
+                .build();
+    }
+
+    private static Behavior<Event> active(Todo data) {
+        return Behaviors.withTimers(timers -> {
+            // State timeouts done with withTimers
+            timers.startSingleTimer(new Timeout(), new Timeout(), Duration.ofSeconds(1));
+            return Behaviors.receive(Event.class)
+                    .onMessage(Flush.class, (ctx, msg) -> {
+                        data.getTarget().tell(new Batch(data.queue));
+                       return idle(data.copy(new ArrayList<>()));
+                    })
+                    .build();
+        });
+    }
+    //#simple-state
+
+}
---
+++ akka-actor-typed-tests/src/test/scala/docs/akka/typed/FSMDocSpec.scala
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2018 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package docs.akka.typed
+
+import akka.actor.testkit.typed.scaladsl.{ ActorTestKit, TestProbe }
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ ActorRef, Behavior, TypedAkkaSpecWithShutdown }
+
+import scala.collection.immutable
+import scala.concurrent.duration._
+
+object FSMDocSpec {
+
+  // messages and data types
+  //#test-code
+
+  //#simple-events
+  // FSM event becomes the type of the message Actor supports
+  sealed trait Event
+  final case class SetTarget(ref: ActorRef[Batch]) extends Event
+  final case class Queue(obj: Any) extends Event
+  case object Flush extends Event
+  case object Timeout extends Event
+  //#simple-events
+
+  final case class Batch(obj: immutable.Seq[Any])
+
+  //#simple-state
+  // states of the FSM represented as behaviors
+  def idle(data: Data): Behavior[Event] = Behaviors.receiveMessage[Event] { msg: Event ⇒
+    (msg, data) match {
+      case (SetTarget(ref), Uninitialized) ⇒
+        idle(Todo(ref, Vector.empty))
+      case (Queue(obj), t @ Todo(_, v)) ⇒
+        active(t.copy(queue = v :+ obj))
+      case _ ⇒
+        Behaviors.unhandled
+    }
+  }
+
+  def active(data: Todo): Behavior[Event] =
+    Behaviors.withTimers[Event] { timers ⇒
+      // instead of FSM state timeout
+      timers.startSingleTimer(Timeout, Timeout, 1.second)
+      Behaviors.receiveMessagePartial {
+        case Flush | Timeout ⇒
+          data.target ! Batch(data.queue)
+          idle(data.copy(queue = Vector.empty))
+        case Queue(obj) ⇒
+          active(data.copy(queue = data.queue :+ obj))
+      }
+    }
+  //#simple-state
+
+  val initialState = idle(Uninitialized)
+
+  //#storing-state
+  sealed trait Data
+  case object Uninitialized extends Data
+  final case class Todo(target: ActorRef[Batch], queue: immutable.Seq[Any]) extends Data
+  //#storing-state
+
+  //#test-code
+}
+
+class FSMDocSpec extends ActorTestKit with TypedAkkaSpecWithShutdown {
+
+  import FSMDocSpec._
+
+  "FSMDocSpec" must {
+    "work" in {
+      val buncher = spawn(initialState)
+      val probe = TestProbe[Batch]
+      buncher ! SetTarget(probe.ref)
+      buncher ! Queue(42)
+      buncher ! Queue(43)
+      probe.expectMessage(Batch(immutable.Seq(42, 43)))
+      buncher ! Queue(44)
+      buncher ! Flush
+      buncher ! Queue(45)
+      probe.expectMessage(Batch(immutable.Seq(44)))
+      probe.expectMessage(Batch(immutable.Seq(45)))
+
+    }
+  }
+}
