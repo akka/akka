8f8a03d2051784f69f0746750f211b5c7b83401f
Justin Peel <jpeelgithub@gmail.com>
2018-06-27 10:56:07 -0600

    Add a message to the require statement.

--- akka-cluster-sharding/src/main/scala/akka/cluster/sharding/Shard.scala
+++ akka-cluster-sharding/src/main/scala/akka/cluster/sharding/Shard.scala
@@ -457,7 +457,7 @@ private[akka] trait RememberingShard { selfType: Shard ⇒
         actor.tell(payload, snd)
       case None ⇒
         if (state.entities.contains(id)) {
-          require(!messageBuffers.contains(id))
+          require(!messageBuffers.contains(id), "Message buffers contains id.")
           getEntity(id).tell(payload, snd)
         } else {
           //Note; we only do this if remembering, otherwise the buffer is an overhead
