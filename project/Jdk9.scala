36071ebab4293752adc874a87e199a4e0ab5327b
Arnout Engelen <arnouten@bzzt.net>
2018-07-11 12:59:45 +0200

    Fix publishing akka-stream_2.12 with jdk8 and jdk9 classes

--- project/Jdk9.scala
+++ project/Jdk9.scala
@@ -11,19 +11,27 @@ object Jdk9 extends AutoPlugin {
 
   lazy val CompileJdk9 = config("CompileJdk9").extend(Compile)
 
+  def notOnScala211[T](scalaBinaryVersion: String, values: Seq[T]): Seq[T] = scalaBinaryVersion match {
+    case "2.11" => Seq()
+    case _ => values
+  }
+
   val compileJdk9Settings = Seq(
     // following the scala-2.12, scala-sbt-1.0, ... convention
-    unmanagedSourceDirectories := Seq(
+    unmanagedSourceDirectories := notOnScala211(scalaBinaryVersion.value, Seq(
       (Compile / sourceDirectory).value / "scala-jdk-9",
       (Compile / sourceDirectory).value / "java-jdk-9"
-    ),
-    scalacOptions := AkkaBuild.DefaultScalacOptions ++ Seq("-release", "9"),
-    javacOptions := AkkaBuild.DefaultJavacOptions ++ Seq("--release", "9")
+    )),
+    scalacOptions := AkkaBuild.DefaultScalacOptions ++ notOnScala211(scalaBinaryVersion.value, Seq("-release", "9")),
+    javacOptions := AkkaBuild.DefaultJavacOptions ++ notOnScala211(scalaBinaryVersion.value, Seq("--release", "9"))
   )
 
   val compileSettings = Seq(
-    Compile / packageBin / mappings ++=
-      (CompileJdk9 / products).value.flatMap(Path.allSubpaths)
+    // It might have been more 'neat' to add the jdk9 products to the jar via packageBin/mappings, but that doesn't work with the OSGi plugin,
+    // so we add them to the fullClasspath instead.
+    //    Compile / packageBin / mappings
+    //      ++= (CompileJdk9 / products).value.flatMap(Path.allSubpaths),
+    Compile / fullClasspath ++= (CompileJdk9 / exportedProducts).value
   )
 
   override def trigger = noTrigger
