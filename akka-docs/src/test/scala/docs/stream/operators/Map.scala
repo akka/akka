d2f58cbee7891d1d292a4f16f8fb0415f80969f5
Gordon Cheung <gtlcheung@hotmail.com>
2018-07-19 09:15:17 +0200

    Added code example for the stream operator map (#25373)
    * Added code example for the stream operator map

---
+++ akka-docs/src/test/scala/docs/stream/operators/Map.scala
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2018 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package docs.stream.operators
+
+//#imports
+import akka.NotUsed
+import akka.stream.scaladsl._
+
+//#imports
+
+object Map {
+
+  //#map
+  val source: Source[Int, NotUsed] = Source(1 to 10)
+  val mapped: Source[String, NotUsed] = source.map(elem ⇒ elem.toString)
+  //#map
+}
