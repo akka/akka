965832660a6b8dee9f62f0a74ea18c24dd52c8da
Christopher Batey <christopher.batey@gmail.com>
2018-07-31 15:46:49 +0100

    Document async and inetaddress DNS
    Closed #25307

---
+++ akka-docs/src/test/java/jdocs/actor/io/dns/DnsCompileOnlyDocTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2018 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package jdocs.actor.io.dns;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.io.Dns;
+import akka.io.IO;
+import akka.io.dns.DnsProtocol;
+
+import static akka.pattern.PatternsCS.ask;
+import static akka.pattern.PatternsCS.pipe;
+
+import scala.Option;
+
+
+public class DnsCompileOnlyDocTest {
+    public static void example() {
+        ActorSystem system = ActorSystem.create();
+
+        ActorRef actorRef = null;
+        long timeout = 1000;
+
+        //#resolve
+        Option<Dns.Resolved> initial = Dns.get(system).cache().resolve("google.com", system, actorRef);
+        Option<Dns.Resolved> cached = Dns.get(system).cache().cached("google.com");
+        //#resolve
+
+        {
+            //#actor-api-inet-address
+            final ActorRef dnsManager = Dns.get(system).manager();
+            ask(dnsManager, new Dns.Resolve("google.com"), timeout);
+            //#actor-api-inet-address
+
+        }
+
+        {
+            //#actor-api-async
+            final ActorRef dnsManager = Dns.get(system).manager();
+            ask(dnsManager, DnsProtocol.resolve("google.com"), timeout);
+            //#actor-api-async
+        }
+
+        {
+            //#srv
+            final ActorRef dnsManager = Dns.get(system).manager();
+            ask(dnsManager, DnsProtocol.resolve("google.com", DnsProtocol.srvRequestType()), timeout);
+            //#srv
+        }
+
+
+    }
+}
