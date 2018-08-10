e30fd3d530f586c6d4efdedcc9d9a5fdc272a980
Konrad Malawski <konrad.malawski@project13.pl>
2018-08-10 12:32:06 +0900

    =dns amend message for failing in wrong protocol use in dns

--- akka-actor/src/main/scala/akka/io/SimpleDnsManager.scala
+++ akka-actor/src/main/scala/akka/io/SimpleDnsManager.scala
@@ -42,13 +42,12 @@ class SimpleDnsManager(val ext: DnsExt) extends Actor with RequiresMessageQueue[
 
     case m: dns.DnsProtocol.Resolve ⇒
       if (inetDnsEnabled) {
-        log.info(
-          "Message of [akka.io.dns.DnsProtocol.Protocol] received ({}) while inet-address dns was configured. Dropping DNS resolve request." +
-            "Only use [akka.io.dns.DnsProtocol.resolve] to create resolution requests for the Async DNS resolver.",
+        log.error(
+          "Message of [akka.io.dns.DnsProtocol.Protocol] received ({}) while inet-address dns was configured. Dropping DNS resolve request. " +
+            "Only use [akka.io.dns.DnsProtocol] to create resolution requests for the Async DNS resolver (enabled by `akka.io.dns = async-dns`). " +
+            "For the classic (now used) DNS resolver use [akka.io.Dns] messages.",
           Logging.simpleName(m))
-      }
-
-      resolver.forward(m)
+      } else resolver.forward(m)
   }
 
   override def postStop(): Unit = {
