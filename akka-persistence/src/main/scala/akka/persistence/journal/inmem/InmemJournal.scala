4b474560ac126b0c1feb4e1733fce1ba823dd1de
Cyrille Chépélov <cch@transparencyrights.com>
2018-08-29 13:06:15 +0200

    InmemJournal: Support diverting incoming messages

--- akka-persistence/src/main/scala/akka/persistence/journal/inmem/InmemJournal.scala
+++ akka-persistence/src/main/scala/akka/persistence/journal/inmem/InmemJournal.scala
@@ -4,12 +4,34 @@
 
 package akka.persistence.journal.inmem
 
+import akka.actor.ActorRef
+import akka.pattern.ask
+
 import scala.collection.immutable
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
 import akka.persistence.journal.AsyncWriteJournal
 import akka.persistence.PersistentRepr
 import akka.persistence.AtomicWrite
+import akka.util.Timeout
+
+import scala.concurrent.duration._
+
+/**
+  * INTERNAL API — used to implement Persistence Query for testing purposes only.
+ */
+private[persistence] object InmemJournal {
+
+  final case class SetDownstreamReceiver(target: ActorRef)
+  case object RemoveDownstreamReceiver
+
+  case object Ping // replies akka.Done
+  case object SpoolExistingMessages
+
+  final case class SpooledMessages(messages: Iterable[PersistentRepr])
+
+  final case class DeleteSpooledMessages(persistenceId: String, toSequenceNr: Long) // replies akka.Done
+}
 
 /**
  * INTERNAL API.
@@ -17,6 +39,8 @@ import akka.persistence.AtomicWrite
  * In-memory journal for testing purposes only.
  */
 private[persistence] class InmemJournal extends AsyncWriteJournal with InmemMessages {
+  import InmemJournal._
+
   override def asyncWriteMessages(messages: immutable.Seq[AtomicWrite]): Future[immutable.Seq[Try[Unit]]] = {
     for (w ← messages; p ← w.payload)
       add(p)
@@ -36,14 +60,66 @@ private[persistence] class InmemJournal extends AsyncWriteJournal with InmemMess
   }
 
   def asyncDeleteMessagesTo(persistenceId: String, toSequenceNr: Long): Future[Unit] = {
+    implicit def executionContext: ExecutionContext = context.dispatcher
+
     val toSeqNr = math.min(toSequenceNr, highestSequenceNr(persistenceId))
     var snr = 1L
     while (snr <= toSeqNr) {
       delete(persistenceId, snr)
       snr += 1
     }
-    Future.successful(())
+
+    for {
+      downstream <- sendDownstreamDeletion(persistenceId, toSeqNr)
+    } yield {
+      ()
+    }
   }
+
+  /* support for diverting messages to a downstream receiver: */
+
+  private var _downstreamReceiver: Option[ActorRef] = None
+
+  protected def sendDownstream(messages: => Iterable[PersistentRepr]): Unit =
+    _downstreamReceiver.foreach(_ ! SpooledMessages(messages))
+
+  protected def sendDownstreamDeletion(persistenceId: String, toSequenceNr: Long): Future[Unit] = {
+    implicit val timeout: Timeout = Timeout(21474835L, SECONDS) // FIXME
+    implicit def executionContext: ExecutionContext = context.dispatcher
+
+    _downstreamReceiver match {
+      case Some(target) => (target ? DeleteSpooledMessages(persistenceId, toSequenceNr)).map(_ => ())
+      case None => Future.successful()
+    }
+  }
+
+  override def receivePluginInternal: Receive = super.receivePluginInternal orElse {
+    case SetDownstreamReceiver(target) =>
+      _downstreamReceiver = Some(target)
+
+    case RemoveDownstreamReceiver =>
+      _downstreamReceiver = None
+
+    case SpoolExistingMessages =>
+      sendDownstream(messages.values.reduce(_ ++ _).sortBy(_.sequenceNr))
+
+    case Ping =>
+      sender() ! akka.Done // this is just an "ordering" marker as
+  }
+
+  override def add(p: PersistentRepr): Unit = {
+    super.add(p)
+    sendDownstream(p :: Nil)
+  }
+
+  override def update(pid: String, snr: Long)(f: PersistentRepr => PersistentRepr): Unit = {
+    _downstreamReceiver match {
+      case Some(tap) => throw new UnsupportedOperationException("cannot update messages in active tap mode") // smell: Refused Bequest
+      case None => super.update(pid, snr)(f)
+    }
+  }
+
+  override def delete(pid: String, snr: Long): Unit = super.delete(pid, snr)
 }
 
 /**
