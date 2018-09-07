bbb0c00280ac4211281d280ddb5f8b75332d64c1
Dollyg <dollyg@thoughtworks.com>
2018-09-07 11:13:29 +0530

    Add Java API for creating typed ActorMaterializer from ActorContext

--- akka-actor-typed/src/main/scala/akka/actor/typed/javadsl/Adapter.scala
+++ akka-actor-typed/src/main/scala/akka/actor/typed/javadsl/Adapter.scala
@@ -4,6 +4,7 @@
 
 package akka.actor.typed.javadsl
 
+import akka.actor
 import akka.actor.typed.Behavior
 import akka.actor.typed.Props
 import akka.actor.typed.EmptyProps
@@ -58,6 +59,9 @@ object Adapter {
 
   def toUntyped(sys: ActorSystem[_]): akka.actor.ActorSystem =
     sys.toUntyped
+
+  def toUntyped(ctx: ActorContext[_]): actor.ActorContext =
+    ActorContextAdapter.toUntyped(ctx)
 
   def watch[U](ctx: akka.actor.ActorContext, other: ActorRef[U]): Unit =
     ctx.watch(other)
