43b3bfa384b73e85d174c44b4a531f3a0eed1023
Andreas Gies <andreas@wayofquality.de>
2018-09-10 12:38:53 +0200

    Reverting version change

--- project/AkkaBuild.scala
+++ project/AkkaBuild.scala
@@ -28,7 +28,7 @@ object AkkaBuild {
     Formatting.formatSettings ++
     Protobuf.settings ++ Seq(
       parallelExecution in GlobalScope := System.getProperty("akka.parallelExecution", parallelExecutionByDefault.toString).toBoolean,
-      version in ThisBuild := "2.5.16"
+      version in ThisBuild := "2.5-SNAPSHOT"
     )
 
   lazy val mayChangeSettings = Seq(
