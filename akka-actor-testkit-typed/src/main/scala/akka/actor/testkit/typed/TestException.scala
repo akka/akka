debe12290b991055a8cda507fe758c63256d419f
Patrik Nordwall <patrik.nordwall@gmail.com>
2018-09-13 12:11:04 +0200

    doc TestException

--- akka-actor-testkit-typed/src/main/scala/akka/actor/testkit/typed/TestException.scala
+++ akka-actor-testkit-typed/src/main/scala/akka/actor/testkit/typed/TestException.scala
@@ -6,5 +6,8 @@ package akka.actor.testkit.typed
 
 import scala.util.control.NoStackTrace
 
+/**
+ * A predefined exception that can be used in tests. It doesn't include a stack trace.
+ */
 class TestException(msg: String) extends RuntimeException(msg) with NoStackTrace
 
