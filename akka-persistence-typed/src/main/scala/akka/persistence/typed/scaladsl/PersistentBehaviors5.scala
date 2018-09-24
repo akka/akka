cad6e4cca7356170cc20b61fc65dac8549fe529f
Patrik Nordwall <patrik.nordwall@gmail.com>
2018-09-24 15:24:55 +0200

    AccountExample5, back to basics

---
+++ akka-persistence-typed/src/main/scala/akka/persistence/typed/scaladsl/PersistentBehaviors5.scala
@@ -0,0 +1,130 @@
+/**
+ * Copyright (C) 2017-2018 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.persistence.typed.scaladsl
+
+import scala.util.Try
+
+import akka.Done
+import akka.actor.typed.BackoffSupervisorStrategy
+import akka.actor.typed.Behavior.DeferredBehavior
+import akka.persistence._
+import akka.persistence.typed.EventAdapter
+
+/*
+API experiment with factory for command and event handler
+- no ActorContext, passed via `Behaviors.setup` (if needed)
+- nothing special, using defs for different command handlers
+*/
+
+object PersistentBehaviors5 {
+
+
+  /**
+   * Type alias for the command handler function that defines how to act on commands.
+   *
+   * The type alias is not used in API signatures because it's easier to see (in IDE) what is needed
+   * when full function type is used. When defining the handler as a separate function value it can
+   * be useful to use the alias for shorter type signature.
+   */
+  type CommandHandler[Command, Event, State] = (State, Command) ⇒ Effect[Event, State]
+
+  /**
+   * Type alias for the command handler function for a subclass of the `State` that defines how to act on commands.
+   *
+   * The type alias is not used in API signatures because it's easier to see (in IDE) what is needed
+   * when full function type is used. When defining the handler as a separate function value it can
+   * be useful to use the alias for shorter type signature.
+   */
+  type SubStateCommandHandler[Command, Event, State, S <: State] = (S, Command) ⇒ Effect[Event, State]
+
+  /**
+   * Type alias for the event handler function for updating the state based on events having been persisted.
+   *
+   * The type alias is not used in API signatures because it's easier to see (in IDE) what is needed
+   * when full function type is used. When defining the handler as a separate function value it can
+   * be useful to use the alias for shorter type signature.
+   */
+  type EventHandler[State, Event] = (State, Event) ⇒ State
+
+  /**
+   * Create a `Behavior` for a persistent actor.
+   */
+  def receive[Command, Event, State](
+    persistenceId:  String,
+    emptyState:     State,
+    commandHandler: (State, Command) ⇒ Effect[Event, State],
+    eventHandler:   (State, Event) ⇒ State): PersistentBehavior4[Command, Event, State] = ???
+
+}
+
+trait PersistentBehavior5[Command, Event, State] extends DeferredBehavior[Command] {
+  /**
+   * The `callback` function is called to notify the actor that the recovery process
+   * is finished.
+   */
+  def onRecoveryCompleted(callback: State ⇒ Unit): PersistentBehavior4[Command, Event, State]
+
+  /**
+   * The `callback` function is called to notify when a snapshot is complete.
+   */
+  def onSnapshot(callback: (SnapshotMetadata, Try[Done]) ⇒ Unit): PersistentBehavior4[Command, Event, State]
+
+  /**
+   * Initiates a snapshot if the given function returns true.
+   * When persisting multiple events at once the snapshot is triggered after all the events have
+   * been persisted.
+   *
+   * `predicate` receives the State, Event and the sequenceNr used for the Event
+   */
+  def snapshotWhen(predicate: (State, Event, Long) ⇒ Boolean): PersistentBehavior4[Command, Event, State]
+  /**
+   * Snapshot every N events
+   *
+   * `numberOfEvents` should be greater than 0
+   */
+  def snapshotEvery(numberOfEvents: Long): PersistentBehavior4[Command, Event, State]
+
+  /**
+   * Change the journal plugin id that this actor should use.
+   */
+  def withJournalPluginId(id: String): PersistentBehavior4[Command, Event, State]
+
+  /**
+   * Change the snapshot store plugin id that this actor should use.
+   */
+  def withSnapshotPluginId(id: String): PersistentBehavior4[Command, Event, State]
+
+  /**
+   * Changes the snapshot selection criteria used by this behavior.
+   * By default the most recent snapshot is used, and the remaining state updates are recovered by replaying events
+   * from the sequence number up until which the snapshot reached.
+   *
+   * You may configure the behavior to skip replaying snapshots completely, in which case the recovery will be
+   * performed by replaying all events -- which may take a long time.
+   */
+  def withSnapshotSelectionCriteria(selection: SnapshotSelectionCriteria): PersistentBehavior4[Command, Event, State]
+
+  /**
+   * The `tagger` function should give event tags, which will be used in persistence query
+   */
+  def withTagger(tagger: Event ⇒ Set[String]): PersistentBehavior4[Command, Event, State]
+
+  /**
+   * Transform the event in another type before giving to the journal. Can be used to wrap events
+   * in types Journals understand but is of a different type than `Event`.
+   */
+  def eventAdapter(adapter: EventAdapter[Event, _]): PersistentBehavior4[Command, Event, State]
+
+  /**
+   * Back off strategy for persist failures.
+   *
+   * Specifically BackOff to prevent resume being used. Resume is not allowed as
+   * it will be unknown if the event has been persisted.
+   *
+   * If not specified the actor will be stopped on failure.
+   */
+  def onPersistFailure(backoffStrategy: BackoffSupervisorStrategy): PersistentBehavior4[Command, Event, State]
+}
+
---
+++ akka-persistence-typed/src/test/scala/docs/akka/persistence/typed/AccountExample5.scala
@@ -0,0 +1,108 @@
+/**
+ * Copyright (C) 2017-2018 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package docs.akka.persistence.typed
+
+import akka.actor.typed.Behavior
+import akka.persistence.typed.scaladsl.Effect
+import akka.persistence.typed.scaladsl.PersistentBehaviors5
+import akka.persistence.typed.scaladsl.PersistentBehaviors5.CommandHandler
+import akka.persistence.typed.scaladsl.PersistentBehaviors5.EventHandler
+
+object AccountExample5 {
+
+  object AccountEntity {
+    sealed trait AccountCommand
+    case object CreateAccount extends AccountCommand
+    case class Deposit(amount: Double) extends AccountCommand
+    case class Withdraw(amount: Double) extends AccountCommand
+    case object CloseAccount extends AccountCommand
+
+    sealed trait AccountEvent
+    case object AccountCreated extends AccountEvent
+    case class Deposited(amount: Double) extends AccountEvent
+    case class Withdrawn(amount: Double) extends AccountEvent
+    case object AccountClosed extends AccountEvent
+
+    sealed trait Account {
+      def applyEvent(event: AccountEvent): Account
+    }
+    case object EmptyAccount extends Account {
+      override def applyEvent(event: AccountEvent): Account = event match {
+        case AccountCreated ⇒ OpenedAccount(0.0)
+        case _              ⇒ throw new IllegalStateException(s"unexpected event [$event] in state [EmptyAccount]")
+      }
+    }
+    case class OpenedAccount(balance: Double) extends Account {
+      override def applyEvent(event: AccountEvent): Account = event match {
+        case Deposited(amount) ⇒ copy(balance = balance + amount)
+        case Withdrawn(amount) ⇒ copy(balance = balance - amount)
+        case AccountClosed     ⇒ ClosedAccount
+        case _                 ⇒ throw new IllegalStateException(s"unexpected event [$event] in state [OpenedAccount]")
+      }
+    }
+    case object ClosedAccount extends Account {
+      override def applyEvent(event: AccountEvent): Account =
+        throw new IllegalStateException(s"unexpected event [$event] in state [ClosedAccount]")
+    }
+
+    private def initialHandler(cmd: AccountCommand): Effect[AccountEvent, Account] =
+      cmd match {
+        case CreateAccount ⇒ Effect.persist(AccountCreated)
+        case _             ⇒ Effect.unhandled
+      }
+
+    private def openedAccountHandler(acc: OpenedAccount, cmd: AccountCommand): Effect[AccountEvent, Account]= {
+        cmd match {
+          case Deposit(amount) ⇒ Effect.persist(Deposited(amount))
+
+          case Withdraw(amount) ⇒
+            if ((acc.balance - amount) < 0.0)
+              Effect.unhandled // TODO replies are missing in this example
+            else {
+              Effect
+                .persist(Withdrawn(amount))
+                .thenRun {
+                  case OpenedAccount(balance) ⇒
+                    // do some side-effect using balance
+                    println(balance)
+                  case _ ⇒ throw new IllegalStateException
+                }
+            }
+          case CloseAccount if acc.balance == 0.0 ⇒
+            Effect.persist(AccountClosed)
+
+          case CloseAccount ⇒
+            Effect.unhandled
+        }
+    }
+
+    private def closedHandler(cmd: AccountCommand): Effect[AccountEvent, Account] =
+      Effect.unhandled
+
+    private val commandHandler: CommandHandler[AccountCommand, AccountEvent, Account] = {
+      (state, cmd) ⇒
+        state match {
+          case EmptyAccount              ⇒ initialHandler(cmd)
+          case opened @ OpenedAccount(_) ⇒ openedAccountHandler(opened, cmd)
+          case ClosedAccount             ⇒ closedHandler(cmd)
+        }
+    }
+
+    private val eventHandler: EventHandler[Account, AccountEvent] = {
+      (state, event) ⇒ state.applyEvent(event)
+    }
+
+    def behavior(accountNumber: String): Behavior[AccountEntity.AccountCommand] = {
+      PersistentBehaviors5.receive(
+        accountNumber,
+        EmptyAccount,
+        commandHandler,
+        eventHandler
+      )
+    }
+  }
+
+}
+
