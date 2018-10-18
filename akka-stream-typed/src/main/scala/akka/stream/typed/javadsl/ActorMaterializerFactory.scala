e26a90f340120fd35a02a0ca023e5dbfed319b2b
Dolly Gyanchandani <10329479+Dollyg@users.noreply.github.com>
2018-10-18 18:33:27 +0530

    Create typed ActorMaterializer from ActorContext, ##25536
    * Create typed ActorMaterializer from ActorContext to bind stream's lifecycle with an actor's lifecycle
    
    * Add Java API for creating typed ActorMaterializer from ActorContext

--- akka-stream-typed/src/main/scala/akka/stream/typed/javadsl/ActorMaterializerFactory.scala
+++ akka-stream-typed/src/main/scala/akka/stream/typed/javadsl/ActorMaterializerFactory.scala
@@ -5,6 +5,7 @@
 package akka.stream.typed.javadsl
 
 import akka.actor.typed.ActorSystem
+import akka.actor.typed.javadsl.{ ActorContext, Adapter }
 import akka.stream.ActorMaterializerSettings
 
 object ActorMaterializerFactory {
@@ -43,4 +44,34 @@ object ActorMaterializerFactory {
   def create[T](settings: ActorMaterializerSettings, namePrefix: String, actorSystem: ActorSystem[T]): akka.stream.ActorMaterializer =
     akka.stream.ActorMaterializer.create(settings, actorSystem.toUntyped, namePrefix)
 
+  /**
+   * Creates an `ActorMaterializer` which will execute every step of a transformation
+   * pipeline within its own [[akka.actor.Actor]]. The lifecycle of the materialized streams
+   * will be bound to the lifecycle of the provided [[akka.actor.typed.javadsl.ActorContext]]
+   *
+   * Defaults the actor name prefix used to name actors running the processing steps to `"flow"`.
+   * The actor names are built up of `namePrefix-flowNumber-flowStepNumber-stepName`.
+   */
+  def create[T](ctx: ActorContext[T]): akka.stream.ActorMaterializer =
+    akka.stream.ActorMaterializer.create(Adapter.toUntyped(ctx))
+
+  /**
+   * Creates an `ActorMaterializer` which will execute every step of a transformation
+   * pipeline within its own [[akka.actor.Actor]]. The lifecycle of the materialized streams
+   * will be bound to the lifecycle of the provided [[akka.actor.typed.javadsl.ActorContext]]
+   */
+  def create[T](settings: ActorMaterializerSettings, ctx: ActorContext[T]): akka.stream.ActorMaterializer =
+    akka.stream.ActorMaterializer.create(settings, Adapter.toUntyped(ctx))
+
+  /**
+   * Creates an `ActorMaterializer` which will execute every step of a transformation
+   * pipeline within its own [[akka.actor.Actor]]. The lifecycle of the materialized streams
+   * will be bound to the lifecycle of the provided [[akka.actor.typed.javadsl.ActorContext]]
+   *
+   * The `namePrefix` is used as the first part of the names of the actors running
+   * the processing steps. The default `namePrefix` is `"flow"`. The actor names are built up of
+   * `namePrefix-flowNumber-flowStepNumber-stepName`.
+   */
+  def create[T](settings: ActorMaterializerSettings, namePrefix: String, ctx: ActorContext[T]): akka.stream.ActorMaterializer =
+    akka.stream.ActorMaterializer.create(settings, Adapter.toUntyped(ctx), namePrefix)
 }
