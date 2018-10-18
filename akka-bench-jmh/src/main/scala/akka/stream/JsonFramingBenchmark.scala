df6a68a73c6f13c79f56edc574daa0b8c5f15b43
Cyrille Chépélov <cch@transparencyrights.com>
2018-10-18 11:42:11 +0200

    New benchmark: what if we receive objects by chunks of 5 bytes?

--- akka-bench-jmh/src/main/scala/akka/stream/JsonFramingBenchmark.scala
+++ akka-bench-jmh/src/main/scala/akka/stream/JsonFramingBenchmark.scala
@@ -29,6 +29,8 @@ class JsonFramingBenchmark {
          |{"fname":"Hank","name":"Smith","age":42,"id":1337,"boardMember":false}""".stripMargin
     )
 
+  val json5by5 = for (i ← 0.until(json5.length, 5)) yield json5.slice(i, i + 5).compact
+
   val jsonLong =
     ByteString(
       s"""{"fname":"Frank","name":"Smith","age":42,"id":1337,"boardMember":false,"description":"${"a" * 1000000}"}"""
@@ -51,6 +53,34 @@ class JsonFramingBenchmark {
     bracket.poll().get
     bracket.poll().get
     bracket.poll().get
+  }
+
+  @Benchmark
+  @OperationsPerInvocation(25)
+  def counting_offer_5x5: ByteString = {
+    /* this test validates that we don't create too much internal buffer misalignment-caused loss of performance.
+     */
+
+    for (i ← 0 until 5) {
+      bracket.offer(json5)
+    }
+
+    for (i ← 0 until 24) {
+      bracket.poll().get
+    }
+    bracket.poll().get // 25
+  }
+
+  @Benchmark
+  @OperationsPerInvocation(5)
+  def counting_offer_5by5bytes: ByteString = {
+    /* this test validates how we JsonObjectParser when the incoming data does not come by object boundaries */
+    json5by5.foreach(bracket.offer)
+
+    for (i ← 0 until 4) {
+      bracket.poll().get
+    }
+    bracket.poll().get // 5
   }
 
   @Benchmark
