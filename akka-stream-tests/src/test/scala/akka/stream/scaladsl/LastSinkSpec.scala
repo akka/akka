09843e6baef6d57cb6608cad21e544941da0ea00
mohammad mehdi <mehdimousavi1995@users.noreply.github.com>
2018-10-25 17:10:36 +0330

    example for some sink operators #25468
    Examples for takeLast, last and lastOption operators

--- akka-stream-tests/src/test/scala/akka/stream/scaladsl/LastSinkSpec.scala
+++ akka-stream-tests/src/test/scala/akka/stream/scaladsl/LastSinkSpec.scala
@@ -4,25 +4,30 @@
 
 package akka.stream.scaladsl
 
-import scala.concurrent.Await
-import scala.concurrent.duration._
-
-import akka.stream.ActorMaterializer
-import akka.stream.ActorMaterializerSettings
+import akka.stream.{ ActorMaterializer, ActorMaterializerSettings }
 import akka.stream.testkit._
-import akka.stream.testkit.Utils._
 import akka.stream.testkit.scaladsl.StreamTestKit._
+
+import scala.concurrent.duration._
+import scala.concurrent.{ Await, Future }
 
 class LastSinkSpec extends StreamSpec with ScriptedTest {
 
   val settings = ActorMaterializerSettings(system)
 
-  implicit val materializer = ActorMaterializer(settings)
+  implicit val materializer: ActorMaterializer = ActorMaterializer(settings)
+  implicit val ec = system.dispatcher
 
   "A Flow with Sink.last" must {
 
-    "yield the last value" in assertAllStagesStopped {
-      Await.result(Source(1 to 42).map(identity).runWith(Sink.last), 1.second) should be(42)
+    "yield the last value" in {
+      //#last-operator-example
+      val source = Source(1 to 10)
+      val result: Future[Int] = source.runWith(Sink.last)
+      result.map(println)
+      // 10
+      //#last-operator-example
+      result.futureValue shouldEqual 10
     }
 
     "yield the first error" in assertAllStagesStopped {
@@ -52,8 +57,14 @@ class LastSinkSpec extends StreamSpec with ScriptedTest {
       } should be theSameInstanceAs (ex)
     }
 
-    "yield None for empty stream" in assertAllStagesStopped {
-      Await.result(Source.empty[Int].runWith(Sink.lastOption), 1.second) should be(None)
+    "yield None for empty stream" in {
+      //#lastOption-operator-example
+      val source = Source.empty[Int]
+      val result: Future[Option[Int]] = source.runWith(Sink.lastOption)
+      result.map(println)
+      // None
+      //#lastOption-operator-example
+      result.futureValue shouldEqual None
     }
 
   }
