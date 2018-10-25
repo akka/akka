8435f97193d6f2d1cebbb24261b662ade5dbe5f2
Sebastian Gavril <sgavril@wehkamp.nl>
2018-10-25 23:10:09 +0200

    Add a backoff and callback to the retry function

--- akka-actor-tests/src/test/scala/akka/pattern/RetrySpec.scala
+++ akka-actor-tests/src/test/scala/akka/pattern/RetrySpec.scala
@@ -7,12 +7,20 @@ package akka.pattern
 import akka.actor.Scheduler
 
 import language.postfixOps
-import akka.testkit.AkkaSpec
+import scala.collection.mutable.ListBuffer
 
-import scala.concurrent.{ Await, ExecutionContextExecutor, Future }
+import akka.testkit.AkkaSpec
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time.Span
+import scala.concurrent.{ Await, Future, ExecutionContextExecutor }
 import scala.concurrent.duration._
 
-class RetrySpec extends AkkaSpec with RetrySupport {
+class RetrySpec extends AkkaSpec with RetrySupport with Eventually {
+
+  override implicit val patience: PatienceConfig = PatienceConfig(
+    1 second,
+    Span(10, org.scalatest.time.Millis))
+
   implicit val ec: ExecutionContextExecutor = system.dispatcher
   implicit val scheduler: Scheduler = system.scheduler
 
@@ -80,6 +88,64 @@ class RetrySpec extends AkkaSpec with RetrySupport {
 
       within(3 seconds) {
         intercept[IllegalStateException] { Await.result(retried, remaining) }.getMessage should ===("6")
+      }
+    }
+
+    "return the result after retrying with a backoff" in {
+      @volatile var failCount = 0
+
+      def attempt() = {
+        failCount += 1
+        Future.failed(new IllegalStateException(failCount.toString))
+      }
+
+      retry(
+        attempt,
+        3,
+        100 milliseconds,
+        2
+      )
+
+      within(100 milliseconds, 3 second) {
+        eventually {
+          failCount should ===(2)
+        }
+      }
+
+      within(200 milliseconds, 3 second) {
+        eventually {
+          failCount should ===(3)
+        }
+      }
+
+      within(400 milliseconds, 3 second) {
+        eventually {
+          failCount should ===(4)
+        }
+      }
+    }
+
+    "execute the provided function during every retry" in {
+      val s = new ListBuffer[String]()
+      val attempts = 3
+      val delay = 100 milliseconds
+      val backoff = 2
+
+      retryF(
+        () ⇒ Future.failed(new Exception("oups")),
+        attempts,
+        delay,
+        backoff
+      ) {
+          case (t, currentRetry) ⇒
+            s.append(s"${t.getMessage}, will retry in ${delay * math.pow(backoff, attempts - currentRetry + 1)}")
+        }
+
+      eventually {
+        s.mkString(System.lineSeparator) should ===(
+          """|oups, will retry in 200 milliseconds
+             |oups, will retry in 400 milliseconds
+             |oups, will retry in 800 milliseconds""".stripMargin)
       }
     }
   }
--- akka-actor/src/main/scala/akka/pattern/RetrySupport.scala
+++ akka-actor/src/main/scala/akka/pattern/RetrySupport.scala
@@ -30,20 +30,74 @@ trait RetrySupport {
    * private val sendReceiveRetry: HttpRequest => Future[HttpResponse] = (req: HttpRequest) => retry[HttpResponse](
    *   attempt = () => sendAndReceive(req),
    *   attempts = 10,
-   *   delay = 2.seconds
+   *   delay = 2 seconds)(
+   *    ec = context.system
+   *    scheduler = context.system.scheduler
    * )
    * }}}
    */
-  def retry[T](attempt: () => Future[T], attempts: Int, delay: FiniteDuration)(
-      implicit ec: ExecutionContext,
-      scheduler: Scheduler): Future[T] = {
+  def retry[T](attempt: () => Future[T], attempts: Int, delay: FiniteDuration)(implicit ec: ExecutionContext, scheduler: Scheduler): Future[T] =
+    retryF(attempt, attempts, delay, 1)((_, _) => ())
+
+  /**
+   * Given a function from Unit to Future, returns an internally retrying Future
+   * The first attempt will be made immediately, each subsequent attempt will be made after 'delay' * 'backoff'
+   * A scheduler (eg context.system.scheduler) must be provided to delay each retry
+   * If attempts are exhausted the returned future is simply the result of invoking attempt.
+   * Note that the attempt function will be invoked on the given execution context for subsequent
+   * tries and therefore must be thread safe (not touch unsafe mutable state).
+   *
+   * <b>Example usage:</b>
+   *
+   * {{{
+   * protected val sendAndReceive: HttpRequest => Future[HttpResponse]
+   * private val sendReceiveRetry: HttpRequest => Future[HttpResponse] = (req: HttpRequest) => retry[HttpResponse](
+   *   attempt = () => sendAndReceive(req),
+   *   attempts = 10,
+   *   delay = 2 seconds,
+   *   backoff = 2)(
+   *    ec = context.system
+   *    scheduler = context.system.scheduler
+   * )
+   * }}}
+   */
+  def retry[T](attempt: () => Future[T], attempts: Int, delay: FiniteDuration, backoff: Int = 1)(implicit ec: ExecutionContext, scheduler: Scheduler): Future[T] =
+    retryF(attempt, attempts, delay, backoff)((_, _) => ())
+
+  /**
+   * Given a function from Unit to Future, returns an internally retrying Future
+   * The first attempt will be made immediately, each subsequent attempt will be made after 'delay' * 'backoff'
+   * The callback function will be executed before each retry.
+   * A scheduler (eg context.system.scheduler) must be provided to delay each retry
+   * If attempts are exhausted the returned future is simply the result of invoking attempt.
+   * Note that the attempt and callback functions will be invoked on the given execution context for subsequent
+   * tries and therefore must be thread safe (not touch unsafe mutable state).
+   *
+   * <b>Example usage:</b>
+   *
+   * {{{
+   * protected val sendAndReceive: HttpRequest => Future[HttpResponse]
+   * private val sendReceiveRetry: HttpRequest => Future[HttpResponse] = (req: HttpRequest) => retry[HttpResponse](
+   *   attempt = () => sendAndReceive(req),
+   *   attempts = 10,
+   *   delay = 2 seconds,
+   *   backoff = 2) { case (t, currentRetry) =>
+   *     log.warning(
+   *       s"""
+   *         |Operation failed due to ${t.getMessage}.
+   *         |Will retry $currentRetry times, after ${delay * math.pow(backoff, attempts - currentRetry + 1)}.
+   *       """.stripMargin)
+   * )
+   * }}}
+   */
+  def retryF[T](attempt: () => Future[T], attempts: Int, delay: FiniteDuration, backoff: Int)(f: (Throwable, Int) => Unit)(implicit ec: ExecutionContext, scheduler: Scheduler): Future[T] = {
     try {
-      if (attempts > 0) {
-        attempt().recoverWith {
-          case NonFatal(_) =>
-            after(delay, scheduler) {
-              retry(attempt, attempts - 1, delay)
-            }
+      if (attempts > 0 && backoff > 0) {
+        attempt() recoverWith {
+          case NonFatal(t) => after(delay, scheduler) {
+            f(t, attempts)
+            retryF(attempt, attempts - 1, delay * backoff, backoff)(f)
+          }
         }
       } else {
         attempt()
@@ -53,5 +107,5 @@ trait RetrySupport {
     }
   }
 }
-
+ 
 object RetrySupport extends RetrySupport
