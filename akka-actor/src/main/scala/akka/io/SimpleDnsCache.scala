dc931a89eb6530169b1cf758f54f45931b98d4a3
Ignasi Marimon-Clos <ignasi@lightbend.com>
2018-11-06 17:35:39 +0100

    Makes code internal. Improves usage of Duration API

--- akka-actor/src/main/scala/akka/io/SimpleDnsCache.scala
+++ akka-actor/src/main/scala/akka/io/SimpleDnsCache.scala
@@ -117,21 +117,41 @@ object SimpleDnsCache {
   }
 }
 
+/**
+  * INTERNAL API
+  */
+@InternalApi
 sealed trait CachePolicy
+/**
+  * INTERNAL API
+  */
+@InternalApi
 case object Never extends CachePolicy
+/**
+  * INTERNAL API
+  */
+@InternalApi
 case object Forever extends CachePolicy
+/**
+  * INTERNAL API
+  */
+@InternalApi
 case class Ttl(value: FiniteDuration) extends CachePolicy {
-  require(value.toSeconds > 0)
+  require(value > 0)
   import akka.util.JavaDurationConverters._
   def getValue: java.time.Duration = value.asJava
 }
+/**
+  * INTERNAL API
+  */
+@InternalApi
 object Ttl {
   // There's places where only a Ttl makes sense (DNS RFC says TTL is a positive 32 but integer)
   // but we know the value can be cached effectively forever (e.g. the Lookup name was the actual IP already)
   val effectivelyForever: Ttl = Ttl(Int.MaxValue.seconds)
 
   implicit object TtlIsOrdered extends Ordering[Ttl] {
-    def compare(a: Ttl, b: Ttl) = a.value.toSeconds compare b.value.toSeconds
+    def compare(a: Ttl, b: Ttl) = a.value.compare(b.value)
   }
 }
 
