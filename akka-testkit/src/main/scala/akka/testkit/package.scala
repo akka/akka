0cc8e20a2ef43ed1a5efd2f4eb17390c830ed4db
Arnout Engelen <arnouten@bzzt.net>
2018-11-21 15:00:29 +0100

    Various scala-2.13.0-M5 fixes
    Clearer imports in TcpDnsClient
    
    Accept SVG's when checking if diagrams were generated
    
    Fix akka-actor MiMa issues
    
    akka-testkit scalafix changes
    
    Update scalatest to 2.13-released snapshot
    
    akka-stream scalafix changes
    
    Update ssl-config
    
    To version that's released with -M5
    
    Take elements directly out of buffer
    
    Manually fix immutable.Seq / Doc link
    
    Make sure the right Concat, Sink is imported
    
    akka-stream binary compat
    
    akka-stream-testkit scalafix
    
    akka-actor-tests scalafix
    
    Eta-expensions

--- akka-testkit/src/main/scala/akka/testkit/package.scala
+++ akka-testkit/src/main/scala/akka/testkit/package.scala
@@ -9,12 +9,13 @@ import scala.concurrent.duration.{ Duration, FiniteDuration }
 import scala.reflect.ClassTag
 import scala.collection.immutable
 import java.util.concurrent.TimeUnit.MILLISECONDS
+import scala.collection.compat._
 
 package object testkit {
   def filterEvents[T](eventFilters: Iterable[EventFilter])(block: ⇒ T)(implicit system: ActorSystem): T = {
     def now = System.currentTimeMillis
 
-    system.eventStream.publish(TestEvent.Mute(eventFilters.to[immutable.Seq]))
+    system.eventStream.publish(TestEvent.Mute(eventFilters.to(immutable.Seq)))
 
     try {
       val result = block
@@ -27,7 +28,7 @@ package object testkit {
 
       result
     } finally {
-      system.eventStream.publish(TestEvent.UnMute(eventFilters.to[immutable.Seq]))
+      system.eventStream.publish(TestEvent.UnMute(eventFilters.to(immutable.Seq)))
     }
   }
 
