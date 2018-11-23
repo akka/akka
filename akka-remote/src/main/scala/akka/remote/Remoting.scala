146205dc091df8fb7e0049b21e466319478bc8d1
Arnout Engelen <arnouten@bzzt.net>
2018-11-23 13:44:53 +0100

    Fix shutdownAll compile error

--- akka-remote/src/main/scala/akka/remote/Remoting.scala
+++ akka-remote/src/main/scala/akka/remote/Remoting.scala
@@ -717,9 +717,11 @@ private[remote] class EndpointManager(conf: Config, log: LoggingAdapter) extends
       // Shutdown all endpoints and signal to sender() when ready (and whether all endpoints were shut down gracefully)
 
       def shutdownAll[T](resources: IterableOnce[T])(shutdown: T ⇒ Future[Boolean]): Future[Boolean] = {
-        (Future sequence resources.map(shutdown)) map { _.forall(identity) } recover {
-          case NonFatal(_) ⇒ false
-        }
+        Future.sequence(resources.toList.map(shutdown))
+          .map(_.forall(identity))
+          .recover {
+            case NonFatal(_) ⇒ false
+          }
       }
 
       (for {
