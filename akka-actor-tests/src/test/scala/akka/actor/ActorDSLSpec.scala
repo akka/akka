daa6903291d7631f34c8768bb93978ea9d683c94
jczuchnowski <jakub.czuchnowski@gmail.com>
2018-12-02 20:01:58 +0100

    Add missing import to the Inbox example

--- akka-actor-tests/src/test/scala/akka/actor/ActorDSLSpec.scala
+++ akka-actor-tests/src/test/scala/akka/actor/ActorDSLSpec.scala
@@ -35,6 +35,8 @@ class ActorDSLSpec extends AkkaSpec {
 
     "function as implicit sender" in {
       //#inbox
+      import akka.actor.ActorDSL._
+      
       implicit val i = inbox()
       echo ! "hello"
       i.receive() should ===("hello")
