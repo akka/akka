dfae0cbff9a72c91368c45b1032728dd4759e8b4
Michael Kholodov <michkhol@users.noreply.github.com>
2018-12-21 05:37:34 -0500

    Akka-typed-testkit: Provides a way of stopping actors #24165
    * Added ActorTestKit#stop method and corresponding tests.
    * Added 'Stopping actors' section to the docs
    * Stop actor now waits until the actor terminates.

--- akka-actor-testkit-typed/src/main/scala/akka/actor/testkit/typed/scaladsl/ActorTestKit.scala
+++ akka-actor-testkit-typed/src/main/scala/akka/actor/testkit/typed/scaladsl/ActorTestKit.scala
@@ -4,6 +4,8 @@
 
 package akka.actor.testkit.typed.scaladsl
 
+import java.util.concurrent.TimeoutException
+
 import akka.actor.typed.scaladsl.AskPattern._
 import akka.actor.typed.{ ActorRef, ActorSystem, Behavior, Props }
 import akka.annotation.{ ApiMayChange, InternalApi }
@@ -172,6 +174,16 @@ final class ActorTestKit private[akka] (val name: String, val config: Config, se
    */
   def spawn[T](behavior: Behavior[T], name: String, props: Props): ActorRef[T] =
     Await.result(internalSystem ? (ActorTestKitGuardian.SpawnActor(name, behavior, _, props)), timeout.duration)
+
+  /**
+   * Stop the actor under test and wait until it terminates.
+   */
+  def stop[T](ref: ActorRef[T], max: FiniteDuration = timeout.duration): Unit = try {
+    Await.result(internalSystem ? { x: ActorRef[ActorTestKitGuardian.Ack.type] ⇒ ActorTestKitGuardian.StopActor(ref, x) }, max)
+  } catch {
+    case _: TimeoutException ⇒
+      assert(false, s"timeout ($max) during stop() waiting for actor [${ref.path}] to stop")
+  }
 
   /**
    * Shortcut for creating a new test probe for the testkit actor system
--- akka-docs/src/main/paradox/typed/testing.md
+++ akka-docs/src/main/paradox/typed/testing.md
@@ -202,6 +202,16 @@ Java
 Note that you can add `import testKit._` to get access to the `spawn` and `createTestProbe` methods at the top level
 without prefixing them with `testKit`.
 
+#### Stopping actors
+The method will wait until the actor stops or throw an assertion error in case of a timeout.
+
+Scala
+:  @@snip [AsyncTestingExampleSpec.scala](/akka-actor-testkit-typed/src/test/scala/docs/akka/actor/testkit/typed/scaladsl/AsyncTestingExampleSpec.scala) { #test-stop-actors }
+
+Java
+:  @@snip [AsyncTestingExampleTest.java](/akka-actor-testkit-typed/src/test/java/jdocs/akka/actor/testkit/typed/javadsl/AsyncTestingExampleTest.java) { #test-stop-actors }
+
+
 ### Test framework integration
 
 @@@ div { .group-java }
