997bdd45ce5b8efed0e768b319db59a1766ef42a
hepin1989 <hepin1989@gmail.com>
2019-01-01 09:19:19 +0800

    Add CopyrightHeader support for `*.proto` files.

---
+++ project/CopyrightHeaderForProtobuf.scala
@@ -0,0 +1,21 @@
+import akka.CopyrightHeader
+import de.heikoseeberger.sbtheader.HeaderPlugin.autoImport.{HeaderFileType, headerMappings, headerSources}
+import sbt.Keys.sourceDirectory
+import sbt.{Compile, Def, Test, inConfig, _}
+
+object CopyrightHeaderForProtobuf extends CopyrightHeader {
+  override protected def headerMappingSettings: Seq[Def.Setting[_]] = {
+    super.headerMappingSettings
+    Seq(Compile, Test).flatMap { config =>
+      inConfig(config) {
+        Seq(
+          headerSources in config ++=
+            (((sourceDirectory in config).value / "protobuf") ** "*.proto").get,
+          headerMappings := headerMappings.value ++ Map(
+            HeaderFileType("proto") -> cStyleComment
+          )
+        )
+      }
+    }
+  }
+}
