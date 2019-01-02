675ee6420de957c3fc328362c33aa754248367ed
Arnout Engelen <arnout@bzzt.net>
2019-01-02 12:09:10 +0100

    Don't be too clever with the copyright year

--- project/CopyrightHeader.scala
+++ project/CopyrightHeader.scala
@@ -47,7 +47,9 @@ trait CopyrightHeader extends AutoPlugin {
     }
   )
 
-  val CurrentYear = java.time.Year.now.getValue.toString
+  // We hard-code this so PR's created in year X will not suddenly fail in X+1.
+  // Of course we should remember to update it early in the year.
+  val CurrentYear = "2019"
   val CopyrightPattern = "Copyright \\([Cc]\\) (\\d{4}([-–]\\d{4})?) (Lightbend|Typesafe) Inc. <.*>".r
   val CopyrightHeaderPattern = s"(?s).*${CopyrightPattern}.*".r
 
