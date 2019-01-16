f44b777e76fcd99f0fccf39885bbf3411bf3894e
Johannes Rudolph <johannes.rudolph@gmail.com>
2019-01-16 11:44:07 +0100

    build: make sure ignore files are searched for in base directory
    Previously, the paths were just relative paths and depended on the working
    directory of the sbt process. However, that's unreliable when the akka
    sources are included in another build as in the akka-http nightlies.

--- project/ProjectFileIgnoreSupport.scala
+++ project/ProjectFileIgnoreSupport.scala
@@ -7,37 +7,41 @@ package akka
 import java.io.File
 
 import com.typesafe.config.ConfigFactory
+import sbt.AutoPlugin
+import sbt.Def
 import sbt.file
 import sbt.internal.sbtscalafix.Compat
 
-trait ProjectFileIgnoreSupport {
-  protected val stdoutLogger = Compat.ConsoleLogger(System.out)
+class ProjectFileIgnoreSupport(ignoreConfigFile: File, descriptor: String) {
+  private val stdoutLogger = Compat.ConsoleLogger(System.out)
 
-  protected def ignoreConfigFileName: String
+  private lazy val ignoreConfig = {
+    require(ignoreConfigFile.exists(), s"Expected ignore configuration for $descriptor at ${ignoreConfigFile.getAbsolutePath} but was missing")
+    ConfigFactory.parseFile(ignoreConfigFile)
+  }
 
-  protected def descriptor: String
-
-  lazy val ignoredFiles: Set[String] = {
+  private lazy val ignoredFiles: Set[String] = {
     import scala.collection.JavaConverters._
-    val config = ConfigFactory.parseFile(file(ignoreConfigFileName))
-    stdoutLogger.debug(s"Loading ignored-files from $ignoreConfigFileName:[${config.origin().url().toURI.getPath}]")
-    config
+    stdoutLogger.debug(s"Loading ignored-files from $ignoreConfigFile:[${ignoreConfig.origin().url().toURI.getPath}]")
+    ignoreConfig
       .getStringList("ignored-files")
       .asScala
       .toSet
   }
 
-  lazy val ignoredPackages: Set[String] = {
+  private lazy val ignoredPackages: Set[String] = {
     import scala.collection.JavaConverters._
-    val config = ConfigFactory.parseFile(file(ignoreConfigFileName))
-    stdoutLogger.debug(s"Loading ignored-packages from $ignoreConfigFileName:[${config.origin().url().toURI.getPath}]")
-    config
+    stdoutLogger.debug(s"Loading ignored-packages from $ignoreConfigFile:[${ignoreConfig.origin().url().toURI.getPath}]")
+    ignoreConfig
       .getStringList("ignored-packages")
       .asScala
       .toSet
   }
 
-  protected def isIgnoredByFile(file: File): Boolean = {
+  def isIgnoredByFileOrPackages(file: File): Boolean =
+    isIgnoredByFile(file) || isIgnoredByPackages(file)
+
+  private def isIgnoredByFile(file: File): Boolean = {
     val ignoredByFile = ignoredFiles(file.getName)
     if (ignoredByFile) {
       stdoutLogger.debug(s"$descriptor ignored file with file name:${file.getName} file:[${file.toPath}]")
@@ -45,7 +49,7 @@ trait ProjectFileIgnoreSupport {
     ignoredByFile
   }
 
-  protected def isIgnoredByPackages(file: File): Boolean = {
+  private def isIgnoredByPackages(file: File): Boolean = {
     val ignoredByPackages = ignoredPackages.exists(pkg => {
       getPackageName(file.toURI.toString) match {
         case Some(packageName) =>
@@ -60,11 +64,7 @@ trait ProjectFileIgnoreSupport {
     ignoredByPackages
   }
 
-  protected def isIgnoredByFileOrPackages(file: File): Boolean = {
-    isIgnoredByFile(file) || isIgnoredByPackages(file)
-  }
-
-  protected def getPackageName(fileName: String): Option[String] = {
+  private def getPackageName(fileName: String): Option[String] = {
     def getPackageName0(fileType: String): String = {
       import java.io.{File => JFile}
       fileName.split(JFile.separatorChar)
