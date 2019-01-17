e5effba88426b06d521f688210222bb6a2e297aa
Pierre-Marie Padiou <pm47@users.noreply.github.com>
2019-01-17 15:58:34 +0100

    revert Expand->Flatten in the comment

--- akka-docs/src/test/scala/docs/stream/TwitterStreamQuickstartDocSpec.scala
+++ akka-docs/src/test/scala/docs/stream/TwitterStreamQuickstartDocSpec.scala
@@ -155,7 +155,7 @@ class TwitterStreamQuickstartDocSpec extends AkkaSpec {
       .filterNot(_.hashtags.contains(akkaTag)) // Remove all tweets containing #akka hashtag
       .map(_.hashtags) // Get all sets of hashtags ...
       .reduce(_ ++ _) // ... and reduce them to a single set, removing duplicates across all tweets
-      .mapConcat(identity) // Expand the set of hashtags to a stream of hashtags
+      .mapConcat(identity) // Flatten the set of hashtags to a stream of hashtags
       .map(_.name.toUpperCase) // Convert all hashtags to upper case
       .runWith(Sink.foreach(println)) // Attach the Flow to a Sink that will finally print the hashtags
 
