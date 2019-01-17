dcb17b1d0c1c4b26a1dff15ba1e8cd7f7fd6f379
Pierre-Marie Padiou <pm47@users.noreply.github.com>
2019-01-17 16:51:28 +0100

    Fix comment in hashtag example of streams quickstart doc

--- akka-docs/src/test/scala/docs/stream/TwitterStreamQuickstartDocSpec.scala
+++ akka-docs/src/test/scala/docs/stream/TwitterStreamQuickstartDocSpec.scala
@@ -156,7 +156,7 @@ class TwitterStreamQuickstartDocSpec extends AkkaSpec {
       .filterNot(_.hashtags.contains(akkaTag)) // Remove all tweets containing #akka hashtag
       .map(_.hashtags) // Get all sets of hashtags ...
       .reduce(_ ++ _) // ... and reduce them to a single set, removing duplicates across all tweets
-      .mapConcat(identity) // Flatten the stream of tweets to a stream of hashtags
+      .mapConcat(identity) // Flatten the set of hashtags to a stream of hashtags
       .map(_.name.toUpperCase) // Convert all hashtags to upper case
       .runWith(Sink.foreach(println)) // Attach the Flow to a Sink that will finally print the hashtags
 
