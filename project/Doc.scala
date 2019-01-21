f134893d43f895e941edd8c7494d49e7dbf9c195
Arnout Engelen <arnout@bzzt.net>
2019-01-21 16:08:04 +0100

    Skip javadoc for scaladsl/GraphDSL.java
    As problematic javadoc seems to be generated for
    akka.stream.scaladsl.GraphDSL.Implicits.ReversePortsOps

--- project/Doc.scala
+++ project/Doc.scala
@@ -5,7 +5,7 @@
 package akka
 
 import sbt._
-import sbtunidoc.BaseUnidocPlugin.autoImport.{unidoc, unidocProjectFilter}
+import sbtunidoc.BaseUnidocPlugin.autoImport.{unidoc, unidocProjectFilter, unidocAllSources}
 import sbtunidoc.JavaUnidocPlugin.autoImport.JavaUnidoc
 import sbtunidoc.ScalaUnidocPlugin.autoImport.ScalaUnidoc
 import sbtunidoc.GenJavadocPlugin.autoImport._
@@ -124,7 +124,13 @@ object UnidocRoot extends AutoPlugin {
     inTask(unidoc)(Seq(
       unidocProjectFilter in ScalaUnidoc := unidocRootProjectFilter(unidocRootIgnoreProjects.value),
       unidocProjectFilter in JavaUnidoc := unidocRootProjectFilter(unidocRootIgnoreProjects.value),
-      apiMappings in ScalaUnidoc := (apiMappings in (Compile, doc)).value))
+      apiMappings in ScalaUnidoc := (apiMappings in (Compile, doc)).value) ++
+      UnidocRoot.CliOptions.genjavadocEnabled.ifTrue(Seq(
+        // akka.stream.scaladsl.GraphDSL.Implicits.ReversePortsOps contains code that
+        // genjavadoc turns into (probably incorrect) Java code that in turn confuses the javadoc tool.
+        unidocAllSources in JavaUnidoc ~= { v => v.map(_.filterNot(_.getAbsolutePath.endsWith("scaladsl/GraphDSL.java"))) },
+      )).getOrElse(Nil)
+    )
   }
 }
 
