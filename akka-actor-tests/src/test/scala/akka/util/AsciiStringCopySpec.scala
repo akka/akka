36ffa1e6de513f0a9f4f771db4a387cfb8657509
Johan Andrén <johan@markatta.com>
2019-02-07 13:04:44 +0100

    Cover the unsafe string access optimizations with direct tests

---
+++ akka-actor-tests/src/test/scala/akka/util/AsciiStringCopySpec.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2009-2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.util
+import org.scalatest.Matchers
+import org.scalatest.WordSpec
+
+class AsciiStringCopySpec extends WordSpec with Matchers {
+
+  "The copyUSAsciiStrToBytes optimization" must {
+
+    "copy string internal representation successfully" in {
+      val ascii = "abcdefghijklmnopqrstuvxyz"
+      val byteArray = new Array[Byte](ascii.length)
+      Unsafe.copyUSAsciiStrToBytes(ascii, byteArray)
+      new String(byteArray) should ===(ascii)
+    }
+  }
+
+  "The fast hash optimization" must {
+
+    "hash strings successfully" in {
+      Unsafe.fastHash("abcdefghijklmnopqrstuvxyz")
+    }
+
+  }
+
+}
