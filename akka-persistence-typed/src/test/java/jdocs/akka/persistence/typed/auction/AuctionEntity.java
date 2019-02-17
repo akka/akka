f70c88860ebd8befd5c4f2c0c3cb30051f3a1d4f
Patrik Nordwall <patrik.nordwall@gmail.com>
2019-02-17 20:54:32 +0100

    Command handler builder for EventSourcedBehaviorWithEnforcedReplies, #25482 (#26272)
    * It uses ReplyEffect instead of Effect
    * Unfortunatley the solution is a copy of the CommandHandler builder with
      a few mechanical changes to the types and names.
    * I tried to separate interface and implementation and use a shared implementation,
      but it didn't work out because methods with such function parameters which only differ in
      their type parameters can't be overloaded.

--- akka-persistence-typed/src/test/java/jdocs/akka/persistence/typed/auction/AuctionEntity.java
+++ akka-persistence-typed/src/test/java/jdocs/akka/persistence/typed/auction/AuctionEntity.java
@@ -22,7 +22,7 @@ import java.util.UUID;
  * https://github.com/lagom/online-auction-java/blob/master/bidding-impl/src/main/java/com/example/auction/bidding/impl/AuctionEntity.java
  */
 public class AuctionEntity
-    extends EventSourcedBehavior<AuctionCommand, AuctionEvent, AuctionState> {
+    extends EventSourcedBehaviorWithEnforcedReplies<AuctionCommand, AuctionEvent, AuctionState> {
 
   private final UUID entityUUID;
 
@@ -33,9 +33,10 @@ public class AuctionEntity
   }
 
   // Command handler for the not started state.
-  private CommandHandlerBuilderByState<AuctionCommand, AuctionEvent, AuctionState, AuctionState>
+  private CommandHandlerWithReplyBuilderByState<
+          AuctionCommand, AuctionEvent, AuctionState, AuctionState>
       notStartedHandler =
-          newCommandHandlerBuilder()
+          newCommandHandlerWithReplyBuilder()
               .forState(state -> state.getStatus() == AuctionStatus.NOT_STARTED)
               .onCommand(StartAuction.class, this::startAuction)
               .onCommand(
@@ -44,18 +45,20 @@ public class AuctionEntity
                       Effect().reply(cmd, createResult(state, PlaceBidStatus.NOT_STARTED)));
 
   // Command handler for the under auction state.
-  private CommandHandlerBuilderByState<AuctionCommand, AuctionEvent, AuctionState, AuctionState>
+  private CommandHandlerWithReplyBuilderByState<
+          AuctionCommand, AuctionEvent, AuctionState, AuctionState>
       underAuctionHandler =
-          newCommandHandlerBuilder()
+          newCommandHandlerWithReplyBuilder()
               .forState(state -> state.getStatus() == AuctionStatus.UNDER_AUCTION)
               .onCommand(StartAuction.class, (state, cmd) -> alreadyDone(cmd))
               .onCommand(PlaceBid.class, this::placeBid)
               .onCommand(FinishBidding.class, this::finishBidding);
 
   // Command handler for the completed state.
-  private CommandHandlerBuilderByState<AuctionCommand, AuctionEvent, AuctionState, AuctionState>
+  private CommandHandlerWithReplyBuilderByState<
+          AuctionCommand, AuctionEvent, AuctionState, AuctionState>
       completedHandler =
-          newCommandHandlerBuilder()
+          newCommandHandlerWithReplyBuilder()
               .forState(state -> state.getStatus() == AuctionStatus.COMPLETE)
               .onCommand(StartAuction.class, (state, cmd) -> alreadyDone(cmd))
               .onCommand(FinishBidding.class, (state, cmd) -> alreadyDone(cmd))
@@ -65,9 +68,10 @@ public class AuctionEntity
                       Effect().reply(cmd, createResult(state, PlaceBidStatus.FINISHED)));
 
   // Command handler for the cancelled state.
-  private CommandHandlerBuilderByState<AuctionCommand, AuctionEvent, AuctionState, AuctionState>
+  private CommandHandlerWithReplyBuilderByState<
+          AuctionCommand, AuctionEvent, AuctionState, AuctionState>
       cancelledHandler =
-          newCommandHandlerBuilder()
+          newCommandHandlerWithReplyBuilder()
               .forState(state -> state.getStatus() == AuctionStatus.CANCELLED)
               .onCommand(StartAuction.class, (state, cmd) -> alreadyDone(cmd))
               .onCommand(FinishBidding.class, (state, cmd) -> alreadyDone(cmd))
@@ -77,9 +81,10 @@ public class AuctionEntity
                   (state, cmd) ->
                       Effect().reply(cmd, createResult(state, PlaceBidStatus.CANCELLED)));
 
-  private CommandHandlerBuilderByState<AuctionCommand, AuctionEvent, AuctionState, AuctionState>
+  private CommandHandlerWithReplyBuilderByState<
+          AuctionCommand, AuctionEvent, AuctionState, AuctionState>
       getAuctionHandler =
-          newCommandHandlerBuilder()
+          newCommandHandlerWithReplyBuilder()
               .forStateType(AuctionState.class)
               .onCommand(GetAuction.class, (state, cmd) -> Effect().reply(cmd, state));
 
@@ -93,26 +98,29 @@ public class AuctionEntity
   // event from us, it will ignore the bidding finished event, so we need to update our state
   // to reflect that.
 
-  private Effect<AuctionEvent, AuctionState> startAuction(AuctionState state, StartAuction cmd) {
+  private ReplyEffect<AuctionEvent, AuctionState> startAuction(
+      AuctionState state, StartAuction cmd) {
     return Effect()
         .persist(new AuctionStarted(entityUUID, cmd.getAuction()))
         .thenReply(cmd, __ -> Done.getInstance());
   }
 
-  private Effect<AuctionEvent, AuctionState> finishBidding(AuctionState state, FinishBidding cmd) {
+  private ReplyEffect<AuctionEvent, AuctionState> finishBidding(
+      AuctionState state, FinishBidding cmd) {
     return Effect()
         .persist(new BiddingFinished(entityUUID))
         .thenReply(cmd, __ -> Done.getInstance());
   }
 
-  private Effect<AuctionEvent, AuctionState> cancelAuction(AuctionState state, CancelAuction cmd) {
+  private ReplyEffect<AuctionEvent, AuctionState> cancelAuction(
+      AuctionState state, CancelAuction cmd) {
     return Effect()
         .persist(new AuctionCancelled(entityUUID))
         .thenReply(cmd, __ -> Done.getInstance());
   }
 
   /** The main logic for handling of bids. */
-  private Effect<AuctionEvent, AuctionState> placeBid(AuctionState state, PlaceBid bid) {
+  private ReplyEffect<AuctionEvent, AuctionState> placeBid(AuctionState state, PlaceBid bid) {
     Auction auction = state.getAuction().get();
 
     Instant now = Instant.now();
@@ -187,7 +195,7 @@ public class AuctionEntity
    * <p>This emits two events, one for the bid currently being replace, and another automatic bid
    * for the current bidder.
    */
-  private Effect<AuctionEvent, AuctionState> handleAutomaticOutbid(
+  private ReplyEffect<AuctionEvent, AuctionState> handleAutomaticOutbid(
       PlaceBid bid,
       Auction auction,
       Instant now,
@@ -215,7 +223,7 @@ public class AuctionEntity
   }
 
   /** Handle the situation where a bid will be accepted as the new winning bidder. */
-  private Effect<AuctionEvent, AuctionState> handleNewWinningBidder(
+  private ReplyEffect<AuctionEvent, AuctionState> handleNewWinningBidder(
       PlaceBid bid, Auction auction, Instant now, int currentBidMaximum) {
     int nextIncrement = Math.min(currentBidMaximum + auction.getIncrement(), bid.getBidPrice());
     int newBidPrice;
@@ -247,7 +255,7 @@ public class AuctionEntity
   }
 
   @Override
-  public CommandHandler<AuctionCommand, AuctionEvent, AuctionState> commandHandler() {
+  public CommandHandlerWithReply<AuctionCommand, AuctionEvent, AuctionState> commandHandler() {
     return notStartedHandler
         .orElse(underAuctionHandler)
         .orElse(completedHandler)
@@ -284,7 +292,7 @@ public class AuctionEntity
     }
   }
 
-  private Effect<AuctionEvent, AuctionState> alreadyDone(ExpectingReply<Done> cmd) {
+  private ReplyEffect<AuctionEvent, AuctionState> alreadyDone(ExpectingReply<Done> cmd) {
     return Effect().reply(cmd, Done.getInstance());
   }
 }
