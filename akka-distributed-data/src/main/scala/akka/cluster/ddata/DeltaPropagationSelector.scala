825d90bf632a879c4bcb85ffd43e32a4642607a2
Patrik Nordwall <patrik.nordwall@gmail.com>
2019-02-21 09:09:20 +0100

    Avoid false removals in ClusterReceptionist, #26284
    * The scenario was (probably) that a node was restarted with
      same host:port and then didn't join the same cluster. The DData
      Replicator in the original cluster would continue sending messages
      to the new incarnation resulting in false removals.
    * The fix is that DData Replicator includes the system uid of the sending
      or target system in messages and if recipient gets a message that is from/to
      unknown it will discard it and thereby not spreading information across
      different clusters.
    * Reproduced in ClusterReceptionistSpec
    * Much hardening of other things in ClusterReceptionistSpec
    * There are also some improvements to ClusterReceptionist to not leak
      Listing with refs of removed nodes.
    * use ClusterShuttingDown
    * The reason for using sender system uid instead of target uid in messages
      like Read and Write is that then the optimization for sending same message
      to many destinations can remain.

--- akka-distributed-data/src/main/scala/akka/cluster/ddata/DeltaPropagationSelector.scala
+++ akka-distributed-data/src/main/scala/akka/cluster/ddata/DeltaPropagationSelector.scala
@@ -6,8 +6,8 @@ package akka.cluster.ddata
 
 import scala.collection.immutable.TreeMap
 
-import akka.actor.Address
 import akka.annotation.InternalApi
+import akka.cluster.UniqueAddress
 import akka.cluster.ddata.Key.KeyId
 import akka.cluster.ddata.Replicator.Internal.DeltaPropagation
 import akka.cluster.ddata.Replicator.Internal.DeltaPropagation.NoDeltaPlaceholder
@@ -25,12 +25,12 @@ private[akka] trait DeltaPropagationSelector {
   def propagationCount: Long = _propagationCount
   private var deltaCounter = Map.empty[KeyId, Long]
   private var deltaEntries = Map.empty[KeyId, TreeMap[Long, ReplicatedData]]
-  private var deltaSentToNode = Map.empty[KeyId, Map[Address, Long]]
+  private var deltaSentToNode = Map.empty[KeyId, Map[UniqueAddress, Long]]
   private var deltaNodeRoundRobinCounter = 0L
 
   def gossipIntervalDivisor: Int
 
-  def allNodes: Vector[Address]
+  def allNodes: Vector[UniqueAddress]
 
   def createDeltaPropagation(deltas: Map[KeyId, (ReplicatedData, Long, Long)]): DeltaPropagation
 
@@ -68,7 +68,7 @@ private[akka] trait DeltaPropagationSelector {
     math.min(math.max((allNodesSize / gossipIntervalDivisor) + 1, 2), math.min(allNodesSize, 10))
   }
 
-  def collectPropagations(): Map[Address, DeltaPropagation] = {
+  def collectPropagations(): Map[UniqueAddress, DeltaPropagation] = {
     _propagationCount += 1
     val all = allNodes
     if (all.isEmpty)
@@ -90,7 +90,7 @@ private[akka] trait DeltaPropagationSelector {
       }
       deltaNodeRoundRobinCounter += sliceSize
 
-      var result = Map.empty[Address, DeltaPropagation]
+      var result = Map.empty[UniqueAddress, DeltaPropagation]
 
       var cache = Map.empty[(KeyId, Long, Long), ReplicatedData]
       slice.foreach { node =>
@@ -99,7 +99,7 @@ private[akka] trait DeltaPropagationSelector {
         var deltas = Map.empty[KeyId, (ReplicatedData, Long, Long)]
         deltaEntries.foreach {
           case (key, entries) =>
-            val deltaSentToNodeForKey = deltaSentToNode.getOrElse(key, TreeMap.empty[Address, Long])
+            val deltaSentToNodeForKey = deltaSentToNode.getOrElse(key, TreeMap.empty[UniqueAddress, Long])
             val j = deltaSentToNodeForKey.getOrElse(node, 0L)
             val deltaEntriesAfterJ = deltaEntriesAfter(entries, j)
             if (deltaEntriesAfterJ.nonEmpty) {
@@ -160,7 +160,7 @@ private[akka] trait DeltaPropagationSelector {
     }
   }
 
-  private def findSmallestVersionPropagatedToAllNodes(key: KeyId, all: Vector[Address]): Long = {
+  private def findSmallestVersionPropagatedToAllNodes(key: KeyId, all: Vector[UniqueAddress]): Long = {
     deltaSentToNode.get(key) match {
       case None => 0L
       case Some(deltaSentToNodeForKey) =>
@@ -188,7 +188,7 @@ private[akka] trait DeltaPropagationSelector {
     }
   }
 
-  def cleanupRemovedNode(address: Address): Unit = {
+  def cleanupRemovedNode(address: UniqueAddress): Unit = {
     deltaSentToNode = deltaSentToNode.map {
       case (key, deltaSentToNodeForKey) =>
         key -> (deltaSentToNodeForKey - address)
