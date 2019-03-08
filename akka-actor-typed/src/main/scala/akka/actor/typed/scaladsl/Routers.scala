d3b3cb25dac1094b4439b1d5b8692ee9b9041357
Helena Edelson <helena@users.noreply.github.com>
2019-03-08 09:36:17 +0100

    Helenas lang fixes
    Co-Authored-By: johanandren <johan@markatta.com>

--- akka-actor-typed/src/main/scala/akka/actor/typed/scaladsl/Routers.scala
+++ akka-actor-typed/src/main/scala/akka/actor/typed/scaladsl/Routers.scala
@@ -28,11 +28,11 @@ object Routers {
 
   /**
    * Spawn `poolSize` children with the given `behavior` and forward messages to them using round robin.
-   * If a child is stopped it is removed from the pool, to have children restart on failure use supervision.
+   * If a child is stopped it is removed from the pool. To have children restart on failure, use supervision.
    * When all children are stopped the pool stops itself. To stop the pool from the outside, use `ActorContext.stop`
    * from the parent actor.
    *
-   * Note that if a child stops there is a slight chance that messages still get delivered to it, and get lost,
+   * Note that if a child stops, there is a slight chance that messages still get delivered to it, and get lost,
    * before the pool sees that the child stopped. Therefore it is best to _not_ stop children arbitrarily.
    */
   def pool[T](poolSize: Int)(behavior: Behavior[T]): PoolRouter[T] =
