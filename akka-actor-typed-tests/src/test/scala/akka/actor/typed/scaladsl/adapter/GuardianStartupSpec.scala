dedc404951845b4602bdd07ee75d15c90693aca8
Johan Andrén <johan@markatta.com>
2019-03-21 10:23:21 +0100

    One more test case

--- akka-actor-typed-tests/src/test/scala/akka/actor/typed/scaladsl/adapter/GuardianStartupSpec.scala
+++ akka-actor-typed-tests/src/test/scala/akka/actor/typed/scaladsl/adapter/GuardianStartupSpec.scala
@@ -11,8 +11,9 @@ import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import org.scalatest.Matchers
 import org.scalatest.WordSpec
+import org.scalatest.concurrent.ScalaFutures
 
-class GuardianStartupSpec extends WordSpec with Matchers {
+class GuardianStartupSpec extends WordSpec with Matchers with ScalaFutures {
 
   "The user guardian" must {
 
@@ -38,18 +39,36 @@ class GuardianStartupSpec extends WordSpec with Matchers {
 
     "should not start before untyped system initialization is complete" in {
       var system: ActorSystem[String] = null
-      @volatile var ok = false
+      @volatile var untypedSystemInitialized = false
       val guardianBehavior = Behaviors.setup[String] { ctx =>
-        ctx.system.toUntyped.asInstanceOf[ActorSystemImpl].assertInitialized
-        ok = true
+        ctx.system.toUntyped.asInstanceOf[ActorSystemImpl].assertInitialized()
+        untypedSystemInitialized = true
         Behaviors.empty
       }
       try {
-        system = ActorSystem(guardianBehavior, "GuardianStartupSpec-get-all")
+        system = ActorSystem(guardianBehavior, "GuardianStartupSpec-initialized")
         system ! "msg"
 
         val probe = TestProbe()(system)
-        probe.awaitAssert(ok should ===(true))
+        probe.awaitAssert(untypedSystemInitialized should ===(true))
+
+      } finally {
+        if (system ne null)
+          ActorTestKit.shutdown(system)
+      }
+    }
+
+    "have its shutdown hook run on immediate shutdown (after start)" in {
+      var system: ActorSystem[String] = null
+      @volatile var stopHookExecuted = false
+      // note that an immediately stopped (ActorSystem(Behaviors.stopped) is not allowed
+      val guardianBehavior = Behaviors.setup[String](_ => Behaviors.stopped(() => stopHookExecuted = true))
+
+      try {
+        system = ActorSystem(guardianBehavior, "GuardianStartupSpec-stop-hook")
+
+        system.whenTerminated.futureValue
+        stopHookExecuted should ===(true)
 
       } finally {
         if (system ne null)
