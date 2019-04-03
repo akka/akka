fbcb8c9f33f201aa8715bf6fef6a2172c96c5df3
Christopher Batey <christopher.batey@gmail.com>
2019-04-03 07:32:38 +0100

    Review follow ups for PR #26653

---
+++ akka-actor/src/main/scala/akka/util/ErrorMessages.scala
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.util
+
+object ErrorMessages {
+  val RemoteDeploymentConfigErrorPrefix = "configuration requested remote deployment"
+}
