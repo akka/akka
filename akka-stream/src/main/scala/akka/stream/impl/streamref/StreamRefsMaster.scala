9dbd1e884aeac50ea0acf91b0a9578c6dd4082da
Johan Andrén <johan@markatta.com>
2019-04-03 18:09:57 +0200

    Unused stuff eliminated

--- akka-stream/src/main/scala/akka/stream/impl/streamref/StreamRefsMaster.scala
+++ akka-stream/src/main/scala/akka/stream/impl/streamref/StreamRefsMaster.scala
@@ -7,14 +7,13 @@ package akka.stream.impl.streamref
 import akka.actor.{ ActorSystem, ExtendedActorSystem, Extension, ExtensionId, ExtensionIdProvider }
 import akka.annotation.InternalApi
 import akka.stream.impl.SeqActorName
-import akka.util.unused
 
 /** INTERNAL API */
 @InternalApi
 private[stream] object StreamRefsMaster extends ExtensionId[StreamRefsMaster] with ExtensionIdProvider {
 
   override def createExtension(system: ExtendedActorSystem): StreamRefsMaster =
-    new StreamRefsMaster(system)
+    new StreamRefsMaster
 
   override def lookup(): StreamRefsMaster.type = this
 
@@ -23,7 +22,7 @@ private[stream] object StreamRefsMaster extends ExtensionId[StreamRefsMaster] wi
 
 /** INTERNAL API */
 @InternalApi
-private[stream] final class StreamRefsMaster(@unused system: ExtendedActorSystem) extends Extension {
+private[stream] final class StreamRefsMaster extends Extension {
 
   private[this] val sourceRefStageNames = SeqActorName("SourceRef") // "local target"
   private[this] val sinkRefStageNames = SeqActorName("SinkRef") // "remote sender"
