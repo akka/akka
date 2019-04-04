525dc94767255761cc03cfac7f4b8d8f90a1f1ee
Johan Andrén <johan@markatta.com>
2019-04-04 14:48:51 +0200

    Messed up plugin config on rebase

--- project/AkkaDisciplinePlugin.scala
+++ project/AkkaDisciplinePlugin.scala
@@ -17,14 +17,15 @@ object AkkaDisciplinePlugin extends AutoPlugin with ScalafixSupport {
   override def requires: Plugins = JvmPlugin && ScalafixPlugin
   override lazy val projectSettings = disciplineSettings
 
-  val fatalWarningsFor =
-    Set(
-      "akka-actor",
-      "akka-discovery",
-      "akka-distributed-data",
-      "akka-coordination",
-      "akka-protobuf",
-      "akka-persistence")
+  val fatalWarningsFor = Set(
+    "akka-actor",
+    "akka-discovery",
+    "akka-distributed-data",
+    "akka-coordination",
+    "akka-protobuf",
+    "akka-stream-typed",
+    "akka-cluster-typed",
+    "akka-persistence")
 
   val strictProjects = Set("akka-discovery", "akka-protobuf", "akka-coordination")
 
@@ -40,7 +41,8 @@ object AkkaDisciplinePlugin extends AutoPlugin with ScalafixSupport {
   lazy val silencerSettings = Seq(
     libraryDependencies ++= Seq(
         compilerPlugin("com.github.ghik" %% "silencer-plugin" % silencerVersion),
-        "com.github.ghik" %% "silencer-lib" % silencerVersion % Provided))
+        "com.github.ghik" %% "silencer-lib" % silencerVersion % Provided
+      ))
 
   lazy val disciplineSettings =
     scalaFixSettings ++
@@ -50,6 +52,7 @@ object AkkaDisciplinePlugin extends AutoPlugin with ScalafixSupport {
           if (fatalWarningsFor(name.value)) Seq("-Xfatal-warnings")
           else Seq.empty
         ),
+      Test / scalacOptions --= testUndicipline,
       Compile / console / scalacOptions --= Seq("-deprecation", "-Xfatal-warnings", "-Xlint", "-Ywarn-unused:imports"),
       Compile / scalacOptions ++= (CrossVersion.partialVersion(scalaVersion.value) match {
           case Some((2, 13)) =>
@@ -59,14 +62,16 @@ object AkkaDisciplinePlugin extends AutoPlugin with ScalafixSupport {
               "-Ywarn-nullary-override",
               "-Ywarn-nullary-unit",
               "-Ypartial-unification",
-              "-Yno-adapted-args")
+              "-Yno-adapted-args"
+            )
           case Some((2, 12)) =>
             disciplineScalacOptions
           case Some((2, 11)) =>
             disciplineScalacOptions ++ Set("-language:existentials") -- Set(
               "-Ywarn-extra-implicit",
               "-Ywarn-unused:_",
-              "-Ypartial-unification")
+              "-Ypartial-unification"
+            )
           case _ =>
             Nil
         }).toSeq,
@@ -83,7 +88,13 @@ object AkkaDisciplinePlugin extends AutoPlugin with ScalafixSupport {
       // different compiler phases from the regular run), and in particular
       // '-Ywarn-unused:explicits' breaks 'sbt ++2.13.0-M5 akka-actor/doc'
       // https://github.com/akka/akka/issues/26119
-      Compile / doc / scalacOptions --= disciplineScalacOptions.toSeq :+ "-Xfatal-warnings")
+      Compile / doc / scalacOptions --= disciplineScalacOptions.toSeq :+ "-Xfatal-warnings"
+    )
+
+  val testUndicipline = Seq(
+    "-Ywarn-dead-code", // ??? used in compile only specs
+    "-Ywarn-value-discard" // Ignoring returned assertions
+  )
 
   /**
    * Remain visibly filtered for future code quality work and removing.
@@ -107,9 +118,7 @@ object AkkaDisciplinePlugin extends AutoPlugin with ScalafixSupport {
     "-Ywarn-nullary-unit",
     "-Ywarn-unused:_",
     "-Ypartial-unification",
-    "-Ywarn-extra-implicit",
-    "akka-protobuf",
-    "akka-stream-typed",
-    "akka-cluster-typed")
+    "-Ywarn-extra-implicit"
+  )
 
 }
