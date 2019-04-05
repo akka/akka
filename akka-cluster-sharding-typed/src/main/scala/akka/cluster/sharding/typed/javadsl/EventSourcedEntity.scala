1bffece4c1234f55ddce2bb82a37f296c7079376
Renato Cavalcanti <renato@cavalcanti.be>
2019-04-05 17:15:14 +0200

    Adds EventSourcedEntity with EnforcingReplies

--- akka-cluster-sharding-typed/src/main/scala/akka/cluster/sharding/typed/javadsl/EventSourcedEntity.scala
+++ akka-cluster-sharding-typed/src/main/scala/akka/cluster/sharding/typed/javadsl/EventSourcedEntity.scala
@@ -8,7 +8,12 @@ import java.util.Optional
 
 import akka.actor.typed.BackoffSupervisorStrategy
 import akka.persistence.typed.PersistenceId
-import akka.persistence.typed.javadsl.EventSourcedBehavior
+import akka.persistence.typed.javadsl.{
+  Effect,
+  EventSourcedBehavior,
+  EventSourcedBehaviorWithEnforcedReplies,
+  ReplyEffect
+}
 
 /**
  * Any [[akka.actor.typed.Behavior]] can be used as a sharded entity actor, but the combination of sharding and persistent
@@ -24,6 +29,43 @@ abstract class EventSourcedEntity[Command, Event, State] private (
     persistenceId: PersistenceId,
     onPersistFailure: Optional[BackoffSupervisorStrategy])
     extends EventSourcedBehavior[Command, Event, State](persistenceId, onPersistFailure) {
+
+  def this(entityTypeKey: EntityTypeKey[Command], entityId: String) = {
+    this(
+      entityTypeKey,
+      entityId,
+      persistenceId = entityTypeKey.persistenceIdFrom(entityId),
+      Optional.empty[BackoffSupervisorStrategy])
+  }
+
+  def this(entityTypeKey: EntityTypeKey[Command], entityId: String, onPersistFailure: BackoffSupervisorStrategy) = {
+    this(
+      entityTypeKey,
+      entityId,
+      persistenceId = entityTypeKey.persistenceIdFrom(entityId),
+      Optional.ofNullable(onPersistFailure))
+  }
+
+}
+
+/**
+ * Any [[Behavior]] can be used as a sharded entity actor, but the combination of sharding and persistent
+ * actors is very common and therefore this `PersistentEntity` class is provided as convenience.
+ *
+ * A [[EventSourcedEntityWithEnforcedReplies]] enforces that replies to commands are not forgotten.
+ * There will be compilation errors if the returned effect isn't a [[ReplyEffect]], which can be
+ * created with `Effects().reply`, `Effects().noReply`, [[Effect.thenReply]], or [[Effect.thenNoReply]].
+ *
+ * It is a [[EventSourcedBehavior]] and is implemented in the same way. It selects the `persistenceId`
+ * automatically from the [[EntityTypeKey]] and `entityId` constructor parameters by using
+ * [[EntityTypeKey.persistenceIdFrom]].
+ */
+abstract class EventSourcedEntityWithEnforcedReplies[Command, Event, State] private (
+    val entityTypeKey: EntityTypeKey[Command],
+    val entityId: String,
+    persistenceId: PersistenceId,
+    onPersistFailure: Optional[BackoffSupervisorStrategy])
+    extends EventSourcedBehaviorWithEnforcedReplies[Command, Event, State](persistenceId, onPersistFailure) {
 
   def this(entityTypeKey: EntityTypeKey[Command], entityId: String) = {
     this(
