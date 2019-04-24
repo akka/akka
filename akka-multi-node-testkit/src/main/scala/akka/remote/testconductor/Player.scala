7de98731f645ca0f660b512694ba34a2833dbdb8
skvithalani <skvithalani@yahoo.com>
2019-04-24 10:39:20 +0530

    Attempt to fix multi-jvm failure when untyped system is adapted from typed system (#26780)
    * Attempt to fix multi-jvm failure when untyped system is adapted from typed system
    
    Spawn system space actors instead of user space in multi-jam
    
    * Attempt to fix multi-jvm failure when untyped system is adapted from typed system in Conductor
    
    * Attempt to fix multi-jvm tests failing due to some ordering issue with systemActorOf
    
    (cherry picked from commit 8f693998eeb7cb5007e047e0de714c863534bcbf)

--- akka-multi-node-testkit/src/main/scala/akka/remote/testconductor/Player.scala
+++ akka-multi-node-testkit/src/main/scala/akka/remote/testconductor/Player.scala
@@ -88,8 +88,8 @@ trait Player { this: TestConductorExt =>
     import Settings.BarrierTimeout
 
     if (_client ne null) throw new IllegalStateException("TestConductorClient already started")
-    _client = system.actorOf(Props(classOf[ClientFSM], name, controllerAddr), "TestConductorClient")
-    val a = system.actorOf(Player.waiterProps)
+    _client = system.systemActorOf(Props(classOf[ClientFSM], name, controllerAddr), "TestConductorClient")
+    val a = system.systemActorOf(Player.waiterProps, "TestConductorWaiter")
     (a ? client).mapTo(classTag[Done])
   }
 
@@ -279,10 +279,10 @@ private[akka] class ClientFSM(name: RoleName, controllerAddr: InetSocketAddress)
           stay
         case TerminateMsg(Left(false)) =>
           context.system.terminate()
-          stay
+          stop()
         case TerminateMsg(Left(true)) =>
           context.system.asInstanceOf[ActorSystemImpl].abort()
-          stay
+          stop()
         case TerminateMsg(Right(exitValue)) =>
           System.exit(exitValue)
           stay // needed because Java doesn’t have Nothing
