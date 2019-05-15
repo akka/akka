18a3569a7e6dfd5ef8f884e85ed9dab7bd544660
Johan Andrén <johan@markatta.com>
2019-05-15 10:28:12 +0200

    Java interop via for FlowWithContext #26896

--- akka-stream/src/main/scala/akka/stream/scaladsl/FlowWithContext.scala
+++ akka-stream/src/main/scala/akka/stream/scaladsl/FlowWithContext.scala
@@ -4,8 +4,11 @@
 
 package akka.stream.scaladsl
 
+import akka.NotUsed
+
 import scala.annotation.unchecked.uncheckedVariance
 import akka.annotation.ApiMayChange
+import akka.japi.Pair
 import akka.stream._
 
 /**
@@ -66,5 +69,12 @@ final class FlowWithContext[-In, -CtxIn, +Out, +CtxOut, +Mat](delegate: Flow[(In
 
   def asJava[JIn <: In, JCtxIn <: CtxIn, JOut >: Out, JCtxOut >: CtxOut, JMat >: Mat]
       : javadsl.FlowWithContext[JIn, JCtxIn, JOut, JCtxOut, JMat] =
-    new javadsl.FlowWithContext(this)
+    new javadsl.FlowWithContext(
+      javadsl.Flow
+        .create[Pair[JIn, JCtxIn]]()
+        .map(_.toScala)
+        .viaMat(delegate.map {
+          case (first, second) =>
+            Pair[JOut, JCtxOut](first, second)
+        }.asJava, javadsl.Keep.right[NotUsed, JMat]))
 }
