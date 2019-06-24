aac732a9c63470c556e3757773bce86d1544038b
CloudZou <Cloud.Zou@quest.com>
2019-06-25 06:15:57 +0800

    fix java test failed

--- akka-stream/src/main/scala/akka/stream/javadsl/Hub.scala
+++ akka-stream/src/main/scala/akka/stream/javadsl/Hub.scala
@@ -79,15 +79,20 @@ object BroadcastHub {
    * cancelled are simply removed from the dynamic set of consumers.
    *
    * @param clazz Type of elements this hub emits and consumes
+   * @param startAfterNrOfConsumers Elements are buffered until this number of consumers have been connected.
+   * This is only used initially when the operator is starting up, i.e. it is not honored when consumers have
+   * been removed (canceled).
    * @param bufferSize Buffer size used by the producer. Gives an upper bound on how "far" from each other two
    *                   concurrent consumers can be in terms of element. If the buffer is full, the producer
    *                   is backpressured. Must be a power of two and less than 4096.
    */
-  def of[T](@unused clazz: Class[T], bufferSize: Int): Sink[T, Source[T, NotUsed]] = {
-    akka.stream.scaladsl.BroadcastHub.sink[T](bufferSize).mapMaterializedValue(_.asJava).asJava
+  def of[T](@unused clazz: Class[T], startAfterNrOfConsumers: Int,  bufferSize: Int): Sink[T, Source[T, NotUsed]] = {
+    akka.stream.scaladsl.BroadcastHub.sink[T](startAfterNrOfConsumers, bufferSize).mapMaterializedValue(_.asJava).asJava
   }
 
-  def of[T](clazz: Class[T]): Sink[T, Source[T, NotUsed]] = of(clazz, 256)
+  def of[T](clazz: Class[T], bufferSize: Int): Sink[T, Source[T, NotUsed]] = of(clazz, 0, bufferSize)
+
+  def of[T](clazz: Class[T]): Sink[T, Source[T, NotUsed]] = of(clazz, 0,  256)
 
 }
 
