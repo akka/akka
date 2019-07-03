ca28eea2da0da1d9ce496eb07a992df1d3658d93
Johannes Rudolph <johannes.rudolph@gmail.com>
2019-07-03 16:58:59 +0200

    fix test compilation

--- akka-stream-tests/src/test/scala/akka/stream/impl/GraphStageLogicSpec.scala
+++ akka-stream-tests/src/test/scala/akka/stream/impl/GraphStageLogicSpec.scala
@@ -227,7 +227,7 @@ class GraphStageLogicSpec extends StreamSpec with GraphInterpreterSpecKit with S
       // note: a bit dangerous assumptions about connection and logic positions here
       // if anything around creating the logics and connections in the builder changes this may fail
       interpreter.complete(interpreter.connections(0))
-      interpreter.cancel(interpreter.connections(1))
+      interpreter.cancel(interpreter.connections(1), SubscriptionWithCancelException.NoMoreElementsNeeded)
       interpreter.execute(2)
 
       expectMsg("postStop2")
