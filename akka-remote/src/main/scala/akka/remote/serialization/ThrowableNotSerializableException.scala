daa2b3a84364474a7f78305fe75855793d1aac73
Patrik Nordwall <patrik.nordwall@gmail.com>
2019-07-11 13:28:48 +0200

    Add ThrowableNotSerializableException and TimeoutException in MiscMessageSerializer
    * but not enabled for serialization
    * added in 2.6.0, added in 2.5.24 for deserialization to support rolling update
    * see issue #27330 for enabling it for toBinary

---
+++ akka-remote/src/main/scala/akka/remote/serialization/ThrowableNotSerializableException.scala
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.remote.serialization
+
+/**
+ * Use as replacement for an original exception when it can't be serialized or deserialized.
+ * @param originalMessage the message of the original exception
+ * @param originalClassName the class name of the original exception
+ * @param cause exception that caused deserialization error, optional and will not be serialized
+ */
+final class ThrowableNotSerializableException(
+    val originalMessage: String,
+    val originalClassName: String,
+    cause: Throwable)
+    extends IllegalArgumentException(s"Serialization of [$originalClassName] failed. $originalMessage", cause) {
+
+  def this(originalMessage: String, originalClassName: String) = this(originalMessage, originalClassName, null)
+}
