8ee9d2aa8382e5e28d60eee677ccbfb91cbc5ed0
Christopher Batey <christopher.batey@gmail.com>
2019-07-24 07:43:34 +0100

    Fix multi jvm test message

--- akka-remote-tests/src/multi-jvm/scala/akka/remote/artery/TestMessage.scala
+++ akka-remote-tests/src/multi-jvm/scala/akka/remote/artery/TestMessage.scala
@@ -7,7 +7,7 @@ package akka.remote.artery
 import akka.actor.ExtendedActorSystem
 import akka.serialization.SerializerWithStringManifest
 import akka.remote.artery.protobuf.{ TestMessages => proto }
-import akka.protobufv3.internalByteString
+import akka.protobufv3.internal.ByteString
 
 object TestMessage {
   final case class Item(id: Long, name: String)
