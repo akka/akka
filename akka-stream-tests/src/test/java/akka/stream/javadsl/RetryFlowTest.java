330c72a7f822656c6511610035475b4f00469926
Martynas Mickevičius <self@2m.lt>
2019-08-26 16:56:28 +0300

    Refactor Java API to take two functions instead of PF of Try

--- akka-stream-tests/src/test/java/akka/stream/javadsl/RetryFlowTest.java
+++ akka-stream-tests/src/test/java/akka/stream/javadsl/RetryFlowTest.java
@@ -5,8 +5,6 @@
 package akka.stream.javadsl;
 
 import akka.NotUsed;
-import akka.japi.JavaPartialFunction;
-import akka.japi.Option;
 import akka.japi.Pair;
 import akka.stream.StreamTest;
 import akka.stream.testkit.TestPublisher;
@@ -17,13 +15,15 @@ import akka.testkit.AkkaJUnitActorSystemResource;
 import akka.testkit.AkkaSpec;
 import org.junit.ClassRule;
 import org.junit.Test;
+import scala.util.Failure;
 import scala.util.Success;
 import scala.util.Try;
 
 import java.time.Duration;
-import java.util.Collection;
 import java.util.Collections;
+import java.util.Optional;
 
+import static akka.NotUsed.notUsed;
 import static org.junit.Assert.assertEquals;
 
 public class RetryFlowTest extends StreamTest {
@@ -36,47 +36,42 @@ public class RetryFlowTest extends StreamTest {
       new AkkaJUnitActorSystemResource("RetryFlowTest", AkkaSpec.testConf());
 
   @Test
-  public void beAbleToUseRetryFlow() {
-    Flow<Pair<Integer, NotUsed>, Pair<Try<Integer>, NotUsed>, NotUsed> flow =
+  public void retrySuccessfulResponses() {
+    final Integer parallelism = 8;
+    final Duration minBackoff = Duration.ofMillis(10);
+    final Duration maxBackoff = Duration.ofSeconds(5);
+    final Integer randomFactor = 0;
+    final Flow<Pair<Integer, NotUsed>, Pair<Try<Integer>, NotUsed>, NotUsed> flow =
         Flow.fromFunction(
             in -> {
               final Integer request = in.first();
-              return Pair.create(Success.apply(request / 2), NotUsed.getInstance());
+              return Pair.create(Success.apply(request / 2), notUsed());
             });
 
-    final JavaPartialFunction<
-            Pair<Try<Integer>, NotUsed>, Option<Collection<Pair<Integer, NotUsed>>>>
-        retryWith =
-            new JavaPartialFunction<
-                Pair<Try<Integer>, NotUsed>, Option<Collection<Pair<Integer, NotUsed>>>>() {
-              public akka.japi.Option<Collection<Pair<Integer, NotUsed>>> apply(
-                  Pair<Try<Integer>, NotUsed> in, boolean isCheck) {
-                final Try<Integer> response = in.first();
-                if (response.isSuccess()) {
-                  final Integer result = response.get();
-                  if (result > 0) {
-                    return akka.japi.Option.some(
-                        Collections.singleton(Pair.create(result, NotUsed.getInstance())));
-                  } else {
-                    return akka.japi.Option.none();
-                  }
-                } else {
-                  return akka.japi.Option.none();
-                }
+    final Flow<Pair<Integer, NotUsed>, Pair<Try<Integer>, NotUsed>, NotUsed> retryFlow =
+        RetryFlow.withBackoff(
+            parallelism,
+            minBackoff,
+            maxBackoff,
+            randomFactor,
+            flow,
+            r -> {
+              final Integer result = r.first();
+              if (result > 0) {
+                return Optional.of(Collections.singleton(Pair.create(result, notUsed())));
+              } else {
+                return Optional.empty();
               }
-            };
+            },
+            r -> Optional.empty());
 
-    Flow<akka.japi.Pair<Integer, NotUsed>, akka.japi.Pair<Try<Integer>, NotUsed>, NotUsed>
-        retryFlow =
-            RetryFlow.withBackoff(
-                8, Duration.ofMillis(10), Duration.ofSeconds(5), 0, flow, retryWith);
-
-    Pair<TestPublisher.Probe<Integer>, TestSubscriber.Probe<Pair<Try<Integer>, NotUsed>>> probes =
-        TestSource.<Integer>probe(system)
-            .map(i -> Pair.create(i, NotUsed.getInstance()))
-            .via(retryFlow)
-            .toMat(TestSink.probe(system), Keep.both())
-            .run(materializer);
+    final Pair<TestPublisher.Probe<Integer>, TestSubscriber.Probe<Pair<Try<Integer>, NotUsed>>>
+        probes =
+            TestSource.<Integer>probe(system)
+                .map(i -> Pair.create(i, notUsed()))
+                .via(retryFlow)
+                .toMat(TestSink.probe(system), Keep.both())
+                .run(materializer);
 
     final TestPublisher.Probe<Integer> source = probes.first();
     final TestSubscriber.Probe<Pair<Try<Integer>, NotUsed>> sink = probes.second();
@@ -88,6 +83,60 @@ public class RetryFlowTest extends StreamTest {
     assertEquals(2, sink.expectNext().first().get().intValue());
     assertEquals(1, sink.expectNext().first().get().intValue());
     assertEquals(0, sink.expectNext().first().get().intValue());
+
+    source.sendComplete();
+    sink.expectComplete();
+  }
+
+  @Test
+  public void retryFailedResponses() {
+    final Integer parallelism = 8;
+    final Duration minBackoff = Duration.ofMillis(10);
+    final Duration maxBackoff = Duration.ofSeconds(5);
+    final Integer randomFactor = 0;
+    final Flow<Pair<Integer, Integer>, Pair<Try<Integer>, Integer>, NotUsed> flow =
+        Flow.fromFunction(
+            in -> {
+              final Integer request = in.first();
+              if (request > 0)
+                return Pair.create(Failure.apply(new Error("Failed response")), request);
+              else return Pair.create(Success.apply(request), request);
+            });
+
+    final Flow<Pair<Integer, Integer>, Pair<Try<Integer>, Integer>, NotUsed> retryFlow =
+        RetryFlow.withBackoff(
+            parallelism,
+            minBackoff,
+            maxBackoff,
+            randomFactor,
+            flow,
+            r -> Optional.empty(),
+            r -> {
+              final Integer state = r.second();
+              if (state > 0) {
+                return Optional.of(Collections.singleton(Pair.create(state / 2, state / 2)));
+              } else {
+                return Optional.empty();
+              }
+            });
+
+    final Pair<TestPublisher.Probe<Integer>, TestSubscriber.Probe<Pair<Try<Integer>, Integer>>>
+        probes =
+            TestSource.<Integer>probe(system)
+                .map(i -> Pair.create(i, i))
+                .via(retryFlow)
+                .toMat(TestSink.probe(system), Keep.both())
+                .run(materializer);
+
+    final TestPublisher.Probe<Integer> source = probes.first();
+    final TestSubscriber.Probe<Pair<Try<Integer>, Integer>> sink = probes.second();
+
+    sink.request(1);
+    source.sendNext(8);
+
+    Pair<Try<Integer>, Integer> response = sink.expectNext();
+    assertEquals(0, response.first().get().intValue());
+    assertEquals(4, response.second().intValue());
 
     source.sendComplete();
     sink.expectComplete();
