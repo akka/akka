27788af792a7b3e7608ec6d8273c94ebf5589126
burbaki <burbakir@gmail.com>
2019-09-03 23:10:36 +0300

    made DnsSettingSpec more readable

--- akka-actor-tests/src/test/scala/akka/io/dns/DnsSettingsSpec.scala
+++ akka-actor-tests/src/test/scala/akka/io/dns/DnsSettingsSpec.scala
@@ -9,24 +9,29 @@ import java.net.InetAddress
 import akka.actor.ExtendedActorSystem
 import akka.testkit.AkkaSpec
 import com.typesafe.config.ConfigFactory
+
 import scala.concurrent.duration._
+
 class DnsSettingsSpec extends AkkaSpec {
 
   val eas = system.asInstanceOf[ExtendedActorSystem]
 
-  "DNS settings" must {
-
-    "use host servers if set to default" in {
-      val dnsSettings = new DnsSettings(
-        eas,
-        ConfigFactory.parseString("""
+  final val defaultConfig = ConfigFactory.parseString("""
           nameservers = "default"
           resolve-timeout = 1s
           search-domains = []
           ndots = 1
           positive-ttl = forever
           negative-ttl = never
-        """))
+        """)
+
+  "DNS settings" must {
+
+    "use host servers if set to default" in {
+      val dnsSettings = new DnsSettings(
+        eas,
+        defaultConfig
+      )
 
       // Will differ based on name OS DNS servers so just validating it does not throw
       dnsSettings.NameServers
@@ -35,14 +40,7 @@ class DnsSettingsSpec extends AkkaSpec {
     "parse a single name server" in {
       val dnsSettings = new DnsSettings(
         eas,
-        ConfigFactory.parseString("""
-          nameservers = "127.0.0.1"
-          resolve-timeout = 1s
-          search-domains = []
-          ndots = 1
-          positive-ttl = forever
-          negative-ttl = never
-        """))
+        defaultConfig.withFallback(ConfigFactory.parseString("nameservers = \"127.0.0.1\"")))
 
       dnsSettings.NameServers.map(_.getAddress) shouldEqual List(InetAddress.getByName("127.0.0.1"))
     }
@@ -50,14 +48,7 @@ class DnsSettingsSpec extends AkkaSpec {
     "parse a list of name servers" in {
       val dnsSettings = new DnsSettings(
         eas,
-        ConfigFactory.parseString("""
-          nameservers = ["127.0.0.1", "127.0.0.2"]
-          resolve-timeout = 1s
-          search-domains = []
-          ndots = 1
-          positive-ttl = forever
-          negative-ttl = never
-        """))
+        defaultConfig.withFallback(ConfigFactory.parseString("nameservers = [\"127.0.0.1\", \"127.0.0.2\"]")))
 
       dnsSettings.NameServers.map(_.getAddress) shouldEqual List(
         InetAddress.getByName("127.0.0.1"),
@@ -67,14 +58,10 @@ class DnsSettingsSpec extends AkkaSpec {
     "use host search domains if set to default" in {
       val dnsSettings = new DnsSettings(
         eas,
-        ConfigFactory.parseString("""
+        defaultConfig.withFallback(ConfigFactory.parseString("""
           nameservers = "127.0.0.1"
-          resolve-timeout = 1s
           search-domains = "default"
-          ndots = 1
-          positive-ttl = forever
-          negative-ttl = never
-        """))
+        """)))
 
       // Will differ based on name OS DNS servers so just validating it does not throw
       dnsSettings.SearchDomains shouldNot equal(List("default"))
@@ -83,14 +70,10 @@ class DnsSettingsSpec extends AkkaSpec {
     "parse a single search domain" in {
       val dnsSettings = new DnsSettings(
         eas,
-        ConfigFactory.parseString("""
+        defaultConfig.withFallback(ConfigFactory.parseString("""
           nameservers = "127.0.0.1"
-          resolve-timeout = 1s
           search-domains = "example.com"
-          ndots = 1
-          positive-ttl = forever
-          negative-ttl = never
-        """))
+        """)))
 
       dnsSettings.SearchDomains shouldEqual List("example.com")
     }
@@ -98,14 +81,10 @@ class DnsSettingsSpec extends AkkaSpec {
     "parse a single list of search domains" in {
       val dnsSettings = new DnsSettings(
         eas,
-        ConfigFactory.parseString("""
+        defaultConfig.withFallback(ConfigFactory.parseString("""
           nameservers = "127.0.0.1"
-          resolve-timeout = 1s
           search-domains = [ "example.com", "example.net" ]
-          ndots = 1
-          positive-ttl = forever
-          negative-ttl = never
-        """))
+        """)))
 
       dnsSettings.SearchDomains shouldEqual List("example.com", "example.net")
     }
@@ -113,14 +92,12 @@ class DnsSettingsSpec extends AkkaSpec {
     "use host ndots if set to default" in {
       val dnsSettings = new DnsSettings(
         eas,
-        ConfigFactory.parseString("""
+        defaultConfig.withFallback(ConfigFactory.parseString("""
           nameservers = "127.0.0.1"
-          resolve-timeout = 1s
           search-domains = "example.com"
           ndots = "default"
-          positive-ttl = forever
-          negative-ttl = never
-        """))
+        """)))
+
 
       // Will differ based on name OS DNS servers so just validating it does not throw
       dnsSettings.NDots
@@ -129,14 +106,11 @@ class DnsSettingsSpec extends AkkaSpec {
     "parse ndots" in {
       val dnsSettings = new DnsSettings(
         eas,
-        ConfigFactory.parseString("""
+        defaultConfig.withFallback(ConfigFactory.parseString("""
           nameservers = "127.0.0.1"
-          resolve-timeout = 1s
           search-domains = "example.com"
           ndots = 5
-          positive-ttl = forever
-          negative-ttl = never
-        """))
+        """)))
 
       dnsSettings.NDots shouldEqual 5
     }
@@ -144,28 +118,18 @@ class DnsSettingsSpec extends AkkaSpec {
     "parse ttl" in {
       val dnsSettingsNeverForever = new DnsSettings(
         eas,
-        ConfigFactory.parseString("""
-          nameservers = "default"
-          resolve-timeout = 1s
-          search-domains = []
-          ndots = 1
-          positive-ttl = forever
-          negative-ttl = never
-        """))
+        defaultConfig)
 
       dnsSettingsNeverForever.positiveCachePolicy shouldEqual CachePolicy.Forever
       dnsSettingsNeverForever.negativeCachePolicy shouldEqual CachePolicy.Never
 
       val dnsSettingsDuration = new DnsSettings(
         eas,
-        ConfigFactory.parseString("""
-          nameservers = "default"
-          resolve-timeout = 1s
-          search-domains = []
-          ndots = 1
+        defaultConfig.withFallback(ConfigFactory.parseString("""
           positive-ttl = 10 s
           negative-ttl = 10 d
-        """))
+        """)))
+
       dnsSettingsDuration.positiveCachePolicy shouldEqual CachePolicy.Ttl.fromPositive(10.seconds)
       dnsSettingsDuration.negativeCachePolicy shouldEqual CachePolicy.Ttl.fromPositive(10.days)
     }
