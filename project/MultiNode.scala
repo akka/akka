36f27318466e1d0967b37bf978c112dc2f155ee9
Helena Edelson <helena@users.noreply.github.com>
2019-09-09 12:36:05 -0700

    Multi-jvm assembly: deduplicate: META-INF default file #27667 (#27668)

--- project/MultiNode.scala
+++ project/MultiNode.scala
@@ -76,6 +76,7 @@ object MultiNode extends AutoPlugin {
       logLevel in multiJvmCreateLogger := Level.Debug, //  to see ssh establishment
       assemblyMergeStrategy in assembly in MultiJvm := {
         case n if n.endsWith("logback-test.xml") ⇒ MergeStrategy.first
+        case n if n.toLowerCase.matches("meta-inf.*\\.default") => MergeStrategy.first
         case n => (assemblyMergeStrategy in assembly in MultiJvm).value.apply(n)
       },
       multiJvmCreateLogger in MultiJvm := { // to use normal sbt logging infra instead of custom sbt-multijvm-one
