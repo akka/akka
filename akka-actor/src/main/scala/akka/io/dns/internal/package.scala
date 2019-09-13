672a6291278b765e103510af1a53b2352417db83
Johan Andrén <johan@markatta.com>
2019-09-13 09:15:13 +0200

    Added ttl configs for AsynchDnsResolve #27694 (#27695)

--- akka-actor/src/main/scala/akka/io/dns/internal/package.scala
+++ akka-actor/src/main/scala/akka/io/dns/internal/package.scala
@@ -7,6 +7,7 @@ package akka.io.dns
 import java.nio.ByteOrder
 
 import akka.annotation.InternalApi
+import akka.io.dns.CachePolicy.{ CachePolicy, Forever, Never, Ttl }
 
 /**
  * INTERNAL API
@@ -20,5 +21,17 @@ package object internal {
    */
   @InternalApi
   private[akka] implicit val networkByteOrder: ByteOrder = ByteOrder.BIG_ENDIAN
+
+  @InternalApi
+  private[akka] implicit object CachePolicyIsOrdered extends Ordering[CachePolicy] {
+    def compare(a: CachePolicy, b: CachePolicy): Int =
+      (a, b) match {
+        case (Forever, Forever) => 0
+        case (Never, Never)     => 0
+        case (Ttl(v1), Ttl(v2)) => v1.compare(v2)
+        case (Never, _)         => -1
+        case (Forever, _)       => 1
+      }
+  }
 
 }
