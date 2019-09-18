fe408923faeb8b329d93f77dcdc860aaf9f33ded
Patrik Nordwall <patrik.nordwall@gmail.com>
2019-09-18 12:20:45 +0200

    Code example for collect and collectType

---
+++ akka-docs/src/test/scala/docs/stream/operators/sourceorflow/Collect.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package docs.stream.operators.sourceorflow
+
+import akka.NotUsed
+import akka.stream.scaladsl.Flow
+
+object Collect {
+  //#collect-elements
+  trait Message
+  final case class Ping(id: Int) extends Message
+  final case class Pong(id: Int)
+  //#collect-elements
+
+  def collectExample(): Unit = {
+    //#collect
+    val flow: Flow[Message, Pong, NotUsed] =
+      Flow[Message].collect {
+        case Ping(id) if id != 0 => Pong(id)
+      }
+    //#collect
+  }
+
+  def collectType(): Unit = {
+    //#collectType
+    val flow: Flow[Message, Pong, NotUsed] =
+      Flow[Message].collectType[Ping].filter(_.id != 0).map(p => Pong(p.id))
+    //#collectType
+  }
+}
