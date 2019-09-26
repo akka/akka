2409add7ead3a30d3fb887f60a516f6b3b5cccec
Johan Andrén <johan@markatta.com>
2019-09-26 14:43:06 +0200

    LogCapture in more tests (#27660)
    * LogCapturing for ClassicSupervisingTypedSpec
    
    * LogCapturing for AdapterSpec
    
    * Spelling in AdapterSpec
    
    * LogCapturing for JoinConfigIncompatibilitySpec and JoinConfigCompatibilitySpec
    
    Also: updates to test to actually verify what they say they verify
    
    * Review feedback fixes
    
    * No depend on AkkaSpec.config

--- akka-actor-typed-tests/src/test/scala/akka/actor/typed/scaladsl/adapter/AdapterSpec.scala
+++ akka-actor-typed-tests/src/test/scala/akka/actor/typed/scaladsl/adapter/AdapterSpec.scala
@@ -5,7 +5,6 @@
 package akka.actor.typed.scaladsl.adapter
 
 import scala.util.control.NoStackTrace
-
 import akka.actor.InvalidMessageException
 import akka.actor.testkit.typed.TestException
 import akka.actor.typed.scaladsl.Behaviors
@@ -16,8 +15,10 @@ import akka.actor.typed.Terminated
 import akka.testkit._
 import akka.Done
 import akka.NotUsed
+import akka.actor.testkit.typed.scaladsl.LogCapturing
 import akka.actor.testkit.typed.scaladsl.LoggingEventFilter
 import akka.{ actor => classic }
+import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpec }
 
 object AdapterSpec {
   val classic1: classic.Props = classic.Props(new Classic1)
@@ -126,18 +127,18 @@ object AdapterSpec {
       case classic.Terminated(_) =>
         probe ! "terminated"
       case "supervise-stop" =>
-        testSupervice(ThrowIt1)
+        testSupervise(ThrowIt1)
       case "supervise-resume" =>
-        testSupervice(ThrowIt2)
+        testSupervise(ThrowIt2)
       case "supervise-restart" =>
-        testSupervice(ThrowIt3)
+        testSupervise(ThrowIt3)
       case "stop-child" =>
         val child = context.spawnAnonymous(typed2)
         context.watch(child)
         context.stop(child)
     }
 
-    private def testSupervice(t: ThrowIt): Unit = {
+    private def testSupervise(t: ThrowIt): Unit = {
       val child = context.spawnAnonymous(typed2)
       context.watch(child)
       child ! t
@@ -162,8 +163,10 @@ object AdapterSpec {
 
 }
 
-class AdapterSpec extends AkkaSpec {
+class AdapterSpec extends WordSpec with Matchers with BeforeAndAfterAll with LogCapturing {
   import AdapterSpec._
+
+  implicit val system = akka.actor.ActorSystem("AdapterSpec")
 
   "ActorSystem adaption" must {
     "only happen once for a given actor system" in {
@@ -175,20 +178,20 @@ class AdapterSpec extends AkkaSpec {
 
     "not crash if guardian is stopped" in {
       for { _ <- 0 to 10 } {
-        var system: akka.actor.typed.ActorSystem[NotUsed] = null
+        var systemN: akka.actor.typed.ActorSystem[NotUsed] = null
         try {
-          system = ActorSystem.create(
+          systemN = ActorSystem.create(
             Behaviors.setup[NotUsed](_ => Behaviors.stopped[NotUsed]),
             "AdapterSpec-stopping-guardian")
-        } finally if (system != null) shutdown(system.toClassic)
+        } finally if (system != null) TestKit.shutdownActorSystem(systemN.toClassic)
       }
     }
 
     "not crash if guardian is stopped very quickly" in {
       for { _ <- 0 to 10 } {
-        var system: akka.actor.typed.ActorSystem[Done] = null
+        var systemN: akka.actor.typed.ActorSystem[Done] = null
         try {
-          system = ActorSystem.create(Behaviors.receive[Done] { (context, message) =>
+          systemN = ActorSystem.create(Behaviors.receive[Done] { (context, message) =>
             context.self ! Done
             message match {
               case Done => Behaviors.stopped
@@ -196,7 +199,7 @@ class AdapterSpec extends AkkaSpec {
 
           }, "AdapterSpec-stopping-guardian-2")
 
-        } finally if (system != null) shutdown(system.toClassic)
+        } finally if (system != null) TestKit.shutdownActorSystem(systemN.toClassic)
       }
     }
   }
@@ -310,5 +313,10 @@ class AdapterSpec extends AkkaSpec {
           Thread.sleep(1000)
         }(system.toTyped)
     }
+  }
+
+  override protected def afterAll(): Unit = {
+    super.afterAll()
+    TestKit.shutdownActorSystem(system)
   }
 }
--- akka-cluster-sharding-typed/src/test/scala/akka/cluster/sharding/typed/JoinConfigCompatCheckerClusterShardingSpec.scala
+++ akka-cluster-sharding-typed/src/test/scala/akka/cluster/sharding/typed/JoinConfigCompatCheckerClusterShardingSpec.scala
@@ -4,14 +4,19 @@
 
 package akka.cluster.sharding.typed
 
-import scala.concurrent.duration._
-import scala.collection.{ immutable => im }
-import scala.util.Try
-
-import akka.actor.ActorSystem
-import akka.cluster.Cluster
-import akka.testkit.{ AkkaSpec, LongRunningTest }
+import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import akka.actor.testkit.typed.scaladsl.LogCapturing
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.adapter._
+import akka.cluster.{ Cluster => ClassicCluster }
+import akka.testkit.LongRunningTest
 import com.typesafe.config.{ Config, ConfigFactory }
+import org.scalatest.{ Matchers, WordSpecLike }
+
+import scala.collection.{ immutable => im }
+import scala.concurrent.duration._
+import scala.util.Try
 
 object JoinConfig {
 
@@ -28,37 +33,36 @@ object JoinConfig {
       akka.coordinated-shutdown.terminate-actor-system = on
       akka.remote.classic.netty.tcp.port = 0
       akka.remote.artery.canonical.port = 0
-    """).withFallback(AkkaSpec.testConf)
+    """)
 
   def joinConfig(configured: Int): Config =
     ConfigFactory.parseString(s"$Key = $configured").withFallback(baseConfig)
 }
 
-abstract class JoinConfigCompatCheckerClusterShardingSpec extends AkkaSpec(JoinConfig.joinConfig(JoinConfig.Shards)) {
+abstract class JoinConfigCompatCheckerClusterShardingSpec
+    extends ScalaTestWithActorTestKit(JoinConfig.joinConfig(JoinConfig.Shards))
+    with WordSpecLike
+    with Matchers
+    with LogCapturing {
 
-  protected val duration = 5.seconds
+  protected val clusterWaitDuration = 5.seconds
 
-  protected var nodes: Seq[ActorSystem] = Seq.empty
-
-  override protected def beforeTermination(): Unit = nodes.foreach(shutdown(_))
-
-  protected def join(sys: ActorSystem): Cluster = {
-    nodes :+= sys
-    sys match {
-      case seed if seed eq system =>
-        configured(system) should ===(JoinConfig.Shards)
-        val seedNode = Cluster(seed)
-        seedNode.join(seedNode.selfAddress)
-        awaitCond(seedNode.readView.isSingletonCluster, duration)
-        seedNode
-      case joining =>
-        val cluster = Cluster(joining)
-        cluster.joinSeedNodes(im.Seq(Cluster(system).readView.selfAddress))
-        cluster
+  protected def join(sys: ActorSystem[_]): ClassicCluster = {
+    if (sys eq system) {
+      configured(system) should ===(JoinConfig.Shards)
+      val seedNode = ClassicCluster(system.toClassic)
+      seedNode.join(seedNode.selfAddress)
+      val probe = createTestProbe()
+      probe.awaitAssert(seedNode.readView.isSingletonCluster should ===(true), clusterWaitDuration)
+      seedNode
+    } else {
+      val joiningNode = ClassicCluster(sys.toClassic)
+      joiningNode.joinSeedNodes(im.Seq(ClassicCluster(system.toClassic).selfAddress))
+      joiningNode
     }
   }
 
-  protected def configured(system: ActorSystem): Int =
+  protected def configured(system: ActorSystem[_]): Int =
     Try(system.settings.config.getInt(JoinConfig.Key)).getOrElse(0)
 
 }
@@ -68,11 +72,13 @@ class JoinConfigIncompatibilitySpec extends JoinConfigCompatCheckerClusterShardi
 
     s"not be allowed to join a cluster with different '${JoinConfig.Key}'" taggedAs LongRunningTest in {
       join(system)
-      val joining = ActorSystem(system.name, JoinConfig.joinConfig(JoinConfig.Shards + 1)) // different
-      configured(joining) should ===(configured(system) + 1)
+      val joining = ActorTestKit(system.name, JoinConfig.joinConfig(JoinConfig.Shards + 1)) // different
+      configured(joining.system) should ===(configured(system) + 1)
 
-      val cluster = join(joining)
-      awaitCond(cluster.readView.isTerminated, duration)
+      val joiningNode = join(joining.system)
+      val probe = createTestProbe()
+      probe.awaitAssert(joiningNode.readView.isTerminated should ===(true), clusterWaitDuration)
+      joining.shutdownTestKit()
     }
   }
 }
@@ -82,14 +88,14 @@ class JoinConfigCompatibilitySpec extends JoinConfigCompatCheckerClusterSharding
 
     s"be allowed to join a cluster with the same '${JoinConfig.Key}'" taggedAs LongRunningTest in {
       val seedNode = join(system)
-      val joining = ActorSystem(system.name, JoinConfig.joinConfig(JoinConfig.Shards)) // same
-      val joinConfig = configured(joining)
+      val joining = ActorTestKit(system.name, JoinConfig.joinConfig(JoinConfig.Shards)) // same
+      val joinConfig = configured(joining.system)
       configured(system) should ===(joinConfig)
+      join(joining.system)
 
-      val cluster = join(joining)
-      for {
-        node <- Set(seedNode, cluster)
-      } awaitCond(node.readView.members.size == joinConfig, duration)
+      val probe = createTestProbe()
+      probe.awaitAssert(seedNode.readView.members.size should ===(2), clusterWaitDuration)
+      joining.shutdownTestKit()
     }
   }
 }
