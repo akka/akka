f8054efb5da42664cba5f640ff85f53b756239a7
Patrik Nordwall <patrik.nordwall@gmail.com>
2019-10-03 11:43:33 +0200

    doc: improve receptionist docs, #25895
    * change intro text, not comparing with Classic
    * exampel of Receptionist.Find
    * link from spawn protocol section as requested in issue

--- akka-cluster-typed/src/test/scala/docs/akka/cluster/typed/ReceptionistExample.scala
+++ akka-cluster-typed/src/test/scala/docs/akka/cluster/typed/ReceptionistExample.scala
@@ -69,6 +69,31 @@ object PingPongExample {
   }
   //#pinger-guardian
 
+  //#find
+  object PingManager {
+    sealed trait Command
+    case object PingAll extends Command
+    private case class ListingResponse(listing: Receptionist.Listing) extends Command
+
+    def apply(): Behavior[Command] = {
+      Behaviors.setup[Command] { context =>
+        val listingResponseAdapter = context.messageAdapter[Receptionist.Listing](ListingResponse)
+
+        context.spawnAnonymous(PingService())
+
+        Behaviors.receiveMessage {
+          case PingAll =>
+            context.system.receptionist ! Receptionist.Find(PingService.PingServiceKey, listingResponseAdapter)
+            Behaviors.same
+          case ListingResponse(PingService.PingServiceKey.Listing(listings)) =>
+            listings.foreach(ps => context.spawnAnonymous(Pinger(ps)))
+            Behaviors.same
+        }
+      }
+    }
+  }
+  //#find
+
 }
 
 object ReceptionistExample {
