7764549cb96b1b779aad5ee392b512ca9adc3ad9
Patrik Nordwall <patrik.nordwall@gmail.com>
2019-10-03 12:37:26 +0200

    java formatting

--- akka-cluster-typed/src/test/java/jdocs/akka/cluster/typed/ReceptionistExample.java
+++ akka-cluster-typed/src/test/java/jdocs/akka/cluster/typed/ReceptionistExample.java
@@ -136,13 +136,15 @@ public interface ReceptionistExample {
   public class PingManager {
 
     interface Command {}
+
     enum PingAll implements Command {
       INSTANCE
     }
+
     private static class ListingResponse implements Command {
       final Receptionist.Listing listing;
 
-      private ListingResponse(Receptionist.Listing listing){
+      private ListingResponse(Receptionist.Listing listing) {
         this.listing = listing;
       }
     }
@@ -157,27 +159,29 @@ public interface ReceptionistExample {
     private PingManager(ActorContext<Command> context) {
       this.context = context;
       this.listingResponseAdapter =
-        context.messageAdapter(Receptionist.Listing.class, ListingResponse::new);
+          context.messageAdapter(Receptionist.Listing.class, ListingResponse::new);
 
       context.spawnAnonymous(PingService.create());
     }
 
     private Behavior<Command> behavior() {
       return Behaviors.receive(Command.class)
-        .onMessage(PingAll.class, notUsed -> onPingAll())
-        .onMessage(ListingResponse.class, response -> onListing(response.listing))
-        .build();
+          .onMessage(PingAll.class, notUsed -> onPingAll())
+          .onMessage(ListingResponse.class, response -> onListing(response.listing))
+          .build();
     }
 
     private Behavior<Command> onPingAll() {
-      context.getSystem().receptionist().tell(
-        Receptionist.find(PingService.pingServiceKey, listingResponseAdapter));
+      context
+          .getSystem()
+          .receptionist()
+          .tell(Receptionist.find(PingService.pingServiceKey, listingResponseAdapter));
       return Behaviors.same();
     }
 
     private Behavior<Command> onListing(Receptionist.Listing msg) {
       msg.getServiceInstances(PingService.pingServiceKey)
-        .forEach(pingService -> context.spawnAnonymous(Pinger.create(pingService)));
+          .forEach(pingService -> context.spawnAnonymous(Pinger.create(pingService)));
       return Behaviors.same();
     }
   }
