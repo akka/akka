fdc2bcdaf0a5a91a3dae0359bd894b71dad6f2c2
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2019-10-04 15:21:52 +0200

    Doc fix.

--- akka-stream/src/main/scala/akka/stream/javadsl/FlowWithContext.scala
+++ akka-stream/src/main/scala/akka/stream/javadsl/FlowWithContext.scala
@@ -69,7 +69,7 @@ final class FlowWithContext[In, CtxIn, Out, CtxOut, +Mat](
   /**
    * Context-preserving variant of [[akka.stream.javadsl.Flow.mapError]].
    *
-   * @see [[akka.stream.scaladsl.Flow.mapError]]
+   * @see [[akka.stream.javadsl.Flow.mapError]]
    */
   def mapError(pf: PartialFunction[Throwable, Throwable]): FlowWithContext[In, CtxIn, Out, CtxOut, Mat] =
     viaScala(_.mapError(pf))
@@ -77,7 +77,7 @@ final class FlowWithContext[In, CtxIn, Out, CtxOut, +Mat](
   /**
    * Context-preserving variant of [[akka.stream.javadsl.Flow.mapMaterializedValue]].
    *
-   * @see [[akka.stream.scaladsl.Flow.mapMaterializedValue]]
+   * @see [[akka.stream.javadsl.Flow.mapMaterializedValue]]
    */
   def mapMaterializedValue[Mat2](f: function.Function[Mat, Mat2]): FlowWithContext[In, CtxIn, Out, CtxOut, Mat2] =
     new FlowWithContext(delegate.mapMaterializedValue[Mat2](f))
