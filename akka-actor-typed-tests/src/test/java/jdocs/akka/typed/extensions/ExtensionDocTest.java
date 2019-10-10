5fe780137966c6ca0a75a510ac6b600571b04cb4
Christopher Batey <christopher.batey@gmail.com>
2019-10-10 13:37:08 +0100

    Review feedback

--- akka-actor-typed-tests/src/test/java/jdocs/akka/typed/extensions/ExtensionDocTest.java
+++ akka-actor-typed-tests/src/test/java/jdocs/akka/typed/extensions/ExtensionDocTest.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
 package jdocs.akka.typed.extensions;
 
 import akka.actor.typed.ActorSystem;
@@ -5,68 +9,76 @@ import akka.actor.typed.Behavior;
 import akka.actor.typed.Extension;
 import akka.actor.typed.ExtensionId;
 import akka.actor.typed.javadsl.Behaviors;
+import com.typesafe.config.ConfigFactory;
+import docs.akka.typed.extensions.DatabasePool;
+import docs.akka.typed.extensions.ExtensionDocSpec;
+
+import java.util.concurrent.CompletionStage;
 
 public class ExtensionDocTest {
 
-   //#shared-resource
-   public static class ExpensiveDatabaseConnection {
-      public Object executeQuery(String query) {
-         throw new RuntimeException("I should do a database query");
-      }
-      //...
-   }
-   //#shared-resource
+  // #shared-resource
+  public static class ExpensiveDatabaseConnection {
+    public CompletionStage<Object> executeQuery(String query) {
+      throw new RuntimeException("I should do a database query");
+    }
+    // ...
+  }
+  // #shared-resource
 
-   //#extension
-   public static class DatabaseConnectionPool implements Extension {
-      private final ExpensiveDatabaseConnection _connection;
+  // #extension
+  public static class DatabaseConnectionPool implements Extension {
+    private final ExpensiveDatabaseConnection _connection;
 
-      public DatabaseConnectionPool(ActorSystem<?> system) {
-         // database configuration can be laoded from config
-         // from the actor system
-         _connection = new ExpensiveDatabaseConnection();
-      }
+    public DatabaseConnectionPool(ActorSystem<?> system) {
+      // database configuration can be loaded from config
+      // from the actor system
+      _connection = new ExpensiveDatabaseConnection();
+    }
 
-      public ExpensiveDatabaseConnection connection() {
-         return _connection;
-      }
-   }
-   //#extension
+    public ExpensiveDatabaseConnection connection() {
+      return _connection;
+    }
+  }
+  // #extension
 
-   //#extension-id
-   public static class DatabaseConnectionPoolId extends ExtensionId<DatabaseConnectionPool> {
+  // #extension-id
+  public static class DatabaseConnectionPoolId extends ExtensionId<DatabaseConnectionPool> {
 
-      private final static DatabaseConnectionPoolId instance = new DatabaseConnectionPoolId();
+    private static final DatabaseConnectionPoolId instance = new DatabaseConnectionPoolId();
 
-      private DatabaseConnectionPool { }
+    private DatabaseConnectionPoolId() {}
 
-      // called once per ActorSystem
-      @Override
-      public DatabaseConnectionPool createExtension(ActorSystem system) {
-         return new DatabaseConnectionPool(system);
-      }
+    // called once per ActorSystem
+    @Override
+    public DatabaseConnectionPool createExtension(ActorSystem system) {
+      return new DatabaseConnectionPool(system);
+    }
 
+    public static DatabaseConnectionPool get(ActorSystem<?> system) {
+      return instance.apply(system);
+    }
+  }
+  // #extension-id
 
-      public static DatabaseConnectionPool get(ActorSystem<?> system) {
-         return instance.apply(system);
-      }
-   }
-   //#extension-id
+  public static Behavior<Object> initialBehavior() {
+    return null;
+  }
 
-   public static Behavior<Object> initialBehavior() {
-      return null;
-   }
+  public static void usage() {
+    // #usage
+    Behaviors.setup(
+        (context) -> {
+          DatabaseConnectionPoolId.get(context.getSystem())
+              .connection()
+              .executeQuery("insert into...");
+          return initialBehavior();
+        });
+    // #usage
+  }
 
-   public static void usage() {
-      //#usage
-      Behaviors.setup((context) -> {
-         DatabaseConnectionPoolId.get(context.getSystem()).connection().executeQuery("insert into...");
-         return initialBehavior();
-      });
-      //#usage
-   }
-
-   public static void checkScalaExtension() {
-      docs.akka.typed.extensions.ExtensionDocSpec.DatabaseConnectionPool.get(null);
-   }
+  public static void checkScalaExtension() {
+    ActorSystem<?> system = null;
+    DatabasePool.get(system);
+  }
 }
--- akka-docs/src/main/paradox/typed/extending.md
+++ akka-docs/src/main/paradox/typed/extending.md
@@ -8,12 +8,12 @@ pool and accessing it from various places in your application.
 
 You can choose to have your Extension loaded on-demand or at `ActorSystem` creation 
 time through the Akka configuration.
-Details on how to make that happens are below, in the "Loading from Configuration" section.
+Details on how to make that happens are below, in the @ref:[Loading from Configuration](extending.md#loading) section.
 
 @@@ warning
 
 Since an extension is a way to hook into Akka itself, the implementor of the extension needs to
-ensure the thread safety of his/her extension.
+ensure the thread safety and that it is non-blocking.
 
 @@@
 
@@ -27,7 +27,7 @@ Scala
 Java
 :  @@snip [ExtensionDocTest.java](/akka-actor-typed-tests/src/test/java/jdocs/akka/typed/extensions/ExtensionDocTest.java) { #shared-resource }
 
-First create an @apidoc[akka.actor.typed.Extension] , this will be created only once per ActorSystem:
+First create an @apidoc[akka.actor.typed.Extension], this will be created only once per ActorSystem:
 
 Scala
 :  @@snip [ExtensionDocSpec.scala](/akka-actor-typed-tests/src/test/scala/docs/akka/typed/extensions/ExtensionDocSpec.scala) { #extension }
@@ -35,7 +35,8 @@ Scala
 Java
 :  @@snip [ExtensionDocTest.java](/akka-actor-typed-tests/src/test/java/jdocs/akka/typed/extensions/ExtensionDocTest.java) { #extension }
 
-This is the public API of your extension. Internally in this example we instantiate our expensive databsae connection.
+This is the public API of your extension. Internally in this example we instantiate our expensive database connection.
+The `DatabaseConnectionPool` can be looked up this way any number of times and it will return the same instance. 
 
 Then create an @apidoc[akka.actor.typed.ExtensionId] to identify the extension.
 
@@ -53,7 +54,28 @@ Scala
 Java
 :  @@snip [ExtensionDocTest.java](/akka-actor-typed-tests/src/test/java/jdocs/akka/typed/extensions/ExtensionDocTest.java) { #usage  }
 
-The `DatabaseConnectionPool` can be looked up this way any number of times and it will return the same instance. 
+<a id="loading"></a>
+## Loading from configuration
+
+To be able to load extensions from your Akka configuration you must add FQCNs of implementations of the `ExtensionId`
+in the `akka.actor.typed.extensions` section of the config you provide to your `ActorSystem`.
+
+Scala
+:  @@snip [ExtensionDocSpec.scala](/akka-actor-typed-tests/src/test/scala/docs/akka/typed/extensions/ExtensionDocSpec.scala) { #config }
+
+Java
+:   ```ruby
+   akka.actor.typed {
+     extensions = ["jdocs.akka.extensions.ExtensionDocTest$DatabaseConnectionPoolId"]
+   }
+   ```
+     
+
+
+
+
+
+
 
 
 
