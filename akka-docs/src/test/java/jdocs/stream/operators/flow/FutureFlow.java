252bf8c53e77a91dff2802c20d3220fa12b39d22
Johan Andrén <johan@markatta.com>
2019-10-14 09:10:11 +0200

    Formatting

--- akka-docs/src/test/java/jdocs/stream/operators/flow/FutureFlow.java
+++ akka-docs/src/test/java/jdocs/stream/operators/flow/FutureFlow.java
@@ -19,8 +19,7 @@ public class FutureFlow {
   // #base-on-first-element
   CompletionStage<Flow<Integer, String, NotUsed>> processingFlow(int id) {
     return CompletableFuture.completedFuture(
-      Flow.of(Integer.class).map(n -> "id: " + id + " value: " + n)
-    );
+        Flow.of(Integer.class).map(n -> "id: " + id + " value: " + n));
   }
   // #base-on-first-element
 
@@ -28,17 +27,17 @@ public class FutureFlow {
     // #base-on-first-element
 
     Source<String, NotUsed> source =
-      Source.range(1, 10)
-        .prefixAndTail(1)
-        .flatMapConcat((pair) -> {
-          List<Integer> head = pair.first();
-          Source<Integer, NotUsed> tail = pair.second();
+        Source.range(1, 10)
+            .prefixAndTail(1)
+            .flatMapConcat(
+                (pair) -> {
+                  List<Integer> head = pair.first();
+                  Source<Integer, NotUsed> tail = pair.second();
 
-          int id = head.get(0);
+                  int id = head.get(0);
 
-          return tail.via(Flow.completionStageFlow(processingFlow(id)));
-        });
+                  return tail.via(Flow.completionStageFlow(processingFlow(id)));
+                });
     // #base-on-first-element
   }
-
 }
