5a283eeb07c5efa637f4b4e3621ea873f137ebf3
Johan Andrén <johan@markatta.com>
2019-10-16 15:32:39 +0200

    Wrong timeout used for subscription timeout

--- akka-cluster/src/multi-jvm/scala/akka/cluster/StreamRefSpec.scala
+++ akka-cluster/src/multi-jvm/scala/akka/cluster/StreamRefSpec.scala
@@ -39,6 +39,7 @@ object StreamRefSpec extends MultiNodeConfig {
   val third = role("third")
 
   commonConfig(debugConfig(on = false).withFallback(ConfigFactory.parseString("""
+        akka.stream.materializer.stream-ref.subscription-timeout = 10 s
         akka.cluster {
           auto-down-unreachable-after = 1s
         }""")).withFallback(MultiNodeClusterSpec.clusterConfig))
@@ -249,7 +250,7 @@ abstract class StreamRefSpec extends MultiNodeSpec(StreamRefSpec) with MultiNode
         // and it triggered the subscription timeout. Therefore we must wait more than the
         // the subscription timeout for a failure
         val timeout = system.settings.config
-            .getDuration("akka.stream.materializer.subscription-timeout.timeout")
+            .getDuration("akka.stream.materializer.stream-ref.subscription-timeout")
             .asScala + 2.seconds
         streamLifecycle3.expectMsg(timeout, "failed-system-42-tmp")
       }
