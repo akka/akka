a56a53734f0aa85541650214727462641725fc11
Johan Andrén <johan@markatta.com>
2019-10-25 18:05:59 +0200

    Avoid deadlock on concurrent extension with materializer init  (#28046)
    * Avoid deadlock on concurrent extension with materializer init #28037
    
    * Wrap blocking extension await with blocking context

--- akka-stream/src/main/scala/akka/stream/SystemMaterializer.scala
+++ akka-stream/src/main/scala/akka/stream/SystemMaterializer.scala
@@ -11,6 +11,7 @@ import akka.actor.Extension
 import akka.actor.ExtensionId
 import akka.actor.ExtensionIdProvider
 import akka.annotation.InternalApi
+import akka.dispatch.Dispatchers
 import akka.stream.impl.MaterializerGuardian
 
 import scala.concurrent.Await
@@ -52,7 +53,8 @@ final class SystemMaterializer(system: ExtendedActorSystem) extends Extension {
   private val materializerGuardian = system.systemActorOf(
     MaterializerGuardian
       .props(systemMaterializerPromise, materializerSettings)
-      .withDispatcher(materializerSettings.dispatcher)
+      // #28037 run on internal dispatcher to make sure default dispatcher starvation doesn't stop materializer creation
+      .withDispatcher(Dispatchers.InternalDispatcherId)
       .withDeploy(Deploy.local),
     "Materializers")
 
