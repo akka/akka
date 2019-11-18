548b3589f2cf541dc45c076820515343469a8f67
Arnout Engelen <arnout@bzzt.net>
2019-11-18 18:02:44 +0200

    doc: Flow.fold (#27850)

---
+++ akka-docs/src/test/scala/docs/stream/operators/sourceorflow/Fold.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package docs.stream.operators.sourceorflow
+
+//#imports
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.Source
+
+//#imports
+object Fold extends App {
+
+  //#histogram
+  case class Histogram(low: Long = 0, high: Long = 0) {
+    def add(i: Int): Histogram = if (i < 100) copy(low = low + 1) else copy(high = high + 1)
+  }
+  //#histogram
+
+  implicit val sys = ActorSystem()
+
+  //#fold
+  Source(1 to 150).fold(Histogram())((acc, n) => acc.add(n)).runForeach(println)
+
+  // Prints: Histogram(99,51)
+  //#fold
+}
