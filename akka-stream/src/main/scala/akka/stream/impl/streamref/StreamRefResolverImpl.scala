aaf6f1e2d8f8d7b5f16581d595147f47f0b8e1cb
Patrik Nordwall <patrik.nordwall@gmail.com>
2019-11-25 17:20:23 +0100

    backport StreamRefResolver, #28215
    ... for serialization of SourceRef and SinkRef

---
+++ akka-stream/src/main/scala/akka/stream/impl/streamref/StreamRefResolverImpl.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2009-2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.stream.impl.streamref
+
+import akka.actor.ExtendedActorSystem
+import akka.annotation.InternalApi
+import akka.stream.SinkRef
+import akka.stream.SourceRef
+import akka.stream.StreamRefResolver
+
+/**
+ * INTERNAL API
+ */
+@InternalApi final class StreamRefResolverImpl(system: ExtendedActorSystem) extends StreamRefResolver {
+
+  def toSerializationFormat[T](ref: SourceRef[T]): String = ref match {
+    case SourceRefImpl(actorRef) =>
+      actorRef.path.toSerializationFormatWithAddress(system.provider.getDefaultAddress)
+  }
+
+  def toSerializationFormat[T](ref: SinkRef[T]): String = ref match {
+    case SinkRefImpl(actorRef) =>
+      actorRef.path.toSerializationFormatWithAddress(system.provider.getDefaultAddress)
+  }
+
+  def resolveSourceRef[T](serializedSourceRef: String): SourceRef[T] =
+    SourceRefImpl(system.provider.resolveActorRef(serializedSourceRef))
+
+  def resolveSinkRef[T](serializedSinkRef: String): SinkRef[T] =
+    SinkRefImpl(system.provider.resolveActorRef(serializedSinkRef))
+}
