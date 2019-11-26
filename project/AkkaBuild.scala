390496bec599b4332ebd89e6481644675802421e
Arnout Engelen <arnout@bzzt.net>
2019-11-26 11:40:13 +0100

    Allow 'higherKinds' without explicit import
    To follow the 2.13.1 convention

--- project/AkkaBuild.scala
+++ project/AkkaBuild.scala
@@ -121,7 +121,13 @@ object AkkaBuild {
 
   private def allWarnings: Boolean = System.getProperty("akka.allwarnings", "false").toBoolean
 
-  final val DefaultScalacOptions = Seq("-encoding", "UTF-8", "-feature", "-unchecked", "-Xlog-reflective-calls")
+  final val DefaultScalacOptions = Seq(
+    "-encoding", "UTF-8",
+    "-feature",
+    "-unchecked",
+    "-Xlog-reflective-calls",
+    // 'blessed' since 2.13.1
+    "-language:higherKinds")
 
   // -XDignore.symbol.file suppresses sun.misc.Unsafe warnings
   final val DefaultJavacOptions = Seq("-encoding", "UTF-8", "-Xlint:unchecked", "-XDignore.symbol.file")
