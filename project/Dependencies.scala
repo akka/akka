ac40a962bb0c50078d11c63a857aef1b3e898143
Arnout Engelen <arnout@bzzt.net>
2019-11-26 11:51:09 +0100

    Update agrona accordingly

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,8 +18,9 @@ object Dependencies {
   val slf4jVersion = "1.7.29"
   // check agrona version when updating this
   val aeronVersion = "1.24.0"
-  // needs to be inline with the aeron version
-  val agronaVersion = "1.0.8"
+  // needs to be inline with the aeron version, check
+  // https://github.com/real-logic/aeron/blob/1.x.y/build.gradle
+  val agronaVersion = "1.1.0"
   val nettyVersion = "3.10.6.Final"
   val jacksonVersion = "2.10.1"
   val protobufJavaVersion = "3.10.0"
