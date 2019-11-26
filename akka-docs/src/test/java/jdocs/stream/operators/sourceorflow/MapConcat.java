8027f2123b22f23465f71da4389d8ea0abd24b0c
Christopher Batey <christopher.batey@gmail.com>
2019-11-26 18:39:42 +0000

    Example for mapConcat (#28243)
    * Example for mapConcat

---
+++ akka-docs/src/test/java/jdocs/stream/operators/sourceorflow/MapConcat.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package jdocs.stream.operators.sourceorflow;
+
+import akka.actor.ActorSystem;
+import akka.stream.javadsl.Source;
+
+import java.util.Arrays;
+
+public class MapConcat {
+  private static ActorSystem system = null;
+
+  // #map-concat
+  Iterable<Integer> duplicate(int i) {
+    return Arrays.asList(i, i);
+  }
+
+  // #map-concat
+
+  void example() {
+    // #map-concat
+    Source.from(Arrays.asList(1, 2, 3))
+        .mapConcat(i -> duplicate(i))
+        .runForeach(System.out::println, system);
+    // prints:
+    // 1
+    // 1
+    // 2
+    // 2
+    // 3
+    // #map-concat
+  }
+}
