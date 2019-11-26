29afcc9272a74cd77fcbe6a3e18df88fc7f2815b
Patrik Nordwall <patrik.nordwall@gmail.com>
2019-11-26 14:52:16 +0100

    include full source and sink

--- akka-docs/src/test/java/jdocs/stream/operators/SourceOrFlow.java
+++ akka-docs/src/test/java/jdocs/stream/operators/SourceOrFlow.java
@@ -328,25 +328,43 @@ class SourceOrFlow {
     // #take-while
   }
 
-  Source<String, NotUsed> someText() {
-    return Source.from(
-        Arrays.asList(
-            ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt "
-                    + "ut labore et dolore magna aliqua.")
-                .split(" ")));
-  }
-
   void filterExample() {
     // #filter
-    Source<String, NotUsed> words = someText();
+    Source<String, NotUsed> words =
+        Source.from(
+            Arrays.asList(
+                ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt "
+                        + "ut labore et dolore magna aliqua.")
+                    .split(" ")));
+
     Source<String, NotUsed> longWords = words.filter(w -> w.length() > 6);
+
+    longWords.runWith(Sink.foreach(System.out::print), system);
+    // consectetur
+    // adipiscing
+    // eiusmod
+    // tempor
+    // incididunt
     // #filter
   }
 
   void filterNotExample() {
     // #filterNot
-    Source<String, NotUsed> words = someText();
+    Source<String, NotUsed> words =
+        Source.from(
+            Arrays.asList(
+                ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt "
+                        + "ut labore et dolore magna aliqua.")
+                    .split(" ")));
+
     Source<String, NotUsed> longWords = words.filterNot(w -> w.length() <= 5);
+
+    longWords.runWith(Sink.foreach(System.out::print), system);
+    // consectetur
+    // adipiscing
+    // eiusmod
+    // tempor
+    // incididunt
     // #filterNot
   }
 }
--- akka-docs/src/test/scala/docs/stream/operators/sourceorflow/Filter.scala
+++ akka-docs/src/test/scala/docs/stream/operators/sourceorflow/Filter.scala
@@ -5,25 +5,46 @@
 package docs.stream.operators.sourceorflow
 
 import akka.NotUsed
+import akka.actor.ActorSystem
 import akka.stream.scaladsl.Source
 
 object Filter {
-  def someText(): Source[String, NotUsed] =
-    Source(
-      ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt " +
-      "ut labore et dolore magna aliqua.").split(" ").toList)
+
+  implicit val system: ActorSystem = ActorSystem()
 
   def filterExample(): Unit = {
     // #filter
-    val words: Source[String, NotUsed] = someText()
+    val words: Source[String, NotUsed] =
+      Source(
+        ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt " +
+        "ut labore et dolore magna aliqua.").split(" ").toList)
+
     val longWords: Source[String, NotUsed] = words.filter(_.length > 6)
+
+    longWords.runForeach(println)
+    // consectetur
+    // adipiscing
+    // eiusmod
+    // tempor
+    // incididunt
     // #filter
   }
 
   def filterNotExample(): Unit = {
     // #filterNot
-    val words: Source[String, NotUsed] = someText()
+    val words: Source[String, NotUsed] =
+      Source(
+        ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt " +
+        "ut labore et dolore magna aliqua.").split(" ").toList)
+
     val longWords: Source[String, NotUsed] = words.filterNot(_.length <= 5)
+
+    longWords.runForeach(println)
+    // consectetur
+    // adipiscing
+    // eiusmod
+    // tempor
+    // incididunt
     // #filterNot
   }
 }
