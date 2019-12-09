cfcac1b79b0cd6dcb90e13533d10fd45a5374115
Patrik Nordwall <patrik.nordwall@gmail.com>
2019-12-09 12:05:34 +0100

    Serializer for tck JournalPerfSpec.Cmd, #28291
    * was using Java serialization
    * a journal may choose another serialization mechanism than Akka serialization,
      but this is needed as fallback for Akka serialization

--- akka-persistence-tck/src/test/scala/akka/persistence/journal/leveldb/LeveldbJournalNativePerfSpec.scala
+++ akka-persistence-tck/src/test/scala/akka/persistence/journal/leveldb/LeveldbJournalNativePerfSpec.scala
@@ -4,21 +4,28 @@
 
 package akka.persistence.journal.leveldb
 
-import akka.persistence.journal.{ JournalPerfSpec }
-import akka.persistence.{ PersistenceSpec, PluginCleanup }
-import org.scalatest.DoNotDiscover
+import akka.persistence.journal.JournalPerfSpec
+import akka.persistence.PersistenceSpec
+import akka.persistence.PluginCleanup
 
-@DoNotDiscover // because only checking that compilation is OK with JournalPerfSpec
 class LeveldbJournalNativePerfSpec
     extends JournalPerfSpec(
       config = PersistenceSpec.config(
         "leveldb",
         "LeveldbJournalNativePerfSpec",
-        extraConfig = Some("akka.persistence.journal.leveldb.native = on")))
+        extraConfig = Some("""
+        akka.persistence.journal.leveldb.native = on
+        akka.actor.allow-java-serialization = off
+        akka.actor.warn-about-java-serializer-usage = on
+        """)))
     with PluginCleanup {
 
   override def supportsRejectingNonSerializableObjects = true
 
   override def supportsSerialization = true
+
+  override def measurementIterations: Int = 3
+
+  override def eventsCount: Int = 1000
 
 }
