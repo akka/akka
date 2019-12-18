ee200e2b3b81aad321f66cb84901c34b68ae8f4f
Eyal Farago-Hagag <efarago@salesforce.com>
2019-12-18 08:51:18 +0200

    akka-28364: fix DslConsistencySpec test failures.

--- akka-stream/src/main/scala/akka/stream/javadsl/SubSource.scala
+++ akka-stream/src/main/scala/akka/stream/javadsl/SubSource.scala
@@ -1358,6 +1358,33 @@ class SubSource[Out, Mat](
     new SubSource(delegate.prefixAndTail(n).map { case (taken, tail) => akka.japi.Pair(taken.asJava, tail.asJava) })
 
   /**
+   * Takes up to `n` elements from the stream (less than `n` only if the upstream completes before emitting `n` elements),
+   * then apply f on these elements in order to obtain a flow, this flow is then materialized and the rest of the input is processed by this flow (similar to via ).
+   * This method returns a flow consuming the rest of the stream producing the materialized flow's output.
+   *
+   * '''Emits when''' the materialized flow emits.
+   *  Notice the first `n` elements are buffered internally before materializing the flow, This flow will then be materialized and connected to the rest of the upstream - producing elements at its own discretion (might 'swallow' or multiply elements).
+   *
+   * '''Backpressures when''' downstream backpressures
+   *
+   * '''Completes when''' the materialized flow completes.
+   *  If upstream completes before producing `n` elements, `f` will be applied with the provided elements,
+   *  the resulting flow will be materialized and signalled for upstream completion, it can then cancel/complete at its own discretion.
+   *
+   * '''Cancels when''' upstream cancels, application of `f` fails, materialization of the resulting flow fails or the materialized flow cancels.
+   *  Notice the materialized flow may apply its own logic in case of upstream or downstream cancellations.
+   *
+   *  @param n the number of elements to accumulate before materializing the downstream flow.
+   *  @param f a function that produces the downstream flow based on the upstream's prefix.
+   **/
+  def prefixAndDownstream[Out2, Mat2](
+                                       n: Int,
+                                       f: function.Function[java.lang.Iterable[Out], javadsl.Flow[Out, Out2, Mat2]]): javadsl.SubSource[Out2, Mat] = {
+    val newDelegate = delegate.prefixAndDownstream(n)(seq => f(seq.asJava).asScala)
+    new javadsl.SubSource(newDelegate)
+  }
+
+  /**
    * Transform each input element into a `Source` of output elements that is
    * then flattened into the output stream by concatenation,
    * fully consuming one Source after the other.
