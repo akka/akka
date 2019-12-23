4e25b0ef1437c154a65c536582c7373e7ca1f1d4
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2019-12-23 17:19:55 +0000

    doc: fix ScalaFiddle integration for Akka 2.6

--- akka-docs/src/main/paradox/assets/js/scalafiddle.js
+++ akka-docs/src/main/paradox/assets/js/scalafiddle.js
@@ -1,8 +1,8 @@
 window.scalaFiddleTemplates = {
   "Akka": {
-    pre: "// $FiddleDependency org.akka-js %%% akkajsactor % 1.2.5.14 \n" +
-    "// $FiddleDependency org.akka-js %%% akkajsactorstream % 1.2.5.14 \n" +
-    "// $FiddleDependency org.akka-js %%% akkajsactortyped % 1.2.5.14 \n",
+    pre: "// $FiddleDependency org.akka-js %%% akkajsactor % 2.2.6.1 \n" +
+    "// $FiddleDependency org.akka-js %%% akkajsactorstream % 2.2.6.1 \n" +
+    "// $FiddleDependency org.akka-js %%% akkajsactortyped % 2.2.6.1 \n",
     post: ""
   }
 }
