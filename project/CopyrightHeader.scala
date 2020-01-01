5123b837fcfa347d6b6be399283b83d64ceb455f
Helena Edelson <helena.edelson@lightbend.com>
2020-01-01 10:41:39 -0800

    Update all copyright headers to 2020 after new year's #27881

--- project/CopyrightHeader.scala
+++ project/CopyrightHeader.scala
@@ -1,55 +1,46 @@
 /*
- * Copyright (C) 2018-2019 Lightbend Inc. <https://www.lightbend.com>
+ * Copyright (C) 2018-2020 Lightbend Inc. <https://www.lightbend.com>
  */
 
 package akka
 
 import akka.AkkaValidatePullRequest.additionalTasks
 import de.heikoseeberger.sbtheader.HeaderPlugin.autoImport._
-import de.heikoseeberger.sbtheader.{CommentCreator, HeaderPlugin}
+import de.heikoseeberger.sbtheader.{ CommentCreator, HeaderPlugin }
 import com.typesafe.sbt.MultiJvmPlugin.MultiJvmKeys._
 import sbt.Keys._
-import sbt.{Def, _}
+import sbt.{ Def, _ }
 
 trait CopyrightHeader extends AutoPlugin {
 
-  override def requires:Plugins = HeaderPlugin
+  override def requires: Plugins = HeaderPlugin
 
   override def trigger: PluginTrigger = allRequirements
 
   protected def headerMappingSettings: Seq[Def.Setting[_]] =
     Seq(Compile, Test, MultiJvm).flatMap { config =>
-    inConfig(config)(
-      Seq(
-        headerLicense := Some(HeaderLicense.Custom(headerFor(CurrentYear))),
-        headerMappings := headerMappings.value ++ Map(
-          HeaderFileType.scala -> cStyleComment,
-          HeaderFileType.java -> cStyleComment,
-          HeaderFileType("template") -> cStyleComment
-        )
-      )
-    )
-  }
-
-  override def projectSettings: Seq[Def.Setting[_]] = Def.settings(
-    headerMappingSettings,
-    additional
-  )
-
-  def additional: Seq[Def.Setting[_]] = Def.settings(
-    (compile in Compile) := {
-      (headerCreate in Compile).value
-      (compile in Compile).value
-    },
-    (compile in Test) := {
-      (headerCreate in Test).value
-      (compile in Test).value
+      inConfig(config)(
+        Seq(
+          headerLicense := Some(HeaderLicense.Custom(headerFor(CurrentYear))),
+          headerMappings := headerMappings.value ++ Map(
+              HeaderFileType.scala -> cStyleComment,
+              HeaderFileType.java -> cStyleComment,
+              HeaderFileType("template") -> cStyleComment)))
     }
-  )
+
+  override def projectSettings: Seq[Def.Setting[_]] = Def.settings(headerMappingSettings, additional)
+
+  def additional: Seq[Def.Setting[_]] = Def.settings((compile in Compile) := {
+    (headerCreate in Compile).value
+    (compile in Compile).value
+  }, (compile in Test) := {
+    (headerCreate in Test).value
+    (compile in Test).value
+  })
 
   // We hard-code this so PR's created in year X will not suddenly fail in X+1.
   // Of course we should remember to update it early in the year.
-  val CurrentYear = "2019"
+  val CurrentYear = "2020"
   val CopyrightPattern = "Copyright \\([Cc]\\) (\\d{4}([-–]\\d{4})?) (Lightbend|Typesafe) Inc. <.*>".r
   val CopyrightHeaderPattern = s"(?s).*${CopyrightPattern}.*".r
 
@@ -59,44 +50,45 @@ trait CopyrightHeader extends AutoPlugin {
   val cStyleComment = HeaderCommentStyle.cStyleBlockComment.copy(commentCreator = new CommentCreator() {
 
     def updateLightbendHeader(header: String): String = header match {
-      case CopyrightHeaderPattern(years, null, _)     =>
+      case CopyrightHeaderPattern(years, null, _) =>
         if (years != CurrentYear)
           CopyrightPattern.replaceFirstIn(header, headerFor(years + "-" + CurrentYear))
         else
           CopyrightPattern.replaceFirstIn(header, headerFor(years))
       case CopyrightHeaderPattern(years, endYears, _) =>
         CopyrightPattern.replaceFirstIn(header, headerFor(years.replace(endYears, "-" + CurrentYear)))
-      case _                                          =>
+      case _ =>
         header
     }
 
-    def parseStartAndEndYear(header:String):Option[(String,Option[String])] = header match {
-      case CopyrightHeaderPattern(years, null, _)     =>
-        Some((years,None))
+    def parseStartAndEndYear(header: String): Option[(String, Option[String])] = header match {
+      case CopyrightHeaderPattern(years, null, _) =>
+        Some((years, None))
       case CopyrightHeaderPattern(years, endYears, _) =>
-        Some((years,Some(endYears)))
-      case _                                          =>
+        Some((years, Some(endYears)))
+      case _ =>
         None
     }
 
     override def apply(text: String, existingText: Option[String]): String = {
       val formatted = existingText match {
-        case Some(existedText) => parseStartAndEndYear(existedText) match {
-          case Some((years,None)) =>
-            if (years != CurrentYear){
-              val header = headerFor(years + "-" + CurrentYear)
-              HeaderCommentStyle.cStyleBlockComment.commentCreator(header,existingText)
-            }else {
-              HeaderCommentStyle.cStyleBlockComment.commentCreator(headerFor(CurrentYear),existingText)
-            }
-          case Some((years,Some(endYears))) =>
-            val header = headerFor(years.replace(endYears, "-" + CurrentYear))
-            HeaderCommentStyle.cStyleBlockComment.commentCreator(header,existingText)
-          case None =>
-            existedText
-        }
+        case Some(existedText) =>
+          parseStartAndEndYear(existedText) match {
+            case Some((years, None)) =>
+              if (years != CurrentYear) {
+                val header = headerFor(years + "-" + CurrentYear)
+                HeaderCommentStyle.cStyleBlockComment.commentCreator(header, existingText)
+              } else {
+                HeaderCommentStyle.cStyleBlockComment.commentCreator(headerFor(CurrentYear), existingText)
+              }
+            case Some((years, Some(endYears))) =>
+              val header = headerFor(years.replace(endYears, "-" + CurrentYear))
+              HeaderCommentStyle.cStyleBlockComment.commentCreator(header, existingText)
+            case None =>
+              existedText
+          }
         case None =>
-          HeaderCommentStyle.cStyleBlockComment.commentCreator(text,existingText)
+          HeaderCommentStyle.cStyleBlockComment.commentCreator(text, existingText)
       }
       formatted.trim
     }
@@ -107,8 +99,6 @@ object CopyrightHeader extends CopyrightHeader
 
 object CopyrightHeaderInPr extends CopyrightHeader {
 
-  override val additional = Def.settings(
-    additionalTasks += headerCheck in Compile,
-    additionalTasks += headerCheck in Test
-  )
+  override val additional =
+    Def.settings(additionalTasks += headerCheck in Compile, additionalTasks += headerCheck in Test)
 }
