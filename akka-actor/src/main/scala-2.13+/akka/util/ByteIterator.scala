b13dd85f3d0d3aa2cae4ecdb4162af79b0d73ec9
Johan Andrén <johan@markatta.com>
2020-01-14 10:51:37 +0100

    Fix problems with byte string copy to array 2.13 (#28468)
    * copyToArray unbroken on 2.13 #28465
    * Skip discipline flags in console (driving me crazy!)

--- akka-actor/src/main/scala-2.13+/akka/util/ByteIterator.scala
+++ akka-actor/src/main/scala-2.13+/akka/util/ByteIterator.scala
@@ -309,7 +309,7 @@ object ByteIterator {
     final override def copyToArray[B >: Byte](xs: Array[B], start: Int, len: Int): Int = {
       var pos = start
       var rest = len
-      while ((rest > 0) && !iterators.isEmpty) {
+      while ((rest > 0) && !iterators.isEmpty && pos < xs.length) {
         val n = 0 max ((xs.length - pos) min current.len min rest)
         current.copyToArray(xs, pos, n)
         pos += n
