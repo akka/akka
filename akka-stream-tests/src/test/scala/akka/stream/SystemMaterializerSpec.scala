50a4ebab88219166214a4348b48106323a65d74a
Arnout Engelen <arnout@bzzt.net>
2020-01-15 17:27:05 +0100

    Don't initialize eagerly

--- akka-stream-tests/src/test/scala/akka/stream/SystemMaterializerSpec.scala
+++ akka-stream-tests/src/test/scala/akka/stream/SystemMaterializerSpec.scala
@@ -4,20 +4,29 @@
 
 package akka.stream
 
+import scala.concurrent.Future
+
+import akka.actor.ActorSystem
 import akka.stream.scaladsl.Keep
 import akka.stream.scaladsl.Sink
 import akka.stream.scaladsl.Source
 import akka.stream.testkit.StreamSpec
+
 import org.scalatest.concurrent.ScalaFutures
 
-import scala.concurrent.Future
-
-class SystemMaterializerEagerStartupSpec extends StreamSpec {
+class SystemMaterializerEagerStartupSpec extends StreamSpec with ScalaFutures {
 
   "The SystemMaterializer" must {
 
     "be eagerly started on system startup" in {
-      system.hasExtension(SystemMaterializer.lookup) should ===(true)
+      system.hasExtension(SystemMaterializer.lookup) should ===(false)
+    }
+
+    "provide a materializer when invoked" in {
+      implicit val mat: Materializer = SystemMaterializer(system).materializer
+      Source.single("asdf")
+        .runWith(Sink.seq)
+        .futureValue should be(Seq("asdf"))
     }
   }
 
