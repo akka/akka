124b711169869f7aa38580bcd0fffc3e8eaac8dc
Bartlomiej Szwej <bartek@moia.io>
2020-02-18 20:27:13 +0100

    Allow to schedule multiple identical tasks

--- akka-testkit/src/test/scala/akka/testkit/ExplicitlyTriggeredSchedulerSpec.scala
+++ akka-testkit/src/test/scala/akka/testkit/ExplicitlyTriggeredSchedulerSpec.scala
@@ -14,6 +14,7 @@ class ExplicitlyTriggeredSchedulerSpec extends AkkaSpec {
 
     "execute a scheduled task" in new TestScope {
       scheduler.schedule(0.seconds, 5.seconds, runnable)(system.dispatcher)
+
       scheduler.timePasses(12.seconds)
 
       counter.get() shouldBe 3
@@ -21,9 +22,28 @@ class ExplicitlyTriggeredSchedulerSpec extends AkkaSpec {
 
     "execute a scheduled task with initial delay" in new TestScope {
       scheduler.schedule(5.seconds, 5.seconds, runnable)(system.dispatcher)
+
       scheduler.timePasses(12.seconds)
 
       counter.get() shouldBe 2
+    }
+
+    "execute a scheduled task only once" in new TestScope {
+      scheduler.scheduleOnce(5.seconds, runnable)(system.dispatcher)
+
+      scheduler.timePasses(100.seconds)
+
+      counter.get() shouldBe 1
+    }
+
+    "schedule multiple identical tasks" in new TestScope {
+      scheduler.scheduleOnce(1.seconds, runnable)(system.dispatcher)
+      scheduler.scheduleOnce(1.seconds, runnable)(system.dispatcher)
+      scheduler.scheduleOnce(1.seconds, runnable)(system.dispatcher)
+
+      scheduler.timePasses(2.seconds)
+
+      counter.get() shouldBe 3
     }
 
     "cancel scheduled task" in new TestScope {
@@ -39,6 +59,30 @@ class ExplicitlyTriggeredSchedulerSpec extends AkkaSpec {
       counter.get() shouldBe 1
     }
 
+    "cancel one out of many scheduled tasks" in new TestScope {
+      scheduler.schedule(5.seconds, 5.seconds, runnable)(system.dispatcher)
+      val task = scheduler.schedule(5.seconds, 5.seconds, runnable)(system.dispatcher)
+      scheduler.schedule(5.seconds, 5.seconds, runnable)(system.dispatcher)
+
+      scheduler.timePasses(7.seconds) // 7s
+      counter.get() shouldBe 3
+
+      val cancellationResult = task.cancel()
+      cancellationResult shouldBe true
+
+      scheduler.timePasses(4.seconds) // 11s
+      counter.get() shouldBe 5
+    }
+
+    "not execute task if cancelled immediately" in new TestScope {
+      val task = scheduler.schedule(5.seconds, 5.seconds, runnable)(system.dispatcher)
+      task.cancel()
+
+      scheduler.timePasses(7.seconds)
+
+      counter.get() shouldBe 0
+    }
+
     "allow to move in time many times" in new TestScope {
       scheduler.schedule(5.seconds, 5.seconds, runnable)(system.dispatcher)
 
@@ -51,18 +95,11 @@ class ExplicitlyTriggeredSchedulerSpec extends AkkaSpec {
       scheduler.timePasses(10.seconds) // 18s
       counter.get() shouldBe 3
     }
-
-    "execute a scheduled task only once" in new TestScope {
-      scheduler.scheduleOnce(5.seconds, runnable)(system.dispatcher)
-      scheduler.timePasses(100.seconds)
-
-      counter.get() shouldBe 1
-    }
   }
 
   trait TestScope {
     val counter = new AtomicInteger()
-    def runnable: Runnable = () => counter.incrementAndGet()
+    val runnable: Runnable = () => counter.incrementAndGet()
     val scheduler = new ExplicitlyTriggeredScheduler(config = null, log = log, tf = null)
   }
 
