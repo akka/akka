6fe2f66adce8e8321e6155a4aebbb38bdb379ca0
Helena Edelson <helena@users.noreply.github.com>
2020-02-27 12:05:55 -0800

    Multi node test hardening for Sharding #27749 (#28631)

--- akka-cluster-sharding/src/multi-jvm/scala/akka/cluster/sharding/ClusterShardingQueriesSpec.scala
+++ akka-cluster-sharding/src/multi-jvm/scala/akka/cluster/sharding/ClusterShardingQueriesSpec.scala
@@ -4,66 +4,43 @@
 
 package akka.cluster.sharding
 
-import akka.actor.{ Actor, ActorLogging, ActorRef, Props }
-import akka.cluster.MultiNodeClusterSpec
-import akka.remote.testkit.{ MultiNodeConfig, MultiNodeSpec }
-import akka.serialization.jackson.CborSerializable
+import scala.concurrent.duration._
+
+import akka.actor.Props
 import akka.testkit.TestProbe
 import com.typesafe.config.ConfigFactory
 import org.scalatest.concurrent.ScalaFutures
 
-import scala.concurrent.duration._
-
 object ClusterShardingQueriesSpec {
-  case class Ping(id: Long) extends CborSerializable
-  case object Pong extends CborSerializable
-
-  class EntityActor extends Actor with ActorLogging {
-    def receive: Receive = {
-      case _: Ping => sender() ! Pong
-    }
-  }
+  import MultiNodeClusterShardingSpec.PingPongActor
 
   val extractEntityId: ShardRegion.ExtractEntityId = {
-    case msg @ Ping(id) => (id.toString, msg)
+    case msg @ PingPongActor.Ping(id) => (id.toString, msg)
   }
 
   val numberOfShards = 6
 
   val extractShardId: ShardRegion.ExtractShardId = {
-    case Ping(id) => (id % numberOfShards).toString
+    case PingPongActor.Ping(id) => (id % numberOfShards).toString
   }
 
   val shardTypeName = "DatatypeA"
 }
 
-object ClusterShardingQueriesSpecConfig extends MultiNodeConfig {
+object ClusterShardingQueriesSpecConfig
+    extends MultiNodeClusterShardingConfig(additionalConfig = """
+        akka.log-dead-letters-during-shutdown = off
+        akka.cluster.sharding {
+          shard-region-query-timeout = 2ms
+          updating-state-timeout = 2s
+          waiting-for-state-timeout = 2s
+        }
+        """) {
 
   val controller = role("controller")
   val busy = role("busy")
   val second = role("second")
   val third = role("third")
-
-  commonConfig(
-    debugConfig(on = false)
-      .withFallback(ConfigFactory.parseString("""
-    akka.loglevel = INFO
-    akka.actor.provider = "cluster"
-    akka.remote.classic.log-remote-lifecycle-events = off
-    akka.log-dead-letters-during-shutdown = off
-    akka.cluster.downing-provider-class = akka.cluster.testkit.AutoDowning
-    akka.cluster.testkit.auto-down-unreachable-after = 0s
-    akka.cluster.sharding {
-      state-store-mode = "ddata"
-      shard-region-query-timeout = 2ms
-      updating-state-timeout = 2s
-      waiting-for-state-timeout = 2s
-    }
-    akka.cluster.sharding.distributed-data.durable.lmdb {
-      dir = target/ClusterShardingGetStatsSpec/sharding-ddata
-      map-size = 10 MiB
-    }
-    """).withFallback(MultiNodeClusterSpec.clusterConfig)))
 
   val shardRoles = ConfigFactory.parseString("""akka.cluster.roles=["shard"]""")
 
@@ -79,29 +56,12 @@ class ClusterShardingQueriesSpecMultiJvmNode3 extends ClusterShardingQueriesSpec
 class ClusterShardingQueriesSpecMultiJvmNode4 extends ClusterShardingQueriesSpec
 
 abstract class ClusterShardingQueriesSpec
-    extends MultiNodeSpec(ClusterShardingQueriesSpecConfig)
-    with MultiNodeClusterSpec
+    extends MultiNodeClusterShardingSpec(ClusterShardingQueriesSpecConfig)
     with ScalaFutures {
 
   import ClusterShardingQueriesSpec._
   import ClusterShardingQueriesSpecConfig._
-
-  def startShard(): ActorRef = {
-    ClusterSharding(system).start(
-      typeName = shardTypeName,
-      entityProps = Props(new EntityActor),
-      settings = ClusterShardingSettings(system).withRole("shard"),
-      extractEntityId = extractEntityId,
-      extractShardId = extractShardId)
-  }
-
-  def startProxy(): ActorRef = {
-    ClusterSharding(system).startProxy(
-      typeName = shardTypeName,
-      role = Some("shard"),
-      extractEntityId = extractEntityId,
-      extractShardId = extractShardId)
-  }
+  import MultiNodeClusterShardingSpec.PingPongActor
 
   lazy val region = ClusterSharding(system).shardRegion(shardTypeName)
 
@@ -111,11 +71,22 @@ abstract class ClusterShardingQueriesSpec
       awaitClusterUp(controller, busy, second, third)
 
       runOn(controller) {
-        startProxy()
+        startProxy(
+          system,
+          typeName = shardTypeName,
+          role = Some("shard"),
+          extractEntityId = extractEntityId,
+          extractShardId = extractShardId)
       }
 
       runOn(busy, second, third) {
-        startShard()
+        startSharding(
+          system,
+          typeName = shardTypeName,
+          entityProps = Props(new PingPongActor),
+          settings = settings.withRole("shard"),
+          extractEntityId = extractEntityId,
+          extractShardId = extractShardId)
       }
 
       enterBarrier("sharding started")
@@ -126,9 +97,9 @@ abstract class ClusterShardingQueriesSpec
         within(10.seconds) {
           awaitAssert {
             val pingProbe = TestProbe()
-            (0 to 20).foreach(n => region.tell(Ping(n), pingProbe.ref))
+            (0 to 20).foreach(n => region.tell(PingPongActor.Ping(n), pingProbe.ref))
             pingProbe.receiveWhile(messages = 20) {
-              case Pong => ()
+              case PingPongActor.Pong => ()
             }
           }
         }
