c3ff4e2faf11821dfbf173bc72c06d4857659b90
Eyal Farago-Hagag <efarago@salesforce.com>
2020-03-21 21:19:31 +0200

    akka-28016__futureFlow: fmt

--- akka-stream-tests/src/test/scala/akka/stream/scaladsl/FlowFutureFlowSpec.scala
+++ akka-stream-tests/src/test/scala/akka/stream/scaladsl/FlowFutureFlowSpec.scala
@@ -10,133 +10,131 @@ import akka.stream.testkit.StreamSpec
 import akka.stream.testkit.Utils.TE
 import akka.stream.testkit.scaladsl.StreamTestKit.assertAllStagesStopped
 
-import scala.concurrent.{Future, Promise}
+import scala.concurrent.{ Future, Promise }
 
 class FlowFutureFlowSpec extends StreamSpec {
   def src10(i: Int = 0) = Source(i until (i + 10))
-  def src10WithFailure(i : Int = 0)(failOn : Int) = src10(i).map{
+  def src10WithFailure(i: Int = 0)(failOn: Int) = src10(i).map {
     case `failOn` => throw TE(s"fail on $failOn")
-    case x => x
+    case x        => x
   }
 
   "a futureFlow" must {
     "work in the simple case with a completed future" in assertAllStagesStopped {
       val (fNotUsed, fSeq) = src10()
-        .viaMat{
-          Flow.futureFlow{
+        .viaMat {
+          Flow.futureFlow {
             Future.successful(Flow[Int])
           }
-        } (Keep.right)
+        }(Keep.right)
         .toMat(Sink.seq)(Keep.both)
         .run()
 
-      fNotUsed.futureValue should be (NotUsed)
-      fSeq.futureValue should equal (0 until 10)
+      fNotUsed.futureValue should be(NotUsed)
+      fSeq.futureValue should equal(0 until 10)
     }
 
     "work in the simple case with a late future" in assertAllStagesStopped {
       val prFlow = Promise[Flow[Int, Int, NotUsed]]
       val (fNotUsed, fSeq) = src10()
-        .viaMat{
+        .viaMat {
           Flow.futureFlow(prFlow.future)
-        } (Keep.right)
+        }(Keep.right)
         .toMat(Sink.seq)(Keep.both)
         .run()
 
-      fNotUsed.value should be (empty)
-      fSeq.value should be (empty)
+      fNotUsed.value should be(empty)
+      fSeq.value should be(empty)
 
       prFlow.success(Flow[Int])
 
-      fNotUsed.futureValue should be (NotUsed)
-      fSeq.futureValue should equal (0 until 10)
+      fNotUsed.futureValue should be(NotUsed)
+      fSeq.futureValue should equal(0 until 10)
     }
 
     "fail properly when future is a completed failed future" in assertAllStagesStopped {
       val (fNotUsed, fSeq) = src10()
-        .viaMat{
-          Flow.futureFlow{
+        .viaMat {
+          Flow.futureFlow {
             Future.failed[Flow[Int, Int, NotUsed]](TE("damn!"))
           }
-        } (Keep.right)
+        }(Keep.right)
         .toMat(Sink.seq)(Keep.both)
         .run()
 
-      fNotUsed.failed.futureValue should be (a[NeverMaterializedException])
-      fNotUsed.failed.futureValue.getCause should equal (TE("damn!"))
+      fNotUsed.failed.futureValue should be(a[NeverMaterializedException])
+      fNotUsed.failed.futureValue.getCause should equal(TE("damn!"))
 
-      fSeq.failed.futureValue should equal (TE("damn!"))
+      fSeq.failed.futureValue should equal(TE("damn!"))
 
     }
 
     "fail properly when future is late completed failed future" in assertAllStagesStopped {
       val prFlow = Promise[Flow[Int, Int, NotUsed]]
       val (fNotUsed, fSeq) = src10()
-        .viaMat{
+        .viaMat {
           Flow.futureFlow(prFlow.future)
-        } (Keep.right)
+        }(Keep.right)
         .toMat(Sink.seq)(Keep.both)
         .run()
 
-      fNotUsed.value should be (empty)
-      fSeq.value should be (empty)
+      fNotUsed.value should be(empty)
+      fSeq.value should be(empty)
 
       prFlow.failure(TE("damn!"))
 
-      fNotUsed.failed.futureValue should be (a[NeverMaterializedException])
-      fNotUsed.failed.futureValue.getCause should equal (TE("damn!"))
+      fNotUsed.failed.futureValue should be(a[NeverMaterializedException])
+      fNotUsed.failed.futureValue.getCause should equal(TE("damn!"))
 
-      fSeq.failed.futureValue should equal (TE("damn!"))
+      fSeq.failed.futureValue should equal(TE("damn!"))
 
     }
 
     "handle upstream failure when future is pre-completed" in assertAllStagesStopped {
       val (fNotUsed, fSeq) = src10WithFailure()(5)
-        .viaMat{
-          Flow.futureFlow{
-            Future.successful{
-              Flow[Int]
-                .recover{
-                  case TE("fail on 5") => 99
-                }
+        .viaMat {
+          Flow.futureFlow {
+            Future.successful {
+              Flow[Int].recover {
+                case TE("fail on 5") => 99
+              }
             }
           }
-        } (Keep.right)
+        }(Keep.right)
         .toMat(Sink.seq)(Keep.both)
         .run()
 
-      fNotUsed.futureValue should be (NotUsed)
-      fSeq.futureValue should equal (List(0,1,2,3,4,99))
+      fNotUsed.futureValue should be(NotUsed)
+      fSeq.futureValue should equal(List(0, 1, 2, 3, 4, 99))
     }
 
     "handle upstream failure when future is late-completed" in assertAllStagesStopped {
       val prFlow = Promise[Flow[Int, Int, NotUsed]]
       val (fNotUsed, fSeq) = src10WithFailure()(5)
-        .viaMat{
+        .viaMat {
           Flow.futureFlow(prFlow.future)
-        } (Keep.right)
+        }(Keep.right)
         .toMat(Sink.seq)(Keep.both)
         .run()
 
-      fNotUsed.value should be (empty)
-      fSeq.value should be (empty)
+      fNotUsed.value should be(empty)
+      fSeq.value should be(empty)
 
-      prFlow.success{
-        Flow[Int]
-          .recover{
-            case TE("fail on 5") => 99
-          }
+      prFlow.success {
+        Flow[Int].recover {
+          case TE("fail on 5") => 99
+        }
       }
 
-      fNotUsed.futureValue should be (NotUsed)
-      fSeq.futureValue should equal (List(0,1,2,3,4,99))
+      fNotUsed.futureValue should be(NotUsed)
+      fSeq.futureValue should equal(List(0, 1, 2, 3, 4, 99))
     }
 
     "handle early upstream error when flow future is pre-completed" in assertAllStagesStopped {
       val (fNotUsed, fSeq) = Source
         .failed(TE("not today my friend"))
-        .viaMat{
-          Flow.futureFlow{
+        .viaMat {
+          Flow.futureFlow {
             Future.successful {
               Flow[Int]
                 .recover {
@@ -145,12 +143,12 @@ class FlowFutureFlowSpec extends StreamSpec {
                 .concat(src10())
             }
           }
-        } (Keep.right)
+        }(Keep.right)
         .toMat(Sink.seq)(Keep.both)
         .run()
 
-      fNotUsed.futureValue should be (NotUsed)
-      fSeq.futureValue should equal (99 +: (0 until 10))
+      fNotUsed.futureValue should be(NotUsed)
+      fSeq.futureValue should equal(99 +: (0 until 10))
 
     }
 
@@ -158,16 +156,16 @@ class FlowFutureFlowSpec extends StreamSpec {
       val prFlow = Promise[Flow[Int, Int, NotUsed]]
       val (fNotUsed, fSeq) = Source
         .failed(TE("not today my friend"))
-        .viaMat{
+        .viaMat {
           Flow.futureFlow(prFlow.future)
-        } (Keep.right)
+        }(Keep.right)
         .toMat(Sink.seq)(Keep.both)
         .run()
 
-      fNotUsed.value should be (empty)
-      fSeq.value should be (empty)
+      fNotUsed.value should be(empty)
+      fSeq.value should be(empty)
 
-      prFlow.success{
+      prFlow.success {
         Flow[Int]
           .recover {
             case TE("not today my friend") => 99
@@ -175,89 +173,89 @@ class FlowFutureFlowSpec extends StreamSpec {
           .concat(src10())
       }
 
-      fNotUsed.futureValue should be (NotUsed)
-      fSeq.futureValue should equal (99 +: (0 until 10))
+      fNotUsed.futureValue should be(NotUsed)
+      fSeq.futureValue should equal(99 +: (0 until 10))
 
     }
 
     "handle closed downstream when flow future is pre completed" in assertAllStagesStopped {
       val (fSeq1, fSeq2) = src10()
-        .viaMat{
-          Flow.futureFlow{
-            Future.successful{
-              Flow[Int]
-                .alsoToMat(Sink.seq)(Keep.right)
+        .viaMat {
+          Flow.futureFlow {
+            Future.successful {
+              Flow[Int].alsoToMat(Sink.seq)(Keep.right)
             }
           }
-        }(Keep.right).mapMaterializedValue(_.flatten)
+        }(Keep.right)
+        .mapMaterializedValue(_.flatten)
         .take(0)
         .toMat(Sink.seq)(Keep.both)
         .run()
 
-      fSeq1.futureValue should be (empty)
-      fSeq2.futureValue should be (empty)
+      fSeq1.futureValue should be(empty)
+      fSeq2.futureValue should be(empty)
 
     }
 
     "handle closed downstream when flow future is late completed" in assertAllStagesStopped {
       val prFlow = Promise[Flow[Int, Int, Future[collection.immutable.Seq[Int]]]]
       val (fSeq1, fSeq2) = src10()
-        .viaMat{
+        .viaMat {
           Flow.futureFlow(prFlow.future)
-        }(Keep.right).mapMaterializedValue(_.flatten)
+        }(Keep.right)
+        .mapMaterializedValue(_.flatten)
         .take(0)
         .toMat(Sink.seq)(Keep.both)
         .run()
 
-      fSeq1.value should be (empty)
-      fSeq2.value should be (empty)
+      fSeq1.value should be(empty)
+      fSeq2.value should be(empty)
 
-      prFlow.success{
-        Flow[Int]
-          .alsoToMat(Sink.seq)(Keep.right)
+      prFlow.success {
+        Flow[Int].alsoToMat(Sink.seq)(Keep.right)
       }
 
-      fSeq1.futureValue should be (empty)
-      fSeq2.futureValue should be (empty)
+      fSeq1.futureValue should be(empty)
+      fSeq2.futureValue should be(empty)
     }
 
     "handle early upstream completion when flow future is pre-completed" in assertAllStagesStopped {
-      val (fNotUsed, fSeq) = Source.empty[Int]
-        .viaMat{
-          Flow.futureFlow{
-            Future.successful{
-              Flow[Int].orElse(Source single 99)
+      val (fNotUsed, fSeq) = Source
+        .empty[Int]
+        .viaMat {
+          Flow.futureFlow {
+            Future.successful {
+              Flow[Int].orElse(Source.single(99))
             }
           }
         }(Keep.right)
         .toMat(Sink.seq)(Keep.both)
         .run()
 
-      fNotUsed.futureValue should be (NotUsed)
-      fSeq.futureValue should equal (99 :: Nil)
+      fNotUsed.futureValue should be(NotUsed)
+      fSeq.futureValue should equal(99 :: Nil)
     }
 
     "handle early upstream completion when flow future is late-completed" in assertAllStagesStopped {
       val prFlow = Promise[Flow[Int, Int, NotUsed]]
-      val (fNotUsed, fSeq) = Source.empty[Int]
-        .viaMat{
+      val (fNotUsed, fSeq) = Source
+        .empty[Int]
+        .viaMat {
           Flow.futureFlow(prFlow.future)
         }(Keep.right)
         .toMat(Sink.seq)(Keep.both)
         .run()
 
-      fNotUsed.value should be (empty)
-      fSeq.value should be (empty)
+      fNotUsed.value should be(empty)
+      fSeq.value should be(empty)
 
-      prFlow.success{
-        Flow[Int].orElse(Source single 99)
+      prFlow.success {
+        Flow[Int].orElse(Source.single(99))
       }
 
-      fNotUsed.futureValue should be (NotUsed)
-      fSeq.futureValue should equal (99 :: Nil)
+      fNotUsed.futureValue should be(NotUsed)
+      fSeq.futureValue should equal(99 :: Nil)
     }
   }
-
-
 
 }
