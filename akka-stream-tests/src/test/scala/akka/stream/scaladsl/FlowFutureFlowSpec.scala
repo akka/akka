20a005bc7d35ae841635e3e794d7087fab9653f9
Eyal Farago-Hagag <efarago@salesforce.com>
2020-03-21 17:57:36 +0200

    akka-28016__futureFlow: few more tests

--- akka-stream-tests/src/test/scala/akka/stream/scaladsl/FlowFutureFlowSpec.scala
+++ akka-stream-tests/src/test/scala/akka/stream/scaladsl/FlowFutureFlowSpec.scala
@@ -5,7 +5,9 @@
 package akka.stream.scaladsl
 
 import akka.NotUsed
+import akka.stream.NeverMaterializedException
 import akka.stream.testkit.StreamSpec
+import akka.stream.testkit.Utils.TE
 import akka.stream.testkit.scaladsl.StreamTestKit.assertAllStagesStopped
 
 import scala.concurrent.{Future, Promise}
@@ -27,9 +29,7 @@ class FlowFutureFlowSpec extends StreamSpec {
       fNotUsed.futureValue should be (NotUsed)
       fSeq.futureValue should equal (0 until 10)
     }
-  }
 
-  "a futureFlow" must {
     "work in the simple case with a late future" in assertAllStagesStopped {
       val prFlow = Promise[Flow[Int, Int, NotUsed]]
       val (fNotUsed, fSeq) = src10()
@@ -47,6 +47,46 @@ class FlowFutureFlowSpec extends StreamSpec {
       fNotUsed.futureValue should be (NotUsed)
       fSeq.futureValue should equal (0 until 10)
     }
+
+    "fail properly when future is a completed failed future" in assertAllStagesStopped {
+      val (fNotUsed, fSeq) = src10()
+        .viaMat{
+          Flow.futureFlow{
+            Future.failed[Flow[Int, Int, NotUsed]](TE("damn!"))
+          }
+        } (Keep.right)
+        .toMat(Sink.seq)(Keep.both)
+        .run()
+
+      fNotUsed.failed.futureValue should be (a[NeverMaterializedException])
+      fNotUsed.failed.futureValue.getCause should equal (TE("damn!"))
+
+      fSeq.failed.futureValue should equal (TE("damn!"))
+
+    }
+
+    "fail properly when future is late completed failed future" in assertAllStagesStopped {
+      val prFlow = Promise[Flow[Int, Int, NotUsed]]
+      val (fNotUsed, fSeq) = src10()
+        .viaMat{
+          Flow.futureFlow(prFlow.future)
+        } (Keep.right)
+        .toMat(Sink.seq)(Keep.both)
+        .run()
+
+      fNotUsed.value should be (empty)
+      fSeq.value should be (empty)
+
+      prFlow.failure(TE("damn!"))
+
+      fNotUsed.failed.futureValue should be (a[NeverMaterializedException])
+      fNotUsed.failed.futureValue.getCause should equal (TE("damn!"))
+
+      fSeq.failed.futureValue should equal (TE("damn!"))
+
+    }
   }
+
+
 
 }
