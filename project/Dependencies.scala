95e0973443c8083e6f905557ee0118ef9d0c4555
Johan Andrén <johan@markatta.com>
2020-04-03 15:39:56 +0200

    Agrona version in sync

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val aeronVersion = "1.27.0"
   // needs to be inline with the aeron version, check
   // https://github.com/real-logic/aeron/blob/1.x.y/build.gradle
-  val agronaVersion = "1.4.0"
+  val agronaVersion = "1.4.1"
   val nettyVersion = "3.10.6.Final"
   val jacksonVersion = "2.10.3"
   val protobufJavaVersion = "3.10.0"
