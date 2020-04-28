2fd4b1545a56a2bebb03d05c3d8126949abd3015
Arnout Engelen <arnout@bzzt.net>
2020-04-28 16:55:16 +0200

    Update scala to 2.13.2 and silencer to 1.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
   val logbackVersion = "1.2.3"
 
   val scala212Version = "2.12.11"
-  val scala213Version = "2.13.1"
+  val scala213Version = "2.13.2"
 
   val reactiveStreamsVersion = "1.0.3"
 
