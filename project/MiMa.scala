3158803a379dc7f4c938e58903730f0d3b5e8ffa
Patrik Nordwall <patrik.nordwall@gmail.com>
2020-05-04 11:45:03 +0200

    update latestPatchOf26 to 2.6.5 (#28997)
    * update latestPatchOf26 to 2.6.5
    * mima exclude for jfr package in 2.6.x

--- project/MiMa.scala
+++ project/MiMa.scala
@@ -13,7 +13,7 @@ import com.typesafe.tools.mima.plugin.MimaPlugin.autoImport._
 object MiMa extends AutoPlugin {
 
   private val latestPatchOf25 = 31
-  private val latestPatchOf26 = 4
+  private val latestPatchOf26 = 5
 
   override def requires = MimaPlugin
   override def trigger = allRequirements
