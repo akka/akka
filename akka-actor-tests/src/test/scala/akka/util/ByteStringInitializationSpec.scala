a4c5675fe39b455aa819cb8412dc903ad06d8b5c
Arnout Engelen <arnout@bzzt.net>
2020-06-16 12:11:22 +0200

    Reproduce ByteString initialization issue

---
+++ akka-actor-tests/src/test/scala/akka/util/ByteStringInitializationSpec.scala
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2020 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.util
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class ByteStringInitializationSpec extends AnyWordSpec with Matchers {
+  "ByteString intialization" should {
+    "not get confused by initializing CompactByteString before ByteString" in {
+      CompactByteString.empty should not be(null)
+      ByteString.empty should not be(null)
+    }
+  }
+}
