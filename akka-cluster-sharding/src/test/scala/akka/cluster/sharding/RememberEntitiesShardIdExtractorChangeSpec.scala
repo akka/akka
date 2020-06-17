5053a1eb554c8d16f18214e1600f83c052cbf654
Johan Andrén <johan@markatta.com>
2020-06-17 15:46:22 +0200

    Missing wildcard port for netty in some sharding tests #29251

--- akka-cluster-sharding/src/test/scala/akka/cluster/sharding/RememberEntitiesShardIdExtractorChangeSpec.scala
+++ akka-cluster-sharding/src/test/scala/akka/cluster/sharding/RememberEntitiesShardIdExtractorChangeSpec.scala
@@ -28,6 +28,7 @@ object RememberEntitiesShardIdExtractorChangeSpec {
        akka.loglevel = INFO
        akka.actor.provider = "cluster"
        akka.remote.artery.canonical.port = 0 
+       akka.remote.classic.netty.tcp.port = 0
        akka.cluster.sharding {
         remember-entities = on
         remember-entities-store = "eventsourced"
