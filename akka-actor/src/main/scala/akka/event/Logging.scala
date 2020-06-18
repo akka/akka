0131efb76e06a66b99f7fc982ed45380057be252
Axel MAUMONT <amaumont@talend.com>
2020-06-18 21:35:38 +0200

    Fix issue in MarkerLoggingAdapter when logging with argument as an Array - #29272
        Logging using an Array as argument such as below:
    
              markerLogging.info(LogMarker.Security, "This is a security problem because of {} and {}",
                Array("this", "that")
              )
    
        Now the ouput is the expected one with:
    
    	"This is a security problem because of this and that"
    
        Instead of the unexpected:
    
    	"This is a security problem because of [Vector(this, that) and {}]"

--- akka-actor/src/main/scala/akka/event/Logging.scala
+++ akka-actor/src/main/scala/akka/event/Logging.scala
@@ -1472,7 +1472,7 @@ trait LoggingAdapter {
     formatImpl(t, arg)
   }
 
-  private def formatImpl(t: String, arg: Seq[Any]): String = {
+  def formatImpl(t: String, arg: Seq[Any]): String = {
     val sb = new java.lang.StringBuilder(64)
     var p = 0
     var startIndex = 0
@@ -1943,8 +1943,8 @@ class MarkerLoggingAdapter(
 
   // Copy of LoggingAdapter.format1 due to binary compatibility restrictions
   private def format1(t: String, arg: Any): String = arg match {
-    case a: Array[_] if !a.getClass.getComponentType.isPrimitive => format(t, a.toIndexedSeq)
-    case a: Array[_]                                             => format(t, a.map(_.asInstanceOf[AnyRef]).toIndexedSeq)
+    case a: Array[_] if !a.getClass.getComponentType.isPrimitive => formatImpl(t, a.toSeq)
+    case a: Array[_]                                             => formatImpl(t, a.map(_.asInstanceOf[AnyRef]).toSeq)
     case x                                                       => format(t, x)
   }
 }
