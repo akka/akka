9311023761732a12c9e9096c448338680d964ee8
Patrik Nordwall <patrik.nordwall@gmail.com>
2020-06-30 09:28:48 +0200

    message adapter in ActiveActiveShardingDirectReplication (#29328)
    * I don't think it's worth cheating with Any and narrow just to save
      one or two allocations given how many other things that are needed for
      sending the messages

--- akka-cluster-sharding-typed/src/main/scala/akka/cluster/sharding/typed/ActiveActiveShardingDirectReplication.scala
+++ akka-cluster-sharding-typed/src/main/scala/akka/cluster/sharding/typed/ActiveActiveShardingDirectReplication.scala
@@ -46,6 +46,8 @@ object ActiveActiveShardingDirectReplication {
   @InternalApi
   private[akka] case class VerifyStarted(replyTo: ActorRef[Done]) extends Command
 
+  private final case class WrappedPublishedEvent(publishedEvent: PublishedEvent) extends Command
+
   /**
    * Java API:
    * @param selfReplica The replica id of the replica that runs on this node
@@ -60,31 +62,30 @@ object ActiveActiveShardingDirectReplication {
    * @param replicaShardingProxies A replica id to sharding proxy mapping for each replica in the system
    */
   def apply[T](selfReplica: String, replicaShardingProxies: Map[String, ActorRef[T]]): Behavior[Command] =
-    Behaviors
-      .setup[Any] { context =>
-        context.log.debug(
-          "Subscribing to event stream to forward events to [{}] sharded replicas",
-          replicaShardingProxies.size - 1)
-        context.system.eventStream ! EventStream.Subscribe[PublishedEvent](context.self)
+    Behaviors.setup[Command] { context =>
+      context.log.debug(
+        "Subscribing to event stream to forward events to [{}] sharded replicas",
+        replicaShardingProxies.size - 1)
+      val publishedEventAdapter = context.messageAdapter[PublishedEvent](WrappedPublishedEvent.apply)
+      context.system.eventStream ! EventStream.Subscribe[PublishedEvent](publishedEventAdapter)
 
-        Behaviors.receiveMessagePartial {
-          case event: PublishedEvent =>
-            context.log.trace(
-              "Forwarding event for persistence id [{}] sequence nr [{}] to replicas",
-              event.persistenceId,
-              event.sequenceNumber)
-            replicaShardingProxies.foreach {
-              case (replica, proxy) =>
-                val envelopedEvent = ShardingEnvelope(event.persistenceId.id, event)
-                if (replica != selfReplica)
-                  proxy.asInstanceOf[ActorRef[ShardingEnvelope[PublishedEvent]]] ! envelopedEvent
-            }
-            Behaviors.same
-          case VerifyStarted(replyTo) =>
-            replyTo ! Done
-            Behaviors.same
-        }
+      Behaviors.receiveMessage {
+        case WrappedPublishedEvent(event) =>
+          context.log.trace(
+            "Forwarding event for persistence id [{}] sequence nr [{}] to replicas",
+            event.persistenceId,
+            event.sequenceNumber)
+          replicaShardingProxies.foreach {
+            case (replica, proxy) =>
+              val envelopedEvent = ShardingEnvelope(event.persistenceId.id, event)
+              if (replica != selfReplica)
+                proxy.asInstanceOf[ActorRef[ShardingEnvelope[PublishedEvent]]] ! envelopedEvent
+          }
+          Behaviors.same
+        case VerifyStarted(replyTo) =>
+          replyTo ! Done
+          Behaviors.same
       }
-      .narrow[Command]
+    }
 
 }
