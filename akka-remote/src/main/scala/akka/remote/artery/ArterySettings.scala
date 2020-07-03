f6ceb4d49aeb7ab28e9e7bd2963695b3a539c3af
Patrik Nordwall <patrik.nordwall@gmail.com>
2020-07-03 09:54:35 +0200

    Flush messages before DeathWatchNotification, #28695 (#28940)
    * Since DeathWatchNotification is sent over the control channel it may overtake
      other messages that have been sent from the same actor before it stopped.
    * It can be confusing that Terminated can't be used as an end-of-conversation marker.
    * In classic Remoting we didn't have this problem because all messages were sent over
      the same connection.
    
    * don't send DeathWatchNotification when system is terminating
    * when using Cluster we can rely on that the other side will publish AddressTerminated
      when the member has been removed
    * it's actually already a race condition that often will result in that the DeathWatchNotification
      from the terminating side
      * in DeathWatch.scala it will remove the watchedBy when receiving AddressTerminated, and that
        may (sometimes) happen before tellWatchersWeDied
    
    * same for Unwatch
    * to avoid sending many Unwatch messages when watcher's ActorSystem is terminated
    * same race exists for Unwatch as for DeathWatchNotification, if RemoteWatcher publishAddressTerminated
      before the watcher is terminated
    
    * config for the flush timeout, and possibility to disable

--- akka-remote/src/main/scala/akka/remote/artery/ArterySettings.scala
+++ akka-remote/src/main/scala/akka/remote/artery/ArterySettings.scala
@@ -152,7 +152,18 @@ private[akka] final class ArterySettings private (config: Config) {
     val ShutdownFlushTimeout: FiniteDuration =
       config
         .getMillisDuration("shutdown-flush-timeout")
-        .requiring(interval => interval > Duration.Zero, "shutdown-flush-timeout must be more than zero")
+        .requiring(timeout => timeout > Duration.Zero, "shutdown-flush-timeout must be more than zero")
+    val DeathWatchNotificationFlushTimeout: FiniteDuration = {
+      toRootLowerCase(config.getString("death-watch-notification-flush-timeout")) match {
+        case "off" => Duration.Zero
+        case _ =>
+          config
+            .getMillisDuration("death-watch-notification-flush-timeout")
+            .requiring(
+              interval => interval > Duration.Zero,
+              "death-watch-notification-flush-timeout must be more than zero, or off")
+      }
+    }
     val InboundRestartTimeout: FiniteDuration =
       config
         .getMillisDuration("inbound-restart-timeout")
