bfe0b58a08145923eec5edfa28b8ea108b8cb29a
Bùi Việt Thành <thanhbv@sandinh.net>
2020-07-09 11:07:16 +0700

    sbt: scalafixDependencies += com.sandinh::scala-rewrite
    Will be used to run rules like:
    + fix.scala213.ParensAroundLambda
    + fix.scala213.NullaryOverride
    + fix.explicittypes.ExplicitImplicitTypes
    + fix.scala213.MultiArgInfix
    + fix.scala213.ConstructorProcedureSyntax
    + fix.scala213.DottyMigrate

--- project/ScalaFixExtraRulesPlugin.scala
+++ project/ScalaFixExtraRulesPlugin.scala
@@ -4,10 +4,10 @@
 
 package akka
 
-import sbt.{AutoPlugin, PluginTrigger, Plugins, ScalafixSupport}
+import sbt.{ AutoPlugin, PluginTrigger, Plugins, ScalafixSupport }
 import scalafix.sbt.ScalafixPlugin
 
-object ScalaFixExtraRulesPlugin extends AutoPlugin with ScalafixSupport{
+object ScalaFixExtraRulesPlugin extends AutoPlugin with ScalafixSupport {
   override def trigger: PluginTrigger = allRequirements
 
   override def requires: Plugins = ScalafixPlugin
@@ -17,7 +17,6 @@ object ScalaFixExtraRulesPlugin extends AutoPlugin with ScalafixSupport{
   override def projectSettings: Seq[Def.Setting[_]] = super.projectSettings ++ {
     scalafixDependencies in ThisBuild ++= Seq(
       "com.nequissimus" %% "sort-imports" % "0.5.5",
-      "com.sandinh" %% "scala-rewrites" % "0.1.7-sd"
-    )
+      "com.sandinh" %% "scala-rewrites" % "0.1.7-sd")
   }
 }
