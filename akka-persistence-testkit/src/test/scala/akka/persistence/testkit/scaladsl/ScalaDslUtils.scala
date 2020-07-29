20db07e6b188e25eb170434c1214250322ec5177
Christopher Batey <christopher.batey@gmail.com>
2020-07-29 08:42:44 +0100

    Revert changes to testkit

--- akka-persistence-testkit/src/test/scala/akka/persistence/testkit/scaladsl/ScalaDslUtils.scala
+++ akka-persistence-testkit/src/test/scala/akka/persistence/testkit/scaladsl/ScalaDslUtils.scala
@@ -8,21 +8,17 @@ import akka.actor.typed.ActorRef
 import akka.persistence.testkit.{ Cmd, CommonUtils, EmptyState, Evt, Passivate, Recovered, Stopped, TestCommand }
 import akka.persistence.typed.PersistenceId
 import akka.persistence.typed.RecoveryCompleted
-import akka.persistence.typed.scaladsl.{ Effect, ReplicatedEventSourcedBehavior }
+import akka.persistence.typed.scaladsl.{ Effect, EventSourcedBehavior }
 
 trait ScalaDslUtils extends CommonUtils {
 
   def eventSourcedBehavior(pid: String, replyOnRecovery: Option[ActorRef[Any]] = None) =
-    ReplicatedEventSourcedBehavior[TestCommand, Evt, EmptyState](
-      PersistenceId.ofUniqueId(pid),
-      EmptyState(),
-      (_, cmd) => {
-        cmd match {
-          case Cmd(data) => Effect.persist(Evt(data))
-          case Passivate => Effect.stop().thenRun(_ => replyOnRecovery.foreach(_ ! Stopped))
-        }
-      },
-      (_, _) => EmptyState()).snapshotWhen((_, _, _) => true).receiveSignal {
+    EventSourcedBehavior[TestCommand, Evt, EmptyState](PersistenceId.ofUniqueId(pid), EmptyState(), (_, cmd) => {
+      cmd match {
+        case Cmd(data) => Effect.persist(Evt(data))
+        case Passivate => Effect.stop().thenRun(_ => replyOnRecovery.foreach(_ ! Stopped))
+      }
+    }, (_, _) => EmptyState()).snapshotWhen((_, _, _) => true).receiveSignal {
       case (_, RecoveryCompleted) => replyOnRecovery.foreach(_ ! Recovered)
     }
 
