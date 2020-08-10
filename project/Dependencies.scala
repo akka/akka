6d7ea25fa63589452c7cd2fd62adf4da3111a3c3
Patrik Nordwall <patrik.nordwall@gmail.com>
2020-08-10 09:07:39 +0200

    Agrona 1.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   val aeronVersion = "1.29.0"
   // needs to be inline with the aeron version, check
   // https://github.com/real-logic/aeron/blob/1.x.y/build.gradle
-  val agronaVersion = "1.4.1"
+  val agronaVersion = "1.6.0"
   val nettyVersion = "3.10.6.Final"
   val jacksonVersion = "2.10.4"
   val protobufJavaVersion = "3.11.4"
