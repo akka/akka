d078a6b65f019551110f2d20caff4ad30cb31c1d
Patrik Nordwall <patrik.nordwall@gmail.com>
2020-08-11 12:50:09 +0200

    LwwTime example
    * show event and command handlers of the blog example in docs
    * add blog example for Java

--- akka-persistence-typed-tests/src/test/scala/docs/akka/persistence/typed/ReplicatedBlogExampleSpec.scala
+++ akka-persistence-typed-tests/src/test/scala/docs/akka/persistence/typed/ReplicatedBlogExampleSpec.scala
@@ -4,44 +4,137 @@
 
 package docs.akka.persistence.typed
 
+import org.scalatest.concurrent.Eventually
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.time.Millis
+import org.scalatest.time.Span
+import org.scalatest.wordspec.AnyWordSpecLike
+
 import akka.Done
-import akka.actor.testkit.typed.scaladsl.{ LogCapturing, ScalaTestWithActorTestKit }
+import akka.actor.testkit.typed.scaladsl.LogCapturing
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import akka.actor.typed.ActorRef
-import akka.actor.typed.scaladsl.{ ActorContext, Behaviors }
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.Behaviors
 import akka.persistence.testkit.PersistenceTestKitPlugin
 import akka.persistence.testkit.query.scaladsl.PersistenceTestKitReadJournal
 import akka.persistence.typed.ReplicaId
 import akka.persistence.typed.crdt.LwwTime
 import akka.persistence.typed.scaladsl._
 import akka.serialization.jackson.CborSerializable
-import org.scalatest.concurrent.{ Eventually, ScalaFutures }
-import org.scalatest.matchers.should.Matchers
-import org.scalatest.time.{ Millis, Span }
-import org.scalatest.wordspec.AnyWordSpecLike
 
 object ReplicatedBlogExampleSpec {
 
-  final case class BlogState(content: Option[PostContent], contentTimestamp: LwwTime, published: Boolean) {
-    def withContent(newContent: PostContent, timestamp: LwwTime): BlogState =
-      copy(content = Some(newContent), contentTimestamp = timestamp)
-    def isEmpty: Boolean = content.isEmpty
+  object BlogEntity {
+
+    object BlogState {
+      val empty: BlogState = BlogState(None, LwwTime(Long.MinValue, ReplicaId("")), published = false)
+    }
+    final case class BlogState(content: Option[PostContent], contentTimestamp: LwwTime, published: Boolean)
+        extends CborSerializable {
+      def withContent(newContent: PostContent, timestamp: LwwTime): BlogState =
+        copy(content = Some(newContent), contentTimestamp = timestamp)
+
+      def isEmpty: Boolean = content.isEmpty
+    }
+
+    final case class PostContent(title: String, body: String) extends CborSerializable
+    final case class Published(postId: String) extends Event
+
+    sealed trait Command extends CborSerializable
+    final case class AddPost(postId: String, content: PostContent, replyTo: ActorRef[AddPostDone]) extends Command
+    final case class AddPostDone(postId: String)
+    final case class GetPost(postId: String, replyTo: ActorRef[PostContent]) extends Command
+    final case class ChangeBody(postId: String, newContent: PostContent, replyTo: ActorRef[Done]) extends Command
+    final case class Publish(postId: String, replyTo: ActorRef[Done]) extends Command
+
+    sealed trait Event extends CborSerializable
+    final case class PostAdded(postId: String, content: PostContent, timestamp: LwwTime) extends Event
+    final case class BodyChanged(postId: String, newContent: PostContent, timestamp: LwwTime) extends Event
+
+    def apply(entityId: String, replicaId: ReplicaId, allReplicaIds: Set[ReplicaId]): Behavior[Command] = {
+      Behaviors.setup[Command] { ctx =>
+        ReplicatedEventSourcing.withSharedJournal(
+          "blog",
+          entityId,
+          replicaId,
+          allReplicaIds,
+          PersistenceTestKitReadJournal.Identifier) { replicationContext =>
+          EventSourcedBehavior[Command, Event, BlogState](
+            replicationContext.persistenceId,
+            BlogState.empty,
+            (state, cmd) => commandHandler(ctx, replicationContext, state, cmd),
+            (state, event) => eventHandler(ctx, replicationContext, state, event))
+        }
+      }
+    }
+
+    //#command-handler
+    private def commandHandler(
+        ctx: ActorContext[Command],
+        replicationContext: ReplicationContext,
+        state: BlogState,
+        cmd: Command): Effect[Event, BlogState] = {
+      cmd match {
+        case AddPost(_, content, replyTo) =>
+          val evt =
+            PostAdded(
+              replicationContext.entityId,
+              content,
+              state.contentTimestamp.increase(replicationContext.currentTimeMillis(), replicationContext.replicaId))
+          Effect.persist(evt).thenRun { _ =>
+            replyTo ! AddPostDone(replicationContext.entityId)
+          }
+        case ChangeBody(_, newContent, replyTo) =>
+          val evt =
+            BodyChanged(
+              replicationContext.entityId,
+              newContent,
+              state.contentTimestamp.increase(replicationContext.currentTimeMillis(), replicationContext.replicaId))
+          Effect.persist(evt).thenRun { _ =>
+            replyTo ! Done
+          }
+        case p: Publish =>
+          Effect.persist(Published("id")).thenRun { _ =>
+            p.replyTo ! Done
+          }
+        case gp: GetPost =>
+          ctx.log.info("GetPost {}", state.content)
+          state.content.foreach(content => gp.replyTo ! content)
+          Effect.none
+      }
+    }
+    //#command-handler
+
+    //#event-handler
+    private def eventHandler(
+        ctx: ActorContext[Command],
+        replicationContext: ReplicationContext,
+        state: BlogState,
+        event: Event): BlogState = {
+      ctx.log.info(s"${replicationContext.entityId}:${replicationContext.replicaId} Received event $event")
+      event match {
+        case PostAdded(_, content, timestamp) =>
+          if (timestamp.isAfter(state.contentTimestamp)) {
+            val s = state.withContent(content, timestamp)
+            ctx.log.info("Updating content. New content is {}", s)
+            s
+          } else {
+            ctx.log.info("Ignoring event as timestamp is older")
+            state
+          }
+        case BodyChanged(_, newContent, timestamp) =>
+          if (timestamp.isAfter(state.contentTimestamp))
+            state.withContent(newContent, timestamp)
+          else state
+        case Published(_) =>
+          state.copy(published = true)
+      }
+    }
+    //#event-handler
   }
-  val emptyState: BlogState = BlogState(None, LwwTime(Long.MinValue, ReplicaId("")), published = false)
-
-  final case class PostContent(title: String, body: String)
-  final case class PostSummary(postId: String, title: String)
-  final case class Published(postId: String) extends BlogEvent
-
-  sealed trait BlogCommand
-  final case class AddPost(postId: String, content: PostContent, replyTo: ActorRef[AddPostDone]) extends BlogCommand
-  final case class AddPostDone(postId: String)
-  final case class GetPost(postId: String, replyTo: ActorRef[PostContent]) extends BlogCommand
-  final case class ChangeBody(postId: String, newContent: PostContent, replyTo: ActorRef[Done]) extends BlogCommand
-  final case class Publish(postId: String, replyTo: ActorRef[Done]) extends BlogCommand
-
-  sealed trait BlogEvent extends CborSerializable
-  final case class PostAdded(postId: String, content: PostContent, timestamp: LwwTime) extends BlogEvent
-  final case class BodyChanged(postId: String, newContent: PostContent, timestamp: LwwTime) extends BlogEvent
 }
 
 class ReplicatedBlogExampleSpec
@@ -51,98 +144,23 @@ class ReplicatedBlogExampleSpec
     with LogCapturing
     with ScalaFutures
     with Eventually {
-  import ReplicatedBlogExampleSpec._
+  import ReplicatedBlogExampleSpec.BlogEntity
+  import ReplicatedBlogExampleSpec.BlogEntity._
 
   implicit val config: PatienceConfig = PatienceConfig(timeout = Span(timeout.duration.toMillis, Millis))
 
-  def behavior(replicationContext: ReplicationContext, ctx: ActorContext[BlogCommand]) =
-    EventSourcedBehavior[BlogCommand, BlogEvent, BlogState](
-      replicationContext.persistenceId,
-      emptyState,
-      (state, cmd) =>
-        cmd match {
-          case AddPost(_, content, replyTo) =>
-            val evt =
-              PostAdded(
-                replicationContext.persistenceId.id,
-                content,
-                state.contentTimestamp.increase(replicationContext.currentTimeMillis(), replicationContext.replicaId))
-            Effect.persist(evt).thenRun { _ =>
-              replyTo ! AddPostDone(replicationContext.entityId)
-            }
-          case ChangeBody(_, newContent, replyTo) =>
-            val evt =
-              BodyChanged(
-                replicationContext.persistenceId.id,
-                newContent,
-                state.contentTimestamp.increase(replicationContext.currentTimeMillis(), replicationContext.replicaId))
-            Effect.persist(evt).thenRun { _ =>
-              replyTo ! Done
-            }
-          case p: Publish =>
-            Effect.persist(Published("id")).thenRun { _ =>
-              p.replyTo ! Done
-            }
-          case gp: GetPost =>
-            ctx.log.info("GetPost {}", state.content)
-            state.content.foreach(content => gp.replyTo ! content)
-            Effect.none
-        },
-      (state, event) => {
-        ctx.log.info(s"${replicationContext.entityId}:${replicationContext.replicaId} Received event $event")
-        event match {
-          case PostAdded(_, content, timestamp) =>
-            if (timestamp.isAfter(state.contentTimestamp)) {
-              val s = state.withContent(content, timestamp)
-              ctx.log.info("Updating content. New content is {}", s)
-              s
-            } else {
-              ctx.log.info("Ignoring event as timestamp is older")
-              state
-            }
-          case BodyChanged(_, newContent, timestamp) =>
-            if (timestamp.isAfter(state.contentTimestamp))
-              state.withContent(newContent, timestamp)
-            else state
-          case Published(_) =>
-            state.copy(published = true)
-        }
-      })
-
   "Blog Example" should {
     "work" in {
-      val refDcA: ActorRef[BlogCommand] =
-        spawn(
-          Behaviors.setup[BlogCommand] { ctx =>
-            ReplicatedEventSourcing.withSharedJournal(
-              "blog",
-              "cat",
-              ReplicaId("DC-A"),
-              Set(ReplicaId("DC-A"), ReplicaId("DC-B")),
-              PersistenceTestKitReadJournal.Identifier) { replicationContext =>
-              behavior(replicationContext, ctx)
-            }
-          },
-          "dc-a")
+      val refDcA: ActorRef[Command] =
+        spawn(BlogEntity("cat", ReplicaId("DC-A"), Set(ReplicaId("DC-A"), ReplicaId("DC-B"))))
 
-      val refDcB: ActorRef[BlogCommand] =
-        spawn(
-          Behaviors.setup[BlogCommand] { ctx =>
-            ReplicatedEventSourcing.withSharedJournal(
-              "blog",
-              "cat",
-              ReplicaId("DC-B"),
-              Set(ReplicaId("DC-A"), ReplicaId("DC-B")),
-              PersistenceTestKitReadJournal.Identifier) { replicationContext =>
-              behavior(replicationContext, ctx)
-            }
-          },
-          "dc-b")
+      val refDcB: ActorRef[Command] =
+        spawn(BlogEntity("cat", ReplicaId("DC-B"), Set(ReplicaId("DC-A"), ReplicaId("DC-B"))))
+
+      import scala.concurrent.duration._
 
       import akka.actor.typed.scaladsl.AskPattern._
       import akka.util.Timeout
-
-      import scala.concurrent.duration._
       implicit val timeout: Timeout = 3.seconds
 
       val content = PostContent("cats are the bets", "yep")
