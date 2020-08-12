4826477c3697c8fac63180aae646495517bdb1a2
Christopher Batey <christopher.batey@gmail.com>
2020-08-12 08:12:08 +0100

    Remove doclint from javac for docs (#29476)

--- project/AkkaDisciplinePlugin.scala
+++ project/AkkaDisciplinePlugin.scala
@@ -81,7 +81,7 @@ object AkkaDisciplinePlugin extends AutoPlugin {
             if (!nonFatalJavaWarningsFor(name.value)) Seq("-Werror", "-Xlint:deprecation", "-Xlint:unchecked")
             else Seq.empty
           ),
-        Compile / javacOptions in doc := Seq(),
+        Compile / javacOptions in doc := Seq("-Xdoclint:none"),
         Compile / scalacOptions ++= (CrossVersion.partialVersion(scalaVersion.value) match {
             case Some((2, 13)) =>
               disciplineScalacOptions -- Set(
