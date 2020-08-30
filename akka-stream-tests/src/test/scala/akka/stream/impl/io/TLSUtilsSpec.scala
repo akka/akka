238f40e34d153a8dbe342ecc5e37633d7cb3de0f
economist <32593716+TigranOhanyan@users.noreply.github.com>
2020-08-30 13:27:12 +0400

    stream: fix client-side mutual TLS bug.
    Connected issues:
    https://github.com/lightbend/ssl-config/issues/62
    https://github.com/akka/akka-http/issues/1433

---
+++ akka-stream-tests/src/test/scala/akka/stream/impl/io/TLSUtilsSpec.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2020 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.stream.impl.io
+
+import akka.stream.TLSClientAuth
+import akka.stream.TLSProtocol.NegotiateNewSession
+import javax.net.ssl.{ SSLContext, SSLEngine, SSLParameters }
+import org.scalatest.funspec.AnyFunSpec
+
+class TLSUtilsSpec extends AnyFunSpec {
+
+  describe("To have Client authentication defined NEED") {
+    val sslEngine: SSLEngine = SSLContext.getDefault.createSSLEngine()
+    val sslParams: SSLParameters = new SSLParameters()
+    val negotiableSession: NegotiateNewSession =
+      NegotiateNewSession(None, None, Some(TLSClientAuth.Need), Some(sslParams))
+
+    it(
+      "SSLParameters does not by default require client authentication when it is not passed to the constructor of HttpConnectionContext. In that case its default value is used by akka-http.") {
+      assert(!sslParams.getNeedClientAuth)
+    }
+
+    it("NegotiableSession has ClientAuth defined Need") {
+      assert(negotiableSession.clientAuth.contains(TLSClientAuth.Need))
+    }
+
+    it(
+      "SSLEngine must have ClientAuth defined Need after calling akka.stream.impl.io.TlsUtils.applySessionParameters with sslEngine and negotiableSession") {
+      TlsUtils.applySessionParameters(sslEngine, negotiableSession)
+      assert(sslEngine.getSSLParameters.getNeedClientAuth)
+    }
+  }
+}
