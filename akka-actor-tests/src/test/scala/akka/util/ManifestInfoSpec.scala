a6361235036f30b7a45a29bb25cef0bb52b0ec2f
Patrik Nordwall <patrik.nordwall@gmail.com>
2020-09-02 14:30:09 +0200

    fix compilation err

--- akka-actor-tests/src/test/scala/akka/util/ManifestInfoSpec.scala
+++ akka-actor-tests/src/test/scala/akka/util/ManifestInfoSpec.scala
@@ -5,16 +5,15 @@
 package akka.util
 
 import akka.testkit.AkkaSpec
-import akka.util.ManifestInfo.Version
 
 class ManifestInfoSpec extends AkkaSpec {
   "ManifestInfo" should {
     "produce a clear message" in {
       val versions = Map(
-        "akka-actor" -> new Version("2.6.4"),
-        "akka-persistence" -> new Version("2.5.3"),
-        "akka-cluster" -> new Version("2.5.3"),
-        "unrelated" -> new Version("2.5.3"))
+        "akka-actor" -> new ManifestInfo.Version("2.6.4"),
+        "akka-persistence" -> new ManifestInfo.Version("2.5.3"),
+        "akka-cluster" -> new ManifestInfo.Version("2.5.3"),
+        "unrelated" -> new ManifestInfo.Version("2.5.3"))
       val allModules = List("akka-actor", "akka-persistence", "akka-cluster")
       ManifestInfo.checkSameVersion("Akka", allModules, versions) shouldBe Some(
         "You are using version 2.6.4 of Akka, but it appears you (perhaps indirectly) also depend on older versions of related artifacts. " +
