d56864248c844be614ff80e41c7ff8b540591b13
Muskan Gupta <muskan.gupta3006@gmail.com>
2020-09-17 14:11:30 +0530

    Formatting files

--- akka-docs/src/test/scala/docs/stream/operators/sink/Fold.scala
+++ akka-docs/src/test/scala/docs/stream/operators/sink/Fold.scala
@@ -1,7 +1,7 @@
 package docs.stream.operators.sink
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{ Sink, Source }
 
 import scala.concurrent.Future
 
@@ -10,8 +10,7 @@ object Fold {
 
   def foldExample: Future[Int] = {
     //#fold
-    Source(1 to 100)
-      .runWith(Sink.fold(0)(_ + _))
+    Source(1 to 100).runWith(Sink.fold(0)(_ + _))
     //Future(Success(5050))
     //#fold
   }
