d00318f02caa55bfdfc51bb0a4f2528407df32b3
Muskan Gupta <muskan.gupta3006@gmail.com>
2020-09-17 14:11:29 +0530

    Formatted Flow.scala

--- akka-docs/src/test/scala/docs/stream/operators/sink/Fold.scala
+++ akka-docs/src/test/scala/docs/stream/operators/sink/Fold.scala
@@ -1,17 +1,19 @@
 package docs.stream.operators.sink
+
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.scaladsl.{Sink, Source}
 
 import scala.concurrent.Future
 
 object Fold {
   implicit val system: ActorSystem = ActorSystem("example")
-def foldExample: Future[Int] ={
 
-  //#fold
-  Source(1 to 100)
-    .runWith(Sink.fold(0)(_+_))
-  //Future(Success(5050))
-  //#fold
-}
+  def foldExample: Future[Int] = {
+
+    //#fold
+    Source(1 to 100)
+      .runWith(Sink.fold(0)(_ + _))
+    //Future(Success(5050))
+    //#fold
+  }
 }
