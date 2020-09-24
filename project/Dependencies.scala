78d81de709aa8d5b2d8d1e365ea9283a67a9a11a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-09-24 09:47:37 +0200

    Update aeron-client, aeron-driver to 1.30.0 (#29655)
    * Update aeron-client, aeron-driver to 1.30.0
    
    * Upgrade to agrona 1.7.2, to keep in line with aeron
    
    Co-authored-by: Christopher Batey <christopher.batey@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,10 +16,10 @@ object Dependencies {
   val junitVersion = "4.13"
   val slf4jVersion = "1.7.30"
   // check agrona version when updating this
-  val aeronVersion = "1.27.0"
+  val aeronVersion = "1.30.0"
   // needs to be inline with the aeron version, check
   // https://github.com/real-logic/aeron/blob/1.x.y/build.gradle
-  val agronaVersion = "1.4.1"
+  val agronaVersion = "1.7.2"
   val nettyVersion = "3.10.6.Final"
   val jacksonVersion = "2.10.5"
   val protobufJavaVersion = "3.11.4"
