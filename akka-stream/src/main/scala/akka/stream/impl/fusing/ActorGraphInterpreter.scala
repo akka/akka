a08e2cd93bfa45d601849bedd17809ae24ffd69a
Eyal Farago-Hagag <efarago@salesforce.com>
2020-10-13 15:04:10 +0300

    akka-29730__register_newshells_when_no_active_interpreters: simplify shortCircuitBatch's condition.

--- akka-stream/src/main/scala/akka/stream/impl/fusing/ActorGraphInterpreter.scala
+++ akka-stream/src/main/scala/akka/stream/impl/fusing/ActorGraphInterpreter.scala
@@ -754,7 +754,7 @@ import akka.util.OptionVal
   }
 
   private def shortCircuitBatch(): Unit = {
-    while (!shortCircuitBuffer.isEmpty && currentLimit > 0 && (activeInterpreters.nonEmpty || newShells.nonEmpty)) shortCircuitBuffer
+    while (!shortCircuitBuffer.isEmpty && currentLimit > 0) shortCircuitBuffer
       .poll() match {
       case b: BoundaryEvent => processEvent(b)
       case Resume           => finishShellRegistration()
