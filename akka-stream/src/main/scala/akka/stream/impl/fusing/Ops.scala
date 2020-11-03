e1af1e070e53a849bfdeb360cfd4056a5f0a9e17
Eyal Farago-Hagag <efarago@salesforce.com>
2020-11-03 12:41:24 +0200

    akka-29784__statefulMapConcat_relax_f_ret_type: remove redundant import.

--- akka-stream/src/main/scala/akka/stream/impl/fusing/Ops.scala
+++ akka-stream/src/main/scala/akka/stream/impl/fusing/Ops.scala
@@ -6,30 +6,30 @@ package akka.stream.impl.fusing
 
 import java.util.concurrent.TimeUnit.NANOSECONDS
 
-import scala.annotation.tailrec
-import scala.collection.{ immutable, TraversableOnce }
-import scala.collection.immutable.VectorBuilder
-import scala.concurrent.Future
-import scala.concurrent.duration.{ FiniteDuration, _ }
-import scala.util.{ Failure, Success, Try }
-import scala.util.control.{ NoStackTrace, NonFatal }
-import scala.util.control.Exception.Catcher
-import com.github.ghik.silencer.silent
 import akka.actor.{ ActorRef, Terminated }
 import akka.annotation.{ DoNotInherit, InternalApi }
-import akka.event.{ LogMarker, LogSource, Logging, LoggingAdapter, MarkerLoggingAdapter }
 import akka.event.Logging.LogLevel
-import akka.stream.{ Supervision, _ }
+import akka.event._
 import akka.stream.ActorAttributes.SupervisionStrategy
 import akka.stream.Attributes.{ InputBuffer, LogLevels }
 import akka.stream.OverflowStrategies._
-import akka.stream.impl.{ ReactiveStreamsCompliance, Buffer => BufferImpl }
 import akka.stream.impl.Stages.DefaultAttributes
 import akka.stream.impl.fusing.GraphStages.SimpleLinearGraphStage
+import akka.stream.impl.{ ReactiveStreamsCompliance, Buffer => BufferImpl }
 import akka.stream.scaladsl.{ DelayStrategy, Source }
 import akka.stream.stage._
-import akka.util.OptionVal
-import akka.util.unused
+import akka.stream.{ Supervision, _ }
+import akka.util.{ unused, OptionVal }
+import com.github.ghik.silencer.silent
+
+import scala.annotation.tailrec
+import scala.collection.immutable
+import scala.collection.immutable.VectorBuilder
+import scala.concurrent.Future
+import scala.concurrent.duration.{ FiniteDuration, _ }
+import scala.util.control.Exception.Catcher
+import scala.util.control.{ NoStackTrace, NonFatal }
+import scala.util.{ Failure, Success, Try }
 
 /**
  * INTERNAL API
