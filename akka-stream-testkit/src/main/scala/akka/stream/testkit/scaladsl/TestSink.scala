7f037fb659b0bd3e6e4def5d1c2a84d11d908cb8
Enno Runne <458526+ennru@users.noreply.github.com>
2020-11-23 10:43:16 +0100

    Stream Testkit: add ClassicActorSystemProvider alternatives

--- akka-stream-testkit/src/main/scala/akka/stream/testkit/scaladsl/TestSink.scala
+++ akka-stream-testkit/src/main/scala/akka/stream/testkit/scaladsl/TestSink.scala
@@ -5,6 +5,7 @@
 package akka.stream.testkit.scaladsl
 
 import akka.actor.ActorSystem
+import akka.actor.ClassicActorSystemProvider
 import akka.stream._
 import akka.stream.Attributes.none
 import akka.stream.scaladsl._
@@ -22,5 +23,11 @@ object TestSink {
    */
   def probe[T](implicit system: ActorSystem): Sink[T, Probe[T]] =
     Sink.fromGraph[T, TestSubscriber.Probe[T]](new ProbeSink(none, SinkShape(Inlet("ProbeSink.in"))))
+
+  /**
+   * A Sink that materialized to a [[akka.stream.testkit.TestSubscriber.Probe]].
+   */
+  def apply[T]()(implicit system: ClassicActorSystemProvider): Sink[T, Probe[T]] =
+    probe(system.classicSystem)
 
 }
