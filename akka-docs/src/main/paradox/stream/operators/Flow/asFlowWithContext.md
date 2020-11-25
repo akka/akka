459e74b921cabe2b7af91eb56bc073cfe29a94e0
Enno Runne <458526+ennru@users.noreply.github.com>
2020-11-25 12:21:16 +0100

    Operator docs: examples for asSource/FlowWithContext (#29822)
    * Operator docs: examples for asSource/FlowWithContext
    
    * javafmtAll
    
    * Try to capture the essence better
    
    * Update index
    
    * Include the test use in the snippets

--- akka-docs/src/main/paradox/stream/operators/Flow/asFlowWithContext.md
+++ akka-docs/src/main/paradox/stream/operators/Flow/asFlowWithContext.md
@@ -1,6 +1,6 @@
 # Flow.asFlowWithContext
 
-Turns a Flow into a FlowWithContext which can propagate a context per element along a stream.
+Extracts context data from the elements of a `Flow` so that it can be turned into a `FlowWithContext` which can propagate that context per element along a stream.
 
 @ref[Simple operators](../index.md#simple-operators)
 
@@ -12,6 +12,21 @@ Turns a Flow into a FlowWithContext which can propagate a context per element al
 
 See @ref[Context Propagation](../../stream-context.md) for a general overview of context propagation.
 
-Turns a @apidoc[Flow] into a @apidoc[FlowWithContext] which can propagate a context per element along a stream.
+Extracts context data from the elements of a @apidoc[Flow] so that it can be turned into a @apidoc[FlowWithContext] which can propagate that context per element along a stream.
 The first function passed into `asFlowWithContext` must turn each incoming pair of element and context value into an element of this @apidoc[Flow].
 The second function passed into `asFlowWithContext` must turn each outgoing element of this @apidoc[Flow] into an outgoing context value.
+
+See also:
+
+* @ref[Context Propagation](../../stream-context.md)
+* @ref[`Source.asSourceWithContext`](../Source/asSourceWithContext.md) Turns a `Source` into a `SourceWithContext` which can propagate a context per element along a stream.
+
+## Example
+
+Elements from this flow have a correlation number, but the flow structure should focus on the text message in the elements. The first converter in `asFlowWithContext` applies to the end of the "with context" flow to turn it into a regular flow again. The second converter function chooses the second value in the @scala[tuple]@java[pair] as the context. Another `map` operator makes the first value the stream elements in the `FlowWithContext`. 
+
+Scala
+:  @@snip [snip](/akka-docs/src/test/scala/docs/stream/operators/WithContextSpec.scala) { #asFlowWithContext }
+
+Java
+:  @@snip [snip](/akka-docs/src/test/java/jdocs/stream/operators/WithContextTest.java) { #imports #asFlowWithContext }
---
+++ akka-docs/src/test/scala/docs/stream/operators/WithContextSpec.scala
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2018-2020 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package docs.stream.operators
+
+import akka.testkit.AkkaSpec
+
+class WithContextSpec extends AkkaSpec {
+
+  "use asSourceWithContext" in {
+    // #asSourceWithContext
+    import akka.NotUsed
+    import akka.stream.scaladsl.Source
+    import akka.stream.scaladsl.SourceWithContext
+    import scala.collection.immutable
+
+    // values with their contexts as tuples
+    val values: immutable.Seq[(String, Int)] = immutable.Seq("eins" -> 1, "zwei" -> 2, "drei" -> 3)
+
+    // a regular source with the tuples as elements
+    val source: Source[(String, Int), NotUsed] = Source(values)
+
+    // split the tuple into stream elements and their context
+    val sourceWithContext: SourceWithContext[String, Int, NotUsed] =
+      source
+        .asSourceWithContext(_._2) // pick the second tuple element as context
+        .map(_._1) // keep the first tuple element as stream element
+
+    val mapped: SourceWithContext[String, Int, NotUsed] = sourceWithContext
+    // regular operators apply to the element without seeing the context
+      .map(s => s.reverse)
+
+    // running the source and asserting the outcome
+    import akka.stream.scaladsl.Sink
+    val result = mapped.runWith(Sink.seq)
+    result.futureValue should contain theSameElementsInOrderAs immutable.Seq("snie" -> 1, "iewz" -> 2, "ierd" -> 3)
+    // #asSourceWithContext
+  }
+
+  "use asFlowWithContext" in {
+    // #asFlowWithContext
+    import akka.NotUsed
+    import akka.stream.scaladsl.Flow
+    import akka.stream.scaladsl.FlowWithContext
+    // a regular flow with pairs as elements
+    val flow: Flow[(String, Int), (String, Int), NotUsed] = // ???
+      // #asFlowWithContext
+      Flow[(String, Int)]
+    // #asFlowWithContext
+
+    // Declare the "flow with context"
+    // ingoing: String and Integer
+    // outgoing: String and Integer
+    val flowWithContext: FlowWithContext[String, Int, String, Int, NotUsed] =
+      // convert the flow of pairs into a "flow with context"
+      flow
+        .asFlowWithContext[String, Int, Int](
+          // at the end of this flow: put the elements and the context back into a tuple
+          collapseContext = Tuple2.apply)(
+          // pick the second element of the incoming pair as context
+          extractContext = _._2)
+        .map(_._1) // keep the first pair element as stream element
+
+    val mapped = flowWithContext
+    // regular operators apply to the element without seeing the context
+      .map(_.reverse)
+
+    // running the flow with some sample data and asserting the outcome
+    import akka.stream.scaladsl.Source
+    import akka.stream.scaladsl.Sink
+    import scala.collection.immutable
+
+    val values: immutable.Seq[(String, Int)] = immutable.Seq("eins" -> 1, "zwei" -> 2, "drei" -> 3)
+    val source = Source(values).asSourceWithContext(_._2).map(_._1)
+
+    val result = source.via(mapped).runWith(Sink.seq)
+    result.futureValue should contain theSameElementsInOrderAs immutable.Seq("snie" -> 1, "iewz" -> 2, "ierd" -> 3)
+    // #asFlowWithContext
+  }
+}
