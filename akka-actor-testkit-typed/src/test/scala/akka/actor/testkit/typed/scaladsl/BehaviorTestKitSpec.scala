4e29b60a4b145a0869d6e613828fdfcb74a09a0d
Eyal Farago-Hagag <efarago@salesforce.com>
2020-12-16 07:48:29 +0200

    akka-29900__stubbed_timer: remove unused import

--- akka-actor-testkit-typed/src/test/scala/akka/actor/testkit/typed/scaladsl/BehaviorTestKitSpec.scala
+++ akka-actor-testkit-typed/src/test/scala/akka/actor/testkit/typed/scaladsl/BehaviorTestKitSpec.scala
@@ -4,22 +4,21 @@
 
 package akka.actor.testkit.typed.scaladsl
 
-import scala.reflect.ClassTag
+import akka.Done
+import akka.actor.Address
+import akka.actor.testkit.typed.Effect._
+import akka.actor.testkit.typed.scaladsl.BehaviorTestKitSpec.Parent._
+import akka.actor.testkit.typed.scaladsl.BehaviorTestKitSpec.{Child, Parent}
+import akka.actor.testkit.typed.{CapturedLogEvent, Effect}
+import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior, Props, Terminated}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.slf4j.event.Level
-import akka.Done
-import akka.actor.Address
-import akka.actor.testkit.typed.{ CapturedLogEvent, Effect }
-import akka.actor.testkit.typed.Effect._
-import akka.actor.testkit.typed.scaladsl.BehaviorTestKitSpec.{ Child, Parent }
-import akka.actor.testkit.typed.scaladsl.BehaviorTestKitSpec.Parent._
-import akka.actor.typed.{ ActorRef, Behavior, Props, Terminated }
-import akka.actor.typed.receptionist.{ Receptionist, ServiceKey }
-import akka.actor.typed.scaladsl.Behaviors
 
-import scala.concurrent.duration.{ Duration, FiniteDuration }
-import scala.concurrent.duration._
+import scala.concurrent.duration.{FiniteDuration, _}
+import scala.reflect.ClassTag
 
 object BehaviorTestKitSpec {
   object Parent {
