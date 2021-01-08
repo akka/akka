31f654768f86db68f4c22daa2cbd0bae28fc1fad
Arnout Engelen <arnout@bzzt.net>
2021-01-08 17:55:38 +0100

    Update copyright to 2021

--- akka-docs/src/test/java/jdocs/stream/operators/sink/Lazy.java
+++ akka-docs/src/test/java/jdocs/stream/operators/sink/Lazy.java
@@ -1,11 +1,8 @@
 /*
- * Copyright (C) 2020 Lightbend Inc. <https://www.lightbend.com>
+ * Copyright (C) 2020-2021 Lightbend Inc. <https://www.lightbend.com>
  */
 
 package jdocs.stream.operators.sink;
-/*
- * Copyright (C) 2009-2020 Lightbend Inc. <https://www.lightbend.com>
- */
 
 import akka.actor.ActorSystem;
 import akka.stream.javadsl.Keep;
