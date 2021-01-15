c83b57674f5ea97b1f7c9e2ca80307908a105fc9
Patrik Nordwall <patrik.nordwall@gmail.com>
2021-01-15 17:13:40 +0100

    update latestPatchOf26 to 2.6.11 (#29952)

--- project/MiMa.scala
+++ project/MiMa.scala
@@ -13,7 +13,7 @@ import com.typesafe.tools.mima.plugin.MimaPlugin.autoImport._
 object MiMa extends AutoPlugin {
 
   private val latestPatchOf25 = 32
-  private val latestPatchOf26 = 10
+  private val latestPatchOf26 = 11
 
   override def requires = MimaPlugin
   override def trigger = allRequirements
