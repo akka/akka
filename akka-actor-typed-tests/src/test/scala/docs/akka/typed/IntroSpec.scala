a083405e4870faea546929dd8f193997a6e0ac1f
taketora <utakaida@yahoo.co.jp>
2021-02-13 23:34:45 +0900

    remove object name because it doesn't work if you write it as documented

--- akka-actor-typed-tests/src/test/scala/docs/akka/typed/IntroSpec.scala
+++ akka-actor-typed-tests/src/test/scala/docs/akka/typed/IntroSpec.scala
@@ -219,7 +219,7 @@ object IntroSpec {
         val chatRoom = context.spawn(ChatRoom(), "chatroom")
         val gabblerRef = context.spawn(Gabbler(), "gabbler")
         context.watch(gabblerRef)
-        chatRoom ! ChatRoom.GetSession("ol’ Gabbler", gabblerRef)
+        chatRoom ! GetSession("ol’ Gabbler", gabblerRef)
 
         Behaviors.receiveSignal {
           case (_, Terminated(_)) =>
