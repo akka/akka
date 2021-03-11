20a8c590cb51792707de870ce2cd1532c9346716
Scala Steward <me@scala-steward.org>
2021-03-11 06:05:01 +0100

    Update junit to 4.13.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
 
   lazy val java8CompatVersion = settingKey[String]("The version of scala-java8-compat to use.")
 
-  val junitVersion = "4.13.1"
+  val junitVersion = "4.13.2"
   val slf4jVersion = "1.7.30"
   // check agrona version when updating this
   val aeronVersion = "1.32.0"
