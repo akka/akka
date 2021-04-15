f66f402cb4c80172f29d78895d879b5e63d57c5d
Muskan Gupta <muskan.gupta3006@gmail.com>
2021-04-15 20:32:17 +0530

    format

--- akka-docs/src/test/scala/docs/stream/operators/sink/AsPublisher.scala
+++ akka-docs/src/test/scala/docs/stream/operators/sink/AsPublisher.scala
@@ -4,30 +4,24 @@
 
 package docs.stream.operators.sink
 
-import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.concurrent.{ ExecutionContextExecutor, Future }
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{ Sink, Source }
 
 object AsPublisher {
   implicit val system: ActorSystem = ???
   implicit val ec: ExecutionContextExecutor = system.dispatcher
   def asPublisherExample() = {
-    //#asPublisher
-    val source = Source(1 to 5)
-    val publisherTrue = source.runWith(Sink.asPublisher(true))
+    def asPublisherExample() = {
+      //#asPublisher
+      val source = Source(1 to 5)
 
-    val resultFromFirstSubscriberTrue: Future[Int] = Source.fromPublisher(publisherTrue).runWith(Sink.fold(0)((acc, element) => acc + element))
-    val resultFromSecondSubscriberTrue: Future[Int] = Source.fromPublisher(publisherTrue).runWith(Sink.fold(1)((acc, element) => acc * element))
-
-    resultFromFirstSubscriberTrue.map(println) //15
-    resultFromSecondSubscriberTrue.map(println) //120
-
-    val publisherFalse = source.runWith(Sink.asPublisher(false))
-    val resultFromFirstSubscriberFalse: Future[Int] = Source.fromPublisher(publisherFalse).runWith(Sink.fold(0)((acc, element) => acc + element))
-    val resultFromSecondSubscriberFalse: Future[Int] = Source.fromPublisher(publisherFalse).runWith(Sink.fold(1)((acc, element) => acc * element))
-
-    resultFromFirstSubscriberFalse.map(println) //15
-    resultFromSecondSubscriberFalse.map(println) //No output, because the source was not able to subscribe to the publisher.
-    //#asPublisher
+      val publisher = source.runWith(Sink.asPublisher(false))
+      Source.fromPublisher(publisher).runWith(Sink.foreach(println)) // 1 2 3 4 5
+      Source
+        .fromPublisher(publisher)
+        .runWith(Sink.foreach(println)) //No output, because the source was not able to subscribe to the publisher.
+      //#asPublisher
+    }
   }
 }
