084b0dff2194ed821eca97f2616a5a7f3329ded9
Johan Andrén <johan@markatta.com>
2021-06-02 13:53:58 +0200

    Missing import added

--- akka-remote/src/main/scala/akka/remote/ByteStringUtils.scala
+++ akka-remote/src/main/scala/akka/remote/ByteStringUtils.scala
@@ -7,7 +7,8 @@ package akka.remote
 import akka.annotation.InternalApi
 import akka.protobufv3.internal.UnsafeByteOperations
 import akka.util.ByteString
-import akka.protobufv3.internal.{ ByteString => ProtoByteString }
+import akka.protobufv3.internal.{ByteString => ProtoByteString}
+import akka.util.ByteString.ByteString1
 import akka.util.ByteString.ByteString1C
 
 /**
