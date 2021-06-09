a5211d61e6ca301df665b9a0a1f729b504e238ad
Scala Steward <me@scala-steward.org>
2021-06-09 09:30:07 +0200

    Update gson to 2.8.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -131,7 +131,7 @@ object Dependencies {
 
     object Docs {
       val sprayJson = "io.spray" %% "spray-json" % "1.3.6" % "test"
-      val gson = "com.google.code.gson" % "gson" % "2.8.6" % "test"
+      val gson = "com.google.code.gson" % "gson" % "2.8.7" % "test"
     }
 
     object Test {
