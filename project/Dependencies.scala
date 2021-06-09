f73a48f97a0d0af70d671ea8737b3bea9c739155
Scala Steward <me@scala-steward.org>
2021-06-09 09:31:33 +0200

    Update scala-library to 2.13.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
   val jacksonVersion = "2.11.4"
 
   val scala212Version = "2.12.13"
-  val scala213Version = "2.13.5"
+  val scala213Version = "2.13.6"
   val scala3Version = "3.0.0"
 
   val reactiveStreamsVersion = "1.0.3"
