0bafbce32797c989bd3aa573c42c8ebd0f2b201b
economist <32593716+TigranOhanyan@users.noreply.github.com>
2021-07-08 11:51:53 +0400

    stream: fix client-side mutual TLS bug (#29534)
    Connected issues:
    https://github.com/lightbend/ssl-config/issues/62
    https://github.com/akka/akka-http/issues/1433
    
    Co-authored-by: Renato Cavalcanti <renato@cavalcanti.be>

--- akka-stream/src/main/scala/akka/stream/impl/io/TLSActor.scala
+++ akka-stream/src/main/scala/akka/stream/impl/io/TLSActor.scala
@@ -497,14 +497,15 @@ import akka.util.ByteString
   def applySessionParameters(engine: SSLEngine, sessionParameters: NegotiateNewSession): Unit = {
     sessionParameters.enabledCipherSuites.foreach(cs => engine.setEnabledCipherSuites(cs.toArray))
     sessionParameters.enabledProtocols.foreach(p => engine.setEnabledProtocols(p.toArray))
+
+    sessionParameters.sslParameters.foreach(engine.setSSLParameters)
+
     sessionParameters.clientAuth match {
       case Some(TLSClientAuth.None) => engine.setNeedClientAuth(false)
       case Some(TLSClientAuth.Want) => engine.setWantClientAuth(true)
       case Some(TLSClientAuth.Need) => engine.setNeedClientAuth(true)
       case _                        => // do nothing
     }
-
-    sessionParameters.sslParameters.foreach(engine.setSSLParameters)
   }
 
   def cloneParameters(old: SSLParameters): SSLParameters = {
