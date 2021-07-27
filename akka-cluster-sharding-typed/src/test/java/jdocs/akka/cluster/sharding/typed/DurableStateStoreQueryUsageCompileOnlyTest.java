2cb1738e1b0474c680cbd557d88090c5b9bdaaaf
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2021-07-27 21:06:46 +0200

    Added exampled hopw to get changes from DurableStateStoreQuery.

--- akka-cluster-sharding-typed/src/test/java/jdocs/akka/cluster/sharding/typed/DurableStateStoreQueryUsageCompileOnlyTest.java
+++ akka-cluster-sharding-typed/src/test/java/jdocs/akka/cluster/sharding/typed/DurableStateStoreQueryUsageCompileOnlyTest.java
@@ -4,18 +4,25 @@
 
 package jdocs.akka.cluster.sharding.typed;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.stream.javadsl.Source;
+import akka.persistence.query.DurableStateChange;
+import akka.persistence.query.Offset;
 import akka.persistence.query.javadsl.DurableStateStoreQuery;
 import akka.persistence.state.DurableStateStoreRegistry;
 import akka.persistence.state.javadsl.*;
 
 class DurableStateStoreQueryUsageCompileOnlySpec {
 
-  public <Record> DurableStateStoreQuery<Record> getQuery(ActorSystem system, String pluginId) {
+  public <Record> DurableStateStoreQuery<Record> getQuery(
+      ActorSystem system, String pluginId, Offset offset) {
     // #get-durable-state-store-query-example
     DurableStateStoreQuery<Record> durableStateStoreQuery =
         DurableStateStoreRegistry.get(system)
             .getDurableStateStoreFor(DurableStateStoreQuery.class, pluginId);
+    Source<DurableStateChange<Record>, NotUsed> source =
+        durableStateStoreQuery.changes("tag", offset);
     // #get-durable-state-store-query-example
     return durableStateStoreQuery;
   }
