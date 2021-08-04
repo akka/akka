f515ac4b8eb081b26cdd2c2b83cbec45128cd672
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2021-08-04 14:35:33 +0200

    split aggregated projects

--- scripts/gh-generate-projects-matrix.sh
+++ scripts/gh-generate-projects-matrix.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-
-
-
-sbt -no-colors --error printAggregatedProjects
+# Reset project list in jobs
+yq e -i '.jobs.*.strategy.matrix.project = []' .github/workflows/multi-node.yml
+# Append the aggregated projects one-by-one
+sbt -no-colors --error printAggregatedProjects | xargs -I {} sh -c 'yq e -i ".jobs.*.strategy.matrix.project += \"$1\"" .github/workflows/multi-node.yml' sh {}
