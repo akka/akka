c1d94293dbddceba91230990e6d5b908f1d746a1
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2021-08-04 17:00:39 +0200

    only multi node

--- scripts/gh-generate-projects-matrix.sh
+++ scripts/gh-generate-projects-matrix.sh
@@ -1,6 +1,20 @@
 #! /bin/bash
 
-# Reset project list in jobs
+rm -f all-projects.txt
+rm -f multi-node-projects.txt
+
+sbt -no-colors --error printAggregatedProjects > all-projects.txt
+touch multi-node-projects.txt
+
+cat all-projects.txt | while read line 
+do
+  if sh -c "sbt -no-colors --error $line/multi-jvm:scalaSource" ; then
+    echo "${line}" >> multi-node-projects.txt
+  fi
+done
+
 yq e -i '.jobs.*.strategy.matrix.project = []' .github/workflows/multi-node.yml
-# Append the aggregated projects one-by-one
-sbt -no-colors --error printAggregatedProjects | xargs -I {} sh -c 'yq e -i ".jobs.*.strategy.matrix.project += \"$1\"" .github/workflows/multi-node.yml' sh {}
+cat multi-node-projects.txt | while read line 
+do
+  yq e -i ".jobs.*.strategy.matrix.project += \"$line\"" .github/workflows/multi-node.yml
+done
