75fc6c4f559f4fbc4c4f2d7f7b5b6b87834d205a
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2021-08-06 15:46:57 +0200

    don't leak keys

--- kubernetes/generateKeys.sh
+++ kubernetes/generateKeys.sh
@@ -4,13 +4,10 @@ rm -rf keys
 mkdir -p keys
 
 cp ${HOME}/.ssh/id_rsa.pub keys/id_rsa.pub
-cp ${HOME}/.ssh/id_rsa keys/id_rsa
 
 cp keys/id_rsa.pub keys/authorized_keys
 
 kubectl delete secret ssh-keys | true
 
 kubectl create secret generic ssh-keys \
---from-file=keys/id_rsa \
---from-file=keys/id_rsa.pub \
 --from-file=keys/authorized_keys
