a20204e78566354244338aa8b47b7d2e91c8fe8f
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2021-08-24 13:29:43 +0100

    more

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -13,7 +13,7 @@ touch ${DEST_HOST_FILE}
 
 for i in `seq 1 "${NUM_OF_NODES}"`;
 do
-  cat test-node-base.yaml | sed "s/test-nodeX/test-node${i}/" > ".tmp/test-node${i}.yml"
+  cat ./kubernetes/test-node-base.yaml | sed "s/test-nodeX/test-node${i}/" > ".tmp/test-node${i}.yml"
   echo $i
   echo "test-node${i}:/usr/lib/jvm/java-11-openjdk-amd64/bin/java -Dmultinode.port=5000" >> ${DEST_HOST_FILE}
 done
