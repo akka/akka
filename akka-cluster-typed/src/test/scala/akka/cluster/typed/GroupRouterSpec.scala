341d4cd9a6f1016727d48502f179d8b02fb86240
Johan Andrén <johan@markatta.com>
2021-09-10 18:32:33 +0200

    Stabilize group router spec #30671

--- akka-cluster-typed/src/test/scala/akka/cluster/typed/GroupRouterSpec.scala
+++ akka-cluster-typed/src/test/scala/akka/cluster/typed/GroupRouterSpec.scala
@@ -4,17 +4,21 @@
 
 package akka.cluster.typed
 
-import scala.concurrent.Promise
-
+import akka.actor.testkit.typed.scaladsl.LogCapturing
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.typed.ActorRef
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.Behavior
+import akka.actor.typed.receptionist.Receptionist
+import akka.actor.typed.receptionist.ServiceKey
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.GroupRouter
+import akka.actor.typed.scaladsl.Routers
+import akka.serialization.jackson.CborSerializable
 import com.typesafe.config.ConfigFactory
 import org.scalatest.wordspec.AnyWordSpecLike
 
-import akka.actor.testkit.typed.scaladsl.{ LogCapturing, ScalaTestWithActorTestKit }
-import akka.actor.typed.{ ActorRef, ActorSystem, Behavior }
-import akka.actor.typed.receptionist.{ Receptionist, ServiceKey }
-import akka.actor.typed.scaladsl.{ Behaviors, GroupRouter, Routers }
-import akka.serialization.jackson.CborSerializable
-import akka.util.Timeout
+import scala.concurrent.duration._
 
 object GroupRouterSpec {
   def config = ConfigFactory.parseString(s"""
@@ -31,153 +35,166 @@ object GroupRouterSpec {
       }
     }
   """)
-  def createSystem[T](behavior: Behavior[T], name: String) = ActorSystem(behavior, name, config)
 
-  case object Ping extends CborSerializable
+  object PingActor {
+    case class Pong(workerActor: ActorRef[_]) extends CborSerializable
+    case class Ping(replyTo: ActorRef[Pong]) extends CborSerializable
 
-  sealed trait Command
-  case class UpdateWorker(actorRef: ActorRef[Ping.type]) extends Command
-  case class GetWorkers(replyTo: ActorRef[Seq[ActorRef[Ping.type]]]) extends Command
-
-  // receive Ping and send self to statsActorRef, so the statsActorRef will know who handle the message
-  def pingPong(statsActorRef: ActorRef[Command]) = Behaviors.receive[Ping.type] { (ctx, msg) =>
-    msg match {
-      case Ping =>
-        statsActorRef ! UpdateWorker(ctx.self)
+    def apply(): Behavior[Ping] = Behaviors.receive {
+      case (ctx, Ping(replyTo)) =>
+        replyTo ! Pong(ctx.self)
         Behaviors.same
     }
   }
 
-  // stats the actors who handle the messages and gives the result if someone ask
-  def workerStatsBehavior(list: List[ActorRef[Ping.type]]): Behavior[Command] = Behaviors.receiveMessage {
-    case UpdateWorker(actorRef) =>
-      workerStatsBehavior(actorRef :: list)
-    case GetWorkers(replyTo) =>
-      replyTo ! list
-      Behaviors.same
+  object Pinger {
+    sealed trait Command
+    private case class SawPong(worker: ActorRef[_]) extends Command
+    private case object RetryPinging extends Command
+    case class DonePinging(pongs: Int, uniqueWorkers: Set[ActorRef[_]])
+    def apply(router: ActorRef[PingActor.Ping], requestedPings: Int, tellMeWhenDone: ActorRef[DonePinging]): Behavior[Command] =
+      Behaviors.setup { ctx =>
+        val pongAdapter = ctx.messageAdapter[PingActor.Pong] {
+          case PingActor.Pong(ref) => SawPong(ref)
+        }
+        ctx.setReceiveTimeout(1.second, RetryPinging)
+        def sendPing(): Unit = {
+          router ! PingActor.Ping(pongAdapter)
+        }
+        (0 to requestedPings).foreach(_ => sendPing())
+        var pongs = 0
+        var uniqueWorkers = Set.empty[ActorRef[_]]
+        var retriesLeft = 3
+
+        Behaviors.receiveMessage {
+          case SawPong(worker) =>
+            pongs += 1
+            uniqueWorkers += worker
+            if (pongs >= requestedPings) {
+              tellMeWhenDone ! DonePinging(pongs, uniqueWorkers)
+              Behaviors.stopped
+            } else {
+              Behaviors.same
+            }
+          case RetryPinging =>
+            // delivery isn't guaranteed so retry sending if we didn't get resonses in a timely fashion
+            retriesLeft -= 1
+            if (retriesLeft == 0) {
+              ctx.log.warn("Gave up retrying re-sending")
+              tellMeWhenDone ! DonePinging(pongs, uniqueWorkers)
+              Behaviors.stopped
+            } else {
+              // unlikely we lost all, so resend a fraction
+              ctx.log.debug("Retrying sending")
+              (0 to (requestedPings / 5)).foreach(_ => sendPing())
+              Behaviors.same
+            }
+        }
+      }
+
   }
 
   case class GroupRouterSpecSettings(node1WorkerCount: Int, node2WorkerCount: Int, messageCount: Int)
 
-  val pingPongKey = ServiceKey[Ping.type]("ping-pong")
+  val pingPongKey = ServiceKey[PingActor.Ping]("ping-pong")
+
+  case class Result(actorsInSystem1: Int, actorsInSystem2: Int)
 
 }
 
 class GroupRouterSpec extends ScalaTestWithActorTestKit(GroupRouterSpec.config) with AnyWordSpecLike with LogCapturing {
   import GroupRouterSpec._
 
-  def checkGroupRouterBehavior[T](groupRouter: GroupRouter[Ping.type], settings: GroupRouterSpecSettings)(
-      resultCheck: (Seq[ActorRef[Ping.type]], Seq[ActorRef[Ping.type]]) => T): T = {
-    import scala.concurrent.duration._
+  /**
+   * Starts a new pair of nodes, forms a cluster, runs a ping-pong session and hands the result to the test for verification
+   */
+  def checkGroupRouterBehavior(groupRouter: GroupRouter[PingActor.Ping], settings: GroupRouterSpecSettings): Result = {
 
-    import akka.actor.typed.scaladsl.AskPattern._
-    implicit val system1 =
-      createSystem(
-        Behaviors.setup[Command] { ctx =>
-          (0 until settings.node1WorkerCount).foreach { i =>
-            val worker = ctx.spawn(pingPong(ctx.self), s"ping-pong-$i")
-            ctx.system.receptionist ! Receptionist.Register(pingPongKey, worker)
-          }
-          val router = ctx.spawn(groupRouter, "group-router")
+    val resultProbe = testKit.createTestProbe[Pinger.DonePinging]()
 
-          // ensure all nodes are joined and all actors are discovered.
-          ctx.system.receptionist ! Receptionist.Subscribe(
-            pingPongKey,
-            ctx.spawn(Behaviors.receiveMessage[Receptionist.Listing] {
-              case pingPongKey.Listing(update)
-                  if update.size == settings.node1WorkerCount + settings.node2WorkerCount =>
-                (0 until settings.messageCount).foreach(_ => router ! Ping)
-                Behaviors.empty
-              case _ =>
-                Behaviors.same
-            }, "waiting-actor-discovery"))
-          workerStatsBehavior(List.empty)
-        },
-        system.name)
-
-    val system2 = createSystem(Behaviors.setup[Command] { ctx =>
-      (0 until settings.node2WorkerCount).foreach { i =>
-        val worker = ctx.spawn(pingPong(ctx.self), s"ping-pong-$i")
+    val system1 = ActorSystem(Behaviors.setup[Receptionist.Listing] { ctx =>
+      (0 until settings.node1WorkerCount).foreach { i =>
+        val worker = ctx.spawn(PingActor(), s"ping-pong-$i")
         ctx.system.receptionist ! Receptionist.Register(pingPongKey, worker)
       }
-      workerStatsBehavior(List.empty)
-    }, system.name)
-    val node1 = Cluster(system1)
-    node1.manager ! Join(node1.selfMember.address)
-    val node2 = Cluster(system2)
-    node2.manager ! Join(node1.selfMember.address)
+      val router = ctx.spawn(groupRouter, "group-router")
+        ctx.system.receptionist ! Receptionist.Subscribe(pingPongKey, ctx.self)
 
-    val statsPromise = Promise[(Seq[ActorRef[Ping.type]], Seq[ActorRef[Ping.type]])]()
-    val cancelable = system.scheduler.scheduleAtFixedRate(200.millis, 200.millis)(() => {
-      implicit val timeout = Timeout(3.seconds)
-      val actorRefsInNode1 = system1.ask[Seq[ActorRef[Ping.type]]](ref => GetWorkers(ref)).futureValue
-      val actorRefsInNode2 = system2.ask[Seq[ActorRef[Ping.type]]](ref => GetWorkers(ref)).futureValue
-      // waiting all messages are handled
-      if (actorRefsInNode1.size + actorRefsInNode2.size == settings.messageCount && !statsPromise.isCompleted) {
-        statsPromise.success((actorRefsInNode1, actorRefsInNode2))
+        Behaviors.receiveMessage {
+          case pingPongKey.Listing(update)
+            if update.size == settings.node1WorkerCount + settings.node2WorkerCount =>
+              // the requested number of workers are started and registered with the receptionist
+              // start pinging them
+              ctx.log.debug("Saw {} workers, starting pinger", update.size)
+              ctx.spawn(Pinger(router, settings.messageCount, resultProbe.ref), "pinger")
+              // ignore further listings
+              Behaviors.empty
+            case _ =>
+              Behaviors.same
+          }
+        },
+        system.name,
+        config)
+
+    val system2 = ActorSystem(Behaviors.setup[Unit] { ctx =>
+      (0 until settings.node2WorkerCount).foreach { i =>
+        val worker = ctx.spawn(PingActor(), s"ping-pong-$i")
+        ctx.system.receptionist ! Receptionist.Register(pingPongKey, worker)
       }
-    })(system.executionContext)
+      Behaviors.empty
+    },
+      system.name,
+      config)
 
     try {
-      val stats = statsPromise.future.futureValue
-      cancelable.cancel()
-      resultCheck(stats._1, stats._2)
+      val node1 = Cluster(system1)
+      node1.manager ! Join(node1.selfMember.address)
+      val node2 = Cluster(system2)
+      node2.manager ! Join(node1.selfMember.address)
+
+      val donePinging = resultProbe.receiveMessage(10.seconds)
+      if (donePinging.pongs < settings.messageCount) {
+        fail(s"Expected ${settings.messageCount} ping-pongs but only saw ${donePinging.pongs}")
+      }
+      // pinger runs in system 1 so those workers are local
+      val (workersInAddress1, workersInAddress2) = donePinging.uniqueWorkers.partition(_.path.address.hasLocalScope)
+      Result(workersInAddress1.size, workersInAddress2.size)
     } finally {
       system1.terminate()
       system2.terminate()
     }
   }
   "GroupRouter" must {
-    "use all reachable routees if preferLocalRoutees is not enabled" in {
+    // default is to not preferLocalRoutees
+    List(
+      "random" -> Routers.group(pingPongKey).withRandomRouting(), // default group is same as this
+      "round robin" -> Routers.group(pingPongKey).withRoundRobinRouting()).foreach { case (strategy, groupRouter) =>
 
-      val settings = GroupRouterSpecSettings(node1WorkerCount = 2, node2WorkerCount = 2, messageCount = 100)
-
-      val groupRouters = List(
-        Routers.group(pingPongKey),
-        Routers.group(pingPongKey).withRandomRouting(),
-        Routers.group(pingPongKey).withRoundRobinRouting(),
-        Routers.group(pingPongKey).withRoundRobinRouting(false),
-        Routers.group(pingPongKey).withRandomRouting(false))
-
-      groupRouters.foreach { groupRouter =>
-        checkGroupRouterBehavior(groupRouter, settings) {
-          case (actorRefsInNode1, actorRefsInNode2) =>
-            (actorRefsInNode1.size + actorRefsInNode2.size) shouldBe settings.messageCount
-            actorRefsInNode1.toSet.size shouldBe settings.node1WorkerCount
-            actorRefsInNode2.toSet.size shouldBe settings.node2WorkerCount
-        }
+      s"use all reachable routees if preferLocalRoutees is not enabled, strategy $strategy" in {
+        val settings = GroupRouterSpecSettings(node1WorkerCount = 2, node2WorkerCount = 2, messageCount = 100)
+        val result = checkGroupRouterBehavior(groupRouter, settings)
+        result.actorsInSystem1 should === (settings.node1WorkerCount)
+        result.actorsInSystem2 should === (settings.node2WorkerCount)
       }
     }
 
-    "only use local routees if preferLocalRoutees is enabled and there are local routees" in {
-      val settings = GroupRouterSpecSettings(node1WorkerCount = 2, node2WorkerCount = 2, messageCount = 100)
+    List(
+      "random" -> Routers.group(pingPongKey).withRandomRouting(true),
+      "round robin" -> Routers.group(pingPongKey).withRoundRobinRouting(true)
+    ).foreach { case (strategy, groupRouter) =>
 
-      val groupRouters =
-        List(Routers.group(pingPongKey).withRoundRobinRouting(true), Routers.group(pingPongKey).withRandomRouting(true))
-
-      groupRouters.foreach { groupRouter =>
-        checkGroupRouterBehavior(groupRouter, settings) {
-          case (actorRefsInNode1, actorRefsInNode2) =>
-            actorRefsInNode1.size shouldBe settings.messageCount
-            actorRefsInNode1.toSet.size shouldBe settings.node1WorkerCount
-            actorRefsInNode2.toSet.size shouldBe 0
-        }
+      s"only use local routees if preferLocalRoutees is enabled and there are local routees, strategy $strategy" in {
+        val settings = GroupRouterSpecSettings(node1WorkerCount = 2, node2WorkerCount = 2, messageCount = 100)
+        val result = checkGroupRouterBehavior(groupRouter, settings)
+        result.actorsInSystem1 should === (settings.node1WorkerCount)
+        result.actorsInSystem2 should === (0)
       }
-    }
 
-    "use remote routees if preferLocalRoutees is enabled but there is no local routees" in {
-      val settings = GroupRouterSpecSettings(node1WorkerCount = 0, node2WorkerCount = 2, messageCount = 100)
-
-      val groupRouters =
-        List(Routers.group(pingPongKey).withRoundRobinRouting(true), Routers.group(pingPongKey).withRandomRouting(true))
-
-      groupRouters.foreach { groupRouter =>
-        checkGroupRouterBehavior(groupRouter, settings) {
-          case (actorRefsInNode1, actorRefsInNode2) =>
-            actorRefsInNode2.size shouldBe settings.messageCount
-            actorRefsInNode1.toSet.size shouldBe 0
-            actorRefsInNode2.toSet.size shouldBe settings.node2WorkerCount
-        }
+      s"use remote routees if preferLocalRoutees is enabled but there is no local routees, strategy $strategy" in {
+        val settings = GroupRouterSpecSettings(node1WorkerCount = 0, node2WorkerCount = 2, messageCount = 100)
+        val result = checkGroupRouterBehavior(groupRouter, settings)
+        result.actorsInSystem1 should ===(0)
+        result.actorsInSystem2 should ===(settings.node2WorkerCount)
       }
     }
   }
