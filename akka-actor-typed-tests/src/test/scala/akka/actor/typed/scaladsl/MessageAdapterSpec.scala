7cfdb64de22236f2555a461588acc908c1c80dd1
Patrik Nordwall <patrik.nordwall@gmail.com>
2021-09-23 10:14:39 +0200

    Harden MessageAdapterSpec, #30698 (#30702)

--- akka-actor-typed-tests/src/test/scala/akka/actor/typed/scaladsl/MessageAdapterSpec.scala
+++ akka-actor-typed-tests/src/test/scala/akka/actor/typed/scaladsl/MessageAdapterSpec.scala
@@ -6,8 +6,11 @@ package akka.actor.typed.scaladsl
 
 import com.typesafe.config.ConfigFactory
 import org.scalatest.wordspec.AnyWordSpecLike
+import org.slf4j.LoggerFactory
 
+import akka.actor.DeadLetter
 import akka.actor.testkit.typed.TestException
+import akka.actor.testkit.typed.scaladsl.FishingOutcomes
 import akka.actor.testkit.typed.scaladsl.LogCapturing
 import akka.actor.testkit.typed.scaladsl.LoggingTestKit
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
@@ -36,6 +39,26 @@ class MessageAdapterSpec
     extends ScalaTestWithActorTestKit(MessageAdapterSpec.config)
     with AnyWordSpecLike
     with LogCapturing {
+
+  private val log = LoggerFactory.getLogger(getClass)
+
+  private def assertDeadLetter(deadLetterProbe: TestProbe[DeadLetter], expectedMessage: Any): Unit = {
+    deadLetterProbe.fishForMessage(deadLetterProbe.remainingOrDefault, s"looking for DeadLetter $expectedMessage") {
+      deadLetter =>
+        deadLetter.message match {
+          case AdaptMessage(msg, _) =>
+            msg shouldBe expectedMessage
+            FishingOutcomes.complete
+          case msg if msg.getClass == expectedMessage.getClass =>
+            msg shouldBe expectedMessage
+            FishingOutcomes.complete
+          case other =>
+            // something else, not from this test
+            log.debug(s"Ignoring other DeadLetter: $other")
+            FishingOutcomes.continueAndIgnore
+        }
+    }
+  }
 
   "Message adapters" must {
 
@@ -210,22 +233,23 @@ class MessageAdapterSpec
           }
       }
 
-      spawn(snitch)
+      val ref = spawn(snitch)
 
       probe.expectMessage(Wrapped(1, Pong("hello")))
       probe.expectMessage(Wrapped(2, Pong("hello")))
       // exception was thrown for  3
 
       probe.expectMessage("stopped")
+      probe.expectTerminated(ref)
     }
 
     "not catch exception thrown after adapter, when processing the message" in {
-      case class Ping(sender: ActorRef[Pong])
+      case class Ping(n: Int, sender: ActorRef[Pong])
       case class Pong(greeting: String)
       case class Wrapped(response: Pong)
 
       val pingPong = spawn(Behaviors.receiveMessage[Ping] { ping =>
-        ping.sender ! Pong("hello")
+        ping.sender ! Pong(s"hello-${ping.n}")
         Behaviors.same
       })
 
@@ -235,8 +259,8 @@ class MessageAdapterSpec
         val replyTo = context.messageAdapter[Pong] { pong =>
           Wrapped(pong)
         }
-        (1 to 5).foreach { _ =>
-          pingPong ! Ping(replyTo)
+        (1 to 5).foreach { n =>
+          pingPong ! Ping(n, replyTo)
         }
 
         def behv(count: Int): Behavior[Wrapped] =
@@ -260,7 +284,7 @@ class MessageAdapterSpec
       val deadLetterProbe = testKit.createDeadLetterProbe()
 
       // Not expecting "Exception thrown out of adapter. Stopping myself"
-      LoggingTestKit.error[TestException].withMessageContains("boom").expect {
+      val ref = LoggingTestKit.error[TestException].withMessageContains("boom").expect {
         spawn(snitch)
       }
 
@@ -269,17 +293,10 @@ class MessageAdapterSpec
       probe.expectMessage(3)
       // exception was thrown for 3
       probe.expectMessage("stopped")
+      probe.expectTerminated(ref)
 
-      deadLetterProbe.receiveMessage().message match {
-        case AdaptMessage(Pong(greeting), _) => greeting shouldBe "hello"
-        case Pong(greeting)                  => greeting shouldBe "hello"
-        case other                           => fail(s"Unexpected dead letter: [$other]")
-      }
-      deadLetterProbe.receiveMessage().message match {
-        case AdaptMessage(Pong(greeting), _) => greeting shouldBe "hello"
-        case Pong(greeting)                  => greeting shouldBe "hello"
-        case other                           => fail(s"Unexpected dead letter: [$other]")
-      }
+      assertDeadLetter(deadLetterProbe, Pong("hello-4"))
+      assertDeadLetter(deadLetterProbe, Pong("hello-5"))
     }
 
   }
@@ -303,17 +320,7 @@ class MessageAdapterSpec
     createTestProbe().expectTerminated(ref)
 
     pingProbe.receiveMessage().sender ! Pong("hi")
-    val deadLetter = deadLetterProbe.receiveMessage()
-    deadLetter.message match {
-      case AdaptMessage(Pong("hi"), _) => // passed through the FunctionRef
-      case Pong("hi")                  => // FunctionRef stopped
-      case unexpected =>
-        fail(s"""
-             |Unexpected message:
-             |Got => $unexpected: ${unexpected.getClass.getName}
-             |Expected => ${classOf[Pong].getName} or AdaptMessage(${classOf[Pong].getName})
-             |""".stripMargin)
-    }
+    assertDeadLetter(deadLetterProbe, Pong("hi"))
   }
 
 }
