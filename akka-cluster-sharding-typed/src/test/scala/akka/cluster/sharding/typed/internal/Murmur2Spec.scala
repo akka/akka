f8ba39df718c21280e435f4680986ed5144cf61a
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2021-10-01 14:16:40 +0100

    Apply suggestions from code review
    a little note

--- akka-cluster-sharding-typed/src/test/scala/akka/cluster/sharding/typed/internal/Murmur2Spec.scala
+++ akka-cluster-sharding-typed/src/test/scala/akka/cluster/sharding/typed/internal/Murmur2Spec.scala
@@ -12,7 +12,7 @@ import java.nio.charset.StandardCharsets.UTF_8
 class Murmur2Spec extends AnyWordSpecLike with Matchers {
   "The Murmur2 hashing" must {
     // expected correct hash values from the kafka murmur2 impl
-    // https://github.com/apache/kafka/blob/trunk/clients/src/main/java/org/apache/kafka/common/utils/Utils.java#L500
+    // https://github.com/apache/kafka/blob/db42afd6e24ef4291390b4d1c1f10758beedefed/clients/src/main/java/org/apache/kafka/common/utils/Utils.java#L500
     Seq(
       "1" -> -1993445489,
       "12" -> 126087238,
