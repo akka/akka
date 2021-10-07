da7dbf12ee6497c4f705c8fcc3a79b809d331dbb
Patrik Nordwall <patrik.nordwall@gmail.com>
2021-10-07 15:25:36 +0200

    Remove a test with race condition in ClusterSpec, #30743
    * not important

--- akka-cluster/src/test/scala/akka/cluster/ClusterSpec.scala
+++ akka-cluster/src/test/scala/akka/cluster/ClusterSpec.scala
@@ -137,17 +137,12 @@ class ClusterSpec extends AkkaSpec(ClusterSpec.config) with ImplicitSender {
 
     // this should be the last test step, since the cluster is shutdown
     "publish MemberRemoved when shutdown" in {
-      val callbackProbe = TestProbe()
-      cluster.registerOnMemberRemoved(callbackProbe.ref ! "OnMemberRemoved")
-
       cluster.subscribe(testActor, classOf[ClusterEvent.MemberRemoved])
       // first, is in response to the subscription
       expectMsgClass(classOf[ClusterEvent.CurrentClusterState])
 
       cluster.shutdown()
       expectMsgType[ClusterEvent.MemberRemoved].member.address should ===(selfAddress)
-
-      callbackProbe.expectMsg("OnMemberRemoved")
     }
 
     "allow join and leave with local address" in {
