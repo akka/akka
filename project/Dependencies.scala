bd1bce87500722caceccd22a11b7aceee8b890aa
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-10-08 16:24:02 +0200

    Update scala-library to 2.12.15 (#30766)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
 
   val jacksonVersion = "2.11.4"
 
-  val scala212Version = "2.12.14"
+  val scala212Version = "2.12.15"
   val scala213Version = "2.13.6"
   // To get the fix for https://github.com/lampepfl/dotty/issues/13106
   val scala3Version = "3.1.1-RC1-bin-20210915-ea871c2-NIGHTLY"
