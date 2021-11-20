f3df5d5a5ca91ee3db0676598e4a17d490fede97
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-20 09:28:49 +0800

    Delete akka-cluster-sharding/src directory

--- akka-cluster-sharding/src/main/scala/akka/cluster/sharding/external/ClientTimeoutException.scala
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (C) 2019-2021 Lightbend Inc. <https://www.lightbend.com>
- */
-
-package akka.cluster.sharding.external
-
-final class ClientTimeoutException(reason: String) extends RuntimeException(reason)
