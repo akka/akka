aa29071b967176771c21a4473ed8fb0cd6cbc431
Patrik Nordwall <patrik.nordwall@gmail.com>
2021-12-16 09:05:37 +0100

    Update to logback 1.2.8 (#30991)
    * LOGBACK-1591
    * documentation, tests and (provided) testkit

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
   val agronaVersion = "1.14.0"
   val nettyVersion = "3.10.6.Final"
   val protobufJavaVersion = "3.11.4"
-  val logbackVersion = "1.2.7"
+  val logbackVersion = "1.2.8"
 
   val jacksonVersion = "2.11.4"
 
