5d5ed5cf9087bbcd1a0eb36847220f87e1e36d3e
Scala Steward <me@scala-steward.org>
2022-01-05 20:03:07 +0100

    Update logback-classic to 1.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
   val agronaVersion = "1.14.0"
   val nettyVersion = "3.10.6.Final"
   val protobufJavaVersion = "3.11.4"
-  val logbackVersion = "1.2.9"
+  val logbackVersion = "1.2.10"
 
   val jacksonVersion = "2.11.4"
 
