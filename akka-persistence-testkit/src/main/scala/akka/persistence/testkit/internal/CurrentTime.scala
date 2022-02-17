b16cf903551b07a4e59421c32d3c88f287581d57
Patrik Nordwall <patrik.nordwall@gmail.com>
2022-02-17 16:30:38 +0100

    Improve timestamp order for eventsBySlices in Persistence TestKit, #31029 (#31133)
    * probably the failing test produced the same timestamps for two events and then
      the order wasn't defined
    * sort by timestamp and sequence number
    * use monotonic increasing timestamps (could otherwise move backwards)

---
+++ akka-persistence-testkit/src/main/scala/akka/persistence/testkit/internal/CurrentTime.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2021-2022 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.persistence.testkit.internal
+
+import java.util.concurrent.atomic.AtomicLong
+
+import akka.annotation.InternalApi
+
+/**
+ * INTERNAL API
+ */
+@InternalApi private[akka] object CurrentTime {
+  private val previous = new AtomicLong(System.currentTimeMillis())
+
+  /**
+   * `System.currentTimeMillis` but always increasing.
+   */
+  def now(): Long = {
+    val current = System.currentTimeMillis()
+    val prev = previous.get()
+    if (current > prev) {
+      previous.compareAndSet(prev, current)
+      current
+    } else {
+      previous.compareAndSet(prev, prev + 1)
+      prev + 1
+    }
+  }
+}
