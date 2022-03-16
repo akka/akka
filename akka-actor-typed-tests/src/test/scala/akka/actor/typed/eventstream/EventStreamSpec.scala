3ac9c21438d2b9836ee512acecce55e2c529bb12
Patrik Nordwall <patrik.nordwall@gmail.com>
2022-03-16 17:08:16 +0100

    Fix stack overflow for dead letters message adapter, #31241

--- akka-actor-typed-tests/src/test/scala/akka/actor/typed/eventstream/EventStreamSpec.scala
+++ akka-actor-typed-tests/src/test/scala/akka/actor/typed/eventstream/EventStreamSpec.scala
@@ -4,12 +4,17 @@
 
 package akka.actor.typed.eventstream
 
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
 import scala.concurrent.duration._
 
 import org.scalatest.wordspec.AnyWordSpecLike
 
+import akka.actor.DeadLetter
 import akka.actor.testkit.typed.scaladsl.{ ScalaTestWithActorTestKit, TestProbe }
 import akka.actor.testkit.typed.scaladsl.LogCapturing
+import akka.actor.typed.scaladsl.Behaviors
 
 class EventStreamSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike with LogCapturing {
 
@@ -42,6 +47,56 @@ class EventStreamSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike wit
       testKit.system.eventStream ! Unsubscribe(eventObjListener.ref)
       testKit.system.eventStream ! Publish(EventObj)
       eventObjListener.expectNoMessage(ShortWait)
+    }
+
+    "be subscribed by message adapter for DeadLetter" in {
+      val probe = createTestProbe[String]()
+      val ref = testKit.spawn(Behaviors.setup[String] { context =>
+        val adapter = context.messageAdapter[DeadLetter](d => d.message.toString)
+        context.system.eventStream ! Subscribe(adapter)
+
+        Behaviors.receiveMessage { msg =>
+          probe.ref ! msg
+          Behaviors.same
+        }
+      })
+
+      ref ! "init"
+      probe.expectMessage("init")
+      probe.expectNoMessage(100.millis) // might still be a risk that the eventStream ! Subscribe hasn't arrived
+
+      testKit.system.deadLetters ! "msg1"
+      testKit.system.deadLetters ! "msg2"
+      testKit.system.deadLetters ! "msg3"
+
+      probe.expectMessage("msg1")
+      probe.expectMessage("msg2")
+      probe.expectMessage("msg3")
+    }
+
+    "not cause infinite loop when stopping subscribed by message adapter for DeadLetter" in {
+      val latch = new CountDownLatch(1)
+      val ref = testKit.spawn(Behaviors.setup[String] { context =>
+        val adapter = context.messageAdapter[DeadLetter](d => d.message.toString)
+        context.system.eventStream ! Subscribe(adapter)
+
+        Behaviors.receiveMessage { msg =>
+          latch.await(10, TimeUnit.SECONDS)
+          if (msg == "stop")
+            Behaviors.stopped
+          else
+            Behaviors.same
+        }
+      })
+
+      ref ! "msg1"
+      ref ! "stop"
+      ref ! "msg2"
+      ref ! "msg3"
+      // msg2 and msg3 in mailbox, will be sent to dead letters
+      latch.countDown() // stop
+
+      testKit.createTestProbe().expectTerminated(ref)
     }
   }
 
