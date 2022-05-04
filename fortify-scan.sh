65eb45079140438cb4b7a2fa74b10dff547bd9e4
Jack Pines (Lightbend GSA) <jack.pines@lightbend.com>
2022-05-04 16:24:30 -0700

    Updates `fortify-scan.sh` script to create a log. Formats the timestamp to be parsable. Reran scans to verify functionality and included the log in commit. Note: scan took ~86 minutes.

--- fortify-scan.sh
+++ fortify-scan.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-alias echo='echo $(date)'
+alias echo='echo $(date +"%F %T")'
+now=$(date +"%s")
+logfile="fortify-scans/sourceanalyzer-${now}.log"
+echo Starting scans
 for D in ~/.fortify/sca21.1/build/*; do
     if [ -d "${D}" ]; then
         d="$(basename -- $D)"
-        echo Starting $d
+        echo Starting $d >> $logfile
         sourceanalyzer -b $d -f fortify-scans/$d.fpr -scan
+        echo Finished $d >> $logfile
         echo Finished $d
     fi
 done
-
+echo Scans complete
