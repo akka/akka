6370c649d4983d507014111edef2d9f43e598ad5
Levi Ramsey <levi.ramsey@alum.cs.umass.edu>
2022-05-13 10:27:16 -0400

    Remove variable

--- akka-actor/src/main/scala/akka/util/Unsafe.java
+++ akka-actor/src/main/scala/akka/util/Unsafe.java
@@ -34,18 +34,16 @@ public final class Unsafe {
             if (found == null) throw new IllegalStateException("Can't find instance of sun.misc.Unsafe");
             else instance = found;
 
-            long fo;
             try {
-              fo = instance.objectFieldOffset(String.class.getDeclaredField("value"));
+              stringValueFieldOffset = instance.objectFieldOffset(String.class.getDeclaredField("value"));
             } catch (NoSuchFieldException nsfe) {
               // The platform's implementation of String doesn't have a 'value' field, so we have to use algorithm 0
-              fo = -1;
+              stringValueFieldOffset = -1;
             }
-            stringValueFieldOffset = fo;
 
             isJavaVersion9Plus = isIsJavaVersion9Plus();
 
-            if (fo > -1) {
+            if (stringValueFieldOffset > -1) {
               // Select optimization algorithm for `copyUSAciiBytesToStr`.
               // For example algorithm 1 will fail with JDK 11 on ARM32 (Raspberry Pi),
               // and therefore algorithm 0 is selected on that architecture.
