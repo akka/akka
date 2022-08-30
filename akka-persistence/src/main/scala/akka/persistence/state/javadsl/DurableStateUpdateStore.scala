f44803e4202f8658efda5a2d69f1ab210e1a9027
Joseph Ausmann <joseph.ausmann.17@gmail.com>
2022-08-30 10:27:58 -0500

    Revert "* Deprecate deleteObject."
    This reverts commit 1f2af24e68e022074a2dfcad1d5d3e130ffba155.

--- akka-persistence/src/main/scala/akka/persistence/state/javadsl/DurableStateUpdateStore.scala
+++ akka-persistence/src/main/scala/akka/persistence/state/javadsl/DurableStateUpdateStore.scala
@@ -4,9 +4,9 @@
 
 package akka.persistence.state.javadsl
 
-import java.util.concurrent.{CompletableFuture, CompletionStage}
+import java.util.concurrent.CompletionStage
+
 import akka.Done
-import scala.annotation.nowarn
 
 /**
  * API for updating durable state objects.
@@ -20,9 +20,5 @@ trait DurableStateUpdateStore[A] extends DurableStateStore[A] {
    */
   def upsertObject(persistenceId: String, revision: Long, value: A, tag: String): CompletionStage[Done]
 
-  @deprecated(message = "Use the deleteObject overload with revision instead.", since = "2.6.20")
-  def deleteObject(@nowarn persistenceId: String): CompletionStage[Done] = CompletableFuture.completedFuture(Done)
-
-  @nowarn
-  def deleteObject(persistenceId: String, @nowarn revision: Long): CompletionStage[Done] = deleteObject(persistenceId)
+  def deleteObject(persistenceId: String, revision: Long): CompletionStage[Done]
 }
