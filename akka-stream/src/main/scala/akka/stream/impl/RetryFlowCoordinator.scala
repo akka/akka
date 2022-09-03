fced29d3c768f112cf8c90a2fcc37962fff92db5
hepin1989 <hepin1989@gmail.com>
2022-09-03 18:29:57 +0800

    =str Fold InHandler and OutHandler for operator RetryFlowCoordinator.

--- akka-stream/src/main/scala/akka/stream/impl/RetryFlowCoordinator.scala
+++ akka-stream/src/main/scala/akka/stream/impl/RetryFlowCoordinator.scala
@@ -49,100 +49,93 @@ import akka.util.OptionVal
   override val shape: BidiShape[In, In, Out, Out] =
     BidiShape(externalIn, internalOut, internalIn, externalOut)
 
-  override def createLogic(attributes: Attributes): GraphStageLogic = new TimerGraphStageLogic(shape) {
+  override def createLogic(attributes: Attributes): GraphStageLogic =
+    new TimerGraphStageLogic(shape) with InHandler with OutHandler {
 
-    private var elementInProgress: OptionVal[In] = OptionVal.none
-    private var retryNo = 0
+      private var elementInProgress: OptionVal[In] = OptionVal.none
+      private var retryNo = 0
 
-    setHandler(
-      externalIn,
-      new InHandler {
-        override def onPush(): Unit = {
-          val element = grab(externalIn)
-          elementInProgress = OptionVal.Some(element)
-          retryNo = 0
-          pushInternal(element)
+      override def onPush(): Unit = {
+        val element = grab(externalIn)
+        elementInProgress = OptionVal.Some(element)
+        retryNo = 0
+        pushInternal(element)
+      }
+
+      override def onUpstreamFinish(): Unit =
+        if (elementInProgress.isEmpty) {
+          completeStage()
         }
 
-        override def onUpstreamFinish(): Unit =
-          if (elementInProgress.isEmpty) {
-            completeStage()
+      override def onPull(): Unit = {
+        if (elementInProgress.isEmpty) {
+          if (!hasBeenPulled(externalIn) && !isClosed(externalIn)) {
+            pull(externalIn)
           }
-      })
+        }
+      }
 
-    setHandler(
-      internalOut,
-      new OutHandler {
+      override def onDownstreamFinish(cause: Throwable): Unit = {
+        if (elementInProgress.isEmpty || !cause.isInstanceOf[NonFailureCancellation]) {
+          super.onDownstreamFinish(cause)
+        } else {
+          // emit elements before finishing
+          setKeepGoing(true)
+        }
+      }
 
-        override def onPull(): Unit = {
-          if (elementInProgress.isEmpty) {
-            if (!hasBeenPulled(externalIn) && !isClosed(externalIn)) {
-              pull(externalIn)
+      setHandlers(externalIn, internalOut, this)
+
+      setHandlers(
+        internalIn,
+        externalOut,
+        new InHandler with OutHandler {
+          override def onPush(): Unit = {
+            val result = grab(internalIn)
+            elementInProgress match {
+              case OptionVal.Some(_) if retryNo == maxRetries => pushExternal(result)
+              case OptionVal.Some(in) =>
+                decideRetry(in, result) match {
+                  case None          => pushExternal(result)
+                  case Some(element) => planRetry(element)
+                }
+              case _ =>
+                failStage(
+                  new IllegalStateException(
+                    s"inner flow emitted unexpected element $result; the flow must be one-in one-out"))
             }
           }
-        }
 
-        override def onDownstreamFinish(cause: Throwable): Unit = {
-          if (elementInProgress.isEmpty || !cause.isInstanceOf[NonFailureCancellation]) {
-            super.onDownstreamFinish(cause)
-          } else {
-            // emit elements before finishing
-            setKeepGoing(true)
-          }
-        }
-      })
+          override def onPull(): Unit =
+            // external demand
+            if (!hasBeenPulled(internalIn)) pull(internalIn)
+        })
 
-    setHandler(
-      internalIn,
-      new InHandler {
-        override def onPush(): Unit = {
-          val result = grab(internalIn)
-          elementInProgress match {
-            case OptionVal.Some(_) if retryNo == maxRetries => pushExternal(result)
-            case OptionVal.Some(in) =>
-              decideRetry(in, result) match {
-                case None          => pushExternal(result)
-                case Some(element) => planRetry(element)
-              }
-            case _ =>
-              failStage(
-                new IllegalStateException(
-                  s"inner flow emitted unexpected element $result; the flow must be one-in one-out"))
-          }
-        }
-      })
-
-    setHandler(externalOut, new OutHandler {
-      override def onPull(): Unit =
-        // external demand
-        if (!hasBeenPulled(internalIn)) pull(internalIn)
-    })
-
-    private def pushInternal(element: In): Unit = {
-      push(internalOut, element)
-    }
-
-    private def pushExternal(result: Out): Unit = {
-      elementInProgress = OptionVal.none
-      push(externalOut, result)
-      if (isClosed(externalIn)) {
-        completeStage()
-      } else if (isAvailable(internalOut)) {
-        pull(externalIn)
+      private def pushInternal(element: In): Unit = {
+        push(internalOut, element)
       }
+
+      private def pushExternal(result: Out): Unit = {
+        elementInProgress = OptionVal.none
+        push(externalOut, result)
+        if (isClosed(externalIn)) {
+          completeStage()
+        } else if (isAvailable(internalOut)) {
+          pull(externalIn)
+        }
+      }
+
+      private def planRetry(element: In): Unit = {
+        val delay = BackoffSupervisor.calculateDelay(retryNo, minBackoff, maxBackoff, randomFactor)
+        elementInProgress = OptionVal.Some(element)
+        retryNo += 1
+        pull(internalIn)
+        scheduleOnce(RetryFlowCoordinator.RetryCurrentElement, delay)
+      }
+
+      override def onTimer(timerKey: Any): Unit = pushInternal(elementInProgress.get)
+
     }
-
-    private def planRetry(element: In): Unit = {
-      val delay = BackoffSupervisor.calculateDelay(retryNo, minBackoff, maxBackoff, randomFactor)
-      elementInProgress = OptionVal.Some(element)
-      retryNo += 1
-      pull(internalIn)
-      scheduleOnce(RetryFlowCoordinator.RetryCurrentElement, delay)
-    }
-
-    override def onTimer(timerKey: Any): Unit = pushInternal(elementInProgress.get)
-
-  }
 }
 
 private object RetryFlowCoordinator {
