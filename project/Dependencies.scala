9fb63034a7d2ed35a84fbd1d1356a143eb3fb1e6
Scala Steward <scala_steward@virtuslab.com>
2022-09-07 12:22:28 +0000

    Update jul-to-slf4j, log4j-over-slf4j, ... to 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     .withRank(KeyRanks.Invisible) // avoid 'unused key' warning
 
   val junitVersion = "4.13.2"
-  val slf4jVersion = "1.7.36"
+  val slf4jVersion = "2.0.0"
   // check agrona version when updating this
   val aeronVersion = "1.38.1"
   // needs to be inline with the aeron version, check
