390191d835ae1e72a1a58ac186b4e4002cc4c815
Johan Andrén <johan@markatta.com>
2022-09-28 16:22:52 +0200

    Formatting

--- akka-actor/src/main/scala/akka/serialization/Serialization.scala
+++ akka-actor/src/main/scala/akka/serialization/Serialization.scala
@@ -456,8 +456,8 @@ class Serialization(val system: ExtendedActorSystem) extends Extension {
   private def warnUnexpectedNonAkkaSerializer(clazz: Class[_], ser: Serializer): Boolean = {
     val className = clazz.getName
     if (className.startsWith("akka.") && !ser.getClass.getName.startsWith("akka.") &&
-      // no serializers for these in Akka
-      !(className.startsWith("akka.grpc") || className.startsWith("akka.http"))) {
+        // no serializers for these in Akka
+        !(className.startsWith("akka.grpc") || className.startsWith("akka.http"))) {
       log.warning(
         "Using serializer [{}] for message [{}]. Note that this serializer " +
         "is not implemented by Akka. It's not recommended to replace serializers for messages " +
