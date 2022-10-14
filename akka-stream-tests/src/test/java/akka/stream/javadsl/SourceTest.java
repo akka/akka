cec7e60d0b1a14e938c9a0afa3865a7a81b525fd
kerr <hepin1989@gmail.com>
2022-10-14 14:09:23 +0800

    =str Add dedicated stream timeout exceptions for timeout related operators. (#31640)

--- akka-stream-tests/src/test/java/akka/stream/javadsl/SourceTest.java
+++ akka-stream-tests/src/test/java/akka/stream/javadsl/SourceTest.java
@@ -46,8 +46,7 @@ import static akka.stream.testkit.StreamTestKit.PublisherProbeSubscription;
 import static akka.stream.testkit.TestPublisher.ManualProbe;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 @SuppressWarnings("serial")
 public class SourceTest extends StreamTest {
@@ -1165,10 +1164,9 @@ public class SourceTest extends StreamTest {
                     .runWith(Sink.head(), system)
                     .toCompletableFuture()
                     .get(3, TimeUnit.SECONDS));
-    assertEquals(
+    assertTrue(
         "The cause of ExecutionException should be TimeoutException",
-        TimeoutException.class,
-        exception.getCause().getClass());
+        TimeoutException.class.isAssignableFrom(exception.getCause().getClass()));
   }
 
   @Test
@@ -1183,10 +1181,9 @@ public class SourceTest extends StreamTest {
                     .runWith(Sink.head(), system)
                     .toCompletableFuture()
                     .get(3, TimeUnit.SECONDS));
-    assertEquals(
+    assertTrue(
         "The cause of ExecutionException should be TimeoutException",
-        TimeoutException.class,
-        exception.getCause().getClass());
+        TimeoutException.class.isAssignableFrom(exception.getCause().getClass()));
   }
 
   @Test
@@ -1201,10 +1198,9 @@ public class SourceTest extends StreamTest {
                     .runWith(Sink.head(), system)
                     .toCompletableFuture()
                     .get(3, TimeUnit.SECONDS));
-    assertEquals(
+    assertTrue(
         "The cause of ExecutionException should be TimeoutException",
-        TimeoutException.class,
-        exception.getCause().getClass());
+        TimeoutException.class.isAssignableFrom(exception.getCause().getClass()));
   }
 
   @Test
---
+++ akka-stream/src/main/scala/akka/stream/StreamTimeoutException.scala
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2022 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.stream
+
+import scala.concurrent.TimeoutException
+import scala.util.control.NoStackTrace
+import akka.annotation.DoNotInherit
+
+/**
+ * Base class for timeout exceptions specific to Akka Streams
+ *
+ * Not for user extension
+ */
+@DoNotInherit
+sealed class StreamTimeoutException(msg: String) extends TimeoutException(msg) with NoStackTrace
+
+final class InitialTimeoutException(msg: String) extends StreamTimeoutException(msg)
+
+final class CompletionTimeoutException(msg: String) extends StreamTimeoutException(msg)
+
+final class StreamIdleTimeoutException(msg: String) extends StreamTimeoutException(msg)
+
+final class BackpressureTimeoutException(msg: String) extends StreamTimeoutException(msg)
