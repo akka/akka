7bd2eeb717b2d35beb3e4a9f0b3b56cdb55adf02
kerr <hepin1989@gmail.com>
2022-11-08 11:44:34 +0800

    =str Fix `CompletionStage#toCompletableFuture` may throw UnsupportedOperationException.

--- akka-stream/src/main/scala/akka/stream/impl/Unfold.scala
+++ akka-stream/src/main/scala/akka/stream/impl/Unfold.scala
@@ -11,6 +11,7 @@ import akka.stream.impl.Stages.DefaultAttributes
 import akka.stream.stage.{ GraphStage, GraphStageLogic, OutHandler }
 
 import java.util.Optional
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CompletionStage
 import scala.concurrent.Future
 import scala.util.{ Failure, Success, Try }
@@ -113,20 +114,21 @@ import scala.util.{ Failure, Success, Try }
       }
 
       def onPull(): Unit = {
-        val future = f.apply(state).toCompletableFuture
-        if (future.isDone && !future.isCompletedExceptionally) {
-          handle(future.getNow(null))
-        } else {
-          future.handle((r, ex) => {
-            if (ex != null) {
-              asyncHandler(Failure(ex))
-            } else {
-              asyncHandler(Success(r))
-            }
-            null
-          })
+        f.apply(state) match {
+          case cf: CompletableFuture[Optional[Pair[S, E]] @unchecked] if cf.isDone && !cf.isCompletedExceptionally =>
+            handle(cf.join())
+          case future =>
+            future.handle((r, ex) => {
+              if (ex != null) {
+                asyncHandler(Failure(ex))
+              } else {
+                asyncHandler(Success(r))
+              }
+              null
+            })
         }
       }
+
       setHandler(out, this)
     }
 }
