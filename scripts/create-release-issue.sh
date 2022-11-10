963a091821724af0dc80d2c606a8204a7493a750
Johan Andrén <johan@markatta.com>
2022-11-10 16:15:17 +0100

    Fix release script typo

--- scripts/create-release-issue.sh
+++ scripts/create-release-issue.sh
@@ -5,6 +5,6 @@ if [ -z $VERSION ]
 then
   echo specify the version name to be released, eg. 2.7.0
 else
-  sed -e 's/\$VERSION\$/'$VERSION'/g' akka-docs/release-train-issue-template.md > /tmp/release-$VERSION.tmp
+  sed -e 's/\$VERSION\$/'$VERSION'/g' akka-docs/release-train-issue-template.md > /tmp/release-$VERSION.md
   echo Created $(gh issue create --title "Release $VERSION" --body-file /tmp/release-$VERSION.md --milestone $VERSION --web)
 fi
