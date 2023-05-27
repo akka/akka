45b97b7035fb59b81100a62d27b0510b8f687c56
Scala Steward <scala_steward@virtuslab.com>
2023-05-27 12:35:48 +0000

    bump: aeron-client, aeron-driver 1.41.3 (was 1.40.0)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val junitVersion = "4.13.2"
   val slf4jVersion = "1.7.36"
   // check agrona version when updating this
-  val aeronVersion = "1.40.0"
+  val aeronVersion = "1.41.3"
   // needs to be inline with the aeron version, check
   // https://github.com/real-logic/aeron/blob/1.x.y/build.gradle
   val agronaVersion = "1.17.1"
