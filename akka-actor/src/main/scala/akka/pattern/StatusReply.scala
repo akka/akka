2ca18d112000cc14d80f44906e57ebd1effc83ad
Johan Andrén <johan@markatta.com>
2023-06-07 10:46:42 +0200

    fix: Better exception string in StatusReply #31963 (#31965)

--- akka-actor/src/main/scala/akka/pattern/StatusReply.scala
+++ akka-actor/src/main/scala/akka/pattern/StatusReply.scala
@@ -56,7 +56,7 @@ final class StatusReply[+T] private (private val status: Try[T]) {
   override def toString: String = status match {
     case ScalaSuccess(t)                => s"Success($t)"
     case ScalaFailure(ex: ErrorMessage) => s"Error(${ex.getMessage})"
-    case ScalaFailure(ex)               => s"Error(${ex.getClass.getName}: ${ex.getMessage})"
+    case ScalaFailure(ex)               => s"Error($ex)"
   }
 
 }
