518b694a607235545caa138706c22b5f1e2b2cc2
Johan Andrén <johan@markatta.com>
2023-06-16 11:12:18 +0200

    fix: Try to limit spin loop in TLSActor #29922

--- akka-stream/src/main/scala/akka/stream/impl/io/TLSActor.scala
+++ akka-stream/src/main/scala/akka/stream/impl/io/TLSActor.scala
@@ -62,6 +62,8 @@ import akka.util.ByteString
   import TLSActor._
 
   private var stopped = false
+  private var unwrapPutBackCounter: Int = 0
+
   protected val outputBunch = new OutputBunch(outputCount = 2, self, this)
   outputBunch.markAllOutputs()
 
@@ -355,6 +357,7 @@ import akka.util.ByteString
 
   def flushToUser(): Unit = {
     if (tracing) log.debug("flushToUser")
+    if (unwrapPutBackCounter > 0) unwrapPutBackCounter = 0
     userOutBuffer.flip()
     if (userOutBuffer.hasRemaining) {
       val bs = ByteString(userOutBuffer)
@@ -409,6 +412,17 @@ import akka.util.ByteString
       case OK =>
         result.getHandshakeStatus match {
           case NEED_WRAP =>
+            // https://github.com/akka/akka/issues/29922
+            // A second workaround for an infinite loop we have not been able to reproduce/isolate,
+            // if you see this, and can reproduce consistently, please report back to the Akka team
+            // with a reproducer or details about the client causing it
+            unwrapPutBackCounter += 1
+            if (unwrapPutBackCounter > 1000) {
+              throw new IllegalStateException(
+                s"Stuck in unwrap loop, bailing out, last handshake status [$lastHandshakeStatus], " +
+                s"remaining=${transportInBuffer.remaining}, out=${userOutBuffer.position()}, " +
+                "(https://github.com/akka/akka/issues/29922)")
+            }
             transportInChoppingBlock.putBack(transportInBuffer)
           case FINISHED =>
             flushToUser()
