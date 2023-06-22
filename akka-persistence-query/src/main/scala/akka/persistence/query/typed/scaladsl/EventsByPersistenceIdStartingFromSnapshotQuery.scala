3a710fd3d39b19736c286a785060183d048185ef
Patrik Nordwall <patrik.nordwall@gmail.com>
2023-06-22 14:46:51 +0200

    feat: Query traits for using snapshots as starting point
    * eventsBySlicesStartingFromSnapshots
    * currentEventsBySlicesStartingFromSnapshots
    * eventsByPersistenceIdStartingFromSnapshot
    * EventsBySliceFirehoseQuery also implementing eventsBySlicesStartingFromSnapshots

---
+++ akka-persistence-query/src/main/scala/akka/persistence/query/typed/scaladsl/EventsByPersistenceIdStartingFromSnapshotQuery.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2023 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.persistence.query.typed.scaladsl
+
+import akka.NotUsed
+import akka.annotation.ApiMayChange
+import akka.persistence.query.scaladsl.ReadJournal
+import akka.persistence.query.typed.EventEnvelope
+import akka.stream.scaladsl.Source
+
+/**
+ * A plugin may optionally support this query by implementing this trait.
+ */
+@ApiMayChange
+trait EventsByPersistenceIdStartingFromSnapshotQuery extends ReadJournal {
+
+  /**
+   * Same as [[EventsByPersistenceIdTypedQuery]] but with the purpose to use snapshot as starting point
+   * and thereby reducing number of events that have to be loaded.
+   */
+  def eventsByPersistenceIdStartingFromSnapshot[Snapshot, Event](
+      persistenceId: String,
+      fromSequenceNr: Long,
+      toSequenceNr: Long,
+      transformSnapshot: Snapshot => Event): Source[EventEnvelope[Event], NotUsed]
+
+}
