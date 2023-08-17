f36cd57ab28655f5805e0784a7b74ad58b26d255
Johan Andrén <johan@markatta.com>
2023-08-17 13:44:52 +0200

    feat: Predefined marker interfaces for JSON and CBOR

---
+++ akka-serialization-jackson/src/main/scala/akka/serialization/jackson/JsonSerializable.scala
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2023 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.serialization.jackson
+
+/**
+ * Predefined marker trait for serialization with Jackson JSON.
+ * Enabled in reference.conf `akka.actor.serialization-bindings` (via application.conf).
+ */
+trait JsonSerializable
+
+/**
+ * Predefined marker trait for serialization with Jackson CBOR.
+ * Enabled in reference.conf `akka.actor.serialization-bindings` (via application.conf).
+ */
+trait CborSerializable
