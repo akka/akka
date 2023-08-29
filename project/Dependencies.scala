adc9327d72fa6d1fbd62a59c68ae6d3dce26b2cd
Scala Steward <scala_steward@virtuslab.com>
2023-08-29 12:33:07 +0000

    bump: netty-handler, netty-transport 4.1.97.Final (was 4.1.96.Final)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   // needs to be inline with the aeron version, check
   // https://github.com/real-logic/aeron/blob/1.x.y/build.gradle
   val agronaVersion = "1.17.1"
-  val nettyVersion = "4.1.96.Final"
+  val nettyVersion = "4.1.97.Final"
   val protobufJavaVersion = "3.16.1"
   val logbackVersion = "1.2.12"
   val scalaFortifyVersion = "1.0.22"
