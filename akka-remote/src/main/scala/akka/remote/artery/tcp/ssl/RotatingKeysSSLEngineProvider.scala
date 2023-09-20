7806502900dad1f8d60476020559984037159079
Patrik Nordwall <patrik.nordwall@gmail.com>
2023-09-20 10:37:17 +0200

    chore: Remove ApiMayChange for RotatingKeysSSLEngineProvider

--- akka-remote/src/main/scala/akka/remote/artery/tcp/ssl/RotatingKeysSSLEngineProvider.scala
+++ akka-remote/src/main/scala/akka/remote/artery/tcp/ssl/RotatingKeysSSLEngineProvider.scala
@@ -22,7 +22,6 @@ import scala.concurrent.duration._
 import com.typesafe.config.Config
 
 import akka.actor.ActorSystem
-import akka.annotation.ApiMayChange
 import akka.annotation.InternalApi
 import akka.event.Logging
 import akka.event.MarkerLoggingAdapter
@@ -37,15 +36,10 @@ import akka.stream.TLSRole
  * Variation on ConfigSSLEngineProvider that will periodically reload the keys and certificates
  * from disk, to facilitate rolling updates of certificates.
  *
- * This class is still ApiMayChange because it can likely be further harmonized with
- * the standard ConfigSSLEngineProvider. Also the location and default values of the
- * configuration may change in future versions of Akka.
- *
  * This provider does not perform hostname verification, but instead allows checking
  * that the remote certificate has a subject name that matches the subject name of
  * the configured certificate.
  */
-@ApiMayChange
 final class RotatingKeysSSLEngineProvider(val config: Config, protected val log: MarkerLoggingAdapter)
     extends SSLEngineProvider {
 
