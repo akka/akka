e941eb073927b4eaa098b7dd627d4a4c114a7659
Patrik Nordwall <patrik.nordwall@gmail.com>
2023-10-03 11:56:52 +0200

    bump: Shaded protobuf-java 3.21.12 (#32149)
    * and corresponding protoc version
    
    * CVE-2022-3171 CVE-2022-3509 CVE-2022-3510

--- project/Protobuf.scala
+++ project/Protobuf.scala
@@ -33,7 +33,7 @@ object Protobuf {
     // this keeps intellij happy for files that use the shaded protobuf
     Compile / unmanagedJars += (LocalProject("akka-protobuf-v3") / assembly).value,
     protoc := "protoc",
-    protocVersion := "3.11.4",
+    protocVersion := "3.21.12", // also sync with protobufJavaVersion in Dependencies.scala
     generate := {
       val sourceDirs = paths.value
       val targetDirs = outputPaths.value
