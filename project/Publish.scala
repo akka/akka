aab3ab80d1e0e16d10e4fafe20bbd43bd2b67afe
Patrik Nordwall <patrik.nordwall@gmail.com>
2023-10-05 10:39:23 +0200

    build: publish to the Akka repository (#32155)
    * build: publish to the Akka repository
    
    * respect sbt macros
    
    ---------
    
    Co-authored-by: Enno Runne <458526+ennru@users.noreply.github.com>

--- project/Publish.scala
+++ project/Publish.scala
@@ -7,8 +7,9 @@ package akka
 import sbt._
 import sbt.Keys._
 import java.io.File
+
 import com.lightbend.sbt.publishrsync.PublishRsyncPlugin.autoImport.publishRsyncHost
-import xerial.sbt.Sonatype.autoImport.sonatypeProfileName
+import sbt.Def
 
 object Publish extends AutoPlugin {
 
@@ -17,37 +18,47 @@ object Publish extends AutoPlugin {
   override def trigger = allRequirements
 
   override lazy val projectSettings = Seq(
-    publishTo := Some(akkaPublishTo.value),
-    publishRsyncHost := "akkarepo@gustav.akka.io",
-    credentials ++= akkaCredentials,
-    organizationName := "Lightbend Inc.",
-    organizationHomepage := Some(url("https://www.lightbend.com")),
-    sonatypeProfileName := "com.typesafe",
-    startYear := Some(2009),
-    developers := List(
-        Developer(
-          "akka-contributors",
-          "Akka Contributors",
-          "akka.official@gmail.com",
-          url("https://github.com/akka/akka/graphs/contributors"))),
-    publishMavenStyle := true,
-    pomIncludeRepository := { x =>
-      false
-    },
-    defaultPublishTo := target.value / "repository")
+      publishRsyncHost := "akkarepo@gustav.akka.io",
+      organizationName := "Lightbend Inc.",
+      organizationHomepage := Some(url("https://www.lightbend.com")),
+      startYear := Some(2009),
+      developers := List(
+          Developer(
+            "akka-contributors",
+            "Akka Contributors",
+            "akka.official@gmail.com",
+            url("https://github.com/akka/akka/graphs/contributors"))),
+      publishMavenStyle := true,
+      pomIncludeRepository := { x =>
+        false
+      },
+      defaultPublishTo := target.value / "repository") ++ publishingSettings
 
-  private def akkaPublishTo = Def.setting {
-    val key = new java.io.File(
-      Option(System.getProperty("akka.gustav.key"))
-        .getOrElse(System.getProperty("user.home") + "/.ssh/id_rsa_gustav.pem"))
-    if (isSnapshot.value)
-      Resolver.sftp("Akka snapshots", "gustav.akka.io", "/home/akkarepo/www/snapshots").as("akkarepo", key)
-    else
-      Opts.resolver.sonatypeStaging
+  private lazy val publishingSettings: Seq[Def.Setting[_]] = {
+    def snapshotRepoKey =
+      new java.io.File(
+        Option(System.getProperty("akka.gustav.key"))
+          .getOrElse(System.getProperty("user.home") + "/.ssh/id_rsa_gustav.pem"))
+
+    def cloudsmithCredentials: Seq[Credentials] = {
+      val user = System.getProperty("PUBLISH_USER")
+      val password = System.getProperty("PUBLISH_PASSWORD")
+      if (user == null || password == null) {
+        throw new Exception("Publishing credentials expected in `PUBLISH_USER` and `PUBLISH_PASSWORD`.")
+      }
+      Seq(Credentials("Cloudsmith API", "maven.cloudsmith.io", user, password))
+    }
+
+    Def.settings(
+      publishTo := (if (isSnapshot.value)
+                      Some(
+                        Resolver
+                          .sftp("Akka snapshots", "gustav.akka.io", "/home/akkarepo/www/snapshots")
+                          .as("akkarepo", snapshotRepoKey))
+                    else
+                      Some("Cloudsmith API".at("https://maven.cloudsmith.io/lightbend/akka/"))),
+      credentials ++= (if (isSnapshot.value) Seq[Credentials]() else cloudsmithCredentials))
   }
-
-  private def akkaCredentials: Seq[Credentials] =
-    Option(System.getProperty("akka.publish.credentials")).map(f => Credentials(new File(f))).toSeq
 }
 
 /**
