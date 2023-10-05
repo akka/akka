577a1f9da841c5389729bb1209c78b130e1a0c15
Enno Runne <458526+ennru@users.noreply.github.com>
2023-10-05 09:31:57 +0200

    respect sbt macros

--- project/Publish.scala
+++ project/Publish.scala
@@ -18,37 +18,46 @@ object Publish extends AutoPlugin {
   override def trigger = allRequirements
 
   override lazy val projectSettings = Seq(
-    publishRsyncHost := "akkarepo@gustav.akka.io",
-    organizationName := "Lightbend Inc.",
-    organizationHomepage := Some(url("https://www.lightbend.com")),
-    startYear := Some(2009),
-    developers := List(
-        Developer(
-          "akka-contributors",
-          "Akka Contributors",
-          "akka.official@gmail.com",
-          url("https://github.com/akka/akka/graphs/contributors"))),
-    publishMavenStyle := true,
-    pomIncludeRepository := { x =>
-      false
-    },
-    defaultPublishTo := target.value / "repository") ++ publishingSettings
+      publishRsyncHost := "akkarepo@gustav.akka.io",
+      organizationName := "Lightbend Inc.",
+      organizationHomepage := Some(url("https://www.lightbend.com")),
+      startYear := Some(2009),
+      developers := List(
+          Developer(
+            "akka-contributors",
+            "Akka Contributors",
+            "akka.official@gmail.com",
+            url("https://github.com/akka/akka/graphs/contributors"))),
+      publishMavenStyle := true,
+      pomIncludeRepository := { x =>
+        false
+      },
+      defaultPublishTo := target.value / "repository") ++ publishingSettings
 
   private lazy val publishingSettings: Seq[Def.Setting[_]] = {
-    if (isSnapshot.value) {
-      val key = new java.io.File(
+    def snapshotRepoKey =
+      new java.io.File(
         Option(System.getProperty("akka.gustav.key"))
           .getOrElse(System.getProperty("user.home") + "/.ssh/id_rsa_gustav.pem"))
-      publishTo := Some(Resolver.sftp("Akka snapshots", "gustav.akka.io", "/home/akkarepo/www/snapshots").as("akkarepo", key))
-    } else {
-      publishTo := Some("Cloudsmith API".at("https://maven.cloudsmith.io/lightbend/akka/"))
+
+    def cloudsmithCredentials: Seq[Credentials] = {
       val user = System.getProperty("PUBLISH_USER")
       val password = System.getProperty("PUBLISH_PASSWORD")
       if (user == null || password == null) {
         throw new Exception("Publishing credentials expected in `PUBLISH_USER` and `PUBLISH_PASSWORD`.")
       }
-      credentials ++= Seq(Credentials("Cloudsmith API", "maven.cloudsmith.io", user, password))
+      Seq(Credentials("Cloudsmith API", "maven.cloudsmith.io", user, password))
     }
+
+    Def.settings(
+      publishTo := (if (isSnapshot.value)
+                      Some(
+                        Resolver
+                          .sftp("Akka snapshots", "gustav.akka.io", "/home/akkarepo/www/snapshots")
+                          .as("akkarepo", snapshotRepoKey))
+                    else
+                      Some("Cloudsmith API".at("https://maven.cloudsmith.io/lightbend/akka/"))),
+      credentials ++= (if (isSnapshot.value) Seq[Credentials]() else cloudsmithCredentials))
   }
 }
 
