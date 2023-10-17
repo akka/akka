53086c6d415bff44d5e824bf1a31bbc357dab0aa
Andrei Arlou <andrei.arlou@gmail.com>
2023-10-17 17:13:58 +0300

    chore: Remove warning for an untyped mailbox with mailbox-size (#32175)
    Had note to remove it already in Akka 2.3

--- akka-actor/src/main/scala/akka/dispatch/Mailboxes.scala
+++ akka-actor/src/main/scala/akka/dispatch/Mailboxes.scala
@@ -117,7 +117,6 @@ private[akka] class Mailboxes(
     }
 
   // don’t care if this happens twice
-  private var mailboxSizeWarningIssued = false
   private var mailboxNonZeroPushTimeoutWarningIssued = false
 
   def getMailboxRequirement(config: Config) = config.getString("mailbox-requirement") match {
@@ -158,16 +157,6 @@ private[akka] class Mailboxes(
     val hasMailboxType =
       dispatcherConfig.hasPath("mailbox-type") &&
       dispatcherConfig.getString("mailbox-type") != Deploy.NoMailboxGiven
-
-    // TODO remove in 2.3
-    if (!hasMailboxType && !mailboxSizeWarningIssued && dispatcherConfig.hasPath("mailbox-size")) {
-      eventStream.publish(
-        Warning(
-          "mailboxes",
-          getClass,
-          s"ignoring setting 'mailbox-size' for dispatcher [$id], you need to specify 'mailbox-type=bounded'"))
-      mailboxSizeWarningIssued = true
-    }
 
     def verifyRequirements(mailboxType: MailboxType): MailboxType = {
       lazy val mqType: Class[_] = getProducedMessageQueueType(mailboxType)
