dc124f2ecd272241d9a78abfa9dcfe57e5dfa7e5
Johan Andrén <johan@markatta.com>
2023-12-15 10:19:09 +0100

    chore: Add a note about fifos not being supported in FileIO.formPath #32134

--- akka-stream/src/main/scala/akka/stream/scaladsl/FileIO.scala
+++ akka-stream/src/main/scala/akka/stream/scaladsl/FileIO.scala
@@ -51,6 +51,9 @@ object FileIO {
    * and a possible exception if IO operation was not completed successfully. Note that bytes having been read by the source does
    * not give any guarantee that the bytes were seen by downstream stages.
    *
+   * It is not possible to read FIFOs, also known as named pipes, with `fromPath`, trying to do so will potentially first block
+   * and then fail the stream.
+   *
    * @param f         the file path to read from
    * @param chunkSize the size of each read operation, defaults to 8192
    */
@@ -68,6 +71,9 @@ object FileIO {
    * It materializes a [[Future]] of [[IOResult]] containing the number of bytes read from the source file upon completion,
    * and a possible exception if IO operation was not completed successfully. Note that bytes having been read by the source does
    * not give any guarantee that the bytes were seen by downstream stages.
+   *
+   * It is not possible to read FIFOs, also known as named pipes, with `fromPath`, trying to do so will potentially first block
+   * and then fail the stream.
    *
    * @param f         the file path to read from
    * @param chunkSize the size of each read operation, defaults to 8192
