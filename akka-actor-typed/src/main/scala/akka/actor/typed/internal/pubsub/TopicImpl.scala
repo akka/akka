bf97de63d44f689f406006f79256563dcc35a550
Johan Andrén <johan@markatta.com>
2024-01-08 09:29:52 +0100

    group router copy pasta

--- akka-actor-typed/src/main/scala/akka/actor/typed/internal/pubsub/TopicImpl.scala
+++ akka-actor-typed/src/main/scala/akka/actor/typed/internal/pubsub/TopicImpl.scala
@@ -106,7 +106,7 @@ private final class InitialTopicImpl[T](
       else
         context.system.eventStream ! EventStream.Publish(Dropped(
           msg,
-          s"Stash is full in group router for [$topicServiceKey]",
+          s"Stash is full in topic [$topicServiceKey]",
           context.self.toClassic)) // don't fail on full stash
       this
   }
