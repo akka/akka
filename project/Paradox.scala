6575ec7829cf88d331ad6c399c3ec57b1ad29610
Johan Andrén <johan@markatta.com>
2024-01-29 15:57:53 +0100

    doc: API doc links was pointing to 2.8 (#32301)

--- project/Paradox.scala
+++ project/Paradox.scala
@@ -14,54 +14,62 @@ import sbt._
 import scala.concurrent.duration._
 
 object Paradox {
+  private val MajorMinorRegexp = """(\d+\.\d+).*""".r
 
-  val propertiesSettings = Seq(
-    Compile / paradoxProperties ++= Map(
-        "canonical.base_url" -> "https://doc.akka.io/docs/akka/current",
-        "github.base_url" -> GitHub
-          .url(version.value), // for links like this: @github[#1](#1) or @github[83986f9](83986f9)
-        "extref.akka.http.base_url" -> "https://doc.akka.io/docs/akka-http/current/%s",
-        "extref.akka-management.base_url" -> "https://doc.akka.io/docs/akka-management/current/%s",
-        "extref.akka-projection.base_url" -> "https://doc.akka.io/docs/akka-projection/current/%s",
-        "extref.akka-distributed-cluster.base_url" -> "https://doc.akka.io/docs/akka-distributed-cluster/current/%s",
-        "extref.akka-edge.base_url" -> "https://doc.akka.io/docs/akka-edge/current/%s",
-        "extref.platform-guide.base_url" -> "https://developer.lightbend.com/docs/akka-guide/%s",
-        "extref.wikipedia.base_url" -> "https://en.wikipedia.org/wiki/%s",
-        "extref.github.base_url" -> (GitHub.url(version.value) + "/%s"), // for links to our sources
-        "extref.samples.base_url" -> "https://developer.lightbend.com/start/?group=akka&amp;project=%s",
-        "extref.ecs.base_url" -> "https://example.lightbend.com/v1/download/%s",
-        "scaladoc.akka.base_url" -> "https://doc.akka.io/api/akka/2.8",
-        "scaladoc.akka.http.base_url" -> "https://doc.akka.io/api/akka-http/current",
-        "javadoc.java.base_url" -> "https://docs.oracle.com/en/java/javase/11/docs/api/java.base/",
-        "javadoc.java.link_style" -> "direct",
-        "javadoc.akka.base_url" -> "https://doc.akka.io/japi/akka/2.8",
-        "javadoc.akka.link_style" -> "direct",
-        "javadoc.akka.http.base_url" -> "https://doc.akka.io/japi/akka-http/current",
-        "javadoc.akka.http.link_style" -> "frames",
-        "javadoc.com.fasterxml.jackson.annotation.base_url" -> "https://javadoc.io/doc/com.fasterxml.jackson.core/jackson-annotations/latest/",
-        "javadoc.com.fasterxml.jackson.annotation.link_style" -> "direct",
-        "javadoc.com.fasterxml.jackson.databind.base_url" -> "https://javadoc.io/doc/com.fasterxml.jackson.core/jackson-databind/latest/",
-        "javadoc.com.fasterxml.jackson.databind.link_style" -> "direct",
-        "javadoc.com.google.protobuf.base_url" -> "https://javadoc.io/doc/com.google.protobuf/protobuf-java/latest/",
-        "javadoc.com.google.protobuf.link_style" -> "direct",
-        "javadoc.com.typesafe.config.base_url" -> "https://javadoc.io/doc/com.typesafe/config/latest/",
-        "javadoc.com.typesafe.config.link_style" -> "direct",
-        "javadoc.org.slf4j.base_url" -> "https://www.javadoc.io/doc/org.slf4j/slf4j-api/latest/org.slf4j",
-        "javadoc.org.slf4j.link_style" -> "direct",
-        "scala.version" -> scalaVersion.value,
-        "scala.binary.version" -> scalaBinaryVersion.value,
-        "akka.version" -> version.value,
-        "scalatest.version" -> Dependencies.scalaTestVersion,
-        "sigar_loader.version" -> "1.6.6-rev002",
-        "algolia.docsearch.app_id" -> "XUXZ6LW9B1",
-        "algolia.docsearch.api_key" -> "5b6260148e92f7c5e38338fcf7eaa3e0",
-        "algolia.docsearch.index_name" -> "akka_docs",
-        "google.analytics.account" -> "UA-21117439-1",
-        "google.analytics.domain.name" -> "akka.io",
-        "signature.akka.base_dir" -> (ThisBuild / baseDirectory).value.getAbsolutePath,
-        "aeron_version" -> Dependencies.aeronVersion,
-        "netty_version" -> Dependencies.nettyVersion,
-        "logback_version" -> Dependencies.logbackVersion))
+  val propertiesSettings = Seq(Compile / paradoxProperties ++= {
+    val akkaMajorMinorVersion = {
+      version.value match {
+        case MajorMinorRegexp(minor) => minor
+        case surprisingVersion       => surprisingVersion
+      }
+    }
+    Map(
+      "canonical.base_url" -> "https://doc.akka.io/docs/akka/current",
+      "github.base_url" -> GitHub
+        .url(version.value), // for links like this: @github[#1](#1) or @github[83986f9](83986f9)
+      "extref.akka.http.base_url" -> "https://doc.akka.io/docs/akka-http/current/%s",
+      "extref.akka-management.base_url" -> "https://doc.akka.io/docs/akka-management/current/%s",
+      "extref.akka-projection.base_url" -> "https://doc.akka.io/docs/akka-projection/current/%s",
+      "extref.akka-distributed-cluster.base_url" -> "https://doc.akka.io/docs/akka-distributed-cluster/current/%s",
+      "extref.akka-edge.base_url" -> "https://doc.akka.io/docs/akka-edge/current/%s",
+      "extref.platform-guide.base_url" -> "https://developer.lightbend.com/docs/akka-guide/%s",
+      "extref.wikipedia.base_url" -> "https://en.wikipedia.org/wiki/%s",
+      "extref.github.base_url" -> (GitHub.url(version.value) + "/%s"), // for links to our sources
+      "extref.samples.base_url" -> "https://developer.lightbend.com/start/?group=akka&amp;project=%s",
+      "extref.ecs.base_url" -> "https://example.lightbend.com/v1/download/%s",
+      "scaladoc.akka.base_url" -> s"https://doc.akka.io/api/akka/$akkaMajorMinorVersion",
+      "scaladoc.akka.http.base_url" -> "https://doc.akka.io/api/akka-http/current",
+      "javadoc.java.base_url" -> "https://docs.oracle.com/en/java/javase/11/docs/api/java.base/",
+      "javadoc.java.link_style" -> "direct",
+      "javadoc.akka.base_url" -> s"https://doc.akka.io/japi/akka/$akkaMajorMinorVersion",
+      "javadoc.akka.link_style" -> "direct",
+      "javadoc.akka.http.base_url" -> "https://doc.akka.io/japi/akka-http/current",
+      "javadoc.akka.http.link_style" -> "frames",
+      "javadoc.com.fasterxml.jackson.annotation.base_url" -> "https://javadoc.io/doc/com.fasterxml.jackson.core/jackson-annotations/latest/",
+      "javadoc.com.fasterxml.jackson.annotation.link_style" -> "direct",
+      "javadoc.com.fasterxml.jackson.databind.base_url" -> "https://javadoc.io/doc/com.fasterxml.jackson.core/jackson-databind/latest/",
+      "javadoc.com.fasterxml.jackson.databind.link_style" -> "direct",
+      "javadoc.com.google.protobuf.base_url" -> "https://javadoc.io/doc/com.google.protobuf/protobuf-java/latest/",
+      "javadoc.com.google.protobuf.link_style" -> "direct",
+      "javadoc.com.typesafe.config.base_url" -> "https://javadoc.io/doc/com.typesafe/config/latest/",
+      "javadoc.com.typesafe.config.link_style" -> "direct",
+      "javadoc.org.slf4j.base_url" -> "https://www.javadoc.io/doc/org.slf4j/slf4j-api/latest/org.slf4j",
+      "javadoc.org.slf4j.link_style" -> "direct",
+      "scala.version" -> scalaVersion.value,
+      "scala.binary.version" -> scalaBinaryVersion.value,
+      "akka.version" -> version.value,
+      "scalatest.version" -> Dependencies.scalaTestVersion,
+      "sigar_loader.version" -> "1.6.6-rev002",
+      "algolia.docsearch.app_id" -> "XUXZ6LW9B1",
+      "algolia.docsearch.api_key" -> "5b6260148e92f7c5e38338fcf7eaa3e0",
+      "algolia.docsearch.index_name" -> "akka_docs",
+      "google.analytics.account" -> "UA-21117439-1",
+      "google.analytics.domain.name" -> "akka.io",
+      "signature.akka.base_dir" -> (ThisBuild / baseDirectory).value.getAbsolutePath,
+      "aeron_version" -> Dependencies.aeronVersion,
+      "netty_version" -> Dependencies.nettyVersion,
+      "logback_version" -> Dependencies.logbackVersion)
+  })
 
   val rootsSettings = Seq(
     paradoxRoots := List(
