eabb457a908ab35457f724094748784a533b66e8
Johan Andrén <johan@markatta.com>
2024-02-12 15:09:09 +0100

    akka-persistence

--- akka-persistence/src/test/scala/akka/persistence/NativeImageMetadataSpec.scala
+++ akka-persistence/src/test/scala/akka/persistence/NativeImageMetadataSpec.scala
@@ -4,7 +4,17 @@
 
 package akka.persistence
 
+import akka.persistence.journal.PersistencePluginProxy
+import akka.persistence.journal.inmem.InmemJournal
+import akka.persistence.journal.leveldb.LeveldbStore
+import akka.persistence.journal.leveldb.SharedLeveldbJournal
+import akka.persistence.snapshot.NoSnapshotStore
+import akka.persistence.snapshot.local.LocalSnapshotStore
 import akka.testkit.NativeImageUtils
+import akka.testkit.NativeImageUtils.Constructor
+import akka.testkit.NativeImageUtils.ReflectConfigEntry
+import akka.testkit.NativeImageUtils.ReflectMethod
+import com.typesafe.config.Config
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -12,7 +22,32 @@ object NativeImageMetadataSpec {
 
   val metadataDir = NativeImageUtils.metadataDirFor("akka-persistence")
 
-  val additionalEntries = Seq()
+  val additionalEntries = Seq(
+    // akka.persistence.internal-stash-overflow-strategy possible "configurators"
+    ReflectConfigEntry(classOf[ThrowExceptionConfigurator].getName, methods = Seq(ReflectMethod(Constructor))),
+    ReflectConfigEntry(classOf[DiscardConfigurator].getName, methods = Seq(ReflectMethod(Constructor))),
+    // akka.persistence.no-snapshot-store.class
+    ReflectConfigEntry(classOf[NoSnapshotStore].getName, methods = Seq(ReflectMethod(Constructor))),
+    // akka.persistence.journal.inmem.class
+    ReflectConfigEntry(
+      classOf[InmemJournal].getName,
+      methods = Seq(ReflectMethod(Constructor, parameterTypes = Seq(classOf[Config].getName)))),
+    // akka.persistence.snapshot-store.local.class
+    ReflectConfigEntry(
+      classOf[LocalSnapshotStore].getName,
+      methods = Seq(ReflectMethod(Constructor, parameterTypes = Seq(classOf[Config].getName)))),
+    // akka.persistence.journal.leveldb.class
+    ReflectConfigEntry(
+      classOf[LeveldbStore].getName,
+      methods = Seq(ReflectMethod(Constructor, parameterTypes = Seq(classOf[Config].getName)))),
+    // akka.persistence.journal.leveldb-shared.class
+    ReflectConfigEntry(
+      classOf[SharedLeveldbJournal].getName,
+      methods = Seq(ReflectMethod(Constructor, parameterTypes = Seq(classOf[Config].getName)))),
+    // akka.persistence.journal.proxy.class and akka.persistence.snapshot-store.proxy.class
+    ReflectConfigEntry(
+      classOf[PersistencePluginProxy].getName,
+      methods = Seq(ReflectMethod(Constructor, parameterTypes = Seq(classOf[Config].getName)))))
 
   val modulePackages = Seq("akka.persistence")
 
