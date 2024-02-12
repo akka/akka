505b7b51d511b53af88c69df4085af707eac88f3
Johan Andrén <johan@markatta.com>
2024-02-12 15:21:45 +0100

    akka-serialization-jackson

--- akka-serialization-jackson/src/test/scala/akka/serialization/jackson/NativeImageMetadataSpec.scala
+++ akka-serialization-jackson/src/test/scala/akka/serialization/jackson/NativeImageMetadataSpec.scala
@@ -5,14 +5,31 @@
 package akka.serialization.jackson
 
 import akka.testkit.NativeImageUtils
+import akka.testkit.NativeImageUtils.Constructor
+import akka.testkit.NativeImageUtils.ReflectConfigEntry
+import akka.testkit.NativeImageUtils.ReflectMethod
+import com.typesafe.config.ConfigFactory
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+
+import scala.jdk.CollectionConverters.CollectionHasAsScala
 
 object NativeImageMetadataSpec {
 
   val metadataDir = NativeImageUtils.metadataDirFor("akka-serialization-jackson")
 
-  val additionalEntries = Seq()
+  def additionalEntries = {
+    val config = ConfigFactory.load()
+    val jacksonModules = config
+      .getStringList("akka.serialization.jackson.jackson-modules")
+      .asScala
+      .map { className =>
+        ReflectConfigEntry(className, methods = Seq(ReflectMethod(Constructor)))
+      }
+      .toVector
+
+    jacksonModules
+  }
 
   val modulePackages = Seq("akka.serialization.jackson")
 
