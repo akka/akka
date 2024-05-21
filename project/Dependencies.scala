96d06fd4f0a150f4a68eeb065d673f960d9fa351
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-05-21 17:19:25 +0200

    bump: HdrHistogram 2.2.1 (was 2.1.12) (#32427)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -131,7 +131,7 @@ object Dependencies {
     val metrics = "io.dropwizard.metrics" % "metrics-core" % "4.2.25" % Test // ApacheV2
     val metricsJvm = "io.dropwizard.metrics" % "metrics-jvm" % "4.2.25" % Test // ApacheV2
     val latencyUtils = "org.latencyutils" % "LatencyUtils" % "2.0.3" % Test // Free BSD
-    val hdrHistogram = "org.hdrhistogram" % "HdrHistogram" % "2.1.12" % Test // CC0
+    val hdrHistogram = "org.hdrhistogram" % "HdrHistogram" % "2.2.1" % Test // CC0
     val metricsAll = Seq(metrics, metricsJvm, latencyUtils, hdrHistogram)
 
     // sigar logging
