c81e167c2664dfb730be4e9e57879752f0610a78
Scala Steward <scala_steward@virtuslab.com>
2024-06-24 10:07:51 +0000

    bump: jctools-core 4.0.5 (was 3.3.0)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -62,7 +62,7 @@ object Dependencies {
 
     val sigar = "org.fusesource" % "sigar" % "1.6.4" // ApacheV2
 
-    val jctools = "org.jctools" % "jctools-core" % "3.3.0" // ApacheV2
+    val jctools = "org.jctools" % "jctools-core" % "4.0.5" // ApacheV2
 
     // reactive streams
     val reactiveStreams = "org.reactivestreams" % "reactive-streams" % reactiveStreamsVersion // MIT-0
