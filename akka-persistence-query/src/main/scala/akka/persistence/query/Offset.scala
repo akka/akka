2c08adc92a886c40b329731ffd8a7476ce434c4b
Peter Vlugter <59895+pvlugter@users.noreply.github.com>
2024-08-15 14:08:13 +1200

    feat: add timestamp offset by slice for persistence queries

--- akka-persistence-query/src/main/scala/akka/persistence/query/Offset.scala
+++ akka-persistence-query/src/main/scala/akka/persistence/query/Offset.scala
@@ -118,6 +118,61 @@ final class TimestampOffset private (val timestamp: Instant, val readTimestamp: 
 }
 
 /**
+ * Timestamp-based offset by slice.
+ *
+ * API May Change
+ */
+object TimestampOffsetBySlice {
+  val empty: TimestampOffsetBySlice = new TimestampOffsetBySlice(Map.empty)
+
+  def apply(offsets: Map[Int, TimestampOffset]): TimestampOffsetBySlice =
+    new TimestampOffsetBySlice(offsets)
+
+  /** Java API */
+  def create(offsets: java.util.Map[java.lang.Integer, TimestampOffset]): TimestampOffsetBySlice = {
+    import akka.util.ccompat.JavaConverters._
+    new TimestampOffsetBySlice(offsets.asScala.toMap.map { case (slice, offset) => slice.intValue -> offset })
+  }
+
+  def unapply(timestampOffsetBySlice: TimestampOffsetBySlice): Option[Map[Int, TimestampOffset]] =
+    Some(timestampOffsetBySlice.offsets)
+}
+
+/**
+ * Timestamp-based offset by slice.
+ *
+ * API May Change
+ *
+ * @param offsets
+ *   Map of TimestampOffset by slice
+ */
+final class TimestampOffsetBySlice private (val offsets: Map[Int, TimestampOffset]) extends Offset {
+
+  /** Java API */
+  def getOffsets(): java.util.Map[java.lang.Integer, TimestampOffset] = {
+    import akka.util.ccompat.JavaConverters._
+    offsets.map { case (slice, offset) => java.lang.Integer.valueOf(slice) -> offset }.asJava
+  }
+
+  def offset(slice: Int): Option[TimestampOffset] = offsets.get(slice)
+
+  /** Java API */
+  def getOffset(slice: Int): java.util.Optional[TimestampOffset] = {
+    import scala.jdk.OptionConverters._
+    offsets.get(slice).toJava
+  }
+
+  override def hashCode(): Int = offsets.hashCode()
+
+  override def equals(other: Any): Boolean = other match {
+    case that: TimestampOffsetBySlice => offsets == that.offsets
+    case _                            => false
+  }
+
+  override def toString = s"TimestampOffsetBySlice($offsets)"
+}
+
+/**
  * Used when retrieving all events.
  */
 case object NoOffset extends Offset {
