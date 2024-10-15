6267650f0d6e3c67165265da16d869c6f919fcfd
Sebastian <sebastian.alfers@gmail.com>
2024-10-15 11:00:58 +0200

    chore: bump copyright to 2024

--- akka-cluster-sharding/src/test/scala/akka/cluster/sharding/ShardingQueriesSpec.scala
+++ akka-cluster-sharding/src/test/scala/akka/cluster/sharding/ShardingQueriesSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2023 Lightbend Inc. <https://www.lightbend.com>
+ * Copyright (C) 2019-2024 Lightbend Inc. <https://www.lightbend.com>
  */
 
 package akka.cluster.sharding
