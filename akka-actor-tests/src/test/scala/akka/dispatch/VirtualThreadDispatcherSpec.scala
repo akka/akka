01938d35236a0461ad3fbaae3922442817594150
Johan Andrén <johan@markatta.com>
2025-04-02 10:53:34 +0200

    some more test coverage

--- akka-actor-tests/src/test/scala/akka/dispatch/VirtualThreadDispatcherSpec.scala
+++ akka-actor-tests/src/test/scala/akka/dispatch/VirtualThreadDispatcherSpec.scala
@@ -4,14 +4,40 @@
 
 package akka.dispatch
 
+import akka.actor.Actor
 import akka.actor.ActorSystem
+import akka.actor.Props
 import akka.testkit.TestKit
 import akka.testkit.TestProbe
 import akka.util.JavaVersion
 import com.typesafe.config.ConfigFactory
+import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class VirtualThreadDispatcherSpec extends AnyWordSpec {
+object VirtualThreadDispatcherSpec {
+  final case class ThreadInfo(virtual: Boolean, name: String)
+
+  object ThreadInfoActor {
+    def props() = Props(new ThreadInfoActor)
+  }
+  private class ThreadInfoActor extends Actor {
+    override def receive: Receive = {
+      case "give-me-info" =>
+        sender() ! reflectiveVirtualThreadInfo()
+    }
+  }
+
+  private def reflectiveVirtualThreadInfo(): ThreadInfo = {
+    val thread = Thread.currentThread()
+    // can't use methods directly or test won't compile on jdk < 21
+    val isVirtualMethod = thread.getClass.getMethod("isVirtual")
+    val isVirtual = isVirtualMethod.invoke(thread).asInstanceOf[Boolean]
+    ThreadInfo(isVirtual, thread.getName)
+  }
+}
+
+class VirtualThreadDispatcherSpec extends AnyWordSpec with Matchers {
+  import VirtualThreadDispatcherSpec._
 
   "The virtual thread support" should {
 
@@ -31,19 +57,44 @@ class VirtualThreadDispatcherSpec extends AnyWordSpec {
 
         try {
           val vtDispatcher = system.dispatchers.lookup("my-vt-dispatcher")
+          vtDispatcher shouldBe a[BatchingExecutor]
+
           val threadIsVirtualProbe = TestProbe()(system)
           vtDispatcher.execute(() => {
-            val thread = Thread.currentThread()
-            // can't use methods directly or test won't compile on jdk < 21
-            val isVirtualMethod = thread.getClass.getMethod("isVirtual")
-            val result = isVirtualMethod.invoke(thread).asInstanceOf[Boolean]
-            threadIsVirtualProbe.ref ! result
+            threadIsVirtualProbe.ref ! reflectiveVirtualThreadInfo()
           })
-          threadIsVirtualProbe.expectMsg(true)
+          val info = threadIsVirtualProbe.expectMsgType[ThreadInfo]
+          info.virtual shouldBe true
+          info.name should endWith("my-vt-dispatcher")
         } finally {
-          if (system != null) TestKit.shutdownActorSystem(system)
+          TestKit.shutdownActorSystem(system)
         }
 
+      }
+    }
+
+    "can be used as default dispatcher" in {
+      if (JavaVersion.majorVersion < 21) {
+        // loom not available yet here
+        pending
+      } else {
+        // not necessarily a good idea because of the virtual thread per task overhead, but to know it works
+        // and to cover running actors on it
+        implicit val system = ActorSystem(
+          classOf[VirtualThreadDispatcherSpec].getSimpleName,
+          ConfigFactory.parseString("""
+              akka.actor.default-dispatcher.executor="virtual-thread-executor"
+            """).withFallback(ConfigFactory.load()))
+        try {
+          val echo = system.actorOf(ThreadInfoActor.props())
+          val responseProbe = TestProbe()
+          echo.tell("give-me-info", responseProbe.ref)
+          val info = responseProbe.expectMsgType[ThreadInfo]
+          info.virtual shouldBe true
+          info.name should endWith("akka.actor.default-dispatcher")
+        } finally {
+          TestKit.shutdownActorSystem(system)
+        }
       }
     }
   }
