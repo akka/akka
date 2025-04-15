a4ecd419cca809f82eccf3cd2377af634cd3c4d9
Patrik Nordwall <patrik.nordwall@gmail.com>
2025-04-15 13:57:52 +0200

    chore: MiMa latest 2.10.4 (#32703)

--- project/MiMa.scala
+++ project/MiMa.scala
@@ -22,7 +22,7 @@ object MiMa extends AutoPlugin {
   private val firstPatchOf29 = 0
   private val latestPatchOf29 = 8
   private val firstPatchOf210 = 0
-  private val latestPatchOf210 = 3
+  private val latestPatchOf210 = 4
 
   override def requires = MimaPlugin
   override def trigger = allRequirements
