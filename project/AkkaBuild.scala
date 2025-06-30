f9371064f872ccc7cbc22bd3091a9973390b2166
Sebastian Alfers <sebastian.alfers@lightbend.com>
2025-06-30 09:20:57 +0200

    chore: custom runner and allowlisted resolver (#32755)

--- project/AkkaBuild.scala
+++ project/AkkaBuild.scala
@@ -81,6 +81,7 @@ object AkkaBuild {
     }
 
   lazy val resolverSettings = Def.settings(
+    resolvers += "Akka library repository".at("https://repo.akka.io/maven/github_actions"),
     // should we be allowed to use artifacts published to the local maven repository
     if (System.getProperty("akka.build.useLocalMavenResolver", "false").toBoolean)
       resolvers += mavenLocalResolver
