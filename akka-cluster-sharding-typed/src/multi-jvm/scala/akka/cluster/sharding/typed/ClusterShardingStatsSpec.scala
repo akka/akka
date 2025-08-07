998892bc2845dedeaec84aa52e97d97d04675f40
Patrik Nordwall <patrik.nordwall@gmail.com>
2025-08-07 10:53:19 +0200

    fix: Java Integer in ShardRegionStats
    * Was Map of Scala Int, which is not usable
    * Might look like an incompatible change, but since it could not have been used
      as it was it should be fine to change

--- akka-cluster-sharding-typed/src/multi-jvm/scala/akka/cluster/sharding/typed/ClusterShardingStatsSpec.scala
+++ akka-cluster-sharding-typed/src/multi-jvm/scala/akka/cluster/sharding/typed/ClusterShardingStatsSpec.scala
@@ -4,6 +4,8 @@
 
 package akka.cluster.sharding.typed
 
+import java.util.stream.Collectors
+
 import com.typesafe.config.ConfigFactory
 import org.scalatest.concurrent.ScalaFutures
 
@@ -94,6 +96,14 @@ abstract class ClusterShardingStatsSpec
       stats.regions.size shouldEqual 3 // 3 nodes
       stats.regions.values.flatMap(_.stats.values).sum shouldEqual 2 // number of entities
       stats.regions.values.forall(_.failed.isEmpty) shouldBe true
+
+      // javadsl
+      stats
+        .getRegions()
+        .values()
+        .stream()
+        .flatMap(_.getStats().values().stream())
+        .collect(Collectors.summingInt((i: Integer) => i.intValue())) shouldEqual 2
 
       enterBarrier("done")
     }
