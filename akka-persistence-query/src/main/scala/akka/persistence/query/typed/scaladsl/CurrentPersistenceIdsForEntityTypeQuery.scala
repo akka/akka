e9c202c86f669bbf66c68ffc9029e44c92564035
Johan Andrén <johan@markatta.com>
2025-09-11 10:49:15 +0200

    feat: new query api for listing persistence id per entity type (#32793)
    * feat: new query api for listing persistence id per entity type
    
    * headers/formattin

---
+++ akka-persistence-query/src/main/scala/akka/persistence/query/typed/scaladsl/CurrentPersistenceIdsForEntityTypeQuery.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2009-2025 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.persistence.query.typed.scaladsl
+
+import akka.NotUsed
+import akka.stream.scaladsl.Source
+
+/**
+ * A plugin may optionally support this query by implementing this trait.
+ */
+trait CurrentPersistenceIdsForEntityTypeQuery {
+
+  /**
+   * Get the current persistence ids for a given entity type.
+   *
+   * The persistenceIds must start with the entity type followed by default separator ("|") from
+   * [[akka.persistence.typed.PersistenceId]].
+   *
+   * @param entityType
+   *   The entity type name.
+   * @param afterId
+   *   The ID to start returning results from, or empty to return all ids. This should be an id returned from a previous
+   *   invocation of this command. Callers should not assume that ids are returned in sorted order.
+   * @param limit
+   *   The maximum results to return. Use Long.MAX_VALUE to return all results. Must be greater than zero.
+   * @return
+   *   A source containing all the persistence ids, limited as specified.
+   */
+  def currentPersistenceIds(entityType: String, afterId: Option[String], limit: Long): Source[String, NotUsed]
+}
